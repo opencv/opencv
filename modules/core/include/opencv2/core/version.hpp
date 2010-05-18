b34aaace79f791700b659ea970f163a57aaadc4e
Vadim Pisarevsky <no@email>
2010-05-18 16:42:35 +0000

    updated OpenCV version because of the recent reorganization

--- modules/core/include/opencv2/core/version.hpp
+++ modules/core/include/opencv2/core/version.hpp
@@ -49,7 +49,7 @@
 
 #define CV_MAJOR_VERSION    2
 #define CV_MINOR_VERSION    1
-#define CV_SUBMINOR_VERSION 0
+#define CV_SUBMINOR_VERSION 1
 
 #define CVAUX_STR_EXP(__A)  #__A
 #define CVAUX_STR(__A)      CVAUX_STR_EXP(__A)
