8deebea7354726f0744d68ede1adfb4c51547034
Vadim Pisarevsky <no@email>
2010-10-21 12:12:04 +0000

    fixed test failure in matrix-dotproduct test (by increasing the desired accuracy threshold) (ticket #447)

--- tests/cxcore/src/amath.cpp
+++ tests/cxcore/src/amath.cpp
@@ -1048,7 +1048,7 @@ double CxCore_MatrixTestImpl::get_success_error_level( int test_case_idx, int i,
 {
     int input_depth = CV_MAT_DEPTH(cvGetElemType( test_array[INPUT][0] ));
     double input_precision = input_depth < CV_32F ? 0 : input_depth == CV_32F ?
-                            5e-5 : 1e-10;
+                            5e-5 : 5e-10;
     double output_precision = CvArrTest::get_success_error_level( test_case_idx, i, j );
     return MAX(input_precision, output_precision);
 }
