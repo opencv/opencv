dd3c62a4fecc9a378dd16159d5451c8a6a0e08b3
Alexander Shishkov <no@email>
2010-11-23 16:19:08 +0000

    moved failed tests to blacklists: mhi-global                   //ticket 457 canny                          //ticket 702

--- tests/cv/src/tsysa.cpp
+++ tests/cv/src/tsysa.cpp
@@ -48,8 +48,8 @@ const char* blacklist[] =
     "calibrate-camera-artificial",    //ticket 472
     "inpaint",                        //ticket 570
     "warp-resize",                    //ticket 429
-    //"filter-generic",                 //ticket 690
-    //"mhi-global",                     //ticket 457
+    "mhi-global",                     //ticket 457
+    "canny",                          //ticket 702
     0
 };
 
