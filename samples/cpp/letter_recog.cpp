894cde3f407d7a91ba6a09abf5198d857a94df87
Maria Dimashova <no@email>
2010-11-29 09:31:47 +0000

    fixed compile errors from gcc

--- samples/cpp/letter_recog.cpp
+++ samples/cpp/letter_recog.cpp
@@ -1,6 +1,7 @@
 #include "opencv2/core/core_c.h"
 #include "opencv2/ml/ml.hpp"
 
+#include <cstdio>
 /*
 The sample demonstrates how to train Random Trees classifier
 (or Boosting classifier, or MLP - see main()) using the provided dataset.
--- samples/cpp/watershed.cpp
+++ samples/cpp/watershed.cpp
@@ -1,6 +1,7 @@
 #include "opencv2/imgproc/imgproc.hpp"
 #include "opencv2/highgui/highgui.hpp"
 
+#include <cstdio>
 #include <iostream>
 
 using namespace cv;
