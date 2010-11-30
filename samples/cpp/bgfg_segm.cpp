84cd5096399db8f794db91cccd8d6f2cfaa6cdd9
Gary Bradski <no@email>
2010-11-30 01:26:29 +0000

    described

--- samples/cpp/bgfg_segm.cpp
+++ samples/cpp/bgfg_segm.cpp
@@ -2,6 +2,14 @@
 #include <opencv2/highgui/highgui.hpp>
 
 #include <stdio.h>
+void help()
+{
+ printf("\nDo background segmentation, especially demonstrating the use of cvUpdateBGStatModel.\n"
+"Learns the background at the start and then segments.\n"
+"Learning is togged by the space key. Will read from file or camera\n"
+"Call:\n"
+"./  bgfg_segm [file name -- if no name, read from camera]\n\n");
+}
 
 //this is a sample for foreground detection functions
 int main(int argc, char** argv)
@@ -14,6 +22,7 @@ int main(int argc, char** argv)
         cap = cvCaptureFromCAM(0);
     else
         cap = cvCaptureFromFile(argv[1]);
+    help();
     
     if( !cap )
     {
@@ -52,7 +61,13 @@ int main(int argc, char** argv)
         char k = cvWaitKey(5);
         if( k == 27 ) break;
         if( k == ' ' )
+        {
             update_bg_model = !update_bg_model;
+            if(update_bg_model)
+            	printf("Background update is on\n");
+            else
+            	printf("Background update is off\n");
+        }
     }
 
 
