478930649929eb00a031667bcaadd5d42258cfc8
Gary Bradski <no@email>
2010-12-01 20:04:57 +0000

    put in a remark

--- samples/c/facedetect.cmd
+++ samples/c/facedetect.cmd
@@ -1,1 +1,2 @@
+REM an example of using haar cascade recognition for face and eye detection.
 facedetect --cascade="../../data/haarcascades/haarcascade_frontalface_alt.xml" --nested-cascade="../../data/haarcascades/haarcascade_eye.xml" --scale=1.3 %1
