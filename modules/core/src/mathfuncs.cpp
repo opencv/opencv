e6aba3e51c8a652f41f0f8885f72aee8263ae25e
Vadim Pisarevsky <no@email>
2010-12-06 08:49:49 +0000

    turn off SSE2 optimization in cv::exp & cv::log in the case of GCC <=4.1 (not no Mac, where a patched GCC 4.0 is used)

--- modules/core/src/mathfuncs.cpp
+++ modules/core/src/mathfuncs.cpp
@@ -773,7 +773,8 @@ static const double expTab[] = {
 
 
 // the code below uses _mm_cast* intrinsics, which are not avialable on VS2005
-#if defined _MSC_VER && _MSC_VER < 1500
+#if (defined _MSC_VER && _MSC_VER < 1500) || \
+    (!defined __APPLE__ && defined __GNUC__ && __GNUC__*100 + __GNUC_MINOR__ < 402)
 #undef CV_SSE2
 #define CV_SSE2 0
 #endif    
