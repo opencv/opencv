57f917d6f24644c4c1387fb156cbb13d83ce6376
Alexey Spizhevoy <no@email>
2011-01-11 10:15:46 +0000

    replaced adding constant in normalization with taking max

--- modules/gpu/src/cuda/match_template.cu
+++ modules/gpu/src/cuda/match_template.cu
@@ -560,7 +560,7 @@ __global__ void matchTemplatePreparedKernel_CCOFF_NORMED_8U(
                 (image_sqsum.ptr(y + h)[x + w] - image_sqsum.ptr(y)[x + w]) -
                 (image_sqsum.ptr(y + h)[x] - image_sqsum.ptr(y)[x]));
         result.ptr(y)[x] = min(1.f, (ccorr - image_sum_ * templ_sum_scale) * 
-                           rsqrtf(templ_sqsum_scale * (image_sqsum_ - weight * image_sum_ * image_sum_ + 1.f)));
+                                    rsqrtf(templ_sqsum_scale * max(1.f, image_sqsum_ - weight * image_sum_ * image_sum_)));
     }
 }
 
@@ -610,8 +610,8 @@ __global__ void matchTemplatePreparedKernel_CCOFF_NORMED_8UC2(
                 (image_sqsum_g.ptr(y + h)[x + w] - image_sqsum_g.ptr(y)[x + w]) -
                 (image_sqsum_g.ptr(y + h)[x] - image_sqsum_g.ptr(y)[x]));
         float ccorr = result.ptr(y)[x];
-        float rdenom = rsqrtf(templ_sqsum_scale * (image_sqsum_r_ - weight * image_sum_r_ * image_sum_r_ 
-                                                   + image_sqsum_g_ - weight * image_sum_g_ * image_sum_g_ + 1.f));
+        float rdenom = rsqrtf(templ_sqsum_scale * max(1.f, image_sqsum_r_ - weight * image_sum_r_ * image_sum_r_ 
+                                                           + image_sqsum_g_ - weight * image_sum_g_ * image_sum_g_));
         result.ptr(y)[x] = min(1.f, (ccorr - image_sum_r_ * templ_sum_scale_r
                                      - image_sum_g_ * templ_sum_scale_g) * rdenom);
     }
@@ -678,9 +678,9 @@ __global__ void matchTemplatePreparedKernel_CCOFF_NORMED_8UC3(
                 (image_sqsum_b.ptr(y + h)[x + w] - image_sqsum_b.ptr(y)[x + w]) -
                 (image_sqsum_b.ptr(y + h)[x] - image_sqsum_b.ptr(y)[x]));
         float ccorr = result.ptr(y)[x];
-        float rdenom = rsqrtf(templ_sqsum_scale * (image_sqsum_r_ - weight * image_sum_r_ * image_sum_r_ 
-                                                   + image_sqsum_g_ - weight * image_sum_g_ * image_sum_g_
-                                                   + image_sqsum_b_ - weight * image_sum_b_ * image_sum_b_ + 1.f));
+        float rdenom = rsqrtf(templ_sqsum_scale * max(1.f, image_sqsum_r_ - weight * image_sum_r_ * image_sum_r_ 
+                                                           + image_sqsum_g_ - weight * image_sum_g_ * image_sum_g_
+                                                           + image_sqsum_b_ - weight * image_sum_b_ * image_sum_b_));
         result.ptr(y)[x] = min(1.f, (ccorr - image_sum_r_ * templ_sum_scale_r
                                      - image_sum_g_ * templ_sum_scale_g
                                      - image_sum_b_ * templ_sum_scale_b) * rdenom);
@@ -760,10 +760,10 @@ __global__ void matchTemplatePreparedKernel_CCOFF_NORMED_8UC4(
                 (image_sqsum_a.ptr(y + h)[x + w] - image_sqsum_a.ptr(y)[x + w]) -
                 (image_sqsum_a.ptr(y + h)[x] - image_sqsum_a.ptr(y)[x]));
         float ccorr = result.ptr(y)[x];
-        float rdenom = rsqrtf(templ_sqsum_scale * (image_sqsum_r_ - weight * image_sum_r_ * image_sum_r_
-                                                   + image_sqsum_g_ - weight * image_sum_g_ * image_sum_g_
-                                                   + image_sqsum_b_ - weight * image_sum_b_ * image_sum_b_
-                                                   + image_sqsum_a_ - weight * image_sum_a_ * image_sum_a_ + 1.f));
+        float rdenom = rsqrtf(templ_sqsum_scale * max(1.f, image_sqsum_r_ - weight * image_sum_r_ * image_sum_r_
+                                                           + image_sqsum_g_ - weight * image_sum_g_ * image_sum_g_
+                                                           + image_sqsum_b_ - weight * image_sum_b_ * image_sum_b_
+                                                           + image_sqsum_a_ - weight * image_sum_a_ * image_sum_a_));
         result.ptr(y)[x] = min(1.f, (ccorr - image_sum_r_ * templ_sum_scale_r
                                      - image_sum_g_ * templ_sum_scale_g
                                      - image_sum_b_ * templ_sum_scale_b
@@ -822,7 +822,7 @@ __global__ void normalizeKernel_8U(
         float image_sqsum_ = (float)(
                 (image_sqsum.ptr(y + h)[(x + w) * cn] - image_sqsum.ptr(y)[(x + w) * cn]) -
                 (image_sqsum.ptr(y + h)[x * cn] - image_sqsum.ptr(y)[x * cn]));
-        result.ptr(y)[x] = min(1.f, result.ptr(y)[x] * rsqrtf((image_sqsum_ + 1.f) * templ_sqsum));
+        result.ptr(y)[x] = min(1.f, result.ptr(y)[x] * rsqrtf(max(1.f, image_sqsum_) * templ_sqsum));
     }
 }
 
