be4251c143d17905c9580ade1572ab6c3ea091ef
Vadim Pisarevsky <no@email>
2011-01-11 12:14:31 +0000

    fixed compile problems when Makefiles are used

--- modules/core/test/test_arithm.cpp
+++ modules/core/test/test_arithm.cpp
@@ -1,4 +1,4 @@
-#include "precomp.hpp"
+#include "test_precomp.hpp"
 #include <iostream>
 
 using namespace cv;
--- modules/core/test/test_main.cpp
+++ modules/core/test/test_main.cpp
@@ -1,2 +1,2 @@
-#include "precomp.hpp"
+#include "test_precomp.hpp"
 #include "opencv2/gtest/gtest_main.hpp"
