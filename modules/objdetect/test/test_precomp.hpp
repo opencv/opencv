51cee84123ac6e54b3ab0622eda340e8f4aba6ab
Alexey Spizhevoy <no@email>
2011-02-16 11:38:58 +0000

    created GPU new-style test project, ported StereoBP test

--- modules/objdetect/test/test_precomp.hpp
+++ modules/objdetect/test/test_precomp.hpp
@@ -3,8 +3,7 @@
 
 #include "opencv2/ts/ts.hpp"
 #include "opencv2/objdetect/objdetect.hpp"
-#include "opencv2/imgproc/imgproc_c.h"
+#include "opencv2/imgproc/imgproc.hpp"
 #include "opencv2/highgui/highgui.hpp"
-#include <iostream>
 
 #endif
