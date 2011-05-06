ae8f2eeac2a6b1c11f9e1d9a3cfb0dbed134050b
Vadim Pisarevsky <no@email>
2011-05-06 11:51:39 +0000

    restored img_proc_test main() call; removed windows.h from ml.hpp

--- modules/ml/include/opencv2/ml/ml.hpp
+++ modules/ml/include/opencv2/ml/ml.hpp
@@ -41,70 +41,8 @@
 #ifndef __OPENCV_ML_HPP__
 #define __OPENCV_ML_HPP__
 
-// disable deprecation warning which appears in VisualStudio 8.0
-#if _MSC_VER >= 1400
-#pragma warning( disable : 4996 )
-#endif
-
-#ifndef SKIP_INCLUDES
-
-  #include "opencv2/core/core.hpp"
-  #include <limits.h>
-
-  #if defined WIN32 || defined _WIN32
-    #include <windows.h>
-  #endif
-
-#else // SKIP_INCLUDES
-
-  #if defined WIN32 || defined _WIN32
-    #define CV_CDECL __cdecl
-    #define CV_STDCALL __stdcall
-  #else
-    #define CV_CDECL
-    #define CV_STDCALL
-  #endif
-
-  #ifndef CV_EXTERN_C
-    #ifdef __cplusplus
-      #define CV_EXTERN_C extern "C"
-      #define CV_DEFAULT(val) = val
-    #else
-      #define CV_EXTERN_C
-      #define CV_DEFAULT(val)
-    #endif
-  #endif
-
-  #ifndef CV_EXTERN_C_FUNCPTR
-    #ifdef __cplusplus
-      #define CV_EXTERN_C_FUNCPTR(x) extern "C" { typedef x; }
-    #else
-      #define CV_EXTERN_C_FUNCPTR(x) typedef x
-    #endif
-  #endif
-
-  #ifndef CV_INLINE
-    #if defined __cplusplus
-      #define CV_INLINE inline
-    #elif (defined WIN32 || defined _WIN32) && !defined __GNUC__
-      #define CV_INLINE __inline
-    #else
-      #define CV_INLINE static
-    #endif
-  #endif /* CV_INLINE */
-
-  #if (defined WIN32 || defined _WIN32) && defined CVAPI_EXPORTS
-    #define CV_EXPORTS __declspec(dllexport)
-  #else
-    #define CV_EXPORTS
-  #endif
-
-  #ifndef CVAPI
-    #define CVAPI(rettype) CV_EXTERN_C CV_EXPORTS rettype CV_CDECL
-  #endif
-
-#endif // SKIP_INCLUDES
-
+#include "opencv2/core/core.hpp"
+#include <limits.h>
 
 #ifdef __cplusplus
 
