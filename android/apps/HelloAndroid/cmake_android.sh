8a47b3d5d58a46d70ce18ab1d67cec89095e9f80
Kirill Kornyakov <no@email>
2011-05-18 12:48:08 +0000

    fixed OpenCV_DIR

--- android/apps/HelloAndroid/cmake_android.sh
+++ android/apps/HelloAndroid/cmake_android.sh
@@ -3,10 +3,11 @@ cd `dirname $0`
 
 BUILD_DIR=build_armeabi
 opencv_android=`pwd`/../..
-opencv_build_dir=$opencv_android/$BUILD_DIR
+opencv_build_dir=$opencv_android/build
 
 mkdir -p $BUILD_DIR
 cd $BUILD_DIR
 
-cmake -DOpenCV_DIR=$opencv_build_dir -DARM_TARGET=armeabi -DCMAKE_TOOLCHAIN_FILE=$opencv_android/android.toolchain.cmake ..
-
+RUN_CMAKE="cmake -DOpenCV_DIR=$opencv_build_dir -DARM_TARGET=armeabi -DCMAKE_TOOLCHAIN_FILE=$opencv_android/android.toolchain.cmake .."
+echo $RUN_CMAKE
+$RUN_CMAKE
