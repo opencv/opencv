5ca3bd502cdcfdd0eea11de8d91c8c9261c01740
Vadim Pisarevsky <no@email>
2011-06-10 12:21:26 +0000

    reverted broken opencv_test_core launcher

--- modules/core/test/test_main.cpp
+++ modules/core/test/test_main.cpp
@@ -1,26 +1,3 @@
 #include "test_precomp.hpp"
 
-//CV_TEST_MAIN("cv")
-
-#include <iostream>
-#include <cxcore.hpp>
-
-using namespace cv;
-using namespace std;
-
-int main(int,char**)
-{
-    /*cv::Mat img3x3(3, 3, CV_8UC1);
-    cv::Mat img2x2 = img3x3(cv::Rect(0, 0, 2, 2));
-    
-    for(cv::MatIterator_<uchar> it = img3x3.begin<uchar>(); it != img3x3.end<uchar>(); ++it)
-        *it = 1;
-    
-    int sum = 0;
-    for(cv::MatConstIterator_<uchar> it = img2x2.begin<uchar>(); it != img2x2.end<uchar>(); ++it)
-        sum += *it;
-    std::cout << "sum = " << sum << " (should be 4)\n";*/
-    FileStorage fs("empty.yml", CV_STORAGE_READ);
-    cout << fs["a"].type() << endl;
-    return 0;
-}
+CV_TEST_MAIN("cv")
