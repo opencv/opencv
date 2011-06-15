35aa133d9aaf94a646cee6bca0a94186762c288c
Maria Dimashova <no@email>
2011-06-15 13:30:26 +0000

    minor formating

--- modules/features2d/src/descriptors.cpp
+++ modules/features2d/src/descriptors.cpp
@@ -395,9 +395,9 @@ void OpponentColorDescriptorExtractor::computeImpl( const Mat& bgrImage, vector<
     int mergedCount = 0;
     // cp - current channel position
     size_t cp[] = {0, 0, 0}; 
-    while( cp[0] < channelKeypoints[0].size() && 
-                                  cp[1] < channelKeypoints[1].size() && 
-                                  cp[2] < channelKeypoints[2].size() )
+    while( cp[0] < channelKeypoints[0].size() &&
+           cp[1] < channelKeypoints[1].size() &&
+           cp[2] < channelKeypoints[2].size() )
     {
         const int maxInitIdx = std::max( channelKeypoints[0][idxs[0][cp[0]]].class_id,
                                          std::max( channelKeypoints[1][idxs[1][cp[1]]].class_id,
