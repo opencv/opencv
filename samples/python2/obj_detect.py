4582226567b5b7d33dd4d6aa1584b8e814f3ca6e
Alexander Mordvintsev <no@email>
2011-06-17 16:17:10 +0000

    work on obj_detect.py sample (in progress...)

--- samples/python2/obj_detect.py
+++ samples/python2/obj_detect.py
@@ -3,17 +3,16 @@ import cv2, cv
 import common
 
 def detect(img, cascade):
-    min_size = (20, 20)
-    haar_scale = 1.1
-    min_neighbors = 3
-    haar_flags = 0
-    rects = cascade.detectMultiScale(img, haar_scale, min_neighbors, haar_flags, min_size)
+    rects = cascade.detectMultiScale(img, scaleFactor=1.1, minNeighbors=5, minSize=(30, 30))
     if len(rects) == 0:
-        return
+        return []
     rects[:,2:] += rects[:,:2]
     return rects
 
 def detect_turned(img, cascade):
+    img = cv2.cvtColor(img, cv.CV_BGR2GRAY)
+    img = cv2.equalizeHist(img)
+
     img_t = cv2.transpose(img)
     img_cw = cv2.flip(img_t, 1)
     img_ccw = cv2.flip(img_t, 0)
@@ -22,58 +21,68 @@ def detect_turned(img, cascade):
     r_ccw = detect(img_ccw, cascade)
 
     h, w = img.shape[:2]
-    if r_cw is not None:
-        r_cw[:,[0, 2]] = h - r_cw[:,[0, 2]] - 1
-        r_cw = r_cw[:,[1,0,3,2]]
-    if r_ccw is not None:
-        r_ccw[:,[1, 3]] = w - r_ccw[:,[1, 3]] - 1
-        r_ccw = r_ccw[:,[1,0,3,2]]
-    rects = np.vstack( [a for a in [r, r_cw, r_ccw] if a is not None] )
+    rects = []
+    rects += [(x1, y1, x2, y2, 1, 0) for x1, y1, x2, y2 in r]
+    rects += [(y1, h-x1-1, y2, h-x2-1, 0, -1) for x1, y1, x2, y2 in r_cw]
+    rects += [(w-y1-1, x1, w-y2-1, x2, 0,  1) for x1, y1, x2, y2 in r_ccw]
     return rects
 
-def process_image(fn, cascade):
-    pass
+def process_image(fn, cascade, extract_faces=True):
+    img = cv2.imread(fn)
+    h, w = img.shape[:2]
+    scale = max(h, w) / 512.0
+    small = cv2.resize(img, (int(w/scale), int(h/scale)), interpolation=cv2.INTER_AREA)
+    rects = detect_turned(small, cascade)
+
+    for i, (x1, y1, x2, y2, vx, vy) in enumerate(rects):
+        cv2.rectangle(small, (x1, y1), (x2, y2), (0, 255, 0))
+        cv2.circle(small, (x1, y1), 2, (0, 0, 255), -1)
+        cv2.putText(small, str(i), ((x1+x2)/2, (y1+y2)/2), cv2.FONT_HERSHEY_PLAIN, 1.0, (0, 255, 0))
+
+    rects = np.float32(rects).reshape(-1,6)
+    rects[:,:4] = np.around(rects[:,:4]*scale)
+
+    faces = []
+    if extract_faces:
+        path, name, ext = common.splitfn(fn)
+        face_sz = 256
+        for i, r in enumerate(rects):
+            p1, p2, u = r.reshape(3, 2)
+            v = np.float32( [-u[1], u[0]] )
+            w = np.abs(p2-p1).max()
+            fscale = w / face_sz
+            p0 = 0.5*(p1+p2 - w*(u+v))
+            M = np.float32([u*fscale, v*fscale, p0]).T
+            face = cv2.warpAffine(img, M, (face_sz, face_sz), flags=cv2.WARP_INVERSE_MAP | cv2.INTER_AREA)
+            faces.append(face)
+
+            cv2.imwrite('out/%s_%02d.bmp' % (name, i), face)
+        
+    return small, rects, faces
     
     
 
 if __name__ == '__main__':
     import sys
     import getopt
+    from glob import glob
+
     args, img_mask = getopt.getopt(sys.argv[1:], '', ['cascade='])
     args = dict(args)
+    # "../../data/haarcascades/haarcascade_frontalface_default.xml" #haarcascade_frontalface_default
     cascade_fn = args.get('--cascade', "../../data/haarcascades/haarcascade_frontalface_alt.xml")
 
     cascade = cv2.CascadeClassifier(cascade_fn)
 
-
-    img = cv2.imread('test.jpg')
-    h, w = img.shape[:2]
-    r = 512.0 / max(h, w)
-    small = cv2.resize(img, (int(w*r), int(h*r)), interpolation=cv2.INTER_AREA)
-    rects = detect_turned(small, cascade)
-    print rects
-    for x1, y1, x2, y2 in rects:
-        cv2.rectangle(small, (x1, y1), (x2, y2), (0, 255, 0))
-        cv2.circle(small, (x1, y1), 2, (0, 0, 255), -1)
-
-
-
-    cv2.imshow('img', small)
-    cv2.waitKey()
+    mask = 'D:/Dropbox/Photos/2011-06-12 aero/img_08[2-9]*.jpg'
+    for fn in glob(mask):
+        print fn
+        vis, rects, faces = process_image(fn, cascade)
+        cv2.imshow('img', vis)
+        cv2.waitKey(100)
 
     
-
-'''
-
-
-    img = cv2.imread('test.jpg')
-    h, w = img.shape[:2]
-
-
-    r = 512.0 / max(h, w)
-    small = cv2.resize(img, (w*r, h*r), interpolation=cv2.INTER_AREA)
-
-cv2.imshow('img', small)
-cv2.waitKey()
-
-'''
+    #vis, rects = process_image('test.jpg', cascade)
+    #print rects
+    #cv2.imshow('img', vis)
+    cv2.waitKey()
