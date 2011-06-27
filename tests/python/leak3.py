25887e693be7eb3b59658d5cb3cdb03b0abe310d
Vadim Pisarevsky <no@email>
2011-06-27 19:57:56 +0000

    removed the obsolete tests from the trunk as well.

--- tests/python/leak3.py
+++
@@ -1,6 +0,0 @@
-import cv
-import math
-import time
-
-while True:
-    h = cv.CreateHist([40], cv.CV_HIST_ARRAY, [[0,255]], 1)
--- tests/test_cxcore.sh
+++
@@ -1,6 +0,0 @@
-#! /bin/sh
-if [ -n "${TESTDATA_DIR}" ] ; then
-  ./opencv_test_core -d $TESTDATA_DIR/cxcore
-else
-  ./opencv_test_core -d $srcdir/../../opencv_extra/testdata/cxcore
-fi
