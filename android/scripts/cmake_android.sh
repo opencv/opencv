092699783ad3e0fd5b1edff9af22e8816567a331
Andrey Kamaev <no@email>
2011-06-30 13:37:56 +0000

    Added cmake script to build Android samples

--- android/scripts/cmake_android.sh
+++ android/scripts/cmake_android.sh
@@ -4,5 +4,5 @@ cd `dirname $0`/..
 mkdir -p build
 cd build
 
-cmake -C ../CMakeCache.android.initial.cmake -DCMAKE_TOOLCHAIN_FILE=../android.toolchain.cmake ../..
+cmake -C ../CMakeCache.android.initial.cmake -DCMAKE_TOOLCHAIN_FILE=../android.toolchain.cmake $@ ../..
 
