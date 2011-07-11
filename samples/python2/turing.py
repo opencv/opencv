2a19db61e72eef68d7ba4a297091cc94f8dc0f73
Alexander Mordvintsev <no@email>
2011-07-11 08:06:51 +0000

    command line option for avi output in turing.py

--- samples/python2/turing.py
+++ samples/python2/turing.py
@@ -6,8 +6,25 @@ Inspired by http://www.jonathanmccabe.com/Cyclic_Symmetric_Multi-Scale_Turing_Pa
 import numpy as np
 import cv2, cv
 from common import draw_str
+import getopt, sys
+from itertools import count
+
+help_message = '''
+USAGE: turing.py [-o <output.avi>]
+
+Press ESC to stop.
+'''
+print help_message
 
 w, h = 512, 512
+
+args, args_list = getopt.getopt(sys.argv[1:], 'o:', [])
+args = dict(args)
+out = None
+if '-o' in args:
+    fn = args['-o']
+    out = cv2.VideoWriter(args['-o'], cv.CV_FOURCC(*'DIB '), 30.0, (w, h), False)
+    print 'writing %s ...' % fn
 
 a = np.zeros((h, w), np.float32)
 cv2.randu(a, np.array([0]), np.array([1]))
@@ -19,12 +36,8 @@ def process_scale(a_lods, lod):
     v = cv2.GaussianBlur(d*d, (3, 3), 0)
     return np.sign(d), v
     
-print 'Generating AVI file. Press ESC to stop.'
-out = cv2.VideoWriter('turing.avi', cv.CV_FOURCC(*'DIB '), 30.0, (w, h), False)
-
 scale_num = 6
-frame_num = 1000
-for frame_i in xrange(frame_num):
+for frame_i in count():
     a_lods = [a]
     for i in xrange(scale_num):
         a_lods.append(cv2.pyrDown(a_lods[-1])) 
@@ -37,12 +50,10 @@ for frame_i in xrange(frame_num):
     a += np.choose(mi, ms) * 0.025
     a = (a-a.min()) / a.ptp()
 
-    out.write(a)
+    if out:
+        out.write(a)
     vis = a.copy()
-    draw_str(vis, (20, 20), 'frame %d / %d' % (frame_i+1, frame_num))
+    draw_str(vis, (20, 20), 'frame %d' % frame_i)
     cv2.imshow('a', vis)
     if cv2.waitKey(5) == 27:
         break
-else:
-    print 'done'
-    cv2.waitKey()
