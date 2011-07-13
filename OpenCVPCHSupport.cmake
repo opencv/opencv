4d8a261d994920d4caa85270983addefecad2a55
Andrey Kamaev <no@email>
2011-07-13 15:47:04 +0000

    Allowed spaces in path to OpenCV (tested only Android build)

--- OpenCVPCHSupport.cmake
+++ OpenCVPCHSupport.cmake
@@ -129,9 +129,9 @@ MACRO(_PCH_GET_TARGET_COMPILE_FLAGS _cflags  _header_name _pch_path _dowarn )
     # if you have different versions of the headers for different build types
     # you may set _pch_dowarn
     IF (_dowarn)
-      SET(${_cflags} "${PCH_ADDITIONAL_COMPILER_FLAGS} -include ${CMAKE_CURRENT_BINARY_DIR}/${_header_name} -Winvalid-pch " )
+      SET(${_cflags} "${PCH_ADDITIONAL_COMPILER_FLAGS} -include \"${CMAKE_CURRENT_BINARY_DIR}/${_header_name}\" -Winvalid-pch " )
     ELSE (_dowarn)
-      SET(${_cflags} "${PCH_ADDITIONAL_COMPILER_FLAGS} -include ${CMAKE_CURRENT_BINARY_DIR}/${_header_name} " )
+      SET(${_cflags} "${PCH_ADDITIONAL_COMPILER_FLAGS} -include \"${CMAKE_CURRENT_BINARY_DIR}/${_header_name}\" " )
     ENDIF (_dowarn)
   ELSE(CMAKE_COMPILER_IS_GNUCXX)
 
