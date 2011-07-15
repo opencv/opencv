39dab5e0cf9f021e972f15b64ef9a874deb522ae
Andrey Kamaev <no@email>
2011-07-15 15:26:29 +0000

    Headers are included into opencv_java build

--- android/scripts/package.sh
+++ android/scripts/package.sh
@@ -10,19 +10,18 @@ cd package
 PRG_DIR=`pwd`
 mkdir opencv
 
-
 # neon-enabled build
-cd $PRG_DIR
-mkdir build-neon
-cd build-neon
+#cd $PRG_DIR
+#mkdir build-neon
+#cd build-neon
 
-cmake -C "$ANDROID_DIR/CMakeCache.android.initial.cmake" -DARM_TARGET="armeabi-v7a with NEON" -DBUILD_DOCS=OFF -DBUILD_TESTS=OFF -DBUILD_EXAMPLES=OFF -DBUILD_ANDROID_EXAMPLES=OFF -DCMAKE_TOOLCHAIN_FILE="$ANDROID_DIR/android.toolchain.cmake" -DCMAKE_INSTALL_PREFIX="$PRG_DIR/opencv" "$ANDROID_DIR/.."  || exit 1
-make -j8 install/strip || exit 1
+#cmake -C "$ANDROID_DIR/CMakeCache.android.initial.cmake" -DARM_TARGET="armeabi-v7a with NEON" -DBUILD_DOCS=OFF -DBUILD_TESTS=OFF -DBUILD_EXAMPLES=OFF -DBUILD_ANDROID_EXAMPLES=OFF -DCMAKE_TOOLCHAIN_FILE="$ANDROID_DIR/android.toolchain.cmake" -DCMAKE_INSTALL_PREFIX="$PRG_DIR/opencv" "$ANDROID_DIR/.."  || exit 1
+#make -j8 install/strip || exit 1
 
-cd "$PRG_DIR/opencv"
-rm -rf doc include src .classpath .project AndroidManifest.xml default.properties share/OpenCV/haarcascades share/OpenCV/lbpcascades share/OpenCV/*.cmake share/OpenCV/OpenCV.mk
-mv libs/armeabi-v7a libs/armeabi-v7a-neon
-mv share/OpenCV/3rdparty/libs/armeabi-v7a share/OpenCV/3rdparty/libs/armeabi-v7a-neon
+#cd "$PRG_DIR/opencv"
+#rm -rf doc include src .classpath .project AndroidManifest.xml default.properties share/OpenCV/haarcascades share/OpenCV/lbpcascades share/OpenCV/*.cmake share/OpenCV/OpenCV.mk
+#mv libs/armeabi-v7a libs/armeabi-v7a-neon
+#mv share/OpenCV/3rdparty/libs/armeabi-v7a share/OpenCV/3rdparty/libs/armeabi-v7a-neon
 
 
 # armeabi-v7a build
@@ -54,8 +53,8 @@ cp "$ANDROID_DIR/../README" opencv/
 
 # get opencv version
 CV_VERSION=`grep -o "[0-9]\+\.[0-9]\+\.[0-9]\+" opencv/share/OpenCV/OpenCVConfig-version.cmake`
-mv opencv opencv$CV_VERSION
-
+OPENCV_NAME=OpenCV-$CV_VERSION
+mv opencv $OPENCV_NAME
 
 #samples
 cp -r "$ANDROID_DIR/../samples/android" "$PRG_DIR/samples"
@@ -72,7 +71,7 @@ do
     if [ $HAS_REFERENCE = 1 ]
     then
       echo -n > "$dir/default.properties"
-      android update project --name "$dir" --target "android-8" --library "../../opencv$CV_VERSION" --path "$dir"
+      android update project --name "$dir" --target "android-8" --library "../../$OPENCV_NAME" --path "$dir"
       #echo 'android update project --name "$dir" --target "android-8" --library "../opencv$CV_VERSION" --path "$dir"'
     fi
   else
@@ -89,8 +88,6 @@ cd "$PRG_DIR/samples"
 svn status --no-ignore | grep ^I | cut -c9- | xargs -d \\n rm -rf
 #remove unversioned files/folders
 svn status | grep ^\? | cut -c9- | xargs -d \\n rm -rf
-#remove unneded CMakeLists.txt
-rm CMakeLists.txt
 
 
 #generate "gen" folders to eliminate eclipse warnings
@@ -106,11 +103,12 @@ done
 
 #generate folders "gen" and "res" for opencv (dummy eclipse stiff)
 cd $PRG_DIR
-mkdir "opencv$CV_VERSION/gen"
-mkdir "opencv$CV_VERSION/res"
+mkdir "$OPENCV_NAME/gen"
+mkdir "$OPENCV_NAME/res"
 
 # pack all files
 cd $PRG_DIR
-tar cjpf opencv$CV_VERSION.tar.bz2 --exclude-vcs opencv$CV_VERSION samples || exit -1
+PRG_NAME=OpenCV-$CV_VERSION-tp-android-bin.tar.bz2
+tar cjpf $PRG_NAME --exclude-vcs $OPENCV_NAME samples || exit -1
 echo
-echo "Package opencv$CV_VERSION.tar.bz2 is successfully created"
+echo "Package $PRG_NAME is successfully created"
