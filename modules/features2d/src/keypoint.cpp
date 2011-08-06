3f40a1d8c33d4233642f04f6857f3389294972ef
Vadim Pisarevsky <no@email>
2011-08-06 19:42:29 +0000

    fixed KeyPoint::convert(keypoints,point2f) (ticket #1282; thanks to cooperz for the patch)

--- modules/features2d/src/keypoint.cpp
+++ modules/features2d/src/keypoint.cpp
@@ -119,6 +119,7 @@ void KeyPoint::convert(const std::vector<KeyPoint>& keypoints, std::vector<Point
 void KeyPoint::convert( const std::vector<Point2f>& points2f, std::vector<KeyPoint>& keypoints,
                         float size, float response, int octave, int class_id )
 {
+    keypoints.resize(points2f.size());
     for( size_t i = 0; i < points2f.size(); i++ )
         keypoints[i] = KeyPoint(points2f[i], size, -1, response, octave, class_id);
 }
