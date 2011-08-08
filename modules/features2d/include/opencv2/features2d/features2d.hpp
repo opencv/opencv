4839bfb1a56ed8fd4bbd9fa95f3ab9f11ab69f8d
Maria Dimashova <no@email>
2011-08-08 14:03:37 +0000

    fixed grammar of the comment

--- modules/features2d/include/opencv2/features2d/features2d.hpp
+++ modules/features2d/include/opencv2/features2d/features2d.hpp
@@ -2084,7 +2084,7 @@ template<> struct Accumulator<char>   { typedef float Type; };
 template<> struct Accumulator<short>  { typedef float Type; };
 
 /*
- * Squeared Euclidean distance functor
+ * Squared Euclidean distance functor
  */
 template<class T>
 struct CV_EXPORTS SL2
