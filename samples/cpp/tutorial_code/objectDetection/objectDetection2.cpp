dd836f1bdd3a749a36408382b7d9c37fba98bb99
Ana Huaman <no@email>
2011-08-14 22:32:41 +0000

    Modified sample for object detection with LBP in tutorial_code

--- samples/cpp/tutorial_code/objectDetection/objectDetection2.cpp
+++ samples/cpp/tutorial_code/objectDetection/objectDetection2.cpp
@@ -18,8 +18,11 @@ void detectAndDisplay( Mat frame );
 
 /** Global variables */
 String face_cascade_name = "lbpcascade_frontalface.xml";
+String eyes_cascade_name = "haarcascade_eye_tree_eyeglasses.xml";
 CascadeClassifier face_cascade;
+CascadeClassifier eyes_cascade;
 string window_name = "Capture - Face detection";
+
 RNG rng(12345);
 
 /**
@@ -32,6 +35,7 @@ int main( int argc, const char** argv )
 
   //-- 1. Load the cascade
   if( !face_cascade.load( face_cascade_name ) ){ printf("--(!)Error loading\n"); return -1; };
+  if( !eyes_cascade.load( eyes_cascade_name ) ){ printf("--(!)Error loading\n"); return -1; };
 
   //-- 2. Read the video stream
   capture = cvCaptureFromCAM( -1 );
@@ -65,13 +69,31 @@ void detectAndDisplay( Mat frame )
 
    cvtColor( frame, frame_gray, CV_BGR2GRAY );
    equalizeHist( frame_gray, frame_gray );
+
    //-- Detect faces
    face_cascade.detectMultiScale( frame_gray, faces, 1.1, 2, 0, Size(80, 80) );
 
    for( int i = 0; i < faces.size(); i++ )
     {
-      Point center( faces[i].x + faces[i].width*0.5, faces[i].y + faces[i].height*0.5 );
-      ellipse( frame, center, Size( faces[i].width*0.5, faces[i].height*0.5), 0, 0, 360, Scalar( 255, 0, 0 ), 2, 8, 0 );
+      Mat faceROI = frame_gray( faces[i] );
+      std::vector<Rect> eyes;
+
+      //-- In each face, detect eyes
+      eyes_cascade.detectMultiScale( faceROI, eyes, 1.1, 2, 0 |CV_HAAR_SCALE_IMAGE, Size(30, 30) );
+      if( eyes.size() == 2)
+      {
+         //-- Draw the face
+         Point center( faces[i].x + faces[i].width*0.5, faces[i].y + faces[i].height*0.5 );
+         ellipse( frame, center, Size( faces[i].width*0.5, faces[i].height*0.5), 0, 0, 360, Scalar( 255, 0, 0 ), 2, 8, 0 );
+
+         for( int j = 0; j < eyes.size(); j++ )
+          { //-- Draw the eyes
+            Point center( faces[i].x + eyes[j].x + eyes[j].width*0.5, faces[i].y + eyes[j].y + eyes[j].height*0.5 ); 
+            int radius = cvRound( (eyes[j].width + eyes[j].height)*0.25 );
+            circle( frame, center, radius, Scalar( 255, 0, 255 ), 3, 8, 0 );
+          }
+       }
+
     } 
    //-- Show what you got
    imshow( window_name, frame );
