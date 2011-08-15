ed349fc3b52641a03fa16c36eaa3c34cfd548ff8
Andrey Kamaev <no@email>
2011-08-15 09:11:29 +0000

    Fixed Android build

--- modules/java/android_test/src/org/opencv/test/objdetect/ObjdetectTest.java
+++ modules/java/android_test/src/org/opencv/test/objdetect/ObjdetectTest.java
@@ -8,7 +8,8 @@ import org.opencv.test.OpenCVTestCase;
 
 public class ObjdetectTest extends OpenCVTestCase {
 
-    public void testGroupRectanglesListOfRectInt() {
+    public void testGroupRectanglesListOfRectListOfIntegerInt() {
+        fail("Not yet implemented");
         Rect r = new Rect(10, 10, 20, 20);
         ArrayList<Rect> rects = new ArrayList<Rect>();
 
@@ -16,11 +17,12 @@ public class ObjdetectTest extends OpenCVTestCase {
             rects.add(r);
 
         int groupThreshold = 1;
-        Objdetect.groupRectangles(rects, groupThreshold);
+        Objdetect.groupRectangles(rects, null, groupThreshold);//TODO: second parameter should not be null
         assertEquals(1, rects.size());
     }
 
-    public void testGroupRectanglesListOfRectIntDouble() {
+    public void testGroupRectanglesListOfRectListOfIntegerIntDouble() {
+        fail("Not yet implemented");
         Rect r1 = new Rect(10, 10, 20, 20);
         Rect r2 = new Rect(10, 10, 25, 25);
         ArrayList<Rect> rects = new ArrayList<Rect>();
@@ -32,20 +34,7 @@ public class ObjdetectTest extends OpenCVTestCase {
 
         int groupThreshold = 1;
         double eps = 0.2;
-        Objdetect.groupRectangles(rects, groupThreshold, eps);
+        Objdetect.groupRectangles(rects, null, groupThreshold, eps);//TODO: second parameter should not be null
         assertEquals(2, rects.size());
     }
-
-    public void testGroupRectanglesListOfRectIntDoubleListOfIntegerListOfDouble() {
-        fail("Not yet implemented");
-    }
-
-    public void testGroupRectanglesListOfRectListOfIntegerInt() {
-        fail("Not yet implemented");
-    }
-
-    public void testGroupRectanglesListOfRectListOfIntegerIntDouble() {
-        fail("Not yet implemented");
-    }
-
 }
