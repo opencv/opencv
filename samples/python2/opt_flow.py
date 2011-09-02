700ef09a23b1052df2da5e98ca5e13724589f16f
Alexander Mordvintsev <no@email>
2011-09-02 11:54:02 +0000

    opt_flow.py sample fixed

--- samples/python2/opt_flow.py
+++ samples/python2/opt_flow.py
@@ -59,7 +59,7 @@ if __name__ == '__main__':
     while True:
         ret, img = cam.read()
         gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-        flow = cv2.calcOpticalFlowFarneback(prevgray, gray, None, 0.5, 3, 15, 3, 5, 1.2, 0)
+        flow = cv2.calcOpticalFlowFarneback(prevgray, gray, 0.5, 3, 15, 3, 5, 1.2, 0)
         prevgray = gray
         
         cv2.imshow('flow', draw_flow(gray, flow))
