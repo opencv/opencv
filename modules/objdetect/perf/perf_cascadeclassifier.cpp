5c3a93a5b8737b6f8c7449e5344e35965b425a4d
Leonid Beynenson <no@email>
2011-09-09 11:28:19 +0000

    Small modification in test parameters.

--- modules/objdetect/perf/perf_cascadeclassifier.cpp
+++ modules/objdetect/perf/perf_cascadeclassifier.cpp
@@ -7,7 +7,8 @@ using namespace perf;
 typedef std::tr1::tuple<std::string, int> ImageName_MinSize_t;
 typedef perf::TestBaseWithParam<ImageName_MinSize_t> ImageName_MinSize;
 
-PERF_TEST_P( ImageName_MinSize, CascadeClassifierLBPFrontalFace, testing::Values( ImageName_MinSize_t("cv/shared/lena.jpg", 10) ) )
+PERF_TEST_P( ImageName_MinSize, CascadeClassifierLBPFrontalFace, 
+        testing::Combine(testing::Values( std::string("cv/shared/lena.jpg")), testing::Values(24, 30, 40, 50, 60, 70, 80, 90) ) )
 {
     const string filename = std::tr1::get<0>(GetParam());
     int min_size = std::tr1::get<1>(GetParam());
