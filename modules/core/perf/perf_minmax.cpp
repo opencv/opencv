d99f4a2beb80ada3289354b585ae34b36d1a11e0
Leonid Beynenson <no@email>
2011-09-13 21:57:42 +0000

    Since there is no a standard functions cv::min and cv::max which has cv::Scalar argument, removed the performance tests, calling the functions cv::min and cv::max with cv::Scalar argument. Instead of them two performance tests were created which call the functions cv::min and cv::max with 'double' argument.

---
+++ modules/core/perf/perf_minmax.cpp
@@ -0,0 +1,39 @@
+#include "perf_precomp.hpp"
+
+using namespace std;
+using namespace cv;
+using namespace perf;
+
+#define TYPICAL_MAT_SIZES_MINMAX  TYPICAL_MAT_SIZES 
+#define TYPICAL_MAT_TYPES_MINMAX  CV_8SC1, CV_8SC4, CV_32SC1, CV_32FC1
+#define TYPICAL_MATS_MINMAX       testing::Combine( testing::Values( TYPICAL_MAT_SIZES_MINMAX), testing::Values( TYPICAL_MAT_TYPES_MINMAX) )
+
+PERF_TEST_P(Size_MatType, min_double, TYPICAL_MATS_MINMAX) 
+{
+    Size sz = std::tr1::get<0>(GetParam());
+    int type = std::tr1::get<1>(GetParam());
+
+    cv::Mat a = Mat(sz, type);
+    cv::Mat c = Mat(sz, type);
+
+    declare.in(a, ::perf::TestBase::WARMUP_RNG).out(c);
+
+    TEST_CYCLE(100) cv::min(a, 10, c);
+
+    SANITY_CHECK(c);
+}
+
+PERF_TEST_P(Size_MatType, max_double, TYPICAL_MATS_MINMAX) 
+{
+    Size sz = std::tr1::get<0>(GetParam());
+    int type = std::tr1::get<1>(GetParam());
+
+    cv::Mat a = Mat(sz, type);
+    cv::Mat c = Mat(sz, type);
+
+    declare.in(a, ::perf::TestBase::WARMUP_RNG).out(c);
+
+    TEST_CYCLE(100) cv::max(a, 10, c);
+
+    SANITY_CHECK(c);
+}
