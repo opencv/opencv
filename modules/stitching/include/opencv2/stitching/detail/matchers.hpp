0b192cb4ea5b928eee6290e76191a4d68f8e3f82
Alexey Spizhevoy <no@email>
2011-10-27 09:00:28 +0000

    Fixed warning in the ORB features finder

--- modules/stitching/include/opencv2/stitching/detail/matchers.hpp
+++ modules/stitching/include/opencv2/stitching/detail/matchers.hpp
@@ -92,7 +92,7 @@ private:
 class CV_EXPORTS OrbFeaturesFinder : public FeaturesFinder
 {
 public:
-    OrbFeaturesFinder(Size _grid_size = Size(3,1), size_t n_features = 1500, const ORB::CommonParams & detector_params = ORB::CommonParams(1.3, 5));
+    OrbFeaturesFinder(Size _grid_size = Size(3,1), size_t n_features = 1500, const ORB::CommonParams & detector_params = ORB::CommonParams(1.3f, 5));
 
 private:
     void find(const Mat &image, ImageFeatures &features);
