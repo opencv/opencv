2c2616d46f51c0bee77ff85a1ac71856ad4b70ce
Andrey Kamaev <no@email>
2012-03-28 13:37:58 +0000

    #1508 prefer architecture-specific install directory for python bindings, when LIB_SUFFIX is used

--- cmake/OpenCVDetectPython.cmake
+++ cmake/OpenCVDetectPython.cmake
@@ -17,28 +17,35 @@ if(PYTHON_EXECUTABLE)
     OUTPUT_STRIP_TRAILING_WHITESPACE)
 
   string(REGEX MATCH "[0-9]+.[0-9]+" PYTHON_VERSION_MAJOR_MINOR "${PYTHON_VERSION_FULL}")
-  if(CMAKE_HOST_UNIX)
-    execute_process(COMMAND ${PYTHON_EXECUTABLE} -c "from distutils.sysconfig import *; print get_python_lib()"
-                    RESULT_VARIABLE PYTHON_CVPY_PROCESS
-                    OUTPUT_VARIABLE PYTHON_STD_PACKAGES_PATH
-                    OUTPUT_STRIP_TRAILING_WHITESPACE)
-    if("${PYTHON_STD_PACKAGES_PATH}" MATCHES "site-packages")
-      set(PYTHON_PACKAGES_PATH lib/python${PYTHON_VERSION_MAJOR_MINOR}/site-packages CACHE PATH "Where to install the python packages.")
-    else() #debian based assumed, install to the dist-packages.
-      set(PYTHON_PACKAGES_PATH lib/python${PYTHON_VERSION_MAJOR_MINOR}/dist-packages CACHE PATH "Where to install the python packages.")
-    endif()
-  elseif(CMAKE_HOST_WIN32)
-    get_filename_component(PYTHON_PATH "${PYTHON_EXECUTABLE}" PATH)
-    file(TO_CMAKE_PATH "${PYTHON_PATH}" PYTHON_PATH)
-    if(NOT EXISTS "${PYTHON_PATH}/Lib/site-packages")
-      unset(PYTHON_PATH)
-      get_filename_component(PYTHON_PATH "[HKEY_LOCAL_MACHINE\\SOFTWARE\\Python\\PythonCore\\${PYTHON_VERSION_MAJOR_MINOR}\\InstallPath]" ABSOLUTE)
-      file(TO_CMAKE_PATH "${PYTHON_PATH}" PYTHON_PATH)
-    endif()
-    set(PYTHON_PACKAGES_PATH "${PYTHON_PATH}/Lib/site-packages")
-  endif()
 
   if(NOT ANDROID AND NOT IOS)
+    if(CMAKE_HOST_UNIX)
+      execute_process(COMMAND ${PYTHON_EXECUTABLE} -c "from distutils.sysconfig import *; print get_python_lib()"
+                      RESULT_VARIABLE PYTHON_CVPY_PROCESS
+                      OUTPUT_VARIABLE PYTHON_STD_PACKAGES_PATH
+                      OUTPUT_STRIP_TRAILING_WHITESPACE)
+      if("${PYTHON_STD_PACKAGES_PATH}" MATCHES "site-packages")
+        set(PYTHON_PACKAGES_PATH "python${PYTHON_VERSION_MAJOR_MINOR}/site-packages")
+      else() #debian based assumed, install to the dist-packages.
+        set(PYTHON_PACKAGES_PATH "python${PYTHON_VERSION_MAJOR_MINOR}/dist-packages")
+      endif()
+      if(EXISTS "${CMAKE_INSTALL_PREFIX}/lib${LIB_SUFFIX}/${PYTHON_PACKAGES_PATH}")
+        set(PYTHON_PACKAGES_PATH "lib${LIB_SUFFIX}/${PYTHON_PACKAGES_PATH}")
+      else()
+        set(PYTHON_PACKAGES_PATH "lib/${PYTHON_PACKAGES_PATH}")
+      endif()
+      set(PYTHON_PACKAGES_PATH "${PYTHON_PACKAGES_PATH}" CACHE PATH "Where to install the python packages.")
+    elseif(CMAKE_HOST_WIN32)
+      get_filename_component(PYTHON_PATH "${PYTHON_EXECUTABLE}" PATH)
+      file(TO_CMAKE_PATH "${PYTHON_PATH}" PYTHON_PATH)
+      if(NOT EXISTS "${PYTHON_PATH}/Lib/site-packages")
+        unset(PYTHON_PATH)
+        get_filename_component(PYTHON_PATH "[HKEY_LOCAL_MACHINE\\SOFTWARE\\Python\\PythonCore\\${PYTHON_VERSION_MAJOR_MINOR}\\InstallPath]" ABSOLUTE)
+        file(TO_CMAKE_PATH "${PYTHON_PATH}" PYTHON_PATH)
+      endif()
+      set(PYTHON_PACKAGES_PATH "${PYTHON_PATH}/Lib/site-packages")
+    endif()
+
     # Attempt to discover the NumPy include directory. If this succeeds, then build python API with NumPy
     execute_process(COMMAND ${PYTHON_EXECUTABLE} -c "import os; os.environ['DISTUTILS_USE_SDK']='1'; import numpy.distutils; print numpy.distutils.misc_util.get_numpy_include_dirs()[0]"
                     RESULT_VARIABLE PYTHON_NUMPY_PROCESS
@@ -52,7 +59,7 @@ if(PYTHON_EXECUTABLE)
       ocv_include_directories(${PYTHON_NUMPY_INCLUDE_DIRS})
       message(STATUS "    Use NumPy headers from: ${PYTHON_NUMPY_INCLUDE_DIRS}")
     endif()
-  endif()
+  endif(NOT ANDROID AND NOT IOS)
 
   if(BUILD_DOCS)
     # look for Sphinx
