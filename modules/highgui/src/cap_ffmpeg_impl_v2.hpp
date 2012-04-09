d277a6a2d4a349a3cfdb95e44f45120fe4ee2545
Alexander Shishkov <no@email>
2012-04-09 20:17:02 +0000

    test commit for Buildbot

--- modules/highgui/src/cap_ffmpeg_impl_v2.hpp
+++ modules/highgui/src/cap_ffmpeg_impl_v2.hpp
@@ -18,7 +18,7 @@
 // are permitted provided that the following conditions are met:
 //
 //   * Redistribution's of source code must retain the above copyright notice,
-//     this list of conditions and the following disclaimer.
+//     this list of conditions and the following disclaimer.   
 //
 //   * Redistribution's in binary form must reproduce the above copyright notice,
 //     this list of conditions and the following disclaimer in the documentation
