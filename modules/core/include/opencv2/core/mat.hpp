829e558d48fb5eff196924faf601db6bca0b8556
Ilya Lysenkov <no@email>
2012-04-10 13:02:58 +0000

    Andrey, Maria and I fixed Mat::diag()

--- modules/core/include/opencv2/core/mat.hpp
+++ modules/core/include/opencv2/core/mat.hpp
@@ -319,8 +319,8 @@ inline Mat Mat::colRange(const Range& r) const
 
 inline Mat Mat::diag(const Mat& d)
 {
-    CV_Assert( d.cols == 1 );
-    Mat m(d.rows, d.rows, d.type(), Scalar(0)), md = m.diag();
+    CV_Assert( d.cols == 1 || d.rows == 1 );
+    Mat m(d.total(), d.total(), d.type(), Scalar(0)), md = m.diag();
     d.copyTo(md);
     return m;
 }
