917063b7f792e540737243d9340284907d39126a
Vladislav Vinogradov <no@email>
2012-04-28 08:41:10 +0000

    reverted r8003 (CascadeClassifier_GPU)

--- modules/gpu/src/nvidia/core/NCV.hpp
+++ modules/gpu/src/nvidia/core/NCV.hpp
@@ -134,24 +134,15 @@ typedef      unsigned char Ncv8u;
 typedef              float Ncv32f;
 typedef             double Ncv64f;
 
-struct NcvRect8u__
+
+struct NcvRect8u
 {
     Ncv8u x;
     Ncv8u y;
     Ncv8u width;
     Ncv8u height;
-};
-
-struct NcvRect8u : NcvRect8u__
-{
-    __host__ __device__ NcvRect8u() {}
-    __host__ __device__ NcvRect8u(Ncv8u x, Ncv8u y, Ncv8u width, Ncv8u height)
-    {
-        x = x;
-        y = y;
-        width = width;
-        height = height;
-    }
+    __host__ __device__ NcvRect8u() : x(0), y(0), width(0), height(0) {};
+    __host__ __device__ NcvRect8u(Ncv8u x, Ncv8u y, Ncv8u width, Ncv8u height) : x(x), y(y), width(width), height(height) {}
 };
 
 
