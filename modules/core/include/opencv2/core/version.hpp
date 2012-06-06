74786075e3df736c1ff5baf8c140b603e9d5d5e6
Alexander Shishkov <no@email>
2012-06-06 09:21:09 +0000

    test commit

--- modules/core/include/opencv2/core/version.hpp
+++ modules/core/include/opencv2/core/version.hpp
@@ -1,6 +1,6 @@
 /*M///////////////////////////////////////////////////////////////////////////////////////
 //
-//  IMPORTANT: READ BEFORE DOWNLOADING, COPYING, INSTALLING OR USING. 
+//  IMPORTANT: READ BEFORE DOWNLOADING, COPYING, INSTALLING OR USING.
 //
 //  By downloading, copying, installing or using the software you agree to this license.
 //  If you do not agree to this license, do not download, install,
