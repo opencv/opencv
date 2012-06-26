369f2c4903bb17ad6f22ff9e6a78be20e075b965
Alexander Smorkalov <no@email>
2012-06-26 12:58:46 +0000

    Color blob detection example updated for OpenCV Manager model.

--- samples/android/color-blob-detection/src/org/opencv/samples/colorblobdetect/ColorBlobDetectionActivity.java
+++ samples/android/color-blob-detection/src/org/opencv/samples/colorblobdetect/ColorBlobDetectionActivity.java
@@ -1,5 +1,9 @@
 package org.opencv.samples.colorblobdetect;
 
+import org.opencv.android.BaseLoaderCallback;
+import org.opencv.android.LoaderCallbackInterface;
+import org.opencv.android.OpenCVLoader;
+
 import android.app.Activity;
 import android.app.AlertDialog;
 import android.content.DialogInterface;
@@ -12,6 +16,38 @@ public class ColorBlobDetectionActivity extends Activity {
 	private static final String TAG = "Example/ColorBlobDetection";
 	private ColorBlobDetectionView mView;
 	
+	   private BaseLoaderCallback  mOpenCVCallBack = new BaseLoaderCallback(this) {
+	    	@Override
+	    	public void onManagerConnected(int status) {
+	    		switch (status) {
+					case LoaderCallbackInterface.SUCCESS:
+					{
+						Log.i(TAG, "OpenCV loaded successfully");
+						// Create and set View
+						mView = new ColorBlobDetectionView(mAppContext);
+						setContentView(mView);
+						// Check native OpenCV camera
+						if( !mView.openCamera() ) {
+							AlertDialog ad = new AlertDialog.Builder(mAppContext).create();
+							ad.setCancelable(false); // This blocks the 'BACK' button
+							ad.setMessage("Fatal error: can't open camera!");
+							ad.setButton("OK", new DialogInterface.OnClickListener() {
+							    public void onClick(DialogInterface dialog, int which) {
+								dialog.dismiss();
+								finish();
+							    }
+							});
+							ad.show();
+						}
+					} break;
+					default:
+					{
+						super.onManagerConnected(status);
+					} break;
+				}
+	    	}
+		};
+	
 	public ColorBlobDetectionActivity()
 	{
 		Log.i(TAG, "Instantiated new " + this.getClass());
@@ -21,14 +57,15 @@ public class ColorBlobDetectionActivity extends Activity {
 	protected void onPause() {
         Log.i(TAG, "onPause");
 		super.onPause();
-		mView.releaseCamera();
+		if (null != mView)
+			mView.releaseCamera();
 	}
 
 	@Override
 	protected void onResume() {
         Log.i(TAG, "onResume");
 		super.onResume();
-		if( !mView.openCamera() ) {
+		if( (null != mView) && !mView.openCamera() ) {
 			AlertDialog ad = new AlertDialog.Builder(this).create();  
 			ad.setCancelable(false); // This blocks the 'BACK' button  
 			ad.setMessage("Fatal error: can't open camera!");  
@@ -47,8 +84,14 @@ public class ColorBlobDetectionActivity extends Activity {
     public void onCreate(Bundle savedInstanceState) {        
         Log.i(TAG, "onCreate");
         super.onCreate(savedInstanceState);
+        
         requestWindowFeature(Window.FEATURE_NO_TITLE);
-        mView = new ColorBlobDetectionView(this);
-        setContentView(mView);
+        
+        Log.i(TAG, "Trying to load OpenCV library");
+        if (!OpenCVLoader.initAsync(OpenCVLoader.OPEN_CV_VERSION_2_4_0, this, mOpenCVCallBack))
+        {
+        	Log.e(TAG, "Cannot connect to OpenCV Manager");
+        	finish();
+        }
     }
 }
