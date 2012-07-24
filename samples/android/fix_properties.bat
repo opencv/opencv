794b07ba11a666feaddf212986e1bdb3d1b353d7
Andrey Pavlenko <Andrey.Pavlenko@itseez.com>
2012-07-24 23:58:52 +0400

    removing unused files

--- samples/android/fix_properties.bat
+++
@@ -1,8 +0,0 @@
-call android update project --target android-11 --library ../../OpenCV-2.4.1/ --name "Sample - 15-puzzle"                             --path ./15-puzzle
-call android update project --target android-11 --library ../../OpenCV-2.4.1/ --name "Sample - face-detection"                        --path ./face-detection
-call android update project --target android-11 --library ../../OpenCV-2.4.1/ --name "Sample - image-manipulations"                   --path ./image-manipulations
-call android update project --target android-11                               --name "Tutorial 0 (Basic) - Android Camera"            --path ./tutorial-0-androidcamera
-call android update project --target android-11 --library ../../OpenCV-2.4.1/ --name "Tutorial 1 (Basic) - Add OpenCV"                --path ./tutorial-1-addopencv
-call android update project --target android-11 --library ../../OpenCV-2.4.1/ --name "Tutorial 2 (Basic) - Use OpenCV Camera"         --path ./tutorial-2-opencvcamera
-call android update project --target android-11 --library ../../OpenCV-2.4.1/ --name "Tutorial 3 (Advanced) - Add Native OpenCV"      --path ./tutorial-3-native
-call android update project --target android-11 --library ../../OpenCV-2.4.1/ --name "Tutorial 4 (Advanced) - Mix Java+Native OpenCV" --path ./tutorial-4-mixed
