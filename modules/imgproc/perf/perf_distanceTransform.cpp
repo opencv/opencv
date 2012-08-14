f1d41d8b94f3d3007ac489735c5f6c2298f86fe1
Daniil Osokin <daniil.osokin@itseez.com>
2012-08-14 13:01:52 +0400

    disabled whole perf test for distance transform

--- modules/imgproc/perf/perf_distanceTransform.cpp
+++ modules/imgproc/perf/perf_distanceTransform.cpp
@@ -1,4 +1,4 @@
-#include "perf_precomp.hpp"
+/*#include "perf_precomp.hpp"
 #include "distransform.cpp"
 
 using namespace std;
@@ -7,7 +7,7 @@ using namespace perf;
 
 typedef perf::TestBaseWithParam<Size> Size_DistanceTransform;
 
-/*PERF_TEST_P(Size_DistanceTransform, icvTrueDistTrans, testing::Values(TYPICAL_MAT_SIZES))
+PERF_TEST_P(Size_DistanceTransform, icvTrueDistTrans, testing::Values(TYPICAL_MAT_SIZES))
 {
     Size size = GetParam();
     Mat src(size, CV_8UC1);
