7db1323f81fbc9b3de83c6127a36275470060e6b
marina.kolpakova <marina.kolpakova@itseez.com>
2012-10-01 13:49:21 +0400

    add test that stores detections on the specific scale

--- modules/gpu/test/test_softcascade.cpp
+++ modules/gpu/test/test_softcascade.cpp
@@ -71,4 +71,63 @@ TEST(SoftCascade, detect)
     // });
 }
 
+class SCSpecific : public ::testing::TestWithParam<std::tr1::tuple<std::string, int> > {
+};
+
+namespace {
+std::string itoa(long i)
+{
+    static char s[65];
+    sprintf(s, "%ld", i);
+    return std::string(s);
+}
+}
+
+TEST_P(SCSpecific, detect)
+{
+    std::string xml =  cvtest::TS::ptr()->get_data_path() + "../cv/cascadeandhog/sc_cvpr_2012_to_opencv.xml";
+    cv::gpu::SoftCascade cascade;
+    ASSERT_TRUE(cascade.load(xml));
+
+    std::string path = GET_PARAM(0);
+    cv::Mat coloredCpu = cv::imread(cvtest::TS::ptr()->get_data_path() + path);
+
+    ASSERT_FALSE(coloredCpu.empty());
+    GpuMat colored(coloredCpu), objectBoxes(1, 1000, CV_8UC1), rois;
+
+    int level = GET_PARAM(1);
+    cascade.detectMultiScale(colored, rois, objectBoxes, 1, level);
+
+    cv::Mat dt(objectBoxes);
+    typedef cv::gpu::SoftCascade::Detection detection_t;
+
+    detection_t* dts = (detection_t*)dt.data;
+    cv::Mat result(coloredCpu);
+
+
+    std::cout << "Total detections " << (dt.cols / sizeof(detection_t)) << std::endl;
+    for(int i = 0; i  < (int)(dt.cols / sizeof(detection_t)); ++i)
+    {
+        detection_t d = dts[i];
+        std::cout << "detection: [" << std::setw(4) << d.x << " " << std::setw(4) << d.y
+                  << "] [" << std::setw(4) << d.w << " " << std::setw(4) << d.h << "] "
+                  << std::setw(12)  << d.confidence << std::endl;
+
+        cv::rectangle(result, cv::Rect(d.x, d.y, d.w, d.h), cv::Scalar(255, 0, 0, 255), 1);
+    }
+
+    std::cout << "Result stored in " << "/home/kellan/gpu_res_1_oct_" + itoa(level) << "_"
+    + itoa((dt.cols / sizeof(detection_t))) + ".png" << std::endl;
+    cv::imwrite("/home/kellan/gpu_res_1_oct_" + itoa(level) + "_" + itoa((dt.cols / sizeof(detection_t))) + ".png",
+        result);
+    cv::imshow("res", result);
+    cv::waitKey(0);
+}
+
+INSTANTIATE_TEST_CASE_P(inLevel, SCSpecific,
+    testing::Combine(
+        testing::Values(std::string("../cv/cascadeandhog/bahnhof/image_00000000_0.png")),
+        testing::Range(0, 47)
+        ));
+
 #endif
