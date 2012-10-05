3b79c9b971faa719f21cb5841b4ead9b6365f695
Abhinav Gupta <aag999in@gmail.com>
2012-10-05 17:27:56 +0300

    Update samples/cpp/tutorial_code/features2D/SURF_descriptor.cpp
    Changes made for OpenCV 2.4.2 compatibility

--- samples/cpp/tutorial_code/features2D/SURF_descriptor.cpp
+++ samples/cpp/tutorial_code/features2D/SURF_descriptor.cpp
@@ -9,6 +9,7 @@
 #include "opencv2/core/core.hpp"
 #include "opencv2/features2d/features2d.hpp"
 #include "opencv2/highgui/highgui.hpp"
+#include "opencv2/nonfree/features2d.cpp"
 
 using namespace cv;
 
