73cb77842e04c4da0960d9213dcacbd26473b245
Andrey Kamaev <andrey.kamaev@itseez.com>
2012-10-05 18:14:00 +0400

    Replace jpegs with png (see 06a13d9)

--- modules/calib3d/perf/perf_cicrlesGrid.cpp
+++ modules/calib3d/perf/perf_cicrlesGrid.cpp
@@ -10,15 +10,15 @@ typedef std::tr1::tuple<std::string, cv::Size> String_Size_t;
 typedef perf::TestBaseWithParam<String_Size_t> String_Size;
 
 PERF_TEST_P(String_Size, asymm_circles_grid, testing::Values(
-                String_Size_t("cv/cameracalibration/asymmetric_circles/acircles1.jpg", Size(7,13)),
-                String_Size_t("cv/cameracalibration/asymmetric_circles/acircles2.jpg", Size(7,13)),
-                String_Size_t("cv/cameracalibration/asymmetric_circles/acircles3.jpg", Size(7,13)),
-                String_Size_t("cv/cameracalibration/asymmetric_circles/acircles4.jpg", Size(5,5)),
-                String_Size_t("cv/cameracalibration/asymmetric_circles/acircles5.jpg", Size(5,5)),
-                String_Size_t("cv/cameracalibration/asymmetric_circles/acircles6.jpg", Size(5,5)),
-                String_Size_t("cv/cameracalibration/asymmetric_circles/acircles7.jpg", Size(3,9)),
-                String_Size_t("cv/cameracalibration/asymmetric_circles/acircles8.jpg", Size(3,9)),
-                String_Size_t("cv/cameracalibration/asymmetric_circles/acircles9.jpg", Size(3,9))
+                String_Size_t("cv/cameracalibration/asymmetric_circles/acircles1.png", Size(7,13)),
+                String_Size_t("cv/cameracalibration/asymmetric_circles/acircles2.png", Size(7,13)),
+                String_Size_t("cv/cameracalibration/asymmetric_circles/acircles3.png", Size(7,13)),
+                String_Size_t("cv/cameracalibration/asymmetric_circles/acircles4.png", Size(5,5)),
+                String_Size_t("cv/cameracalibration/asymmetric_circles/acircles5.png", Size(5,5)),
+                String_Size_t("cv/cameracalibration/asymmetric_circles/acircles6.png", Size(5,5)),
+                String_Size_t("cv/cameracalibration/asymmetric_circles/acircles7.png", Size(3,9)),
+                String_Size_t("cv/cameracalibration/asymmetric_circles/acircles8.png", Size(3,9)),
+                String_Size_t("cv/cameracalibration/asymmetric_circles/acircles9.png", Size(3,9))
                 )
             )
 {
