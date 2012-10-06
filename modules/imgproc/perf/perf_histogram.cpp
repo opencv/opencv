5bdc480fe345ffcc52981888e442178aa302978f
Daniil-Osokin <daniil.osokin@itseez.com>
2012-10-06 18:23:21 +0400

    perf tests for calcHist 2-3D case

--- modules/imgproc/perf/perf_histogram.cpp
+++ modules/imgproc/perf/perf_histogram.cpp
@@ -13,9 +13,9 @@ typedef TestBaseWithParam<Size_Source_t> Size_Source;
 typedef TestBaseWithParam<Size> MatSize;
 
 
-PERF_TEST_P(Size_Source, calcHist,
+PERF_TEST_P(Size_Source, calcHist1d,
             testing::Combine(testing::Values(TYPICAL_MAT_SIZES),
-                             testing::Values(CV_8U, CV_32F)
+                             testing::Values(CV_8U, CV_16U, CV_32F)
                              )
             )
 {
@@ -30,6 +30,60 @@ PERF_TEST_P(Size_Source, calcHist,
 
     const float r[] = {0.0f, 256.0f};
     const float* ranges[] = {r};
+
+    declare.in(source, WARMUP_RNG).time(20).iterations(1000);
+    TEST_CYCLE()
+    {
+        calcHist(&source, numberOfImages, channels, Mat(), hist, dims, histSize, ranges);
+    }
+
+    SANITY_CHECK(hist);
+}
+
+PERF_TEST_P(Size_Source, calcHist2d,
+            testing::Combine(testing::Values(TYPICAL_MAT_SIZES),
+                             testing::Values(CV_8UC2, CV_16UC2, CV_32FC2)
+                             )
+            )
+{
+    Size size = get<0>(GetParam());
+    MatType type = get<1>(GetParam());
+    Mat source(size.height, size.width, type);
+    Mat hist;
+    int channels [] = {0, 1};
+    int histSize [] = {256, 256};
+    int dims = 2;
+    int numberOfImages = 1;
+
+    const float r[] = {0.0f, 256.0f};
+    const float* ranges[] = {r, r};
+
+    declare.in(source, WARMUP_RNG).time(20).iterations(1000);
+    TEST_CYCLE()
+    {
+        calcHist(&source, numberOfImages, channels, Mat(), hist, dims, histSize, ranges);
+    }
+
+    SANITY_CHECK(hist);
+}
+
+PERF_TEST_P(Size_Source, calcHist3d,
+            testing::Combine(testing::Values(TYPICAL_MAT_SIZES),
+                             testing::Values(CV_8UC3, CV_16UC3, CV_32FC3)
+                             )
+            )
+{
+    Size size = get<0>(GetParam());
+    MatType type = get<1>(GetParam());
+    Mat hist;
+    int channels [] = {0, 1, 2};
+    int histSize [] = {32, 32, 32};
+    int dims = 3;
+    int numberOfImages = 1;
+    Mat source(size.height, size.width, type);
+
+    const float r[] = {0.0f, 256.0f};
+    const float* ranges[] = {r, r, r};
 
     declare.in(source, WARMUP_RNG).time(20).iterations(1000);
     TEST_CYCLE()
