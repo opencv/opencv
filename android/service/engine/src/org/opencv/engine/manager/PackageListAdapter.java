601b87e3ed741de17974f4223d5cc9b33dbef9d5
Alexander Smorkalov <alexander.smorkalov@itseez.com>
2012-10-08 12:41:20 +0400

    Java tests updated. Build fix.

--- android/service/engine/src/org/opencv/engine/manager/PackageListAdapter.java
+++ android/service/engine/src/org/opencv/engine/manager/PackageListAdapter.java
@@ -4,7 +4,6 @@ import java.util.List;
 import java.util.Map;
 
 import android.content.Context;
-import android.util.Log;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.SimpleAdapter;
