2446c9329fa183caf8d727d3e7c343bc0d7a1b07
Andrey Kamaev <andrey.kamaev@itseez.com>
2012-10-08 18:40:41 +0400

    Remove bad perf test for cvRound

--- modules/core/perf/perf_arithm.cpp
+++ modules/core/perf/perf_arithm.cpp
@@ -6,26 +6,9 @@ using namespace perf;
 using std::tr1::make_tuple;
 using std::tr1::get;
 
-#define TYPICAL_MAT_SIZES_CORE_ARITHM   TYPICAL_MAT_SIZES 
+#define TYPICAL_MAT_SIZES_CORE_ARITHM   TYPICAL_MAT_SIZES
 #define TYPICAL_MAT_TYPES_CORE_ARITHM   CV_8UC1, CV_8SC1, CV_16SC1, CV_16SC2, CV_16SC3, CV_16SC4, CV_8UC4, CV_32SC1, CV_32FC1
 #define TYPICAL_MATS_CORE_ARITHM        testing::Combine( testing::Values( TYPICAL_MAT_SIZES_CORE_ARITHM ), testing::Values( TYPICAL_MAT_TYPES_CORE_ARITHM ) )
-
-#ifdef ANDROID
-PERF_TEST(convert, cvRound)
-{
-    double number = theRNG().uniform(-100, 100);
-
-    int result = 0;
-
-    TEST_CYCLE_N(1000)
-    {
-        for (int i = 0; i < 500000; ++i)
-            result += cvRound(number);
-    }
-
-    SANITY_CHECK(result);
-}
-#endif
 
 PERF_TEST_P(Size_MatType, min, TYPICAL_MATS_CORE_ARITHM)
 {
