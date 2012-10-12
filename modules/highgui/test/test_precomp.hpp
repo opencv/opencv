2e7656dbdfaccca8bb3245fc70ed6069f8e9f53f
Evgeny Talanin <evgeny.talanin@itseez.com>
2012-10-12 13:03:43 +0400

    #2192 patch apply

--- modules/highgui/test/test_precomp.hpp
+++ modules/highgui/test/test_precomp.hpp
@@ -31,6 +31,7 @@
     defined(HAVE_OPENNI)       || \
     defined(HAVE_XIMEA)        || \
     defined(HAVE_AVFOUNDATION) || \
+    defined(HAVE_GIGE_API) || \
     (0)
     //defined(HAVE_ANDROID_NATIVE_CAMERA) ||   - enable after #1193
 #  define BUILD_WITH_CAMERA_SUPPORT 1
