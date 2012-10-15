5f1339b319409ab3fdd35d752207f531c3a934e3
Alexander Smorkalov <alexander.smorkalov@itseez.com>
2012-10-15 11:51:27 +0400

    Menu and style removed from sample.

--- samples/android/camera-preview/src/org/opencv/test/camerawriter/CameraWriterActivity.java
+++ samples/android/camera-preview/src/org/opencv/test/camerawriter/CameraWriterActivity.java
@@ -9,34 +9,33 @@ import org.opencv.test.camerawriter.OpenCvCameraBridgeViewBase.CvCameraViewListe
 import android.os.Bundle;
 import android.app.Activity;
 import android.util.Log;
-import android.view.Menu;
 
 public class CameraWriterActivity extends Activity implements CvCameraViewListener {
 
     protected static final String TAG = "CameraWriterActivity";
 
 
-        private OpenCvCameraBridgeViewBase mCameraView;
+    private OpenCvCameraBridgeViewBase mCameraView;
 
 
     private BaseLoaderCallback mLoaderCallback = new BaseLoaderCallback(this) {
-	@Override
-	public void onManagerConnected(int status) {
-	   switch (status) {
-	       case LoaderCallbackInterface.SUCCESS:
-		      Log.i(TAG, "OpenCV loaded successfully");
-		      // Create and set View
-		      mCameraView.setMaxFrameSize(640, 480);
-		      mCameraView.enableView();
-		      break;
-	       default:
-	      super.onManagerConnected(status);
-		break;
-	   }
-	}
+        @Override
+        public void onManagerConnected(int status) {
+           switch (status) {
+               case LoaderCallbackInterface.SUCCESS:
+                  Log.i(TAG, "OpenCV loaded successfully");
+                  // Create and set View
+                  mCameraView.setMaxFrameSize(640, 480);
+                  mCameraView.enableView();
+                  break;
+               default:
+              super.onManagerConnected(status);
+                break;
+           }
+        }
     };
 
-        @Override
+    @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_camera_writer);
@@ -47,26 +46,17 @@ public class CameraWriterActivity extends Activity implements CvCameraViewListen
 
     }
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        getMenuInflater().inflate(R.menu.activity_camera_writer, menu);
-        return true;
+    public void onCameraViewStarted(int width, int height) {
+        // TODO Auto-generated method stub
+
     }
 
-        @Override
-        public void onCameraViewStarted(int width, int height) {
-                // TODO Auto-generated method stub
+    public void onCameraViewStopped() {
+        // TODO Auto-generated method stub
 
-        }
+    }
 
-        @Override
-        public void onCameraViewStopped() {
-                // TODO Auto-generated method stub
-
-        }
-
-        @Override
-        public Mat onCameraFrame(Mat inputFrame) {
-                return inputFrame;
-        }
+    public Mat onCameraFrame(Mat inputFrame) {
+        return inputFrame;
+    }
 }
--- samples/android/camera-preview/src/org/opencv/test/camerawriter/OpenCvNativeCameraView.java
+++ samples/android/camera-preview/src/org/opencv/test/camerawriter/OpenCvNativeCameraView.java
@@ -19,52 +19,52 @@ import android.util.Log;
  */
 public class OpenCvNativeCameraView extends OpenCvCameraBridgeViewBase {
 
-        public static final String TAG = "OpenCvNativeCameraView";
-        private boolean mStopThread;
-        private Thread mThread;
-        private VideoCapture mCamera;
+    public static final String TAG = "OpenCvNativeCameraView";
+    private boolean mStopThread;
+    private Thread mThread;
+    private VideoCapture mCamera;
 
 
     public OpenCvNativeCameraView(Context context, AttributeSet attrs) {
-                super(context, attrs);
+        super(context, attrs);
+    }
+
+
+    @Override
+    protected void connectCamera(int width, int height) {
+
+        /* 1. We need to instantiate camera
+         * 2. We need to start thread which will be getting frames
+         */
+        /* First step - initialize camera connection */
+        initializeCamera(getWidth(), getHeight());
+
+        /* now we can start update thread */
+        mThread = new Thread(new CameraWorker(getWidth(), getHeight()));
+        mThread.start();
+    }
+
+    @Override
+    protected void disconnectCamera() {
+        /* 1. We need to stop thread which updating the frames
+         * 2. Stop camera and release it
+         */
+        try {
+            mStopThread = true;
+            mThread.join();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        } finally {
+            mThread =  null;
+            mStopThread = false;
         }
 
+        /* Now release camera */
+        releaseCamera();
 
-        @Override
-        protected void connectCamera(int width, int height) {
+    }
 
-                /* 1. We need to instantiate camera
-                 * 2. We need to start thread which will be getting frames
-                 */
-                /* First step - initialize camera connection */
-                initializeCamera(getWidth(), getHeight());
-
-                /* now we can start update thread */
-                mThread = new Thread(new CameraWorker(getWidth(), getHeight()));
-                mThread.start();
-        }
-
-        @Override
-        protected void disconnectCamera() {
-                /* 1. We need to stop thread which updating the frames
-                 * 2. Stop camera and release it
-                 */
-                try {
-                        mStopThread = true;
-                        mThread.join();
-                } catch (InterruptedException e) {
-                        e.printStackTrace();
-                } finally {
-                        mThread =  null;
-                        mStopThread = false;
-                }
-
-                /* Now release camera */
-                releaseCamera();
-
-        }
-
-        private void initializeCamera(int width, int height) {
+    private void initializeCamera(int width, int height) {
         mCamera = new VideoCapture(Highgui.CV_CAP_ANDROID);
         //TODO: improve error handling
 
@@ -85,42 +85,41 @@ public class OpenCvNativeCameraView extends OpenCvCameraBridgeViewBase {
         mFrameHeight = (int)frameHeight;
 
         Log.i(TAG, "Selected camera frame size = (" + mFrameWidth + ", " + mFrameHeight + ")");
+    }
+
+    private void releaseCamera() {
+        if (mCamera != null) {
+            mCamera.release();
+        }
+    }
+
+    private class CameraWorker implements Runnable {
+
+        private Mat mRgba = new Mat();
+        private int mWidth;
+        private int mHeight;
+
+        CameraWorker(int w, int h) {
+            mWidth = w;
+            mHeight = h;
         }
 
-        private void releaseCamera() {
-                if (mCamera != null) {
-                        mCamera.release();
+        public void run() {
+            Mat modified;
+
+
+            do {
+                if (!mCamera.grab()) {
+                    Log.e(TAG, "Camera frame grab failed");
+                    break;
                 }
+                mCamera.retrieve(mRgba, Highgui.CV_CAP_ANDROID_COLOR_FRAME_RGBA);
+
+                deliverAndDrawFrame(mRgba);
+
+            } while (!mStopThread);
+
         }
-
-        private class CameraWorker implements Runnable {
-
-            private Mat mRgba = new Mat();
-            private int mWidth;
-            private int mHeight;
-
-            CameraWorker(int w, int h) {
-		mWidth = w;
-		mHeight = h;
-            }
-
-                @Override
-                public void run() {
-                        Mat modified;
-
-
-                        do {
-                                if (!mCamera.grab()) {
-                                        Log.e(TAG, "Camera frame grab failed");
-                                        break;
-                                }
-                    mCamera.retrieve(mRgba, Highgui.CV_CAP_ANDROID_COLOR_FRAME_RGBA);
-
-                    deliverAndDrawFrame(mRgba);
-
-                        } while (!mStopThread);
-
-                }
-        }
+    }
 
 }
