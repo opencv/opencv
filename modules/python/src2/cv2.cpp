f2a7273ece36f3859e4168a8ca396cca0768102b
Vadim Pisarevsky <vadim.pisarevsky@itseez.com>
2012-10-18 15:29:03 +0400

    fixed bugs #2100 (kind of) and #1393

--- modules/python/src2/cv2.cpp
+++ modules/python/src2/cv2.cpp
@@ -278,12 +278,15 @@ static int pyopencv_to(const PyObject* o, Mat& m, const ArgInfo info, bool allow
             (i < ndims-1 && _strides[i] < _strides[i+1]) )
             needcopy = true;
     }
+    
+    if( ismultichannel && _strides[1] != (npy_intp)elemsize*_sizes[2] )
+        needcopy = true;
 
     if (needcopy)
     {
         if (info.outputarg)
         {
-            failmsg("output array %s is not row-contiguous (step[ndims-1] != elemsize)", info.name);
+            failmsg("Layout of the output array %s is compatible with cv::Mat (step[ndims-1] != elemsize or step[1] != elemsize*nchannels)", info.name);
             return false;
         }
         if( needcast )
