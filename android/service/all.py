680fba7e0dedee3f7566075d55069ab26115e78d
Alexander Smorkalov <alexander.smorkalov@itseez.com>
2012-10-24 12:46:36 +0400

    Repo normalization problems fixed.

--- android/service/all.py
+++ android/service/all.py
@@ -8,7 +8,7 @@ LOCAL_LOG_PATH = os.path.join(os.getcwd(), "logs")
 
 if (__name__ ==  "__main__"):
     if (not os.path.exists(LOCAL_LOG_PATH)):
-    os.makedirs(LOCAL_LOG_PATH)
+        os.makedirs(LOCAL_LOG_PATH)
 
     print("Building native part of OpenCV Manager...")
     HomeDir = os.getcwd()
@@ -19,25 +19,25 @@ if (__name__ ==  "__main__"):
     #print(BuildCommand)
     res = os.system(BuildCommand)
     if (0 == res):
-    print("Build\t[OK]")
+        print("Build\t[OK]")
     else:
-    print("Build\t[FAILED]")
+        print("Build\t[FAILED]")
     sys.exit(-1)
 
     os.chdir(HomeDir)
     ConfFile = open("device.conf", "rt")
 
     for s in ConfFile.readlines():
-    keys = s.split(";")
-    if (len(keys) < 2):
-        print("Error: invalid config line: \"%s\"" % s)
-        continue
-    Arch = keys[0]
-    Name = keys[1]
-    print("testing \"%s\" arch" % Arch)
-    print("Pushing to device \"%s\"" % Name)
-    PushCommand = "%s \"%s\" \"%s\" 2>&1" % (os.path.join(HomeDir, "push_native.py"), Arch, Name)
-    os.system(PushCommand)
-    print("Testing on device \"%s\"" % Name)
-    TestCommand = "%s \"%s\" \"%s\" 2>&1" % (os.path.join(HomeDir, "test_native.py"), Arch, Name)
-    os.system(TestCommand)
+        keys = s.split(";")
+        if (len(keys) < 2):
+           print("Error: invalid config line: \"%s\"" % s)
+           continue
+        Arch = keys[0]
+        Name = keys[1]
+        print("testing \"%s\" arch" % Arch)
+        print("Pushing to device \"%s\"" % Name)
+        PushCommand = "%s \"%s\" \"%s\" 2>&1" % (os.path.join(HomeDir, "push_native.py"), Arch, Name)
+        os.system(PushCommand)
+        print("Testing on device \"%s\"" % Name)
+        TestCommand = "%s \"%s\" \"%s\" 2>&1" % (os.path.join(HomeDir, "test_native.py"), Arch, Name)
+        os.system(TestCommand)
