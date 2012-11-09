a126532cb7e2a02f162ae4c179ea46f8b0a20893
Andrey Kamaev <andrey.kamaev@itseez.com>
2012-11-09 11:29:52 +0400

    Fix OS X build warnings

--- modules/video/perf/perf_precomp.hpp
+++ modules/video/perf/perf_precomp.hpp
@@ -1,6 +1,6 @@
 #ifdef __GNUC__
 #  pragma GCC diagnostic ignored "-Wmissing-declarations"
-#  ifdef __clang__
+#  if defined __clang__ || defined __APPLE__
 #    pragma GCC diagnostic ignored "-Wmissing-prototypes"
 #    pragma GCC diagnostic ignored "-Wextra"
 #  endif
