719b2121ddf4c10216e2a97826154c0319ae9323
ml01172 <damaksim@cisco.com>
2012-12-06 14:44:38 +0100

    Prevent imread from illegal mm. access (Bug #2602)

--- modules/highgui/src/grfmt_jpeg.cpp
+++ modules/highgui/src/grfmt_jpeg.cpp
@@ -229,7 +229,8 @@ bool  JpegDecoder::readHeader()
             if( m_f )
                 jpeg_stdio_src( &state->cinfo, m_f );
         }
-        jpeg_read_header( &state->cinfo, TRUE );
+        if (state->cinfo.src != 0)
+            jpeg_read_header( &state->cinfo, TRUE );
 
         m_width = state->cinfo.image_width;
         m_height = state->cinfo.image_height;
