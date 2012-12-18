1ba76c2ffb625b3f0132c16c22920573f7e48494
Eric Christiansen <ericmartinchristiansen@gmail.com>
2012-12-18 14:24:06 -0800

    Integrated apavlenko's comments and updated build.xml

--- modules/java/android_test/src/org/opencv/test/features2d/FASTFeatureDetectorTest.java
+++ modules/java/android_test/src/org/opencv/test/features2d/FASTFeatureDetectorTest.java
@@ -13,8 +13,6 @@ import org.opencv.features2d.KeyPoint;
 import org.opencv.test.OpenCVTestCase;
 import org.opencv.test.OpenCVTestRunner;
 
-import android.util.Log;
-
 public class FASTFeatureDetectorTest extends OpenCVTestCase {
 
     FeatureDetector detector;
@@ -131,7 +129,6 @@ public class FASTFeatureDetectorTest extends OpenCVTestCase {
 
         String truth = "<?xml version=\"1.0\"?>\n<opencv_storage>\n<name>Feature2D.FAST</name>\n<nonmaxSuppression>1</nonmaxSuppression>\n<threshold>10</threshold>\n<type>2</type>\n</opencv_storage>\n";
         String data = readFile(filename);
-        Log.d("qqq", "\"" + data + "\"");
         assertEquals(truth, data);
     }
 
@@ -143,7 +140,6 @@ public class FASTFeatureDetectorTest extends OpenCVTestCase {
         String truth = "%YAML:1.0\nname: \"Feature2D.FAST\"\nnonmaxSuppression: 1\nthreshold: 10\ntype: 2\n";
         String data = readFile(filename);
 
-        Log.d("qqq", "\"" + data + "\"");
         assertEquals(truth, data);
     }
 }
