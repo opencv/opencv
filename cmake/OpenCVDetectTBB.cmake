26c7e7f2924ac733923033c86f9282160f601d3e
Ivan Korolev <ivan.korolev@itseez.com>
2013-01-30 17:17:04 +0400

    fix default include dirs for TBB

--- cmake/OpenCVDetectTBB.cmake
+++ cmake/OpenCVDetectTBB.cmake
@@ -22,7 +22,7 @@ endif()
 
 if(NOT HAVE_TBB)
   set(TBB_DEFAULT_INCLUDE_DIRS
-    "/opt/intel/tbb" "/usr/local/include" "/usr/include"
+    "/opt/intel/tbb/include" "/usr/local/include" "/usr/include"
     "C:/Program Files/Intel/TBB" "C:/Program Files (x86)/Intel/TBB"
     "C:/Program Files (x86)/tbb/include"
     "C:/Program Files (x86)/tbb/include"
