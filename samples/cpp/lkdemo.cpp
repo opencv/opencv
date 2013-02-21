e3e6056f5169f3d24bd80149d07b2d03c13846f1
Nikolay Rogooschenkov <nickolayr@gmail.com>
2013-02-20 20:14:15 -0500

    Bugfix for lkdemo.cpp with "delete all the points" option, now it really delete it

--- samples/cpp/lkdemo.cpp
+++ samples/cpp/lkdemo.cpp
@@ -134,6 +134,7 @@ int main( int argc, char** argv )
             needToInit = true;
             break;
         case 'c':
+            points[0].clear();
             points[1].clear();
             break;
         case 'n':
