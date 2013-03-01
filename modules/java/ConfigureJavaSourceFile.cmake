opencv/opencv

commit 38f3cc2608fe133911854062bf7b472ce0b471b0

Removed requirement to manually call System.loadLibrary("opencv_java") on desktop.

modules/java/CMakeLists.txt
@@ -16,6 +16,7 @@ endif()
 # output locations
 set(JAVA_INSTALL_ROOT "sdk/java")
 set(JNI_INSTALL_ROOT  "sdk/native")
+set(native_module_output_name ${the_module}${OPENCV_DLLVERSION})
 
 # get list of modules to wrap
 string(REPLACE "opencv_" "" OPENCV_JAVA_MODULES "${OPENCV_MODULE_${the_module}_REQ_DEPS};${OPENCV_MODULE_${the_module}_OPT_DEPS}")
@@ -33,10 +34,27 @@ set(scripts_hdr_parser "${CMAKE_CURRENT_SOURCE_DIR}/../python/src2/hdr_parser.py
 set(scripts_gen_javadoc "${CMAKE_CURRENT_SOURCE_DIR}/generator/gen_javadoc.py")
 set(scripts_rst_parser "${CMAKE_CURRENT_SOURCE_DIR}/generator/rst_parser.py")
 
+# arguments for gen_java
+if(ANDROID)
+	set(arguments_gen_java "true")
+else()
+	set(arguments_gen_java "false")
+endif()
+
+# handwritten Java sources which need configuration
+file(GLOB handwritten_java_config_sources "${CMAKE_CURRENT_SOURCE_DIR}/generator/src/java/*.java")
+set(configured_java_source_dir "${CMAKE_CURRENT_BINARY_DIR}/configured_java_out")
+file(REMOVE_RECURSE "${configured_java_source_dir}/")
+file(MAKE_DIRECTORY "${configured_java_source_dir}")
+foreach(config_source ${handwritten_java_config_sources})
+  get_filename_component(config_source_name "${config_source}" NAME)
+  configure_file("${config_source}" "${configured_java_source_dir}/${config_source_name}")
+endforeach()
+
 # handwritten C/C++ and Java sources
 file(GLOB handwrittren_h_sources "${CMAKE_CURRENT_SOURCE_DIR}/generator/src/cpp/*.h" "${CMAKE_CURRENT_SOURCE_DIR}/generator/src/cpp/*.hpp")
 file(GLOB handwrittren_cpp_sources "${CMAKE_CURRENT_SOURCE_DIR}/generator/src/cpp/*.cpp")
-file(GLOB handwrittren_java_sources "${CMAKE_CURRENT_SOURCE_DIR}/generator/src/java/*.java")
+file(GLOB handwrittren_java_sources "${configured_java_source_dir}/*.java")
 file(GLOB handwrittren_aidl_sources  "${CMAKE_CURRENT_SOURCE_DIR}/generator/src/java/*.aidl")
 if(NOT ANDROID)
   ocv_list_filterout(handwrittren_java_sources "/(engine|android)\\\\+")
@@ -100,7 +118,7 @@ foreach(module ${OPENCV_JAVA_MODULES})
   # first run of gen_java.py (to get list of generated files)
   file(REMOVE_RECURSE "${CMAKE_CURRENT_BINARY_DIR}/gen_java_out/")
   file(MAKE_DIRECTORY "${CMAKE_CURRENT_BINARY_DIR}/gen_java_out")
-  execute_process(COMMAND ${PYTHON_EXECUTABLE} "${scripts_gen_java}" "${scripts_hdr_parser}" ${module} ${opencv_public_headers_${module}}
+  execute_process(COMMAND ${PYTHON_EXECUTABLE} "${scripts_gen_java}" ${arguments_gen_java} ${native_module_output_name} "${scripts_hdr_parser}" ${module} ${opencv_public_headers_${module}}
                   WORKING_DIRECTORY "${CMAKE_CURRENT_BINARY_DIR}/gen_java_out"
                   OUTPUT_QUIET ERROR_QUIET)
   unset(generated_java_sources_${module})
@@ -124,7 +142,7 @@ set(step1_depends "${scripts_gen_java}" "${scripts_hdr_parser}" ${opencv_public_
 foreach(module ${OPENCV_JAVA_MODULES})
   # second run of gen_java.py (at build time)
   add_custom_command(OUTPUT ${generated_java_sources_${module}} "${CMAKE_CURRENT_BINARY_DIR}/${module}.cpp"
-                     COMMAND ${PYTHON_EXECUTABLE} "${scripts_gen_java}" "${scripts_hdr_parser}" ${module} ${opencv_public_headers_${module}}
+                     COMMAND ${PYTHON_EXECUTABLE} "${scripts_gen_java}" ${arguments_gen_java} ${native_module_output_name} "${scripts_hdr_parser}" ${module} ${opencv_public_headers_${module}}
                      WORKING_DIRECTORY ${CMAKE_CURRENT_BINARY_DIR}
                      DEPENDS "${scripts_gen_java}" "${scripts_hdr_parser}" ${opencv_public_headers_${module}}
                     )
@@ -134,7 +152,7 @@ endforeach()
 set(step2_depends ${step1_depends} ${scripts_gen_javadoc} ${scripts_rst_parser} ${javadoc_rst_sources} ${generated_java_sources} ${handwrittren_java_sources})
 string(REPLACE ";" "," OPENCV_JAVA_MODULES_STR "${OPENCV_JAVA_MODULES}")
 add_custom_command(OUTPUT ${documented_java_files}
-                   COMMAND ${PYTHON_EXECUTABLE} "${scripts_gen_javadoc}" --modules ${OPENCV_JAVA_MODULES_STR} "${CMAKE_CURRENT_SOURCE_DIR}/generator/src/java" "${CMAKE_CURRENT_BINARY_DIR}" 2>"${CMAKE_CURRENT_BINARY_DIR}/get_javadoc_errors.log"
+                   COMMAND ${PYTHON_EXECUTABLE} "${scripts_gen_javadoc}" --modules ${OPENCV_JAVA_MODULES_STR} "${configured_java_source_dir}/java" "${CMAKE_CURRENT_BINARY_DIR}" 2>"${CMAKE_CURRENT_BINARY_DIR}/get_javadoc_errors.log"
                    WORKING_DIRECTORY ${CMAKE_CURRENT_BINARY_DIR}
                    DEPENDS ${step2_depends}
                   )
@@ -294,7 +312,7 @@ endif()
 
 # Additional target properties
 set_target_properties(${the_module} PROPERTIES
-    OUTPUT_NAME "${the_module}${OPENCV_DLLVERSION}"
+    OUTPUT_NAME "${native_module_output_name}"
     DEBUG_POSTFIX "${OPENCV_DEBUG_POSTFIX}"
     ARCHIVE_OUTPUT_DIRECTORY ${LIBRARY_OUTPUT_PATH}
     RUNTIME_OUTPUT_DIRECTORY ${EXECUTABLE_OUTPUT_PATH}

modules/java/generator/gen_java.py
@@ -655,7 +655,9 @@ class FuncFamilyInfo(object):
 
 
 class JavaWrapperGenerator(object):
-    def __init__(self):
+    def __init__(self, generate_for_android, native_module_name):
+        self.generate_for_android = generate_for_android
+        self.native_module_name = native_module_name
         self.clear()
 
     def clear(self):
@@ -719,12 +721,13 @@ $imports
 public class %(jc)s {
 """ % { 'm' : self.module, 'jc' : jname } )
 
-#        self.java_code[class_name]["jn_code"].write("""
-#    //
-#    // native stuff
-#    //
-#    static { System.loadLibrary("opencv_java"); }
-#""" )
+        if not self.generate_for_android:
+            self.java_code[class_name]["jn_code"].write("""
+    //
+    // native stuff
+    //
+    static { System.loadLibrary("%(n)s"); }
+""" % { 'n' : self.native_module_name } )
 
 
 
@@ -1388,19 +1391,21 @@ if __name__ == "__main__":
     if len(sys.argv) < 4:
         print "Usage:\n", \
             os.path.basename(sys.argv[0]), \
-            "<full path to hdr_parser.py> <module name> <C++ header> [<C++ header>...]"
+            "<android> <native module name> <full path to hdr_parser.py> <module name> <C++ header> [<C++ header>...]"
         print "Current args are: ", ", ".join(["'"+a+"'" for a in sys.argv])
         exit(0)
 
+    generate_for_android = sys.argv[1].lower() == "true"
+    native_module_name = sys.argv[2]
     dstdir = "."
-    hdr_parser_path = os.path.abspath(sys.argv[1])
+    hdr_parser_path = os.path.abspath(sys.argv[3])
     if hdr_parser_path.endswith(".py"):
         hdr_parser_path = os.path.dirname(hdr_parser_path)
     sys.path.append(hdr_parser_path)
     import hdr_parser
-    module = sys.argv[2]
-    srcfiles = sys.argv[3:]
+    module = sys.argv[4]
+    srcfiles = sys.argv[5:]
     #print "Generating module '" + module + "' from headers:\n\t" + "\n\t".join(srcfiles)
-    generator = JavaWrapperGenerator()
+    generator = JavaWrapperGenerator(generate_for_android, native_module_name)
     generator.gen(srcfiles, module, dstdir)
 

modules/java/generator/src/java/core+Mat.java
@@ -1081,6 +1081,17 @@ public class Mat {
     public long getNativeObjAddr() {
         return nativeObj;
     }
+	
+    private static boolean isAndroid() {
+        try {
+            Class.forName("org.opencv.android.OpenCVLoader");
+            return true;
+        } catch(ClassNotFoundException e) {
+            return false;
+        }
+    }
+    
+    static { if (!isAndroid()) { System.loadLibrary("@native_module_output_name@"); } }
 
     // C++: Mat::Mat()
     private static native long n_Mat();

modules/java/generator/src/java/highgui+VideoCapture.java
@@ -193,6 +193,17 @@ public class VideoCapture {
         n_delete(nativeObj);
         super.finalize();
     }
+    
+    private static boolean isAndroid() {
+        try {
+            Class.forName("org.opencv.android.OpenCVLoader");
+            return true;
+        } catch(ClassNotFoundException e) {
+            return false;
+        }
+    }
+    
+    static { if (!isAndroid()) { System.loadLibrary("%native_module_output_name%"); } }
 
     // C++: VideoCapture::VideoCapture()
     private static native long n_VideoCapture();

commit 055a5d022513865aca0b63c7d407a97133ae8dce

Fixed java binding build issues introduced in 38f3cc26.

modules/java/CMakeLists.txt
@@ -41,20 +41,10 @@ else()
 	set(arguments_gen_java "false")
 endif()
 
-# handwritten Java sources which need configuration
-file(GLOB handwritten_java_config_sources "${CMAKE_CURRENT_SOURCE_DIR}/generator/src/java/*.java")
-set(configured_java_source_dir "${CMAKE_CURRENT_BINARY_DIR}/configured_java_out")
-file(REMOVE_RECURSE "${configured_java_source_dir}/")
-file(MAKE_DIRECTORY "${configured_java_source_dir}")
-foreach(config_source ${handwritten_java_config_sources})
-  get_filename_component(config_source_name "${config_source}" NAME)
-  configure_file("${config_source}" "${configured_java_source_dir}/${config_source_name}")
-endforeach()
-
 # handwritten C/C++ and Java sources
 file(GLOB handwrittren_h_sources "${CMAKE_CURRENT_SOURCE_DIR}/generator/src/cpp/*.h" "${CMAKE_CURRENT_SOURCE_DIR}/generator/src/cpp/*.hpp")
 file(GLOB handwrittren_cpp_sources "${CMAKE_CURRENT_SOURCE_DIR}/generator/src/cpp/*.cpp")
-file(GLOB handwrittren_java_sources "${configured_java_source_dir}/*.java")
+file(GLOB handwrittren_java_sources "${CMAKE_CURRENT_SOURCE_DIR}/generator/src/java/*.java")
 file(GLOB handwrittren_aidl_sources  "${CMAKE_CURRENT_SOURCE_DIR}/generator/src/java/*.aidl")
 if(NOT ANDROID)
   ocv_list_filterout(handwrittren_java_sources "/(engine|android)\\\\+")
@@ -152,12 +142,12 @@ endforeach()
 set(step2_depends ${step1_depends} ${scripts_gen_javadoc} ${scripts_rst_parser} ${javadoc_rst_sources} ${generated_java_sources} ${handwrittren_java_sources})
 string(REPLACE ";" "," OPENCV_JAVA_MODULES_STR "${OPENCV_JAVA_MODULES}")
 add_custom_command(OUTPUT ${documented_java_files}
-                   COMMAND ${PYTHON_EXECUTABLE} "${scripts_gen_javadoc}" --modules ${OPENCV_JAVA_MODULES_STR} "${configured_java_source_dir}/java" "${CMAKE_CURRENT_BINARY_DIR}" 2>"${CMAKE_CURRENT_BINARY_DIR}/get_javadoc_errors.log"
+                   COMMAND ${PYTHON_EXECUTABLE} "${scripts_gen_javadoc}" --modules ${OPENCV_JAVA_MODULES_STR} "${CMAKE_CURRENT_SOURCE_DIR}/generator/src/java" "${CMAKE_CURRENT_BINARY_DIR}" 2>"${CMAKE_CURRENT_BINARY_DIR}/get_javadoc_errors.log"
                    WORKING_DIRECTORY ${CMAKE_CURRENT_BINARY_DIR}
                    DEPENDS ${step2_depends}
                   )
 
-# step 3: copy files to destination
+# step 3: copy files to destination (replacing %native_module_output_name% with "java_opencvNNN")
 set(step3_input_files ${documented_java_files} ${handwrittren_aidl_sources})
 set(copied_files "")
 foreach(java_file ${step3_input_files})
@@ -167,7 +157,11 @@ foreach(java_file ${step3_input_files})
   set(output_name "${OpenCV_BINARY_DIR}/src/org/opencv/${java_file_name}")
 
   add_custom_command(OUTPUT "${output_name}"
-                     COMMAND ${CMAKE_COMMAND} -E copy "${java_file}" "${output_name}"
+                     COMMAND ${CMAKE_COMMAND} 
+                        -D INPUT_FILE="${java_file}" 
+                        -D OUTPUT_FILE="${output_name}" 
+                        -D native_module_output_name="${native_module_output_name}"
+                        -P "${CMAKE_CURRENT_SOURCE_DIR}/ConfigureJavaSourceFile.cmake"
                      MAIN_DEPENDENCY "${java_file}"
                      DEPENDS ${step2_depends}
                      COMMENT "Generating src/org/opencv/${java_file_name}"

modules/java/ConfigureJavaSourceFile.cmake
@@ -1,0 +1,3 @@
+# ConfigureJavaSourceFile.cmake:
+# Needs to be called with -D INPUT_FILE=input_file -D OUTPUT_FILE=output_file -D native_module_output_name=opencv_javaNNN
+configure_file(${INPUT_FILE} ${OUTPUT_FILE} @ONLY)