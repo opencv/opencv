d2591acd767012d8218e044acc537ca832553e6d
Elias Holzer <elias@vvvv.org>
2013-03-01 21:19:40 +0100

    Added HelloCV example for Mono / .NET.

---
+++ samples/mono/VS2010/HelloCV/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using org.opencv.core;
+using System.IO;
+
+namespace HelloCV
+{
+    /// <summary>
+    /// Demonstrates usage of Mono / .NET bindings for OpenCV.
+    /// opencv-mono.dll and IKVM.OpenJDK.Core can be found in OpenCV/mono.
+    /// opencv_javaNNN.dll can be found in OpenCV/java.
+    /// opencv_javaNNN.dll must either be in the output path or reachable
+    /// through PATH environment variable.
+    /// </summary>
+    class Program
+    {
+        static Program()
+        {
+            // In case opencv_javaNNN.dll is NOT located in the output path extend
+            // the process local PATH environment variable before accessing any 
+            // OpenCV type.
+            /*
+            var path = Environment.GetEnvironmentVariable("PATH");
+            var openCVInstallPath = "path/to/opencv";
+            Environment.SetEnvironmentVariable(
+                "PATH", 
+                path + Path.PathSeparator + Path.Combine(openCVInstallPath, "java")
+            );
+            */
+        }
+
+        static void Main(string[] args)
+        {
+            var mat = Mat.eye(3, 3, CvType.CV_8UC1);
+            Console.WriteLine("m = {0}", mat);
+            Console.ReadLine();
+        }
+    }
+}
---
+++ samples/mono/VS2010/HelloCV/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("HelloCV")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("HelloCV")]
+[assembly: AssemblyCopyright("Copyright ©  2013")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("fe1fcf96-e2f8-4774-ad6e-7d0722d873ea")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers 
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
