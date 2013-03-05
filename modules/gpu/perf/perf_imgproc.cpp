ade0300dc4a62aae67f685deb4bbbac55a2a6af6
Josh Klontz <josh.klontz@gmail.com>
2013-03-05 08:46:58 -0500

    fixed casts

--- modules/gpu/perf/perf_imgproc.cpp
+++ modules/gpu/perf/perf_imgproc.cpp
@@ -260,7 +260,7 @@ PERF_TEST_P(Sz_Depth_Cn_Inter_Border, ImgProc_WarpPerspective,
     declare.in(src, WARMUP_RNG);
 
     const double aplha = CV_PI / 4;
-    double mat[3][3] = { {std::cos(aplha), -std::sin(aplha), src.cols / 2.0},
+    double mat[3][3] = { {std::cos(aplha), -std::sin(aplha), (double)(src.cols / 2)},
                          {std::sin(aplha),  std::cos(aplha), 0.0},
                          {0.0,              0.0,             1.0}};
     const cv::Mat M(3, 3, CV_64F, (void*) mat);
