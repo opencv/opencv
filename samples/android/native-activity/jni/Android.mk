dc2bca07720392c9a62026bbd5cd5d5a605f6a2d
Alexander Smorkalov <alexander.smorkalov@itseez.com>
2013-03-07 15:13:30 +0400

    Initialization with Manager instead static link added.

--- samples/android/native-activity/jni/Android.mk
+++ samples/android/native-activity/jni/Android.mk
@@ -2,6 +2,8 @@ LOCAL_PATH := $(call my-dir)
 
 include $(CLEAR_VARS)
 
+include ../../sdk/native/jni/OpenCV.mk
+
 LOCAL_MODULE    := native_activity
 LOCAL_SRC_FILES := native.cpp
 LOCAL_LDLIBS    := -lm -llog -landroid
