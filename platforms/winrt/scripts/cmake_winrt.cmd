429cd85b46f85418a3fea5a6e2d70e9ccfba9639
Alexander Smorkalov <alexander.smorkalov@itseez.com>
2013-03-20 12:12:58 -0700

    More accurate guards for WinRT features in CMake.

--- platforms/winrt/scripts/cmake_winrt.cmd
+++ platforms/winrt/scripts/cmake_winrt.cmd
@@ -3,6 +3,4 @@ cd build
 
 rem call "C:\Program Files\Microsoft Visual Studio 11.0\VC\bin\x86_arm\vcvarsx86_arm.bat"
 
-SET PATH=C:\Program Files\Ninja;%PATH%
-
-"C:\Program Files\CMake 2.8\bin\cmake.exe" -GNinja -DCMAKE_BUILD_TYPE=Release -DWITH_TIFF=OFF -DWITH_FFMPEG=OFF -DBUILD_opencv_gpu=OFF -DENABLE_SSE=OFF -DENABLE_SSE2=OFF ..\..\..
+cmake.exe -GNinja -DWITH_FFMPEG=OFF -DBUILD_opencv_gpu=OFF -DBUILD_opencv_python=OFF -DCMAKE_TOOLCHAIN_FILE=..\..\winrt\arm.winrt.toolchain.cmake ..\..\..
