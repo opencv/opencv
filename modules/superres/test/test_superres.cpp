9b3c318e8536ce58d3007d0b298c338b56aecc56
peng xiao <hisenxpress@gmail.com>
2013-06-26 08:43:23 +0800

    Add super resolution's OpenCL implementation. Accuracy and performance tests are also provided.

--- modules/superres/test/test_superres.cpp
+++ modules/superres/test/test_superres.cpp
@@ -274,5 +274,12 @@ TEST_F(SuperResolution, BTVL1_GPU)
 {
     RunTest(cv::superres::createSuperResolution_BTVL1_GPU());
 }
-
 #endif
+#if defined(HAVE_OPENCV_OCL) && defined(HAVE_OPENCL)
+TEST_F(SuperResolution, BTVL1_OCL)
+{
+    std::vector<cv::ocl::Info> infos;
+    cv::ocl::getDevice(infos);
+    RunTest(cv::superres::createSuperResolution_BTVL1_OCL());
+}
+#endif 
