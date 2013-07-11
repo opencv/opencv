06742247be51fbe8cf600b683b92cd52dbaca511
Roman Donchenko <roman.donchenko@itseez.com>
2013-07-11 14:26:47 +0400

    Removed unused PACKAGE_* macros.

--- cmake/templates/cvconfig.h.cmake
+++ cmake/templates/cvconfig.h.cmake
@@ -91,24 +91,6 @@
 /* OpenCV compiled as static or dynamic libs */
 #cmakedefine  BUILD_SHARED_LIBS
 
-/* Name of package */
-#define  PACKAGE "${PACKAGE}"
-
-/* Define to the address where bug reports for this package should be sent. */
-#define  PACKAGE_BUGREPORT "${PACKAGE_BUGREPORT}"
-
-/* Define to the full name of this package. */
-#define  PACKAGE_NAME "${PACKAGE_NAME}"
-
-/* Define to the full name and version of this package. */
-#define  PACKAGE_STRING "${PACKAGE_STRING}"
-
-/* Define to the one symbol short name of this package. */
-#define  PACKAGE_TARNAME "${PACKAGE_TARNAME}"
-
-/* Define to the version of this package. */
-#define  PACKAGE_VERSION "${PACKAGE_VERSION}"
-
 /* Version number of package */
 #define  VERSION "${PACKAGE_VERSION}"
 
