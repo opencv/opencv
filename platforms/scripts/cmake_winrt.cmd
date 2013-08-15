ddbeac503cc4bc8cc8a6664b65c16f5bb8be49a6
Alexander Smorkalov <alexander.smorkalov@itseez.com>
2013-08-15 06:01:38 -0700

    WindowsRT availability check reorganized. Build script updated.

--- platforms/scripts/cmake_winrt.cmd
+++ platforms/scripts/cmake_winrt.cmd
@@ -1,6 +1,9 @@
 mkdir build_winrt_arm
 cd build_winrt_arm
 
-rem call "C:\Program Files\Microsoft Visual Studio 11.0\VC\bin\x86_arm\vcvarsx86_arm.bat"
+set msvc_path=C:\Program Files\Microsoft Visual Studio 11.0
 
-cmake.exe -GNinja -DWITH_TBB=ON -DBUILD_TBB=ON -DCMAKE_BUILD_TYPE=Release -DWITH_FFMPEG=OFF -DBUILD_opencv_gpu=OFF -DBUILD_opencv_python=OFF -DCMAKE_TOOLCHAIN_FILE=..\winrt\arm.winrt.toolchain.cmake ..\..
+call "%msvc_path%\Common7\Tools\VsDevCmd.bat"
+call "%msvc_path%\VC\bin\x86_arm\vcvarsx86_arm.bat"
+
+cmake.exe -GNinja -DCMAKE_BUILD_TYPE=Release -DENABLE_WINRT_MODE=ON -DWITH_FFMPEG=OFF -DWITH_MSMF=OFF -DWITH_DSHOW=OFF -DWITH_VFW=OFF -DWITH_TIFF=OFF -DWITH_OPENEXR=OFF -DWITH_CUDA=OFF -DBUILD_opencv_gpu=OFF -DBUILD_opencv_python=OFF -DBUILD_opencv_java=OFF -DCMAKE_TOOLCHAIN_FILE=..\winrt\arm.winrt.toolchain.cmake  %* ..\..
