2cfd635e9e4d2c4aafbb9c3e4e593f19e9ddce7d
Alexander Smorkalov <alexander.smorkalov@itseez.com>
2013-08-29 03:42:30 -0700

    Image processing sample for WinRT added.

---
+++ samples/winrt/OcvImageProcessing/OcvImageProcessing/MainPage.xaml.h
@@ -0,0 +1,27 @@
+﻿//
+// MainPage.xaml.h
+// Declaration of the MainPage class.
+//
+
+#pragma once
+
+#include "MainPage.g.h"
+
+namespace OcvImageProcessing
+{
+	/// <summary>
+	/// An empty page that can be used on its own or navigated to within a Frame.
+	/// </summary>
+	public ref class MainPage sealed
+	{
+	public:
+		MainPage();
+
+	protected:
+		virtual void OnNavigatedTo(Windows::UI::Xaml::Navigation::NavigationEventArgs^ e) override;
+	private:
+		void Button_Click(Platform::Object^ sender, Windows::UI::Xaml::RoutedEventArgs^ e);
+
+		unsigned int frameWidth, frameHeight;
+	};
+}
