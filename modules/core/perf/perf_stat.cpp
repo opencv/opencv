f4aa7ff919e6a017226d5fa1f2f4fd23599f895c
kdrobnyh <klim.drobnyh@gmail.com>
2013-08-29 21:05:35 +0400

    Add IPP support in meanStdDev, minMaxIdx, norm functions

--- modules/core/perf/perf_stat.cpp
+++ modules/core/perf/perf_stat.cpp
@@ -83,8 +83,8 @@ PERF_TEST_P(Size_MatType, meanStdDev_mask, TYPICAL_MATS)
 
     TEST_CYCLE() meanStdDev(src, mean, dev, mask);
 
-    SANITY_CHECK(mean, 1e-6);
-    SANITY_CHECK(dev, 1e-6);
+    SANITY_CHECK(mean, 1e-5);
+    SANITY_CHECK(dev, 1e-5);
 }
 
 PERF_TEST_P(Size_MatType, countNonZero, testing::Combine( testing::Values( TYPICAL_MAT_SIZES ), testing::Values( CV_8UC1, CV_8SC1, CV_16UC1, CV_16SC1, CV_32SC1, CV_32FC1, CV_64FC1 ) ))
