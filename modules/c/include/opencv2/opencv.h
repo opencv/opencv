62f8502e716c7eb14ebd12e774895ae4e4802a39
Arjun Comar <nrujac@gmail.com>
2013-10-09 00:15:57 -0400

    Added Size.

--- modules/c/include/opencv2/opencv.h
+++ modules/c/include/opencv2/opencv.h
@@ -3,4 +3,6 @@
 #include <opencv2/c/opencv_generated.hpp>
 #include <opencv2/c/mat.hpp>
 #include <opencv2/c/point.hpp>
+#include <opencv2/c/rect.hpp>
+#include <opencv2/c/size.hpp>
 #endif //__OPENCV_C_H
