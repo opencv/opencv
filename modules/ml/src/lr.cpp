378313921ee9588c8caf9a19b5f42088873d03aa
Rahul Kavi <leorahul16@gmail.com>
2013-11-05 06:05:29 -0500

    fixed white space in logistic regression classifier

--- modules/ml/src/lr.cpp
+++ modules/ml/src/lr.cpp
@@ -177,7 +177,6 @@ bool LogisticRegression::train(cv::InputArray data_ip, cv::InputArray labels_ip)
         {
             new_local_labels = (labels_l == it->second)/255;
             new_local_labels.convertTo(labels, CV_32F);
-            
             // new_theta = compute_batch_gradient(data_t, labels, init_theta);
             // currently supported training methods LogisticRegression::BATCH and LogisticRegression::MINI_BATCH
             if(this->params.train_method == LogisticRegression::BATCH)
