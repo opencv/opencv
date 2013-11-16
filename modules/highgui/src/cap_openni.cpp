f4ad267f5dc8423ffd02d1c671fcb6198616702c
Anatoly Baksheev <no@email>
2013-11-16 21:04:21 +0400

    fixed warnings

--- modules/highgui/src/cap_openni.cpp
+++ modules/highgui/src/cap_openni.cpp
@@ -171,6 +171,9 @@ public:
     xn::ImageGenerator &imageGenerator;
 
 private:
+    ApproximateSyncGrabber(const ApproximateSyncGrabber&);
+    ApproximateSyncGrabber& operator=(const ApproximateSyncGrabber&);
+
     int maxBufferSize;
     bool isCircleBuffer;
     int maxTimeDuration;
@@ -214,7 +217,7 @@ private:
         virtual bool grab( xn::DepthMetaData& depthMetaData,
                            xn::ImageMetaData& imageMetaData )
         {
-            while(1)
+            for(;;)
             {
                 if( !isDepthFilled )
                     isDepthFilled = popDepthMetaData(depth);
@@ -951,7 +954,7 @@ double CvCapture_OpenNI::getDepthGeneratorProperty( int propIdx )
         propValue = depthGenerator.GetAlternativeViewPointCap().IsViewPointAs(imageGenerator) ? 1.0 : 0.0;
         break;
     case CV_CAP_PROP_POS_MSEC :
-        propValue = depthGenerator.GetTimestamp();
+        propValue = (double)depthGenerator.GetTimestamp();
         break;
     case CV_CAP_PROP_POS_FRAMES :
         propValue = depthGenerator.GetFrameID();
@@ -1039,7 +1042,7 @@ double CvCapture_OpenNI::getImageGeneratorProperty( int propIdx )
             propValue = mode.nFPS;
         break;
     case CV_CAP_PROP_POS_MSEC :
-        propValue = imageGenerator.GetTimestamp();
+        propValue = (double)imageGenerator.GetTimestamp();
         break;
     case CV_CAP_PROP_POS_FRAMES :
         propValue = imageGenerator.GetFrameID();
