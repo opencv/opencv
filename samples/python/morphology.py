5566f09bcac677b9cb5fde0a95dd682c4fae80c0
StevenPuttemans <steven.puttemans@kuleuven.be>
2013-11-21 16:45:48 +0100

    Applied feature #3375 and fix of pull 1842 to avoid merging problems later

--- samples/python/morphology.py
+++ samples/python/morphology.py
@@ -31,7 +31,7 @@ if __name__ == "__main__":
     if len(sys.argv) > 1:
         src = cv.LoadImage(sys.argv[1], cv.CV_LOAD_IMAGE_COLOR)
     else:
-        url = 'http://code.opencv.org/projects/opencv/repository/revisions/master/raw/samples/c/fruits.jpg'
+        url = 'https://raw.github.com/Itseez/opencv/master/samples/c/fruits.jpg'
         filedata = urllib2.urlopen(url).read()
         imagefiledata = cv.CreateMatHeader(1, len(filedata), cv.CV_8UC1)
         cv.SetData(imagefiledata, filedata, len(filedata))
