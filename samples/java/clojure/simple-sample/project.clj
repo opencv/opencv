ae52cd0c3d94c08c476ba9c9db2239c71b8b79b3
Mimmmo Cosenza <mimmo.cosenza@gmail.com>
2013-12-10 15:49:48 +0100

    add main setting

--- samples/java/clojure/simple-sample/project.clj
+++ samples/java/clojure/simple-sample/project.clj
@@ -10,4 +10,5 @@
   :dependencies [[org.clojure/clojure "1.5.1"]
                  [opencv/opencv "2.4.7"]
                  [opencv/opencv-native "2.4.7"]]
+  :main simple-sample.core
   :injections [(clojure.lang.RT/loadLibrary org.opencv.core.Core/NATIVE_LIBRARY_NAME)])
