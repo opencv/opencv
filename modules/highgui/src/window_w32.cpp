734bf8babd1b365401bda9c0ab33ee8cbd780254
Andrey Pavlenko <andrey.pavlenko@itseez.com>
2013-12-26 15:49:12 +0400

    removing legacy stuff

--- modules/highgui/src/window_w32.cpp
+++ modules/highgui/src/window_w32.cpp
@@ -62,10 +62,6 @@
 #  pragma GCC diagnostic ignored "-Wmissing-declarations"
 #endif
 
-#if defined(_MSC_VER) && (_MSC_VER < 1700)
-#include <MultiMon.h>
-#endif
-
 #include <commctrl.h>
 #include <winuser.h>
 #include <stdlib.h>
