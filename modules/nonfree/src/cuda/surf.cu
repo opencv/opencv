c48777a1c39e66dc38a809047ba8764e3be354b6
Alexander Smorkalov <alexander.smorkalov@itseez.com>
2013-12-27 11:18:10 +0400

    CUDA dependency in nonfree nodule removed. OpenCV.mk generation fixed.

--- modules/nonfree/src/cuda/surf.cu
+++ modules/nonfree/src/cuda/surf.cu
@@ -42,7 +42,7 @@
 
 #include "opencv2/opencv_modules.hpp"
 
-#ifdef HAVE_OPENCV_GPU
+#if defined(HAVE_OPENCV_GPU) && !defined(ANDROID)
 
 #include "opencv2/gpu/device/common.hpp"
 #include "opencv2/gpu/device/limits.hpp"
