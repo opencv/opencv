e844c08203fa84b9cbe5271490c7e2c3789cb463
Ilya Lavrenov <ilya.lavrenov@itseez.com>
2014-02-01 23:34:54 +0400

    some fixes and improvements in cv::matchTemplate

--- modules/core/include/opencv2/core/opencl/ocl_defs.hpp
+++ modules/core/include/opencv2/core/opencl/ocl_defs.hpp
@@ -5,8 +5,6 @@
 // Copyright (C) 2014, Advanced Micro Devices, Inc., all rights reserved.
 // Third party copyrights are property of their respective owners.
 
-#define CV_OPENCL_RUN_ASSERT
-
 #ifdef HAVE_OPENCL
 
 #ifdef CV_OPENCL_RUN_VERBOSE
