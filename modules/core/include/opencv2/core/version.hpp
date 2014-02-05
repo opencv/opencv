61f824a840e63c5a4de6497f176526bd368c8b5a
Roman Donchenko <roman.donchenko@itseez.com>
2014-02-05 13:57:04 +0400

    Set status to -tp2.

--- modules/core/include/opencv2/core/version.hpp
+++ modules/core/include/opencv2/core/version.hpp
@@ -51,7 +51,7 @@
 #define CV_VERSION_MAJOR    3
 #define CV_VERSION_MINOR    0
 #define CV_VERSION_REVISION 0
-#define CV_VERSION_STATUS   "-dev"
+#define CV_VERSION_STATUS   "-tp2"
 
 #define CVAUX_STR_EXP(__A)  #__A
 #define CVAUX_STR(__A)      CVAUX_STR_EXP(__A)
