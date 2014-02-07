9f072fe1d9f31f330ac4071d383d54be76b245a8
Stuart Cunningham <stuartc@hobbes.(none)>
2014-02-07 23:12:23 +1100

    Fix build warning on Windows

--- modules/highgui/test/test_grfmt.cpp
+++ modules/highgui/test/test_grfmt.cpp
@@ -453,7 +453,7 @@ TEST(Highgui_Tiff, write_read_16bit_big_little_endian)
 
         // Write sample TIFF file
         FILE* fp = fopen(filename.c_str(), "wb");
-        ASSERT_TRUE(fp);
+        ASSERT_TRUE(fp != NULL);
         ASSERT_EQ((size_t)1, fwrite(tiff_sample_data, 86, 1, fp));
         fclose(fp);
 
