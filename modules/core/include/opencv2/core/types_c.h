510680a5df77f520449e0e74680486e00e88fb96
Ilya Lavrenov <ilya.lavrenov@itseez.com>
2014-02-16 01:42:02 +0400

    typo

--- modules/core/include/opencv2/core/types_c.h
+++ modules/core/include/opencv2/core/types_c.h
@@ -245,7 +245,7 @@ enum {
  CV_StsVecLengthErr=           -28, /* incorrect vector length */
  CV_StsFilterStructContentErr= -29, /* incorr. filter structure content */
  CV_StsKernelStructContentErr= -30, /* incorr. transform kernel content */
- CV_StsFilterOffsetErr=        -31, /* incorrect filter ofset value */
+ CV_StsFilterOffsetErr=        -31, /* incorrect filter offset value */
  CV_StsBadSize=                -201, /* the input/output structure size is incorrect  */
  CV_StsDivByZero=              -202, /* division by zero */
  CV_StsInplaceNotSupported=    -203, /* in-place operation is not supported */
