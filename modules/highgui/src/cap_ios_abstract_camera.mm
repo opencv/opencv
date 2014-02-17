cc529d971b7fb6b459c9cc5acde44cf1afded5d9
Alexander Shishkov <alexander.shishkov@itseez.com>
2014-02-17 17:37:02 +0400

    removed tabs

--- modules/highgui/src/cap_ios_abstract_camera.mm
+++ modules/highgui/src/cap_ios_abstract_camera.mm
@@ -279,20 +279,20 @@
     self.captureVideoPreviewLayer = [[AVCaptureVideoPreviewLayer alloc] initWithSession:self.captureSession];
 
     if ([self.captureVideoPreviewLayer respondsToSelector:@selector(connection)])
-	{
-		if ([self.captureVideoPreviewLayer.connection isVideoOrientationSupported])
-		{
-			[self.captureVideoPreviewLayer.connection setVideoOrientation:self.defaultAVCaptureVideoOrientation];
-		}
-	}
-	else
-	{
-		// Deprecated in 6.0; here for backward compatibility
-		if ([self.captureVideoPreviewLayer isOrientationSupported])
-		{
-			[self.captureVideoPreviewLayer setOrientation:self.defaultAVCaptureVideoOrientation];
-		}                
-	}
+    {
+        if ([self.captureVideoPreviewLayer.connection isVideoOrientationSupported])
+        {
+            [self.captureVideoPreviewLayer.connection setVideoOrientation:self.defaultAVCaptureVideoOrientation];
+        }
+    }
+    else
+    {
+        // Deprecated in 6.0; here for backward compatibility
+        if ([self.captureVideoPreviewLayer isOrientationSupported])
+        {
+            [self.captureVideoPreviewLayer setOrientation:self.defaultAVCaptureVideoOrientation];
+        }                
+    }
 
     if (parentView != nil) {
         self.captureVideoPreviewLayer.frame = self.parentView.bounds;
@@ -301,9 +301,6 @@
     }
     NSLog(@"[Camera] created AVCaptureVideoPreviewLayer");
 }
-
-
-
 
 - (void)setDesiredCameraPosition:(AVCaptureDevicePosition)desiredPosition;
 {
