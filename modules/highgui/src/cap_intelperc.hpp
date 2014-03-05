d4a1df85be5bbda269e6d877c7dfa6bb00d890fc
vbystricky <user@user-pc.(none)>
2014-03-05 13:06:36 +0400

    Change text of copyright

--- modules/highgui/src/cap_intelperc.hpp
+++ modules/highgui/src/cap_intelperc.hpp
@@ -4,7 +4,7 @@
 // It is subject to the license terms in the LICENSE file found in the top-level directory
 // of this distribution and at http://opencv.org/license.html.
 //
-// Copyright (C) 2014, OpenCV Foundation, all rights reserved.
+// Copyright (C) 2014, Itseez, Inc., all rights reserved.
 // Third party copyrights are property of their respective owners.
 //
 //M*/
