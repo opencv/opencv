0cf3f8b6022899b742cf02dc8676632e833f8fea
Ilya Lavrenov <ilya.lavrenov@itseez.com>
2014-03-06 16:49:22 +0400

    disable SURF

--- modules/nonfree/src/surf.ocl.cpp
+++ modules/nonfree/src/surf.ocl.cpp
@@ -90,7 +90,7 @@ bool SURF_OCL::init(const SURF* p)
                 return false;
             haveImageSupport = false;//dev.imageSupport();
             kerOpts = haveImageSupport ? "-D HAVE_IMAGE2D -D DOUBLE_SUPPORT" : "";
-            status = 1;
+//            status = 1;
         }
     }
     return status > 0;
