320962185ff2c67b5f71c85a50ede363ffd9bc00
ahb <andreas.bihlmaier@gmx.de>
2014-03-26 10:07:51 +0100

    add the usual include-all-of-module header

---
+++ modules/cvv/include/opencv2/cvv/cvv.hpp
@@ -0,0 +1,6 @@
+#include <opencv2/cvv/call_meta_data.hpp>
+#include <opencv2/cvv/debug_mode.hpp>
+#include <opencv2/cvv/dmatch.hpp>
+#include <opencv2/cvv/filter.hpp>
+#include <opencv2/cvv/final_show.hpp>
+#include <opencv2/cvv/show_image.hpp>
