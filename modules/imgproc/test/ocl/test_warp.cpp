b0462102960572ab622b45e8ec8aadd52a00d9ae
Alexander Karsakov <alexander.karsakov@itseez.com>
2014-04-28 11:37:46 +0400

    Changed epsilon for hog test, because hog detector is sensible to resize accuracy.

--- modules/imgproc/test/ocl/test_warp.cpp
+++ modules/imgproc/test/ocl/test_warp.cpp
@@ -184,8 +184,7 @@ PARAM_TEST_CASE(Resize, MatType, double, double, Interpolation, bool, int)
 
         Size srcRoiSize = randomSize(1, MAX_VALUE), dstRoiSize;
         // Make sure the width is a multiple of the requested value, and no more
-        srcRoiSize.width &= ~((widthMultiple * 2) - 1);
-        srcRoiSize.width += widthMultiple;
+        srcRoiSize.width += widthMultiple - 1 - (srcRoiSize.width - 1) % widthMultiple;
         dstRoiSize.width = cvRound(srcRoiSize.width * fx);
         dstRoiSize.height = cvRound(srcRoiSize.height * fy);
 
