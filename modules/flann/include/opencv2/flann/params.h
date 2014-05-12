6c118ebc514c805be3350c5fd1125933e0ddda18
1Hyena <hyena@hyena.net.ee>
2014-05-12 23:01:44 +0300

    Changed ostringstream to ostream for new print_params and added the old version of print_params for backwards compatibility.

--- modules/flann/include/opencv2/flann/params.h
+++ modules/flann/include/opencv2/flann/params.h
@@ -79,14 +79,19 @@ T get_param(const IndexParams& params, std::string name)
     }
 }
 
-inline void print_params(const IndexParams& params, std::ostringstream& stream)
+inline void print_params(const IndexParams& params, std::ostream& stream)
 {
     IndexParams::const_iterator it;
+
     for(it=params.begin(); it!=params.end(); ++it) {
         stream << it->first << " : " << it->second << std::endl;
     }
 }
 
+inline void print_params(const IndexParams& params)
+{
+    print_params(params, std::cout);
+}
 
 }
 
