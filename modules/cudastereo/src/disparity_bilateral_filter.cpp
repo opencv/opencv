e532bd50d5f631cb27585176087096461012073d
Aaron Denney <adenney@appliedminds.com>
2014-06-26 12:06:25 -0700

    Move shared interface to header file rather than repeating.

--- modules/cudastereo/src/disparity_bilateral_filter.cpp
+++ modules/cudastereo/src/disparity_bilateral_filter.cpp
@@ -51,14 +51,7 @@ Ptr<cuda::DisparityBilateralFilter> cv::cuda::createDisparityBilateralFilter(int
 
 #else /* !defined (HAVE_CUDA) */
 
-namespace cv { namespace cuda { namespace device
-{
-    namespace disp_bilateral_filter
-    {
-        template<typename T>
-        void disp_bilateral_filter(PtrStepSz<T> disp, PtrStepSzb img, int channels, int iters, const float *, const float *, size_t, int radius, short edge_disc, short max_disc, cudaStream_t stream);
-    }
-}}}
+#include "cuda/disparity_bilateral_filter.hpp"
 
 namespace
 {
