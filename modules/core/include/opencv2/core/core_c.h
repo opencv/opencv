dd317eec496c80d4720413e6fdbb41f18043f2e7
Roman Donchenko <roman.donchenko@itseez.com>
2014-06-30 16:12:04 +0400

    Remove a couple of useless casts in core headers
    This helps users who compile their code with -Wuseless-cast.

--- modules/core/include/opencv2/core/core_c.h
+++ modules/core/include/opencv2/core/core_c.h
@@ -1107,7 +1107,7 @@ CV_INLINE  CvSetElem* cvSetNew( CvSet* set_header )
         set_header->active_count++;
     }
     else
-        cvSetAdd( set_header, NULL, (CvSetElem**)&elem );
+        cvSetAdd( set_header, NULL, &elem );
     return elem;
 }
 
