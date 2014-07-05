opencv/opencv

commit a73809e6fc986ca84f1b7c58492ba7433396b5e6

Fix GCC 4.9 compiler warning

modules/imgproc/src/morph.cpp
@@ -1257,7 +1257,7 @@ static bool IPPMorphReplicate(int op, const Mat &src, Mat &dst, const Mat &kerne
         }
         #undef IPP_MORPH_CASE
 
-#if defined(__GNUC__) && __GNUC__ == 4 && __GNUC_MINOR__ == 8
+#if defined(__GNUC__) && __GNUC__ == 4 && __GNUC_MINOR__ > 8
         return false; /// It disables false positive warning in GCC 4.8.2
 #endif
     }

commit 71659d15387416b0645757a06dcbf50e719f20be

imgproc: fix compiler error for gcc 4.8
this bug was introduced in a73809e6.

modules/imgproc/src/morph.cpp
@@ -1257,7 +1257,7 @@ static bool IPPMorphReplicate(int op, const Mat &src, Mat &dst, const Mat &kerne
         }
         #undef IPP_MORPH_CASE
 
-#if defined(__GNUC__) && __GNUC__ == 4 && __GNUC_MINOR__ > 8
+#if defined(__GNUC__) && __GNUC__ == 4 && __GNUC_MINOR__ >= 8
         return false; /// It disables false positive warning in GCC 4.8 and further
 #endif
     }