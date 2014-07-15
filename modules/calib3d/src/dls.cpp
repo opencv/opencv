7a6dc8bb40e0023ede6d61f642ac5a6254a7fe4b
edgarriba <edgar.riba@gmail.com>
2014-07-15 09:58:49 +0200

    Initial DLS add

---
+++ modules/calib3d/src/dls.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include "dls.h"
+
+
+dls::dls()
+{
+  // TODO Auto-generated constructor stub
+}
+
+
+dls::~dls()
+{
+  // TODO Auto-generated destructor stub
+}
---
+++ modules/calib3d/src/dls.h
@@ -0,0 +1,17 @@
+#ifndef dls_h
+#define dls_h
+
+#include "opencv2/core/core_c.h"
+
+class dls
+{
+public:
+	dls();
+	~dls();
+
+private:
+
+};
+
+
+#endif
