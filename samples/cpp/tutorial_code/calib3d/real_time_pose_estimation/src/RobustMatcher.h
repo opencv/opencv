7da98006944531a2a335b1ecbf06ff9bc3563181
edgarriba <edgar.riba@gmail.com>
2014-08-12 17:22:35 +0200

    removed old headers

--- samples/cpp/tutorial_code/calib3d/real_time_pose_estimation/src/RobustMatcher.h
+++ samples/cpp/tutorial_code/calib3d/real_time_pose_estimation/src/RobustMatcher.h
@@ -13,7 +13,6 @@
 #include <opencv2/core/core.hpp>
 #include <opencv2/highgui/highgui.hpp>
 #include <opencv2/features2d/features2d.hpp>
-#include <opencv2/nonfree/nonfree.hpp>
 
 class RobustMatcher {
 public:
