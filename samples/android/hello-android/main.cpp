00b212487635f67e49e10e7fecb2a9d0eb1b2421
Vadim Pisarevsky <vadim.pisarevsky@gmail.com>
2014-08-14 13:48:52 +0400

    fixing more compile problems with Java bindings etc

--- samples/android/hello-android/main.cpp
+++ samples/android/hello-android/main.cpp
@@ -1,6 +1,7 @@
-#include <opencv2/core/core.hpp>
-#include <opencv2/imgcodecs/imgcodecs.hpp>
-#include <opencv2/highgui/highgui.hpp>
+#include <opencv2/core.hpp>
+#include <opencv2/imgproc.hpp>
+#include <opencv2/imgcodecs.hpp>
+#include <opencv2/highgui.hpp>
 
 using namespace cv;
 const char* message = "Hello Android!";
