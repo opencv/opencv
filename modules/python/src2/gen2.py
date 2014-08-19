295fe27f258a93e0d4b8b486d51183ee1b512742
Alexander Mordvintsev <Alexander.Mordvintsev@transas.com>
2014-08-19 16:57:37 +0400

    fixing warnings keep old const names for compatibility

--- modules/python/src2/gen2.py
+++ modules/python/src2/gen2.py
@@ -821,8 +821,11 @@ class PythonWrapperGenerator(object):
 
         self.code_ns_reg.write('static ConstDef consts_%s[] = {\n'%wname)
         for name, cname in sorted(ns.consts.items()):
-            self.code_ns_reg.write('    {"%s", (long)(%s)},\n'%(name, cname))
-        self.code_ns_reg.write('    {NULL, NULL}\n};\n\n')
+            self.code_ns_reg.write('    {"%s", %s},\n'%(name, cname))
+            compat_name = re.sub(r"([a-z])([A-Z])", r"\1_\2", name).upper()
+            if name != compat_name:
+                self.code_ns_reg.write('    {"%s", %s},\n'%(compat_name, cname))
+        self.code_ns_reg.write('    {NULL, 0}\n};\n\n')
 
     def gen_namespaces_reg(self):
         self.code_ns_reg.write('static void init_submodules(PyObject * root) \n{\n')
