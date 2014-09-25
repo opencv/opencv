43855db40857e90891f9bccc448117af33e6be4f
Artur Wieczorek <artwik@wp.pl>
2014-09-25 11:51:31 +0200

    Fixed assertion warning in MSMF frame grabber
    Applied in PR3251 to 2.4 branch

--- modules/videoio/src/cap_msmf.cpp
+++ modules/videoio/src/cap_msmf.cpp
@@ -1471,7 +1471,6 @@ void ImageGrabber::stopGrabbing()
 
 HRESULT ImageGrabber::startGrabbing(void)
 {
-    _ComPtr<IMFMediaEvent> pEvent = NULL;
     PROPVARIANT var;
     PropVariantInit(&var);
     HRESULT hr = ig_pSession->SetTopology(0, ig_pTopology);
@@ -1479,6 +1478,7 @@ HRESULT ImageGrabber::startGrabbing(void)
     hr = ig_pSession->Start(&GUID_NULL, &var);
     for(;;)
     {
+        _ComPtr<IMFMediaEvent> pEvent = NULL;
         HRESULT hrStatus = S_OK;
         MediaEventType met;
         if(!ig_pSession) break;
