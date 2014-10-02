43e6bf400f221c3c33e75adf546642afb2a5cc64
edgarriba <edgar.riba@gmail.com>
2014-10-02 16:43:15 +0200

    Added UPNP flag

--- modules/calib3d/include/opencv2/calib3d.hpp
+++ modules/calib3d/include/opencv2/calib3d.hpp
@@ -59,7 +59,9 @@ enum { LMEDS  = 4, //!< least-median algorithm
 enum { SOLVEPNP_ITERATIVE = 0,
        SOLVEPNP_EPNP      = 1, // F.Moreno-Noguer, V.Lepetit and P.Fua "EPnP: Efficient Perspective-n-Point Camera Pose Estimation"
        SOLVEPNP_P3P       = 2, // X.S. Gao, X.-R. Hou, J. Tang, H.-F. Chang; "Complete Solution Classification for the Perspective-Three-Point Problem"
-       SOLVEPNP_DLS       = 3  // Joel A. Hesch and Stergios I. Roumeliotis. "A Direct Least-Squares (DLS) Method for PnP"
+       SOLVEPNP_DLS       = 3, // Joel A. Hesch and Stergios I. Roumeliotis. "A Direct Least-Squares (DLS) Method for PnP"
+       SOLVEPNP_UPNP      = 4  // A.Penate-Sanchez, J.Andrade-Cetto, F.Moreno-Noguer. "Exhaustive Linearization for Robust Camera Pose and Focal Length Estimation"
+
 };
 
 enum { CALIB_CB_ADAPTIVE_THRESH = 1,
