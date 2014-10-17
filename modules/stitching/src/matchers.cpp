4acb0308b667cb76e42adbc6f01d417b8f251b0c
Maksim Shabunin <maksim.shabunin@itseez.com>
2014-10-17 18:29:30 +0400

    final fix

--- modules/stitching/src/matchers.cpp
+++ modules/stitching/src/matchers.cpp
@@ -346,6 +346,11 @@ SurfFeaturesFinder::SurfFeaturesFinder(double hess_thresh, int num_octaves, int 
         extractor_->set(SURF::NOCTAVE_LAYERS, num_layers_descr);
     }
 #else
+    (void)hess_thresh;
+    (void)num_octaves;
+    (void)num_layers;
+    (void)num_octaves_descr;
+    (void)num_layers_descr;
     CV_Error( Error::StsNotImplemented, "OpenCV was built without SURF support" );
 #endif
 }
