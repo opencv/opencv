0eec76d5355370bde08e1815a199452a529fcaad
Andrey Kamaev <andrey.kamaev@itseez.com>
2014-11-18 02:27:02 +0300

    Add cv::setWindowTitle to highgui

--- modules/highgui/src/window_cocoa.mm
+++ modules/highgui/src/window_cocoa.mm
@@ -603,6 +603,27 @@ void cvSetModeWindow_COCOA( const char* name, double prop_value )
     __END__;
 }
 
+void cv::setWindowTitle(const String& winname, const String& title)
+{
+    CVWindow *window = cvGetWindow(winname.c_str());
+
+    if (window == NULL)
+    {
+        namedWindow(winname);
+        window = cvGetWindow(winname.c_str());
+    }
+
+    if (window == NULL)
+        CV_Error(Error::StsNullPtr, "NULL window");
+
+    NSAutoreleasePool* localpool = [[NSAutoreleasePool alloc] init];
+
+    NSString *windowTitle = [NSString stringWithFormat:@"%s", title.c_str()];
+    [window setTitle:windowTitle];
+
+    [localpool drain];
+}
+
 @implementation CVWindow
 
 @synthesize mouseCallback;
