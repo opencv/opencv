fc63f419fdd3260de7cb0e8ad913c2d1f0d4f4d6
Alexander Smorkalov <alexander.smorkalov@itseez.com>
2014-12-26 13:08:44 +0300

    Full AARCH64 support with NEON and new CUDA toolkit.

--- cmake/OpenCVDetectCXXCompiler.cmake
+++ cmake/OpenCVDetectCXXCompiler.cmake
@@ -109,6 +109,8 @@ elseif(CMAKE_SYSTEM_PROCESSOR MATCHES "i686.*|i386.*|x86.*|amd64.*|AMD64.*")
   set(X86 1)
 elseif (CMAKE_SYSTEM_PROCESSOR MATCHES "arm.*|ARM.*")
   set(ARM 1)
+elseif (CMAKE_SYSTEM_PROCESSOR MATCHES "aarch64.*|AARCH64.*")
+  set(AARCH64 1)
 endif()
 
 
