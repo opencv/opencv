opencv/opencv

commit 854a722c70f16da5429c0aebd40b1641077122e2

Implement VideoCapture::get, CvCapture::getProperty, IVideoCapture::getProperty as constant methods.

modules/videoio/src/cap_openni.cpp
@@ -446,7 +446,7 @@ public:
     CvCapture_OpenNI(const char * filename);
     virtual ~CvCapture_OpenNI();
 
-    virtual double getProperty(int propIdx);
+    virtual double getProperty(int propIdx) const;
     virtual bool setProperty(int probIdx, double propVal);
     virtual bool grabFrame();
     virtual IplImage* retrieveFrame(int outputType);
@@ -771,7 +771,7 @@ bool CvCapture_OpenNI::readCamerasParams()
     return true;
 }
 
-double CvCapture_OpenNI::getProperty( int propIdx )
+double CvCapture_OpenNI::getProperty( int propIdx ) const
 {
     double propValue = 0;
 

modules/videoio/include/opencv2/videoio.hpp
@@ -558,7 +558,7 @@ public:
     **Note**: When querying a property that is not supported by the backend used by the VideoCapture
     class, value 0 is returned.
      */
-    CV_WRAP virtual double get(int propId);
+    CV_WRAP virtual double get(int propId) const;
 
 protected:
     Ptr<CvCapture> cap;

modules/videoio/src/cap.cpp
@@ -61,6 +61,11 @@ template<> void DefaultDeleter<CvVideoWriter>::operator ()(CvVideoWriter* obj) c
 
 /************************* Reading AVIs & Camera data **************************/
 
+static inline double icvGetCaptureProperty( const CvCapture* capture, int id )
+{
+    return capture ? capture->getProperty(id) : 0;
+}
+
 CV_IMPL void cvReleaseCapture( CvCapture** pcapture )
 {
     if( pcapture && *pcapture )
@@ -92,7 +97,7 @@ CV_IMPL IplImage* cvRetrieveFrame( CvCapture* capture, int idx )
 
 CV_IMPL double cvGetCaptureProperty( CvCapture* capture, int id )
 {
-    return capture ? capture->getProperty(id) : 0;
+    return icvGetCaptureProperty(capture, id);
 }
 
 CV_IMPL int cvSetCaptureProperty( CvCapture* capture, int id, double value )
@@ -597,11 +602,11 @@ bool VideoCapture::set(int propId, double value)
     return cvSetCaptureProperty(cap, propId, value) != 0;
 }
 
-double VideoCapture::get(int propId)
+double VideoCapture::get(int propId) const
 {
     if (!icap.empty())
         return icap->getProperty(propId);
-    return cvGetCaptureProperty(cap, propId);
+    return icvGetCaptureProperty(cap, propId);
 }
 
 Ptr<IVideoCapture> VideoCapture::createCameraCapture(int index)

modules/videoio/src/cap_android.cpp
@@ -65,7 +65,7 @@ public:
     CvCapture_Android(int);
     virtual ~CvCapture_Android();
 
-    virtual double getProperty(int propIdx);
+    virtual double getProperty(int propIdx) const;
     virtual bool setProperty(int probIdx, double propVal);
     virtual bool grabFrame();
     virtual IplImage* retrieveFrame(int outputType);
@@ -257,7 +257,7 @@ CvCapture_Android::~CvCapture_Android()
     }
 }
 
-double CvCapture_Android::getProperty( int propIdx )
+double CvCapture_Android::getProperty( int propIdx ) const
 {
     switch ( propIdx )
     {

modules/videoio/src/cap_avfoundation.mm
@@ -89,7 +89,7 @@ class CvCaptureCAM : public CvCapture {
         virtual bool grabFrame();
         virtual IplImage* retrieveFrame(int);
         virtual IplImage* queryFrame();
-        virtual double getProperty(int property_id);
+        virtual double getProperty(int property_id) const;
         virtual bool setProperty(int property_id, double value);
         virtual int didStart();
 
@@ -132,7 +132,7 @@ class CvCaptureFile : public CvCapture {
         virtual bool grabFrame();
         virtual IplImage* retrieveFrame(int);
         virtual IplImage* queryFrame();
-        virtual double getProperty(int property_id);
+        virtual double getProperty(int property_id) const;
         virtual bool setProperty(int property_id, double value);
         virtual int didStart();
 
@@ -476,7 +476,7 @@ enum {
 typedef NSInteger AVCaptureWhiteBalanceMode;
 */
 
-double CvCaptureCAM::getProperty(int property_id){
+double CvCaptureCAM::getProperty(int property_id) const{
     NSAutoreleasePool* localpool = [[NSAutoreleasePool alloc] init];
 
     /*
@@ -1010,7 +1010,7 @@ double CvCaptureFile::getFPS() {
     return 30.0; //TODO: Debugging
 }
 
-double CvCaptureFile::getProperty(int /*property_id*/){
+double CvCaptureFile::getProperty(int /*property_id*/) const{
 
     /*
          if (mCaptureSession == nil) return 0;

modules/videoio/src/cap_cmu.cpp
@@ -68,7 +68,7 @@ public:
 
     virtual bool open(int cameraId);
     virtual void close();
-    virtual double getProperty(int);
+    virtual double getProperty(int) const;
     virtual bool setProperty(int, double);
     virtual bool grabFrame();
     virtual IplImage* retrieveFrame(int);
@@ -422,7 +422,7 @@ IplImage* CvCaptureCAM_CMU::retrieveFrame(int)
 }
 
 
-double CvCaptureCAM_CMU::getProperty( int property_id )
+double CvCaptureCAM_CMU::getProperty( int property_id ) const
 {
     C1394Camera* cmucam = camera();
     if( !cmucam )

modules/videoio/src/cap_dc1394.cpp
@@ -1049,7 +1049,7 @@ public:
     virtual bool open( int index );
     virtual void close();
 
-    virtual double getProperty(int);
+    virtual double getProperty(int) const;
     virtual bool setProperty(int, double);
     virtual bool grabFrame();
     virtual IplImage* retrieveFrame(int);
@@ -1085,9 +1085,13 @@ IplImage* CvCaptureCAM_DC1394_CPP::retrieveFrame(int)
     return captureDC1394 ? (IplImage*)icvRetrieveFrameCAM_DC1394( captureDC1394, 0 ) : 0;
 }
 
-double CvCaptureCAM_DC1394_CPP::getProperty( int propId )
+double CvCaptureCAM_DC1394_CPP::getProperty( int propId ) const
 {
-    return captureDC1394 ? icvGetPropertyCAM_DC1394( captureDC1394, propId ) : 0;
+    // Simulate mutable (C++11-like) member variable
+    // (some members are used to cache property settings).
+    CvCaptureCAM_DC1394* cap = const_cast<CvCaptureCAM_DC1394*>(captureDC1394);
+
+    return cap ? icvGetPropertyCAM_DC1394( cap, propId ) : 0;
 }
 
 bool CvCaptureCAM_DC1394_CPP::setProperty( int propId, double value )

modules/videoio/src/cap_dc1394_v2.cpp
@@ -207,7 +207,7 @@ public:
     virtual bool open(int index);
     virtual void close();
 
-    virtual double getProperty(int);
+    virtual double getProperty(int) const;
     virtual bool setProperty(int, double);
     virtual bool grabFrame();
     virtual IplImage* retrieveFrame(int);
@@ -652,8 +652,11 @@ IplImage* CvCaptureCAM_DC1394_v2_CPP::retrieveFrame(int idx)
     return 0 <= idx && idx < nimages ? img[idx] : 0;
 }
 
-double CvCaptureCAM_DC1394_v2_CPP::getProperty(int propId)
+double CvCaptureCAM_DC1394_v2_CPP::getProperty(int propId) const
 {
+    // Simulate mutable (C++11-like) member variable
+    dc1394featureset_t& fs = const_cast<dc1394featureset_t&>(feature_set);
+
     switch (propId)
     {
     case CV_CAP_PROP_FRAME_WIDTH:
@@ -666,14 +669,14 @@ double CvCaptureCAM_DC1394_v2_CPP::getProperty(int propId)
         return rectify ? 1 : 0;
     case CV_CAP_PROP_WHITE_BALANCE_BLUE_U:
         if (dc1394_feature_whitebalance_get_value(dcCam,
-                                                  &feature_set.feature[DC1394_FEATURE_WHITE_BALANCE-DC1394_FEATURE_MIN].BU_value,
-                                                  &feature_set.feature[DC1394_FEATURE_WHITE_BALANCE-DC1394_FEATURE_MIN].RV_value) == DC1394_SUCCESS)
+                                                  &fs.feature[DC1394_FEATURE_WHITE_BALANCE-DC1394_FEATURE_MIN].BU_value,
+                                                  &fs.feature[DC1394_FEATURE_WHITE_BALANCE-DC1394_FEATURE_MIN].RV_value) == DC1394_SUCCESS)
         return feature_set.feature[DC1394_FEATURE_WHITE_BALANCE-DC1394_FEATURE_MIN].BU_value;
         break;
     case CV_CAP_PROP_WHITE_BALANCE_RED_V:
         if (dc1394_feature_whitebalance_get_value(dcCam,
-                                                  &feature_set.feature[DC1394_FEATURE_WHITE_BALANCE-DC1394_FEATURE_MIN].BU_value,
-                                                  &feature_set.feature[DC1394_FEATURE_WHITE_BALANCE-DC1394_FEATURE_MIN].RV_value) == DC1394_SUCCESS)
+                                                  &fs.feature[DC1394_FEATURE_WHITE_BALANCE-DC1394_FEATURE_MIN].BU_value,
+                                                  &fs.feature[DC1394_FEATURE_WHITE_BALANCE-DC1394_FEATURE_MIN].RV_value) == DC1394_SUCCESS)
         return feature_set.feature[DC1394_FEATURE_WHITE_BALANCE-DC1394_FEATURE_MIN].RV_value;
         break;
     case CV_CAP_PROP_GUID:
@@ -690,7 +693,7 @@ double CvCaptureCAM_DC1394_v2_CPP::getProperty(int propId)
             && dcCam)
             //&& feature_set.feature[dc1394properties[propId]-DC1394_FEATURE_MIN].on_off_capable)
             if (dc1394_feature_get_value(dcCam,(dc1394feature_t)dc1394properties[propId],
-                &feature_set.feature[dc1394properties[propId]-DC1394_FEATURE_MIN].value) == DC1394_SUCCESS)
+                &fs.feature[dc1394properties[propId]-DC1394_FEATURE_MIN].value) == DC1394_SUCCESS)
               return feature_set.feature[dc1394properties[propId]-DC1394_FEATURE_MIN].value;
     }
     return -1; // the value of the feature can be 0, so returning 0 as an error is wrong

commit b399c4b1cf0157ee7329eb5a4411c38207b11ca0

modules/videoio/src/cap_openni.cpp: fix const-ness related compile errors
Commit 854a722c introduced compile errors (linux, gcc 4.8.3) because
CvCapture_OpenNI::getProperty was const-ified, while the methods it
calls - CvCapture_OpenNI::getImageGeneratorProperty(),
CvCapture_OpenNI::getDepthGeneratorProperty(), and
CvCapture_OpenNI::getCommonProperty() were not.

This commit const-ifies the above three methods. In addition, a
const_cast is needed for the argument of
depthGenerator.GetAlternativeViewPointCap().IsViewPointAs() in
getDepthGeneratorProperty().

modules/videoio/src/cap_openni.cpp
@@ -477,11 +477,11 @@ protected:
 
     bool readCamerasParams();
 
-    double getDepthGeneratorProperty(int propIdx);
+    double getDepthGeneratorProperty(int propIdx) const;
     bool setDepthGeneratorProperty(int propIdx, double propVal);
-    double getImageGeneratorProperty(int propIdx);
+    double getImageGeneratorProperty(int propIdx) const;
     bool setImageGeneratorProperty(int propIdx, double propVal);
-    double getCommonProperty(int propIdx);
+    double getCommonProperty(int propIdx) const;
     bool setCommonProperty(int propIdx, double propVal);
 
     // OpenNI context
@@ -820,7 +820,7 @@ bool CvCapture_OpenNI::setProperty( int propIdx, double propValue )
     return isSet;
 }
 
-double CvCapture_OpenNI::getCommonProperty( int propIdx )
+double CvCapture_OpenNI::getCommonProperty( int propIdx ) const
 {
     double propValue = 0;
 
@@ -917,7 +917,7 @@ bool CvCapture_OpenNI::setCommonProperty( int propIdx, double propValue )
     return isSet;
 }
 
-double CvCapture_OpenNI::getDepthGeneratorProperty( int propIdx )
+double CvCapture_OpenNI::getDepthGeneratorProperty( int propIdx ) const
 {
     double propValue = 0;
     if( !depthGenerator.IsValid() )
@@ -953,7 +953,7 @@ double CvCapture_OpenNI::getDepthGeneratorProperty( int propIdx )
         propValue = (double)depthFocalLength_VGA;
         break;
     case CV_CAP_PROP_OPENNI_REGISTRATION :
-        propValue = depthGenerator.GetAlternativeViewPointCap().IsViewPointAs(imageGenerator) ? 1.0 : 0.0;
+        propValue = depthGenerator.GetAlternativeViewPointCap().IsViewPointAs(const_cast<xn::ImageGenerator &>(imageGenerator)) ? 1.0 : 0.0;
         break;
     case CV_CAP_PROP_POS_MSEC :
         propValue = (double)depthGenerator.GetTimestamp();
@@ -1018,7 +1018,7 @@ bool CvCapture_OpenNI::setDepthGeneratorProperty( int propIdx, double propValue 
     return isSet;
 }
 
-double CvCapture_OpenNI::getImageGeneratorProperty( int propIdx )
+double CvCapture_OpenNI::getImageGeneratorProperty( int propIdx ) const
 {
     double propValue = 0.;
     if( !imageGenerator.IsValid() )