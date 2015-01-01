a358239252e8ecb952065e55bb3b1e1f5cbfd670
berak <px1704@web.de>
2015-01-01 10:30:44 +0100

    fixes for latest changes in opencv3.0 api
    fixes for latest changes in opencv3.0 api
    
    waitKey() normalization
    
    fixed mser bindings

--- samples/python2/edge.py
+++ samples/python2/edge.py
@@ -45,7 +45,7 @@ if __name__ == '__main__':
         vis /= 2
         vis[edge != 0] = (0, 255, 0)
         cv2.imshow('edge', vis)
-        ch = cv2.waitKey(5)
+        ch = cv2.waitKey(5) & 0xFF
         if ch == 27:
             break
     cv2.destroyAllWindows()
