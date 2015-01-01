58ad952b1addb2732179a81942959665ab030d1e
Alexander Alekhin <alexander.alekhin@itseez.com>
2015-01-02 01:59:36 +0300

    UMat: added USAGE_ALLOCATE_SHARED_MEMORY

--- modules/core/include/opencv2/core/mat.hpp
+++ modules/core/include/opencv2/core/mat.hpp
@@ -376,9 +376,10 @@ enum UMatUsageFlags
 {
     USAGE_DEFAULT = 0,
 
-    // default allocation policy is platform and usage specific
+    // buffer allocation policy is platform and usage specific
     USAGE_ALLOCATE_HOST_MEMORY = 1 << 0,
     USAGE_ALLOCATE_DEVICE_MEMORY = 1 << 1,
+    USAGE_ALLOCATE_SHARED_MEMORY = 1 << 2, // It is not equal to: USAGE_ALLOCATE_HOST_MEMORY | USAGE_ALLOCATE_DEVICE_MEMORY
 
     __UMAT_USAGE_FLAGS_32BIT = 0x7fffffff // Binary compatibility hint
 };
