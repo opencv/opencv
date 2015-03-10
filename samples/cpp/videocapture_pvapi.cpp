a5ec75f595be6c670ca4b31a2a468d86cf0a09b4
StevenPuttemans <steven.puttemans@kuleuven.be>
2015-03-10 09:28:37 +0100

    extra camera specific information for IP settings

--- samples/cpp/videocapture_pvapi.cpp
+++ samples/cpp/videocapture_pvapi.cpp
@@ -5,6 +5,23 @@
 // Succesfully tested on Prosilica and Manta series //
 //////////////////////////////////////////////////////
 
+// --------------------------------------------------------------------------------
+// Some remarks for ensuring the correct working of the interface between camera
+// and the pc from which you will capture data - Linux based settings. The settings
+// for Windows are the same, but edited in the graphical interface of the
+// network card.
+//
+// You have to be sure that OpenCV is built with the PvAPI interface enabled.
+//
+// FIRST CONFIGURE IP SETTINGS
+// - Change the IP address of your pc to 169.254.1.1
+// - Change the subnet mask of your pc to 255.255.0.0
+// - Change the gateway of your pc to 169.254.1.2
+//
+// CHANGE SOME NETWORK CARD SETTINGS
+// - sudo ifconfig eth0 mtu 9000 - or 9016 ideally if your card supports that
+// --------------------------------------------------------------------------------
+
 #include <iostream>
 #include "opencv2/opencv.hpp"
 
