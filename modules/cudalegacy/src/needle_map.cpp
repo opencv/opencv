3465cef689b11215d66cc41ce8ec5a4742669574
Dikay900 <dark900@xyz.de>
2015-03-21 19:18:54 +0100

    disable cuda::OpticalFlowNeedleMap if configured without cudaimgproc disable cuda::FastOpticalFlowBM if configured without cudaarithm

--- modules/cudalegacy/src/needle_map.cpp
+++ modules/cudalegacy/src/needle_map.cpp
@@ -45,7 +45,7 @@
 using namespace cv;
 using namespace cv::cuda;
 
-#if !defined (HAVE_CUDA) || defined (CUDA_DISABLER)
+#if !defined (HAVE_CUDA) || !defined(HAVE_OPENCV_CUDAIMGPROC) || defined (CUDA_DISABLER)
 
 void cv::cuda::createOpticalFlowNeedleMap(const GpuMat&, const GpuMat&, GpuMat&, GpuMat&) { throw_no_cuda(); }
 
