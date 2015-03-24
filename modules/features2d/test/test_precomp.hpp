7c740b6a3196cbac385e226ae6d3b413d461fda3
ippei ito <github@mysrv.link>
2015-03-25 00:08:47 +0900

    Test accuracy for Pull Request # 3829

--- modules/features2d/test/test_precomp.hpp
+++ modules/features2d/test/test_precomp.hpp
@@ -14,6 +14,7 @@
 #include "opencv2/imgproc/imgproc_c.h"
 #include "opencv2/features2d/features2d.hpp"
 #include "opencv2/highgui/highgui.hpp"
+#include "opencv2/calib3d/calib3d.hpp"
 #include <iostream>
 
 #endif
