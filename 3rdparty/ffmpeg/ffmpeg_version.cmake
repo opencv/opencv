3d909d1da78ad9bdce1eb5ee0d3e05801800ff4f
Alexander Alekhin <alexander.alekhin@itseez.com>
2015-06-22 18:48:25 +0300

    ffmpeg binaries downloader (based on code: c47d8e894ed3f88e6a324c8a6a70f60abd8d5e6d)

--- 3rdparty/ffmpeg/ffmpeg_version.cmake
+++ 3rdparty/ffmpeg/ffmpeg_version.cmake
@@ -1,3 +1,15 @@
+set(FFMPEG_DOWNLOAD_URL ${OPENCV_FFMPEG_URL};$ENV{OPENCV_FFMPEG_URL};http://build.opencv.org/buildbot/storage/exports/ffmpeg_20150622/)
+
+ocv_download(PACKAGE opencv_ffmpeg.dll
+             HASH "02a6637ed4b26b830bc58c18e21ea672"
+             URL ${FFMPEG_DOWNLOAD_URL}
+             DESTINATION_DIR ${CMAKE_CURRENT_LIST_DIR})
+
+ocv_download(PACKAGE opencv_ffmpeg_64.dll
+             HASH "ea83e3d6409899f8590de3edbdccc60d"
+             URL ${FFMPEG_DOWNLOAD_URL}
+             DESTINATION_DIR ${CMAKE_CURRENT_LIST_DIR})
+
 set(HAVE_FFMPEG 1)
 set(HAVE_FFMPEG_CODEC 1)
 set(HAVE_FFMPEG_FORMAT 1)
