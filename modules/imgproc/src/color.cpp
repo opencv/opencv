beed99dbc18205760dc9a7c8721abd6e7b8a1026
Josh Klontz <josh@rankone.io>
2015-07-14 20:56:43 -0400

    fixed conditional compilation of RGB2Gray<ushort> template specialization

--- modules/imgproc/src/color.cpp
+++ modules/imgproc/src/color.cpp
@@ -1674,7 +1674,9 @@ struct RGB2Gray<float>
     bool haveSIMD;
 };
 
-#else
+#endif // CV_SSE2
+
+#if !CV_NEON && !CV_SSE4_1
 
 template<> struct RGB2Gray<ushort>
 {
@@ -1698,7 +1700,7 @@ template<> struct RGB2Gray<ushort>
     int coeffs[3];
 };
 
-#endif
+#endif // !CV_NEON && !CV_SSE4_1
 
 ///////////////////////////////////// RGB <-> YCrCb //////////////////////////////////////
 
