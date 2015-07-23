415df06d7cef8d01dde131f91e2131b7c6a09131
daewoo myoung <ttagu99@gmail.com>
2015-07-23 19:11:07 +0900

    android build error cerr -> cout

--- samples/cpp/tutorial_code/Histograms_Matching/MatchTemplate_Demo.cpp
+++ samples/cpp/tutorial_code/Histograms_Matching/MatchTemplate_Demo.cpp
@@ -31,7 +31,7 @@ int main( int argc, char** argv )
 {
   if (argc < 3)
   {
-    cerr << "Not enough parmeters" << endl;
+    cout << "Not enough parmeters" << endl;
     return -1;
   }
   /// Load image and template
