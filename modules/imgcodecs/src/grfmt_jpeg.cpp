59e7f44e6f3ed83fd7356596d471835259069d14
Suleyman TURKMEN <sturkmen@hotmail.com>
2015-07-26 04:50:23 +0300

    Update grfmt_jpeg.cpp

--- modules/imgcodecs/src/grfmt_jpeg.cpp
+++ modules/imgcodecs/src/grfmt_jpeg.cpp
@@ -242,8 +242,8 @@ bool  JpegDecoder::readHeader()
         {
             jpeg_read_header( &state->cinfo, TRUE );
 
-            int m_scale_denom=m_size.width>8 ? 1 : m_size.width;
-            state->cinfo.scale_num=1;
+            int m_scale_denom = m_size.width ==0 || m_size.width>8 ? 1 : m_size.width;
+            state->cinfo.scale_num = 1;
             state->cinfo.scale_denom = m_scale_denom;
             m_size=Size(); // trick! to know which decoder used scale_denom see imread_
             jpeg_calc_output_dimensions(&state->cinfo);
