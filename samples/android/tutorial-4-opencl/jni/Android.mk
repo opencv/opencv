be1e975120e44865b206305b7c8a85c13e2a0481
Andrey Pavlenko <andrey.pavlenko@itseez.com>
2015-07-30 23:18:45 +0300

    excluding libOpenCL.so from the APK

--- samples/android/tutorial-4-opencl/jni/Android.mk
+++ samples/android/tutorial-4-opencl/jni/Android.mk
@@ -1,11 +1,6 @@
 LOCAL_PATH := $(call my-dir)
 
-include $(CLEAR_VARS)
-LOCAL_MODULE := OpenCL
-LOCAL_SRC_FILES := $(OPENCL_SDK)/lib/$(TARGET_ARCH_ABI)/libOpenCL.so
-LOCAL_EXPORT_C_INCLUDES := $(OPENCL_SDK)/include
-include $(PREBUILT_SHARED_LIBRARY)
-
+# add OpenCV
 include $(CLEAR_VARS)
 OPENCV_INSTALL_MODULES:=on
 ifeq ($(O4A_SDK_ROOT),)
@@ -14,8 +9,11 @@ else
     include $(O4A_SDK_ROOT)/sdk/native/jni/OpenCV.mk
 endif
 
+# add OpenCL
+LOCAL_C_INCLUDES += $(OPENCL_SDK)/include
+LOCAL_LDLIBS += -L$(OPENCL_SDK)/lib/$(TARGET_ARCH_ABI) -lOpenCL
+
 LOCAL_MODULE    := JNIrender
 LOCAL_SRC_FILES := jni.c GLrender.cpp CLprocessor.cpp
 LOCAL_LDLIBS    += -llog -lGLESv2 -lEGL
-LOCAL_SHARED_LIBRARIES += OpenCL
 include $(BUILD_SHARED_LIBRARY)
