56bde913d449cc81b0ecd24347fa3cab971815e7
Andrey Pavlenko <andrey.pavlenko@itseez.com>
2015-07-31 03:39:26 +0300

    adding mode switching via menu

--- samples/android/tutorial-4-opencl/src/org/opencv/samples/tutorial4/NativeGLRenderer.java
+++ samples/android/tutorial-4-opencl/src/org/opencv/samples/tutorial4/NativeGLRenderer.java
@@ -6,8 +6,14 @@ public class NativeGLRenderer {
         System.loadLibrary("opencv_java3");
         System.loadLibrary("JNIrender");
     }
+
+    public static final int PROCESSING_MODE_CPU = 1;
+    public static final int PROCESSING_MODE_OCL_DIRECT = 2;
+    public static final int PROCESSING_MODE_OCL_OCV = 3;
+
     public static native int initGL();
     public static native void closeGL();
     public static native void drawFrame();
     public static native void changeSize(int width, int height);
+    public static native void setProcessingMode(int mode);
 }
--- samples/android/tutorial-4-opencl/src/org/opencv/samples/tutorial4/Tutorial4Activity.java
+++ samples/android/tutorial-4-opencl/src/org/opencv/samples/tutorial4/Tutorial4Activity.java
@@ -3,6 +3,9 @@ package org.opencv.samples.tutorial4;
 import android.app.Activity;
 import android.content.pm.ActivityInfo;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.Window;
 import android.view.WindowManager;
 import android.widget.TextView;
@@ -10,6 +13,7 @@ import android.widget.TextView;
 public class Tutorial4Activity extends Activity {
 
     private MyGLSurfaceView mView;
+    private TextView mProcMode;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -27,7 +31,14 @@ public class Tutorial4Activity extends Activity {
         mView = (MyGLSurfaceView) findViewById(R.id.my_gl_surface_view);
         TextView tv = (TextView)findViewById(R.id.fps_text_view);
         mView.setFpsTextView(tv);
-    }
+        mProcMode = (TextView)findViewById(R.id.proc_mode_text_view);
+        runOnUiThread(new Runnable() {
+            public void run() {
+                mProcMode.setText("Processing mode: CPU");
+            }
+        });
+
+        NativeGLRenderer.setProcessingMode(NativeGLRenderer.PROCESSING_MODE_CPU);    }
 
     @Override
     protected void onPause() {
@@ -39,5 +50,44 @@ public class Tutorial4Activity extends Activity {
     protected void onResume() {
         super.onResume();
         mView.onResume();
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.menu, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+        case R.id.cpu:
+            runOnUiThread(new Runnable() {
+                public void run() {
+                    mProcMode.setText("Processing mode: CPU");
+                }
+            });
+            NativeGLRenderer.setProcessingMode(NativeGLRenderer.PROCESSING_MODE_CPU);
+            return true;
+        case R.id.ocl_direct:
+            runOnUiThread(new Runnable() {
+                public void run() {
+                    mProcMode.setText("Processing mode: OpenCL direct");
+                }
+            });
+            NativeGLRenderer.setProcessingMode(NativeGLRenderer.PROCESSING_MODE_OCL_DIRECT);
+            return true;
+        case R.id.ocl_ocv:
+            runOnUiThread(new Runnable() {
+                public void run() {
+                    mProcMode.setText("Processing mode: OpenCL via OpenCV (TAPI)");
+                }
+            });
+            NativeGLRenderer.setProcessingMode(NativeGLRenderer.PROCESSING_MODE_OCL_OCV);
+            return true;
+        default:
+            return false;
+        }
     }
 }
