984c337de8337b67933ca1a5e5b370d757d70125
Suleyman TURKMEN <sturkmen@hotmail.com>
2015-08-03 18:57:33 +0300

    documentation patch

--- modules/imgproc/include/opencv2/imgproc.hpp
+++ modules/imgproc/include/opencv2/imgproc.hpp
@@ -1968,8 +1968,8 @@ way:
     // specify fx and fy and let the function compute the destination image size.
     resize(src, dst, Size(), 0.5, 0.5, interpolation);
 @endcode
-To shrink an image, it will generally look best with CV_INTER_AREA interpolation, whereas to
-enlarge an image, it will generally look best with CV_INTER_CUBIC (slow) or CV_INTER_LINEAR
+To shrink an image, it will generally look best with INTER_AREA interpolation, whereas to
+enlarge an image, it will generally look best with INTER_CUBIC (slow) or INTER_LINEAR
 (faster but still looks OK).
 
 @param src input image.
