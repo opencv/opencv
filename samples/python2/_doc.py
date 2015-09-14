04a8c2b7adb87ab8b34c4c29af6b2635e5121614
Adam Gibson <adam.gibson@tc4k.net>
2015-09-14 12:24:08 +0800

    Reverted _doc.py to Python 2-only version. Interim measure to prevent docs from failing during build, until all samples are updated with Python 2/3 compatibility.

--- samples/python2/_doc.py
+++ samples/python2/_doc.py
@@ -5,20 +5,12 @@ Scans current directory for *.py files and reports
 ones with missing __doc__ string.
 '''
 
-# Python 2/3 compatibility
-from __future__ import print_function
-import sys
-PY3 = sys.version_info[0] == 3
-
 from glob import glob
 
 if __name__ == '__main__':
-    print('--- undocumented files:')
+    print '--- undocumented files:'
     for fn in glob('*.py'):
         loc = {}
-        if PY3:
-            exec(open(fn).read(), loc)
-        else:
-            execfile(fn, loc)
+        execfile(fn, loc)
         if '__doc__' not in loc:
-            print(fn)
+            print fn
