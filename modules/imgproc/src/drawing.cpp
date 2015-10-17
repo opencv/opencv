d341c8b593324a4833d939166c359364aeca8098
Suleyman TURKMEN <sturkmen@hotmail.com>
2015-10-17 22:22:20 +0300

    Update drawing.cpp

--- modules/imgproc/src/drawing.cpp
+++ modules/imgproc/src/drawing.cpp
@@ -2090,10 +2090,6 @@ void putText( InputOutputArray _img, const String& text, Point org,
               int thickness, int line_type, bool bottomLeftOrigin )
 
 {
-    if ( text.empty() )
-    {
-        return;
-    }
     Mat img = _img.getMat();
     const int* ascii = getFontData(fontFace);
 
