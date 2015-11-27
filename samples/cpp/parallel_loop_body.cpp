65890b73b17b077078c6854d02b8fa803748d7ae
LaurentBerger <laurent.berger@univ-lemans.fr>
2015-11-27 14:53:16 +0100

    comment https://github.com/Itseez/opencv/pull/5716#issuecomment-160123751

--- samples/cpp/parallel_loop_body.cpp
+++ samples/cpp/parallel_loop_body.cpp
@@ -8,10 +8,10 @@ using namespace std;
 
 // This class use ParallelLoopBody to process image using thread
 // ParallelMeanLine process image to have for each line a mean value of 128 and standard deviation of 64
-class ParallelMeanLine: public cv::ParallelLoopBody
+class ParallelMeanLine: public ParallelLoopBody
 {
 private:
-    cv::Mat &imgSrc;
+    Mat &imgSrc;
     vector<double> &meanLine;
     vector<double> &stdLine;
     bool verbose;
@@ -27,7 +27,7 @@ public:
     virtual void operator()(const Range& range) const
     {
 
-        int h = imgSrc.cols;
+        int width = imgSrc.cols;
         if (verbose)
             cout << getThreadNum()<<"# :Start from row " << range.start << " to "  << range.end-1<<" ("<<range.end-range.start<<" loops)" << endl;
     // First mean and variance for each line are computed
@@ -37,29 +37,22 @@ public:
             uchar *ptr=imgSrc.ptr(y);
             int m = 0;
             int s2=0;
-            for (int x = 0; x<h; x++,ptr++)
+            for (int x = 0; x<width; x++,ptr++)
             {
                     m += *ptr;
                     s2 += *ptr * *ptr;
             }
-            meanLine[y] = m / double(h);
-            stdLine[y] = s2/h-meanLine[y]*meanLine[y];
+            meanLine[y] = m / double(width);
+            stdLine[y] = s2/width-meanLine[y]*meanLine[y];
         }
     // We want : all lines must have a mean value of 128 and standard deviation of 64. Negative value or value greater than 255 are threshold
         for (int y = range.start; y < range.end; y++)
         {
 
             uchar *ptr=imgSrc.ptr(y);
-            int v;
-            for (int x = 0; x<h; x++,ptr++)
+            for (int x = 0; x<width; x++,ptr++)
             {
-                   v = int((*ptr-meanLine[y])*sqrt(64*64/stdLine[y])+128);
-                   if (v>255)
-                       *ptr=255;
-                   else if (v<0)
-                       *ptr=0;
-                   else
-                       *ptr=(uchar)v;
+                   *ptr = saturate_cast<uchar>((*ptr-meanLine[y])*sqrt(64*64/stdLine[y])+128);
             }
         }
 
@@ -75,7 +68,7 @@ public:
 const String keys =
 "{help h usage ? |      | This program demonstrated the use of class ParallelLoopBody for threading\n ./ParallelLoopBody [image_name -- default ../data/lena.jpg]  --thread <int> default 16}"
 "{@image         |../data/lena.jpg      | image to process     }"
-    "{@thread        |16    | number o fthread     }"
+    "{@thread        |16    | number of thread     }"
     "{@verbose       |true   | verbose mode     }"
     ;
 
@@ -112,7 +105,7 @@ int main (int argc,char **argv)
     ParallelMeanLine x(m,meanLine,stdLine);
     x.Verbose(verbose);
     int64 tpsIni = getTickCount();
-    parallel_for_(cv::Range(0,m.rows), x,nbThreads);
+    parallel_for_(Range(0,m.rows), x,nbThreads);
     int64  tpsFin = getTickCount();
     tps=(tpsFin - tpsIni) / getTickFrequency();
     cout << "For " << nbThreads << " thread times is " << tps << "s\n";
