f59e2de172af06c7d973ec6e32066c775e138537
flp <git@huebnerphilipp.de>
2015-12-15 00:31:41 +0100

    Adapted for py2/py3 compatibility

--- samples/python2/_doc.py
+++ samples/python2/_doc.py
@@ -5,12 +5,23 @@ Scans current directory for *.py files and reports
 ones with missing __doc__ string.
 '''
 
+# Python 2/3 compatibility
+from __future__ import print_function
+import sys
+PY3 = sys.version_info[0] == 3
+
 from glob import glob
 
 if __name__ == '__main__':
-    print '--- undocumented files:'
+    print('--- undocumented files:')
     for fn in glob('*.py'):
         loc = {}
-        execfile(fn, loc)
+        try:
+            if PY3:
+                exec(open(fn).read(), loc)
+            else:
+                execfile(fn, loc)
+        except:
+            pass
         if '__doc__' not in loc:
-            print fn
+            print(fn)
