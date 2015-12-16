278f21ba2b29996369720162122c57b728f280c6
Alexander Alekhin <alexander.alekhin@itseez.com>
2015-12-16 17:28:03 +0300

    update osx and ios build_framework.py

--- cmake/OpenCVGenInfoPlist.cmake
+++ cmake/OpenCVGenInfoPlist.cmake
@@ -1,10 +1,5 @@
-if(OPENCV_EXTRA_WORLD)
-  set(OPENCV_APPLE_BUNDLE_NAME "OpenCV_contrib")
-  set(OPENCV_APPLE_BUNDLE_ID "org.opencv_contrib")
-else()
-  set(OPENCV_APPLE_BUNDLE_NAME "OpenCV")
-  set(OPENCV_APPLE_BUNDLE_ID "org.opencv")
-endif()
+set(OPENCV_APPLE_BUNDLE_NAME "OpenCV")
+set(OPENCV_APPLE_BUNDLE_ID "org.opencv")
 
 if(IOS)
   configure_file("${OpenCV_SOURCE_DIR}/platforms/ios/Info.plist.in"
