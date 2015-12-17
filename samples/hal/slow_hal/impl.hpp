48a8892a63eb4f4b190696fc9386442d35b004c5
Maksim Shabunin <maksim.shabunin@itseez.com>
2015-12-17 13:25:47 +0300

    HAL samples: renamed and added readme

--- samples/hal/slow_hal/impl.hpp
+++ samples/hal/slow_hal/impl.hpp
@@ -6,15 +6,15 @@
 int slow_and8u(const uchar* src1, size_t step1, const uchar* src2, size_t step2, uchar* dst, size_t step, int width, int height);
 int slow_or8u(const uchar* src1, size_t step1, const uchar* src2, size_t step2, uchar* dst, size_t step, int width, int height);
 int slow_xor8u(const uchar* src1, size_t step1, const uchar* src2, size_t step2, uchar* dst, size_t step, int width, int height);
-int slow_not8u(const uchar* src1, size_t step1, const uchar* src2, size_t step2, uchar* dst, size_t step, int width, int height);
+int slow_not8u(const uchar* src1, size_t step1, uchar* dst, size_t step, int width, int height);
 
-#undef hal_and8u
-#define hal_and8u slow_and8u
-#undef hal_or8u
-#define hal_or8u slow_or8u
-#undef hal_xor8u
-#define hal_xor8u slow_xor8u
-#undef hal_not8u
-#define hal_not8u slow_not8u
+#undef cv_hal_and8u
+#define cv_hal_and8u slow_and8u
+#undef cv_hal_or8u
+#define cv_hal_or8u slow_or8u
+#undef cv_hal_xor8u
+#define cv_hal_xor8u slow_xor8u
+#undef cv_hal_not8u
+#define cv_hal_not8u slow_not8u
 
 #endif
