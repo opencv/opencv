a4c990862bdd0ea4c69af4510193fdbf3f19f64d
PhenixI <zhanzhanlove.hi@gmail.com>
2016-01-22 12:52:39 +0800

    Modify matrix.cpp create function to fix #5991 issue

--- modules/core/src/matrix.cpp
+++ modules/core/src/matrix.cpp
@@ -49,12 +49,6 @@
 *                           [scaled] Identity matrix initialization                      *
 \****************************************************************************************/
 
-//if(_sizes == (this->size.p)){
-//	if( u && CV_XADD(&u->refcount, -1) == 1 )
-//		deallocate();
-//	u = NULL;
-//	datastart = dataend = datalimit = data = 0;
-//}else
 
 namespace cv {
 
@@ -390,8 +384,14 @@ void Mat::create(int d, const int* _sizes, int _type)
         if( i == d && (d > 1 || size[1] == 1))
             return;
     }
-
-    release();
+    if(_sizes == (this->size.p)){
+        if( u && CV_XADD(&u->refcount, -1) == 1 )
+            deallocate();
+        u = NULL;
+        datastart = dataend = datalimit = data = 0;
+    }else{
+        release();
+    }
     if( d == 0 )
         return;
     flags = (_type & CV_MAT_TYPE_MASK) | MAGIC_VAL;
