86bd1fcec4c48e78a6984d80698c69edd2e606c1
Victor Vedovato <victor@10imaging.com>
2016-02-26 15:50:03 -0700

    make & install sdk.

--- platforms/scripts/cmake_android_arm_custom.sh
+++ platforms/scripts/cmake_android_arm_custom.sh
@@ -7,8 +7,10 @@ fi
 mkdir -p build_android_arm_neon
 pushd build_android_arm_neon
 cmake -DANDROID_ABI="armeabi-v7a-hard with NEON" -DINSTALL_ANDROID_EXAMPLES=OFF -DENABLE_NEON=ON -DENABLE_VFPV3=ON \
-        -DWITH_TBB=ON -DBUILD_TBB=ON -DWITH_OPENCL=ON -DWITH_CUDA=OFF -DCMAKE_BUILD_WITH_INSTALL_RPATH=ON -DENABLE_PRECOMPILED_HEADERS=OFF \
+        -DWITH_TBB=ON -DBUILD_TBB=ON -DWITH_OPENCL=OFF -DWITH_CUDA=OFF -DCMAKE_BUILD_WITH_INSTALL_RPATH=ON -DENABLE_PRECOMPILED_HEADERS=OFF \
                 -DCMAKE_TOOLCHAIN_FILE=../android/android.toolchain.cmake $@ ../..
+make -j8
+make install
 popd
 
 if [ -d build_android_arm64 ]; then
@@ -19,4 +21,6 @@ pushd build_android_arm64
 cmake -DANDROID_ABI="arm64-v8a" -DINSTALL_ANDROID_EXAMPLES=OFF -DENABLE_NEON=ON -DENABLE_VFPV3=ON -DWITH_TBB=ON \
         -DBUILD_TBB=ON -DWITH_OPENCL=ON -DWITH_CUDA=OFF -DCMAKE_BUILD_WITH_INSTALL_RPATH=ON -DENABLE_PRECOMPILED_HEADERS=OFF \
                 -DCMAKE_TOOLCHAIN_FILE=../android/android.toolchain.cmake $@ ../..
+make -j8
+make install
 popd
