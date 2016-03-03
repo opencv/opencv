2fb406f7214d16fbf725de84d483f992255dda22
Maksim Shabunin <maksim.shabunin@itseez.com>
2016-03-03 18:55:47 +0300

    Added separate hal::SepFilter2D class

--- modules/imgproc/include/opencv2/imgproc/hal/hal.hpp
+++ modules/imgproc/include/opencv2/imgproc/hal/hal.hpp
@@ -12,26 +12,36 @@ namespace cv { namespace hal {
 
 struct CV_EXPORTS Filter2D
 {
-    static Ptr<hal::Filter2D> createFilter2D(uchar * kernel_data, size_t kernel_step, int kernel_type,
-                                             int kernel_width, int kernel_height,
-                                             int max_width, int max_height,
-                                             int stype, int dtype,
-                                             int borderType, double delta,
-                                             int anchor_x, int anchor_y,
-                                             bool isSubmatrix, bool isInplace);
-    static Ptr<hal::Filter2D> createSepFilter2D(int stype, int dtype, int ktype,
-                                                uchar * kernelx_data, size_t kernelx_step,
-                                                int kernelx_width, int kernelx_height,
-                                                uchar * kernely_data, size_t kernely_step,
-                                                int kernely_width, int kernely_height,
-                                                int anchor_x, int anchor_y,
-                                                double delta, int borderType);
+    static Ptr<hal::Filter2D> create(uchar * kernel_data, size_t kernel_step, int kernel_type,
+                                     int kernel_width, int kernel_height,
+                                     int max_width, int max_height,
+                                     int stype, int dtype,
+                                     int borderType, double delta,
+                                     int anchor_x, int anchor_y,
+                                     bool isSubmatrix, bool isInplace);
     virtual void apply(uchar * src_data, size_t src_step,
                        uchar * dst_data, size_t dst_step,
                        int width, int height,
                        int full_width, int full_height,
                        int offset_x, int offset_y) = 0;
     virtual ~Filter2D() {}
+};
+
+struct CV_EXPORTS SepFilter2D
+{
+    static Ptr<hal::SepFilter2D> create(int stype, int dtype, int ktype,
+                                        uchar * kernelx_data, size_t kernelx_step,
+                                        int kernelx_width, int kernelx_height,
+                                        uchar * kernely_data, size_t kernely_step,
+                                        int kernely_width, int kernely_height,
+                                        int anchor_x, int anchor_y,
+                                        double delta, int borderType);
+    virtual void apply(uchar * src_data, size_t src_step,
+                       uchar * dst_data, size_t dst_step,
+                       int width, int height,
+                       int full_width, int full_height,
+                       int offset_x, int offset_y) = 0;
+    virtual ~SepFilter2D() {}
 };
 
 //! @}
