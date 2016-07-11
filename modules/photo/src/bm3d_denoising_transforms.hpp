2acab33fe566ebde52d06a69ba6c2ec385f25d8b
Bartek Pawlik <bartek.pawlik@outlook.com>
2016-07-11 15:15:40 +0800

    BM3D: optimized Haar transforms.
    Signed-off-by: Bartek Pawlik <bartek.pawlik@outlook.com>

--- modules/photo/src/bm3d_denoising_transforms.hpp
+++ modules/photo/src/bm3d_denoising_transforms.hpp
@@ -95,7 +95,7 @@ static const float kCoeff[4] = {
 
 // Forward transform 4x4 block
 template <typename T>
-static void HaarColumn4x4(const T *src, short *dst, const int &step)
+inline static void HaarColumn4x4(const T *src, short *dst, const int &step)
 {
     const T *src0 = src;
     const T *src1 = src + 1 * step;
@@ -117,7 +117,7 @@ static void HaarColumn4x4(const T *src, short *dst, const int &step)
 }
 
 template <typename T>
-static void HaarRow4x4(const T *src, short *dst)
+inline static void HaarRow4x4(const T *src, short *dst)
 {
     short sum0 = (src[0] + src[1] + 1) >> 1;
     short sum1 = (src[2] + src[3] + 1) >> 1;
@@ -134,34 +134,28 @@ static void HaarRow4x4(const T *src, short *dst)
 }
 
 template <typename T>
-static void Haar4x4(const T *ptr, short *dst, const int &step)
+inline static void Haar4x4(const T *ptr, short *dst, const int &step)
 {
     short temp[16];
 
-    // Trans col
-    HaarColumn4x4(ptr, temp, step);
-    HaarColumn4x4(ptr + 1, temp + 1, step);
-    HaarColumn4x4(ptr + 2, temp + 2, step);
-    HaarColumn4x4(ptr + 3, temp + 3, step);
+    // Transform columns first
+    for (int i = 0; i < 4; ++i)
+        HaarColumn4x4(ptr + i, temp + i, step);
 
-    // Trans rows
-    HaarRow4x4(temp, dst);
-    HaarRow4x4(temp + 1 * 4, dst + 1 * 4);
-    HaarRow4x4(temp + 2 * 4, dst + 2 * 4);
-    HaarRow4x4(temp + 3 * 4, dst + 3 * 4);
+    // Then transform rows
+    for (int i = 0; i < 4; ++i)
+        HaarRow4x4(temp + i * 4, dst + i * 4);
 }
 
-static void InvHaarColumn4x4(short *src, short *dst)
+inline static void InvHaarColumn4x4(short *src, short *dst)
 {
     short src0 = src[0 * 4] * 2;
     short src1 = src[1 * 4];
     short src2 = src[2 * 4];
     short src3 = src[3 * 4];
 
-    short sum0 = (src0 + src1) >> 1;
-    short dif0 = (src0 - src1) >> 1; // this stinks!
-    sum0 *= 2;
-    dif0 *= 2;
+    short sum0 = src0 + src1;
+    short dif0 = src0 - src1;
 
     dst[0 * 4] = (sum0 + src2) >> 1;
     dst[1 * 4] = (sum0 - src2) >> 1;
@@ -169,17 +163,15 @@ static void InvHaarColumn4x4(short *src, short *dst)
     dst[3 * 4] = (dif0 - src3) >> 1;
 }
 
-static void InvHaarRow4x4(short *src, short *dst)
+inline static void InvHaarRow4x4(short *src, short *dst)
 {
     short src0 = src[0] * 2;
     short src1 = src[1];
     short src2 = src[2];
     short src3 = src[3];
 
-    short sum0 = (src0 + src1) >> 1;
-    short dif0 = (src0 - src1) >> 1; // this stinks!
-    sum0 *= 2;
-    dif0 *= 2;
+    short sum0 = src0 + src1;
+    short dif0 = src0 - src1;
 
     dst[0] = (sum0 + src2) >> 1;
     dst[1] = (sum0 - src2) >> 1;
@@ -187,24 +179,22 @@ static void InvHaarRow4x4(short *src, short *dst)
     dst[3] = (dif0 - src3) >> 1;
 }
 
-static void InvHaar4x4(short *src)
+inline static void InvHaar4x4(short *src)
 {
     short temp[16];
 
-    InvHaarColumn4x4(src, temp);
-    InvHaarColumn4x4(src + 1, temp + 1);
-    InvHaarColumn4x4(src + 2, temp + 2);
-    InvHaarColumn4x4(src + 3, temp + 3);
+    // Invert columns first
+    for (int i = 0; i < 4; ++i)
+        InvHaarColumn4x4(src + i, temp + i);
 
-    InvHaarRow4x4(temp, src);
-    InvHaarRow4x4(temp + 1 * 4, src + 1 * 4);
-    InvHaarRow4x4(temp + 2 * 4, src + 2 * 4);
-    InvHaarRow4x4(temp + 3 * 4, src + 3 * 4);
+    // Then invert rows
+    for (int i = 0; i < 4; ++i)
+        InvHaarRow4x4(temp + i * 4, src + i * 4);
 }
 
 /// 1D forward transformations
 
-static short HaarTransformShrink2(short **z, const int &n, short *&thrMap)
+inline static short HaarTransformShrink2(short **z, const int &n, short *&thrMap)
 {
     short sum = (z[0][n] + z[1][n] + 1) >> 1;
     short dif = z[0][n] - z[1][n];
@@ -219,7 +209,7 @@ static short HaarTransformShrink2(short **z, const int &n, short *&thrMap)
     return nonZeroCount;
 }
 
-static short HaarTransformShrink4(short **z, const int &n, short *&thrMap)
+inline static short HaarTransformShrink4(short **z, const int &n, short *&thrMap)
 {
     short sum0 = (z[0][n] + z[1][n] + 1) >> 1;
     short sum1 = (z[2][n] + z[3][n] + 1) >> 1;
@@ -243,7 +233,7 @@ static short HaarTransformShrink4(short **z, const int &n, short *&thrMap)
     return nonZeroCount;
 }
 
-static short HaarTransformShrink8(short **z, const int &n, short *&thrMap)
+inline static short HaarTransformShrink8(short **z, const int &n, short *&thrMap)
 {
     short sum0 = (z[0][n] + z[1][n] + 1) >> 1;
     short sum1 = (z[2][n] + z[3][n] + 1) >> 1;
@@ -287,7 +277,7 @@ static short HaarTransformShrink8(short **z, const int &n, short *&thrMap)
 /// Functions for inverse 1D transforms
 
 template <typename T>
-static void InverseHaarTransform2(T **src, const int &n)
+inline static void InverseHaarTransform2(T **src, const int &n)
 {
     T src0 = src[0][n] * 2;
     T src1 = src[1][n];
@@ -297,17 +287,15 @@ static void InverseHaarTransform2(T **src, const int &n)
 }
 
 template <typename T>
-static void InverseHaarTransform4(T **src, const int &n)
+inline static void InverseHaarTransform4(T **src, const int &n)
 {
     T src0 = src[0][n] * 2;
     T src1 = src[1][n];
     T src2 = src[2][n];
     T src3 = src[3][n];
 
-    T sum0 = (src0 + src1) >> 1;
-    T dif0 = (src0 - src1) >> 1;
-    sum0 *= 2;
-    dif0 *= 2;
+    T sum0 = src0 + src1;
+    T dif0 = src0 - src1;
 
     src[0][n] = (sum0 + src2) >> 1;
     src[1][n] = (sum0 - src2) >> 1;
@@ -316,7 +304,7 @@ static void InverseHaarTransform4(T **src, const int &n)
 }
 
 template <typename T>
-static void InverseHaarTransform8(T **src, const int &n)
+inline static void InverseHaarTransform8(T **src, const int &n)
 {
     T src0 = src[0][n] * 2;
     T src1 = src[1][n];
@@ -327,19 +315,13 @@ static void InverseHaarTransform8(T **src, const int &n)
     T src6 = src[6][n];
     T src7 = src[7][n];
 
-    T sum0 = (src0 + src1) >> 1;
-    T dif0 = (src0 - src1) >> 1;
-    sum0 *= 2;
-    dif0 *= 2;
+    T sum0 = src0 + src1;
+    T dif0 = src0 - src1;
 
-    T sum00 = (sum0 + src2) >> 1;
-    T dif00 = (sum0 - src2) >> 1;
-    T sum11 = (dif0 + src3) >> 1;
-    T dif11 = (dif0 - src3) >> 1;
-    sum00 *= 2;
-    dif00 *= 2;
-    sum11 *= 2;
-    dif11 *= 2;
+    T sum00 = sum0 + src2;
+    T dif00 = sum0 - src2;
+    T sum11 = dif0 + src3;
+    T dif11 = dif0 - src3;
 
     src[0][n] = (sum00 + src4) >> 1;
     src[1][n] = (sum00 - src4) >> 1;
