b0b46553edbaa08e2e0db122e80309b5d42d1a16
David Wolever <david@wolever.net>
2016-08-04 12:44:07 -0400

    Cleaner fix for crash on double-stop of CvVideoCamera

--- modules/videoio/src/cap_ios_video_camera.mm
+++ modules/videoio/src/cap_ios_video_camera.mm
@@ -122,10 +122,6 @@ static CGFloat DegreesToRadians(CGFloat degrees) {return degrees * M_PI / 180;}
 
 - (void)stop;
 {
-    if (self.running == NO) {
-        return;
-    }
-
     [super stop];
 
     self.videoDataOutput = nil;
@@ -134,21 +130,24 @@ static CGFloat DegreesToRadians(CGFloat degrees) {return degrees * M_PI / 180;}
     }
 
     if (self.recordVideo == YES) {
-
-        if (self.recordAssetWriter.status == AVAssetWriterStatusWriting) {
-            [self.recordAssetWriter finishWriting];
-            NSLog(@"[Camera] recording stopped");
-        } else {
-            NSLog(@"[Camera] Recording Error: asset writer status is not writing");
+        if (self.recordAssetWriter) {
+            if (self.recordAssetWriter.status == AVAssetWriterStatusWriting) {
+                [self.recordAssetWriter finishWriting];
+                NSLog(@"[Camera] recording stopped");
+            } else {
+                NSLog(@"[Camera] Recording Error: asset writer status is not writing");
+            }
+            self.recordAssetWriter = nil;
         }
 
-        self.recordAssetWriter = nil;
         self.recordAssetWriterInput = nil;
         self.recordPixelBufferAdaptor = nil;
     }
 
-    [self.customPreviewLayer removeFromSuperlayer];
-    self.customPreviewLayer = nil;
+    if (self.customPreviewLayer) {
+        [self.customPreviewLayer removeFromSuperlayer];
+        self.customPreviewLayer = nil;
+    }
 }
 
 // TODO fix
