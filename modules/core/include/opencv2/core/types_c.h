c7045184ce220a04796d8e9072ac42d2d362dc71
Bernd Kuhls <bernd.kuhls@t-online.de>
2016-09-06 11:49:00 +0200

    types_c.h: Fix compiling VFP assembler code
    Replace asm by __asm__ according to
    https://gcc.gnu.org/onlinedocs/gcc/Alternate-Keywords.html#Alternate-Keywords
    as suggested by Arnout Vandecappelle:
    http://lists.busybox.net/pipermail/buildroot/2016-September/171491.html
    
    to fix build errors in ffmpeg with opencv2 support detected by
    buildroot autobuilders:
    http://autobuild.buildroot.net/results/c32/c32a21240a9933796ee850349a62ff3c2314f25c/build-end.log
    
    Signed-off-by: Bernd Kuhls <bernd.kuhls@t-online.de>

--- modules/core/include/opencv2/core/types_c.h
+++ modules/core/include/opencv2/core/types_c.h
@@ -318,7 +318,7 @@ enum {
     int res; \
     float temp; \
     (void)temp; \
-    asm(_asm_string : [res] "=r" (res), [temp] "=w" (temp) : [value] "w" (_value)); \
+    __asm__(_asm_string : [res] "=r" (res), [temp] "=w" (temp) : [value] "w" (_value)); \
     return res;
 // 2. version for double
 #ifdef __clang__
