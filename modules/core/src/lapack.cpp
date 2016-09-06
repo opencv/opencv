cb52d249fed9c72fb2bb75a7c5f46a915ec209b6
Rostislav Vasilikhin <rostislav.vasilikhin@intel.com>
2016-09-06 14:09:44 +0300

    fixed optional args processing in SVD::compute()

--- modules/core/src/lapack.cpp
+++ modules/core/src/lapack.cpp
@@ -1560,18 +1560,18 @@ static void _SVDcompute( InputArray _aarr, OutputArray _w,
         JacobiSVD(temp_a.ptr<double>(), temp_u.step, temp_w.ptr<double>(),
               temp_v.ptr<double>(), temp_v.step, m, n, compute_uv ? urows : 0);
     }
-    temp_w.copyTo(_w);
+    if(_w.needed()) temp_w.copyTo(_w);
     if( compute_uv )
     {
         if( !at )
         {
-            transpose(temp_u, _u);
-            temp_v.copyTo(_vt);
+            if(_u.needed()) transpose(temp_u, _u);
+            if(_vt.needed()) temp_v.copyTo(_vt);
         }
         else
         {
-            transpose(temp_v, _u);
-            temp_u.copyTo(_vt);
+            if(_u.needed()) transpose(temp_v, _u);
+            if(_vt.needed()) temp_u.copyTo(_vt);
         }
     }
 }
