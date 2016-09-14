20e2f045148abc122f4480d1e18ce4a593b3d8fb
Vladislav Sovrasov <sovrasov.vlad@gmail.com>
2016-09-14 12:16:10 +0300

    Add license to interactive-calibration app sources

--- apps/interactive-calibration/calibPipeline.hpp
+++ apps/interactive-calibration/calibPipeline.hpp
@@ -1,7 +1,13 @@
+// This file is part of OpenCV project.
+// It is subject to the license terms in the LICENSE file found in the top-level directory
+// of this distribution and at http://opencv.org/license.html.
+
+
 #ifndef CALIB_PIPELINE_HPP
 #define CALIB_PIPELINE_HPP
 
 #include <vector>
+
 #include <opencv2/highgui.hpp>
 
 #include "calibCommon.hpp"
