ae28345ca58ae03f3168ac2ee3df6ba1432332fb
Vladislav Sovrasov <sovrasov.vlad@gmail.com>
2016-09-21 17:11:00 +0300

    Make test_texture_flow.py more robust

--- modules/python/test/test_texture_flow.py
+++ modules/python/test/test_texture_flow.py
@@ -21,31 +21,24 @@ class texture_flow_test(NewOpenCVTests):
 
     def test_texture_flow(self):
 
-        img = self.get_sample('samples/data/pic6.png')
+        img = self.get_sample('samples/data/chessboard.png')
 
         gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
         h, w = img.shape[:2]
 
-        eigen = cv2.cornerEigenValsAndVecs(gray, 15, 3)
+        eigen = cv2.cornerEigenValsAndVecs(gray, 5, 3)
         eigen = eigen.reshape(h, w, 3, 2)  # [[e1, e2], v1, v2]
         flow = eigen[:,:,2]
 
-        vis = img.copy()
-        vis[:] = (192 + np.uint32(vis)) / 2
-        d = 80
+        d = 300
+        eps = d / 30
+
         points =  np.dstack( np.mgrid[d/2:w:d, d/2:h:d] ).reshape(-1, 2)
 
         textureVectors = []
-
         for x, y in np.int32(points):
             textureVectors.append(np.int32(flow[y, x]*d))
 
-        eps = 0.05
-
-        testTextureVectors = [[0, 0], [0, 0], [0, 0], [0, 0], [0, 0],
-        [-38, 70], [-79, 3], [0, 0], [0, 0], [-39, 69], [-79, -1],
-        [0, 0], [0, 0], [0, -79], [17, -78], [-48, -63], [65, -46],
-        [-69, -39], [-48, -63], [-45, 66]]
-
         for i in range(len(textureVectors)):
-            self.assertLessEqual(cv2.norm(textureVectors[i] - testTextureVectors[i], cv2.NORM_L2), eps)
+            self.assertTrue(cv2.norm(textureVectors[i], cv2.NORM_L2) < eps
+            or abs(cv2.norm(textureVectors[i], cv2.NORM_L2) - d) < eps)
