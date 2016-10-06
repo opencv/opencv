72bf83d5d71958d3243d9bbc1ded3212614ad22b
Arkadiusz Raj <arek.raj@gmail.com>
2016-10-06 16:04:09 +0200

    Initial support for Aravis

--- cmake/OpenCVFindLibsVideo.cmake
+++ cmake/OpenCVFindLibsVideo.cmake
@@ -124,6 +124,20 @@ if(WITH_GIGEAPI)
   endif()
 endif(WITH_GIGEAPI)
 
+# --- Aravis SDK ---
+#ocv_clear_vars(HAVE_ARAVIS_API)
+if(WITH_ARAVIS)
+  find_path(ARAVIS_INCLUDE_PATH "arv.h"
+            PATHS /usr/local /var /opt /usr ENV ProgramFiles ENV ProgramW6432
+            PATH_SUFFIXES include "aravis-0.6"
+            DOC "The path to Aravis SDK header")
+  FIND_LIBRARY(ARAVIS_LIBRARIES NAMES libaravis-0.6)
+	MESSAGE( STATUS "Checking ARAVIS:         " ${ARAVIS_INCLUDE_PATH} )
+  if(ARAVIS_LIBRARIES AND ARAVIS_INCLUDE_PATH)
+    set(HAVE_ARAVIS_API TRUE)
+  endif()
+endif(WITH_ARAVIS)
+
 # --- Dc1394 ---
 ocv_clear_vars(HAVE_DC1394 HAVE_DC1394_2)
 if(WITH_1394)
