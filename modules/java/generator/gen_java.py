0b0142ea4ec0cd7a895a652042575b562551ab2c
abratchik <abratchik@snap2cart.com>
2016-10-07 09:42:42 +0400

    Revert "rollback of one change as requested (similar change already merged)"
    This reverts commit f3acc278c53675e81efd16b0d1be7fc2055e0e14.

--- modules/java/generator/gen_java.py
+++ modules/java/generator/gen_java.py
@@ -1349,7 +1349,7 @@ class JavaWrapperGenerator(object):
                 ret = "return (jlong) new %s(_retval_);" % self.fullTypeName(fi.ctype)
             elif fi.ctype.startswith('Ptr_'):
                 c_prologue.append("typedef Ptr<%s> %s;" % (self.fullTypeName(fi.ctype[4:]), fi.ctype))
-                ret = "%(ctype)s* curval = new %(ctype)s(_retval_);return (jlong)curval->get();" % { 'ctype':fi.ctype }
+                ret = "%(ctype)s* curval = new %(ctype)s(_retval_);return (jlong)curval;" % { 'ctype':fi.ctype }
             elif self.isWrapped(ret_type): # pointer to wrapped class:
                 ret = "return (jlong) _retval_;"
             elif type_dict[fi.ctype]["jni_type"] == "jdoubleArray":
