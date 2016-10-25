5728f796f6860246134066b4338c31c612adde77
StevenPuttemans <steven.puttemans@kuleuven.be>
2016-10-25 11:41:03 +0200

    updating python tutorials + providing necessary data

--- samples/gpu/bgfg_segm.cpp
+++ samples/gpu/bgfg_segm.cpp
@@ -23,10 +23,10 @@ enum Method
 int main(int argc, const char** argv)
 {
     cv::CommandLineParser cmd(argc, argv,
-        "{ c camera |             | use camera }"
-        "{ f file   | ../data/768x576.avi | input video file }"
-        "{ m method | mog         | method (mog, mog2, gmg, fgd) }"
-        "{ h help   |             | print help message }");
+        "{ c camera |                    | use camera }"
+        "{ f file   | ../data/vtest.avi  | input video file }"
+        "{ m method | mog                | method (mog, mog2, gmg, fgd) }"
+        "{ h help   |                    | print help message }");
 
     if (cmd.has("help") || !cmd.check())
     {
