9c1f5930ddf47df6849683053f2b7f21d8c511a4
landfillbaby <landfillbaby@users.noreply.github.com>
2016-10-31 08:15:15 +0000

    fix samples/python/common RectSelector

--- samples/python/common.py
+++ samples/python/common.py
@@ -170,15 +170,14 @@ class RectSelector:
         self.drag_start = None
         self.drag_rect = None
     def onmouse(self, event, x, y, flags, param):
-        x, y = np.int16([x, y]) # BUG
         if event == cv2.EVENT_LBUTTONDOWN:
             self.drag_start = (x, y)
             return
         if self.drag_start:
             if flags & cv2.EVENT_FLAG_LBUTTON:
                 xo, yo = self.drag_start
-                x0, y0 = np.minimum([xo, yo], [x, y])
-                x1, y1 = np.maximum([xo, yo], [x, y])
+                x0, x1 = sorted([xo, x])
+                y0, y1 = sorted([yo, y])
                 self.drag_rect = None
                 if x1-x0 > 0 and y1-y0 > 0:
                     self.drag_rect = (x0, y0, x1, y1)
