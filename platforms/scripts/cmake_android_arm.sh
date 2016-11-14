b7f16e21915c2b65bf910424babfd4c798f08158
Declan Moran <dec1@geoaze.com>
2016-11-14 17:19:22 +0100

    fix error when calling scripts/cmake_android_arm.sh with current android studio (platform version 8 no longer included, only 9 and later)
    "CMake Error at platforms/android/android.toolchain.cmake:838 (message):
    Specified Android native API level 'android-8' is not supported by your
    NDK/toolchain."

--- platforms/scripts/cmake_android_arm.sh
+++ platforms/scripts/cmake_android_arm.sh
@@ -4,4 +4,4 @@ cd `dirname $0`/..
 mkdir -p build_android_arm
 cd build_android_arm
 
-cmake -DCMAKE_BUILD_WITH_INSTALL_RPATH=ON -DCMAKE_TOOLCHAIN_FILE=../android/android.toolchain.cmake $@ ../..
+cmake -DCMAKE_BUILD_WITH_INSTALL_RPATH=ON -DANDROID_NATIVE_API_LEVEL=9 -DCMAKE_TOOLCHAIN_FILE=../android/android.toolchain.cmake $@ ../..
