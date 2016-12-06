cac93f93e069cae692829e293a5fd615e6cc8180
Alexander Alekhin <alexander.a.alekhin@gmail.com>
2016-12-07 01:34:27 +0300

    test: putText regression garbage test

--- modules/imgproc/test/test_drawing.cpp
+++ modules/imgproc/test/test_drawing.cpp
@@ -710,5 +710,20 @@ TEST(Drawing, polylines)
 }
 
 
+TEST(Drawing, putText_no_garbage)
+{
+    Size sz(640, 480);
+    Mat mat = Mat::zeros(sz, CV_8UC1);
+
+    mat = Scalar::all(0);
+    putText(mat, "029", Point(10, 350), 0, 10, Scalar(128), 15);
+
+    EXPECT_EQ(0, cv::countNonZero(mat(Rect(0, 0,           10, sz.height))));
+    EXPECT_EQ(0, cv::countNonZero(mat(Rect(sz.width-10, 0, 10, sz.height))));
+    EXPECT_EQ(0, cv::countNonZero(mat(Rect(205, 0,         10, sz.height))));
+    EXPECT_EQ(0, cv::countNonZero(mat(Rect(405, 0,         10, sz.height))));
+}
+
+
 
 } // namespace
