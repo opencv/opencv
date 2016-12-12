ffcf866e2f64397bc25dc609e1b2765b474b6784
bedbad <i.a.antonuk@gmail.com>
2016-12-12 14:29:53 -0500

    Merge pull request #7754 from LAZI-2240:toupstream2.4
    Introducing boundingRect2f() (#7754)
    
    * Introducing boundingRect2f to return exact bounding float rectangle for RotatedRect
    
    * Introducing boundingRect2f to return exact bounding float rectangle for RotatedRect
    
    * Introducing boundingRect2f() - update
    
    * Introducing boundingRect2f to return exact bounding float rectangle for RotatedRect
    
    * Introducing boundingRect2f() - update - tested

--- modules/core/src/matrix.cpp
+++ modules/core/src/matrix.cpp
@@ -4277,6 +4277,16 @@ Rect RotatedRect::boundingRect() const
     return r;
 }
 
+
+Rect_<float> RotatedRect::boundingRect2f() const
+{
+    Point2f pt[4];
+    points(pt);
+    Rect_<float> r(Point_<float>(min(min(min(pt[0].x, pt[1].x), pt[2].x), pt[3].x), min(min(min(pt[0].y, pt[1].y), pt[2].y), pt[3].y)),
+                   Point_<float>(max(max(max(pt[0].x, pt[1].x), pt[2].x), pt[3].x), max(max(max(pt[0].y, pt[1].y), pt[2].y), pt[3].y)));
+    return r;
+}
+
 }
 
 /* End of file. */
