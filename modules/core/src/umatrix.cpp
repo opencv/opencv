17feb66759e29e9898d36699b5da3c7ebd7cca81
Addison Elliott <addison.elliott@gmail.com>
2016-12-14 19:10:51 -0600

    Revert "Added Mat submatrix selection with vector<Range>"
    This reverts commit 368869f7e64156834b9d6fe54e0294470ce16417.

--- modules/core/src/umatrix.cpp
+++ modules/core/src/umatrix.cpp
@@ -512,11 +512,6 @@ UMat::UMat(const UMat& m, const Range* ranges)
     updateContinuityFlag(*this);
 }
 
-UMat::UMat(const UMat& m, const std::vector<Range>& ranges)
-    : UMat(m, ranges.data())
-{
-}
-
 UMat UMat::diag(int d) const
 {
     CV_Assert( dims <= 2 );
