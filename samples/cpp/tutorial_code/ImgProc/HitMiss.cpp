dc04ccc7b2bcf9c72462fc41afaca7a2fabeddd4
logic1988 <1988heqian@163.com>
2017-01-16 10:13:57 +0800

    Update HitMiss.cpp

--- samples/cpp/tutorial_code/ImgProc/HitMiss.cpp
+++ samples/cpp/tutorial_code/ImgProc/HitMiss.cpp
@@ -24,19 +24,14 @@ int main(){
     morphologyEx(input_image, output_image, MORPH_HITMISS, kernel);
 
     const int rate = 10;
-    
     kernel = (kernel + 1) * 127;
     kernel.convertTo(kernel, CV_8U);
     cv::resize(kernel, kernel, cv::Size(), rate, rate, INTER_NEAREST);
     imshow("kernel", kernel);
-    
     cv::resize(input_image, input_image, cv::Size(), rate, rate, INTER_NEAREST);
     imshow("Original", input_image);
-    
     cv::resize(output_image, output_image, cv::Size(), rate, rate, INTER_NEAREST);
     imshow("Hit or Miss", output_image);
-    
     waitKey(0);
-    
     return 0;
 }
