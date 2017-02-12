cd4ad716708b2a59dd1ab4900448dbba20435908
atinfinity <dandelion1124@gmail.com>
2017-02-13 00:53:35 +0900

    refactoring

--- samples/gpu/pyrlk_optical_flow.cpp
+++ samples/gpu/pyrlk_optical_flow.cpp
@@ -1,14 +1,14 @@
 #include <iostream>
 #include <vector>
 
-#include "opencv2/core.hpp"
-#include "opencv2/core/utility.hpp"
-#include "opencv2/imgproc.hpp"
-#include "opencv2/highgui.hpp"
-#include "opencv2/video.hpp"
-#include "opencv2/cudaoptflow.hpp"
-#include "opencv2/cudaimgproc.hpp"
-#include "opencv2/cudaarithm.hpp"
+#include <opencv2/core.hpp>
+#include <opencv2/core/utility.hpp>
+#include <opencv2/imgproc.hpp>
+#include <opencv2/highgui.hpp>
+#include <opencv2/video.hpp>
+#include <opencv2/cudaoptflow.hpp>
+#include <opencv2/cudaimgproc.hpp>
+#include <opencv2/cudaarithm.hpp>
 
 using namespace std;
 using namespace cv;
