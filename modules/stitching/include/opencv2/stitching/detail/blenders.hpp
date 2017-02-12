b76e88354ca6157265e3123760411c7e1c58a39a
Wenju He <hewj03@gmail.com>
2017-02-12 17:08:05 +0800

    improve MultiBandBlender cuda, add normalizeUsingWeight and addSrcWeight kernels

--- modules/stitching/include/opencv2/stitching/detail/blenders.hpp
+++ modules/stitching/include/opencv2/stitching/detail/blenders.hpp
@@ -142,6 +142,10 @@ private:
     Rect dst_roi_final_;
     bool can_use_gpu_;
     int weight_type_; //CV_32F or CV_16S
+#if defined(HAVE_OPENCV_CUDAARITHM) && defined(HAVE_OPENCV_CUDAWARPING)
+    std::vector<cuda::GpuMat> gpu_dst_pyr_laplace_;
+    std::vector<cuda::GpuMat> gpu_dst_band_weights_;
+#endif
 };
 
 
