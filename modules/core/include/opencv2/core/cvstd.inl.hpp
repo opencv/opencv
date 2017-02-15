16fc9ac752c4f4bc521e0de10866a7eb16f17af7
Fangjun KUANG <csukuangfj@gmail.com>
2017-02-15 14:55:10 +0100

    Conform to the format of std::complex.

--- modules/core/include/opencv2/core/cvstd.inl.hpp
+++ modules/core/include/opencv2/core/cvstd.inl.hpp
@@ -186,7 +186,7 @@ std::ostream& operator << (std::ostream& out, const Mat& mtx)
 template<typename _Tp> static inline
 std::ostream& operator << (std::ostream& out, const Complex<_Tp>& c)
 {
-	out << "(" << c.re << ", " << c.im << ")";
+	out << "(" << c.re << "," << c.im << ")";
 	return out;
 }    
 
