0aadab0941a4fb3d6560ed19cda3b34aa12c0040
ziggy90127 <ziggy90127@users.noreply.github.com>
2017-02-25 15:48:09 -0700

    Add AVMediaTypeMuxed capture devices to AVFoundation, just as QTKit does.

--- modules/videoio/src/cap_avfoundation_mac.mm
+++ modules/videoio/src/cap_avfoundation_mac.mm
@@ -305,7 +305,8 @@ int CvCaptureCAM::startCaptureDevice(int cameraNum) {
     NSAutoreleasePool *localpool = [[NSAutoreleasePool alloc] init];
 
     // get capture device
-    NSArray *devices = [AVCaptureDevice devicesWithMediaType: AVMediaTypeVideo];
+    NSArray *devices = [[AVCaptureDevice devicesWithMediaType: AVMediaTypeVideo]
+            arrayByAddingObjectsFromArray:[AVCaptureDevice devicesWithMediaType:AVMediaTypeMuxed]];
 
     if ( devices.count == 0 ) {
         fprintf(stderr, "OpenCV: AVFoundation didn't find any attached Video Input Devices!\n");
