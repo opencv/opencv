6e12391c919db2cb89002c8bee1f0ab1ad4a66c6
vskarlsruhe <valentinschild@gmail.com>
2017-02-28 10:03:09 +0100

    Code cleanup, removed cv:: because using namespace cv is already used.

--- samples/cpp/tutorial_code/ImgProc/HitMiss.cpp
+++ samples/cpp/tutorial_code/ImgProc/HitMiss.cpp
@@ -26,11 +26,11 @@ int main(){
     const int rate = 10;
     kernel = (kernel + 1) * 127;
     kernel.convertTo(kernel, CV_8U);
-    cv::resize(kernel, kernel, cv::Size(), rate, rate, INTER_NEAREST);
+    resize(kernel, kernel, Size(), rate, rate, INTER_NEAREST);
     imshow("kernel", kernel);
-    cv::resize(input_image, input_image, cv::Size(), rate, rate, INTER_NEAREST);
+    resize(input_image, input_image, Size(), rate, rate, INTER_NEAREST);
     imshow("Original", input_image);
-    cv::resize(output_image, output_image, cv::Size(), rate, rate, INTER_NEAREST);
+    resize(output_image, output_image, Size(), rate, rate, INTER_NEAREST);
     imshow("Hit or Miss", output_image);
     waitKey(0);
     return 0;
