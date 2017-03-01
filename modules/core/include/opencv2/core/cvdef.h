0c1bcf354c78f367a7af4f0ced9cc2b130bc499e
ville-v <ville-v@users.noreply.github.com>
2017-03-01 08:47:49 +0200

    Fix issue #8278: "CV_XADD compile errors with Embarcadero C++ Builder 10.1"

--- modules/core/include/opencv2/core/cvdef.h
+++ modules/core/include/opencv2/core/cvdef.h
@@ -438,7 +438,7 @@ Cv64suf;
 
 #ifdef CV_XADD
   // allow to use user-defined macro
-#elif defined __GNUC__
+#elif defined __GNUC__ || defined __clang__
 #  if defined __clang__ && __clang_major__ >= 3 && !defined __ANDROID__ && !defined __EMSCRIPTEN__ && !defined(__CUDACC__)
 #    ifdef __ATOMIC_ACQ_REL
 #      define CV_XADD(addr, delta) __c11_atomic_fetch_add((_Atomic(int)*)(addr), delta, __ATOMIC_ACQ_REL)
