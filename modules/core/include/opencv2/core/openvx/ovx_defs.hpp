6c04eeb687eee50743d97f535b91d0ce1b0cc458
Vitaly Tuzov <terfendail@mediana.jetos.com>
2017-04-11 13:22:38 +0300

    Disabled vxuConvolution call for Sobel, GaussianBlur and Box filter evaluation

--- modules/core/include/opencv2/core/openvx/ovx_defs.hpp
+++ modules/core/include/opencv2/core/openvx/ovx_defs.hpp
@@ -32,7 +32,6 @@ template <> inline bool skipSmallImages<VX_KERNEL_GAUSSIAN_3x3>(int w, int h) { 
 template <> inline bool skipSmallImages<VX_KERNEL_BOX_3x3>(int w, int h) { return w*h < 640 * 480; }
 template <> inline bool skipSmallImages<VX_KERNEL_HISTOGRAM>(int w, int h) { return w*h < 2048 * 1536; }
 template <> inline bool skipSmallImages<VX_KERNEL_SOBEL_3x3>(int w, int h) { return w*h < 320 * 240; }
-template <> inline bool skipSmallImages<VX_KERNEL_CUSTOM_CONVOLUTION>(int w, int h) { return w*h < 640 * 480; }
 
 }}
 
