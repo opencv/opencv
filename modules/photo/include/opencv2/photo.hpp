39954cc6afababe25be1e004cef80c55af3e7011
Amro <amroamroamro@gmail.com>
2017-05-18 18:45:09 +0300

    generalize number of channels
    plus minor edits and fixes

--- modules/photo/include/opencv2/photo.hpp
+++ modules/photo/include/opencv2/photo.hpp
@@ -591,7 +591,7 @@ public:
 @param samples number of pixel locations to use
 @param lambda smoothness term weight. Greater values produce smoother results, but can alter the
 response.
-@param random if true sample pixel locations are chosen at random, otherwise the form a
+@param random if true sample pixel locations are chosen at random, otherwise they form a
 rectangular grid.
  */
 CV_EXPORTS_W Ptr<CalibrateDebevec> createCalibrateDebevec(int samples = 70, float lambda = 10.0f, bool random = false);
