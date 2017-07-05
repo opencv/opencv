9a0a47d3693b29c3e05c175edd9c254e804fbed3
Arkadiusz Raj <arek.raj@gmail.com>
2017-07-05 11:30:43 +0200

    Fixing Aravis support compilation issue

--- cmake/OpenCVFindLibsVideo.cmake
+++ cmake/OpenCVFindLibsVideo.cmake
@@ -127,13 +127,18 @@ endif(WITH_GIGEAPI)
 # --- Aravis SDK ---
 ocv_clear_vars(HAVE_ARAVIS_API)
 if(WITH_ARAVIS)
-  find_path(ARAVIS_INCLUDE_PATH "arv.h"
-            PATHS /usr/local /var /opt /usr ENV ProgramFiles ENV ProgramW6432
-            PATH_SUFFIXES include "aravis-0.6" "aravis-0.4"
-            DOC "The path to Aravis SDK headers")
-  find_library(ARAVIS_LIBRARIES NAMES "aravis-0.6" "aravis-0.4" )
-  if(ARAVIS_LIBRARIES AND ARAVIS_INCLUDE_PATH)
-    set(HAVE_ARAVIS_API TRUE)
+  check_module(glib-2.0 HAVE_ARAVIS_GLIB VIDEOIO)
+  if(HAVE_ARAVIS_GLIB)
+    find_path(ARAVIS_INCLUDE_PATH "arv.h"
+              PATHS /usr/local /var /opt /usr ENV ProgramFiles ENV ProgramW6432
+              PATH_SUFFIXES include "aravis-0.6" "aravis-0.4"
+              DOC "The path to Aravis SDK headers")
+    find_library(ARAVIS_LIBRARIES NAMES "aravis-0.6" "aravis-0.4" )
+    if(ARAVIS_LIBRARIES AND ARAVIS_INCLUDE_PATH)
+      set(HAVE_ARAVIS_API TRUE)
+    endif()
+  else()
+    message("Can not build Aravis support without glib2")
   endif()
 endif(WITH_ARAVIS)
 
