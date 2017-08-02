8be1ba708e970e67e5d4274d4c12a64757fdb6ff
Alexander Alekhin <alexander.alekhin@intel.com>
2017-08-02 23:48:54 +0300

    features2d: don't use rand(), because it is not thread-safe
    Results are not stable with srand()

--- modules/features2d/src/kaze/AKAZEFeatures.cpp
+++ modules/features2d/src/kaze/AKAZEFeatures.cpp
@@ -2077,7 +2077,7 @@ void generateDescriptorSubsample(Mat& sampleList, Mat& comparisons, int nbits,
     }
   }
 
-  srand(1024);
+  RNG rng(1024);
   Mat_<int> comps = Mat_<int>(nchannels * (int)ceil(nbits / (float)nchannels), 2);
   comps = 1000;
 
@@ -2089,7 +2089,7 @@ void generateDescriptorSubsample(Mat& sampleList, Mat& comparisons, int nbits,
   samples = -1;
 
   for (int i = 0; i < npicks; i++) {
-    int k = rand() % (fullM.rows - i);
+    int k = rng(fullM.rows - i);
     if (i < 6) {
       // Force use of the coarser grid values and comparisons
       k = i;
