c9d1d626e269a0b9bf8e4ffa1462af7f3ad799a7
Emanuele Ruffaldi <emanuele.ruffaldi@gmail.com>
2017-08-20 00:26:59 +0200

    core ipp for mingw32/64: stub RunTmChk and fixed lookup of IPP name

--- cmake/OpenCVFindIPP.cmake
+++ cmake/OpenCVFindIPP.cmake
@@ -140,28 +140,34 @@ macro(ipp_detect_version)
       set(IPP_LIB_PREFIX ${CMAKE_STATIC_LIBRARY_PREFIX})
       set(IPP_LIB_SUFFIX ${CMAKE_STATIC_LIBRARY_SUFFIX})
     endif ()
-    if (EXISTS ${IPP_LIBRARY_DIR}/${IPP_LIB_PREFIX}${IPP_PREFIX}${name}${IPP_SUFFIX}${IPP_LIB_SUFFIX})
+    set(IPP_LIBRARYFILEPART ${IPP_LIB_PREFIX}${IPP_PREFIX}${name}${IPP_SUFFIX}${IPP_LIB_SUFFIX})
+    if (MINGW AND NOT EXISTS ${IPP_LIBRARY_DIR}/${IPP_LIBRARYFILEPART})
+      # IPP is named ippicvmt.lib
+      set(IPP_LIBRARYFILEPART ippicvmt.lib)
+    endif()
+
+    if (EXISTS ${IPP_LIBRARY_DIR}/${IPP_LIBRARYFILEPART})
       if (BUILD_WITH_DYNAMIC_IPP AND NOT HAVE_IPP_ICV_ONLY)
         # When using dynamic libraries from standalone Intel IPP it is your responsibility to install those on the target system
-        list(APPEND IPP_LIBRARIES ${IPP_LIBRARY_DIR}/${IPP_LIB_PREFIX}${IPP_PREFIX}${name}${IPP_SUFFIX}${IPP_LIB_SUFFIX})
+        list(APPEND IPP_LIBRARIES ${IPP_LIBRARY_DIR}/${IPP_LIBRARYFILEPART})
       else ()
         add_library(ipp${name} STATIC IMPORTED)
         set_target_properties(ipp${name} PROPERTIES
           IMPORTED_LINK_INTERFACE_LIBRARIES ""
-          IMPORTED_LOCATION ${IPP_LIBRARY_DIR}/${IPP_LIB_PREFIX}${IPP_PREFIX}${name}${IPP_SUFFIX}${IPP_LIB_SUFFIX}
+          IMPORTED_LOCATION ${IPP_LIBRARY_DIR}/${IPP_LIBRARYFILEPART}
         )
         list(APPEND IPP_LIBRARIES ipp${name})
         if (NOT BUILD_SHARED_LIBS)
           # CMake doesn't support "install(TARGETS ${IPP_PREFIX}${name} " command with imported targets
-          install(FILES ${IPP_LIBRARY_DIR}/${IPP_LIB_PREFIX}${IPP_PREFIX}${name}${IPP_SUFFIX}${IPP_LIB_SUFFIX}
+          install(FILES ${IPP_LIBRARY_DIR}/${IPP_LIBRARYFILEPART}
                   DESTINATION ${OPENCV_3P_LIB_INSTALL_PATH} COMPONENT dev)
           string(TOUPPER ${name} uname)
-          set(IPP${uname}_INSTALL_PATH "${CMAKE_INSTALL_PREFIX}/${OPENCV_3P_LIB_INSTALL_PATH}/${IPP_LIB_PREFIX}${IPP_PREFIX}${name}${IPP_SUFFIX}${IPP_LIB_SUFFIX}" CACHE INTERNAL "" FORCE)
-          set(IPP${uname}_LOCATION_PATH "${IPP_LIBRARY_DIR}/${IPP_LIB_PREFIX}${IPP_PREFIX}${name}${IPP_SUFFIX}${IPP_LIB_SUFFIX}" CACHE INTERNAL "" FORCE)
+          set(IPP${uname}_INSTALL_PATH "${CMAKE_INSTALL_PREFIX}/${OPENCV_3P_LIB_INSTALL_PATH}/${IPP_LIBRARYFILEPART}" CACHE INTERNAL "" FORCE)
+          set(IPP${uname}_LOCATION_PATH "${IPP_LIBRARY_DIR}/${IPP_LIBRARYFILEPART}" CACHE INTERNAL "" FORCE)
         endif()
       endif()
     else()
-      message(STATUS "Can't find Intel IPP library: ${name} at ${IPP_LIBRARY_DIR}/${IPP_LIB_PREFIX}${IPP_PREFIX}${name}${IPP_SUFFIX}${IPP_LIB_SUFFIX}")
+      message(STATUS "Can't find Intel IPP library: ${name} at ${IPP_LIBRARY_DIR}/${IPP_LIBRARYFILEPART}")
     endif()
   endmacro()
 
@@ -259,8 +265,11 @@ if(WIN32 AND MINGW AND NOT IPP_VERSION_MAJOR LESS 7)
     # From Windows SDK 7.1
     #   (usually in "C:\Program Files\Microsoft Visual Studio 10.0\VC\lib"),
     # to avoid undefined reference to __security_cookie and _chkstk:
-    set(MSV_RUNTMCHK "RunTmChk")
-    set(IPP_LIBRARIES ${IPP_LIBRARIES} ${MSV_RUNTMCHK}${IPP_LIB_SUFFIX})
+
+    # New version with custom implementation (e.g. for mingw32 mxe)
+    add_library(RunTmChkAlt ${OpenCV_SOURCE_DIR}/3rdparty/ippicv/runtmchk_mingw.c)
+    set(MSV_RUNTMCHK "RunTmChkAlt")
+    set(IPP_LIBRARIES ${IPP_LIBRARIES} ${MSV_RUNTMCHK}) #${IPP_LIB_SUFFIX})
 
     # To avoid undefined reference to _alldiv and _chkstk
     # ===================================================
