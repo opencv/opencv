cd4e01109a3c5c6ec727cc5da2ece9a46e9039a1
Ningxin Hu <ningxin.hu@intel.com>
2017-08-24 15:30:39 +0800

    Fix space before semicolon

--- modules/js/src/core_bindings.cpp
+++ modules/js/src/core_bindings.cpp
@@ -524,40 +524,40 @@ EMSCRIPTEN_BINDINGS(binding_utils)
 
     function("meanShift", select_overload<emscripten::val(const cv::Mat&, Rect&, TermCriteria)>(&binding_utils::meanShiftWrapper));
 
-    constant("CV_8UC1", CV_8UC1) ;
-    constant("CV_8UC2", CV_8UC2) ;
-    constant("CV_8UC3", CV_8UC3) ;
-    constant("CV_8UC4", CV_8UC4) ;
+    constant("CV_8UC1", CV_8UC1);
+    constant("CV_8UC2", CV_8UC2);
+    constant("CV_8UC3", CV_8UC3);
+    constant("CV_8UC4", CV_8UC4);
 
-    constant("CV_8SC1", CV_8SC1) ;
-    constant("CV_8SC2", CV_8SC2) ;
-    constant("CV_8SC3", CV_8SC3) ;
-    constant("CV_8SC4", CV_8SC4) ;
+    constant("CV_8SC1", CV_8SC1);
+    constant("CV_8SC2", CV_8SC2);
+    constant("CV_8SC3", CV_8SC3);
+    constant("CV_8SC4", CV_8SC4);
 
-    constant("CV_16UC1", CV_16UC1) ;
-    constant("CV_16UC2", CV_16UC2) ;
-    constant("CV_16UC3", CV_16UC3) ;
-    constant("CV_16UC4", CV_16UC4) ;
+    constant("CV_16UC1", CV_16UC1);
+    constant("CV_16UC2", CV_16UC2);
+    constant("CV_16UC3", CV_16UC3);
+    constant("CV_16UC4", CV_16UC4);
 
-    constant("CV_16SC1", CV_16SC1) ;
-    constant("CV_16SC2", CV_16SC2) ;
-    constant("CV_16SC3", CV_16SC3) ;
-    constant("CV_16SC4", CV_16SC4) ;
+    constant("CV_16SC1", CV_16SC1);
+    constant("CV_16SC2", CV_16SC2);
+    constant("CV_16SC3", CV_16SC3);
+    constant("CV_16SC4", CV_16SC4);
 
-    constant("CV_32SC1", CV_32SC1) ;
-    constant("CV_32SC2", CV_32SC2) ;
-    constant("CV_32SC3", CV_32SC3) ;
-    constant("CV_32SC4", CV_32SC4) ;
+    constant("CV_32SC1", CV_32SC1);
+    constant("CV_32SC2", CV_32SC2);
+    constant("CV_32SC3", CV_32SC3);
+    constant("CV_32SC4", CV_32SC4);
 
-    constant("CV_32FC1", CV_32FC1) ;
-    constant("CV_32FC2", CV_32FC2) ;
-    constant("CV_32FC3", CV_32FC3) ;
-    constant("CV_32FC4", CV_32FC4) ;
+    constant("CV_32FC1", CV_32FC1);
+    constant("CV_32FC2", CV_32FC2);
+    constant("CV_32FC3", CV_32FC3);
+    constant("CV_32FC4", CV_32FC4);
 
-    constant("CV_64FC1", CV_64FC1) ;
-    constant("CV_64FC2", CV_64FC2) ;
-    constant("CV_64FC3", CV_64FC3) ;
-    constant("CV_64FC4", CV_64FC4) ;
+    constant("CV_64FC1", CV_64FC1);
+    constant("CV_64FC2", CV_64FC2);
+    constant("CV_64FC3", CV_64FC3);
+    constant("CV_64FC4", CV_64FC4);
 
     constant("CV_8U", CV_8U);
     constant("CV_8S", CV_8S);
