9a77375fcf375b91e87e4168a2e3dd7170241545
Ningxin Hu <ningxin.hu@intel.com>
2017-08-25 09:19:46 +0800

    Fix coding style of tests.js

--- modules/js/test/tests.js
+++ modules/js/test/tests.js
@@ -39,12 +39,13 @@
 //
 //M*/
 
-var testrunner = require('qunit');
+let testrunner = require('qunit');
 testrunner.options.maxBlockDuration = 20000; // cause opencv_js.js need time to load
 
 
 testrunner.run({
     code: 'opencv.js',
-    tests: ['test_mat.js', 'test_utils.js', 'test_imgproc.js', 'test_objdetect.js', 'test_video.js']}, function(err, report) {
+    tests: ['test_mat.js', 'test_utils.js', 'test_imgproc.js',
+            'test_objdetect.js', 'test_video.js']}, function(err, report) {
         console.log(report.failed + ' failed, ' + report.passed + ' passed');
 });
