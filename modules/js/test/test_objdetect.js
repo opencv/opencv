d78cd61c23fcbb74270d3ca580e7cc1d5a995beb
Ningxin Hu <ningxin.hu@intel.com>
2017-08-25 09:17:42 +0800

    Fix coding style of test_objdetect.js

--- modules/js/test/test_objdetect.js
+++ modules/js/test/test_objdetect.js
@@ -70,104 +70,92 @@
 
 if (typeof module !== 'undefined' && module.exports) {
     // The envrionment is Node.js
-    var cv = require('./opencv.js');
-    cv.FS_createLazyFile('/', 'haarcascade_frontalface_default.xml', 'haarcascade_frontalface_default.xml', true, false);
+    let cv = require('./opencv.js');
+    cv.FS_createLazyFile('/', 'haarcascade_frontalface_default.xml',
+                         'haarcascade_frontalface_default.xml', true, false);
 }
 
-QUnit.module ("Object Detection", {});
-QUnit.test("Cascade classification", function(assert) {
-	// Group rectangle
-	// CV_EXPORTS_W void groupRectangles(CV_IN_OUT std::vector<Rect>& rectList, CV_OUT std::vector<int>& weights,
-	//                                int groupThreshold, double eps = 0.2);
-	{
-		let rectList = new cv.RectVector(),
-			weights = new cv.IntVector(),
-			groupThreshold = 1,
-			eps = 0.2;
+QUnit.module('Object Detection', {});
+QUnit.test('Cascade classification', function(assert) {
+    // Group rectangle
+    {
+        let rectList = new cv.RectVector();
+        let weights = new cv.IntVector();
+        let groupThreshold = 1;
+        const eps = 0.2;
 
-		let rect1 = new cv.Rect(1, 2, 3, 4),
-			rect2 = new cv.Rect(1, 4, 2, 3);
+        let rect1 = new cv.Rect(1, 2, 3, 4);
+        let rect2 = new cv.Rect(1, 4, 2, 3);
 
-		rectList.push_back(rect1);
-		rectList.push_back(rect2);
+        rectList.push_back(rect1);
+        rectList.push_back(rect2);
 
-		cv.groupRectangles(rectList, weights, groupThreshold, eps);
+        cv.groupRectangles(rectList, weights, groupThreshold, eps);
 
 
-		rectList.delete();
-		weights.delete();
-	}
+        rectList.delete();
+        weights.delete();
+    }
 
-	// CascadeClassifier
-	{
-		let classifier = new cv.CascadeClassifier(),
-			modelPath = '/haarcascade_frontalface_default.xml';
+    // CascadeClassifier
+    {
+        let classifier = new cv.CascadeClassifier();
+        const modelPath = '/haarcascade_frontalface_default.xml';
 
-		assert.equal(classifier.empty(), true);
-
-
-		classifier.load(modelPath);
-		assert.equal(classifier.empty(), false);
-
-		// cv.HAAR = 0
-		//assert.equal(classifier.getFeatureType(), 0);
-
-		let image = cv.Mat.eye({height: 10, width: 10}, cv.CV_8UC3),
-			objects = new cv.RectVector(),
-			numDetections = new cv.IntVector(),
-			scaleFactor = 1.1,
-			minNeighbors = 3,
-			flags = 0,
-			minSize = {height: 0, width: 0},
-			maxSize = {height: 10, width: 10};
-
-		classifier.detectMultiScale2(image, objects, numDetections, scaleFactor,
-		minNeighbors, flags, minSize, maxSize);
-
-		// test default parameters
-		classifier.detectMultiScale2(image, objects, numDetections, scaleFactor,
-		minNeighbors, flags, minSize);
-		classifier.detectMultiScale2(image, objects, numDetections, scaleFactor,
-		minNeighbors, flags);
-		classifier.detectMultiScale2(image, objects, numDetections, scaleFactor,
-		minNeighbors);
-		classifier.detectMultiScale2(image, objects, numDetections, scaleFactor);
-
-		classifier.delete();
-		objects.delete();
-		numDetections.delete();
-	}
-
-	// HOGDescriptor
-	{
-		let hog = new cv.HOGDescriptor(),
-			mat = new cv.Mat({height: 10, width: 10}, cv.CV_8UC1),
-			descriptors = new cv.FloatVector(),
-			locations = new cv.PointVector();
+        assert.equal(classifier.empty(), true);
 
 
-		assert.equal(hog.winSize.height, 128);
-		assert.equal(hog.winSize.width, 64);
-		assert.equal(hog.nbins, 9);
-		assert.equal(hog.derivAperture, 1);
-		assert.equal(hog.winSigma, -1);
-		assert.equal(hog.histogramNormType, 0);
-		assert.equal(hog.nlevels, 64);
+        classifier.load(modelPath);
+        assert.equal(classifier.empty(), false);
 
-		hog.nlevels = 32;
-		assert.equal(hog.nlevels, 32);
+        let image = cv.Mat.eye({height: 10, width: 10}, cv.CV_8UC3);
+        let objects = new cv.RectVector();
+        let numDetections = new cv.IntVector();
+        const scaleFactor = 1.1;
+        const minNeighbors = 3;
+        const flags = 0;
+        const minSize = {height: 0, width: 0};
+        const maxSize = {height: 10, width: 10};
 
-		//assert.equal(hog.empty(), false);
+        classifier.detectMultiScale2(image, objects, numDetections, scaleFactor,
+        minNeighbors, flags, minSize, maxSize);
 
-		//hog.compute(mat, descriptors, [4, 4], [4, 4], locations);
+        // test default parameters
+        classifier.detectMultiScale2(image, objects, numDetections, scaleFactor,
+        minNeighbors, flags, minSize);
+        classifier.detectMultiScale2(image, objects, numDetections, scaleFactor,
+        minNeighbors, flags);
+        classifier.detectMultiScale2(image, objects, numDetections, scaleFactor,
+        minNeighbors);
+        classifier.detectMultiScale2(image, objects, numDetections, scaleFactor);
 
-		// hog.detectMultiScale();
+        classifier.delete();
+        objects.delete();
+        numDetections.delete();
+    }
 
-		// hog.computeGradient();
+    // HOGDescriptor
+    {
+        let hog = new cv.HOGDescriptor();
+        let mat = new cv.Mat({height: 10, width: 10}, cv.CV_8UC1);
+        let descriptors = new cv.FloatVector();
+        let locations = new cv.PointVector();
 
-		hog.delete();
-		mat.delete();
-		descriptors.delete();
-		locations.delete();
-	}
+
+        assert.equal(hog.winSize.height, 128);
+        assert.equal(hog.winSize.width, 64);
+        assert.equal(hog.nbins, 9);
+        assert.equal(hog.derivAperture, 1);
+        assert.equal(hog.winSigma, -1);
+        assert.equal(hog.histogramNormType, 0);
+        assert.equal(hog.nlevels, 64);
+
+        hog.nlevels = 32;
+        assert.equal(hog.nlevels, 32);
+
+        hog.delete();
+        mat.delete();
+        descriptors.delete();
+        locations.delete();
+    }
 });
