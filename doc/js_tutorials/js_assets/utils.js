a0e69fb15b1eb6c0722191dbb791aca682cbf211
Ningxin Hu <ningxin.hu@intel.com>
2017-09-20 09:25:06 +0800

    Fix coding style of utils.js

--- doc/js_tutorials/js_assets/utils.js
+++ doc/js_tutorials/js_assets/utils.js
@@ -1,27 +1,27 @@
-function loadImageToCanvas(url, cavansId) {
-  var canvas = document.getElementById(cavansId);
-  var ctx = canvas.getContext('2d');
-  var img = new Image();
+function loadImageToCanvas(url, cavansId) { // eslint-disable-line no-unused-vars
+  let canvas = document.getElementById(cavansId);
+  let ctx = canvas.getContext('2d');
+  let img = new Image();
   img.crossOrigin = 'anonymous';
   img.onload = function() {
     canvas.width = img.width;
     canvas.height = img.height;
     ctx.drawImage(img, 0, 0, img.width, img.height);
-  }
+  };
   img.src = url;
 }
 
-function executeCode(codeEditorId, errorOutputId) {
+function executeCode(codeEditorId, errorOutputId) { // eslint-disable-line no-unused-vars
   let code = document.getElementById(codeEditorId).value;
   try {
     eval(code);
     document.getElementById(errorOutputId).innerHTML = ' ';
-  } catch(err) {
+  } catch (err) {
     document.getElementById(errorOutputId).innerHTML = err;
   }
 }
 
-function loadCode(scriptId, codeEditorId) {
+function loadCode(scriptId, codeEditorId) { // eslint-disable-line no-unused-vars
   let scriptNode = document.getElementById(scriptId);
   let codeEditor = document.getElementById(codeEditorId);
   if (scriptNode.type !== 'text/code-snippet') {
