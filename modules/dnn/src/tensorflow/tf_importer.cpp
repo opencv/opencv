bdd8cc697a3288af2681eb442866c5e4ec87aa4a
Dmitry Kurtaev <dmitry.kurtaev+github@gmail.com>
2017-09-20 13:30:25 +0300

    Import wrapped Dropout subgraphs from TensorFlow

--- modules/dnn/src/tensorflow/tf_importer.cpp
+++ modules/dnn/src/tensorflow/tf_importer.cpp
@@ -363,7 +363,7 @@ void RemoveIdentityOps(tensorflow::GraphDef& net) {
         const tensorflow::NodeDef &layer = net.node(li);
         String type = layer.op();
 
-        if (type == "Identity") {
+        if (type == "Identity" || type == "Dropout") {
             identity_ops_idx.push_back(li);
             identity_ops[layer.name()] = layer.input(0);
         }
