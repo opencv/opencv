e81e3f9531999f57cd44b3c5b592a2fd227b4e23
Ningxin Hu <ningxin.hu@intel.com>
2017-09-21 16:27:40 +0800

    Use 4 spaces indention for js examples

--- doc/js_tutorials/js_assets/utils.js
+++ doc/js_tutorials/js_assets/utils.js
@@ -1,55 +1,55 @@
 function loadImageToCanvas(url, cavansId) { // eslint-disable-line no-unused-vars
-  let canvas = document.getElementById(cavansId);
-  let ctx = canvas.getContext('2d');
-  let img = new Image();
-  img.crossOrigin = 'anonymous';
-  img.onload = function() {
-    canvas.width = img.width;
-    canvas.height = img.height;
-    ctx.drawImage(img, 0, 0, img.width, img.height);
-  };
-  img.src = url;
+    let canvas = document.getElementById(cavansId);
+    let ctx = canvas.getContext('2d');
+    let img = new Image();
+    img.crossOrigin = 'anonymous';
+    img.onload = function() {
+        canvas.width = img.width;
+        canvas.height = img.height;
+        ctx.drawImage(img, 0, 0, img.width, img.height);
+    };
+    img.src = url;
 }
 
 function executeCode(codeEditorId, errorOutputId) { // eslint-disable-line no-unused-vars
-  let code = document.getElementById(codeEditorId).value;
-  try {
-    eval(code);
-    document.getElementById(errorOutputId).innerHTML = ' ';
-  } catch (err) {
-    if (typeof err === 'number') {
-      if (!isNaN(err)) {
-        err = 'Exception: ' + cv.exceptionFromPtr(err).msg;
-      }
-    } else if (typeof err === 'string') {
-      let ptr = Number(err.split(' ')[0]);
-      if (!isNaN(ptr)) {
-        err = 'Exception: ' + cv.exceptionFromPtr(ptr).msg;
-      }
-    } else if (err instanceof Error) {
-      err = err.stack.replace(/\n/g, '<br>');
+    let code = document.getElementById(codeEditorId).value;
+    try {
+        eval(code);
+        document.getElementById(errorOutputId).innerHTML = ' ';
+    } catch (err) {
+        if (typeof err === 'number') {
+            if (!isNaN(err)) {
+                err = 'Exception: ' + cv.exceptionFromPtr(err).msg;
+            }
+        } else if (typeof err === 'string') {
+            let ptr = Number(err.split(' ')[0]);
+            if (!isNaN(ptr)) {
+                err = 'Exception: ' + cv.exceptionFromPtr(ptr).msg;
+            }
+        } else if (err instanceof Error) {
+            err = err.stack.replace(/\n/g, '<br>');
+        }
+        document.getElementById(errorOutputId).innerHTML = err;
     }
-    document.getElementById(errorOutputId).innerHTML = err;
-  }
 }
 
 function loadCode(scriptId, codeEditorId) { // eslint-disable-line no-unused-vars
-  let scriptNode = document.getElementById(scriptId);
-  let codeEditor = document.getElementById(codeEditorId);
-  if (scriptNode.type !== 'text/code-snippet') {
-    throw Error('Unknown code snippet type');
-  }
-  codeEditor.value = scriptNode.text.replace(/^\n/, '');
+    let scriptNode = document.getElementById(scriptId);
+    let codeEditor = document.getElementById(codeEditorId);
+    if (scriptNode.type !== 'text/code-snippet') {
+        throw Error('Unknown code snippet type');
+    }
+    codeEditor.value = scriptNode.text.replace(/^\n/, '');
 }
 
 function addFileInputHandler(fileInputId, canvasId) { // eslint-disable-line no-unused-vars
-  let inputElement = document.getElementById(fileInputId);
-  inputElement.addEventListener('change', (e) => {
-    let imgUrl = URL.createObjectURL(e.target.files[0]);
-    loadImageToCanvas(imgUrl, canvasId);
-  }, false);
+    let inputElement = document.getElementById(fileInputId);
+    inputElement.addEventListener('change', (e) => {
+        let imgUrl = URL.createObjectURL(e.target.files[0]);
+        loadImageToCanvas(imgUrl, canvasId);
+    }, false);
 }
 
 function onOpenCvLoadError(errorOutputId) { // eslint-disable-line no-unused-vars
-  document.getElementById(errorOutputId).innerHTML = 'Failed to load opencv.js';
+    document.getElementById(errorOutputId).innerHTML = 'Failed to load opencv.js';
 }
