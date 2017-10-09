bd6fb497bc4e7629c577b9a5b07dff355c424073
Alexander Alekhin <alexander.alekhin@intel.com>
2017-10-09 14:22:38 +0300

    OpenCV version++
    OpenCV 3.3.1

--- platforms/android/service/engine/src/org/opencv/engine/OpenCVEngineService.java
+++ platforms/android/service/engine/src/org/opencv/engine/OpenCVEngineService.java
@@ -134,7 +134,7 @@ public class OpenCVEngineService extends Service {
 
             @Override
             public int getEngineVersion() throws RemoteException {
-                int version = 3300;
+                int version = 3310;
                 try {
                     version = getPackageManager().getPackageInfo(getPackageName(), 0).versionCode;
                 } catch (NameNotFoundException e) {
