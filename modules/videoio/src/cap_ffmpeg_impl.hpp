fd00529bb77bd550b6d2a945d5b8e8bc79a228ac
collin <yaochengxiang@126.com>
2017-10-26 16:57:24 +0800

    add nvidia hardware encode/decode support in videoio with ffmpeg

--- modules/videoio/src/cap_ffmpeg_impl.hpp
+++ modules/videoio/src/cap_ffmpeg_impl.hpp
@@ -168,6 +168,16 @@ extern "C" {
 #endif
 #endif
 
+#if LIBAVCODEC_BUILD >= CALC_FFMPEG_VERSION(58, 0, 100)
+#ifndef PKT_FLAG_KEY
+#define PKT_FLAG_KEY AV_PKT_FLAG_KEY
+#endif
+
+#ifndef CODEC_FLAG_GLOBAL_HEADER
+#define CODEC_FLAG_GLOBAL_HEADER AV_CODEC_FLAG_GLOBAL_HEADER
+#endif
+#endif
+
 #if USE_AV_INTERRUPT_CALLBACK
 #define LIBAVFORMAT_INTERRUPT_OPEN_TIMEOUT_MS 30000
 #define LIBAVFORMAT_INTERRUPT_READ_TIMEOUT_MS 30000
@@ -1566,15 +1576,12 @@ static int icv_av_write_frame_FFMPEG( AVFormatContext * oc, AVStream * video_st,
 #endif
     int ret = OPENCV_NO_FRAMES_WRITTEN_CODE;
 
+#if LIBAVFORMAT_VERSION_MAJOR < 58    
     if (oc->oformat->flags & AVFMT_RAWPICTURE) {
         /* raw video case. The API will change slightly in the near
            futur for that */
         AVPacket pkt;
         av_init_packet(&pkt);
-
-#ifndef PKT_FLAG_KEY
-#define PKT_FLAG_KEY AV_PKT_FLAG_KEY
-#endif
 
         pkt.flags |= PKT_FLAG_KEY;
         pkt.stream_index= video_st->index;
@@ -1583,6 +1590,9 @@ static int icv_av_write_frame_FFMPEG( AVFormatContext * oc, AVStream * video_st,
 
         ret = av_write_frame(oc, &pkt);
     } else {
+#else
+    {
+#endif
         /* encode the image */
         AVPacket pkt;
         av_init_packet(&pkt);
@@ -1740,7 +1750,11 @@ void CvVideoWriter_FFMPEG::close()
     /* write the trailer, if any */
     if(ok && oc)
     {
+    #if LIBAVFORMAT_VERSION_MAJOR < 58        
         if( (oc->oformat->flags & AVFMT_RAWPICTURE) == 0 )
+    #else
+        if( oc->oformat->flags == 0 )
+    #endif
         {
             for(;;)
             {
@@ -2036,7 +2050,11 @@ bool CvVideoWriter_FFMPEG::open( const char * filename, int fourcc,
 
     outbuf = NULL;
 
+#if LIBAVFORMAT_VERSION_MAJOR < 58
     if (!(oc->oformat->flags & AVFMT_RAWPICTURE)) {
+#else
+    {
+#endif
         /* allocate output buffer */
         /* assume we will never get codec output with more than 4 bytes per pixel... */
         outbuf_size = width*height*4;
