f2a06771f0afee715a3d7c5d990ef6bbfff5b137
Florian Echtler <floe@butterbrot.org>
2017-11-09 21:22:11 +0100

    fix method signature

--- modules/java/generator/src/cpp/Mat.cpp
+++ modules/java/generator/src/cpp/Mat.cpp
@@ -55,13 +55,13 @@ JNIEXPORT jlong JNICALL Java_org_opencv_core_Mat_n_1Mat__
 //   Mat::Mat(int rows, int cols, int type, void* data)
 //
 
-JNIEXPORT jlong JNICALL Java_org_opencv_core_Mat_n_1Mat__IIIO
+JNIEXPORT jlong JNICALL Java_org_opencv_core_Mat_n_1Mat__IIILjava_nio_ByteBuffer_2
   (JNIEnv* env, jclass, jint rows, jint cols, jint type, jobject data);
 
-JNIEXPORT jlong JNICALL Java_org_opencv_core_Mat_n_1Mat__IIIO
+JNIEXPORT jlong JNICALL Java_org_opencv_core_Mat_n_1Mat__IIILjava_nio_ByteBuffer_2
   (JNIEnv* env, jclass, jint rows, jint cols, jint type, jobject data)
 {
-    static const char method_name[] = "Mat::n_1Mat__IIIL()";
+    static const char method_name[] = "Mat::n_1Mat__IIILByteBuffer()";
     try {
         LOGD("%s", method_name);
         return (jlong) new Mat( rows, cols, type, (void*)env->GetDirectBufferAddress(data) );
