6117d083c91b4e5b6a1e966828d564ab17cb3513
elenagvo <elena.gvozdeva@intel.com>
2017-12-22 12:55:38 +0300

    HAL for minMaxIdx

--- modules/core/src/hal_replacement.hpp
+++ modules/core/src/hal_replacement.hpp
@@ -712,6 +712,22 @@ inline int hal_ni_gemm64fc(const double* src1, size_t src1_step, const double* s
 #define cv_hal_gemm64fc hal_ni_gemm64fc
 //! @endcond
 
+/**
+   @brief Finds the global minimum and maximum in an array.
+   @param src_data,src_step Source image
+   @param width,height Source image dimensions
+   @param depth Depth of source image
+   @param minVal,maxVal Pointer to the returned global minimum and maximum in an array.
+   @param minIdx,maxIdx Pointer to the returned minimum and maximum location.
+   @param mask Specified array region.
+*/
+inline int hal_ni_minMaxIdx(const uchar* src_data, size_t src_step, int width, int height, int depth, double* minVal, double* maxVal,
+                            int* minIdx, int* maxIdx, uchar* mask) { return CV_HAL_ERROR_NOT_IMPLEMENTED; }
+
+//! @cond IGNORED
+#define cv_hal_minMaxIdx hal_ni_minMaxIdx
+//! @endcond
+
 //! @}
 
 
