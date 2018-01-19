763119ea91efc0d50ed76c7f0c2978bc1cf735cf
Maksim Shabunin <maksim.shabunin@gmail.com>
2018-01-19 13:57:54 +0300

    python: added persistence test, remove temp files

--- modules/python/test/test_algorithm_rw.py
+++ modules/python/test/test_algorithm_rw.py
@@ -1,19 +1,19 @@
 #!/usr/bin/env python
-
-'''
-Algorithm serializaion test
-'''
+"""Algorithm serializaion test."""
+from tempfile import NamedTemporaryFile
 import cv2 as cv
-
 from tests_common import NewOpenCVTests
+
 
 class algorithm_rw_test(NewOpenCVTests):
     def test_algorithm_rw(self):
+        f = NamedTemporaryFile(prefix="opencv_python_algorithm_", suffix=".yml")
+
         # some arbitrary non-default parameters
         gold = cv.AKAZE_create(descriptor_size=1, descriptor_channels=2, nOctaves=3, threshold=4.0)
-        gold.write(cv.FileStorage("params.yml", 1), "AKAZE")
+        gold.write(cv.FileStorage(f.name, cv.FILE_STORAGE_WRITE), "AKAZE")
 
-        fs = cv.FileStorage("params.yml", 0)
+        fs = cv.FileStorage(f.name, cv.FILE_STORAGE_READ)
         algorithm = cv.AKAZE_create()
         algorithm.read(fs.getNode("AKAZE"))
 
@@ -21,3 +21,5 @@ class algorithm_rw_test(NewOpenCVTests):
         self.assertEqual(algorithm.getDescriptorChannels(), 2)
         self.assertEqual(algorithm.getNOctaves(), 3)
         self.assertEqual(algorithm.getThreshold(), 4.0)
+
+        f.close()
---
+++ modules/python/test/test_persistence.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python
+""""Core serializaion tests."""
+from tempfile import NamedTemporaryFile
+import cv2 as cv
+import numpy as np
+from tests_common import NewOpenCVTests
+
+
+class persistence_test(NewOpenCVTests):
+    def test_basic(self):
+        f = NamedTemporaryFile(prefix="opencv_python_persistence_", suffix=".yml")
+
+        # Writing ...
+        expected = np.array([[[0, 1, 2, 3, 4]]])
+        fs = cv.FileStorage(f.name, cv.FILE_STORAGE_WRITE)
+        fs.write("test", expected)
+        fs.release()
+
+        # Reading ...
+        fs = cv.FileStorage(f.name, cv.FILE_STORAGE_READ)
+        root = fs.getFirstTopLevelNode()
+        self.assertEqual(root.name(), "test")
+        test = fs.getNode("test")
+        self.assertEqual(test.empty(), False)
+        self.assertEqual(test.name(), "test")
+        self.assertEqual(test.type(), cv.FILE_NODE_MAP)
+        self.assertEqual(test.isMap(), True)
+        actual = test.mat()
+        self.assertEqual(actual.shape, expected.shape)
+        self.assertEqual(np.array_equal(expected, actual), True)
+        fs.release()
+
+        f.close()
