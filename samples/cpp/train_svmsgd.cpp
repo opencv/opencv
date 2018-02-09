633b0e56a52ebd88910a91ea84d1472c2d0f3cea
Maksim Shabunin <maksim.shabunin@gmail.com>
2018-02-09 13:24:18 +0300

    Fixed compilation warnings in samples:
    - use per-module includes instead of "opencv.hpp"
    - unused function warnings
    - undefined macro warning

--- samples/cpp/train_svmsgd.cpp
+++ samples/cpp/train_svmsgd.cpp
@@ -1,7 +1,8 @@
-#include <opencv2/opencv.hpp>
+#include "opencv2/core.hpp"
 #include "opencv2/video/tracking.hpp"
-#include "opencv2/imgproc/imgproc.hpp"
-#include "opencv2/highgui/highgui.hpp"
+#include "opencv2/imgproc.hpp"
+#include "opencv2/highgui.hpp"
+#include "opencv2/ml.hpp"
 
 using namespace cv;
 using namespace cv::ml;
--- samples/cpp/tutorial_code/snippets/core_various.cpp
+++ samples/cpp/tutorial_code/snippets/core_various.cpp
@@ -1,4 +1,7 @@
-#include <opencv2/opencv.hpp>
+#include <opencv2/core.hpp>
+#include <opencv2/imgproc.hpp>
+#include <opencv2/highgui.hpp>
+#include <opencv2/features2d.hpp>
 
 using namespace std;
 using namespace cv;
