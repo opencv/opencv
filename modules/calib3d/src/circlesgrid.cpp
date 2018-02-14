bbd4eb6097ba9f6e81a76c8dc7a1d48731eff849
Vadim Pisarevsky <vadim.pisarevsky@gmail.com>
2018-02-14 15:14:20 +0300

    revert the previous commit

--- modules/calib3d/src/circlesgrid.cpp
+++ modules/calib3d/src/circlesgrid.cpp
@@ -117,7 +117,8 @@ void CirclesGridClusterFinder::hierarchicalClustering(const std::vector<Point2f>
         Mat tmpRow = dists.row(minIdx);
         Mat tmpCol = dists.col(minIdx);
         cv::min(dists.row(minLoc.x), dists.row(minLoc.y), tmpRow);
-        transpose(tmpRow, tmpCol);
+        tmpRow = tmpRow.t();
+        tmpRow.copyTo(tmpCol);
 
         clusters[minIdx].splice(clusters[minIdx].end(), clusters[maxIdx]);
         patternClusterIdx = minIdx;
