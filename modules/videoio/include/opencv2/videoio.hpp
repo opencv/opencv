9819ebc0954c2df62943ebbd5936d325e5dc89e1
Alexander Alekhin <alexander.alekhin@intel.com>
2018-02-21 18:42:40 +0300

    videoio: add missing CAP_PROP_SAR_NUM/DEN propepties into .hpp file
    reuse av_guess_sample_aspect_ratio() if available in ffmpeg

--- modules/videoio/include/opencv2/videoio.hpp
+++ modules/videoio/include/opencv2/videoio.hpp
@@ -164,7 +164,12 @@ enum VideoCaptureProperties {
        CAP_PROP_IRIS          =36,
        CAP_PROP_SETTINGS      =37, //!< Pop up video/camera filter dialog (note: only supported by DSHOW backend currently. The property value is ignored)
        CAP_PROP_BUFFERSIZE    =38,
-       CAP_PROP_AUTOFOCUS     =39
+       CAP_PROP_AUTOFOCUS     =39,
+       CAP_PROP_SAR_NUM       =40, //!< Sample aspect ratio: num/den (num)
+       CAP_PROP_SAR_DEN       =41, //!< Sample aspect ratio: num/den (den)
+#ifndef CV_DOXYGEN
+       CV__CAP_PROP_LATEST
+#endif
      };
 
 
