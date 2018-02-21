318ac6b8c9e62ca20a96f2ab3c71252b1a7b3cbc
Alexander Alekhin <alexander.alekhin@intel.com>
2018-02-21 13:15:10 +0300

    imgcodecs: fix RBaseStream hang on truncated inputs
    6e8241b78dfddbc3a5053aa105f13373b44535cb

--- modules/highgui/src/bitstrm.cpp
+++ modules/highgui/src/bitstrm.cpp
@@ -104,7 +104,6 @@ void  RBaseStream::readBlock()
     fseek( m_file, m_block_pos, SEEK_SET );
     size_t readed = fread( m_start, 1, m_block_size, m_file );
     m_end = m_start + readed;
-    m_current = m_start;
 
     if( readed == 0 || m_current >= m_end )
         throw RBS_THROW_EOS;
