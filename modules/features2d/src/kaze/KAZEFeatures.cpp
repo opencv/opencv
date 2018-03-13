a2e73015d32f26d8078082f57079e9b47e72e222
vasiliev-vb <vasilievvladimir89@gmail.com>
2018-03-13 15:09:36 +0000

    Fixed bug in KAZE features orientation.
    Bug was added in f6ceeaa commit, different angle computation functions have different parameter order.

--- modules/features2d/src/kaze/KAZEFeatures.cpp
+++ modules/features2d/src/kaze/KAZEFeatures.cpp
@@ -606,7 +606,7 @@ void KAZEFeatures::Compute_Main_Orientation(KeyPoint &kpt, const std::vector<TEv
                     resY[idx] = 0.0;
                 }
 
-                Ang[idx] = fastAtan2(resX[idx], resY[idx]) * (float)(CV_PI / 180.0f);
+                Ang[idx] = fastAtan2(resY[idx], resX[idx]) * (float)(CV_PI / 180.0f);
                 ++idx;
             }
         }
@@ -638,7 +638,7 @@ void KAZEFeatures::Compute_Main_Orientation(KeyPoint &kpt, const std::vector<TEv
         if (sumX*sumX + sumY*sumY > max) {
             // store largest orientation
             max = sumX*sumX + sumY*sumY;
-            kpt.angle = fastAtan2(sumX, sumY);
+            kpt.angle = fastAtan2(sumY, sumX);
         }
     }
 }
