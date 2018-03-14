539680d3a586335940a26cbcf34352050a361997
berak <px1704@web.de>
2018-03-14 05:58:45 +0100

    fix some samples colliding with std::beta
    also add comments explaining the change

--- samples/cpp/tutorial_code/ImgProc/changing_contrast_brightness_image/changing_contrast_brightness_image.cpp
+++ samples/cpp/tutorial_code/ImgProc/changing_contrast_brightness_image/changing_contrast_brightness_image.cpp
@@ -2,7 +2,7 @@
 #include "opencv2/imgcodecs.hpp"
 #include "opencv2/highgui.hpp"
 
-using namespace std;
+// we're NOT "using namespace std;" here, to avoid collisions between the beta variable and std::beta in c++17
 using namespace cv;
 
 namespace
