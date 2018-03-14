182b52d23dc117c59a348343c35a060bffdb3fa4
berak <px1704@web.de>
2018-03-14 05:58:45 +0100

    fix some samples colliding with std::beta
    also add comments explaining the change

--- samples/cpp/tutorial_code/HighGUI/BasicLinearTransformsTrackbar.cpp
+++ samples/cpp/tutorial_code/HighGUI/BasicLinearTransformsTrackbar.cpp
@@ -8,6 +8,7 @@
 #include "opencv2/imgcodecs.hpp"
 #include "opencv2/highgui.hpp"
 
+// we're NOT "using namespace std;" here, to avoid collisions between the beta variable and std::beta in c++17
 using namespace cv;
 
 /** Global Variables */
