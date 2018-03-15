2912991edcc43a29951aca2921b9187e70b416c8
Rostislav Vasilikhin <savuor@gmail.com>
2018-03-15 14:10:40 +0300

    Merge pull request #10869 from savuor:color_cpp_split
    color.cpp split (#10869)
    
    * initial split is done
    
    * files renamed (these names are excluded during compilation)
    
    * IPP code moved to corresponding files
    
    * splineBuild, splineInterpolate -> color_lab.cpp
    
    * Lab, Luv: little refactored
    
    * it compiles (didn't check work); Lab OCL code moved to color_lab.cpp
    
    * cvtcolor.cl: Lab/Luv part moved to color_lab.cl
    
    * cvtcolor.cl: color_rgb.cl extracted
    
    * cvtcolor.cl: color_yuv.cl separated
    
    * cvtcolor.cl: color_hsv.cl extracted
    
    * cvtcolor.cl: extracted to color_lab.cl and color_rgb.cl
    
    * helper functions moved to hpp file
    
    * Lab, Luv: moved to color_lab.cpp
    
    * CPU XYZ: to color_lab.cpp
    
    * OCL XYZ: to color_lab.cpp
    
    * warning fixed
    
    * CvtHelper added
    
    * CPU YUV: to color_yuv.cpp, helpers to color.hpp
    
    * CPU HLS/HSV: to color_hsv.cpp
    
    * CPU BGR2BGR: to color_rgb.cpp
    
    * CPU RGB: to color_rgb.cpp
    
    * extra arg removed
    
    * CPU YUV: to color_yuv.cpp
    
    * color code decoded
    
    * OclHelper added, some funcs rewritten
    
    * color_lab.cpp: refactored to use OclHelper
    
    * OCL RGB: to color_rgb.cpp
    
    * OCL HLS/HSV: to color_hsv.cpp
    
    * OCL YUV: to color_yuv.cpp
    
    * OCL YUV planes: to color_yuv.cpp
    
    * OCL: color code reduced
    
    * licence to demosaicing.cpp
    
    * IPP func tables to color_rgb.cpp
    
    * code cleanup
    
    * HAVE_OPENCL ifdefs added
    
    * helpers made more common
    
    * fixed two plane YUV with separate mats
    
    * fixed warning in gcc7.2.0
    
    * precomp header fixed
    
    * color space classification functions fixed
    
    * helpers fixed
    
    * rename: isSRGB -> is_sRGB

--- modules/imgproc/src/demosaicing.cpp
+++ modules/imgproc/src/demosaicing.cpp
@@ -41,6 +41,50 @@
 //
 //M*/
 
+/********************************* COPYRIGHT NOTICE *******************************\
+  Original code for Bayer->BGR/RGB conversion is provided by Dirk Schaefer
+  from MD-Mathematische Dienste GmbH. Below is the copyright notice:
+
+    IMPORTANT: READ BEFORE DOWNLOADING, COPYING, INSTALLING OR USING.
+    By downloading, copying, installing or using the software you agree
+    to this license. If you do not agree to this license, do not download,
+    install, copy or use the software.
+
+    Contributors License Agreement:
+
+      Copyright (c) 2002,
+      MD-Mathematische Dienste GmbH
+      Im Defdahl 5-10
+      44141 Dortmund
+      Germany
+      www.md-it.de
+
+    Redistribution and use in source and binary forms,
+    with or without modification, are permitted provided
+    that the following conditions are met:
+
+    Redistributions of source code must retain
+    the above copyright notice, this list of conditions and the following disclaimer.
+    Redistributions in binary form must reproduce the above copyright notice,
+    this list of conditions and the following disclaimer in the documentation
+    and/or other materials provided with the distribution.
+    The name of Contributor may not be used to endorse or promote products
+    derived from this software without specific prior written permission.
+
+    THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+    AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO,
+    THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR
+    PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE CONTRIBUTORS BE LIABLE
+    FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+    DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS
+    OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
+    HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT,
+    STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+    ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF
+    THE POSSIBILITY OF SUCH DAMAGE.
+\**********************************************************************************/
+
+
 #include "precomp.hpp"
 
 #include <limits>
