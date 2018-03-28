ee6748acb5f4fa9adc88171a79451aca9df40704
Rajkiran Natarajan <saskatchewancatch@gmail.com>
2018-03-27 21:15:45 -0700

    fix cv::imwrite to allow cv::UMat and vectors of it to be written

--- modules/imgcodecs/src/loadsave.cpp
+++ modules/imgcodecs/src/loadsave.cpp
@@ -717,9 +717,9 @@ bool imwrite( const String& filename, InputArray _img,
     CV_TRACE_FUNCTION();
     std::vector<Mat> img_vec;
     //Did we get a Mat or a vector of Mats?
-    if (_img.isMat())
+    if (_img.isMat() || _img.isUMat())
         img_vec.push_back(_img.getMat());
-    else if (_img.isMatVector())
+    else if (_img.isMatVector() || _img.isUMatVector())
         _img.getMatVector(img_vec);
 
     return imwrite_(filename, img_vec, params, false);
