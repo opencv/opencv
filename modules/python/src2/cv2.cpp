eae3ed0c08e1a4d05eefe7976b93844b3fcfb24a
Dan Mašek <dan.masek@gmail.com>
2018-04-03 03:02:21 +0200

    Fix a leak described in issue #11205

--- modules/python/src2/cv2.cpp
+++ modules/python/src2/cv2.cpp
@@ -1584,6 +1584,8 @@ static void OnChange(int pos, void *param)
     PyObject *r = PyObject_Call(PyTuple_GetItem(o, 0), args, NULL);
     if (r == NULL)
         PyErr_Print();
+    else
+        Py_DECREF(r);
     Py_DECREF(args);
     PyGILState_Release(gstate);
 }
@@ -1626,6 +1628,8 @@ static void OnButtonChange(int state, void *param)
     PyObject *r = PyObject_Call(PyTuple_GetItem(o, 0), args, NULL);
     if (r == NULL)
         PyErr_Print();
+    else
+        Py_DECREF(r);
     Py_DECREF(args);
     PyGILState_Release(gstate);
 }
