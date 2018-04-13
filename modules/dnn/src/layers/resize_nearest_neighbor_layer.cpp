97fec07d9630e3c3010210490dd73bb8d3008ae5
Dmitry Kurtaev <dmitry.kurtaev+github@gmail.com>
2018-04-13 18:53:12 +0300

    Support YOLOv3 model from Darknet

--- modules/dnn/src/layers/resize_nearest_neighbor_layer.cpp
+++ modules/dnn/src/layers/resize_nearest_neighbor_layer.cpp
@@ -16,9 +16,11 @@ public:
     ResizeNearestNeighborLayerImpl(const LayerParams& params)
     {
         setParamsFrom(params);
-        CV_Assert(params.has("width"), params.has("height"));
-        outWidth = params.get<float>("width");
-        outHeight = params.get<float>("height");
+        CV_Assert(params.has("width") && params.has("height") || params.has("zoom_factor"));
+        CV_Assert(!params.has("width") && !params.has("height") || !params.has("zoom_factor"));
+        outWidth = params.get<float>("width", 0);
+        outHeight = params.get<float>("height", 0);
+        zoomFactor = params.get<int>("zoom_factor", 1);
         alignCorners = params.get<bool>("align_corners", false);
         if (alignCorners)
             CV_Error(Error::StsNotImplemented, "Nearest neighborhood resize with align_corners=true is not implemented");
@@ -31,10 +33,19 @@ public:
     {
         CV_Assert(inputs.size() == 1, inputs[0].size() == 4);
         outputs.resize(1, inputs[0]);
-        outputs[0][2] = outHeight;
-        outputs[0][3] = outWidth;
+        outputs[0][2] = outHeight > 0 ? outHeight : (outputs[0][2] * zoomFactor);
+        outputs[0][3] = outWidth > 0 ? outWidth : (outputs[0][3] * zoomFactor);
         // We can work in-place (do nothing) if input shape == output shape.
         return (outputs[0][2] == inputs[0][2]) && (outputs[0][3] == inputs[0][3]);
+    }
+
+    virtual void finalize(const std::vector<Mat*>& inputs, std::vector<Mat> &outputs) CV_OVERRIDE
+    {
+        if (!outWidth && !outHeight)
+        {
+            outHeight = outputs[0].size[2];
+            outWidth = outputs[0].size[3];
+        }
     }
 
     void forward(InputArrayOfArrays inputs_arr, OutputArrayOfArrays outputs_arr, OutputArrayOfArrays internals_arr) CV_OVERRIDE
@@ -65,7 +76,7 @@ public:
         }
     }
 private:
-    int outWidth, outHeight;
+    int outWidth, outHeight, zoomFactor;
     bool alignCorners;
 };
 
