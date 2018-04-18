c2e42cbf18d34d03218a16921578bd66fe1ea9fb
Simon Que <sque@chromium.org>
2018-04-18 13:42:42 -0400

    Fix CV_Asserts with negation of strings
    {!"string"} causes some compilers to throw a warning.
    
    The value of the string is not that important -- it's only for printing
    the assertion message.
    
    Replace these calls with:
    
      CV_Error(Error::StsError, "string")
    
    to suppress the warning.

--- modules/core/src/persistence_base64.cpp
+++ modules/core/src/persistence_base64.cpp
@@ -640,7 +640,9 @@ private:
                     pack.func = to_binary<double>;
                     break;
                 case 'r':
-                default: { CV_Assert(!"type not support"); break; }
+                default:
+                    CV_Error(cv::Error::StsError, "type is not supported");
+                    break;
                 };
 
                 offset = static_cast<size_t>(cvAlign(static_cast<int>(offset), static_cast<int>(size)));
@@ -799,7 +801,9 @@ private:
                     pack.func = binary_to<double>;
                     break;
                 case 'r':
-                default:  { CV_Assert(!"type not support"); break; }
+                default:
+                    CV_Error(cv::Error::StsError, "type is not supported");
+                    break;
                 }; // need a better way for outputting error.
 
                 offset = static_cast<size_t>(cvAlign(static_cast<int>(offset), static_cast<int>(size)));
@@ -817,7 +821,9 @@ private:
                 case 'f': { pack.cv_type = CV_32F; break; }
                 case 'd': { pack.cv_type = CV_64F; break; }
                 case 'r':
-                default:  { CV_Assert(!"type is not support"); break; }
+                default:
+                    CV_Error(cv::Error::StsError, "type is not supported");
+                    break;
                 } // need a better way for outputting error.
 
                 binary_to_funcs.push_back(pack);
