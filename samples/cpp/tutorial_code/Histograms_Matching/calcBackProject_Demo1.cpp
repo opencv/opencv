4c1c3147d989f5b38ea3417b39d1fab4c8102990
catree <catree.catreus@outlook.com>
2018-05-23 19:44:27 +0200

    Add Java and Python code for the following imgproc tutorials: Affine Transformations, Histogram Equalization, Histogram Calculation, Histogram Comparison, Back Projection.

--- samples/cpp/tutorial_code/Histograms_Matching/calcBackProject_Demo1.cpp
+++ samples/cpp/tutorial_code/Histograms_Matching/calcBackProject_Demo1.cpp
@@ -14,48 +14,54 @@ using namespace cv;
 using namespace std;
 
 /// Global Variables
-Mat src; Mat hsv; Mat hue;
+Mat hue;
 int bins = 25;
 
 /// Function Headers
 void Hist_and_Backproj(int, void* );
 
-
 /**
  * @function main
  */
-int main( int, char** argv )
+int main( int argc, char* argv[] )
 {
-  /// Read the image
-  src = imread( argv[1], IMREAD_COLOR );
-
-  if( src.empty() )
-    { cout<<"Usage: ./calcBackProject_Demo1 <path_to_image>"<<endl;
-      return -1;
+    //! [Read the image]
+    CommandLineParser parser( argc, argv, "{@input |  | input image}" );
+    Mat src = imread( parser.get<String>( "@input" ) );
+    if( src.empty() )
+    {
+        cout << "Could not open or find the image!\n" << endl;
+        cout << "Usage: " << argv[0] << " <Input image>" << endl;
+        return -1;
     }
+    //! [Read the image]
 
-  /// Transform it to HSV
-  cvtColor( src, hsv, COLOR_BGR2HSV );
+    //! [Transform it to HSV]
+    Mat hsv;
+    cvtColor( src, hsv, COLOR_BGR2HSV );
+    //! [Transform it to HSV]
 
-  /// Use only the Hue value
-  hue.create( hsv.size(), hsv.depth() );
-  int ch[] = { 0, 0 };
-  mixChannels( &hsv, 1, &hue, 1, ch, 1 );
+    //! [Use only the Hue value]
+    hue.create(hsv.size(), hsv.depth());
+    int ch[] = { 0, 0 };
+    mixChannels( &hsv, 1, &hue, 1, ch, 1 );
+    //! [Use only the Hue value]
 
-  /// Create Trackbar to enter the number of bins
-  const char* window_image = "Source image";
-  namedWindow( window_image, WINDOW_AUTOSIZE );
-  createTrackbar("* Hue  bins: ", window_image, &bins, 180, Hist_and_Backproj );
-  Hist_and_Backproj(0, 0);
+    //! [Create Trackbar to enter the number of bins]
+    const char* window_image = "Source image";
+    namedWindow( window_image );
+    createTrackbar("* Hue  bins: ", window_image, &bins, 180, Hist_and_Backproj );
+    Hist_and_Backproj(0, 0);
+    //! [Create Trackbar to enter the number of bins]
 
-  /// Show the image
-  imshow( window_image, src );
+    //! [Show the image]
+    imshow( window_image, src );
+    // Wait until user exits the program
+    waitKey();
+    //! [Show the image]
 
-  /// Wait until user exits the program
-  waitKey(0);
-  return 0;
+    return 0;
 }
-
 
 /**
  * @function Hist_and_Backproj
@@ -63,30 +69,38 @@ int main( int, char** argv )
  */
 void Hist_and_Backproj(int, void* )
 {
-  MatND hist;
-  int histSize = MAX( bins, 2 );
-  float hue_range[] = { 0, 180 };
-  const float* ranges = { hue_range };
+    //! [initialize]
+    int histSize = MAX( bins, 2 );
+    float hue_range[] = { 0, 180 };
+    const float* ranges = { hue_range };
+    //! [initialize]
 
-  /// Get the Histogram and normalize it
-  calcHist( &hue, 1, 0, Mat(), hist, 1, &histSize, &ranges, true, false );
-  normalize( hist, hist, 0, 255, NORM_MINMAX, -1, Mat() );
+    //! [Get the Histogram and normalize it]
+    Mat hist;
+    calcHist( &hue, 1, 0, Mat(), hist, 1, &histSize, &ranges, true, false );
+    normalize( hist, hist, 0, 255, NORM_MINMAX, -1, Mat() );
+    //! [Get the Histogram and normalize it]
 
-  /// Get Backprojection
-  MatND backproj;
-  calcBackProject( &hue, 1, 0, hist, backproj, &ranges, 1, true );
+    //! [Get Backprojection]
+    Mat backproj;
+    calcBackProject( &hue, 1, 0, hist, backproj, &ranges, 1, true );
+    //! [Get Backprojection]
 
-  /// Draw the backproj
-  imshow( "BackProj", backproj );
+    //! [Draw the backproj]
+    imshow( "BackProj", backproj );
+    //! [Draw the backproj]
 
-  /// Draw the histogram
-  int w = 400; int h = 400;
-  int bin_w = cvRound( (double) w / histSize );
-  Mat histImg = Mat::zeros( w, h, CV_8UC3 );
+    //! [Draw the histogram]
+    int w = 400, h = 400;
+    int bin_w = cvRound( (double) w / histSize );
+    Mat histImg = Mat::zeros( h, w, CV_8UC3 );
 
-  for( int i = 0; i < bins; i ++ )
-     { rectangle( histImg, Point( i*bin_w, h ), Point( (i+1)*bin_w, h - cvRound( hist.at<float>(i)*h/255.0 ) ), Scalar( 0, 0, 255 ), -1 ); }
+    for (int i = 0; i < bins; i++)
+    {
+        rectangle( histImg, Point( i*bin_w, h ), Point( (i+1)*bin_w, h - cvRound( hist.at<float>(i)*h/255.0 ) ),
+                   Scalar( 0, 0, 255 ), FILLED );
+    }
 
-  imshow( "Histogram", histImg );
-
+    imshow( "Histogram", histImg );
+    //! [Draw the histogram]
 }
