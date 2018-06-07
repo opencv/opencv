e0d28aa893639c3cf0a5a01e475050a7f68c038e
Sayed Adel <seiko@imavr.com>
2018-06-07 14:16:48 +0300

    core:ppc Fix java CoreTest/testMahalanobis (#11677)
    * core:ppc Fix java CoreTest/testMahalanobis
    
    * core:ppc Fix warning unused variable on clang

--- modules/core/src/system.cpp
+++ modules/core/src/system.cpp
@@ -739,7 +739,6 @@ int64 getCPUTickCount(void)
 
 int64 getCPUTickCount(void)
 {
-    int64 result = 0;
     unsigned upper, lower, tmp;
     __asm__ volatile(
                      "0:                  \n"
