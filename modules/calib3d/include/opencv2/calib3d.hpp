8742db82128f2263fe71deab02da8069204d5ff1
Vadim Pisarevsky <vadim.pisarevsky@gmail.com>
2018-06-27 15:21:22 +0300

    updated the test (made it shorter and providing better diagnostic) and significantly improved implementation (get rid of heavy repeated and/or unnecessary operations)

--- modules/calib3d/include/opencv2/calib3d.hpp
+++ modules/calib3d/include/opencv2/calib3d.hpp
@@ -1999,7 +1999,7 @@ CV_EXPORTS_W int decomposeHomographyMat(InputArray H,
 @param beforePoints Vector of (rectified) visible reference points before the homography is applied
 @param afterPoints Vector of (rectified) visible reference points after the homography is applied
 @param possibleSolutions Vector of int indices representing the viable solution set after filtering
-@param pointsMask Mat representing the mask for the inliers as given by the findHomography function
+@param pointsMask optional Mat/Vector of 8u type representing the mask for the inliers as given by the findHomography function
 
 This function is intended to filter the output of the decomposeHomographyMat based on additional
 information as described in @cite Malis . The summary of the method: the decomposeHomographyMat function
