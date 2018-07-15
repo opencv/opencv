531e8a8d6913485c21edb99625a59fa5647b9d42
berak <px1704@web.de>
2018-07-15 11:32:00 +0200

    core: remove unused dataAsRow flag from LDA

--- modules/core/include/opencv2/core.hpp
+++ modules/core/include/opencv2/core.hpp
@@ -2599,7 +2599,6 @@ public:
     static Mat subspaceReconstruct(InputArray W, InputArray mean, InputArray src);
 
 protected:
-    bool _dataAsRow; // unused, but needed for 3.0 ABI compatibility.
     int _num_components;
     Mat _eigenvectors;
     Mat _eigenvalues;
