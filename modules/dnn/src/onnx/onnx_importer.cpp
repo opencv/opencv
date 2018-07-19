be614eacd042c3a78d86d320214073433c7f1132
l-bat <liubov.batanina@intel.com>
2018-07-19 17:51:12 +0300

    Add import from pb

--- modules/dnn/src/onnx/onnx_importer.cpp
+++ modules/dnn/src/onnx/onnx_importer.cpp
@@ -97,18 +97,57 @@ std::map<std::string, cv::Mat> ONNXImporter::getWeights(const onnx::GraphProto& 
   std::map<std::string, cv::Mat> layers_weights;
   onnx::TensorProto_DataType datatype;
   for (int i = 0; i < graph_proto.initializer_size(); i++) {
+      std::cout << "name = " << graph_proto.initializer(i).name() << '\n';
     tensor_proto = graph_proto.initializer(i);
     datatype = tensor_proto.data_type();
 
     if (datatype == onnx::TensorProto_DataType_FLOAT) {
-       std::vector<int> sizes;
-       for (int i = 0; i < tensor_proto.dims_size(); i++) {
-          sizes.push_back(tensor_proto.dims(i));
-       }
+        int* sizes = new int [tensor_proto.dims_size()];
+        for (int i = 0; i < tensor_proto.dims_size(); i++) {
+            sizes[i] = tensor_proto.dims(i);
+        }
        char* val = const_cast<char*>(tensor_proto.raw_data().c_str());
-       cv::Mat blob(tensor_proto.dims_size(), sizes.data(), CV_32FC1, val);
-      // std::cout << "blob" << blob << '\n';
+       cv::Mat blob(tensor_proto.dims_size(), sizes, CV_32FC1, val);
        layers_weights.insert(std::pair<std::string, cv::Mat>(tensor_proto.name(), blob.clone()));
+       std::cout << "blob sizes : " <<  blob.size << '\n';
+       delete[] sizes;
+    }
+    else if (datatype == onnx::TensorProto_DataType_INT64) {
+        int* sizes = new int [tensor_proto.dims_size()];
+        for (int i = 0; i < tensor_proto.dims_size(); i++) {
+            sizes[i] = tensor_proto.dims(i);
+        }
+        cv::Mat blob(tensor_proto.dims_size(), sizes, CV_32SC1);
+
+        char* val = const_cast<char*>(tensor_proto.raw_data().c_str());
+        int64_t* src = reinterpret_cast<int64_t*>(val);
+        int32_t* dst = reinterpret_cast<int32_t*>(blob.data);
+
+        for (int i = 0; i < blob.total(); i++) {
+            //*(blob.data + i) = reinterpret_cast<uchar*>(ptr64 + i);
+        //    blob.data = reinterpret_cast<uchar*>(ptr64);
+            // ptr64++;
+            // blob.data++;
+            dst[i] = static_cast<int32_t>(src[i]);
+        }
+        std::cout << "blob sizes : " << blob.size << '\n';
+        std::cout << "blob" << '\n' << blob;
+        layers_weights.insert(std::pair<std::string, cv::Mat>(tensor_proto.name(), blob.clone()));
+        delete[] sizes;
+    }
+    // else if () {
+    //     std::vector<double> v(10);
+    //     std::vector<float> v1(10);
+    //     double* src = v.data();
+    //     float* dst  = v1.data();
+    //     for (size_t i = 0; i < v.size(); i++) {
+    //         //*(dst + i) = *(src + i);
+    //         dst[i] = static_cast<float>(src[i]);
+    //     }
+    // }
+    else {
+        std::cout << "datatype = " << datatype << '\n';
+        CV_Error(Error::StsUnsupportedFormat, "Failed to get weights");
     }
   }
   return layers_weights;
@@ -117,16 +156,27 @@ std::map<std::string, cv::Mat> ONNXImporter::getWeights(const onnx::GraphProto& 
 LayerParams ONNXImporter::getLayerParams(const onnx::NodeProto& node_proto)
 {
   LayerParams lp;
+    //    std::cout << "node_proto.attribute_size() = " << node_proto.attribute_size() << '\n';
   for(int i = 0; i < node_proto.attribute_size(); i++) {
     const onnx::AttributeProto& attribute_proto = node_proto.attribute(i);
     std::string attribute_name = attribute_proto.name();
     if (attribute_proto.has_i()) {
+        if(attribute_name == "size" && node_proto.op_type() == "LRN") {   // remove and add to populateNet
+             lp.set("local_size", attribute_proto.i());
+        } else
       lp.set(attribute_proto.name(), attribute_proto.i());
     } else if (attribute_proto.has_f()) {
       // if(attribute_name == "ratio") lp.set("dropout_ratio", attribute_proto.f());
       lp.set(attribute_proto.name(), attribute_proto.f());
     } else if (attribute_proto.has_s()) {
       lp.set(attribute_proto.name(), attribute_proto.s());
+    }
+    else if (attribute_proto.has_t()) {
+      std::cout << "I have tensor with sizes " << attribute_proto.t().dims_size()<< '\n';
+     // lp.set(attribute_proto.name(), attribute_proto.t());
+    } else if (attribute_proto.has_g()) {
+       std::cout << "I have graph" << '\n';
+      //lp.set(attribute_proto.name(), attribute_proto.g());
     }
     for (int i = 0; i < attribute_proto.floats_size(); i++) {
       lp.set(attribute_proto.name(), attribute_proto.floats(i));
@@ -147,9 +197,18 @@ LayerParams ONNXImporter::getLayerParams(const onnx::NodeProto& node_proto)
     for (int i = 0; i < attribute_proto.strings_size(); i++) {
       lp.set(attribute_proto.name(), attribute_proto.strings(i));
     }
+    for (int i = 0; i < attribute_proto.tensors_size(); i++) {
+      std::cout << "I have " << attribute_proto.tensors_size() << " tensors" << '\n';
+      std::cout << "Tensor with sizes " << attribute_proto.tensors(i).dims_size()<< '\n';
+     // lp.set(attribute_proto.name(), attribute_proto.tensors(i));
+    }
+    for (int i = 0; i < attribute_proto.graphs_size(); i++) {
+        std::cout << "I have " << attribute_proto.graphs_size() << " graphs" << '\n';
+     // lp.set(attribute_proto.name(), attribute_proto.graphs(i));
+    }
   }
   std::string layer_type = node_proto.op_type();
-  if (layer_type == "MaxPool") {
+  if (layer_type == "MaxPool") {  // remove and add to populateNet
     lp.type = "Pooling";
     lp.set("pool", "MAX");
   } else if (layer_type == "Gemm") {
@@ -178,21 +237,27 @@ void ONNXImporter::populateNet(Net dstNet)
         layerParams = getLayerParams(node_proto);
         // if (node_proto.has_name()) {
         //     layerParams.name = node_proto.name();
+        //     std::cout << "node name = "<< node_proto.name() << '\n';
         // } else {
             std::ostringstream s;
             s << i;
             std::string id(s.str());
             layerParams.name = node_proto.op_type() + "_" + id;
-    //    }
-        // std::cout << "__layer params__" << '\n' << layerParams << '\n';
+     //  }
         std::cout << layerParams.name << '\n';
+    //    std::cout << "__layer params__" << '\n' << layerParams << '\n';
         int numLayerWeights = 0;
         for (int i = 0; i < node_proto.input_size(); i++) {
+            std::cout << "input " << node_proto.input(i) <<  '\n';
             weight = weights.find(node_proto.input(i));
-            if (weight != weights.end()) {
+            if (layerParams.type == "Reshape" && weight != weights.end()) {
+                std::cout << "I found weigths with sizes : " << weight->second.size << '\n';
+                layerParams.set("dim", DictValue::arrayInt<int*>(weight->second.ptr<int>(),  weight->second.total() ));
+            } else if (weight != weights.end()) {
               layerParams.blobs.push_back(weight->second);
               numLayerWeights++;
             }
+
         }
         if (numLayerWeights == 2) {
             layerParams.set("bias_term", true);
@@ -201,6 +266,7 @@ void ONNXImporter::populateNet(Net dstNet)
         } else {
             layerParams.set("bias_term", false);
         }
+        std::cout << '\n';
         dstNet.addLayerToPrev(layerParams.name, layerParams.type, layerParams);
     }
   }
