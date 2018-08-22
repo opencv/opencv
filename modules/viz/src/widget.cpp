f5510fc4c53c5e9430e9610564c4af3656eb0f88
Alexander Alekhin <alexander.alekhin@intel.com>
2018-08-22 15:39:14 +0300

    viz: call "mapper->Update()" before and after SetInputData()

--- modules/viz/src/widget.cpp
+++ modules/viz/src/widget.cpp
@@ -301,6 +301,7 @@ void cv::viz::Widget3D::applyTransform(const Affine3d &transform)
     vtkSmartPointer<vtkPolyDataMapper> mapper = vtkPolyDataMapper::SafeDownCast(actor->GetMapper());
     CV_Assert("Widget doesn't have a polydata mapper" && mapper);
 
+    mapper->Update(); // #10945
     VtkUtils::SetInputData(mapper, VtkUtils::TransformPolydata(mapper->GetInput(), transform));
     mapper->Update();
 }
