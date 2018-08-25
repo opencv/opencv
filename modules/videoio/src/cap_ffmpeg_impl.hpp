b330c4fab7d5f970f19f8d1aad40f859b37dedae
shawndfernandes <shawndfernandes@yahoo.com>
2018-08-25 15:39:00 -0500

    Formatted to spec, changed RGB48 to BGR48

--- modules/videoio/src/cap_ffmpeg_impl.hpp
+++ modules/videoio/src/cap_ffmpeg_impl.hpp
@@ -2075,7 +2075,7 @@ bool CvVideoWriter_FFMPEG::open( const char * filename, int fourcc,
                         input_pix_fmt == AV_PIX_FMT_GRAY16BE ? input_pix_fmt : AV_PIX_FMT_YUV420P;
         break;
     case CV_CODEC(CODEC_ID_FFV1):
-	codec_pix_fmt = AV_PIX_FMT_RGB48LE;//enable 48Bit RGB for FFV1 lossless
+        codec_pix_fmt = AV_PIX_FMT_BGR48LE;//enable 48Bit RGB for FFV1 lossless
         break;
     default:
         // good for lossy formats, MPEG, etc.
