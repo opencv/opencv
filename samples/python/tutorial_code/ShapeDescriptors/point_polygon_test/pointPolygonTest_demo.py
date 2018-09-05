f6720d8698927a5d0e925fc17a68792d7dd3b6bd
jsxyhelu <jsxyhelu@foxmail.com>
2018-09-05 20:24:01 +0800

    Update pointPolygonTest_demo.py

--- samples/python/tutorial_code/ShapeDescriptors/point_polygon_test/pointPolygonTest_demo.py
+++ samples/python/tutorial_code/ShapeDescriptors/point_polygon_test/pointPolygonTest_demo.py
@@ -45,6 +45,7 @@ for i in range(src.shape[0]):
             drawing[i,j,0] = 255
             drawing[i,j,1] = 255
             drawing[i,j,2] = 255
+
 cv.circle(drawing,maxDistPt, np.int(maxVal),(255,255,255), 1, cv.LINE_8, 0)
 cv.imshow('Source', src)
 cv.imshow('Distance and inscribed circle', drawing)
