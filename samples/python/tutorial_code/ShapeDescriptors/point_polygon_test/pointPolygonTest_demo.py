842407fce2d9b9338d00517e08712b7929fbe11f
jsxyhelu <jsxyhelu@foxmail.com>
2018-09-05 20:13:59 +0800

    Update pointPolygonTest_demo.py

--- samples/python/tutorial_code/ShapeDescriptors/point_polygon_test/pointPolygonTest_demo.py
+++ samples/python/tutorial_code/ShapeDescriptors/point_polygon_test/pointPolygonTest_demo.py
@@ -6,6 +6,7 @@ import numpy as np
 # Create an image
 r = 100
 src = np.zeros((4*r, 4*r), dtype=np.uint8)
+
 # Create a sequence of points to make a contour
 vert = [None]*6
 vert[0] = (3*r//2, int(1.34*r))
@@ -14,11 +15,14 @@ vert[2] = (3*r//2, int(2.866*r))
 vert[3] = (5*r//2, int(2.866*r))
 vert[4] = (3*r, 2*r)
 vert[5] = (5*r//2, int(1.34*r))
+
 # Draw it in src
 for i in range(6):
     cv.line(src, vert[i],  vert[(i+1)%6], ( 255 ), 3)
+    
 # Get the contours
 _, contours, _ = cv.findContours(src, cv.RETR_TREE, cv.CHAIN_APPROX_SIMPLE)
+
 # Calculate the distances to the contour
 raw_dist = np.empty(src.shape, dtype=np.float32)
 for i in range(src.shape[0]):
@@ -27,6 +31,7 @@ for i in range(src.shape[0]):
 minVal, maxVal, _, maxDistPt = cv.minMaxLoc(raw_dist)
 minVal = abs(minVal)
 maxVal = abs(maxVal)
+
 # Depicting the  distances graphically
 drawing = np.zeros((src.shape[0], src.shape[1], 3), dtype=np.uint8)
 for i in range(src.shape[0]):
