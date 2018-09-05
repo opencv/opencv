ef2c344cb35fee2566c6aa6af272a1115f655972
jsxyhelu <jsxyhelu@foxmail.com>
2018-09-05 20:28:41 +0800

    Update PointPolygonTestDemo.java

--- samples/java/tutorial_code/ShapeDescriptors/point_polygon_test/PointPolygonTestDemo.java
+++ samples/java/tutorial_code/ShapeDescriptors/point_polygon_test/PointPolygonTestDemo.java
@@ -13,8 +13,10 @@ import org.opencv.highgui.HighGui;
 import org.opencv.imgproc.Imgproc;
 class PointPolygonTest {
     public void run() {
+        /// Create an image
         int r = 100;
         Mat src = Mat.zeros(new Size(4 * r, 4 * r), CvType.CV_8U);
+        /// Create a sequence of points to make a contour
         List<Point> vert = new ArrayList<>(6);
         vert.add(new Point(3 * r / 2, 1.34 * r));
         vert.add(new Point(1 * r, 2 * r));
@@ -22,12 +24,15 @@ class PointPolygonTest {
         vert.add(new Point(5 * r / 2, 2.866 * r));
         vert.add(new Point(3 * r, 2 * r));
         vert.add(new Point(5 * r / 2, 1.34 * r));
+        /// Draw it in src
         for (int i = 0; i < 6; i++) {
             Imgproc.line(src, vert.get(i), vert.get((i + 1) % 6), new Scalar(255), 3);
         }
+        /// Get the contours
         List<MatOfPoint> contours = new ArrayList<>();
         Mat hierarchy = new Mat();
         Imgproc.findContours(src, contours, hierarchy, Imgproc.RETR_TREE, Imgproc.CHAIN_APPROX_SIMPLE);
+        /// Calculate the distances to the contour
         Mat rawDist = new Mat(src.size(), CvType.CV_32F);
         float[] rawDistData = new float[(int) (rawDist.total() * rawDist.channels())];
         for (int i = 0; i < src.rows(); i++) {
@@ -40,6 +45,7 @@ class PointPolygonTest {
         MinMaxLocResult res = Core.minMaxLoc(rawDist);
         double minVal = Math.abs(res.minVal);
         double maxVal = Math.abs(res.maxVal);
+        /// Depicting the distances graphically
         Mat drawing = Mat.zeros(src.size(), CvType.CV_8UC3);
         byte[] drawingData = new byte[(int) (drawing.total() * drawing.channels())];
         for (int i = 0; i < src.rows(); i++) {
