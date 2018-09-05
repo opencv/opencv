541aaed77d5fa77073035c9c578b05027fad6e86
jsxyhelu <jsxyhelu@foxmail.com>
2018-09-05 20:29:39 +0800

    Update PointPolygonTestDemo.java

--- samples/java/tutorial_code/ShapeDescriptors/point_polygon_test/PointPolygonTestDemo.java
+++ samples/java/tutorial_code/ShapeDescriptors/point_polygon_test/PointPolygonTestDemo.java
@@ -17,6 +17,7 @@ class PointPolygonTest {
         /// Create an image
         int r = 100;
         Mat src = Mat.zeros(new Size(4 * r, 4 * r), CvType.CV_8U);
+
         /// Create a sequence of points to make a contour
         List<Point> vert = new ArrayList<>(6);
         vert.add(new Point(3 * r / 2, 1.34 * r));
@@ -25,10 +26,12 @@ class PointPolygonTest {
         vert.add(new Point(5 * r / 2, 2.866 * r));
         vert.add(new Point(3 * r, 2 * r));
         vert.add(new Point(5 * r / 2, 1.34 * r));
+
         /// Draw it in src
         for (int i = 0; i < 6; i++) {
             Imgproc.line(src, vert.get(i), vert.get((i + 1) % 6), new Scalar(255), 3);
         }
+
         /// Get the contours
         List<MatOfPoint> contours = new ArrayList<>();
         Mat hierarchy = new Mat();
@@ -46,6 +49,7 @@ class PointPolygonTest {
         MinMaxLocResult res = Core.minMaxLoc(rawDist);
         double minVal = Math.abs(res.minVal);
         double maxVal = Math.abs(res.maxVal);
+
         /// Depicting the distances graphically
         Mat drawing = Mat.zeros(src.size(), CvType.CV_8UC3);
         byte[] drawingData = new byte[(int) (drawing.total() * drawing.channels())];
