40c3378f037ca58af026dff43c10a4bd239b51e6
jsxyhelu <jsxyhelu@foxmail.com>
2018-09-09 21:20:08 +0800

    Update PointPolygonTestDemo.java

--- samples/java/tutorial_code/ShapeDescriptors/point_polygon_test/PointPolygonTestDemo.java
+++ samples/java/tutorial_code/ShapeDescriptors/point_polygon_test/PointPolygonTestDemo.java
@@ -12,11 +12,13 @@ import org.opencv.core.Scalar;
 import org.opencv.core.Size;
 import org.opencv.highgui.HighGui;
 import org.opencv.imgproc.Imgproc;
+
 class PointPolygonTest {
     public void run() {
         /// Create an image
         int r = 100;
         Mat src = Mat.zeros(new Size(4 * r, 4 * r), CvType.CV_8U);
+
         /// Create a sequence of points to make a contour
         List<Point> vert = new ArrayList<>(6);
         vert.add(new Point(3 * r / 2, 1.34 * r));
@@ -25,14 +27,17 @@ class PointPolygonTest {
         vert.add(new Point(5 * r / 2, 2.866 * r));
         vert.add(new Point(3 * r, 2 * r));
         vert.add(new Point(5 * r / 2, 1.34 * r));
+
         /// Draw it in src
         for (int i = 0; i < 6; i++) {
             Imgproc.line(src, vert.get(i), vert.get((i + 1) % 6), new Scalar(255), 3);
         }
+
         /// Get the contours
         List<MatOfPoint> contours = new ArrayList<>();
         Mat hierarchy = new Mat();
         Imgproc.findContours(src, contours, hierarchy, Imgproc.RETR_TREE, Imgproc.CHAIN_APPROX_SIMPLE);
+
         /// Calculate the distances to the contour
         Mat rawDist = new Mat(src.size(), CvType.CV_32F);
         float[] rawDistData = new float[(int) (rawDist.total() * rawDist.channels())];
@@ -46,6 +51,7 @@ class PointPolygonTest {
         MinMaxLocResult res = Core.minMaxLoc(rawDist);
         double minVal = Math.abs(res.minVal);
         double maxVal = Math.abs(res.maxVal);
+
         /// Depicting the distances graphically
         Mat drawing = Mat.zeros(src.size(), CvType.CV_8UC3);
         byte[] drawingData = new byte[(int) (drawing.total() * drawing.channels())];
@@ -66,6 +72,7 @@ class PointPolygonTest {
         }
         drawing.put(0, 0, drawingData);
         Imgproc.circle(drawing, res.maxLoc, (int)res.maxVal, new Scalar(255, 255, 255), 2, 8, 0);
+
         /// Show your results
         HighGui.imshow("Source", src);
         HighGui.imshow("Distance and inscribed circle", drawing);
