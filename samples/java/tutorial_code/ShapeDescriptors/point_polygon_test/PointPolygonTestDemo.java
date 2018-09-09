b760b9c946c9f9fe48856921e290b106a852af04
jsxyhelu <jsxyhelu@foxmail.com>
2018-09-09 20:59:00 +0800

    Update PointPolygonTestDemo.java

--- samples/java/tutorial_code/ShapeDescriptors/point_polygon_test/PointPolygonTestDemo.java
+++ samples/java/tutorial_code/ShapeDescriptors/point_polygon_test/PointPolygonTestDemo.java
@@ -17,7 +17,6 @@ class PointPolygonTest {
         /// Create an image
         int r = 100;
         Mat src = Mat.zeros(new Size(4 * r, 4 * r), CvType.CV_8U);
-
         /// Create a sequence of points to make a contour
         List<Point> vert = new ArrayList<>(6);
         vert.add(new Point(3 * r / 2, 1.34 * r));
@@ -26,12 +25,10 @@ class PointPolygonTest {
         vert.add(new Point(5 * r / 2, 2.866 * r));
         vert.add(new Point(3 * r, 2 * r));
         vert.add(new Point(5 * r / 2, 1.34 * r));
-
         /// Draw it in src
         for (int i = 0; i < 6; i++) {
             Imgproc.line(src, vert.get(i), vert.get((i + 1) % 6), new Scalar(255), 3);
         }
-
         /// Get the contours
         List<MatOfPoint> contours = new ArrayList<>();
         Mat hierarchy = new Mat();
@@ -49,7 +46,6 @@ class PointPolygonTest {
         MinMaxLocResult res = Core.minMaxLoc(rawDist);
         double minVal = Math.abs(res.minVal);
         double maxVal = Math.abs(res.maxVal);
-
         /// Depicting the distances graphically
         Mat drawing = Mat.zeros(src.size(), CvType.CV_8UC3);
         byte[] drawingData = new byte[(int) (drawing.total() * drawing.channels())];
@@ -70,10 +66,9 @@ class PointPolygonTest {
         }
         drawing.put(0, 0, drawingData);
         Imgproc.circle(drawing, res.maxLoc, (int)res.maxVal, new Scalar(255, 255, 255), 2, 8, 0);
-
         /// Show your results
         HighGui.imshow("Source", src);
-        HighGui.imshow("Distance", drawing);
+        HighGui.imshow("Distance and inscribed circle", drawing);
 
         HighGui.waitKey();
         System.exit(0);
