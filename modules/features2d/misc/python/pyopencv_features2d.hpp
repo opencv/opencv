8c776b149df5e27397c11a004ea58beb08587ae9
Hamdi Sahloul <hamdisahloul@hotmail.com>
2018-09-20 06:34:01 +0900

    Rename unnamed enum to FastFeatureDetector.DetectorType

--- modules/features2d/misc/python/pyopencv_features2d.hpp
+++ modules/features2d/misc/python/pyopencv_features2d.hpp
@@ -2,6 +2,7 @@
 typedef SimpleBlobDetector::Params SimpleBlobDetector_Params;
 typedef AKAZE::DescriptorType AKAZE_DescriptorType;
 typedef AgastFeatureDetector::DetectorType AgastFeatureDetector_DetectorType;
+typedef FastFeatureDetector::DetectorType FastFeatureDetector_DetectorType;
 
 CV_PY_FROM_ENUM(AKAZE::DescriptorType);
 CV_PY_TO_ENUM(AKAZE::DescriptorType);
@@ -9,4 +10,6 @@ CV_PY_FROM_ENUM(AgastFeatureDetector::DetectorType);
 CV_PY_TO_ENUM(AgastFeatureDetector::DetectorType);
 CV_PY_FROM_ENUM(DrawMatchesFlags);
 CV_PY_TO_ENUM(DrawMatchesFlags);
+CV_PY_FROM_ENUM(FastFeatureDetector::DetectorType);
+CV_PY_TO_ENUM(FastFeatureDetector::DetectorType);
 #endif
