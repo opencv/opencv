e104fcc00003a1ac4c2f936e65ddfa4e28f4ef64
Hamdi Sahloul <hamdisahloul@hotmail.com>
2018-09-28 15:39:51 +0900

    Generate enum bindings automatically

--- modules/objdetect/misc/python/pyopencv_objdetect.hpp
+++ modules/objdetect/misc/python/pyopencv_objdetect.hpp
@@ -5,9 +5,4 @@
 typedef HOGDescriptor::HistogramNormType HOGDescriptor_HistogramNormType;
 typedef HOGDescriptor::DescriptorStorageFormat HOGDescriptor_DescriptorStorageFormat;
 
-CV_PY_FROM_ENUM(HOGDescriptor::HistogramNormType);
-CV_PY_TO_ENUM(HOGDescriptor::HistogramNormType);
-CV_PY_FROM_ENUM(HOGDescriptor::DescriptorStorageFormat);
-CV_PY_TO_ENUM(HOGDescriptor::DescriptorStorageFormat);
-
 #endif
