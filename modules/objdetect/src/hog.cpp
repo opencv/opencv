ee0c985491fe1d7af2d40eb626638aae67f46e64
Suleyman TURKMEN <sturkmen@hotmail.com>
2018-10-01 22:22:37 +0300

    Merge pull request #12649 from sturkmen72:patch-9
    Update hog.cpp (#12649)

--- modules/objdetect/src/hog.cpp
+++ modules/objdetect/src/hog.cpp
@@ -138,6 +138,8 @@ void HOGDescriptor::setSVMDetector(InputArray _svmDetector)
 
 bool HOGDescriptor::read(FileNode& obj)
 {
+    CV_Assert(!obj["winSize"].empty());
+
     if( !obj.isMap() )
         return false;
     FileNodeIterator it = obj["winSize"].begin();
