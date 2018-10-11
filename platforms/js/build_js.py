3e8b892bb5a1c215aa9991243748b1c1a45fc1b5
Alexander Alekhin <alexander.a.alekhin@gmail.com>
2018-10-11 00:49:45 +0000

    js: enable calib3d code (no bindings)

--- platforms/js/build_js.py
+++ platforms/js/build_js.py
@@ -112,7 +112,7 @@ class Builder:
                "-DWITH_ITT=OFF",
                "-DBUILD_ZLIB=ON",
                "-DBUILD_opencv_apps=OFF",
-               "-DBUILD_opencv_calib3d=OFF",
+               "-DBUILD_opencv_calib3d=ON",  # No bindings provided. This module is used as a dependency for other modules.
                "-DBUILD_opencv_dnn=ON",
                "-DBUILD_opencv_features2d=ON",
                "-DBUILD_opencv_flann=OFF",
