4c0f79824bdbdf96d413890a09e1eb3ba78f573d
Dmitry Budnikov <Dmitry.Budnikov@intel.com>
2018-11-01 12:24:46 +0300

    missing license headers

--- modules/gapi/include/opencv2/gapi/gpu/core.hpp
+++ modules/gapi/include/opencv2/gapi/gpu/core.hpp
@@ -1,3 +1,10 @@
+// This file is part of OpenCV project.
+// It is subject to the license terms in the LICENSE file found in the top-level directory
+// of this distribution and at http://opencv.org/license.html.
+//
+// Copyright (C) 2018 Intel Corporation
+
+
 #ifndef OPENCV_GAPI_GPU_CORE_API_HPP
 #define OPENCV_GAPI_GPU_CORE_API_HPP
 
