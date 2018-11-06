255b9344d50c7a6209939d2dac237a853e609ebc
lqy123000 <luoqiaoyu@sina.cn>
2018-11-07 00:13:48 +0800

    Merge pull request #12916 from lqy123000:bugfix_templmatch
    * avoid rounding errors
    
    * imgproc: replace condition in matchTemplate

--- modules/imgproc/src/templmatch.cpp
+++ modules/imgproc/src/templmatch.cpp
@@ -947,7 +947,12 @@ static void common_matchTemplate( Mat& img, Mat& templ, Mat& result, int method,
 
             if( isNormed )
             {
-                t = std::sqrt(MAX(wndSum2 - wndMean2,0))*templNorm;
+                double diff2 = MAX(wndSum2 - wndMean2, 0);
+                if (diff2 <= std::min(0.5, 10 * FLT_EPSILON * wndSum2))
+                    t = 0; // avoid rounding errors
+                else
+                    t = std::sqrt(diff2)*templNorm;
+
                 if( fabs(num) < t )
                     num /= t;
                 else if( fabs(num) < t*1.125 )
