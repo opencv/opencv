275379c83c60566e1eb46cf17f5b18f653bad2af
Evgeny Latkin <evgeny.latkin@intel.com>
2018-11-15 18:14:27 +0300

    Merge pull request #13162 from elatkin:el/gapi_perf_rgb2gray
    GAPI (fluid): RGB/BGR to gray: optimization (#13162)
    
    * GAPI (fluid): RGB/BGR to Gray: add performance tests
    
    * GAPI (fluid): RGB/BGR to Gray: speedup 8-12x with manual CV_SIMD
    
    * GAPI (fluid): RGB/BGR to Gray: fix compiler warning
    
    * GAPI (fluid): RGB/BGR to Gray: dynamic dispatching to AVX2
    
    * GAPI (fluid): RGB/BGR to Gray: check R/G/B coefficients
    
    * GAPI (fluid): RGB/BGR to Gray: fixed compilation error (caused by change in master)

--- modules/gapi/perf/cpu/gapi_imgproc_perf_tests_fluid.cpp
+++ modules/gapi/perf/cpu/gapi_imgproc_perf_tests_fluid.cpp
@@ -33,4 +33,14 @@ namespace opencv_test
             Values(1, 2),
             Values(cv::compile_args(IMGPROC_FLUID))));
 
+    INSTANTIATE_TEST_CASE_P(RGB2GrayPerfTestFluid, RGB2GrayPerfTest,
+        Combine(Values(ToleranceColor(1e-3).to_compare_f()),
+                Values(szVGA, sz720p, sz1080p),
+                Values(cv::compile_args(IMGPROC_FLUID))));
+
+    INSTANTIATE_TEST_CASE_P(BGR2GrayPerfTestFluid, BGR2GrayPerfTest,
+        Combine(Values(ToleranceColor(1e-3).to_compare_f()),
+                Values(szVGA, sz720p, sz1080p),
+                Values(cv::compile_args(IMGPROC_FLUID))));
+
 }
