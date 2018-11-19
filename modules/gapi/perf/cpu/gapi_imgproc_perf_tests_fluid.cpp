6757c2c5a67deb11d7ff36e8aa2571f337be1abf
Evgeny Latkin <evgeny.latkin@intel.com>
2018-11-19 17:30:14 +0300

    Merge pull request #13174 from elatkin:el/gapi_perf_rgb2yuv
    GAPI (fluid): RGB to YUV optimization (#13174)
    
    * GAPI (fluid): RGB to YUV: activate performance tests
    
    * GAPI (fluid): speedup 4-8x RGB-to-YUV, 2.5x YUV to RGB with int16 arithmetic
    
    * GAPI (fluid): RGB <--> YUV: fixed compiler warning
    
    * GAPI (fluid): RGB <--> YUV: additional speedup 2-3x times (10-15x over original) via manual CV_SIMD
    
    * GAPI (fluid): RGB <--> YUV: dynamic CV_SIMD dispatching

--- modules/gapi/perf/cpu/gapi_imgproc_perf_tests_fluid.cpp
+++ modules/gapi/perf/cpu/gapi_imgproc_perf_tests_fluid.cpp
@@ -43,4 +43,24 @@ namespace opencv_test
                 Values(szVGA, sz720p, sz1080p),
                 Values(cv::compile_args(IMGPROC_FLUID))));
 
+    INSTANTIATE_TEST_CASE_P(RGB2YUVPerfTestFluid, RGB2YUVPerfTest,
+        Combine(Values(ToleranceColor(1e-3).to_compare_f()),
+                Values(szVGA, sz720p, sz1080p),
+                Values(cv::compile_args(IMGPROC_FLUID))));
+
+    INSTANTIATE_TEST_CASE_P(YUV2RGBPerfTestFluid, YUV2RGBPerfTest,
+        Combine(Values(ToleranceColor(1e-3).to_compare_f()),
+                Values(szVGA, sz720p, sz1080p),
+                Values(cv::compile_args(IMGPROC_FLUID))));
+
+    INSTANTIATE_TEST_CASE_P(BGR2YUVPerfTestFluid, BGR2YUVPerfTest,
+        Combine(Values(ToleranceColor(1e-3).to_compare_f()),
+                Values(szVGA, sz720p, sz1080p),
+                Values(cv::compile_args(IMGPROC_FLUID))));
+
+    INSTANTIATE_TEST_CASE_P(YUV2BGRPerfTestFluid, YUV2BGRPerfTest,
+        Combine(Values(ToleranceColor(1e-3).to_compare_f()),
+                Values(szVGA, sz720p, sz1080p),
+                Values(cv::compile_args(IMGPROC_FLUID))));
+
 }
