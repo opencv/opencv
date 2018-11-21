4a583a80067b41dac6bedcd6918a68f4f68ac1b6
Dmitry Budnikov <Dmitry.Budnikov@intel.com>
2018-11-21 17:09:41 +0300

    remove kernel generation, add kernel string

---
+++ modules/gapi/test/opencl_kernels_test_gapi.hpp
@@ -0,0 +1,19 @@
+// This file is auto-generated. Do not edit!
+
+#include "opencv2/core/ocl.hpp"
+#include "opencv2/core/ocl_genbase.hpp"
+#include "opencv2/core/opencl/ocl_defs.hpp"
+
+#ifdef HAVE_OPENCL
+
+namespace cv
+{
+namespace ocl
+{
+namespace gapi
+{
+
+extern struct cv::ocl::internal::ProgramEntry symm7x7_test_oclsrc;
+
+}}}
+#endif
