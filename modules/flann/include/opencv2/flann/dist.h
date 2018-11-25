243a43262b65963954a413dc256aca43e1d94650
1over <ua.over@gmail.com>
2018-11-25 01:31:54 +0100

    fixed memory issue in flann

--- modules/flann/include/opencv2/flann/dist.h
+++ modules/flann/include/opencv2/flann/dist.h
@@ -462,10 +462,9 @@ struct Hamming
             }
         }
 #else // NO NEON and NOT GNUC
-        typedef unsigned long long pop_t;
         HammingLUT lut;
         result = lut(reinterpret_cast<const unsigned char*> (a),
-                     reinterpret_cast<const unsigned char*> (b), size * sizeof(pop_t));
+                     reinterpret_cast<const unsigned char*> (b), size);
 #endif
         return result;
     }
