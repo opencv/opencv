ef42baf9f07251ee4325d3eec94147a94d722935
okriof <okriof@users.noreply.github.com>
2018-12-05 16:44:23 +0100

    Merge pull request #13361 from okriof:brisk_getset
    * Get/Set functions for BRISK parameters, issue #11527.
    
    Allows setting threshold and octaves parameters after creating a brisk object. These parameters do not affect the initial pattern initialization and can be changed later without re-initialization.
    
    * Fix doc parameter name.
    
    * Brisk get/set functions tests. Check for correct value and make tests independent of default parameter values.
    
    * Add dummy implementations for BRISK get/set functions not to break API in case someone has overloaded the Feature2d::BRISK interface. This makes BRISK different from the other detectors/descriptors on the other hand, where get/set functions are pure virtual in the interface.

--- modules/features2d/include/opencv2/features2d.hpp
+++ modules/features2d/include/opencv2/features2d.hpp
@@ -285,6 +285,18 @@ public:
         const std::vector<int> &numberList, float dMax=5.85f, float dMin=8.2f,
         const std::vector<int>& indexChange=std::vector<int>());
     CV_WRAP virtual String getDefaultName() const CV_OVERRIDE;
+
+    /** @brief Set detection threshold.
+    @param threshold AGAST detection threshold score.
+    */
+    CV_WRAP virtual void setThreshold(int threshold) { CV_UNUSED(threshold); return; };
+    CV_WRAP virtual int getThreshold() const { return -1; };
+
+    /** @brief Set detection octaves.
+    @param octaves detection octaves. Use 0 to do single scale.
+    */
+    CV_WRAP virtual void setOctaves(int octaves) { CV_UNUSED(octaves); return; };
+    CV_WRAP virtual int getOctaves() const { return -1; };
 };
 
 /** @brief Class implementing the ORB (*oriented BRIEF*) keypoint detector and descriptor extractor
