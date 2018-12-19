b01787660cb11ad852708a794dbd4ccfae26bdf2
Dmitry Kurtaev <dmitry.kurtaev+github@gmail.com>
2018-12-19 23:37:55 +0300

    Merge pull request #13482 from dkurt:fix_python_bindings_without_stitching
    * Fix python bindings without stitching module
    
    * stitching: move stitching specific code into modules/stitching/misc

--- modules/core/misc/python/pyopencv_rotationwarper.hpp
+++
@@ -1,8 +0,0 @@
-#ifdef HAVE_OPENCV_STITCHING
-
-typedef std::vector<detail::ImageFeatures> vector_ImageFeatures;
-typedef std::vector<detail::MatchesInfo> vector_MatchesInfo;
-typedef std::vector<detail::CameraParams> vector_CameraParams;
-
-
-#endif
