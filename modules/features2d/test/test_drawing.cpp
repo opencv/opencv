595f5e8307233e03bda11e1150bdcb1d5d08395b
Alexander Alekhin <alexander.alekhin@intel.com>
2018-12-21 16:11:42 +0300

    keep BGRA too

--- modules/features2d/test/test_drawing.cpp
+++ modules/features2d/test/test_drawing.cpp
@@ -8,54 +8,63 @@
 
 namespace opencv_test { namespace {
 
-Mat ref = (Mat_<uint8_t>(11, 11) << 1,   1,   1,   1,   1,   1,   1,   1,   1,   1,   1,
-                                    1,   1,   1,   1,  15,  54,  15,   1,   1,   1,   1,
-                                    1,   1,   1,  76, 217, 217, 221,  81,   1,   1,   1,
-                                    1,   1, 100, 224, 111,  57, 115, 225, 101,   1,   1,
-                                    1,  44, 215, 100,   1,   1,   1, 101, 214,  44,   1,
-                                    1,  54, 212,  57,   1,   1,   1,  55, 212,  55,   1,
-                                    1,  40, 215, 104,   1,   1,   1, 105, 215,  40,   1,
-                                    1,   1, 102, 221, 111,  55, 115, 222, 103,   1,   1,
-                                    1,   1,   1,  76, 218, 217, 220,  81,   1,   1,   1,
-                                    1,   1,   1,   1,  15,  55,  15,   1,   1,   1,   1,
-                                    1,   1,   1,   1,   1,   1,   1,   1,   1,   1,   1);
+static
+Mat getReference_DrawKeypoint(int cn)
+{
+    static Mat ref = (Mat_<uint8_t>(11, 11) <<
+        1,   1,   1,   1,   1,   1,   1,   1,   1,   1,   1,
+        1,   1,   1,   1,  15,  54,  15,   1,   1,   1,   1,
+        1,   1,   1,  76, 217, 217, 221,  81,   1,   1,   1,
+        1,   1, 100, 224, 111,  57, 115, 225, 101,   1,   1,
+        1,  44, 215, 100,   1,   1,   1, 101, 214,  44,   1,
+        1,  54, 212,  57,   1,   1,   1,  55, 212,  55,   1,
+        1,  40, 215, 104,   1,   1,   1, 105, 215,  40,   1,
+        1,   1, 102, 221, 111,  55, 115, 222, 103,   1,   1,
+        1,   1,   1,  76, 218, 217, 220,  81,   1,   1,   1,
+        1,   1,   1,   1,  15,  55,  15,   1,   1,   1,   1,
+        1,   1,   1,   1,   1,   1,   1,   1,   1,   1,   1);
+    Mat res;
+    cvtColor(ref, res, (cn == 4) ? COLOR_GRAY2BGRA : COLOR_GRAY2BGR);
+    return res;
+}
 
-typedef testing::TestWithParam<int> Features2D;
+typedef testing::TestWithParam<MatType> Features2D;
 TEST_P(Features2D, drawKeypoints)
 {
-    Mat inpImg(11, 11, GetParam(), Scalar::all(1)), outImg;
+    const int cn = CV_MAT_CN(GetParam());
+    Mat inpImg(11, 11, GetParam(), Scalar(1, 1, 1, 255)), outImg;
 
     std::vector<KeyPoint> keypoints(1, KeyPoint(5, 5, 1));
     drawKeypoints(inpImg, keypoints, outImg, Scalar::all(255));
-    ASSERT_EQ(outImg.channels(), 3);
+    ASSERT_EQ(outImg.channels(), (cn == 4) ? 4 : 3);
 
-    Mat ref3;
-    cvtColor(ref, ref3, COLOR_GRAY2BGR);
-    ASSERT_EQ(countNonZero(outImg != ref3), 0);
+    Mat ref_ = getReference_DrawKeypoint(cn);
+    EXPECT_EQ(0, cv::norm(outImg, ref_, NORM_INF));
 }
 
 TEST_P(Features2D, drawMatches)
 {
-    Mat inpImg1(11, 11, GetParam(), Scalar::all(1));
-    Mat inpImg2(11, 11, GetParam(), Scalar::all(2)), outImg2, outImg;
+    const int cn = CV_MAT_CN(GetParam());
+    Mat inpImg1(11, 11, GetParam(), Scalar(1, 1, 1, 255));
+    Mat inpImg2(11, 11, GetParam(), Scalar(2, 2, 2, 255)), outImg2, outImg;
 
     std::vector<KeyPoint> keypoints(1, KeyPoint(5, 5, 1));
 
     // Get outImg2 using drawKeypoints assuming that it works correctly (see the test above).
     drawKeypoints(inpImg2, keypoints, outImg2, Scalar::all(255));
-    ASSERT_EQ(outImg2.channels(), 3);
+    ASSERT_EQ(outImg2.channels(), (cn == 4) ? 4 : 3);
 
     // Merge both references.
-    Mat ref3, concattedRef;
-    cvtColor(ref, ref3, COLOR_GRAY2BGR);
-    hconcat(ref3, outImg2, concattedRef);
+    Mat ref_ = getReference_DrawKeypoint(cn);
+    Mat concattedRef;
+    hconcat(ref_, outImg2, concattedRef);
 
     std::vector<DMatch> matches;
     drawMatches(inpImg1, keypoints, inpImg2, keypoints, matches, outImg,
                 Scalar::all(255), Scalar::all(255));
-    ASSERT_EQ(outImg.channels(), 3);
+    ASSERT_EQ(outImg.channels(), (cn == 4) ? 4 : 3);
 
-    ASSERT_EQ(countNonZero(outImg != concattedRef), 0);
+    EXPECT_EQ(0, cv::norm(outImg, concattedRef, NORM_INF));
 }
 INSTANTIATE_TEST_CASE_P(/**/, Features2D, Values(CV_8UC1, CV_8UC3, CV_8UC4));
 
