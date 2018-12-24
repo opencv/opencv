cb2bdf5891a2bc636213f8a2a93b2c06029191aa
Dmitry Kurtaev <dmitry.kurtaev+github@gmail.com>
2018-12-24 13:59:50 +0300

    Combine different input types for drawMatches test

--- modules/features2d/test/test_drawing.cpp
+++ modules/features2d/test/test_drawing.cpp
@@ -28,8 +28,8 @@ Mat getReference_DrawKeypoint(int cn)
     return res;
 }
 
-typedef testing::TestWithParam<MatType> Features2D;
-TEST_P(Features2D, drawKeypoints)
+typedef testing::TestWithParam<MatType> Features2D_drawKeypoints;
+TEST_P(Features2D_drawKeypoints, Accuracy)
 {
     const int cn = CV_MAT_CN(GetParam());
     Mat inpImg(11, 11, GetParam(), Scalar(1, 1, 1, 255)), outImg;
@@ -41,20 +41,24 @@ TEST_P(Features2D, drawKeypoints)
     Mat ref_ = getReference_DrawKeypoint(cn);
     EXPECT_EQ(0, cv::norm(outImg, ref_, NORM_INF));
 }
+INSTANTIATE_TEST_CASE_P(/**/, Features2D_drawKeypoints, Values(CV_8UC1, CV_8UC3, CV_8UC4));
 
-TEST_P(Features2D, drawMatches)
+typedef testing::TestWithParam<tuple<MatType, MatType> > Features2D_drawMatches;
+TEST_P(Features2D_drawMatches, Accuracy)
 {
-    const int cn = CV_MAT_CN(GetParam());
-    Mat inpImg1(11, 11, GetParam(), Scalar(1, 1, 1, 255));
-    Mat inpImg2(11, 11, GetParam(), Scalar(2, 2, 2, 255)), outImg2, outImg;
+    Mat inpImg1(11, 11, get<0>(GetParam()), Scalar(1, 1, 1, 255));
+    Mat inpImg2(11, 11, get<1>(GetParam()), Scalar(2, 2, 2, 255)), outImg2, outImg;
 
     std::vector<KeyPoint> keypoints(1, KeyPoint(5, 5, 1));
 
     // Get outImg2 using drawKeypoints assuming that it works correctly (see the test above).
     drawKeypoints(inpImg2, keypoints, outImg2, Scalar::all(255));
-    ASSERT_EQ(outImg2.channels(), (cn == 4) ? 4 : 3);
+    ASSERT_EQ(outImg2.channels(), (inpImg2.channels() == 4) ? 4 : 3);
 
     // Merge both references.
+    const int cn = max(3, max(inpImg1.channels(), inpImg2.channels()));
+    if (cn == 4 && outImg2.channels() == 3)
+        cvtColor(outImg2, outImg2, COLOR_BGR2BGRA);
     Mat ref_ = getReference_DrawKeypoint(cn);
     Mat concattedRef;
     hconcat(ref_, outImg2, concattedRef);
@@ -62,10 +66,13 @@ TEST_P(Features2D, drawMatches)
     std::vector<DMatch> matches;
     drawMatches(inpImg1, keypoints, inpImg2, keypoints, matches, outImg,
                 Scalar::all(255), Scalar::all(255));
-    ASSERT_EQ(outImg.channels(), (cn == 4) ? 4 : 3);
+    ASSERT_EQ(outImg.channels(), cn);
 
     EXPECT_EQ(0, cv::norm(outImg, concattedRef, NORM_INF));
 }
-INSTANTIATE_TEST_CASE_P(/**/, Features2D, Values(CV_8UC1, CV_8UC3, CV_8UC4));
+INSTANTIATE_TEST_CASE_P(/**/, Features2D_drawMatches, Combine(
+    Values(CV_8UC1, CV_8UC3, CV_8UC4),
+    Values(CV_8UC1, CV_8UC3, CV_8UC4)
+));
 
 }} // namespace
