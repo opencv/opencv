ac6d1d498b6c631df14daf234d2300da8f25607b
Wu Zhiwen <zhiwen.wu@intel.com>
2018-12-24 11:00:45 +0800

    dnn: fixup missing vkcom/vulkan combination of backend/target in dnn test

--- modules/dnn/test/test_common.hpp
+++ modules/dnn/test/test_common.hpp
@@ -236,6 +236,12 @@ testing::internal::ParamGenerator< tuple<Backend, Target> > dnnBackendsAndTarget
         for (std::vector< Target >::const_iterator i = available.begin(); i != available.end(); ++i)
             targets.push_back(make_tuple(DNN_BACKEND_INFERENCE_ENGINE, *i));
     }
+    if (withVkCom)
+    {
+        available = getAvailableTargets(DNN_BACKEND_VKCOM);
+        for (std::vector< Target >::const_iterator i = available.begin(); i != available.end(); ++i)
+            targets.push_back(make_tuple(DNN_BACKEND_VKCOM, *i));
+    }
     {
         available = getAvailableTargets(DNN_BACKEND_OPENCV);
         for (std::vector< Target >::const_iterator i = available.begin(); i != available.end(); ++i)
