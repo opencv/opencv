9efc5f437ae9727be8b1e87f3c648b79cbb10e52
Lucas Towers <luctowers@gmail.com>
2018-12-31 05:57:38 -0800

    Fix improper defining of CV_XADD when using Intel C++

--- modules/core/include/opencv2/core/cvdef.h
+++ modules/core/include/opencv2/core/cvdef.h
@@ -463,7 +463,7 @@ Cv64suf;
 #ifdef CV_XADD
   // allow to use user-defined macro
 #elif defined __GNUC__ || defined __clang__
-#  if defined __clang__ && __clang_major__ >= 3 && !defined __ANDROID__ && !defined __EMSCRIPTEN__ && !defined(__CUDACC__)
+#  if defined __clang__ && __clang_major__ >= 3 && !defined __ANDROID__ && !defined __EMSCRIPTEN__ && !defined(__CUDACC__)  && !defined __INTEL_COMPILER
 #    ifdef __ATOMIC_ACQ_REL
 #      define CV_XADD(addr, delta) __c11_atomic_fetch_add((_Atomic(int)*)(addr), delta, __ATOMIC_ACQ_REL)
 #    else
