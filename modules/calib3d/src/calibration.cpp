121d04843ee8ce41275ec2698c8d9c5eb29abb19
x3lif <guillaume.peradon@gmail.com>
2019-02-25 18:39:06 +0100

    Fix Issue 13908
    Allocate the matrix _Jo only when the solver needs it (ie when solver.state == CvLevMarq::CALC_J)

--- modules/calib3d/src/calibration.cpp
+++ modules/calib3d/src/calibration.cpp
@@ -1520,10 +1520,6 @@ static double cvCalibrateCamera2Internal( const CvMat* objectPoints,
     nparams = NINTRINSIC + nimages*6;
     if( releaseObject )
         nparams += maxPoints * 3;
-    Mat _Ji( maxPoints*2, NINTRINSIC, CV_64FC1, Scalar(0));
-    Mat _Je( maxPoints*2, 6, CV_64FC1 );
-    Mat _Jo( maxPoints*2, maxPoints*3, CV_64FC1, Scalar(0) );
-    Mat _err( maxPoints*2, 1, CV_64FC1 );
 
     _k = cvMat( distCoeffs->rows, distCoeffs->cols, CV_MAKETYPE(CV_64F,CV_MAT_CN(distCoeffs->type)), k);
     if( distCoeffs->rows*distCoeffs->cols*CV_MAT_CN(distCoeffs->type) < 8 )
@@ -1586,6 +1582,11 @@ static double cvCalibrateCamera2Internal( const CvMat* objectPoints,
     }
 
     CvLevMarq solver( nparams, 0, termCrit );
+    
+    Mat _Ji( maxPoints*2, NINTRINSIC, CV_64FC1, Scalar(0));
+    Mat _Je( maxPoints*2, 6, CV_64FC1 );
+    Mat _Jo = (solver.state == CvLevMarq::CALC_J) ? Mat( maxPoints*2, maxPoints*3, CV_64FC1, Scalar(0) ) : Mat();
+    Mat _err( maxPoints*2, 1, CV_64FC1 );
 
     if(flags & CALIB_USE_LU) {
         solver.solveMethod = DECOMP_LU;
@@ -1725,7 +1726,7 @@ static double cvCalibrateCamera2Internal( const CvMat* objectPoints,
             CvMat _dpdf = cvMat(_Ji.colRange(0, 2));
             CvMat _dpdc = cvMat(_Ji.colRange(2, 4));
             CvMat _dpdk = cvMat(_Ji.colRange(4, NINTRINSIC));
-            CvMat _dpdo = cvMat(_Jo.colRange(0, ni * 3));
+            CvMat _dpdo = _Jo.empty() ? CvMat() : cvMat(_Jo.colRange(0, ni * 3));
             CvMat _mp = cvMat(_err.reshape(2, 1));
 
             if( calcJ )
@@ -1733,7 +1734,7 @@ static double cvCalibrateCamera2Internal( const CvMat* objectPoints,
                  cvProjectPoints2Internal( &_Mi, &_ri, &_ti, &matA, &_k, &_mp, &_dpdr, &_dpdt,
                                   (flags & CALIB_FIX_FOCAL_LENGTH) ? 0 : &_dpdf,
                                   (flags & CALIB_FIX_PRINCIPAL_POINT) ? 0 : &_dpdc, &_dpdk,
-                                  &_dpdo,
+                                  (_Jo.empty()) ? NULL: &_dpdo,
                                   (flags & CALIB_FIX_ASPECT_RATIO) ? aspectRatio : 0);
             }
             else
