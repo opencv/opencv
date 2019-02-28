e762080373004f59d326ccc06f9a514de0623f24
kinchungwong <kinchungwong@users.noreply.github.com>
2019-02-28 02:47:04 -0800

    Started testing LogTagConfigParser, incomplete.
    The intention of this commit is to illustrate the capabilities of
    the current design of LogTagConfigParser.
    
    The test contained in this commit is not complete. Also, design changes
    may require throwing away this commit and rewriting test code from
    scratch.
    
    Does not test whitespace segmentation (multiple tags on the config);
    will do in next commit.

---
+++ modules/core/test/test_logtagmanager.cpp
@@ -0,0 +1,17 @@
+// This file is part of OpenCV project.
+// It is subject to the license terms in the LICENSE file found in the top-level directory
+// of this distribution and at http://opencv.org/license.html.
+
+#include "test_precomp.hpp"
+#include <opencv2/core/utils/logger.hpp>
+#include "../src/utils/logtagmanager.hpp"
+#include "../src/utils/logtagconfigparser.hpp"
+
+namespace opencv_test {
+namespace {
+
+// STUB
+// to be implemented
+// see "test_logtagconfigparser.cpp"
+
+}} // namespace
