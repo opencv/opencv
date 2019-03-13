7db9607017d323e4c66f4765ee248957134f95be
chacha21 <pierre@chachatelier.fr>
2019-03-13 21:26:01 +0100

    revert changes
    revert changes on  original LineIterator implementation, that will be superseded by the new LineVirtualIterator anyway

--- modules/imgproc/include/opencv2/imgproc.hpp
+++ modules/imgproc/include/opencv2/imgproc.hpp
@@ -4689,14 +4689,7 @@ public:
     not to depend on the ordering of pt1 and pt2 parameters
     */
     LineIterator( const Mat& img, Point pt1, Point pt2,
-                  int connectivity = 8, bool forceLeftToRight = false ) {
-      init(img.size(), img.type(), (uchar*)img.ptr(), img.step1()*img.elemSize1(), pt1, pt2, connectivity, forceLeftToRight);
-    }
-    LineIterator( const Size& size, int type, Point pt1, Point pt2,
-                  int connectivity = 8, bool forceLeftToRight = false ) {
-      init(size, type, 0, CV_ELEM_SIZE(type)*size.width, pt1, pt2, connectivity, forceLeftToRight);
-    }
-    void init(const Size& size, int type, uchar* data, size_t dataStep, Point pt1, Point pt2, int connectivity = 8, bool forceLeftToRight = false);
+                  int connectivity = 8, bool forceLeftToRight = false );
 
     /** @brief returns pointer to the current pixel
     */
