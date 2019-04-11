8b88361a79e1bfd89a13f14bbada75264fff26b8
Maksim Shabunin <maksim.shabunin@gmail.com>
2019-04-11 19:16:50 +0300

    Merge pull request #14278 from mshabunin:fix-osx-camera-auth-rt
    * AVFoundation: Use runtime check for camera authorization

--- modules/videoio/src/cap_avfoundation_mac.mm
+++ modules/videoio/src/cap_avfoundation_mac.mm
@@ -325,24 +325,27 @@ int CvCaptureCAM::startCaptureDevice(int cameraNum) {
     NSAutoreleasePool *localpool = [[NSAutoreleasePool alloc] init];
 
 #if defined(__MAC_OS_X_VERSION_MAX_ALLOWED) && __MAC_OS_X_VERSION_MAX_ALLOWED >= 101400
-    AVAuthorizationStatus status = [AVCaptureDevice authorizationStatusForMediaType:AVMediaTypeVideo];
-    if (status == AVAuthorizationStatusDenied)
+    if (@available(macOS 10.14, *))
     {
-        fprintf(stderr, "OpenCV: camera access has been denied. Either run 'tccutil reset Camera' "
-                        "command in same terminal to reset application authorization status, "
-                        "either modify 'System Preferences -> Security & Privacy -> Camera' "
-                        "settings for your application.\n");
-        [localpool drain];
-        return 0;
-    }
-    else if (status != AVAuthorizationStatusAuthorized)
-    {
-        fprintf(stderr, "OpenCV: not authorized to capture video (status %ld), requesting...\n", status);
-        // TODO: doesn't work via ssh
-        [AVCaptureDevice requestAccessForMediaType:AVMediaTypeVideo completionHandler:^(BOOL) { /* we don't care */}];
-        // we do not wait for completion
-        [localpool drain];
-        return 0;
+        AVAuthorizationStatus status = [AVCaptureDevice authorizationStatusForMediaType:AVMediaTypeVideo];
+        if (status == AVAuthorizationStatusDenied)
+        {
+            fprintf(stderr, "OpenCV: camera access has been denied. Either run 'tccutil reset Camera' "
+                            "command in same terminal to reset application authorization status, "
+                            "either modify 'System Preferences -> Security & Privacy -> Camera' "
+                            "settings for your application.\n");
+            [localpool drain];
+            return 0;
+        }
+        else if (status != AVAuthorizationStatusAuthorized)
+        {
+            fprintf(stderr, "OpenCV: not authorized to capture video (status %ld), requesting...\n", status);
+            // TODO: doesn't work via ssh
+            [AVCaptureDevice requestAccessForMediaType:AVMediaTypeVideo completionHandler:^(BOOL) { /* we don't care */}];
+            // we do not wait for completion
+            [localpool drain];
+            return 0;
+        }
     }
 #endif
 
