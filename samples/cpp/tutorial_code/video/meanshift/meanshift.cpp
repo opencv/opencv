28cd3da7acd027f6d6e3e05c5c702c142486eff4
Lukas Mehl <lukas.mehl@gmail.com>
2019-04-22 17:47:32 +0200

    add cpp code, fix python code

---
+++ samples/cpp/tutorial_code/video/meanshift/meanshift.cpp
@@ -0,0 +1,64 @@
+#include <iostream>
+#include <opencv2/imgcodecs.hpp>
+#include <opencv2/imgproc.hpp>
+#include <opencv2/videoio.hpp>
+#include <opencv2/highgui.hpp>
+#include <opencv2/video.hpp>
+
+using namespace cv;
+using namespace std;
+
+int main() {
+    VideoCapture capture("slow_traffic_small.mp4");
+
+    if (!capture.isOpened()){
+        //error in opening the video input
+        cerr << "Unable to open file!" << endl;
+        return 0;
+    }
+
+    Mat frame, roi, hsv_roi, mask;
+    // take first frame of the video
+    capture >> frame;
+
+    // setup initial location of window
+    Rect track_window(300, 200, 100, 50); // simply hardcoded the values
+
+    // set up the ROI for tracking
+    roi = frame(track_window);
+    cvtColor(roi, hsv_roi, COLOR_BGR2HSV);
+    inRange(hsv_roi, Scalar(0, 60, 32), Scalar(180, 255, 255), mask);
+
+    float range_[] = {0, 180};
+    const float* range[] = {range_};
+    Mat roi_hist;
+    int histSize[] = {180};
+    int channels[] = {0};
+    calcHist(&hsv_roi, 1, channels, mask, roi_hist, 1, histSize, range);
+    normalize(roi_hist, roi_hist, 0, 255, NORM_MINMAX);
+
+    // Setup the termination criteria, either 10 iteration or move by atleast 1 pt
+    TermCriteria term_crit(TermCriteria::EPS | TermCriteria::COUNT, 10, 1);
+
+    while(true){
+        Mat frame, hsv, dst;
+        capture >> frame;
+        if (frame.empty())
+            break;
+        cvtColor(frame, hsv, COLOR_BGR2HSV);
+        calcBackProject(&frame, 1, channels, roi_hist, dst, range);
+
+        // apply meanshift to get the new location
+        meanShift(dst, track_window, term_crit);
+
+        // Draw it on image
+        rectangle(frame, track_window, 255, 2);
+        imshow("img2", frame);
+
+        int keyboard = waitKey(30);
+        if (keyboard == 'q' || keyboard == 27)
+            break;
+    }
+    destroyAllWindows();
+    capture.release();
+}
--- samples/python/tutorial_code/video/meanshift/meanshift.py
+++ samples/python/tutorial_code/video/meanshift/meanshift.py
@@ -1,17 +1,17 @@
 import numpy as np
 import cv2 as cv
 
-cap = cv.VideoCapture('slow.flv')
+cap = cv.VideoCapture("slow_traffic_small.mp4")
 
 # take first frame of the video
 ret,frame = cap.read()
 
 # setup initial location of window
-r,h,c,w = 250,90,400,125  # simply hardcoded the values
-track_window = (c,r,w,h)
+x, y, w, h = 300, 200, 100, 50 # simply hardcoded the values
+track_window = (x, y, w, h)
 
 # set up the ROI for tracking
-roi = frame[r:r+h, c:c+w]
+roi = frame[y:y+h, x:x+w]
 hsv_roi =  cv.cvtColor(roi, cv.COLOR_BGR2HSV)
 mask = cv.inRange(hsv_roi, np.array((0., 60.,32.)), np.array((180.,255.,255.)))
 roi_hist = cv.calcHist([hsv_roi],[0],mask,[180],[0,180])
@@ -21,7 +21,7 @@ cv.normalize(roi_hist,roi_hist,0,255,cv.NORM_MINMAX)
 term_crit = ( cv.TERM_CRITERIA_EPS | cv.TERM_CRITERIA_COUNT, 10, 1 )
 
 while(1):
-    ret ,frame = cap.read()
+    ret, frame = cap.read()
 
     if ret == True:
         hsv = cv.cvtColor(frame, cv.COLOR_BGR2HSV)
@@ -38,9 +38,6 @@ while(1):
         k = cv.waitKey(60) & 0xff
         if k == 27:
             break
-        else:
-            cv.imwrite(chr(k)+".jpg",img2)
-
     else:
         break
 
