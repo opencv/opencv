190dca0dc7d9bbcd4e373395e8be29b6b149bc97
ihsan314 <ihsanolawale@outlook.com>
2019-05-02 17:47:52 -0700

    Nearly done Pythonizing file_input_output.cpp

--- samples/python/tutorial_code/core/file_input_output/file_input_output.py
+++ samples/python/tutorial_code/core/file_input_output/file_input_output.py
@@ -3,35 +3,37 @@ import numpy as np
 import sys
 
 def help(filename):
-    print (filename, "shows the usage of the OpenCV serialization functionality.",
-            "\n\nusage: ", 
-            "\n    python3", filename, "outputfile.yml.gz",
-            "\n\nThe output file may be either in XML, YAML or JSON. You can even compress it",
-            "\nby specifying this in its extension like xml.gz yaml.gz etc... With",
-            "\nFileStorage you can serialize objects in OpenCV.",
-            "\n\nFor example: - create a class and have it serialized",
-            "\n             - use it to read and write matrices.\n"
+    print (filename, 'shows the usage of the OpenCV serialization functionality.',
+            '\n\nusage: ', 
+            '\n    python3', filename, 'outputfile.yml.gz',
+            '\n\nThe output file may be either in XML, YAML or JSON. You can even compress it',
+            '\nby specifying this in its extension like xml.gz yaml.gz etc... With',
+            '\nFileStorage you can serialize objects in OpenCV.',
+            '\n\nFor example: - create a class and have it serialized',
+            '\n             - use it to read and write matrices.\n'
             )
 
 class MyData: 
     A = 97
     X = np.pi
-    name = "mydata1234" 
+    name = 'mydata1234' 
 
     def write(self, fs):
-        fs.write("A", self.A)
-        fs.write("X", self.X)
-        fs.write("name", self.name)
+        fs.write('MyData','{')
+        fs.write('A', self.A)
+        fs.write('X', self.X)
+        fs.write('name', self.name)
+        fs.write('MyData','}')
 
     def read(self, node):
-        self.A = node.getNode("A")
-        self.X = node.getNode("X")
-        self.name = node.getNode("name")
+        self.A = node.getNode('A')
+        self.X = node.getNode('X')
+        self.name = node.getNode('name')
 
 if __name__ == '__main__':
     if len(sys.argv) != 2:
         help(sys.argv[0])
-        exit(1);
+        exit(1)
 
     # write
     R = np.eye(3,3)
@@ -42,27 +44,64 @@ if __name__ == '__main__':
 
     s = cv.FileStorage(filename, 1)
 
-    s.write("iterationNr", 100)
+    s.write('iterationNr', 100)
 
-    array = np.array(["image1.jpg", "Awesomeness", "../data/baboon.jpg"],dtype='S18')
-    print(repr(array))
-    # s.write("strings", array)
+    s.write('strings', '[')
+    s.write('image1.jpg','Awesomeness')
+    s.write('../data/baboon.jpg',']')
 
-    mapping = {'One': 1,'Two': 2}
-    names = ['id', 'data']
-    formats = ['S3', 'int8']
-    dtype = dict(names=names, formats=formats)
-    n_mapping = np.array(list(mapping.items()),dtype)
-    print(repr(n_mapping))
-    s.write ("Mapping", n_mapping)
+    s.write ('Mapping', '{')
+    s.write ('One', 1)
+    s.write ('Two', 2)
+    s.write ('Mapping', '}')
 
-    s.write("R", R)
-    s.write("T", T)
+    s.write('R', R)
+    s.write('T', T)
 
     m.write(s)
     s.release()
+    print ('Write Done.')
 
     # read
+    print ('\nReading: ')
     s = cv.FileStorage()
     s.open(filename, 0)
-    n = cv.FileNode()
+    
+    n = fs.getNode('iterationNr')
+    itNr = int(n.real())
+    print (itNr)
+
+    if (!fs.isOpened()):
+        print ('Failed to open ', filename, file=sys.stderr)
+        help(sys.argv[0])
+        return 1
+
+    n = fs.getNode('strings')
+    if (!n.isSeq()):
+        print ('strings is not a sequence! FAIL', file=sys.stderr)
+        return 1
+
+    for (i in range(n.size())):
+        print (n.at(i))
+
+    n = fs.getNode('Mapping')
+    print ('Two',int(n.getNode('Two').real()),'; ')
+    print ('One',int(n.getNode('One').real()),'\n')
+
+    R = fs.getNode('R').mat()
+    T = fs.getNode('T').mat()
+    # m = (MyData)(fs.getNode('MyData'))
+
+    print ('\nR =',R)
+    print ('T =',T,'\n')
+    '''
+    print ('MyData =','\n',m,'\n')
+
+    print ('Attempt to read NonExisting (should initialize the data structure',
+            'with its default).')
+    m = (MyData)(fs.getNode('MyData'))
+    print ('\nNonExisting =','\n',m)
+    '''
+
+    print ('\nTip: Open up',filename,'with a text editor to see the serialized data.')
+    return 0
