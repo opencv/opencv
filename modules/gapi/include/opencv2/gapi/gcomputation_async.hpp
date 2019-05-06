4c4a8f3e2665aa3321bae5ac2df72c854421508d
Anton Potapov <anton.potapov@intel.com>
2019-05-06 19:53:17 +0300

    Async API for GAPI
    - minor cosmetic changes : comments, typos

--- modules/gapi/include/opencv2/gapi/gcomputation_async.hpp
+++ modules/gapi/include/opencv2/gapi/gcomputation_async.hpp
@@ -19,6 +19,9 @@ namespace cv {
     class GComputation;
 namespace gapi {
 namespace wip  {
+    //These functions asynchronously (i.e. probably on a separate thread of execution) call apply member function of their first argument with copies of rest of arguments (except callback) passed in.
+    //The difference between the function is the way to get the completion notification (via callback or a waiting on std::future object)
+    //If exception is occurred during execution of apply it is transfered to the callback (via function parameter) or passed to future (and will be thrown on call to std::future::get)
     GAPI_EXPORTS void                async_apply(GComputation& gcomp, std::function<void(std::exception_ptr)>&& callback, GRunArgs &&ins, GRunArgsP &&outs, GCompileArgs &&args = {});
     GAPI_EXPORTS std::future<void>   async_apply(GComputation& gcomp, GRunArgs &&ins, GRunArgsP &&outs, GCompileArgs &&args = {});
 } // nmaepspace gapi
