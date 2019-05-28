d57243711513b2a8b1199e50339f08141f96be8a
Talamanov, Anatoliy <anatoliy.talamanov@intel.com>
2019-05-28 14:12:57 +0300

    Remove text2points

--- modules/gapi/test/gapi_render_test.cpp
+++ modules/gapi/test/gapi_render_test.cpp
@@ -37,7 +37,7 @@ TEST_F(RenderTestFixture, PutText)
         int pos_y = 40 + i * 50;
 
         cv::putText(ref_mat, text, cv::Point(pos_x, pos_y), ff, fs, color, thick);
-        events.emplace_back(cv::gapi::TextEvent{text, pos_x, pos_y, ff, fs, color, thick});
+        events.emplace_back(cv::gapi::TextEvent{text, pos_x, pos_y, ff, fs, color, thick, LINE_8, false});
     }
 
     cv::gapi::render(out_mat, events);
@@ -55,10 +55,12 @@ TEST_F(RenderTestFixture, Rectangle)
         int pos_y = 40 + i * 50;
 
         cv::rectangle(ref_mat, cv::Rect(pos_x, pos_y, w, h), color, thick);
-        events.emplace_back(cv::gapi::RectEvent{pos_x, pos_y, w, h, color, thick});
+        events.emplace_back(cv::gapi::RectEvent{pos_x, pos_y, w, h, color, thick, LINE_8, 0});
     }
 
     cv::gapi::render(out_mat, events);
 
     EXPECT_EQ(0, cv::countNonZero(out_mat != ref_mat));
 }
+
+}  // opencv_test
