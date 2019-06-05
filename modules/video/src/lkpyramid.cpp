1aca1d582eb2f1156f1cde4197c79fedd55c3cdb
Ahmed Ashour <asashour@yahoo.com>
2019-06-05 12:24:13 +0200

    Fix some typos

--- modules/video/src/lkpyramid.cpp
+++ modules/video/src/lkpyramid.cpp
@@ -1283,7 +1283,7 @@ void SparsePyrLKOpticalFlowImpl::calc( InputArray _prevImg, InputArray _nextImg,
             levels1 /= 2;
         }
 
-        // ensure that pyramid has reqired padding
+        // ensure that pyramid has required padding
         if(levels1 > 0)
         {
             Size fullSize;
@@ -1311,7 +1311,7 @@ void SparsePyrLKOpticalFlowImpl::calc( InputArray _prevImg, InputArray _nextImg,
             levels2 /= 2;
         }
 
-        // ensure that pyramid has reqired padding
+        // ensure that pyramid has required padding
         if(levels2 > 0)
         {
             Size fullSize;
