a6fae78c2824e7299a139065ce8fd4649f1c9f21
Sushanti Kerani <sushantii.kerani@gmail.com>
2019-06-06 17:08:46 +0530

    Add files via upload

---
+++ samples/python/fps.py
@@ -0,0 +1,29 @@
+import cv2
+import time
+
+# cv2.VideoCapture(0) gives return as True 
+# but then print(frame) gives me an array filles with zeros
+
+# cv2.VideoCapture(cv2.CAP_DSHOW) works just fine in this case
+
+cap = cv2.VideoCapture(cv2.CAP_DSHOW)
+fps = cap.get(cv2.CAP_PROP_FPS)
+
+ref = 60
+
+while True:
+	start = time.time()
+
+	for i in range(0, ref):
+		ret, frame = cap.read()
+
+	end = time.time()
+	number_of_seconds = end-start
+	fps=ref/number_of_seconds
+	print(fps)
+	k=cv2.waitKey(0)
+	if k==27:
+		break
+
+cap.release()
+cv2.destroyAllWindows()
