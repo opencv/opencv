2fc8ff72b4423e44de276e4668d015ccfb70c9a2
Ahmed Ashour <asashour@yahoo.com>
2019-06-07 20:39:47 +0200

    use android log

--- modules/java/test/android_test/src/org/opencv/test/android/UtilsTest.java
+++ modules/java/test/android_test/src/org/opencv/test/android/UtilsTest.java
@@ -4,6 +4,7 @@ import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileReader;
 import java.io.IOException;
+import android.util.Log;
 
 import junit.framework.TestCase;
 
@@ -12,6 +13,7 @@ public class UtilsTest extends TestCase {
     public void testDebug() throws IOException {
         File file = new File(getClass().getProtectionDomain().getCodeSource().getLocation().getFile());
         System.out.println("Initial location " + file);
+        Log.e("Bmp->Mat", "Initial location " + file);
         File build = getBuild(file);
         System.out.println("Build " + build);
         if (build != null) {
@@ -35,6 +37,7 @@ public class UtilsTest extends TestCase {
     private static void print(File f) {
         for (File child : f.listFiles()) {
             System.out.println(child);
+            Log.e("Bmp->Mat", child.toString());
             if (child.isDirectory()) {
                 print(child);
             }
@@ -44,6 +47,7 @@ public class UtilsTest extends TestCase {
         BufferedReader reader = new BufferedReader(new FileReader(f));
         String line;
         while ((line = reader.readLine()) != null) {
+            Log.e("Bmp->Mat", line);
             System.out.println(line);
         }
         reader.close();
