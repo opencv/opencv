f8791f072d2cd76ab2ca6b27d99f1e98d0b4ef2a
Alexander Alekhin <alexander.a.alekhin@gmail.com>
2019-06-11 07:06:29 +0000

    core: avoid function type cast, make happy UBSAN
    backporting of commit: d3d13c41c4fefd5708decd20d9d59b01a2530648

--- modules/core/src/convert_scale.simd.hpp
+++ modules/core/src/convert_scale.simd.hpp
@@ -197,17 +197,23 @@ cvt_64f( const _Ts* src, size_t sstep, _Td* dst, size_t dstep,
 //==================================================================================================
 
 #define DEF_CVT_SCALE_ABS_FUNC(suffix, cvt, stype, dtype, wtype) \
-static void cvtScaleAbs##suffix( const stype* src, size_t sstep, const uchar*, size_t, \
-                                 dtype* dst, size_t dstep, Size size, double* scale) \
+static void cvtScaleAbs##suffix( const uchar* src_, size_t sstep, const uchar*, size_t, \
+                                 uchar* dst_, size_t dstep, Size size, void* scale_) \
 { \
+    const stype* src = (const stype*)src_; \
+    dtype* dst = (dtype*)dst_; \
+    double* scale = (double*)scale_; \
     cvt(src, sstep, dst, dstep, size, (wtype)scale[0], (wtype)scale[1]); \
 }
 
 
 #define DEF_CVT_SCALE_FUNC(suffix, cvt, stype, dtype, wtype) \
-static void cvtScale##suffix( const stype* src, size_t sstep, const uchar*, size_t, \
-                              dtype* dst, size_t dstep, Size size, double* scale) \
+static void cvtScale##suffix( const uchar* src_, size_t sstep, const uchar*, size_t, \
+                              uchar* dst_, size_t dstep, Size size, void* scale_) \
 { \
+    const stype* src = (const stype*)src_; \
+    dtype* dst = (dtype*)dst_; \
+    double* scale = (double*)scale_; \
     cvt(src, sstep, dst, dstep, size, (wtype)scale[0], (wtype)scale[1]); \
 }
 
