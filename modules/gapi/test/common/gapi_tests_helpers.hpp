1ea50334bb15f83b7ddb50dab4fa535739903565
Andrey Golubev <andrey.golubev@intel.com>
2019-06-13 15:32:40 +0300

    Fix Windows issues with macro expansion

--- modules/gapi/test/common/gapi_tests_helpers.hpp
+++ modules/gapi/test/common/gapi_tests_helpers.hpp
@@ -19,48 +19,51 @@ enum {
     SAME_TYPE = std::numeric_limits<int>::max()
 };
 
+// Ensure correct __VA_ARGS__ expansion on Windows
+#define FIX_VAARGS(x) x
+
 // implementation of recursive in-class declaration and initialization of member variables
-#define __DEFINE_PARAMS_IMPL1(params_type, params, index, param_name, ...) \
+#define __DEFINE_PARAMS_IMPL1(params_type, params, index, param_name) \
     std::tuple_element<index, params_type>::type param_name = std::get<index>(params);
 
 #define __DEFINE_PARAMS_IMPL2(params_type, params, index, param_name, ...) \
     std::tuple_element<index, params_type>::type param_name = std::get<index>(params); \
-    __DEFINE_PARAMS_IMPL1(params_type, params, index+1, __VA_ARGS__)
+    FIX_VAARGS(__DEFINE_PARAMS_IMPL1(params_type, params, index+1, __VA_ARGS__))
 
 #define __DEFINE_PARAMS_IMPL3(params_type, params, index, param_name, ...) \
     std::tuple_element<index, params_type>::type param_name = std::get<index>(params); \
-    __DEFINE_PARAMS_IMPL2(params_type, params, index+1, __VA_ARGS__)
+    FIX_VAARGS(__DEFINE_PARAMS_IMPL2(params_type, params, index+1, __VA_ARGS__))
 
 #define __DEFINE_PARAMS_IMPL4(params_type, params, index, param_name, ...) \
     std::tuple_element<index, params_type>::type param_name = std::get<index>(params); \
-    __DEFINE_PARAMS_IMPL3(params_type, params, index+1, __VA_ARGS__)
+    FIX_VAARGS(__DEFINE_PARAMS_IMPL3(params_type, params, index+1, __VA_ARGS__))
 
 #define __DEFINE_PARAMS_IMPL5(params_type, params, index, param_name, ...) \
     std::tuple_element<index, params_type>::type param_name = std::get<index>(params); \
-    __DEFINE_PARAMS_IMPL4(params_type, params, index+1, __VA_ARGS__)
+    FIX_VAARGS(__DEFINE_PARAMS_IMPL4(params_type, params, index+1, __VA_ARGS__))
 
 #define __DEFINE_PARAMS_IMPL6(params_type, params, index, param_name, ...) \
     std::tuple_element<index, params_type>::type param_name = std::get<index>(params); \
-    __DEFINE_PARAMS_IMPL5(params_type, params, index+1, __VA_ARGS__)
+    FIX_VAARGS(__DEFINE_PARAMS_IMPL5(params_type, params, index+1, __VA_ARGS__))
 
 // user interface to define member variables of specified names
 #define DEFINE_SPECIFIC_PARAMS_1(...) \
-    __DEFINE_PARAMS_IMPL1(specific_params_t, GetParam().specificParams(), 0, __VA_ARGS__)
+    FIX_VAARGS(__DEFINE_PARAMS_IMPL1(specific_params_t, GetParam().specificParams(), 0, __VA_ARGS__))
 
 #define DEFINE_SPECIFIC_PARAMS_2(...) \
-    __DEFINE_PARAMS_IMPL2(specific_params_t, GetParam().specificParams(), 0, __VA_ARGS__)
+    FIX_VAARGS(__DEFINE_PARAMS_IMPL2(specific_params_t, GetParam().specificParams(), 0, __VA_ARGS__))
 
 #define DEFINE_SPECIFIC_PARAMS_3(...) \
-    __DEFINE_PARAMS_IMPL3(specific_params_t, GetParam().specificParams(), 0, __VA_ARGS__)
+    FIX_VAARGS(__DEFINE_PARAMS_IMPL3(specific_params_t, GetParam().specificParams(), 0, __VA_ARGS__))
 
 #define DEFINE_SPECIFIC_PARAMS_4(...) \
-    __DEFINE_PARAMS_IMPL4(specific_params_t, GetParam().specificParams(), 0, __VA_ARGS__)
+    FIX_VAARGS(__DEFINE_PARAMS_IMPL4(specific_params_t, GetParam().specificParams(), 0, __VA_ARGS__))
 
 #define DEFINE_SPECIFIC_PARAMS_5(...) \
-    __DEFINE_PARAMS_IMPL5(specific_params_t, GetParam().specificParams(), 0, __VA_ARGS__)
+    FIX_VAARGS(__DEFINE_PARAMS_IMPL5(specific_params_t, GetParam().specificParams(), 0, __VA_ARGS__))
 
 #define DEFINE_SPECIFIC_PARAMS_6(...) \
-    __DEFINE_PARAMS_IMPL6(specific_params_t, GetParam().specificParams(), 0, __VA_ARGS__)
+    FIX_VAARGS(__DEFINE_PARAMS_IMPL6(specific_params_t, GetParam().specificParams(), 0, __VA_ARGS__))
 } // namespace opencv_test
 
 #endif //OPENCV_GAPI_TESTS_HELPERS_HPP
