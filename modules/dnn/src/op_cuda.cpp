adad256226f0e05f8092d245315a2b4b026fc7d2
Yashas <yashas_2010@yahoo.com>
2019-06-21 05:37:22 +0530

    integrate csl::Tensor into backbone code

--- modules/dnn/src/op_cuda.cpp
+++ modules/dnn/src/op_cuda.cpp
@@ -5,8 +5,114 @@
 #include "precomp.hpp"
 #include "op_cuda.hpp"
 
+#ifdef HAVE_CUDA
+#include "cuda4dnn/csl/stream.hpp"
+#include "cuda4dnn/csl/tensor.hpp"
+#include "cuda4dnn/csl/pointer.hpp"
+using namespace cv::dnn::cuda4dnn;
+#endif
+
+#include <opencv2/dnn/shape_utils.hpp>
+#include <opencv2/core.hpp>
+
+#include <memory>
+
 namespace cv {
     namespace dnn {
+#ifdef HAVE_CUDA
+        CUDABackendWrapperFP32::CUDABackendWrapperFP32(Mat& m)
+            : BackendWrapper(DNN_BACKEND_OPENCV, DNN_TARGET_CUDA_FP32)
+        {
+            CV_Assert(m.isContinuous());
+            CV_Assert(m.type() == CV_32F);
+            CV_Assert(m.size.dims() <= tensor_type::rank);
 
+            shape = cv::dnn::shape(m);
+
+            shared_block = std::make_shared<shared_block_type>();
+            shared_block->host_dirty = true;
+            shared_block->device_dirty = false;
+            shared_block->host = m;
+            shared_block->memGuard = csl::MemoryLockGuard(m.data, m.total() * sizeof(float));
+            shared_block->parent = createTensorHeaderFromMat<tensor_type>(m);
+        }
+
+        CUDABackendWrapperFP32::CUDABackendWrapperFP32(const Ptr<BackendWrapper>& base_, const MatShape& shape_)
+            : BackendWrapper(DNN_BACKEND_OPENCV, DNN_TARGET_CUDA_FP32)
+        {
+            const Ptr<CUDABackendWrapperFP32> base = base_.dynamicCast<CUDABackendWrapperFP32>();
+
+            shape = shape_;
+            shared_block = base->shared_block;
+        }
+
+        Ptr<BackendWrapper> CUDABackendWrapperFP32::create(Mat& m)
+        {
+            return Ptr<BackendWrapper>(new CUDABackendWrapperFP32(m));
+        }
+
+        Ptr<BackendWrapper> CUDABackendWrapperFP32::create(const Ptr<BackendWrapper>& base, const MatShape& shape)
+        {
+            return Ptr<BackendWrapper>(new CUDABackendWrapperFP32(base, shape));
+        }
+
+        /* blocking */
+        void CUDABackendWrapperFP32::copyToHost() {
+            if(shared_block->device_dirty) {
+                shared_block->host_dirty = false;
+                shared_block->device_dirty = false;
+
+                /* If the wrapper is being reused, the device tensor might be larger in size.
+                 * Using the device tensor does not give incorrect code, but it leads to unused regions
+                 * of memory being copied.
+                 *
+                 * We use a view to ensure that only the required region of memory is copied.
+                 */
+                auto view = tensor_view_type(shared_block->parent).subview(0, std::begin(shape), std::end(shape));
+                copyTensorToMat(shared_block->host, view, shared_block->stream);
+
+                shared_block->stream.synchronize();
+            }
+        }
+
+        void CUDABackendWrapperFP32::setHostDirty() {
+            shared_block->device_dirty = false;
+            shared_block->host_dirty = true;
+        }
+
+        /* non-blocking
+         * we don't have to block for copying to device because all operations are put into a stream which
+         * ensures that the operations added to the stream are performed in order
+         */
+        void CUDABackendWrapperFP32::copyToDevice() {
+            if(shared_block->host_dirty) {
+                shared_block->host_dirty = false;
+                shared_block->device_dirty = false;
+
+                auto span = tensor_span_type(shared_block->parent).subspan(0, std::begin(shape), std::end(shape));
+                copyMatToTensor(span, shared_block->host, shared_block->stream);
+            }
+        }
+
+        void CUDABackendWrapperFP32::setDeviceDirty() noexcept {
+            shared_block->device_dirty = true;
+            shared_block->host_dirty = false;
+        }
+
+        void CUDABackendWrapperFP32::setStream(csl::Stream stream) noexcept {
+            shared_block->stream = std::move(stream);
+        }
+
+        CUDABackendWrapperFP32::tensor_span_type CUDABackendWrapperFP32::getSpan() noexcept {
+            setDeviceDirty();
+            return tensor_span_type(shared_block->parent).subspan(0, std::begin(shape), std::end(shape));
+        }
+
+        CUDABackendWrapperFP32::tensor_view_type CUDABackendWrapperFP32::getView() noexcept {
+            copyToDevice();
+            return tensor_view_type(shared_block->parent).subview(0, std::begin(shape), std::end(shape));
+        }
+
+#endif /* ifdef HAVE_CUDA */
     } /* namespace dnn */
 } /* namespace cv */
--- modules/dnn/src/op_cuda.hpp
+++ modules/dnn/src/op_cuda.hpp
@@ -5,6 +5,17 @@
 #ifndef OPENCV_DNN_SRC_OP_CUDA_HPP
 #define OPENCV_DNN_SRC_OP_CUDA_HPP
 
+#ifdef HAVE_CUDA
+#include "cuda4dnn/csl/stream.hpp"
+#include "cuda4dnn/csl/tensor.hpp"
+#include "cuda4dnn/csl/pointer.hpp"
+#endif
+
+#include <opencv2/dnn/shape_utils.hpp>
+#include <opencv2/core.hpp>
+
+#include <memory>
+
 namespace cv {
     namespace dnn {
         inline bool haveCUDA() {
@@ -16,47 +27,121 @@ namespace cv {
         }
 
 #ifdef HAVE_CUDA
-        /* CUDA Tensors are represented by csl::Tensor
-        ** CUDABackendWrapperFP32 wraps a csl::TensorSpan<float>
-        ** It also maintains a reference to the csl::Tensor.
-        */
-        class CUDABackendWrapperFP32 : public BackendWrapper {
+        /** @brief creates csl::Tensor object from cv::Mat */
+        template <class TensorT = cuda4dnn::csl::Tensor<float>> inline
+            TensorT createTensorHeaderFromMat(const cv::Mat& mat) {
+            auto is_matrix_type_same_as_tensor_type = [&mat]() {
+                switch (mat.type()) {
+                case CV_32F: return std::is_same<TensorT::value_type, float>::value;
+                default: return false;
+                }
+            };
+            CV_Assert(is_matrix_type_same_as_tensor_type());
+
+            auto sizes = shape(mat);
+            return TensorT(std::begin(sizes), std::end(sizes));
+        }
+
+        /** @brief copies data from a cv::Mat and fills a TensorType
+         *
+         * Pre-conditions:
+         * - \p mat must be larger or equal to the tensor in size
+         *
+         * @note performance is best for continuous and page-locked cv::Mat
+         */
+        template <class TensorSpanT> inline
+        void copyMatToTensor(const TensorSpanT& tensor, const cv::Mat& mat, const cuda4dnn::csl::Stream& stream) {
+            CV_Assert(mat.total() >= tensor.size());
+
+            cv::Mat source = mat.isContinuous() ? mat : mat.clone();
+            CV_Assert(source.isContinuous());
+
+            using T = typename TensorSpanT::value_type;
+            cuda4dnn::csl::memcpy<T>(tensor.get(), reinterpret_cast<T*>(source.data), tensor.size(), stream);
+        }
+
+        /** @brief copies data from a TensorType to a cv::Mat
+         *
+         * Pre-conditions:
+         * - \p mat must be larger or equal to the tensor in size
+         *
+         * @note performance is best for continuous and page-locked cv::Mat
+         */
+        template <class TensorType = cuda4dnn::csl::TensorView<float>> inline
+        void copyTensorToMat(cv::Mat& mat, TensorType& tensor, const cuda4dnn::csl::Stream& stream) {
+            CV_Assert(mat.total() >= tensor.size());
+
+            cv::Mat source = mat.isContinuous() ? mat : mat.clone();
+            CV_Assert(source.isContinuous());
+
+            using T = typename TensorType::value_type;
+            cuda4dnn::csl::memcpy<T>(reinterpret_cast<T*>(source.data), tensor.get(), tensor.size(), stream);
+
+            if(source.data != mat.data)
+                source.copyTo(mat);
+        }
+
+        class CUDABackendWrapperFP32 final : public BackendWrapper {
         public:
-            CUDABackendWrapperFP32(Mat& m) : BackendWrapper(DNN_BACKEND_OPENCV, DNN_TARGET_CUDA_FP32) {
-                /* TODO:
-                ** 1. store a reference to cv::Mat
-                ** 2. create a csl::Tensor<float>
-                ** 3. create a csl::TensorSpan<float> (or store shape)
-                */
-            }
+            using value_type = float;
+            using tensor_type = cuda4dnn::csl::Tensor<value_type>;
+            using tensor_span_type = cuda4dnn::csl::TensorSpan<value_type>;
+            using tensor_view_type = cuda4dnn::csl::TensorView<value_type>;
 
-            CUDABackendWrapperFP32(const Ptr<BackendWrapper>& base, const MatShape& shape)
-                : BackendWrapper(DNN_BACKEND_OPENCV, DNN_TARGET_CUDA_FP32) {
-                /* TODO:
-                ** 1. copy reference to csl::Tensor<float> of base
-                ** 2. set TensorSpan<float> to mimic `shape` (or store shape)
-                */
-            }
+            CUDABackendWrapperFP32(Mat&);
+            CUDABackendWrapperFP32(const Ptr<BackendWrapper>& base, const MatShape& shape);
 
-            static Ptr<BackendWrapper> create(Mat& m)
-            {
-                return Ptr<BackendWrapper>(new CUDABackendWrapperFP32(m));
-            }
+            static Ptr<BackendWrapper> create(Mat&);
+            static Ptr<BackendWrapper> create(const Ptr<BackendWrapper>& base, const MatShape& shape);
 
-            static Ptr<BackendWrapper> create(const Ptr<BackendWrapper>& base, const MatShape& shape)
-            {
-                return Ptr<BackendWrapper>(new CUDABackendWrapperFP32(base, shape));
-            }
+            void copyToHost() override;
+            void setHostDirty() override;
 
-            virtual void copyToHost() CV_OVERRIDE { }
-            virtual void setHostDirty() CV_OVERRIDE { }
+            void copyToDevice();
+            void setDeviceDirty() noexcept;
 
-            //TensorSpan<float> getSpan();
-            //TensorView<float> getView();
+            MatShape getShape() const noexcept { return shape; }
 
-            /* TensorSpan member vs create in getSpan()
-            ** member tensor span can save shape changes
-            */
+            /** @note setting the stream updates the stream for all wrappers which use the same buffer */
+            void setStream(cuda4dnn::csl::Stream stream) noexcept;
+
+            /* Optimization Note: use getSpan() and getView() judiciously
+             *
+             * getSpan() is meant to be used when the memory is going to be modified
+             * getView() is meant to be used when the memory is only going to be read
+             *
+             * getSpan() marks the device memory as dirty but getView() does not
+             *
+             * getView() implicitly performs host to device memory transfer if required
+             * getSpan() does not perform any synchronization (use copyToDevice if sync. is required)
+             */
+            tensor_span_type getSpan() noexcept;
+            tensor_view_type getView() noexcept;
+
+        private:
+            /* The same device memory can be reused by different layers whenever possible.
+             * Hence, it is possible for different backend warppers to point to the same device memory.
+             * However, it may use only a part of the total device memory and have a different shape.
+             *
+             * We store the common information such as device tensor and its corresponding host memory in
+             * a shared block. The shared block is shared by all backend wrappers which use the same device memory.
+             * The shape, which can be different for different wrappers, is stored as a member object.
+             */
+
+            MatShape shape;
+
+            struct shared_block_type {
+                bool host_dirty;
+                bool device_dirty;
+
+                cv::Mat host;
+                cuda4dnn::csl::MemoryLockGuard memGuard; /* keeps host memory page-locked */
+
+                tensor_type parent;
+                cuda4dnn::csl::Stream stream;
+            };
+
+            std::shared_ptr<shared_block_type> shared_block;
         };
 #endif
     } /* namespace dnn */
