746e64e2fe038e62b0594ce1937df9e17e38090a
Andrey Golubev <andrey.golubev@intel.com>
2019-06-28 13:07:41 +0300

    Merge pull request #14757 from andrey-golubev:core_tests_update
    G-API: Introduce new approach to write accuracy tests (#14757)
    
    * G-API: Introduce new common accuracy test fixture
    
    * Enable Range<> to Seq<> implicit conversion
    
    * Fix shadowing parameters
    
    * Update license headers
    
    * Rename ALIGNED_TYPE to SAME_TYPE
    
    * Move MkRange to tests
    
    * Fix TODO(agolubev) in test instantiations
    
    * Squash simple fixture declarations in one line
    
    * Remove unused line
    
    * Fix Windows issues with macro expansion
    
    * Choose between 1 or 2 matrix initialization
    
    * Redesign common class behavior
    
    Use "views" for GetParam() provided by GTest
    base class instead of doing segregation
    (with copy!) of common and specific parameters:
    request common or specific parameter directly
    by index from GetParam()-returned parameters
    
    * Refine user-level API and usage of new test model
    
    * Fix -fpermissive errors
    
    * Remove unnecessary init calls
    
    * Replace GCompileArgs member variable with func ptr
    
    * Rename initMatsRandN to make its behavior explicit
    
    Rename initMatsRandN to initMatrixRandN to eliminate confusion:
    initMatsRandN only initialized first matrix (similarly to
    initMatrixRandU)
    
    * Fix common of initNothing
    
    * Update copyright dates in missed files
    
    * Add check for specific parameters
    
    * Fix coment stlye

--- modules/gapi/test/common/gapi_core_tests.hpp
+++ modules/gapi/test/common/gapi_core_tests.hpp
@@ -2,7 +2,7 @@
 // It is subject to the license terms in the LICENSE file found in the top-level directory
 // of this distribution and at http://opencv.org/license.html.
 //
-// Copyright (C) 2018 Intel Corporation
+// Copyright (C) 2018-2019 Intel Corporation
 
 
 #ifndef OPENCV_GAPI_CORE_TESTS_HPP
@@ -46,16 +46,18 @@ struct PrintMathOpCoreParams
     std::string operator()(const ::testing::TestParamInfo<TestParams>& info) const
     {
         std::stringstream ss;
-        cv::Size sz = std::get<4>(info.param);
-        ss<<MathOperations[std::get<0>(info.param)]
-                    <<"_"<<std::get<1>(info.param)
-                    <<"_"<<std::get<2>(info.param)
-                    <<"_"<<(int)std::get<3>(info.param)
+        using AllParams = Params<mathOp,bool,double,bool>;
+        const AllParams::params_t& params = info.param;
+        cv::Size sz = AllParams::getCommon<1>(params);  // size
+        ss<<MathOperations[AllParams::getSpecific<0>(params)]  // mathOp
+                    <<"_"<<AllParams::getSpecific<1>(params)  // testWithScalar
+                    <<"_"<<AllParams::getCommon<0>(params)  // type
+                    <<"_"<<(int)AllParams::getSpecific<2>(params)  // scale
                     <<"_"<<sz.width
                     <<"x"<<sz.height
-                    <<"_"<<(std::get<5>(info.param)+1)
-                    <<"_"<<std::get<6>(info.param)
-                    <<"_"<<std::get<7>(info.param);
+                    <<"_"<<(AllParams::getCommon<2>(params)+1)  // dtype
+                    <<"_"<<AllParams::getCommon<3>(params)  // createOutputMatrices
+                    <<"_"<<AllParams::getSpecific<3>(params);  // doReverseOp
         return ss.str();
    }
 };
@@ -66,13 +68,15 @@ struct PrintCmpCoreParams
     std::string operator()(const ::testing::TestParamInfo<TestParams>& info) const
     {
         std::stringstream ss;
-        cv::Size sz = std::get<3>(info.param);
-        ss<<CompareOperations[std::get<0>(info.param)]
-                    <<"_"<<std::get<1>(info.param)
-                    <<"_"<<std::get<2>(info.param)
+        using AllParams = Params<CmpTypes,bool>;
+        const AllParams::params_t& params = info.param;
+        cv::Size sz = AllParams::getCommon<1>(params);  // size
+        ss<<CompareOperations[AllParams::getSpecific<0>(params)]  // CmpType
+                    <<"_"<<AllParams::getSpecific<1>(params)  // testWithScalar
+                    <<"_"<<AllParams::getCommon<0>(params)  // type
                     <<"_"<<sz.width
                     <<"x"<<sz.height
-                    <<"_"<<std::get<4>(info.param);
+                    <<"_"<<AllParams::getCommon<3>(params);  // createOutputMatrices
         return ss.str();
    }
 };
@@ -83,12 +87,14 @@ struct PrintBWCoreParams
     std::string operator()(const ::testing::TestParamInfo<TestParams>& info) const
     {
         std::stringstream ss;
-        cv::Size sz = std::get<2>(info.param);
-        ss<<BitwiseOperations[std::get<0>(info.param)]
-                    <<"_"<<std::get<1>(info.param)
+        using AllParams = Params<bitwiseOp>;
+        const AllParams::params_t& params = info.param;
+        cv::Size sz = AllParams::getCommon<1>(params);  // size
+        ss<<BitwiseOperations[AllParams::getSpecific<0>(params)]  // bitwiseOp
+                    <<"_"<<AllParams::getCommon<0>(params)  // type
                     <<"_"<<sz.width
                     <<"x"<<sz.height
-                    <<"_"<<std::get<3>(info.param);
+                    <<"_"<<AllParams::getCommon<3>(params);  // createOutputMatrices
         return ss.str();
    }
 };
@@ -99,56 +105,64 @@ struct PrintNormCoreParams
     std::string operator()(const ::testing::TestParamInfo<TestParams>& info) const
     {
         std::stringstream ss;
-        cv::Size sz = std::get<2>(info.param);
-        ss<<NormOperations[std::get<0>(info.param)]
-                    <<"_"<<std::get<1>(info.param)
+        using AllParams = Params<compare_scalar_f,NormTypes>;
+        const AllParams::params_t& params = info.param;
+        cv::Size sz = AllParams::getCommon<1>(params);  // size
+        ss<<NormOperations[AllParams::getSpecific<1>(params)]  // NormTypes
+                    <<"_"<<AllParams::getCommon<0>(params)  // type
                     <<"_"<<sz.width
                     <<"x"<<sz.height;
         return ss.str();
    }
 };
 
-struct MathOpTest        : public TestParams<std::tuple<mathOp,bool,int,double,cv::Size,int,bool,bool,cv::GCompileArgs>>{};
-struct MulDoubleTest     : public TestParams<std::tuple<int,cv::Size,int,bool,cv::GCompileArgs>>{};
-struct DivTest           : public TestParams<std::tuple<int,cv::Size,int,bool, cv::GCompileArgs>>{};
-struct DivCTest          : public TestParams<std::tuple<int,cv::Size,int,bool, cv::GCompileArgs>>{};
-struct MeanTest          : public TestParams<std::tuple<int,cv::Size,bool, cv::GCompileArgs>> {};
-struct MaskTest          : public TestParams<std::tuple<int,cv::Size,bool, cv::GCompileArgs>> {};
-struct Polar2CartTest    : public TestParams<std::tuple<cv::Size,bool, cv::GCompileArgs>> {};
-struct Cart2PolarTest    : public TestParams<std::tuple<cv::Size,bool, cv::GCompileArgs>> {};
-struct CmpTest           : public TestParams<std::tuple<CmpTypes,bool,int,cv::Size,bool, cv::GCompileArgs>>{};
-struct BitwiseTest       : public TestParams<std::tuple<bitwiseOp,int,cv::Size,bool, cv::GCompileArgs>>{};
-struct NotTest           : public TestParams<std::tuple<int,cv::Size,bool, cv::GCompileArgs>> {};
-struct SelectTest        : public TestParams<std::tuple<int,cv::Size,bool, cv::GCompileArgs>> {};
-struct MinTest           : public TestParams<std::tuple<int,cv::Size,bool, cv::GCompileArgs>>{};
-struct MaxTest           : public TestParams<std::tuple<int,cv::Size,bool, cv::GCompileArgs>>{};
-struct AbsDiffTest       : public TestParams<std::tuple<int,cv::Size,bool, cv::GCompileArgs>>{};
-struct AbsDiffCTest      : public TestParams<std::tuple<int,cv::Size,bool, cv::GCompileArgs>> {};
-struct SumTest           : public TestParams<std::tuple<int, cv::Size,bool, compare_scalar_f, cv::GCompileArgs>> {};
-struct AddWeightedTest   : public TestParams<std::tuple<int,cv::Size,int,bool, compare_f,cv::GCompileArgs>>{};
-struct NormTest          : public TestParams<std::tuple<NormTypes,int,cv::Size, compare_scalar_f, cv::GCompileArgs>>{};
-struct IntegralTest      : public TestWithParam<std::tuple<int,cv::Size, cv::GCompileArgs>> {};
-struct ThresholdTest     : public TestParams<std::tuple<int,cv::Size,int,bool, cv::GCompileArgs>> {};
-struct ThresholdOTTest   : public TestParams<std::tuple<int,cv::Size,int,bool, cv::GCompileArgs>> {};
-struct InRangeTest       : public TestParams<std::tuple<int,cv::Size,bool, cv::GCompileArgs>> {};
-struct Split3Test        : public TestParams<std::tuple<cv::Size, cv::GCompileArgs>> {};
-struct Split4Test        : public TestParams<std::tuple<cv::Size, cv::GCompileArgs>> {};
-struct ResizeTest        : public TestWithParam<std::tuple<compare_f, int, int, cv::Size, cv::Size, cv::GCompileArgs>> {};
-struct ResizeTestFxFy    : public TestWithParam<std::tuple<compare_f, int, int, cv::Size, double, double, cv::GCompileArgs>> {};
-struct Merge3Test        : public TestParams<std::tuple<cv::Size, cv::GCompileArgs>> {};
-struct Merge4Test        : public TestParams<std::tuple<cv::Size, cv::GCompileArgs>> {};
-struct RemapTest         : public TestParams<std::tuple<int,cv::Size,bool, cv::GCompileArgs>> {};
-struct FlipTest          : public TestParams<std::tuple<int, int, cv::Size,bool, cv::GCompileArgs>> {};
-struct CropTest          : public TestParams<std::tuple<int,cv::Rect,cv::Size,bool, cv::GCompileArgs>> {};
-struct ConcatHorTest     : public TestWithParam<std::tuple<int, cv::Size, cv::GCompileArgs>> {};
-struct ConcatVertTest    : public TestWithParam<std::tuple<int, cv::Size, cv::GCompileArgs>> {};
-struct ConcatVertVecTest : public TestWithParam<std::tuple<int, cv::Size, cv::GCompileArgs>> {};
-struct ConcatHorVecTest  : public TestWithParam<std::tuple<int, cv::Size, cv::GCompileArgs>> {};
-struct LUTTest           : public TestParams<std::tuple<int, int, cv::Size,bool, cv::GCompileArgs>> {};
-struct ConvertToTest     : public TestParams<std::tuple<int, int, cv::Size, double, double, compare_f, cv::GCompileArgs>> {};
-struct PhaseTest         : public TestParams<std::tuple<int, cv::Size, bool, cv::GCompileArgs>> {};
-struct SqrtTest          : public TestParams<std::tuple<int, cv::Size, cv::GCompileArgs>> {};
-struct NormalizeTest : public TestParams<std::tuple<compare_f,MatType,cv::Size,double,double,int,MatType,bool,cv::GCompileArgs>> {};
+GAPI_TEST_FIXTURE(MathOpTest, initMatsRandU, FIXTURE_API(mathOp,bool,double,bool), 4,
+    opType, testWithScalar, scale, doReverseOp)
+GAPI_TEST_FIXTURE(MulDoubleTest, initMatrixRandU, <>, 0)
+GAPI_TEST_FIXTURE(DivTest, initMatrixRandU, <>, 0)
+GAPI_TEST_FIXTURE(DivCTest, initMatrixRandU, <>, 0)
+GAPI_TEST_FIXTURE(MeanTest, initMatrixRandU, <>, 0)
+GAPI_TEST_FIXTURE(MaskTest, initMatrixRandU, <>, 0)
+GAPI_TEST_FIXTURE(Polar2CartTest, initMatsRandU, <>, 0)
+GAPI_TEST_FIXTURE(Cart2PolarTest, initMatsRandU, <>, 0)
+GAPI_TEST_FIXTURE(CmpTest, initMatsRandU, FIXTURE_API(CmpTypes,bool), 2, opType, testWithScalar)
+GAPI_TEST_FIXTURE(BitwiseTest, initMatsRandU, FIXTURE_API(bitwiseOp), 1, opType)
+GAPI_TEST_FIXTURE(NotTest, initMatrixRandU, <>, 0)
+GAPI_TEST_FIXTURE(SelectTest, initMatsRandU, <>, 0)
+GAPI_TEST_FIXTURE(MinTest, initMatsRandU, <>, 0)
+GAPI_TEST_FIXTURE(MaxTest, initMatsRandU, <>, 0)
+GAPI_TEST_FIXTURE(AbsDiffTest, initMatsRandU, <>, 0)
+GAPI_TEST_FIXTURE(AbsDiffCTest, initMatsRandU, <>, 0)
+GAPI_TEST_FIXTURE(SumTest, initMatrixRandU, FIXTURE_API(compare_scalar_f), 1, cmpF)
+GAPI_TEST_FIXTURE(AddWeightedTest, initMatsRandU, FIXTURE_API(compare_f), 1, cmpF)
+GAPI_TEST_FIXTURE(NormTest, initMatrixRandU, FIXTURE_API(compare_scalar_f,NormTypes), 2,
+    cmpF, opType)
+GAPI_TEST_FIXTURE(IntegralTest, initNothing, <>, 0)
+GAPI_TEST_FIXTURE(ThresholdTest, initMatrixRandU, FIXTURE_API(int), 1, tt)
+GAPI_TEST_FIXTURE(ThresholdOTTest, initMatrixRandU, FIXTURE_API(int), 1, tt)
+GAPI_TEST_FIXTURE(InRangeTest, initMatrixRandU, <>, 0)
+GAPI_TEST_FIXTURE(Split3Test, initMatrixRandU, <>, 0)
+GAPI_TEST_FIXTURE(Split4Test, initMatrixRandU, <>, 0)
+GAPI_TEST_FIXTURE(ResizeTest, initNothing, FIXTURE_API(compare_f,int,cv::Size), 3,
+    cmpF, interp, sz_out)
+GAPI_TEST_FIXTURE(ResizeTestFxFy, initNothing, FIXTURE_API(compare_f,int,double,double), 4,
+    cmpF, interp, fx, fy)
+GAPI_TEST_FIXTURE(Merge3Test, initMatsRandU, <>, 0)
+GAPI_TEST_FIXTURE(Merge4Test, initMatsRandU, <>, 0)
+GAPI_TEST_FIXTURE(RemapTest, initMatrixRandU, <>, 0)
+GAPI_TEST_FIXTURE(FlipTest, initMatrixRandU, FIXTURE_API(int), 1, flipCode)
+GAPI_TEST_FIXTURE(CropTest, initMatrixRandU, FIXTURE_API(cv::Rect), 1, rect_to)
+GAPI_TEST_FIXTURE(ConcatHorTest, initNothing, <>, 0)
+GAPI_TEST_FIXTURE(ConcatVertTest, initNothing, <>, 0)
+GAPI_TEST_FIXTURE(ConcatVertVecTest, initNothing, <>, 0)
+GAPI_TEST_FIXTURE(ConcatHorVecTest, initNothing, <>, 0)
+GAPI_TEST_FIXTURE(LUTTest, initNothing, <>, 0)
+GAPI_TEST_FIXTURE(ConvertToTest, initNothing, FIXTURE_API(compare_f, double, double), 3,
+    cmpF, alpha, beta)
+GAPI_TEST_FIXTURE(PhaseTest, initMatsRandU, FIXTURE_API(bool), 1, angle_in_degrees)
+GAPI_TEST_FIXTURE(SqrtTest, initMatrixRandU, <>, 0)
+GAPI_TEST_FIXTURE(NormalizeTest, initNothing, FIXTURE_API(compare_f,double,double,int,MatType), 5,
+    cmpF, a, b, norm_type, ddepth)
 } // opencv_test
 
 #endif //OPENCV_GAPI_CORE_TESTS_HPP
