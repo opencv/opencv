299775df2198b99df1c465229900fa850277f575
Hugo Lindström <hugo.lindstrom@trimble.se>
2019-08-01 15:28:04 +0200

    Support building shared libraries on WINCE.

--- platforms/wince/arm-wince-headless-overrides.cmake
+++ platforms/wince/arm-wince-headless-overrides.cmake
@@ -3,10 +3,10 @@ if(WINCE)
   # Try_Compile succeed and therefore also C/C++ ABI Detetection work
   # https://gitlab.kitware.com/cmake/cmake/blob/master/Modules/Platform/Windows-
   # MSVC.cmake
-  set(CMAKE_C_STANDARD_LIBRARIES_INIT "coredll.lib")
+  set(CMAKE_C_STANDARD_LIBRARIES_INIT "coredll.lib oldnames.lib")
   set(CMAKE_CXX_STANDARD_LIBRARIES_INIT ${CMAKE_C_STANDARD_LIBRARIES_INIT})
   foreach(ID EXE SHARED MODULE)
     string(APPEND CMAKE_${ID}_LINKER_FLAGS_INIT
-           " /NODEFAULTLIB:libc.lib /NODEFAULTLIB:oldnames.lib")
+           " /NODEFAULTLIB:libc.lib")
   endforeach()
 endif()
