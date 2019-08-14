0ffc1fa484bd0315a2ffedf4e3a89f92678565cd
Yashas <yashas_2010@yahoo.com>
2019-08-14 07:00:33 +0530

    vectorize fill kernel

--- modules/dnn/src/cuda/fill.cu
+++ modules/dnn/src/cuda/fill.cu
@@ -7,6 +7,7 @@
 
 #include "grid_stride_range.hpp"
 #include "execution.hpp"
+#include "vector_traits.hpp"
 
 #include "../cuda4dnn/csl/stream.hpp"
 #include "../cuda4dnn/csl/span.hpp"
@@ -17,18 +18,38 @@ using namespace cv::dnn::cuda4dnn::csl::device;
 namespace cv { namespace dnn { namespace cuda4dnn { namespace kernels {
 
     namespace raw {
-        template <class T>
-        __global__ void fill(span<T> output, T value) {
-            for (auto i : grid_stride_range(output.size()))
-                output[i] = value;
+        template <class T, std::size_t N>
+        __global__ void fill_vec(span<T> output, T value) {
+            using vector_type = get_vector_type_t<T, N>;
+
+            auto output_vPtr = vector_type::get_pointer(output.data());
+            for (auto i : grid_stride_range(output.size() / vector_type::size())) {
+                vector_type vec;
+                for (int j = 0; j < vector_type::size(); j++)
+                    vec.data[j] = value;
+                v_store(output_vPtr[i], vec);
+            }
         }
+    }
+
+    template <class T, std::size_t N>
+    void launch_vectorized_fill(const Stream& stream, span<T> output, T value) {
+        CV_Assert(is_fully_aligned<T>(output, N));
+
+        auto kernel = raw::fill_vec<T, N>;
+        auto policy = make_policy(kernel, output.size() / N, 0, stream);
+        launch_kernel(kernel, policy, output, value);
     }
 
     template <class T>
     void fill(const Stream& stream, span<T> output, T value) {
-        auto kernel = raw::fill<T>;
-        auto policy = make_policy(kernel, output.size(), 0, stream);
-        launch_kernel(kernel, policy, output, value);
+        if (is_fully_aligned<T>(output, 4)) {
+            launch_vectorized_fill<T, 4>(stream, output, value);
+        } else if (is_fully_aligned<T>(output, 2)) {
+            launch_vectorized_fill<T, 2>(stream, output, value);
+        } else {
+            launch_vectorized_fill<T, 1>(stream, output, value);
+        }
     }
 
     template void fill(const Stream&, span<__half>, __half);
