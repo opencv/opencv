27b804b61995283e6c9713273c1bfe20f64bb115
APrigarina <ann73617@gmail.com>
2019-08-29 14:08:32 +0300

    resize improvement

--- modules/objdetect/src/qrcode.cpp
+++ modules/objdetect/src/qrcode.cpp
@@ -5,13 +5,6 @@
 // Copyright (C) 2018, Intel Corporation, all rights reserved.
 // Third party copyrights are property of their respective owners.
 
-// This file is part of OpenCV project.
-// It is subject to the license terms in the LICENSE file found in the top-level directory
-// of this distribution and at http://opencv.org/license.html.
-//
-// Copyright (C) 2018, Intel Corporation, all rights reserved.
-// Third party copyrights are property of their respective owners.
-
 #include "precomp.hpp"
 #include "opencv2/objdetect.hpp"
 #include "opencv2/calib3d.hpp"
