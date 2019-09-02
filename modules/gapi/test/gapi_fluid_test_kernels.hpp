8a7e74673e42e75d5cd7d7a3e50126dc59cb2de7
Alexey Smirnov <alexey.smirnov@intel.com>
2019-09-02 15:51:58 +0300

    Merge pull request #15312 from smirnov-alexey:gapi_fluid_garray
    * Support GArray as input in fluid kernels
    
    * Create tests on GArray input in fluid
    
    * Some fixes to fully support GArray
    
    * Refactor code and change the kernel according to review
    
    * Add histogram calculation as a G-API kernel
    
    Add assert that input GArgs in fluid contain at least one GMat

--- modules/gapi/test/gapi_fluid_test_kernels.hpp
+++ modules/gapi/test/gapi_fluid_test_kernels.hpp
@@ -15,6 +15,7 @@ namespace cv
 namespace gapi_test_kernels
 {
 using cv::gapi::core::GMat3;
+using GMat2 = std::tuple<GMat, GMat>;
 
 G_TYPED_KERNEL(TAddSimple, <GMat(GMat, GMat)>, "test.fluid.add_simple") {
     static cv::GMatDesc outMeta(cv::GMatDesc a, cv::GMatDesc) {
@@ -109,6 +110,20 @@ G_TYPED_KERNEL_M(TSplit3_4lpi, <GMat3(GMat)>, "test.fluid.split3_4lpi") {
         const auto out_depth = in.depth;
         const auto out_desc  = in.withType(out_depth, 1);
         return std::make_tuple(out_desc, out_desc, out_desc);
+    }
+};
+
+G_TYPED_KERNEL(TEqualizeHist, <GMat(GMat, GArray<int>)>, "test.fluid.equalize_hist")
+{
+    static GMatDesc outMeta(GMatDesc in, const cv::GArrayDesc&) {
+        return in;
+    }
+};
+
+G_TYPED_KERNEL(TCalcHist, <GArray<int>(GMat)>, "test.ocv.calc_hist")
+{
+    static GArrayDesc outMeta(GMatDesc) {
+        return {};
     }
 };
 
