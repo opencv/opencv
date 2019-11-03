53139e6ebe5cb76adddb9bf08590c878dc6a10e3
Oleg Alexandrov <oleg.alexandrov@gmail.com>
2019-11-03 05:37:25 -0800

    Merge pull request #15838 from oleg-alexandrov:patch-2
    Correct stereoRectify documentation

--- modules/calib3d/include/opencv2/calib3d.hpp
+++ modules/calib3d/include/opencv2/calib3d.hpp
@@ -1543,8 +1543,8 @@ CV_EXPORTS_W double stereoCalibrate( InputArrayOfArrays objectPoints,
 @param cameraMatrix2 Second camera matrix.
 @param distCoeffs2 Second camera distortion parameters.
 @param imageSize Size of the image used for stereo calibration.
-@param R Rotation matrix from the coordinate system of the second camera to the first.
-@param T Translation vector from the coordinate system of the second camera to the first.
+@param R Rotation matrix from the coordinate system of the first camera to the second.
+@param T Translation vector from the coordinate system of the first camera to the second.
 @param R1 Output 3x3 rectification transform (rotation matrix) for the first camera.
 @param R2 Output 3x3 rectification transform (rotation matrix) for the second camera.
 @param P1 Output 3x4 projection matrix in the new (rectified) coordinate systems for the first
