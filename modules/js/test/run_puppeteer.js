6a0eddea228a83be14200b7dbf6ae6fabf23fd23
Alexander Alekhin <alexander.a.alekhin@gmail.com>
2019-11-09 02:11:45 +0000

    js: update OpenCV.js tests infrastructure

--- modules/js/test/run_puppeteer.js
+++ modules/js/test/run_puppeteer.js
@@ -4,7 +4,19 @@ const path = require("path");
 const fs = require("fs");
 const http = require("http");
 
-main(require('minimist')(process.argv.slice(2)));
+run_main(require('minimist')(process.argv.slice(2)));
+
+async function run_main(o = {}) {
+  try {
+    await main(o);
+    console.magenta("FATAL: Unexpected exit!");
+    process.exit(1);
+  } catch (e) {
+    console.error(colors.magenta("FATAL: Unexpected exception!"));
+    console.error(e);
+    process.exit(1);
+  }
+}
 
 async function main(o = {}) {
   o = Object.assign({}, {
@@ -14,11 +26,14 @@ async function main(o = {}) {
     noHeadless: false,
     serverPrefix: `http://localhost`,
     noExit: false,
-    screenshot: false,
+    screenshot: undefined,
     help: false,
     noTryCatch: false,
     maxBlockDuration: 30000
   }, o)
+  if (typeof o.screenshot == 'string' && o.screenshot == 'false') {
+    console.log(colors.red('ERROR: misused screenshot option, use --no-screenshot instead'));
+  }
   if (o.noExit) {
     o.maxBlockDuration = 999999999
   }
@@ -29,7 +44,7 @@ async function main(o = {}) {
   const serverAddress = `${o.serverPrefix}:${o.port}`
   const url = `${serverAddress}/tests.html${o.noTryCatch ? '?notrycatch=1' : ''}`;
   if (!fs.existsSync(o.buildFolder)) {
-    console.error(`Expected folder "${o.buildFolder} to exists. Aborting`);
+    console.error(`Expected folder "${o.buildFolder}" to exists. Aborting`);
   }
   o.debug && debug('Server Listening at ' + url);
   const server = await staticServer(o.buildFolder, o.port, m => debug, m => error);
@@ -37,11 +52,12 @@ async function main(o = {}) {
   const browser = await puppeteer.launch({ headless: !o.noHeadless });
   const page = await browser.newPage();
   page.on('console', e => {
+    locationMsg = formatMessage(`${e.location().url}:${e.location().lineNumber}:${e.location().columnNumber}`);
     if (e.type() === 'error') {
-      console.log('error: ', e.location(), e.text().split('\n').map(l => l.replace(serverAddress, o.buildFolder)).join('\n'))
+      console.log(colors.red(formatMessage('' + e.text(), `-- ERROR:${locationMsg}: `, )));
     }
     else if (o.debug) {
-      o.debug && console.log('log: ', e.location(), e.text())
+      o.debug && console.log(colors.grey(formatMessage('' + e.text(), `-- ${locationMsg}: `)));
     }
   });
   o.debug && debug(`Opening page address ${url}`);
@@ -51,14 +67,21 @@ async function main(o = {}) {
   if (!text) {
     return await fail(`An error occurred extracting test results. Check the build folder ${o.buildFolder} is correct and has build with tests enabled.`);
   }
-  o.debug && debug(await getText('#qunit-userAgent'));
+  o.debug && debug(colors.blackBright("* UserAgent: " + await getText('#qunit-userAgent')));
   const testFailed = !text.includes('0 failed');
+  if (testFailed && !o.debug) {
+    process.stdout.write(colors.grey("* Use '--debug' parameter to see details of failed tests.\n"));
+  }
+  if (o.screenshot || (o.screenshot === undefined && testFailed)) {
+    await page.screenshot({ path: 'screenshot.png', fullPage: 'true' });
+    process.stdout.write(colors.grey(`* Screenshot taken: ${o.buildFolder}/screenshot.png\n`));
+  }
   if (testFailed) {
     const report = await failReport();
     process.stdout.write(`
 ${colors.red.bold.underline('Failed tests ! :(')}
 
-${colors.redBright(colors.symbols.cross + ' ' + report.join(`\n ${colors.symbols.cross} `))}
+${colors.redBright(colors.symbols.cross + ' ' + report.join(`\n${colors.symbols.cross} `))}
 
 ${colors.redBright(`=== Summary ===\n${text}`)}
 `);
@@ -75,7 +98,6 @@ ${colors.redBright(`=== Summary ===\n${text}`)}
       await new Promise(r => setTimeout(r, 5000));
     }
   }
-  o.screenshot && await page.screenshot({ path: 'screenshot.png' });
   await server && server.close();
   await browser.close();
   process.exit(testFailed ? 1 : 0);
@@ -86,25 +108,31 @@ ${colors.redBright(`=== Summary ===\n${text}`)}
   async function failReport() {
     const failures = await page.evaluate(() => Array.from(document.querySelectorAll('#qunit-tests .fail')).filter(e => e.querySelector('.module-name')).map(e => ({
       moduleName: e.querySelector('.module-name') && e.querySelector('.module-name').textContent,
-      textName: e.querySelector('.test-name') && e.querySelector('.test-name').textContent,
+      testName: e.querySelector('.test-name') && e.querySelector('.test-name').textContent,
       expected: e.querySelector('.test-expected pre') && e.querySelector('.test-expected pre').textContent,
       actual: e.querySelector('.test-actual pre') && e.querySelector('.test-actual pre').textContent,
-      code: e.querySelector('.qunit-source') && e.querySelector('.qunit-source').textContent,
+      code: e.querySelector('.test-source') && e.querySelector('.test-source').textContent.replace("Source:     at ", ""),
     })));
-    return failures.map(f => `${f.moduleName}:`);
+    return failures.map(f => `${f.moduleName}: ${f.testName} (${formatMessage(f.code)})`);
   }
   async function fail(s) {
     await failReport();
-    o.screenshot && await page.screenshot({ path: 'screenshot.png' });
-    process.stdout.write(colors.grey(s + `
- * Screenshot taken: ${o.buildFolder}/tmp_screenshot.png\n`));
+    process.stdout.write(colors.red(s) + '\n');
+    if (o.screenshot || o.screenshot === undefined) {
+      await page.screenshot({ path: 'screenshot.png', fullPage: 'true' });
+      process.stdout.write(colors.grey(`* Screenshot taken: ${o.buildFolder}/screenshot.png\n`));
+    }
     process.exit(1);
   }
   async function debug(s) {
-    process.stdout.write(colors.blackBright(s + '\n'));
+    process.stdout.write(s + '\n');
   }
   async function error(s) {
-    process.stdout.write(colors.redBright(s + '\n'));
+    process.stdout.write(s + '\n');
+  }
+  function formatMessage(message, prefix) {
+    prefix = prefix || '';
+    return prefix + message.split('\n').map(l => l.replace(serverAddress, o.buildFolder)).join('\n' + prefix);
   }
 }
 
@@ -113,11 +141,11 @@ function printHelpAndExit() {
   console.log(`
 Usage:
 
-  # First, remember to build opencv.s with tests enabled:
+  # First, remember to build opencv.js with tests enabled:
   ${colors.blueBright(`python ./platforms/js/build_js.py build_js --build_test`)}
 
   # Install the tool locally (needed only once) and run it
-  ${colors.blueBright(`cd build/bin`)}
+  ${colors.blueBright(`cd build_js/bin`)}
   ${colors.blueBright(`npm install`)}
   ${colors.blueBright(`node run_puppeteer`)}
 
@@ -125,7 +153,7 @@ By default will run a headless browser silently printing a small report in the t
 But it could used to debug the tests in the browser, take screenshots, global tool or
 targeting external servers exposing the tests.
 
-TIP: you could install the tool globally (npm install --global build/bin) to execute it from any local folder.
+TIP: you could install the tool globally (npm install --global build_js/bin) to execute it from any local folder.
 
 # Options
 
@@ -135,7 +163,7 @@ TIP: you could install the tool globally (npm install --global build/bin) to exe
  * noHeadless?: boolean. Default false
  * serverPrefix?: string . Default http://localhost
  * help?: boolean
- * screenshot?: boolean
+ * screenshot?: boolean . Make screenshot on failure by default. Use --no-screenshot to disable screenshots completely.
  * noExit?: boolean default false. If true it will keep running the server - together with noHeadless you can debug in the browser.
  * noTryCatch?: boolean will disable Qunit tryCatch - so exceptions are dump to stdout rather than in the browser.
  * maxBlockDuration: QUnit timeout . if noExist then is infinity.
