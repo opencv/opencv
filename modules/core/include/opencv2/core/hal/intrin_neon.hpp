2f85bb89217d0106b09a47c436307a8b55161ba2
Tomoaki Teshima <tomoaki.teshima@gmail.com>
2019-12-13 17:46:11 +0900

    fix missing bracket

--- modules/core/include/opencv2/core/hal/intrin_neon.hpp
+++ modules/core/include/opencv2/core/hal/intrin_neon.hpp
@@ -1209,7 +1209,7 @@ inline _Tpvec v_load(const _Tp* ptr) \
 inline _Tpvec v_load_aligned(const _Tp* ptr) \
 { return _Tpvec(vreinterpretq_##suffix##_u8(vld1q_u8((const unsigned char*)ptr))); } \
 inline _Tpvec v_load_low(const _Tp* ptr) \
-{ return _Tpvec(vreinterpretq_##suffix##_u8(vcombine_u8(vld1_u8((const unsigned char*)ptr), vdup_n_u8((_Tp)0))); } \
+{ return _Tpvec(vreinterpretq_##suffix##_u8(vcombine_u8(vld1_u8((const unsigned char*)ptr), vdup_n_u8((_Tp)0)))); } \
 inline _Tpvec v_load_halves(const _Tp* ptr0, const _Tp* ptr1) \
 { return _Tpvec(vreinterpretq_##suffix##_u8(vcombine_u8(vld1_u8((const unsigned char*)ptr0), vld1_u8((const unsigned char*)ptr1)))); } \
 inline void v_store(_Tp* ptr, const _Tpvec& a) \
