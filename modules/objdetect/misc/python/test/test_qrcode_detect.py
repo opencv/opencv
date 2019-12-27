837791a32c860ff10d204dbb6684d097e3b17589
olramde <rladhkstn8@gmail.com>
2019-12-27 10:05:53 +0900

    Remove '/' from PATH

--- modules/objdetect/misc/python/test/test_qrcode_detect.py
+++ modules/objdetect/misc/python/test/test_qrcode_detect.py
@@ -12,7 +12,7 @@ from tests_common import NewOpenCVTests
 
 class qrcode_detector_test(NewOpenCVTests):
     def test_detect_and_decode(self):
-        img = cv.imread(os.path.join(self.extraTestDataPath, '/cv/qrcode/link_ocv.jpg'))
+        img = cv.imread(os.path.join(self.extraTestDataPath, 'cv/qrcode/link_ocv.jpg'))
         detector = cv.QRCodeDetector()
         retval, points, straight_qrcode = detector.detectAndDecode(img)
         self.assertEqual(retval, "https://opencv.org/");
