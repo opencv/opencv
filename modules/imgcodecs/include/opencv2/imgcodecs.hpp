3cc07a4411643c86ef3e1e7d4fb3bb332ed8e7c0
Collin Brake <collin@bec-systems.com>
2020-02-06 17:49:14 -0500

    improved documentation for IMREAD_UNCHANGED flag

--- modules/imgcodecs/include/opencv2/imgcodecs.hpp
+++ modules/imgcodecs/include/opencv2/imgcodecs.hpp
@@ -62,7 +62,7 @@ namespace cv
 
 //! Imread flags
 enum ImreadModes {
-       IMREAD_UNCHANGED            = -1, //!< If set, return the loaded image as is (with alpha channel, otherwise it gets cropped).
+       IMREAD_UNCHANGED            = -1, //!< If set, return the loaded image as is (with alpha channel, otherwise it gets cropped). Ignore EXIF orientation.
        IMREAD_GRAYSCALE            = 0,  //!< If set, always convert image to the single channel grayscale image (codec internal conversion).
        IMREAD_COLOR                = 1,  //!< If set, always convert image to the 3 channel BGR color image.
        IMREAD_ANYDEPTH             = 2,  //!< If set, return 16-bit/32-bit image when the input has the corresponding depth, otherwise convert it to 8-bit.
