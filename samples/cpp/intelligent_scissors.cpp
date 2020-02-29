6966d3a15ab5f49d39ded65022b19b0be4a52545
Dizhenin Vladislav <vdizhenin@gmail.com>
2020-02-29 12:12:11 +0300

    Re onMouse

--- samples/cpp/intelligent_scissors.cpp
+++ samples/cpp/intelligent_scissors.cpp
@@ -3,18 +3,17 @@
 #include <string>
 #include <vector>
 #include <queue>
-#include <opencv2/opencv.hpp>
 #include <opencv2/core/core.hpp>
 #include <opencv2/highgui/highgui.hpp>
 #include <opencv2/imgproc/imgproc.hpp>
 
 using namespace cv;
 
-bool cont;
-Mat img, img_pre_draw;
+Mat img, img_pre_render, img_render;
 Point end;
+std::vector<std::vector<Point> > contours(1);
+std::vector<Point> tmp_contour; 
 
-Scalar colored_line(255,0,0);
 
 Mat zero_crossing, gradient_magnitude, Ix, Iy, hit_map_x, hit_map_y;
 
@@ -28,15 +27,6 @@ struct Pix
         return value > b.value;
     }
 };
-
-void grad_mag()
-{
-    double max_val = 0.0;
-    gradient_magnitude.create(img.rows, img.cols, CV_64FC1);
-    magnitude(Iy, Ix, gradient_magnitude);
-    minMaxLoc(gradient_magnitude, 0, &max_val);
-    gradient_magnitude = 1.0 - gradient_magnitude / max_val;
-}
 
 double local_cost(const Point& p, const Point& q, bool diag)
 {
@@ -136,31 +126,43 @@ void onMouse(int event, int x, int y, int flags, void *param)
     if (event == EVENT_LBUTTONDOWN)
     {
         end = Point(x, y);
-        cont = true;
+        if (!contours.back().empty())
+        {
+            for(int i = tmp_contour.size() - 1; i >= 0; i--)
+            {
+                contours.back().push_back(tmp_contour[i]);
+            }
+            tmp_contour.clear();
+        }
+        else
+        {
+            contours.back().push_back(end);
+        }
         find_min_path(end);
-        img.copyTo(img_pre_draw);
-        imshow("lasso", img);
+
+        img_render.copyTo(img_pre_render);
+        imshow("lasso", img_render);    
     }
-    else
-     if (event == EVENT_RBUTTONDOWN)
-       {
-        cont = false;
-        img_pre_draw.copyTo(img);
-        imshow("lasso", img);
-       }
-    else
-       if (event == EVENT_MOUSEMOVE && cont)
-       {
-          img_pre_draw.copyTo(img);
-          Point val_point = Point(x, y);
-          while (val_point != end)
-          {
-              Point  cur = Point(hit_map_x.at<int>(val_point), hit_map_y.at<int>(val_point));
-              line(img, val_point, cur, colored_line, 2);
-              val_point = cur;
-          }
-          imshow("lasso", img);
-       }
+    else if (event == EVENT_RBUTTONDOWN)
+    {
+        img_pre_render.copyTo(img_render);
+        contours.resize(contours.size() + 1); 
+        imshow("lasso", img_render);
+    }
+    else if (event == EVENT_MOUSEMOVE && !contours.back().empty())
+    {
+        tmp_contour.clear();
+        img_pre_render.copyTo(img_render);
+        Point val_point = Point(x, y);
+        while (val_point != end)
+        {
+            tmp_contour.push_back(val_point);
+            Point cur = Point(hit_map_x.at<int>(val_point), hit_map_y.at<int>(val_point));
+            line(img_render, val_point, cur, Scalar(255, 0, 0), 2);
+            val_point = cur;
+        }
+        imshow("lasso", img_render);
+    }
 }
 
 const char* keys =
@@ -171,24 +173,33 @@ const char* keys =
 
 int main( int argc, const char** argv )
 {
-    Mat grayscale;
-    Mat img_canny;
-    Mat test_sobel;
     CommandLineParser parser(argc, argv, keys);
-    std ::  string filename = parser.get<std :: string>(0);
-    img = imread(samples::findFile(filename), IMREAD_COLOR);
-    namedWindow("lasso");
+    std::string filename = parser.get<std::string>(0);
+
+    Mat grayscale, img_canny;
+    img = imread(samples::findFile(filename));
     hit_map_x.create(img.rows, img.cols, CV_32SC1);
     hit_map_y.create(img.rows, img.cols, CV_32SC1);
+
     cvtColor(img, grayscale, COLOR_BGR2GRAY);
     Canny(grayscale, img_canny, 50, 100);
+
     threshold(img_canny, zero_crossing, 254, 1, THRESH_BINARY_INV);
     Sobel(grayscale, Ix, CV_64FC1, 1, 0, 1);
     Sobel(grayscale, Iy, CV_64FC1, 0, 1, 1);
     Ix = Ix / 255.0;
     Iy = Iy / 255.0;
-    grad_mag();
-    img.copyTo(img_pre_draw);
+
+    // Compute gradients magnitude.
+    double max_val = 0.0;
+    magnitude(Iy, Ix, gradient_magnitude); 
+    minMaxLoc(gradient_magnitude, 0, &max_val);
+    gradient_magnitude = 1.0 - gradient_magnitude / max_val;
+
+    img.copyTo(img_pre_render);
+    img.copyTo(img_render);
+
+    namedWindow("lasso");
     setMouseCallback("lasso", onMouse, 0);
     imshow("lasso", img);
     waitKey(0);
