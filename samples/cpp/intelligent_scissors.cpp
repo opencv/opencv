510fe6054add397f3a6aeb9d9120ad95764fb821
Dizhenin Vladislav <vdizhenin@gmail.com>
2020-03-06 18:08:30 +0300

    double to float

--- samples/cpp/intelligent_scissors.cpp
+++ samples/cpp/intelligent_scissors.cpp
@@ -52,8 +52,8 @@ static float local_cost(const Point& p, const Point& q)
     {
         fG /= sqrtf(2);
     }
-    return  0.43 * zero_crossing.at<uchar>(q) + 0.43 * (acos(dp)
-            + acos(dq)) / CV_PI + 0.14 * fG;
+    return  0.43f * zero_crossing.at<uchar>(q) + 0.43f * (acosf(dp)
+            + acosf(dq)) / CV_PI + 0.14f * fG;
 }
 
 static void find_min_path(const Point& start)
