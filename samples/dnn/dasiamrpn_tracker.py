01c0e52bad0323d5336f511fbd58a1c96de81ee1
ilbalance <ilbalance476@gmail.com>
2020-03-11 18:06:24 +0300

    fix case with video

--- samples/dnn/dasiamrpn_tracker.py
+++ samples/dnn/dasiamrpn_tracker.py
@@ -10,6 +10,7 @@ import cv2 as cv
 import argparse
 
 class DaSiamRPNTracker:
+    #initialization of used values, initial bounding box, used network
     def __init__(self, im, target_pos, target_sz, net, kernel_r1, kernel_cls1):
         self.windowing = "cosine"
         self.exemplar_size = 127
@@ -35,26 +36,20 @@ class DaSiamRPNTracker:
              raise AssertionError("Initializing BB is too small-try to restart tracker with larger BB")
 
         self.anchor = self.__generate_anchor()
-
         wc_z = self.target_sz[0] + self.context_amount * sum(self.target_sz)
         hc_z = self.target_sz[1] + self.context_amount * sum(self.target_sz)
         s_z = round(np.sqrt(wc_z * hc_z))
 
         z_crop = self.__get_subwindow_tracking(im, self.exemplar_size, s_z)
         z_crop = z_crop.transpose(2, 0, 1).reshape(1, 3, 127, 127).astype(np.float32)
-
         self.net.setInput(z_crop)
         z_f = self.net.forward('63')
-
         kernel_r1.setInput(z_f)
         r1 = kernel_r1.forward()
-
         kernel_cls1.setInput(z_f)
         cls1 = kernel_cls1.forward()
-
         r1 = r1.reshape(20, 256, 4, 4)
         cls1 = cls1.reshape(10, 256 , 4, 4)
-
         self.net.setParam(self.net.getLayerId('65'), 0, r1)
         self.net.setParam(self.net.getLayerId('68'), 0, cls1)
 
@@ -64,11 +59,11 @@ class DaSiamRPNTracker:
             self.window = np.ones((self.score_size, self.score_size))
         self.window = np.tile(self.window.flatten(), self.anchor_num)
 
+    #creating anchor for tracking bounding box
     def __generate_anchor(self):
         self.anchor = np.zeros((self.anchor_num, 4),  dtype = np.float32)
         size = self.total_stride * self.total_stride
         count = 0
-
         for ratio in self.ratios:
             ws = int(np.sqrt(size / ratio))
             hs = int(ws * ratio)
@@ -86,14 +81,12 @@ class DaSiamRPNTracker:
         xx, yy = np.meshgrid([ori + self.total_stride * dx for dx in range(score_sz)], [ori + self.total_stride * dy for dy in range(score_sz)])
         xx, yy = np.tile(xx.flatten(), (self.anchor_num, 1)).flatten(), np.tile(yy.flatten(), (self.anchor_num, 1)).flatten()
         self.anchor[:, 0], self.anchor[:, 1] = xx.astype(np.float32), yy.astype(np.float32)
-
         return self.anchor
 
-#track function
+    #track function
     def track(self, im):
         wc_z = self.target_sz[1] + self.context_amount * sum(self.target_sz)
         hc_z = self.target_sz[0] + self.context_amount * sum(self.target_sz)
-
         s_z = np.sqrt(wc_z * hc_z)
         scale_z = self.exemplar_size / s_z
         d_search = (self.instance_size - self.exemplar_size) / 2
@@ -102,31 +95,26 @@ class DaSiamRPNTracker:
         #region preprocessing
         x_crop = self.__get_subwindow_tracking(im, self.instance_size, s_x)
         x_crop = x_crop.transpose(2, 0, 1).reshape(1, 3, 271, 271).astype(np.float32)
-
         self.score = self.__tracker_eval(x_crop, scale_z)
-
         self.target_pos[0] = max(0, min(self.im_w, self.target_pos[0]))
         self.target_pos[1] = max(0, min(self.im_h, self.target_pos[1]))
         self.target_sz[0] = max(10, min(self.im_w, self.target_sz[0]))
         self.target_sz[1] = max(10, min(self.im_h, self.target_sz[1]))
 
-    #evaluations with cropped images
+    #update bounding box position
     def __tracker_eval(self, x_crop, scale_z):
         target_size = self.target_sz * scale_z
         self.net.setInput(x_crop)
         outNames = self.net.getUnconnectedOutLayersNames()
         outNames = ['66', '68']
         delta, score = self.net.forward(outNames)
-
         delta = np.transpose(delta, (1, 2, 3, 0))
         delta = np.ascontiguousarray(delta, dtype = np.float32)
         delta = np.reshape(delta, (4, -1))
-
         score = np.transpose(score, (1, 2, 3, 0))
         score = np.ascontiguousarray(score, dtype = np.float32)
         score = np.reshape(score, (2, -1))
         score = self.__softmax(score)[1, :]
-
         delta[0, :] = delta[0, :] * self.anchor[:, 2] + self.anchor[:, 0]
         delta[1, :] = delta[1, :] * self.anchor[:, 3] + self.anchor[:, 1]
         delta[2, :] = np.exp(delta[2, :]) * self.anchor[:, 2]
@@ -147,23 +135,17 @@ class DaSiamRPNTracker:
 
         s_c = __change(__sz(delta[2, :], delta[3, :]) / (__sz_wh(target_size)))
         r_c = __change((target_size[0] / target_size[1]) / (delta[2, :] / delta[3, :]))
-
         penalty = np.exp(-(r_c * s_c - 1.) * self.penalty_k)
         pscore = penalty * score
-
         pscore = pscore * (1 - self.window_influence) + self.window * self.window_influence
         best_pscore_id = np.argmax(pscore)
-
         target = delta[:, best_pscore_id] / scale_z
         target_size = target_size / scale_z
         lr = penalty[best_pscore_id] * score[best_pscore_id] * self.lr
-
         res_x = target[0] + self.target_pos[0]
         res_y = target[1] + self.target_pos[1]
-
         res_w = target_size[0] * (1 - lr) + target[2] * lr
         res_h = target_size[1] * (1 - lr) + target[3] * lr
-
         self.target_pos = np.array([res_x, res_y])
         self.target_sz = np.array([res_w, res_h])
 
@@ -175,20 +157,18 @@ class DaSiamRPNTracker:
         y = e_x / e_x.sum(axis = 0)
         return y
 
+    #evaluations with cropped image
     def __get_subwindow_tracking(self, im, model_size, original_sz):
         im_sz = im.shape
         c = (original_sz + 1) / 2
-
         context_xmin = round(self.target_pos[0] - c)
         context_xmax = context_xmin + original_sz - 1
         context_ymin = round(self.target_pos[1] - c)
         context_ymax = context_ymin + original_sz - 1
-
         left_pad = int(max(0., -context_xmin))
         top_pad = int(max(0., -context_ymin))
         right_pad = int(max(0., context_xmax - im_sz[1] + 1))
         bottom_pad = int(max(0., context_ymax - im_sz[0] + 1))
-
         context_xmin = context_xmin + left_pad
         context_xmax = context_xmax + left_pad
         context_ymin = context_ymin + top_pad
@@ -217,14 +197,14 @@ class DaSiamRPNTracker:
 
         return im_patch.astype(float)
 
+#function for reading paths, bounding box drawing, showing results
 def main():
     parser = argparse.ArgumentParser(description = "Run tracker")
     parser.add_argument("--net", type = str, help = "Full path to onnx model of net")
     parser.add_argument("--kernel_r1", type = str, help = "Full path to onnx model of kernel_r1")
     parser.add_argument("--kernel_cls1", type = str, help = "Full path to onnx model of kernel_cls1")
-    parser.add_argument("--input", type = str, default = " ", help = "Full path to input. Do not use if input is camera")
+    parser.add_argument("--input", type = str, help = "Full path to input. Do not use if input is camera")
     args = parser.parse_args()
-
     point1 = ()
     point2 = ()
     mark = True
@@ -249,14 +229,13 @@ def main():
             w = abs(point1[0] - point2[0])
             h = abs(point1[1] - point2[1])
             mark = False
-
+    #loading network`s and kernel`s models
     net = cv.dnn.readNet(args.net)
     kernel_r1 = cv.dnn.readNet(args.kernel_r1)
     kernel_cls1 = cv.dnn.readNet(args.kernel_cls1)
-
-    if args.input == " ":
+    #case with camera
+    if args.input is None:
         cap = cv.VideoCapture(0)
-        # cap = cv.VideoCapture(0, cv.CAP_V4L2)
         cap.set(cv.CAP_PROP_FRAME_HEIGHT, 480)
         cap.set(cv.CAP_PROP_FRAME_WIDTH, 640)
         cv.namedWindow("DaSiamRPN")
@@ -267,7 +246,7 @@ def main():
                 cv.rectangle(frame, point1, point2, (0, 255, 255), 3)
             cv.imshow("DaSiamRPN", frame)
             cv.waitKey(1)
-
+    #case with video
     else:
         cap = cv.VideoCapture(args.input)
         cap.set(cv.CAP_PROP_FRAME_HEIGHT, 480)
@@ -275,33 +254,23 @@ def main():
         cv.namedWindow("DaSiamRPN")
         cv.setMouseCallback("DaSiamRPN", get_bb)
         ref, frame = cap.read()
-        # while mark == True:
-        while True:
-            # ref, frame = cap.read()
-            if point1 and point2:
-                cv.rectangle(frame, point1, point2, (0, 255, 255), 3)
+        while mark == True:
             cv.imshow("DaSiamRPN", frame)
             key = cv.waitKey(1)
-            if key == ord("q"):
-                break
-
+    #initializing bounding box
     target_pos, target_sz = np.array([cx, cy]), np.array([w, h])
     tracker = DaSiamRPNTracker(frame, target_pos, target_sz, net, kernel_r1, kernel_cls1)
-
+    #tracking loop
     while (cap.isOpened):
-        ret,frame = cap.read()
+        ret, frame = cap.read()
         tracker.track(frame)
-
         w, h = tracker.target_sz
         cx, cy = tracker.target_pos
-
         cv.rectangle(frame, (int(cx - w // 2), int(cy - h // 2)), (int(cx - w // 2) + int(w), int(cy - h // 2) + int(h)),(0, 255, 255), 3)
-
         cv.imshow("DaSiamRPN", frame)
         key = cv.waitKey(1)
         if key == ord("q"):
             break
-
     cap.release()
     cv.destroyAllWindows()
 
