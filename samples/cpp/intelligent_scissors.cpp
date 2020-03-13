b88bda47c5a70203a8a51497165409b37788c6a6
Dizhenin Vladislav <vdizhenin@gmail.com>
2020-03-13 14:02:37 +0300

    Change name

--- samples/cpp/intelligent_scissors.cpp
+++ samples/cpp/intelligent_scissors.cpp
@@ -19,13 +19,13 @@ struct Pix
     }
 };
 
-struct Calculated_parameters
+struct Parameters
 {
-        Mat img, img_pre_render, img_render;
-        Point end;
-        std::vector<std::vector<Point> > contours;
-        std::vector<Point> tmp_contour;
-        Mat zero_crossing, gradient_magnitude, Ix, Iy, hit_map_x, hit_map_y;
+    Mat img, img_pre_render, img_render;
+    Point end;
+    std::vector<std::vector<Point> > contours;
+    std::vector<Point> tmp_contour;
+    Mat zero_crossing, gradient_magnitude, Ix, Iy, hit_map_x, hit_map_y;
 };
 
 
@@ -34,6 +34,9 @@ static float local_cost(const Point& p, const Point& q, const Mat& gradient_magn
     float fG = gradient_magnitude.at<float>(q.y, q.x);
     float dp;
     float dq;
+    float WEIGHT_LAP_ZERO_CROSS = 0.43;
+    float WEIGHT_GRADIENT_MAGNITUDE = 0.14;
+    float WEIGHT_GRADIENT_DIRECTION = 0.43;
     bool isDiag = (p.x != q.x) && (p.y != q.y);
 
     if ((Iy.at<float>(p) * (q.x - p.x) - Ix.at<float>(p) * (q.y - p.y)) >= 0)
@@ -55,17 +58,18 @@ static float local_cost(const Point& p, const Point& q, const Mat& gradient_magn
     {
         fG /= sqrtf(2);
     }
-    return  0.43f * zero_crossing.at<uchar>(q) + 0.43f * (acosf(dp)
-            + acosf(dq)) / static_cast<float>(CV_PI) + 0.14f * fG;
+    return  WEIGHT_LAP_ZERO_CROSS * zero_crossing.at<uchar>(q) +
+            WEIGHT_GRADIENT_DIRECTION * (acosf(dp) + acosf(dq)) / static_cast<float>(CV_PI) +
+            WEIGHT_GRADIENT_MAGNITUDE * fG;
 }
 
-static void find_min_path(const Point& start, Calculated_parameters* tmp)
+static void find_min_path(const Point& start, Parameters* param)
 {
     Pix begin;
-    Mat cost_map(tmp->img.size(), CV_32F, Scalar(FLT_MAX));
-    Mat expand(tmp->img.size(), CV_8UC1, Scalar(0));
-    Mat processed(tmp->img.size(), CV_8UC1, Scalar(0));
-    Mat removed(tmp->img.size(), CV_8UC1, Scalar(0));
+    Mat cost_map(param->img.size(), CV_32F, Scalar(FLT_MAX));
+    Mat expand(param->img.size(), CV_8UC1, Scalar(0));
+    Mat processed(param->img.size(), CV_8UC1, Scalar(0));
+    Mat removed(param->img.size(), CV_8UC1, Scalar(0));
     std::priority_queue < Pix, std::vector<Pix>, std::greater<Pix> > L;
 
     cost_map.at<float>(start) = 0;
@@ -89,12 +93,12 @@ static void find_min_path(const Point& start, Calculated_parameters* tmp)
                 {
                     int tx = p.x + i;
                     int ty = p.y + j;
-                    if (tx < 0 || tx >= tmp->img.cols || ty < 0 || ty >= tmp->img.rows)
+                    if (tx < 0 || tx >= param->img.cols || ty < 0 || ty >= param->img.rows)
                         continue;
                     if (expand.at<uchar>(ty, tx) == 0)
                     {
                         Point q = Point(tx, ty);
-                        float cost = cost_map.at<float>(p) + local_cost(p, q, tmp->gradient_magnitude, tmp->Iy, tmp->Ix, tmp->zero_crossing);
+                        float cost = cost_map.at<float>(p) + local_cost(p, q, param->gradient_magnitude, param->Iy, param->Ix, param->zero_crossing);
                         if (processed.at<uchar>(q) == 1 && cost < cost_map.at<float>(q))
                         {
                             removed.at<uchar>(q) = 1;
@@ -102,8 +106,8 @@ static void find_min_path(const Point& start, Calculated_parameters* tmp)
                         if (processed.at<uchar>(q) == 0)
                         {
                             cost_map.at<float>(q) = cost;
-                            tmp->hit_map_x.at<int>(q)= p.x;
-                            tmp->hit_map_y.at<int>(q) = p.y;
+                            param->hit_map_x.at<int>(q)= p.x;
+                            param->hit_map_y.at<int>(q) = p.y;
                             processed.at<uchar>(q) = 1;
                             Pix val;
                             val.cost = cost_map.at<float>(q);
@@ -120,48 +124,48 @@ static void find_min_path(const Point& start, Calculated_parameters* tmp)
 
 static void onMouse(int event, int x, int y, int , void* userdata)
 {
-    Calculated_parameters* tmp = reinterpret_cast<Calculated_parameters*>(userdata);
+    Parameters* param = reinterpret_cast<Parameters*>(userdata);
     if (event == EVENT_LBUTTONDOWN)
     {
-        tmp->end = Point(x, y);
-        if (!tmp->contours.back().empty())
+        param->end = Point(x, y);
+        if (!param->contours.back().empty())
         {
-            for (int i = static_cast<int>(tmp->tmp_contour.size()) - 1; i >= 0; i--)
+            for (int i = static_cast<int>(param->tmp_contour.size()) - 1; i >= 0; i--)
             {
-                tmp->contours.back().push_back(tmp->tmp_contour[i]);
+                param->contours.back().push_back(param->tmp_contour[i]);
             }
-            tmp->tmp_contour.clear();
+            param->tmp_contour.clear();
         }
         else
         {
-            tmp->contours.back().push_back(tmp->end);
+            param->contours.back().push_back(param->end);
         }
-        find_min_path(tmp->end, tmp);
+        find_min_path(param->end, param);
 
-        tmp->img_render.copyTo(tmp->img_pre_render);
-        imshow("lasso", tmp->img_render);
+        param->img_render.copyTo(param->img_pre_render);
+        imshow("lasso", param->img_render);
     }
     else if (event == EVENT_RBUTTONDOWN)
     {
-        tmp->img_pre_render.copyTo(tmp->img_render);
-        drawContours(tmp->img_pre_render, tmp->contours, static_cast<int>(tmp->contours.size()) - 1, Scalar(0,255,0), FILLED);
-        addWeighted(tmp->img_pre_render, 0.3, tmp->img_render, 0.7, 0, tmp->img_render);
-        tmp->contours.resize(tmp->contours.size() + 1);
-        imshow("lasso", tmp->img_render);
+        param->img_pre_render.copyTo(param->img_render);
+        drawContours(param->img_pre_render, param->contours, static_cast<int>(param->contours.size()) - 1, Scalar(0,255,0), FILLED);
+        addWeighted(param->img_pre_render, 0.3, param->img_render, 0.7, 0, param->img_render);
+        param->contours.resize(param->contours.size() + 1);
+        imshow("lasso", param->img_render);
     }
-    else if (event == EVENT_MOUSEMOVE && !tmp->contours.back().empty())
+    else if (event == EVENT_MOUSEMOVE && !param->contours.back().empty())
     {
-        tmp->tmp_contour.clear();
-        tmp->img_pre_render.copyTo(tmp->img_render);
+        param->tmp_contour.clear();
+        param->img_pre_render.copyTo(param->img_render);
         Point val_point = Point(x, y);
-        while (val_point != tmp->end)
+        while (val_point != param->end)
         {
-            tmp->tmp_contour.push_back(val_point);
-            Point cur = Point(tmp->hit_map_x.at<int>(val_point), tmp->hit_map_y.at<int>(val_point));
-            line(tmp->img_render, val_point, cur, Scalar(255, 0, 0), 2);
+            param->tmp_contour.push_back(val_point);
+            Point cur = Point(param->hit_map_x.at<int>(val_point), param->hit_map_y.at<int>(val_point));
+            line(param->img_render, val_point, cur, Scalar(255, 0, 0), 2);
             val_point = cur;
         }
-        imshow("lasso", tmp->img_render);
+        imshow("lasso", param->img_render);
     }
 }
 
@@ -173,7 +177,7 @@ const char* keys =
 
 int main( int argc, const char** argv )
 {
-    Calculated_parameters tmp;
+    Parameters param;
 
     CommandLineParser parser(argc, argv, keys);
     parser.about("\nThis program demonstrates implementation of 'intelligent scissors' algorithm\n"
@@ -186,34 +190,34 @@ int main( int argc, const char** argv )
         return 1;
     }
     std::vector<std::vector<Point> > c(1);
-    tmp.contours=c;
+    param.contours=c;
     std::string filename = parser.get<std::string>(0);
 
     Mat grayscale, img_canny;
-    tmp.img = imread(samples::findFile(filename));
-    tmp.hit_map_x.create(tmp.img.rows, tmp.img.cols, CV_32SC1);
-    tmp.hit_map_y.create(tmp.img.rows, tmp.img.cols, CV_32SC1);
+    param.img = imread(samples::findFile(filename));
+    param.hit_map_x.create(param.img.rows, param.img.cols, CV_32SC1);
+    param.hit_map_y.create(param.img.rows, param.img.cols, CV_32SC1);
 
-    cvtColor(tmp.img, grayscale, COLOR_BGR2GRAY);
+    cvtColor(param.img, grayscale, COLOR_BGR2GRAY);
     Canny(grayscale, img_canny, 50, 100);
 
-    threshold(img_canny, tmp.zero_crossing, 254, 1, THRESH_BINARY_INV);
-    Sobel(grayscale, tmp.Ix, CV_32FC1, 1, 0, 1);
-    Sobel(grayscale, tmp.Iy, CV_32FC1, 0, 1, 1);
-    tmp.Ix.convertTo(tmp.Ix, CV_32F, 1.0/255);
-    tmp.Iy.convertTo(tmp.Iy, CV_32F, 1.0/255);
+    threshold(img_canny, param.zero_crossing, 254, 1, THRESH_BINARY_INV);
+    Sobel(grayscale, param.Ix, CV_32FC1, 1, 0, 1);
+    Sobel(grayscale, param.Iy, CV_32FC1, 0, 1, 1);
+    param.Ix.convertTo(param.Ix, CV_32F, 1.0/255);
+    param.Iy.convertTo(param.Iy, CV_32F, 1.0/255);
 
     // Compute gradients magnitude.
     double max_val = 0.0;
-    magnitude(tmp.Iy, tmp.Ix, tmp.gradient_magnitude);
-    minMaxLoc(tmp.gradient_magnitude, 0, &max_val);
-    tmp.gradient_magnitude.convertTo(tmp.gradient_magnitude, CV_32F, -1/max_val, 1.0);
+    magnitude(param.Iy, param.Ix, param.gradient_magnitude);
+    minMaxLoc(param.gradient_magnitude, 0, &max_val);
+    param.gradient_magnitude.convertTo(param.gradient_magnitude, CV_32F, -1/max_val, 1.0);
 
-    tmp.img.copyTo(tmp.img_pre_render);
-    tmp.img.copyTo(tmp.img_render);
+    param.img.copyTo(param.img_pre_render);
+    param.img.copyTo(param.img_render);
 
     namedWindow("lasso");
-    setMouseCallback("lasso", onMouse, &tmp);
-    imshow("lasso", tmp.img);
+    setMouseCallback("lasso", onMouse, &param);
+    imshow("lasso", param.img);
     waitKey(0);
 }
