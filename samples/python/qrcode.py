033395358388871f99f0f9c7012daec40f5b78b6
jshiwam <jaiswalshiwam369@gmail.com>
2020-03-20 22:00:34 +0530

    input taken using argparse and the indents were removed

--- samples/python/qrcode.py
+++ samples/python/qrcode.py
@@ -1,14 +1,23 @@
 from __future__ import print_function
 import cv2 as cv
 import numpy as np
-import sys,getopt
+import sys
 from glob import glob
-
 PY3 = sys.version_info[0] == 3
+import argparse
 
 if PY3:
     xrange = range
 
+def str2bool(v):
+    if isinstance(v, bool):
+        return v
+    elif v.lower() in ('true'):
+        return True
+    elif v.lower() in ('false'):
+        return False
+    else:
+        raise argparse.ArgumentTypeError('Boolean value expected.')
 #Input From the camera is to be implemented
 
 #Output Filename and It's extension
@@ -16,26 +25,19 @@ o_f_name=''
 o_f_ext=''
 
 #Default values of input parameters
-inputfile = '../../../opencv_extra/testdata/cv/qrcode/multiple/*_qrcodes.png'
-outputfile = 'qr_code.png'
-multi='True'
-detect='False'
+parser = argparse.ArgumentParser(description='This program detects the QR-codes input images using OpenCV Library.')
+parser.add_argument('-i','--input',help="input image path (default input file path is 'opencv_extra/testdata/cv/qrcode/multiple/*_qrcodes.png",default="opencv_extra/testdata/cv/qrcode/multiple/*_qrcodes.png",metavar="")
+parser.add_argument('-d','--detect',help="detect QR code only (skip decoding) (value True/False default value is False)",metavar='',type=str2bool,default=False)
+parser.add_argument('-m','--multi',help="use detect for multiple qr-codes (value True/False default value is True)",metavar="",type=str2bool,default=True)
+parser.add_argument('-o','--out',help="path to result file (default output filename is qr_code.png)",metavar="",default="qr_code.png")
+args = parser.parse_args()
 #save_detection ='False'
 #save_all='False'
 
 
-helper="""
-This program detects the QR-codes input images using OpenCV Library.
-Usage: python qrcodesample.py [params]
-   -h = print help messages
-   -i, --in (default input file path is 'opencv_extra/testdata/cv/qrcode/multiple/*_qrcodes.png')= input image path
-   -d, --detect (value True/False default value is False) = detect QR code only (skip decoding)
-   -m, --multi (value True/False default value is True) = use detect for multiple qr-codes
-   -o, --out (default output filename is qr_code.png) = path to result file
-"""
 def getQRModeString():
-    msg1="multi" if(multi=='True') else ""
-    msg2="detector" if(detect=='True') else "decoder"
+    msg1="multi" if(args.multi) else ""
+    msg2="detector" if(args.detect) else "decoder"
     msg="QR %s %s"%(msg1,msg2)
     return msg
 
@@ -75,15 +77,15 @@ def drawQRCodeResults(result,points,decode_info,fps):
     drawFPS(result,fps)
 
 def runQR(qrCode,inputimg):
-    if(multi=='False'):
-        if(detect=='False'):
+    if(not args.multi):
+        if(not args.detect):
             decode_info,points,straight_qrcode=qrCode.detectAndDecode(inputimg)
             dec_info=decode_info
         else:
             retval,points=qrCode.detect(inputimg)
             dec_info=[]
     else:
-        if(detect=='False'):
+        if(not args.detect):
             retval,decode_info,points,straight_qrcode=qrCode.detectAndDecodeMulti(inputimg)
             dec_info=decode_info
         else:
@@ -108,7 +110,7 @@ def DetectQRFrmImage(inputfile):
     drawQRCodeResults(result,points,decode_info,fps)
     cv.imshow("QR",result)
     cv.waitKey(1)
-    if(outputfile!=''):
+    if(args.out!=''):
         outfile = o_f_name+o_f_ext
         print ("Saving Result: ", outfile)
         cv.imwrite(outfile,result)
@@ -120,39 +122,15 @@ def DetectQRFrmImage(inputfile):
 def main(argv):
     global o_f_name
     global o_f_ext
-    global outputfile
-    global inputfile
-    global multi
-    global detect
-    #global save_all
-    #global save_detection
-    try:
-        opts, args = getopt.getopt(argv,"hi:o:m:d:",["in=","out=","multi=","detect="])
-    except getopt.GetoptError:
-        print(helper)
-        sys.exit(2)
-    for opt, arg in opts:
-        if opt == '-h':
-            print(helper)
-            sys.exit()
-        elif opt in ("-i", "--ifile"):
-            inputfile = arg
-        elif opt in ("-o", "--ofile"):
-            outputfile = arg
-        elif opt in ("-m","--multi"):
-            multi = arg
-        elif opt in ("-d",'--detect'):
-            detect = arg
-
-    if(outputfile!=''):
-        index=outputfile.rfind('.')
+    if(args.out!=''):
+        index=args.out.rfind('.')
         if(index!=-1):
-            o_f_name=outputfile[:index]
-            o_f_ext=outputfile[index:]
+            o_f_name=args.out[:index]
+            o_f_ext=args.out[index:]
         else:
-            o_f_name=outputfile
+            o_f_name=args.out
             o_f_ext=".png"
-    for fn in glob(inputfile):
+    for fn in glob(args.input):
         DetectQRFrmImage(fn)
 
 
