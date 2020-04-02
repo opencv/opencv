016769fc4dcef5ac6ca84781d5f80f2be286cff1
Gourav Roy <gouravroy261999@gmail.com>
2020-04-02 10:41:14 +0530

    add text recognition sample

--- samples/dnn/text_recognition.py
+++ samples/dnn/text_recognition.py
@@ -2,12 +2,14 @@
 import cv2 as cv
 import math
 import argparse
+import numpy as np
 
 ############ Add argument parser for command line arguments ############
-parser = argparse.ArgumentParser(description='Use this script to run TensorFlow implementation (https://github.com/argman/EAST) of EAST: An Efficient and Accurate Scene Text Detector (https://arxiv.org/abs/1704.03155v2)')
+parser = argparse.ArgumentParser(description='Use this script to run TensorFlow implementation (https://github.com/argman/EAST) of EAST: An Efficient and Accurate Scene Text Detector (https://arxiv.org/abs/1704.03155v2) and the Pytorch implementation from (https://github.com/meijieru/crnn.pytorch)')
 parser.add_argument('--input', help='Path to input image or video file. Skip this argument to capture frames from a camera.')
-parser.add_argument('--model', required=True,
+parser.add_argument('--detection_model', required=True,
                     help='Path to a binary .pb file of model contains trained weights.')
+parser.add_argument('--recognition_model', required=True, default="crnn.onnx", help="Path to crnn.onnx file", )
 parser.add_argument('--width', type=int, default=320,
                     help='Preprocess input image by resizing to a specific width. It should be multiple by 32.')
 parser.add_argument('--height',type=int, default=320,
@@ -18,8 +20,86 @@ parser.add_argument('--nms',type=float, default=0.4,
                     help='Non-maximum suppression threshold.')
 args = parser.parse_args()
 
+
 ############ Utility functions ############
-def decode(scores, geometry, scoreThresh):
+
+def order_points(pts):
+    # initialzie a list of coordinates that will be ordered
+    # such that the first entry in the list is the top-left,
+    # the second entry is the top-right, the third is the
+    # bottom-right, and the fourth is the bottom-left
+    rect = np.zeros((4, 2), dtype="float32")
+    # the top-left point will have the smallest sum, whereas
+    # the bottom-right point will have the largest sum
+    s = pts.sum(axis=1)
+    rect[0] = pts[np.argmin(s)]
+    rect[2] = pts[np.argmax(s)]
+    # now, compute the difference between the points, the
+    # top-right point will have the smallest difference,
+    # whereas the bottom-left will have the largest difference
+    diff = np.diff(pts, axis=1)
+    rect[1] = pts[np.argmin(diff)]
+    rect[3] = pts[np.argmax(diff)]
+    # return the ordered coordinates
+    return rect
+
+
+def four_point_transform(image, pts):
+    # obtain a consistent order of the points and unpack them
+    # individually
+    rect = order_points(pts)
+    (tl, tr, br, bl) = rect
+    # compute the width of the new image, which will be the
+    # maximum distance between bottom-right and bottom-left
+    # x-coordiates or the top-right and top-left x-coordinates
+    widthA = np.sqrt(((br[0] - bl[0]) ** 2) + ((br[1] - bl[1]) ** 2))
+    widthB = np.sqrt(((tr[0] - tl[0]) ** 2) + ((tr[1] - tl[1]) ** 2))
+    maxWidth = max(int(widthA), int(widthB))
+    # compute the height of the new image, which will be the
+    # maximum distance between the top-right and bottom-right
+    # y-coordinates or the top-left and bottom-left y-coordinates
+    heightA = np.sqrt(((tr[0] - br[0]) ** 2) + ((tr[1] - br[1]) ** 2))
+    heightB = np.sqrt(((tl[0] - bl[0]) ** 2) + ((tl[1] - bl[1]) ** 2))
+    maxHeight = max(int(heightA), int(heightB))
+    # now that we have the dimensions of the new image, construct
+    # the set of destination points to obtain a "birds eye view",
+    # (i.e. top-down view) of the image, again specifying points
+    # in the top-left, top-right, bottom-right, and bottom-left
+    # order
+    dst = np.array([
+        [0, 0],
+        [maxWidth - 1, 0],
+        [maxWidth - 1, maxHeight - 1],
+        [0, maxHeight - 1]], dtype="float32")
+    # compute the perspective transform matrix and then apply it
+    M = cv.getPerspectiveTransform(rect, dst)
+    warped = cv.warpPerspective(image, M, (maxWidth, maxHeight))
+    # return the warped image
+    return warped
+
+
+def decode_text(out, raw=False):
+    raw_text = ""
+    alphabet = "0123456789abcdefghijklmnopqrstuvwxyz"
+    for i in range(out.shape[0]):
+        c = np.argmax(out[i][0])
+        if c != 0:
+            raw_text += alphabet[c - 1]
+        else:
+            raw_text += '-'
+    if raw:
+        return raw_text
+
+    # adjacent same letters as well as background text must be removed to get the final output
+    char_list = []
+    for i in range(len(raw_text)):
+        if raw_text[i] != '-' and (not (i > 0 and raw_text[i] == raw_text[i - 1])):
+            char_list.append(raw_text[i])
+    return ''.join(char_list)
+
+
+
+def decode_bounding_boxes(scores, geometry, scoreThresh):
     detections = []
     confidences = []
 
@@ -80,10 +160,12 @@ def main():
     nmsThreshold = args.nms
     inpWidth = args.width
     inpHeight = args.height
-    model = args.model
+    detection_model = args.detection_model
+    recognition_model = args.recognition_model
 
     # Load network
-    net = cv.dnn.readNet(model)
+    detector = cv.dnn.readNet(detection_model)
+    recognizer = cv.dnn.readNet(recognition_model)
 
     # Create a new named window
     kWinName = "EAST: An Efficient and Accurate Scene Text Detector"
@@ -111,16 +193,15 @@ def main():
         # Create a 4D blob from frame.
         blob = cv.dnn.blobFromImage(frame, 1.0, (inpWidth, inpHeight), (123.68, 116.78, 103.94), True, False)
 
-        # Run the model
-        net.setInput(blob)
-        outs = net.forward(outNames)
-        t, _ = net.getPerfProfile()
-        label = 'Inference time: %.2f ms' % (t * 1000.0 / cv.getTickFrequency())
+        # Run the detection model
+        detector.setInput(blob)
+        outs = detector.forward(outNames)
+        t_detection, _ = detector.getPerfProfile()
 
         # Get scores and geometry
         scores = outs[0]
         geometry = outs[1]
-        [boxes, confidences] = decode(scores, geometry, confThreshold)
+        [boxes, confidences] = decode_bounding_boxes(scores, geometry, confThreshold)
 
         # Apply NMS
         indices = cv.dnn.NMSBoxesRotated(boxes, confidences, confThreshold,nmsThreshold)
@@ -128,15 +209,38 @@ def main():
             # get 4 corners of the rotated rect
             vertices = cv.boxPoints(boxes[i[0]])
             # scale the bounding box coordinates based on the respective ratios
+            cnt = []
             for j in range(4):
                 vertices[j][0] *= rW
                 vertices[j][1] *= rH
+                cnt.append((vertices[j][0], vertices[j][1]))
+
+            cnt = np.asarray(cnt)
+
+            # get cropped image using perspective transform
+            cropped = four_point_transform(frame, cnt)
+            cropped = cv.cvtColor(cropped, cv.COLOR_BGR2GRAY)
+            cropped = cropped.astype("float32")
+
+            # Create a 4D blob from cropped image
+            blob = cv.dnn.blobFromImage(cropped, size=(100, 32), mean=0.5, scalefactor=2 / 255.0)
+            recognizer.setInput(blob)
+
+            # Run the recognition model
+            out = recognizer.forward()
+            t_recognition, _ = detector.getPerfProfile()
+            recognized_text = decode_text(out)
+            center, size, angle = cv.minAreaRect(cnt)
+            point = (int(center[0] - size[1]/2), int(center[1] - size[0]/2 ))
+            cv.putText(frame,recognized_text, point, cv.FONT_HERSHEY_SIMPLEX, 0.5, (255, 0, 0))
+
             for j in range(4):
                 p1 = (vertices[j][0], vertices[j][1])
                 p2 = (vertices[(j + 1) % 4][0], vertices[(j + 1) % 4][1])
                 cv.line(frame, p1, p2, (0, 255, 0), 1)
 
         # Put efficiency information
+        label = 'Inference time: %.2f ms' % ( (t_detection + t_recognition)  * 1000.0 / cv.getTickFrequency())
         cv.putText(frame, label, (0, 15), cv.FONT_HERSHEY_SIMPLEX, 0.5, (0, 255, 0))
 
         # Display the frame
