49f70fc25032c10b8b14cc7b14e6b4d840d92802
Alexander Shashkin <shashkind@yandex.ru>
2020-04-18 14:26:33 +0300

    updated test/test_resize_pil.cpp for non-quadratic, downsize and than upsize cases

--- modules/imgproc/test/test_resize_pil.cpp
+++ modules/imgproc/test/test_resize_pil.cpp
@@ -13,14 +13,94 @@ TEST(Imgproc_Resize, accuracy_INTER_NEAREST_PIL_QUADRATIC)
     const string filename = root + "gradient.png";
 
     Mat img;
-    ASSERT_NO_THROW(img = imread(filename));
+    ASSERT_NO_THROW(img = imread(filename, IMREAD_GRAYSCALE));
     ASSERT_FALSE(img.empty());
 
     Mat target_img;
     resize(img, target_img, Size(3, 3), 0, 0, INTER_NEAREST_PIL);
 
-    Mat expected_img = (Mat_<CV_8U>(3,3) << 40, 125, 209, 43, 127, 211, 45, 129, 213);
-    EXPECT_EQ(target_img, expected_img);
+    uchar data[] = {41, 125, 209, 43, 127, 211, 45, 129, 213};
+    Mat expected_img = Mat(3, 3, CV_8UC1, data);
+    ASSERT_EQ( cvtest::norm(target_img, expected_img, NORM_INF), 0.);
+}
+
+TEST(Imgproc_Resize, accuracy_INTER_NEAREST_PIL_NONQUADRATIC)
+{
+    const string root = cvtest::TS::ptr()->get_data_path();
+    const string filename = root + "gradient.png";
+
+    Mat img;
+    ASSERT_NO_THROW(img = imread(filename, IMREAD_GRAYSCALE));
+    ASSERT_FALSE(img.empty());
+
+    Mat target_img;
+    resize(img, target_img, Size(7, 11), 0, 0, INTER_NEAREST_PIL);
+
+    uchar data[] = {16,  52,  88, 125, 160, 196, 232,
+                    17,  52,  89, 125, 160, 196, 233,
+                    17,  53,  89, 126, 161, 197, 233,
+                    17,  54,  89, 126, 162, 197, 234,
+                    18,  54,  90, 126, 162, 198, 234,
+                    18,  54,  91, 127, 162, 198, 235,
+                    19,  55,  91, 127, 162, 199, 235,
+                    19,  55,  91, 127, 163, 199, 235,
+                    20,  56,  92, 128, 164, 200, 236,
+                    20,  56,  92, 129, 164, 200, 236,
+                    20,  56,  93, 129, 165, 201, 237};
+    Mat expected_img = Mat(11, 7, CV_8UC1, data);
+    ASSERT_EQ( cvtest::norm(target_img, expected_img, NORM_INF), 0.);
+}
+
+TEST(Imgproc_Resize, accuracy_INTER_NEAREST_PIL_DOWN_UP_7_11)
+{
+    const string root = cvtest::TS::ptr()->get_data_path();
+    const string filename = root + "gradient.png";
+
+    Mat img;
+    ASSERT_NO_THROW(img = imread(filename, IMREAD_GRAYSCALE));
+    ASSERT_FALSE(img.empty());
+
+    Mat target_img;
+    resize(img, target_img, Size(3, 3), 0, 0, INTER_NEAREST_PIL);
+    resize(target_img, target_img, Size(7, 11), 0, 0, INTER_NEAREST_PIL);
+
+    uchar data[] = {41,  41, 125, 125, 125, 209, 209,
+                  41,  41, 125, 125, 125, 209, 209,
+                  41,  41, 125, 125, 125, 209, 209,
+                  41,  41, 125, 125, 125, 209, 209,
+                  43,  43, 127, 127, 127, 211, 211,
+                  43,  43, 127, 127, 127, 211, 211,
+                  43,  43, 127, 127, 127, 211, 211,
+                  45,  45, 129, 129, 129, 213, 213,
+                  45,  45, 129, 129, 129, 213, 213,
+                  45,  45, 129, 129, 129, 213, 213,
+                  45,  45, 129, 129, 129, 213, 213};
+    Mat expected_img = Mat(11, 7, CV_8UC1, data);
+    ASSERT_EQ( cvtest::norm(target_img, expected_img, NORM_INF), 0.);
+}
+
+TEST(Imgproc_Resize, accuracy_INTER_NEAREST_PIL_DOWN_UP_11_7)
+{
+    const string root = cvtest::TS::ptr()->get_data_path();
+    const string filename = root + "gradient.png";
+
+    Mat img;
+    ASSERT_NO_THROW(img = imread(filename, IMREAD_GRAYSCALE));
+    ASSERT_FALSE(img.empty());
+
+    Mat target_img;
+    resize(img, target_img, Size(3, 3), 0, 0, INTER_NEAREST_PIL);
+    resize(target_img, target_img, Size(11, 7), 0, 0, INTER_NEAREST_PIL);
+
+    uchar data[] = {41,  41,  41,  41, 125, 125, 125, 209, 209, 209, 209,
+                  41,  41,  41,  41, 125, 125, 125, 209, 209, 209, 209,
+                  43,  43,  43,  43, 127, 127, 127, 211, 211, 211, 211,
+                  43,  43,  43,  43, 127, 127, 127, 211, 211, 211, 211,
+                  43,  43,  43,  43, 127, 127, 127, 211, 211, 211, 211,
+                  45,  45,  45,  45, 129, 129, 129, 213, 213, 213, 213,
+                  45,  45,  45,  45, 129, 129, 129, 213, 213, 213, 213};
+    Mat expected_img = Mat(7, 11, CV_8UC1, data);
+    ASSERT_EQ( cvtest::norm(target_img, expected_img, NORM_INF), 0.);
 }
 
 }} // namespace
