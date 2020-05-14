cbadaa0bac1ab703b0b2b88282fccc8fc722868f
jsxyhelu <jsxyhelu@foxmail.com>
2020-05-14 22:45:20 +0800

    Update and rename multiscale_sharpen to multiscale_sharpen.cpp
    also remove license

--- samples/cpp/multiscale_sharpen.cpp
+++ samples/cpp/multiscale_sharpen.cpp
@@ -1,7 +1,3 @@
-// This file is part of OpenCV project.
-// It is subject to the license terms in the LICENSE file found in the top-level directory
-// of this distribution and at http://opencv.org/license.html.
-
 #include "opencv2/core/utility.hpp"
 #include "opencv2/imgproc.hpp"
 #include "opencv2/imgcodecs.hpp"
