af8376d77961b9fd299b2bc762fa0811218451b4
Liubov Batanina <piccione-mail@yandex.ru>
2020-05-19 15:29:50 +0300

    Merge pull request #16575 from l-bat:flownet2
    Support FlowNet2 model
    
    * Support DataAugmentation layer
    
    * Fix warnings
    
    * Fix comments
    
    * Support Correlation layer
    
    * TEST
    
    * Support Correlation layer
    
    * Supported Accum and FlowWarp layers
    
    * Supported ChannelNorm layer
    
    * Supported Resample with inputs.size() > 1
    
    * Fixed comments
    
    * Refactoring
    
    * Added tests
    
    * Add resample test
    
    * Added asserts in resize layer
    
    * Updated DataAugmentation layer
    
    * Update convolution layer
    
    * Refactoring
    
    * Fix data augmentation layer
    
    * Fix caffe importer
    
    * Fix resize
    
    * Switch to Mat ptr
    
    * Remove useless resize type
    
    * Used ResizeLayer in Accum
    
    * Split ChannelNormLayer
    
    * Delete duplicate assert
    
    * Add sample
    
    * Fix sample
    
    * Added colormap

--- modules/dnn/src/init.cpp
+++ modules/dnn/src/init.cpp
@@ -132,6 +132,10 @@ void initializeLayerFactory()
     CV_DNN_REGISTER_LAYER_CLASS(Padding,        PaddingLayer);
     CV_DNN_REGISTER_LAYER_CLASS(Proposal,       ProposalLayer);
     CV_DNN_REGISTER_LAYER_CLASS(Scale,          ScaleLayer);
+    CV_DNN_REGISTER_LAYER_CLASS(DataAugmentation, DataAugmentationLayer);
+    CV_DNN_REGISTER_LAYER_CLASS(Correlation,    CorrelationLayer);
+    CV_DNN_REGISTER_LAYER_CLASS(Accum,          AccumLayer);
+    CV_DNN_REGISTER_LAYER_CLASS(FlowWarp,       FlowWarpLayer);
 
     CV_DNN_REGISTER_LAYER_CLASS(LSTM,           LSTMLayer);
 }
