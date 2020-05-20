c946d58b7c00bfad9a5c83807a509e94d69b562c
Pierre-Emmanuel Viel <p.emmanuel.viel@gmail.com>
2020-05-20 21:43:13 +0200

    Fix: Transpose the Flann repo commit "Fixing unreachable code in lsh_table.h" from hypevr

--- modules/flann/include/opencv2/flann/lsh_table.h
+++ modules/flann/include/opencv2/flann/lsh_table.h
@@ -245,7 +245,7 @@ public:
     {
         std::cerr << "LSH is not implemented for that type" << std::endl;
         assert(0);
-        return 1;
+        return -1;
     }
 
     /** Get statistics about the table
