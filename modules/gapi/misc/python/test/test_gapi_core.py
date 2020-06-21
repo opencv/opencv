6281f438be2a910d451408173561f5c2b7d8fa08
Anatoliy Talamanov <anatoliy.talamanov@intel.com>
2020-06-21 23:57:59 +0300

    Remove combine decorator from tests

--- modules/gapi/misc/python/test/test_gapi_core.py
+++ modules/gapi/misc/python/test/test_gapi_core.py
@@ -2,7 +2,6 @@
 
 import numpy as np
 import cv2 as cv
-import itertools
 
 from tests_common import NewOpenCVTests
 
@@ -16,20 +15,11 @@ pkgs = [
        ]
 
 
-def combine(cases):
-    def decorate(func):
-        def call(*args):
-            for case in itertools.product(*cases):
-                func(*args, *case)
-        return call
-    return decorate
-
-
 class gapi_core_test(NewOpenCVTests):
 
-    @combine([pkgs, [(1280, 720), (640, 480)]])
-    def test_add(self, pkg, sz):
-        # TODO: Extend to use any type here
+    def test_add(self):
+        # TODO: Extend to use any type and size here
+        sz = (1280, 720)
         in1 = np.random.randint(0, 100, sz).astype(np.uint8)
         in2 = np.random.randint(0, 100, sz).astype(np.uint8)
 
@@ -42,9 +32,10 @@ class gapi_core_test(NewOpenCVTests):
         g_out = cv.gapi.add(g_in1, g_in2)
         comp = cv.GComputation(g_in1, g_in2, g_out)
 
-        actual = comp.apply(in1, in2, args=cv.compile_args(pkg))
-        # Comparison
-        np.testing.assert_array_equal(expected, actual)
+        for pkg in pkgs:
+            actual = comp.apply(in1, in2, args=cv.compile_args(pkg))
+            # Comparison
+            np.testing.assert_array_equal(expected, actual)
 
 
 if __name__ == '__main__':
