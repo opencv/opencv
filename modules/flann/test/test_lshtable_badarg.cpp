92774b6728b3ed79a6adf3840b18875619e49bbe
Pierre-Emmanuel Viel <p.emmanuel.viel@gmail.com>
2020-06-28 17:34:56 +0200

    Add test checking we don't throw when creating GenericIndex with LshIndexParams()

--- modules/flann/test/test_lshtable_badarg.cpp
+++ modules/flann/test/test_lshtable_badarg.cpp
@@ -90,4 +90,10 @@ void CV_LshTableBadArgTest::run( int /* start_from */ )
 
 TEST(Flann_LshTable, badarg) { CV_LshTableBadArgTest test; test.safe_run(); }
 
+TEST(Flann_LshTable, bad_any_cast) {
+    Mat features = Mat::ones(1, 64, CV_8U);
+    EXPECT_NO_THROW(flann::GenericIndex<cvflann::Hamming2<unsigned char> >(
+                                            features, cvflann::LshIndexParams()));
+}
+
 }} // namespace
