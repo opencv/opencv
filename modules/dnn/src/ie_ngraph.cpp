332f14d210a8af72236867e791d0c5ce7a6372b4
Maksim Doronin <maksim.doronin@intel.com>
2020-07-30 17:46:37 +0300

    [IE][VPU]: Refactor vpu configs

--- modules/dnn/src/ie_ngraph.cpp
+++ modules/dnn/src/ie_ngraph.cpp
@@ -684,7 +684,11 @@ void InfEngineNgraphNet::initPlugin(InferenceEngine::CNNNetwork& net)
         }
         std::map<std::string, std::string> config;
         if (device_name == "MYRIAD") {
+#if INF_ENGINE_VER_MAJOR_GT(INF_ENGINE_RELEASE_2020_4)
+            config.emplace("MYRIAD_DETECT_NETWORK_BATCH", CONFIG_VALUE(NO));
+#else
             config.emplace("VPU_DETECT_NETWORK_BATCH", CONFIG_VALUE(NO));
+#endif
         }
 
         bool isHetero = device_name == "FPGA";
