d981e40959c1efe9cc592fd8eda30597e7d255c9
ZhengQiushi <woaiwo6666@qq.com>
2020-08-02 23:21:23 +0800

    try again

--- modules/objdetect/src/qrcode.cpp
+++ modules/objdetect/src/qrcode.cpp
@@ -8,7 +8,7 @@
 #include "precomp.hpp"
 #include "opencv2/objdetect.hpp"
 #include "opencv2/calib3d.hpp"
-#include <opencv2/highgui.hpp>
+//#include <opencv2/highgui.hpp>
 #include <opencv2/imgproc.hpp>
 #include <opencv2/imgproc/types_c.h>
 
