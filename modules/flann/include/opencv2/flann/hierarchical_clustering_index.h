e6ec42d462b562e9eddc8405bf89e2bb3c7a5b39
pemmanuelviel <p.emmanuel.viel@gmail.com>
2020-08-03 20:00:59 +0200

    Merge pull request #17735 from pemmanuelviel:pev-fix-trees-descent
    * Fix trees parsing behavior in hierarchical_clustering_index:
    Before, when maxCheck was reached in the first descent of a tree, time was still wasted parsing
    the next trees till their best leaf, just to skip the points stored there.
    Now we can choose either to keep this behavior, and so we skip parsing other trees after reaching
    maxCheck, or we choose to do one descent in each tree, even if in one tree we reach maxCheck.
    
    * Apply the same change to kdtree.
    As each leaf contains only 1 point (unlike hierarchical_clustering), difference is visible if trees > maxCheck
    
    * Add the new explore_all_trees parameters to miniflann
    
    * Adapt the FlannBasedMatcher read_write test to the additional search parameter
    
    * Adapt java tests to the additional parameter in SearchParams
    
    * Fix the ABI dumps failure on SearchParams interface change
    
    * Support of ctor calling another ctor of the class is only fully supported from C+11

--- modules/flann/include/opencv2/flann/hierarchical_clustering_index.h
+++ modules/flann/include/opencv2/flann/hierarchical_clustering_index.h
@@ -548,6 +548,7 @@ public:
     {
 
         const int maxChecks = get_param(searchParams,"checks",32);
+        const bool explore_all_trees = get_param(searchParams,"explore_all_trees",false);
 
         // Priority queue storing intermediate branches in the best-bin-first search
         Heap<BranchSt>* heap = new Heap<BranchSt>((int)size_);
@@ -555,15 +556,15 @@ public:
         std::vector<bool> checked(size_,false);
         int checks = 0;
         for (int i=0; i<trees_; ++i) {
-            findNN(root[i], result, vec, checks, maxChecks, heap, checked);
-            if ((checks >= maxChecks) && result.full())
+            findNN(root[i], result, vec, checks, maxChecks, heap, checked, explore_all_trees);
+            if (!explore_all_trees && (checks >= maxChecks) && result.full())
                 break;
         }
 
         BranchSt branch;
         while (heap->popMin(branch) && (checks<maxChecks || !result.full())) {
             NodePtr node = branch.node;
-            findNN(node, result, vec, checks, maxChecks, heap, checked);
+            findNN(node, result, vec, checks, maxChecks, heap, checked, false);
         }
 
         delete heap;
@@ -746,10 +747,10 @@ private:
 
 
     void findNN(NodePtr node, ResultSet<DistanceType>& result, const ElementType* vec, int& checks, int maxChecks,
-                Heap<BranchSt>* heap, std::vector<bool>& checked)
+                Heap<BranchSt>* heap, std::vector<bool>& checked, bool explore_all_trees = false)
     {
         if (node->childs==NULL) {
-            if ((checks>=maxChecks) && result.full()) {
+            if (!explore_all_trees && (checks>=maxChecks) && result.full()) {
                 return;
             }
             for (int i=0; i<node->size; ++i) {
@@ -778,7 +779,7 @@ private:
                 }
             }
             delete[] domain_distances;
-            findNN(node->childs[best_index],result,vec, checks, maxChecks, heap, checked);
+            findNN(node->childs[best_index],result,vec, checks, maxChecks, heap, checked, explore_all_trees);
         }
     }
 
