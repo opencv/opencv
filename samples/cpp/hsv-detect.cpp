8232919c40dc5446fe0b9a5bd015e3a449dcd431
cabelo <cabelo@opensuse.org>
2020-08-20 02:56:03 -0300

    HSV detect sample

--- samples/cpp/hsv-detect.cpp
+++ samples/cpp/hsv-detect.cpp
@@ -23,82 +23,82 @@ using namespace std;
 
 Scalar calcLargestArea(Mat mask, vector<vector<Point>> &contours)
 {
-	int largest_contour_index = 0;
-	int largest_area = 0;
-	double a;
-	dilate(mask,mask, Mat(), Point(-1,-1),1);
-	erode(mask,mask, Mat(), Point(-1,-1), 3);
-	findContours(mask, contours, RETR_LIST, CHAIN_APPROX_NONE);
-   for( int i = 0; i< contours.size(); i++ )
-   {
-		a=contourArea( contours[i],false); 
-		if(a>largest_area)
-		{
- 			largest_area=a;
-			largest_contour_index=i;               
-		}
-	}
-	return Scalar(largest_contour_index, largest_area,countNonZero(mask));
+    int largest_contour_index = 0;
+    int largest_area = 0;
+    double a;
+    dilate(mask,mask, Mat(), Point(-1,-1),1);
+    erode(mask,mask, Mat(), Point(-1,-1), 3);
+    findContours(mask, contours, RETR_LIST, CHAIN_APPROX_NONE);
+    for( int i = 0; i< contours.size(); i++ )
+    {
+        a=contourArea( contours[i],false); 
+        if(a>largest_area)
+        {
+            largest_area=a;
+            largest_contour_index=i;               
+        }
+    }
+    return Scalar(largest_contour_index, largest_area,countNonZero(mask));
 }
 
 int main(int argc, char** argv)
 {
-	CommandLineParser parser(argc, argv, keys);
-	parser = CommandLineParser(argc, argv, keys);
-	parser.about("Use this example o run object detection with HSV using OpenCV.");
-	if (parser.has("help"))
-	{
-		parser.printMessage();
-		return 0;
-	}
+    CommandLineParser parser(argc, argv, keys);
+    parser = CommandLineParser(argc, argv, keys);
+    parser.about("Use this example o run object detection with HSV using OpenCV.");
+    if (parser.has("help"))
+    {
+        parser.printMessage();
+        return 0;
+    }
 
-	String labels[] = { parser.get<String>("label1"), parser.get<String>("label2") };
+    String labels[] = { parser.get<String>("label1"), parser.get<String>("label2") };
 
-	Scalar obj1Lower = parser.get<Scalar>("obj1Lower");
-	Scalar obj1Upper = parser.get<Scalar>("obj1Upper");
+    Scalar obj1Lower = parser.get<Scalar>("obj1Lower");
+    Scalar obj1Upper = parser.get<Scalar>("obj1Upper");
 
-	Scalar obj2Lower = parser.get<Scalar>("obj2Lower");
-	Scalar obj2Upper = parser.get<Scalar>("obj2Upper");
-	int minArea      = parser.get<int>("minArea");
+    Scalar obj2Lower = parser.get<Scalar>("obj2Lower");
+    Scalar obj2Upper = parser.get<Scalar>("obj2Upper");
+    int minArea      = parser.get<int>("minArea");
 
-	bool testObj; 
-	Mat frame,image,OutputImageMask1,OutputImageMask2;
-	vector<vector<Point>>  contours;
-	float _x,_y,radius;
-	Scalar result;
-	Point2f center,_center;
+    bool testObj; 
+    Mat frame,image,OutputImageMask1,OutputImageMask2;
+    vector<vector<Point>>  contours;
+    float _x,_y,radius;
+    Scalar result;
+    Point2f center,_center;
 
-	VideoCapture cap;
-	cap.open(0);
-	while(true)
-	{
-		cap>>frame;
-		cvtColor(frame, image,  COLOR_BGR2HSV);
+    VideoCapture cap;
+    cap.open(0);
+    while(true)
+    {
+        cap>>frame;
+        cvtColor(frame, image,  COLOR_BGR2HSV);
 
-		inRange(image,obj1Lower,obj1Upper,OutputImageMask1);
-		inRange(image,obj2Lower,obj2Upper,OutputImageMask2);
+        inRange(image,obj1Lower,obj1Upper,OutputImageMask1);
+        inRange(image,obj2Lower,obj2Upper,OutputImageMask2);
 
-		testObj = (countNonZero(OutputImageMask1)> countNonZero(OutputImageMask2));
-		result = calcLargestArea((testObj == 1?OutputImageMask1:OutputImageMask2),contours);
+        testObj = (countNonZero(OutputImageMask1)> countNonZero(OutputImageMask2));
+        result = calcLargestArea((testObj == 1?OutputImageMask1:OutputImageMask2),contours);
 	
-		if(result[1]>minArea)
-		{      
-			minEnclosingCircle( contours[result[0]], center, radius);
-			cv::Moments m=cv::moments(contours[result[0]]);
-			_x = (m.m10/m.m00);
-			_y = (m.m01/m.m00);
-			_center = Point2f(_x,_y);
-			if(radius>10)
-			{
-				circle(frame, center, cvRound(radius), Scalar(0, 255, 255), 2, LINE_AA);
-				circle(frame, _center, 5, Scalar(255,0,0), -1, LINE_AA);
-			}
-			imshow("image",frame);
-			cv::displayOverlay("image",labels[testObj],2000); 
-		}
-		else
-		imshow("image",frame);
-		if (waitKey(5) >= 0) break;
-	}
-	cap.release();
+        if(result[1]>minArea)
+        {      
+            minEnclosingCircle( contours[result[0]], center, radius);
+            cv::Moments m=cv::moments(contours[result[0]]);
+            _x = (m.m10/m.m00);
+            _y = (m.m01/m.m00);
+            _center = Point2f(_x,_y);
+            if(radius>10)
+            {
+                circle(frame, center, cvRound(radius), Scalar(0, 255, 255), 2, LINE_AA);
+                circle(frame, _center, 5, Scalar(255,0,0), -1, LINE_AA);
+            }
+            imshow("image",frame);
+            cv::displayOverlay("image",labels[testObj],2000); 
+            }
+            else
+            imshow("image",frame);
+            if (waitKey(5) >= 0) break;
+    }
+    cap.release();
 }
