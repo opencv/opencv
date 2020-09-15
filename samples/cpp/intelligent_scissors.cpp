cbaba1cf3ff1f363a45d9c4a958082a940a19d55
SimpleVlad <vdizhenin@gmail.com>
2020-09-15 23:38:25 +0300

    Re sampl

--- samples/cpp/intelligent_scissors.cpp
+++ samples/cpp/intelligent_scissors.cpp
@@ -3,11 +3,13 @@
 #include <string>
 #include <vector>
 #include <queue>
+
 #include <opencv2/core/core.hpp>
 #include <opencv2/highgui/highgui.hpp>
 #include <opencv2/imgproc/imgproc.hpp>
 
 using namespace cv;
+
 struct Pix
 {
     Point next_point;
@@ -29,112 +31,98 @@ struct Parameters
 };
 
 
-static float local_cost(const Point& p, const Point& q, const Mat& gradient_magnitude, const Mat& Iy, const Mat& Ix, const Mat& zero_crossing)
-{
-    float fG = gradient_magnitude.at<float>(q.y, q.x);
-    float dp;
-    float dq;
-    const float WEIGHT_LAP_ZERO_CROSS = 0.43f;
-    const float WEIGHT_GRADIENT_MAGNITUDE = 0.14f;
-    const float WEIGHT_GRADIENT_DIRECTION = 0.43f;
-    bool isDiag = (p.x != q.x) && (p.y != q.y);
+// static float local_cost(const Point& p, const Point& q, const Mat& gradient_magnitude, const Mat& Iy, const Mat& Ix, const Mat& zero_crossing)
+// {
+//     float fG = gradient_magnitude.at<float>(q.y, q.x);
+//     float dp;
+//     float dq;
+//     const float WEIGHT_LAP_ZERO_CROSS = 0.43f;
+//     const float WEIGHT_GRADIENT_MAGNITUDE = 0.14f;
+//     const float WEIGHT_GRADIENT_DIRECTION = 0.43f;
+//     bool isDiag = (p.x != q.x) && (p.y != q.y);
 
-    if ((Iy.at<float>(p) * (q.x - p.x) - Ix.at<float>(p) * (q.y - p.y)) >= 0)
-    {
-        dp = Iy.at<float>(p) * (q.x - p.x) - Ix.at<float>(p) * (q.y - p.y);
-        dq = Iy.at<float>(q) * (q.x - p.x) - Ix.at<float>(q) * (q.y - p.y);
-    }
-    else
-    {
-        dp = Iy.at<float>(p) * (p.x - q.x) + (-Ix.at<float>(p)) * (p.y - q.y);
-        dq = Iy.at<float>(q) * (p.x - q.x) + (-Ix.at<float>(q)) * (p.y - q.y);
-    }
-    if (isDiag)
-    {
-        dp /= sqrtf(2);
-        dq /= sqrtf(2);
-    }
-    else
-    {
-        fG /= sqrtf(2);
-    }
-    return  WEIGHT_LAP_ZERO_CROSS * zero_crossing.at<uchar>(q) +
-            WEIGHT_GRADIENT_DIRECTION * (acosf(dp) + acosf(dq)) / static_cast<float>(CV_PI) +
-            WEIGHT_GRADIENT_MAGNITUDE * fG;
-}
+//     if ((Iy.at<float>(p) * (q.x - p.x) - Ix.at<float>(p) * (q.y - p.y)) >= 0)
+//     {
+//         dp = Iy.at<float>(p) * (q.x - p.x) - Ix.at<float>(p) * (q.y - p.y);
+//         dq = Iy.at<float>(q) * (q.x - p.x) - Ix.at<float>(q) * (q.y - p.y);
+//     }
+//     else
+//     {
+//         dp = Iy.at<float>(p) * (p.x - q.x) + (-Ix.at<float>(p)) * (p.y - q.y);
+//         dq = Iy.at<float>(q) * (p.x - q.x) + (-Ix.at<float>(q)) * (p.y - q.y);
+//     }
+//     if (isDiag)
+//     {
+//         dp /= sqrtf(2);
+//         dq /= sqrtf(2);
+//     }
+//     else
+//     {
+//         fG /= sqrtf(2);
+//     }
+//     return  WEIGHT_LAP_ZERO_CROSS * zero_crossing.at<uchar>(q) +
+//             WEIGHT_GRADIENT_DIRECTION * (acosf(dp) + acosf(dq)) / static_cast<float>(CV_PI) +
+//             WEIGHT_GRADIENT_MAGNITUDE * fG;
+// }
 
-static void find_min_path(const Point& start, Parameters* param)
-{
-    Pix begin;
-    Mat &img = param->img;
-    Mat cost_map(img.size(), CV_32F, Scalar(FLT_MAX));
-    Mat expand(img.size(), CV_8UC1, Scalar(0));
-    Mat processed(img.size(), CV_8UC1, Scalar(0));
-    Mat removed(img.size(), CV_8UC1, Scalar(0));
-    std::priority_queue < Pix, std::vector<Pix>, std::greater<Pix> > L;
+// static void find_min_path(const Point& start, Parameters* param)
+// {
+//     Pix begin;
+//     Mat &img = param->img;
+//     Mat cost_map(img.size(), CV_32F, Scalar(FLT_MAX));
+//     Mat expand(img.size(), CV_8UC1, Scalar(0));
+//     Mat processed(img.size(), CV_8UC1, Scalar(0));
+//     Mat removed(img.size(), CV_8UC1, Scalar(0));
+//     std::priority_queue < Pix, std::vector<Pix>, std::greater<Pix> > L;
 
-    cost_map.at<float>(start) = 0;
-    processed.at<uchar>(start) = 1;
-    begin.cost = 0;
-    begin.next_point = start;
+//     cost_map.at<float>(start) = 0;
+//     processed.at<uchar>(start) = 1;
+//     begin.cost = 0;
+//     begin.next_point = start;
 
-    L.push(begin);
-    int iter=0;
-    while (!L.empty())
-    {
-        Pix P = L.top();
-        std::cout<<P.next_point<<" "<<P.cost<<std::endl;
-        L.pop();
-        Point p = P.next_point;
-        processed.at<uchar>(p) = 0;
-        if (removed.at<uchar>(p) == 0)
-        {
-            expand.at<uchar>(p) = 1;
-            for (int i = -1; i <= 1; i++)
-            {
-                for(int j = -1; j <= 1; j++)
-                {
-                    int tx = p.x + i;
-                    int ty = p.y + j;
-                    if (tx < 0 || tx >= img.cols || ty < 0 || ty >= img.rows)
-                        continue;
-                    if (expand.at<uchar>(ty, tx) == 0)
-                    {
-                        Point q = Point(tx, ty);
-                        float cost = cost_map.at<float>(p) + local_cost(p, q, param->gradient_magnitude, param->Iy, param->Ix, param->zero_crossing);
-                        std::cout<< " Point: "<<tx << " "<< ty<<std::endl;
-                        // std::cout<<"loc "<< cost<<std::endl;
-                        if (processed.at<uchar>(q) == 1 && cost < cost_map.at<float>(q))
-                        {
-                            removed.at<uchar>(q) = 1;
-                        }
-                        if (processed.at<uchar>(q) == 0)
-                        {
-                            cost_map.at<float>(q) = cost;
-                            param->hit_map_x.at<int>(q)= p.x;
-                            param->hit_map_y.at<int>(q) = p.y;
-                            // std::cout<< " Point: "<<q.x << " "<< q.y<<" to "<<std::endl;
-                            std::cout<< p.x <<" "<<p.y<<std::endl;
-                            processed.at<uchar>(q) = 1;
-                            Pix val;
-                            val.cost = cost_map.at<float>(q);
-                            val.next_point = q;
-                            L.push(val);
-                        }
-                    }
-                }
-            }
-        }
-        iter++;
-        if(iter>0)
-        while (!L.empty())
-        {
-             Pix P = L.top();
-             std::cout<<P.next_point<<" "<<P.cost<<std::endl;
-             L.pop();
-        }
-    }
-}
+//     L.push(begin);
+//     while (!L.empty())
+//     {
+//         Pix P = L.top();
+//         L.pop();
+//         Point p = P.next_point;
+//         processed.at<uchar>(p) = 0;
+//         if (removed.at<uchar>(p) == 0)
+//         {
+//             expand.at<uchar>(p) = 1;
+//             for (int i = -1; i <= 1; i++)
+//             {
+//                 for(int j = -1; j <= 1; j++)
+//                 {
+//                     int tx = p.x + i;
+//                     int ty = p.y + j;
+//                     if (tx < 0 || tx >= img.cols || ty < 0 || ty >= img.rows)
+//                         continue;
+//                     if (expand.at<uchar>(ty, tx) == 0)
+//                     {
+//                         Point q = Point(tx, ty);
+//                         float cost = cost_map.at<float>(p) + local_cost(p, q, param->gradient_magnitude, param->Iy, param->Ix, param->zero_crossing);
+//                         if (processed.at<uchar>(q) == 1 && cost < cost_map.at<float>(q))
+//                         {
+//                             removed.at<uchar>(q) = 1;
+//                         }
+//                         if (processed.at<uchar>(q) == 0)
+//                         {
+//                             cost_map.at<float>(q) = cost;
+//                             param->hit_map_x.at<int>(q)= p.x;
+//                             param->hit_map_y.at<int>(q) = p.y;
+//                             processed.at<uchar>(q) = 1;
+//                             Pix val;
+//                             val.cost = cost_map.at<float>(q);
+//                             val.next_point = q;
+//                             L.push(val);
+//                         }
+//                     }
+//                 }
+//             }
+//         }
+//     }
+// }
 
 
 static void onMouse(int event, int x, int y, int , void* userdata)
@@ -161,7 +149,7 @@ static void onMouse(int event, int x, int y, int , void* userdata)
         {
             contours.back().push_back(end);
         }
-        find_min_path(end, param);
+        find_min_path(end,  param->img,  param->zero_crossing, param->gradient_magnitude, param->Ix,  param->Iy,  param->hit_map_x,  param->hit_map_y);
 
         img_render.copyTo(img_pre_render);
         imshow("lasso", img_render);
@@ -199,6 +187,7 @@ const char* keys =
 
 int main( int argc, const char** argv )
 {
+    
     Parameters param;
     const int EDGE_THRESHOLD_LOW = 50;
     const int EDGE_THRESHOLD_HIGH = 100;
@@ -218,17 +207,11 @@ int main( int argc, const char** argv )
     std::vector<std::vector<Point> > c(1);
     param.contours = c;
     std::string filename = parser.get<std::string>(0);
- 
+
     Mat grayscale, img_canny;
-    // param.img = imread(samples::findFile(filename));
-
-    param.img = imread("/home/vlad/Desktop/test/test.png");
-
-    Point test_point = Point(20, 20);
-   
+    param.img = imread(samples::findFile(filename));
     param.hit_map_x.create(param.img.rows, param.img.cols, CV_32SC1);
     param.hit_map_y.create(param.img.rows, param.img.cols, CV_32SC1);
-
 
     cvtColor(param.img, grayscale, COLOR_BGR2GRAY);
     Canny(grayscale, img_canny, EDGE_THRESHOLD_LOW, EDGE_THRESHOLD_HIGH);
@@ -239,8 +222,6 @@ int main( int argc, const char** argv )
     param.Ix.convertTo(param.Ix, CV_32F, 1.0/255);
     param.Iy.convertTo(param.Iy, CV_32F, 1.0/255);
 
-    imshow("tr", param.Ix);
-
     // Compute gradients magnitude.
     double max_val = 0.0;
     magnitude(param.Iy, param.Ix, param.gradient_magnitude);
@@ -249,7 +230,6 @@ int main( int argc, const char** argv )
 
     param.img.copyTo(param.img_pre_render);
     param.img.copyTo(param.img_render);
-    find_min_path(test_point, &param);
 
     namedWindow("lasso");
     setMouseCallback("lasso", onMouse, &param);
