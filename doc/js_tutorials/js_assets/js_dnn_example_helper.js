491b5d643d138b8cab2c8eeba407be91a14c3c3a
akineeic <zhiming.zeng@intel.com>
2020-09-16 00:03:30 +0800

    [Opencv.js doc] Change code format

--- doc/js_tutorials/js_assets/js_dnn_example_helper.js
+++ doc/js_tutorials/js_assets/js_dnn_example_helper.js
@@ -1,13 +1,13 @@
 getBlobFromImage = function(inputSize, mean, std, swapRB, image) {
     let mat;
-    if(typeof(image) === 'string') {
+    if (typeof(image) === 'string') {
         mat = cv.imread(image);
     } else {
         mat = image;
     }
 
     let matC3 = new cv.Mat(mat.matSize[0], mat.matSize[1], cv.CV_8UC3);
-    cv.cvtColor(mat, matC3, cv.COLOR_RGBA2RGB);
+    cv.cvtColor(mat, matC3, cv.COLOR_RGBA2BGR);
     let input = cv.blobFromImage(matC3, std, new cv.Size(inputSize[0], inputSize[1]),
                                  new cv.Scalar(mean[0], mean[1], mean[2]), swapRB);
 
@@ -29,7 +29,7 @@ loadModel = async function(e) {
         let reader = new FileReader();
         reader.readAsArrayBuffer(file);
         reader.onload = function(ev) {
-            if(reader.readyState === 2){
+            if (reader.readyState === 2) {
                 let buffer = reader.result;
                 let data = new Uint8Array(buffer);
                 cv.FS_createDataFile('/', path, data, true, false, false);
@@ -37,6 +37,27 @@ loadModel = async function(e) {
             }
         }
     });
+}
+
+getTopClasses = function(probs, labels, topK = 3) {
+    probs = Array.from(probs);
+    let indexes = probs.map((prob, index) => [prob, index]);
+    let sorted = indexes.sort((a, b) => {
+        if (a[0] === b[0]) {return 0;}
+        return a[0] < b[0] ? -1 : 1;
+    });
+    sorted.reverse();
+    let classes = [];
+    for (let i = 0; i < topK; ++i) {
+        let prob = sorted[i][0];
+        let index = sorted[i][1];
+        let c = {
+            label: labels[index],
+            prob: (prob * 100).toFixed(2)
+        }
+        classes.push(c);
+    }
+    return classes;
 }
 
 loadImageToCanvas = function(e, canvasId) {
@@ -57,14 +78,14 @@ drawInfoTable = async function(jsonUrl, divId) {
     let json = await response.json();
 
     let appendix = document.getElementById(divId);
-    for(key of Object.keys(json)) {
+    for (key of Object.keys(json)) {
         let h3 = document.createElement('h3');
         h3.textContent = key + " model";
         appendix.appendChild(h3);
 
         let table = document.createElement('table');
         let head_tr = document.createElement('tr');
-        for(head of Object.keys(json[key][0])) {
+        for (head of Object.keys(json[key][0])) {
             let th = document.createElement('th');
             th.textContent = head;
             th.style.border = "1px solid black";
@@ -72,12 +93,12 @@ drawInfoTable = async function(jsonUrl, divId) {
         }
         table.appendChild(head_tr)
 
-        for(model of json[key]) {
+        for (model of json[key]) {
             let tr = document.createElement('tr');
-            for(params of Object.keys(model)) {
+            for (params of Object.keys(model)) {
                 let td = document.createElement('td');
                 td.style.border = "1px solid black";
-                if(params !== "modelUrl" && params !== "configUrl" && params !== "labelsUrl") {
+                if (params !== "modelUrl" && params !== "configUrl" && params !== "labelsUrl") {
                     td.textContent = model[params];
                     tr.appendChild(td);
                 } else {
