cac31fecc73fe5f0dada6093de76d9788c0e9da1
Anatoliy Talamanov <anatoliy.talamanov@intel.com>
2020-10-12 17:00:06 +0300

    Add test

--- modules/gapi/src/backends/common/gbackend.hpp
+++ modules/gapi/src/backends/common/gbackend.hpp
@@ -27,7 +27,8 @@ namespace gimpl {
                                 : cv::Mat(v.dims(), v.type(), v.ptr());
     }
     inline RMat::View asView(const Mat& m, RMat::View::DestroyCallback&& cb = nullptr) {
-        return RMat::View(cv::descr_of(m), m.data, m.step, std::move(cb));
+        // FIXME: View doesn't support multidimensional cv::Mat's
+        return RMat::View(cv::descr_of(m), m.data, m.step[0], std::move(cb));
     }
 
     class RMatAdapter : public RMat::Adapter {
