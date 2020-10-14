661deea175c111a41c3ab5e5b36f8e8f0d26ab9c
Anatoliy Talamanov <anatoliy.talamanov@intel.com>
2020-10-14 15:11:52 +0300

    Add checkIEtarget to infer test

--- modules/gapi/misc/python/test/test_gapi_infer.py
+++ modules/gapi/misc/python/test/test_gapi_infer.py
@@ -9,29 +9,43 @@ from tests_common import NewOpenCVTests
 
 class test_gapi_infer(NewOpenCVTests):
 
-    def test_age_gender_infer(self):
-        root_path    = '/omz_intel_models/intel/age-gender-recognition-retail-0013/FP32/age-gender-recognition-retail-0013'
+    def checkIETarget(self, backend, target):
+        proto = self.find_file('layers/layer_convolution.prototxt',   [os.environ.get('OPENCV_DNN_TEST_DATA_PATH')])
+        model = self.find_file('layers/layer_convolution.caffemodel', [os.environ.get('OPENCV_DNN_TEST_DATA_PATH')])
+        net = cv.dnn.readNet(proto, model)
+        net.setPreferableBackend(backend)
+        net.setPreferableTarget(target)
+        inp = np.random.standard_normal([1, 2, 10, 11]).astype(np.float32)
+        try:
+            net.setInput(inp)
+            net.forward()
+        except BaseException as e:
+            return False
+        return True
 
+
+    def test_age_gender_infer(self):
+        if not self.checkIETarget(cv.dnn.DNN_BACKEND_INFERENCE_ENGINE, cv.dnn.DNN_TARGET_CPU):
+            # FIXME: OpenCV build without IE support, so skip this test
+            # Is there more graceful solution ?
+            return
+
+        root_path    = '/omz_intel_models/intel/age-gender-recognition-retail-0013/FP32/age-gender-recognition-retail-0013'
         model_path   = self.find_file(root_path + '.xml',   [os.environ.get('OPENCV_DNN_TEST_DATA_PATH')])
         weights_path = self.find_file(root_path + '.bin',   [os.environ.get('OPENCV_DNN_TEST_DATA_PATH')])
         img_path     = self.find_file('cv/face/david2.jpg', [os.environ.get('OPENCV_TEST_DATA_PATH')])
         device_id    = 'CPU'
         img          = cv.resize(cv.imread(img_path), (62,62))
 
-        try:
-            # OpenCV DNN
-            net = cv.dnn.readNetFromModelOptimizer(model_path, weights_path)
-            net.setPreferableBackend(cv.dnn.DNN_BACKEND_INFERENCE_ENGINE)
-            net.setPreferableTarget(cv.dnn.DNN_TARGET_CPU)
+        # OpenCV DNN
+        net = cv.dnn.readNetFromModelOptimizer(model_path, weights_path)
+        net.setPreferableBackend(cv.dnn.DNN_BACKEND_INFERENCE_ENGINE)
+        net.setPreferableTarget(cv.dnn.DNN_TARGET_CPU)
 
-            blob = cv.dnn.blobFromImage(img)
+        blob = cv.dnn.blobFromImage(img)
 
-            net.setInput(blob)
-            dnn_age, dnn_gender = net.forward(net.getUnconnectedOutLayersNames())
-        except BaseException as e:
-            # FIXME: OpenCV build without IE support, so skip this test
-            # Is there more graceful solution ?
-            return
+        net.setInput(blob)
+        dnn_age, dnn_gender = net.forward(net.getUnconnectedOutLayersNames())
 
         # OpenCV G-API
         g_in   = cv.GMat()
