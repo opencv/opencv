9022aff2dbed006190a413ef22ef7178352fdcc7
Jonathan Cole <joncole04412@gmail.com>
2020-11-11 15:18:58 -0500

    Add build-xcframework.sh

---
+++ platforms/apple/build-xcframework.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+set -e
+
+cd "`dirname \"$0\"`"
+
+mkdir -p ./xcframework-build/catalyst
+mkdir -p ./xcframework-build/iphoneos
+mkdir -p ./xcframework-build/iphonesimulator
+mkdir -p ./xcframework-build/osx
+
+# Build OSX slices
+python3 ../osx/build_framework.py --catalyst_archs x86_64 --without=objc --build-only-specified-archs ./xcframework-build/catalyst
+python3 ../osx/build_framework.py --archs x86_64 --without=objc --build-only-specified-archs ./xcframework-build/osx
+# Build iOS slices
+python3 ../ios/build_framework.py --iphoneos_archs arm64 --without=objc --build-only-specified-archs ./xcframework-build/iphoneos
+python3 ../ios/build_framework.py --iphonesimulator_archs x86_64 --without=objc --build-only-specified-archs ./xcframework-build/iphonesimulator
+
+xcframework -create-xcframework -framework ./xcframework-build/catalyst/opencv2.framework -framework ./xcframework-build/osx/opencv2.framework -framework ./xcframework-build/iphonesimulator/opencv2.framework -framework ./xcframework-build/iphoneos/opencv2.framework -output ./xcframework-build/opencv2.xcframework
