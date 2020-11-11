74600815f0b1f3900da98980c3c1ff226b817ad5
Jonathan Cole <joncole04412@gmail.com>
2020-11-11 16:11:45 -0500

    Update build-xcframework.sh

--- platforms/apple/build-xcframework.sh
+++ platforms/apple/build-xcframework.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
+
+# This script builds OpenCV into an xcframework compatible with all Apple platforms.
+# Just run it and grab a snack -- you'll be waiting a while.
+
 set -e
 
 cd "`dirname \"$0\"`"
@@ -14,5 +18,5 @@ python3 ../osx/build_framework.py --archs x86_64 --without=objc --build-only-spe
 # Build iOS slices
 python3 ../ios/build_framework.py --iphoneos_archs arm64 --without=objc --build-only-specified-archs ./xcframework-build/iphoneos
 python3 ../ios/build_framework.py --iphonesimulator_archs x86_64 --without=objc --build-only-specified-archs ./xcframework-build/iphonesimulator
-
-xcframework -create-xcframework -framework ./xcframework-build/catalyst/opencv2.framework -framework ./xcframework-build/osx/opencv2.framework -framework ./xcframework-build/iphonesimulator/opencv2.framework -framework ./xcframework-build/iphoneos/opencv2.framework -output ./xcframework-build/opencv2.xcframework
+# Stitch them all together into an xcframework
+xcodebuild -create-xcframework -framework ./xcframework-build/catalyst/opencv2.framework -framework ./xcframework-build/osx/opencv2.framework -framework ./xcframework-build/iphonesimulator/opencv2.framework -framework ./xcframework-build/iphoneos/opencv2.framework -output ./xcframework-build/opencv2.xcframework
