2e4a3d6e7619da775db9f68db31d8a7362535fa4
Jonathan Cole <joncole04412@gmail.com>
2020-11-12 15:34:27 -0500

    Document

--- platforms/apple/build-xcframework.py
+++ platforms/apple/build-xcframework.py
@@ -15,10 +15,26 @@ def get_or_create_folder_for_platform(platform):
     pathlib.Path(folder_name).mkdir(parents=True, exist_ok=True)
     return folder_name
 
-def get_build_command_for_platform(platform, destination, framework_name, only_64_bit=False):
+def get_framework_build_command_for_platform(platform, destination, framework_name, only_64_bit=False):
+    """
+    Generates the build command that creates a framework supporting the given platform.
+    This command can be handed off to the command line for execution.
+
+    Parameters
+    ----------
+    platform : str
+        The name of the platform you want to build for.
+    destination : str
+        The directory you want to build the framework into.
+    framework_name : str
+        The name of the generated framework.
+    only_64_bit : bool, optional
+        Build only 64-bit archs, by default False
+    """
     if platform == 'macos':
         return "python3 ../osx/build_framework.py --archs %s --framework_name %s --without=objc --build-only-specified-archs %s" % ("x86_64,arm64", framework_name, destination)
     elif platform == 'ios-maccatalyst':
+        # This is not a mistake. For Catalyst, we use the osx toolchain.
         return "python3 ../osx/build_framework.py --catalyst_archs %s --framework_name %s --without=objc --build-only-specified-archs %s" % ("x86_64,arm64", framework_name, destination)
     elif platform == 'ios':
         return "python3 ../ios/build_framework.py --iphoneos_archs %s --framework_name %s --without=objc --build-only-specified-archs %s" % ("arm64" if only_64_bit else "arm64,armv7,armv7s", framework_name, destination)
@@ -40,16 +56,19 @@ if __name__ == "__main__":
     print(f'Building for platforms: {platforms}')
 
     try:
+        # Build .frameworks for each platform
         build_folders = []
         for platform in platforms:
             folder = get_or_create_folder_for_platform(platform)
             build_folders.append(folder)
-            framework_build_command = get_build_command_for_platform(platform, folder, args.framework_name, args.only_64_bit)
+            framework_build_command = get_framework_build_command_for_platform(platform, folder, args.framework_name, args.only_64_bit)
             execute(framework_build_command)
 
+        # Put all the built .frameworks together into a .xcframework
         framework_args = " ".join([f"-framework {folder}/{args.framework_name}.framework" for folder in build_folders])
         xcframework_build_command = f"xcodebuild -create-xcframework {framework_args} -output {args.out}/{args.framework_name}.xcframework"
         execute(xcframework_build_command)
+
         print(f"Finished building {args.out}/{args.framework_name}.xcframework")
     except Exception as e:
         print_error(e)
