00e50670b9a2890b72273a31ce15b47d80a7ea5b
Chris Ballinger <cballinger@rightpoint.com>
2020-11-17 12:58:54 -0800

    Working on Swift Package Manager manifest

---
+++ Package.swift
@@ -0,0 +1,36 @@
+// swift-tools-version:5.3
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
+import PackageDescription
+
+let package = Package(
+    name: "OpenCV",
+    products: [
+        .library(
+            name: "OpenCV",
+            targets: ["OpenCV"]),
+    ],
+    dependencies: [],
+    targets: [
+        .target(
+            name: "OpenCV",
+            dependencies: ["opencv2"],
+            path: "modules/core/misc/objc/swift-package-manager/Sources"),
+        // Recompute checksum via `swift package --package-path /path/to/opencv compute-checksum /path/to/opencv2.xcframework.zip`
+//        .binaryTarget(
+//            name: "opencv2",
+//            url: "https://url/to/some/remote/opencv2.xcframework.zip",
+//            checksum: "The checksum of the ZIP archive that contains the XCFramework."
+//        ),
+        // If you are compiling OpenCV locally, you can uncomment the below block to use a custom copy
+        .binaryTarget(
+            name: "opencv2",
+            path: "platforms/apple/xcframework-build/opencv2.xcframework"
+        ),
+        .testTarget(
+            name: "OpenCVTests",
+            dependencies: ["OpenCV"],
+            path: "modules/core/misc/objc/swift-package-manager/Tests"
+            ),
+    ]
+)
---
+++ modules/core/misc/objc/swift-package-manager/Tests/OpenCVTests.swift
@@ -0,0 +1,8 @@
+//
+//  OpenCVTests.swift
+//
+//  Created by Chris Ballinger on 2020/11/17.
+//
+
+import Foundation
+import OpenCV
