ed46c569d903c631547369dadf29fc649442dc95
Chris Ballinger <cballinger@rightpoint.com>
2020-11-18 13:43:35 -0800

    Add backwards compatibility Swift typealiases for Point, Rect, Size

---
+++ modules/core/misc/objc/common/Typealiases.swift
@@ -0,0 +1,11 @@
+//
+//  Typealiases.swift
+//
+//  Created by Chris Ballinger on 2020/11/18.
+//
+
+import Foundation
+
+public typealias Rect = Rect2i
+public typealias Point = Point2i
+public typealias Size = Size2i
