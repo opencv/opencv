f4339f715f5358bb5e09e9c744042d5aa5b5ff44
Chris Ballinger <cballinger@rightpoint.com>
2020-11-18 14:07:17 -0800

    Add binary xcframework URL

--- Package.swift
+++ Package.swift
@@ -25,17 +25,17 @@ let package = Package(
             ]
         ),
         // Recompute checksum via `swift package --package-path /path/to/opencv compute-checksum /path/to/opencv2.xcframework.zip`
-//        .binaryTarget(
-//            name: "opencv2",
-//            url: "https://url/to/some/remote/opencv2.xcframework.zip",
-//            checksum: "The checksum of the ZIP archive that contains the XCFramework."
-//        ),
-        // If you are compiling OpenCV locally, you can uncomment the below block to use a custom copy
-        // e.g. `$ python platforms/apple/build_xcframework.py platforms/apple/xcframework-build/`
         .binaryTarget(
             name: "opencv2",
-            path: "platforms/apple/xcframework-build/opencv2.xcframework"
+            url: "https://github.com/Rightpoint/opencv/releases/download/4.5.1/opencv-4.5.1-xcframework.zip",
+            checksum: "891e8756cb7e4073ff7d319c1edbff907eab022408f3ca639320d3d660ac9873"
         ),
+        // If you are compiling OpenCV locally, you can uncomment the below block to use a custom copy
+        // e.g. `$ python platforms/apple/build_xcframework.py platforms/apple/xcframework-build/`
+//        .binaryTarget(
+//            name: "opencv2",
+//            path: "platforms/apple/xcframework-build/opencv2.xcframework"
+//        ),
         .testTarget(
             name: "OpenCVTests",
             dependencies: ["OpenCV"],
