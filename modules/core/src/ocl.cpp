472f2d0a25d184532153868886396fb2656d1123
Dale Phurrough <dale@hidale.com>
2020-11-25 01:53:41 +0100

    fix missing addref() in ocl::Context::create(str)
    - fix https://github.com/opencv/opencv/issues/18906
    - unable to add related test cases as there is
      no public access to Context:Impl refcounts

--- modules/core/src/ocl.cpp
+++ modules/core/src/ocl.cpp
@@ -2437,6 +2437,7 @@ public:
         if (impl)
         {
             CV_LOG_INFO(NULL, "OpenCL: reuse context@" << impl->contextId << " for configuration: " << configuration)
+            impl->addref();
             return impl;
         }
 
