980c4d00316af6491dcf857de3bafdba7419aac7
Sergey Slashchinin <sergei.slashchinin@xperience.ai>
2020-11-30 23:45:48 +0300

    Fix build when HAVE_VULKAN is ON

--- modules/dnn/src/layers/pooling_layer.cpp
+++ modules/dnn/src/layers/pooling_layer.cpp
@@ -440,9 +440,9 @@ public:
     {
         int padding_mode;
         vkcom::PoolType pool_type;
-        int filter_size[2] = {kernel.height, kernel.width};
-        int pad_size[2] = {pad.height, pad.width};
-        int stride_size[2] = {stride.height, stride.width};
+        int filter_size[2] = {kernel_size[0], kernel_size[1]};
+        int pad_size[2] = {pads_begin[0], pads_begin[1]};
+        int stride_size[2] = {strides[0], strides[1]};
         pool_type = type == MAX ? vkcom::kPoolTypeMax:
                    (type == AVE ? vkcom::kPoolTypeAvg:
                             vkcom::kPoolTypeNum);
