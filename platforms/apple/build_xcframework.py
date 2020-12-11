9f522445748949f7ec2a62aaeb350deec66b0996
Jonathan Cole <joncole04412@gmail.com>
2020-12-11 02:14:28 -0500

    Merge pull request #19076 from Rightpoint:bugfix/colejd/fix-path-resolution-bugs
    Fix path resolution bugs for XCFramework builds
    
    * Fix incorrect paths for intermediate frameworks
    
    * Remove unnecessary `./` prepend preventing use of absolute paths

--- platforms/apple/build_xcframework.py
+++ platforms/apple/build_xcframework.py
@@ -74,7 +74,7 @@ if __name__ == "__main__":
         build_folders = []
 
         def get_or_create_build_folder(base_dir, platform):
-            build_folder = "./{}/{}".format(base_dir, platform).replace(" ", "\\ ")  # Escape spaces in output path
+            build_folder = "{}/{}".format(base_dir, platform).replace(" ", "\\ ")  # Escape spaces in output path
             pathlib.Path(build_folder).mkdir(parents=True, exist_ok=True)
             return build_folder
 
@@ -122,7 +122,7 @@ if __name__ == "__main__":
             xcframework_path,
         ]
         for folder in build_folders:
-            xcframework_build_command += ["-framework", xcframework_path]
+            xcframework_build_command += ["-framework", "{}/{}.framework".format(folder, args.framework_name)]
         execute(xcframework_build_command, cwd=os.getcwd())
 
         print("")
