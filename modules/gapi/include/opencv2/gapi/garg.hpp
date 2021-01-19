46ca990db6851d7aacd740b2b94cec3dbba4ce8c
Anatoliy Talamanov <anatoliy.talamanov@intel.com>
2021-01-19 14:54:51 +0300

    Add using to a void exceeded length for windows build

--- modules/gapi/include/opencv2/gapi/garg.hpp
+++ modules/gapi/include/opencv2/gapi/garg.hpp
@@ -257,13 +257,15 @@ namespace detail {
     struct ExtractArgsCallback
     {
         cv::GRunArgs operator()(const cv::GTypesInfo& info) const { return c(info); }
-        std::function<cv::GRunArgs(const cv::GTypesInfo& info)> c;
+        using CallBackT = std::function<cv::GRunArgs(const cv::GTypesInfo& info)>;
+        CallBackT c;
     };
 
     struct ExtractMetaCallback
     {
         cv::GMetaArgs operator()(const cv::GTypesInfo& info) const { return c(info); }
-        std::function<cv::GMetaArgs(const cv::GTypesInfo& info)> c;
+        using CallBackT = std::function<cv::GMetaArgs(const cv::GTypesInfo& info)>;
+        CallBackT c;
     };
 } // namespace detail
 
