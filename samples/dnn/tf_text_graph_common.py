681a85a4fc43efa33a76332c14727bb24c778749
Anastasia Murzova <anastasia.murzova@xperience.ai>
2021-01-28 20:53:29 +0300

    Corrected SSD text graph generation for Identity nodes

--- samples/dnn/tf_text_graph_common.py
+++ samples/dnn/tf_text_graph_common.py
@@ -270,7 +270,10 @@ def removeIdentity(graph_def):
     identities = {}
     for node in graph_def.node:
         if node.op == 'Identity' or node.op == 'IdentityN':
-            identities[node.name] = node.input[0]
+            if node.input[0] in identities:
+                identities[node.name] = identities[node.input[0]]
+            else:
+                identities[node.name] = node.input[0]
             graph_def.node.remove(node)
 
     for node in graph_def.node:
