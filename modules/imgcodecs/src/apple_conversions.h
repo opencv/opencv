fb78db7c17607a31c083c44791d5117a8392d5c0
Mark Harfouche <mark.harfouche@gmail.com>
2021-02-05 23:24:56 -0500

    CHange import order to avoid apple check error

--- modules/imgcodecs/src/apple_conversions.h
+++ modules/imgcodecs/src/apple_conversions.h
@@ -2,10 +2,10 @@
 // It is subject to the license terms in the LICENSE file found in the top-level directory
 // of this distribution and at http://opencv.org/license.html.
 
+#include "opencv2/core.hpp"
 #import <Accelerate/Accelerate.h>
 #import <AVFoundation/AVFoundation.h>
 #import <ImageIO/ImageIO.h>
-#include "opencv2/core.hpp"
 
 CV_EXPORTS CGImageRef MatToCGImage(const cv::Mat& image) CF_RETURNS_RETAINED;
 CV_EXPORTS void CGImageToMat(const CGImageRef image, cv::Mat& m, bool alphaExist);
