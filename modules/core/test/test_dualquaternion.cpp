d6a38c56a7b36e9ca0bbab7625a7b1f8a9c6ef17
arsaratovtsev <arsaratovtsev@intel.com>
2021-02-16 11:43:07 +0300

    win cl warning fix

--- modules/core/test/test_dualquaternion.cpp
+++ modules/core/test/test_dualquaternion.cpp
@@ -150,8 +150,8 @@ TEST_F(DualQuatTest, interpolation) {
     gdqb_d.at<Vec<double, 7>>(0, 1) = Vec<double, 7>{1,2,3,4,5,6,7};
     EXPECT_ANY_THROW(DualQuatd::gdqblend(gdqb_d, Vec2d{0.5, 0.5}));
     Mat gdqb_f(1, 2, CV_32FC(8));
-    gdqb_f.at<Vec<float, 8>>(0, 0) = Vec<float, 8>{1,2,3,4,5,6,7,8};
-    gdqb_f.at<Vec<float, 8>>(0, 1) = Vec<float, 8>{1,2,3,4,5,6,7,8};
+    gdqb_f.at<Vec<float, 8>>(0, 0) = Vec<float, 8>{1.f,2.f,3.f,4.f,5.f,6.f,7.f,8.f};
+    gdqb_f.at<Vec<float, 8>>(0, 1) = Vec<float, 8>{1.f,2.f,3.f,4.f,5.f,6.f,7.f,8.f};
     EXPECT_ANY_THROW(DualQuatd::gdqblend(gdqb_f, Vec2d{0.5, 0.5}));
     EXPECT_ANY_THROW(DualQuatd::gdqblend(Vec<DualQuatd, 3>{dqIdentity, dq, dq2}, Vec3f{0.4, 0, 0.6}, QUAT_ASSUME_UNIT));
     EXPECT_EQ(gdb, DualQuatd::gdqblend(Vec<DualQuatd, 3>{dqIdentity, dq * dualNumber1, -dq2}, Vec3d{0.4, 0, 0.6}));
