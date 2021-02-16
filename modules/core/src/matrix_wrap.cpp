6d3502833f85f0ea97817942498fe662abdf7175
Alexander Alekhin <alexander.a.alekhin@gmail.com>
2021-02-16 10:06:31 +0000

    core: include version.hpp in cvdef.h, fix precomp.hpp usage

--- modules/core/src/matrix_wrap.cpp
+++ modules/core/src/matrix_wrap.cpp
@@ -2,9 +2,8 @@
 // It is subject to the license terms in the LICENSE file found in the top-level directory
 // of this distribution and at http://opencv.org/license.html
 
-
-#include "opencv2/core/mat.hpp"
 #include "precomp.hpp"
+#include "opencv2/core/mat.hpp"
 
 namespace cv {
 
