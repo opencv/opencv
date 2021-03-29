b2942b947c2cb21e594d748f022ff5747866498c
Maxim Pashchenkov <maxim.pashchenkov@intel.com>
2021-03-29 23:59:55 +0300

    Merge pull request #19792 from mpashchenkov:mp/ie-add-int32
    G-API: IE. Adding support for INT32 type.
    
    * Added support for int32
    
    * Added sample for semantic-segmentation-adas-0001
    
    * Alignment
    
    * Alignment 2
    
    * Rstrt build
    
    * Removed test for sem seg

--- modules/gapi/src/backends/ie/giebackend.cpp
+++ modules/gapi/src/backends/ie/giebackend.cpp
@@ -104,6 +104,7 @@ inline IE::Layout toIELayout(const std::size_t ndims) {
 inline IE::Precision toIE(int depth) {
     switch (depth) {
     case CV_8U:  return IE::Precision::U8;
+    case CV_32S: return IE::Precision::I32;
     case CV_32F: return IE::Precision::FP32;
     default:     GAPI_Assert(false && "Unsupported data type");
     }
@@ -113,6 +114,7 @@ inline int toCV(IE::Precision prec) {
     switch (prec) {
     case IE::Precision::U8:   return CV_8U;
     case IE::Precision::FP32: return CV_32F;
+    case IE::Precision::I32:  return CV_32S;
     default:     GAPI_Assert(false && "Unsupported data type");
     }
     return -1;
@@ -154,6 +156,7 @@ inline IE::Blob::Ptr wrapIE(const cv::Mat &mat, cv::gapi::ie::TraitAs hint) {
         case CV_##E: return IE::make_shared_blob<T>(tDesc, const_cast<T*>(mat.ptr<T>()))
         HANDLE(8U, uint8_t);
         HANDLE(32F, float);
+        HANDLE(32S, int);
 #undef HANDLE
     default: GAPI_Assert(false && "Unsupported data type");
     }
@@ -189,6 +192,7 @@ inline void copyFromIE(const IE::Blob::Ptr &blob, MatType &mat) {
             break;
         HANDLE(U8, uint8_t);
         HANDLE(FP32, float);
+        HANDLE(I32, int);
 #undef HANDLE
     default: GAPI_Assert(false && "Unsupported data type");
     }
