4e691587ef9c2fb3bea5f3451bccb2ac2a04f9c0
danielenricocahall <danielenricocahall@gmail.com>
2021-04-04 09:06:21 -0400

    remove virtual inheritance from features2d

--- modules/features2d/include/opencv2/features2d.hpp
+++ modules/features2d/include/opencv2/features2d.hpp
@@ -141,11 +141,7 @@ public:
 
 /** @brief Abstract base class for 2D image feature detectors and descriptor extractors
 */
-#ifdef __EMSCRIPTEN__
 class CV_EXPORTS_W Feature2D : public Algorithm
-#else
-class CV_EXPORTS_W Feature2D : public virtual Algorithm
-#endif
 {
 public:
     virtual ~Feature2D();
