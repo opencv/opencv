fd5e03b8ac0c13034acb2fe561b4368479a596ea
danielenricocahall <danielenricocahall@gmail.com>
2021-04-09 06:54:51 -0400

    check 1..n-1 and n+1 in feature size validation test

--- modules/ml/test/test_rtrees.cpp
+++ modules/ml/test/test_rtrees.cpp
@@ -97,16 +97,22 @@ TEST(ML_RTrees, 11142_sample_weights_classification)
 
 TEST(ML_RTrees, bug_12974_throw_exception_when_predict_different_feature_count)
 {
-    // create a simple 2 feature dataset and train + save the model
+    int numFeatures = 5;
+    // create a 5 feature dataset and train the model
     cv::Ptr<RTrees> model = RTrees::create();
-    Mat samples(2, 2, CV_32F);
+    Mat samples(10, numFeatures, CV_32F);
     randu(samples, 0, 10);
-    Mat labels = (Mat_<int>(2,1) << 0,1);
+    Mat labels = (Mat_<int>(10,1) << 0,0,0,0,0,1,1,1,1,1);
     cv::Ptr<TrainData> trainData = TrainData::create(samples, cv::ml::ROW_SAMPLE, labels);
     model->train(trainData);
-    // load the model and try to predict on a 1 feature sample
-    Mat test(1, 1, CV_32FC1);
-    ASSERT_THROW(model -> predict(test), Exception);
+    // try to predict on data which have fewer features - this should throw an exception
+    for(int i = 1; i < numFeatures - 1; ++i) {
+        Mat test(1, i, CV_32FC1);
+        ASSERT_THROW(model->predict(test), Exception);
+    }
+    // try to predict on data which have more features - this should also throw an exception
+    Mat test(1, numFeatures + 1, CV_32FC1);
+    ASSERT_THROW(model->predict(test), Exception);
 }
 
 
