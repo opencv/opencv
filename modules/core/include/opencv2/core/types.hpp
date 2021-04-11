da9382fbc0b6c42fae7bb8732f81c500b6ea3b74
danielenricocahall <danielenricocahall@gmail.com>
2021-04-11 09:02:26 -0400

    convert type and add docs for layer

--- modules/core/include/opencv2/core/types.hpp
+++ modules/core/include/opencv2/core/types.hpp
@@ -802,7 +802,7 @@ public:
     CV_PROP_RW float response; //!< the response by which the most strong keypoints have been selected. Can be used for the further sorting or subsampling
     CV_PROP_RW int octave; //!< octave (pyramid layer) from which the keypoint has been extracted
     CV_PROP_RW int class_id; //!< object class (if the keypoints need to be clustered by an object they belong to)
-    CV_PROP_RW float layer; //!< layer >
+    CV_PROP_RW int layer; //!< layer of the octave (0 if not applicable) >
 };
 
 #ifdef OPENCV_TRAITS_ENABLE_DEPRECATED
