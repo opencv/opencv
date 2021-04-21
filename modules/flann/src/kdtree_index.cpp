aa92a49c0632162409fb7f42caee756cf6c9decf
danielenricocahall <danielenricocahall@gmail.com>
2021-04-20 21:53:49 -0400

    delete unnecessary file

--- modules/flann/src/kdtree_index.cpp
+++
@@ -1,9 +0,0 @@
-//
-// Created by daniel on 4/18/21.
-//
-
-#include "general.h"
-#include "kdtree_index.h"
-
-cvflann::KDTreeIndex<cvflann::L2<float> >::heap_ = nullptr;
-cvflann::KDTreeIndex<cvflann::L1<float> >::heap_ = nullptr;
