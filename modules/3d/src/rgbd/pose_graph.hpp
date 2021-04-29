c6c2000b5908a8a6baf361aaeccdf141375ce05d
Rostislav Vasilikhin <rostislav.vasilikhin@intel.com>
2021-04-30 02:44:55 +0300

    removed duplicates, file version updated

--- modules/3d/src/rgbd/pose_graph.hpp
+++ modules/3d/src/rgbd/pose_graph.hpp
@@ -1,321 +1,59 @@
-#ifndef OPENCV_RGBD_GRAPH_NODE_H
-#define OPENCV_RGBD_GRAPH_NODE_H
+// This file is part of OpenCV project.
+// It is subject to the license terms in the LICENSE file found in the top-level directory
+// of this distribution and at http://opencv.org/license.html
 
-#include <map>
-#include <unordered_map>
+#ifndef OPENCV_RGBD_POSE_GRAPH_HPP
+#define OPENCV_RGBD_POSE_GRAPH_HPP
 
 #include "opencv2/core/affine.hpp"
-#if defined(HAVE_EIGEN)
-#include <Eigen/Core>
-#include <Eigen/Geometry>
-#include "opencv2/core/eigen.hpp"
-#endif
-
-#if defined(CERES_FOUND)
-#include <ceres/ceres.h>
-#endif
+#include "opencv2/core/quaternion.hpp"
 
 namespace cv
 {
-namespace kinfu
+namespace detail
 {
-/*! \class GraphNode
- *  \brief Defines a node/variable that is optimizable in a posegraph
- *
- *  Detailed description
- */
-#if defined(HAVE_EIGEN)
-struct Pose3d
+
+// ATTENTION! This class is used internally in Large KinFu.
+// It has been pushed to publicly available headers for tests only.
+// Source compatibility of this API is not guaranteed in the future.
+
+// This class provides tools to solve so-called pose graph problem often arisen in SLAM problems
+// The pose graph format, cost function and optimization techniques
+// repeat the ones used in Ceres 3D Pose Graph Optimization:
+// http://ceres-solver.org/nnls_tutorial.html#other-examples, pose_graph_3d.cc bullet
+class CV_EXPORTS_W PoseGraph
 {
-    EIGEN_MAKE_ALIGNED_OPERATOR_NEW
+public:
+    static Ptr<PoseGraph> create();
+    virtual ~PoseGraph();
 
-    Eigen::Vector3d t;
-    Eigen::Quaterniond r;
+    // Node may have any id >= 0
+    virtual void addNode(size_t _nodeId, const Affine3d& _pose, bool fixed) = 0;
+    virtual bool isNodeExist(size_t nodeId) const = 0;
+    virtual bool setNodeFixed(size_t nodeId, bool fixed) = 0;
+    virtual bool isNodeFixed(size_t nodeId) const = 0;
+    virtual Affine3d getNodePose(size_t nodeId) const = 0;
+    virtual std::vector<size_t> getNodesIds() const = 0;
+    virtual size_t getNumNodes() const = 0;
 
-    Pose3d()
-    {
-        t.setZero();
-        r.setIdentity();
-    };
-    Pose3d(const Eigen::Matrix3d& rotation, const Eigen::Vector3d& translation)
-        : t(translation), r(Eigen::Quaterniond(rotation))
-    {
-        normalizeRotation();
-    }
+    // Edges have consequent indices starting from 0
+    virtual void addEdge(size_t _sourceNodeId, size_t _targetNodeId, const Affine3f& _transformation,
+                         const Matx66f& _information = Matx66f::eye()) = 0;
+    virtual size_t getEdgeStart(size_t i) const = 0;
+    virtual size_t getEdgeEnd(size_t i) const = 0;
+    virtual size_t getNumEdges() const = 0;
 
-    Pose3d(const Matx33d& rotation, const Vec3d& translation)
-    {
-        Eigen::Matrix3d R;
-        cv2eigen(rotation, R);
-        cv2eigen(translation, t);
-        r = Eigen::Quaterniond(R);
-        normalizeRotation();
-    }
+    // checks if graph is connected and each edge connects exactly 2 nodes
+    virtual bool isValid() const = 0;
 
-    explicit Pose3d(const Matx44f& pose)
-    {
-        Matx33d rotation(pose.val[0], pose.val[1], pose.val[2], pose.val[4], pose.val[5],
-                         pose.val[6], pose.val[8], pose.val[9], pose.val[10]);
-        Vec3d translation(pose.val[3], pose.val[7], pose.val[11]);
-        Pose3d(rotation, translation);
-    }
+    // Termination criteria are max number of iterations and min relative energy change to current energy
+    // Returns number of iterations elapsed or -1 if max number of iterations was reached or failed to optimize
+    virtual int optimize(const cv::TermCriteria& tc = cv::TermCriteria(TermCriteria::COUNT+TermCriteria::EPS, 100, 1e-6)) = 0;
 
-    // NOTE: Eigen overloads quaternion multiplication appropriately
-    inline Pose3d operator*(const Pose3d& otherPose) const
-    {
-        Pose3d out(*this);
-        out.t += r * otherPose.t;
-        out.r *= otherPose.r;
-        out.normalizeRotation();
-        return out;
-    }
-
-    inline Pose3d& operator*=(const Pose3d& otherPose)
-    {
-        t += otherPose.t;
-        r *= otherPose.r;
-        normalizeRotation();
-        return *this;
-    }
-
-    inline Pose3d inverse() const
-    {
-        Pose3d out;
-        out.r = r.conjugate();
-        out.t = out.r * (t * -1.0);
-        return out;
-    }
-
-    inline void normalizeRotation()
-    {
-        if (r.w() < 0)
-            r.coeffs() *= -1.0;
-        r.normalize();
-    }
-};
-#endif
-
-struct PoseGraphNode
-{
-   public:
-    explicit PoseGraphNode(int _nodeId, const Affine3f& _pose)
-        : nodeId(_nodeId), isFixed(false), pose(_pose)
-    {
-#if defined(HAVE_EIGEN)
-        se3Pose = Pose3d(_pose.rotation(), _pose.translation());
-#endif
-    }
-    virtual ~PoseGraphNode() = default;
-
-    int getId() const { return nodeId; }
-    inline Affine3f getPose() const
-    {
-        return pose;
-    }
-    void setPose(const Affine3f& _pose)
-    {
-        pose = _pose;
-#if defined(HAVE_EIGEN)
-        se3Pose = Pose3d(pose.rotation(), pose.translation());
-#endif
-    }
-#if defined(HAVE_EIGEN)
-    void setPose(const Pose3d& _pose)
-    {
-        se3Pose = _pose;
-        const Eigen::Matrix3d& rotation    = se3Pose.r.toRotationMatrix();
-        const Eigen::Vector3d& translation = se3Pose.t;
-        Matx33d rot;
-        Vec3d trans;
-        eigen2cv(rotation, rot);
-        eigen2cv(translation, trans);
-        Affine3d poseMatrix(rot, trans);
-        pose = poseMatrix;
-    }
-#endif
-    void setFixed(bool val = true) { isFixed = val; }
-    bool isPoseFixed() const { return isFixed; }
-
-   public:
-    int nodeId;
-    bool isFixed;
-    Affine3f pose;
-#if defined(HAVE_EIGEN)
-    Pose3d se3Pose;
-#endif
+    // calculate cost function based on current nodes parameters
+    virtual double calcEnergy() const = 0;
 };
 
-/*! \class PoseGraphEdge
- *  \brief Defines the constraints between two PoseGraphNodes
- *
- *  Detailed description
- */
-struct PoseGraphEdge
-{
-   public:
-    PoseGraphEdge(int _sourceNodeId, int _targetNodeId, const Affine3f& _transformation,
-                  const Matx66f& _information = Matx66f::eye())
-        : sourceNodeId(_sourceNodeId),
-          targetNodeId(_targetNodeId),
-          transformation(_transformation),
-          information(_information)
-    {
-    }
-    virtual ~PoseGraphEdge() = default;
-
-    int getSourceNodeId() const { return sourceNodeId; }
-    int getTargetNodeId() const { return targetNodeId; }
-
-    bool operator==(const PoseGraphEdge& edge)
-    {
-        if ((edge.getSourceNodeId() == sourceNodeId && edge.getTargetNodeId() == targetNodeId) ||
-            (edge.getSourceNodeId() == targetNodeId && edge.getTargetNodeId() == sourceNodeId))
-            return true;
-        return false;
-    }
-
-   public:
-    int sourceNodeId;
-    int targetNodeId;
-    Affine3f transformation;
-    Matx66f information;
-};
-
-//! @brief Reference: A tutorial on SE(3) transformation parameterizations and on-manifold
-//! optimization Jose Luis Blanco Compactly represents the jacobian of the SE3 generator
-// clang-format off
-/* static const std::array<Matx44f, 6> generatorJacobian = { */
-/*     // alpha */
-/*     Matx44f(0, 0,  0, 0, */
-/*             0, 0, -1, 0, */
-/*             0, 1,  0, 0, */
-/*             0, 0,  0, 0), */
-/*     // beta */
-/*     Matx44f( 0, 0, 1, 0, */
-/*              0, 0, 0, 0, */
-/*             -1, 0, 0, 0, */
-/*              0, 0, 0, 0), */
-/*     // gamma */
-/*     Matx44f(0, -1, 0, 0, */
-/*             1,  0, 0, 0, */
-/*             0,  0, 0, 0, */
-/*             0,  0, 0, 0), */
-/*     // x */
-/*     Matx44f(0, 0, 0, 1, */
-/*             0, 0, 0, 0, */
-/*             0, 0, 0, 0, */
-/*             0, 0, 0, 0), */
-/*     // y */
-/*     Matx44f(0, 0, 0, 0, */
-/*             0, 0, 0, 1, */
-/*             0, 0, 0, 0, */
-/*             0, 0, 0, 0), */
-/*     // z */
-/*     Matx44f(0, 0, 0, 0, */
-/*             0, 0, 0, 0, */
-/*             0, 0, 0, 1, */
-/*             0, 0, 0, 0) */
-/* }; */
-// clang-format on
-
-class PoseGraph
-{
-   public:
-    typedef std::vector<PoseGraphNode> NodeVector;
-    typedef std::vector<PoseGraphEdge> EdgeVector;
-
-    explicit PoseGraph(){};
-    virtual ~PoseGraph() = default;
-
-    //! PoseGraph can be copied/cloned
-    PoseGraph(const PoseGraph& _poseGraph) = default;
-    PoseGraph& operator=(const PoseGraph& _poseGraph) = default;
-
-    void addNode(const PoseGraphNode& node) { nodes.push_back(node); }
-    void addEdge(const PoseGraphEdge& edge) { edges.push_back(edge); }
-
-    bool nodeExists(int nodeId) const
-    {
-        return std::find_if(nodes.begin(), nodes.end(), [nodeId](const PoseGraphNode& currNode) {
-                   return currNode.getId() == nodeId;
-               }) != nodes.end();
-    }
-
-    bool isValid() const;
-
-    int getNumNodes() const { return int(nodes.size()); }
-    int getNumEdges() const { return int(edges.size()); }
-
-   public:
-    NodeVector nodes;
-    EdgeVector edges;
-};
-
-namespace Optimizer
-{
-void optimize(PoseGraph& poseGraph);
-
-#if defined(CERES_FOUND)
-void createOptimizationProblem(PoseGraph& poseGraph, ceres::Problem& problem);
-
-//! Error Functor required for Ceres to obtain an auto differentiable cost function
-class Pose3dErrorFunctor
-{
-   public:
-    EIGEN_MAKE_ALIGNED_OPERATOR_NEW
-    Pose3dErrorFunctor(const Pose3d& _poseMeasurement, const Matx66d& _sqrtInformation)
-        : poseMeasurement(_poseMeasurement)
-    {
-        cv2eigen(_sqrtInformation, sqrtInfo);
-    }
-    Pose3dErrorFunctor(const Pose3d& _poseMeasurement,
-                       const Eigen::Matrix<double, 6, 6>& _sqrtInformation)
-        : poseMeasurement(_poseMeasurement), sqrtInfo(_sqrtInformation)
-    {
-    }
-
-    template<typename T>
-    bool operator()(const T* const _pSourceTrans, const T* const _pSourceQuat,
-                    const T* const _pTargetTrans, const T* const _pTargetQuat, T* _pResidual) const
-    {
-        Eigen::Map<const Eigen::Matrix<T, 3, 1>> sourceTrans(_pSourceTrans);
-        Eigen::Map<const Eigen::Matrix<T, 3, 1>> targetTrans(_pTargetTrans);
-        Eigen::Map<const Eigen::Quaternion<T>> sourceQuat(_pSourceQuat);
-        Eigen::Map<const Eigen::Quaternion<T>> targetQuat(_pTargetQuat);
-        Eigen::Map<Eigen::Matrix<T, 6, 1>> residual(_pResidual);
-
-        Eigen::Quaternion<T> targetQuatInv = targetQuat.conjugate();
-
-        Eigen::Quaternion<T> relativeQuat    = targetQuatInv * sourceQuat;
-        Eigen::Matrix<T, 3, 1> relativeTrans = targetQuatInv * (targetTrans - sourceTrans);
-
-        //! Definition should actually be relativeQuat * poseMeasurement.r.conjugate()
-        Eigen::Quaternion<T> deltaRot =
-            poseMeasurement.r.template cast<T>() * relativeQuat.conjugate();
-
-        residual.template block<3, 1>(0, 0) = relativeTrans - poseMeasurement.t.template cast<T>();
-        residual.template block<3, 1>(3, 0) = T(2.0) * deltaRot.vec();
-
-        residual.applyOnTheLeft(sqrtInfo.template cast<T>());
-
-        return true;
-    }
-
-    static ceres::CostFunction* create(const Pose3d& _poseMeasurement,
-                                       const Matx66f& _sqrtInformation)
-    {
-        return new ceres::AutoDiffCostFunction<Pose3dErrorFunctor, 6, 3, 4, 3, 4>(
-            new Pose3dErrorFunctor(_poseMeasurement, _sqrtInformation));
-    }
-
-   private:
-    const Pose3d poseMeasurement;
-    Eigen::Matrix<double, 6, 6> sqrtInfo;
-};
-#endif
-
-}  // namespace Optimizer
-
-}  // namespace kinfu
+}  // namespace detail
 }  // namespace cv
-#endif /* ifndef OPENCV_RGBD_GRAPH_NODE_H */
+#endif /* ifndef OPENCV_RGBD_POSE_GRAPH_HPP */
