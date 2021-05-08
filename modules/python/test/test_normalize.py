09ab5ac37b2d49d2e30e45f90efaf6bf5aa2f7a1
danielenricocahall <danielenricocahall@gmail.com>
2021-05-07 22:29:23 -0400

    update test normalize

--- modules/python/test/test_normalize.py
+++ modules/python/test/test_normalize.py
@@ -6,7 +6,6 @@ class normalize_test(NewOpenCVTests):
 
     def test_normalize(self):
         img = np.ones((2, 2))
-        img[0, 0] = 10
         # we should be able to call normalize with no additional args now
-        res = cv.normalize(img, 0, 1, cv.NORM_MINMAX)
-        self.assertEqual(res.tolist(), [[1.0, 0.0], [0.0, 0.0]])
+        res = cv.normalize(img)
+        self.assertEqual(res.tolist(), [[0.5, 0.5], [0.5, 0.5]])
