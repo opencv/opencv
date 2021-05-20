115e471515813182b93066f5d0f445f25675f68b
HattrickGenerator <71079716+HattrickGenerator@users.noreply.github.com>
2021-05-20 21:21:34 +0200

    Merge pull request #19967 from HattrickGenerator:master
    * Adding functions rbegin() and rend() functions to matrix class.
    This is important to be more standard compliant with C++ and an ever increasing number of people using standard algorithms for better code readability- and maintainability.
    
    The functions are copy pated from their counterparts (even though they should probably call the counterparts but this gave me some troube).
    They return iterators using std::reverse_iterators
    
    Follow up of an open feature request:
    https://github.com/opencv/opencv/issues/4641
    
    * Fix rbegin() and rend() and provide tests for them
    
    * Removing unnecessary whitespaces
    
    * Adding rbegin and rend to Mat_ class with the right parameters so we don't need to repeat the template argument.
    An instantiating cv::Mat_<int> for example can call it's rbegin() function and doesn't need rbegin<int>() with this convience addition.
    
    Follows what is done for forward iterators
    
    * static cast the vector size (return size_t) to an int (that is required for opencv mat constructor)
    
    Co-authored-by: Stefan <stefan.gerl@tum.de>

--- modules/core/include/opencv2/core/mat.hpp
+++ modules/core/include/opencv2/core/mat.hpp
@@ -2011,6 +2011,11 @@ public:
     template<typename _Tp> MatIterator_<_Tp> begin();
     template<typename _Tp> MatConstIterator_<_Tp> begin() const;
 
+    /** @brief Same as begin() but for inverse traversal
+     */
+    template<typename _Tp> std::reverse_iterator<MatIterator_<_Tp>> rbegin();
+    template<typename _Tp> std::reverse_iterator<MatConstIterator_<_Tp>> rbegin() const;
+
     /** @brief Returns the matrix iterator and sets it to the after-last matrix element.
 
     The methods return the matrix read-only or read-write iterators, set to the point following the last
@@ -2018,6 +2023,12 @@ public:
      */
     template<typename _Tp> MatIterator_<_Tp> end();
     template<typename _Tp> MatConstIterator_<_Tp> end() const;
+
+    /** @brief Same as end() but for inverse traversal
+     */
+    template<typename _Tp> std::reverse_iterator< MatIterator_<_Tp>> rend();
+    template<typename _Tp> std::reverse_iterator< MatConstIterator_<_Tp>> rend() const;
+
 
     /** @brief Runs the given functor over all matrix elements in parallel.
 
@@ -2249,6 +2260,12 @@ public:
     iterator end();
     const_iterator begin() const;
     const_iterator end() const;
+
+    //reverse iterators
+    std::reverse_iterator<iterator> rbegin();
+    std::reverse_iterator<iterator> rend();
+    std::reverse_iterator<const_iterator> rbegin() const;
+    std::reverse_iterator<const_iterator> rend() const;
 
     //! template methods for for operation over all matrix elements.
     // the operations take care of skipping gaps in the end of rows (if any)
