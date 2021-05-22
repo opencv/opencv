d1ee19eee5b28b6f064e2fa979867d8619550269
danielenricocahall <danielenricocahall@gmail.com>
2021-05-22 09:29:56 -0400

    fix cu cond

--- modules/cudaimgproc/src/cuda/match_template.cu
+++ modules/cudaimgproc/src/cuda/match_template.cu
@@ -277,6 +277,8 @@ namespace cv { namespace cuda { namespace device
 
         __device__ float normAcc_SQDIFF(float num, float denum)
         {
+            if (::fabs(num) < denum * 1.125f)
+                return num > 0 ? 1 : -1;
             return num / denum;
         }
 
