5f637e5a020e5e15e52c5c2be5393175a5b8b0fb
damonyu1989 <71262988+damonyu1989@users.noreply.github.com>
2021-05-26 01:15:12 +0800

    Merge pull request #19778 from damonyu1989:master-riscv-0.7.1
    * Add the support for riscv64 vector 0.7.1.
    
    * fixed GCC warnings
    
    * cleaned whitespaces
    
    * Remove the worning by the use of internal API of compiler.
    
    * Update the license header.
    
    * removed trailing whitespaces
    
    Co-authored-by: Vadim Pisarevsky <vadim.pisarevsky@me.com>
    Co-authored-by: yulj <linjie.ylj@alibaba-inc.com>
    Co-authored-by: Vadim Pisarevsky <vadim.pisarevsky@gmail.com>

---
+++ platforms/linux/riscv64-071-gcc.toolchain.cmake
@@ -0,0 +1,9 @@
+set(CMAKE_SYSTEM_NAME "Linux")
+set(CMAKE_C_COMPILER  riscv64-unknown-linux-gnu-gcc)
+set(CMAKE_CXX_COMPILER riscv64-unknown-linux-gnu-g++)
+
+set(CMAKE_CXX_FLAGS ""    CACHE STRING "")
+set(CMAKE_C_FLAGS ""    CACHE STRING "")
+
+set(CMAKE_CXX_FLAGS "-static -march=rv64gcvxthead -mabi=lp64v -pthread")
+set(CMAKE_C_FLAGS "-static -march=rv64gcvxthead -mabi=lp64v -pthread")
