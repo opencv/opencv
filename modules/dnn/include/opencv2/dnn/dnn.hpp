2bc4332b3008c413f80c30e167fd34654bbb951f
Alexander Alekhin <alexander.alekhin@intel.com>
2021-06-02 17:53:41 +0000

    dnn.hpp: fix doxygen formatting

--- modules/dnn/include/opencv2/dnn/dnn.hpp
+++ modules/dnn/include/opencv2/dnn/dnn.hpp
@@ -712,9 +712,11 @@ CV__DNN_EXPERIMENTAL_NS_BEGIN
         CV_WRAP void enableFusion(bool fusion);
 
         /** @brief Returns overall time for inference and timings (in ticks) for layers.
+         *
          * Indexes in returned vector correspond to layers ids. Some layers can be fused with others,
          * in this case zero ticks count will be return for that skipped layers. Supported by DNN_BACKEND_OPENCV on DNN_TARGET_CPU only.
-         * @param timings vector for tick timings for all layers.
+         *
+         * @param[out] timings vector for tick timings for all layers.
          * @return overall ticks for model inference.
          */
         CV_WRAP int64 getPerfProfile(CV_OUT std::vector<double>& timings);
