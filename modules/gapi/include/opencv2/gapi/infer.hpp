616192fd53d2ccfb607d912c1a052e5f1c5faff8
Anatoliy Talamanov <anatoliy.talamanov@intel.com>
2021-06-08 11:59:57 +0300

    Merge pull request #20169 from TolyaTalamanov:at/doc-generic-type
    [G-API] Generic type documentation
    
    * Put doc about generic type
    
    * Fix comments to review

--- modules/gapi/include/opencv2/gapi/infer.hpp
+++ modules/gapi/include/opencv2/gapi/infer.hpp
@@ -531,7 +531,11 @@ typename Net::Result infer(Args&&... args) {
 }
 
 /**
- * @brief Special network type
+ * @brief Generic network type: input and output layers are configured dynamically at runtime
+ *
+ * Unlike the network types defined with G_API_NET macro, this one
+ * doesn't fix number of network inputs and outputs at the compilation stage
+ * thus providing user with an opportunity to program them in runtime.
  */
 struct Generic { };
 
