f2d38284cd327210124869c7800604eb0f174e57
Rostislav Vasilikhin <rostislav.vasilikhin@intel.com>
2021-06-21 01:20:09 +0300

    (temporarily) removing wrappers for Volume and VolumeParams

--- modules/3d/include/opencv2/rgbd/volume.hpp
+++ modules/3d/include/opencv2/rgbd/volume.hpp
@@ -12,7 +12,7 @@
 
 namespace cv
 {
-class CV_EXPORTS_W Volume
+class CV_EXPORTS Volume
 {
    public:
     Volume(float _voxelSize, Matx44f _pose, float _raycastStepFactor)
@@ -61,66 +61,66 @@ struct CV_EXPORTS VolumeParams
     /** @brief Type of Volume
         Values can be TSDF (single volume) or HASHTSDF (hashtable of volume units)
     */
-    CV_PROP_RW VolumeType type;
+    VolumeType type;
 
     /** @brief Resolution of voxel space
         Number of voxels in each dimension.
         Applicable only for TSDF Volume.
         HashTSDF volume only supports equal resolution in all three dimensions
     */
-    CV_PROP_RW int resolutionX;
-    CV_PROP_RW int resolutionY;
-    CV_PROP_RW int resolutionZ;
+    int resolutionX;
+    int resolutionY;
+    int resolutionZ;
 
     /** @brief Resolution of volumeUnit in voxel space
         Number of voxels in each dimension for volumeUnit
         Applicable only for hashTSDF.
     */
-    CV_PROP_RW int unitResolution = {0};
+    int unitResolution = {0};
 
     /** @brief Initial pose of the volume in meters, should be 4x4 float or double matrix */
-    CV_PROP_RW Mat pose;
+    Mat pose;
 
     /** @brief Length of voxels in meters */
-    CV_PROP_RW float voxelSize;
+    float voxelSize;
 
     /** @brief TSDF truncation distance
         Distances greater than value from surface will be truncated to 1.0
     */
-    CV_PROP_RW float tsdfTruncDist;
+    float tsdfTruncDist;
 
     /** @brief Max number of frames to integrate per voxel
         Represents the max number of frames over which a running average
         of the TSDF is calculated for a voxel
     */
-    CV_PROP_RW int maxWeight;
+    int maxWeight;
 
     /** @brief Threshold for depth truncation in meters
         Truncates the depth greater than threshold to 0
     */
-    CV_PROP_RW float depthTruncThreshold;
+    float depthTruncThreshold;
 
     /** @brief Length of single raycast step
         Describes the percentage of voxel length that is skipped per march
     */
-    CV_PROP_RW float raycastStepFactor;
+    float raycastStepFactor;
 
     /** @brief Default set of parameters that provide higher quality reconstruction
         at the cost of slow performance.
     */
-    CV_WRAP static Ptr<VolumeParams> defaultParams(VolumeType _volumeType);
+    static Ptr<VolumeParams> defaultParams(VolumeParams::VolumeType _volumeType);
 
     /** @brief Coarse set of parameters that provides relatively higher performance
         at the cost of reconstrution quality.
     */
-    CV_WRAP static Ptr<VolumeParams> coarseParams(VolumeType _volumeType);
+    static Ptr<VolumeParams> coarseParams(VolumeParams::VolumeType _volumeType);
 };
 
 
-CV_EXPORTS_W Ptr<Volume> makeVolume(const VolumeParams& _volumeParams);
-CV_EXPORTS_W Ptr<Volume> makeVolume(VolumeType _volumeType, float _voxelSize, Matx44f _pose,
-                                    float _raycastStepFactor, float _truncDist, int _maxWeight,
-                                    float _truncateThreshold, Point3i _resolution);
+CV_EXPORTS Ptr<Volume> makeVolume(const VolumeParams& _volumeParams);
+CV_EXPORTS Ptr<Volume> makeVolume(VolumeParams::VolumeType _volumeType, float _voxelSize, Matx44f _pose,
+                                  float _raycastStepFactor, float _truncDist, int _maxWeight,
+                                  float _truncateThreshold, Point3i _resolution);
 
 }  // namespace cv
 #endif
