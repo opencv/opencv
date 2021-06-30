07dd0e652a76af8e7416dff38ee61373d9498d85
APrigarina <ann73617@gmail.com>
2021-06-30 12:50:21 +0300

    fix samples 3.4

--- samples/python/lk_track.py
+++ samples/python/lk_track.py
@@ -65,7 +65,7 @@ class App:
                     if len(tr) > self.track_len:
                         del tr[0]
                     new_tracks.append(tr)
-                    cv.circle(vis, (x, y), 2, (0, 255, 0), -1)
+                    cv.circle(vis, (int(x), int(y)), 2, (0, 255, 0), -1)
                 self.tracks = new_tracks
                 cv.polylines(vis, [np.int32(tr) for tr in self.tracks], False, (0, 255, 0))
                 draw_str(vis, (20, 20), 'track count: %d' % len(self.tracks))
