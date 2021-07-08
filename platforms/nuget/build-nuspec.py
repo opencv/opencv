48ed37efcba161618e941a8f78ac8ebc5a59b25e
Aditya Mulgundkar <adityamulgundkar.nmims@gmail.com>
2021-07-08 09:38:58 +0530

    Updated build parser file for nuspec and targets files

--- platforms/nuget/build-nuspec.py
+++ platforms/nuget/build-nuspec.py
@@ -1,6 +1,14 @@
 # run script
-# .\build-framework.py --package_name "OpenCV CPP" --package_version "0.0.1" --native_build_path "C:\opencv-cpp-build" --packages_path "C:\opencv-cpp-build" --sources_path "D:\Github\opencv"
+# .\build-nuspec.py --package_name "OpenCV CPP" --package_version "0.0.1" --native_build_path "C:\opencv-cpp-build" --packages_path "C:\opencv-cpp-build" --sources_path "D:\Github\opencv"
 import os, sys, argparse
+
+authors = 'OpenCV'
+owners = 'OpenCV'
+description = 'This is description.'
+copyright = '\xc2\xa9 ' + 'OpenCV. All rights reserved.'
+tags = 'OpenCV Computer Vision'
+iconUrl = 'https://avatars.githubusercontent.com/u/5009934?s=200&v=4'
+project_url = 'https://github.com/opencv/opencv'
 
 def parse_arguments():
     parser = argparse.ArgumentParser(description="OpenCV CPP create nuget spec script ",
@@ -12,53 +20,10 @@ def parse_arguments():
     parser.add_argument("--native_build_path", required=True, help="Native build output directory.")
     parser.add_argument("--packages_path", required=True, help="Nuget packages output directory.")
     parser.add_argument("--sources_path", required=True, help="OpenCV source code root.")
+    parser.add_argument("--targets_path", required=True, help="Path name for the generated targets file.")
+    parser.add_argument("--targets_file", required=True, help="File name for the generated targets file.")
 
     return parser.parse_args()
-
-
-
-def generate_id(list, package_name):
-    list.append(f'<id>{package_name}</id>')
-
-
-def generate_version(list, package_version):
-    list.append(f'<version>{package_version}</version>')
-
-
-def generate_authors(list, authors):
-    list.append(f'<authors>{authors}</authors>')
-
-
-def generate_owners(list, owners):
-    list.append(f'<owners>{owners}</owners>')
-
-
-def generate_description(list, description):
-    list.append(f'<description>{description}</description>')
-
-
-def generate_copyright(list, copyright):
-    list.append(f'<copyright>{copyright}</copyright>')
-
-
-def generate_tags(list, tags):
-    list.append(f'<tags>{tags}</tags>')
-
-
-def generate_icon_url(list, icon_url):
-    list.append(f'<iconUrl>{icon_url}</iconUrl>')
-
-
-def generate_license(list):
-    list.append(f'<license type="file">LICENSE.txt</license>')
-
-
-def generate_project_url(list, project_url):
-    list.append(f'<projectUrl>{project_url}</projectUrl>')
-
-
-def generate_repo_url(list, repo_url, commit_id):
-    list.append(f'<repository type="git" url="{repo_url}" commit="{commit_id}" />')
 
 
 
@@ -72,8 +37,41 @@ def generate_files(list, args):
 
     # Process Readme, License, ThirdPartyNotices, Privacy
     # files_list.append('<file src=' + '"' + os.path.join(args.sources_path, 'LICENSE.txt') + '" target="LICENSE.txt" />')
+    
+    # add source includes folder
+    derived_path = os.path.join(args.sources_path, 'include\**\*.*')
+    files_list.append(f'<file src="{os.path.join(derived_path)}" target="build\\native\\include" />')
+
+    # add vc14 compiled lib & dlls
+    derived_path = os.path.join(args.sources_path, 'x64\\vc14\\lib\\**\\opencv_world*.lib')
+    files_list.append(f'<file src="{os.path.join(derived_path)}" target="build\\native\\x64\\vc14.0\\lib" />')
+    derived_path = os.path.join(args.sources_path, 'x64\\vc14\\lib\\**\\opencv_world*.dll')
+    files_list.append(f'<file src="{os.path.join(derived_path)}" target="build\\native\\x64\\vc14.0\\bin" />')
+    derived_path = os.path.join(args.sources_path, 'x64\\vc14\\lib\\**\\opencv_world*.pdb')
+    files_list.append(f'<file src="{os.path.join(derived_path)}" target="build\\native\\x64\\vc14.0\\bin" />')
+
+    # add vc15 compiled lib & dlls
+    derived_path = os.path.join(args.sources_path, 'x64\\vc15\\lib\\**\\opencv_world*.lib')
+    files_list.append(f'<file src="{os.path.join(derived_path)}" target="build\\native\\x64\\vc15.0\\lib" />')
+    derived_path = os.path.join(args.sources_path, 'x64\\vc15\\lib\\**\\opencv_world*.dll')
+    files_list.append(f'<file src="{os.path.join(derived_path)}" target="build\\native\\x64\\vc15.0\\bin" />')
+    derived_path = os.path.join(args.sources_path, 'x64\\vc15\\lib\\**\\opencv_world*.pdb')
+    files_list.append(f'<file src="{os.path.join(derived_path)}" target="build\\native\\x64\\vc15.0\\bin" />')
+
+    # add vc16 compiled lib & dlls
+    derived_path = os.path.join(args.sources_path, 'x64\\vc16\\lib\\**\\opencv_world*.lib')
+    files_list.append(f'<file src="{os.path.join(derived_path)}" target="build\\native\\x64\\vc16.0\\lib" />')
+    derived_path = os.path.join(args.sources_path, 'x64\\vc16\\lib\\**\\opencv_world*.dll')
+    files_list.append(f'<file src="{os.path.join(derived_path)}" target="build\\native\\x64\\vc16.0\\bin" />')
+    derived_path = os.path.join(args.sources_path, 'x64\\vc16\\lib\\**\\opencv_world*.pdb')
+    files_list.append(f'<file src="{os.path.join(derived_path)}" target="build\\native\\x64\\vc16.0\\bin" />')
+
+    # add readme file
     derived_path = os.path.join(args.sources_path, 'README.md')
     files_list.append(f'<file src="{os.path.join(derived_path)}" target="README.md" />')
+
+    # add generated targets file
+    files_list.append(f'<file src="{args.targets_path}" target="{args.targets_file}" />')
     files_list.append('</files>')
 
     list += files_list
@@ -91,16 +89,28 @@ def generate_nuspec(args):
 
 def generate_metadata(list, args):
     metadata_list = ['<metadata>']
-    generate_id(metadata_list, args.package_name)
-    generate_version(metadata_list, args.package_version)
-    generate_authors(metadata_list, 'OpenCV')
-    generate_owners(metadata_list, 'OpenCV')
-    generate_description(metadata_list, 'This is description.')
-    generate_copyright(metadata_list, '\xc2\xa9 ' + 'OpenCV. All rights reserved.')
-    generate_tags(metadata_list, 'OpenCV Computer Vision')
-    generate_icon_url(metadata_list, 'https://avatars.githubusercontent.com/u/5009934?s=200&v=4')
-    generate_license(metadata_list)
-    generate_project_url(metadata_list, 'https://github.com/opencv/opencv')
+    # package id
+    metadata_list.append(f'<id>{args.package_name}</id>')
+    # package version
+    metadata_list.append(f'<version>{args.package_version}</version>')
+    # package authors
+    metadata_list.append(f'<authors>{authors}</authors>')
+    # package owners
+    metadata_list.append(f'<owners>{owners}</owners>')
+    # package description
+    metadata_list.append(f'<description>{description}</description>')
+    # package copyright
+    metadata_list.append(f'<copyright>{copyright}</copyright>')
+    # package tags
+    metadata_list.append(f'<tags>{tags}</tags>')
+    # package icon
+    metadata_list.append(f'<iconUrl>{iconUrl}</iconUrl>')
+    # package license
+    derived_path = os.path.join(args.sources_path, 'LICENSE.txt')
+    metadata_list.append(f'<license type="file">{os.path.join(derived_path)}</license>')
+    # package project url
+    metadata_list.append(f'<projectUrl>{project_url}</projectUrl>')
+
     # generate_repo_url(metadata_list, 'https://github.com/opencv/opencv.git', args.commit_id)
     metadata_list.append('</metadata>')
 
@@ -122,6 +132,7 @@ def main():
             f.write(line)
             f.write('\n')
 
+    print("stopped parser")
 
 if __name__ == "__main__":
     sys.exit(main())
--- platforms/nuget/build-targets.py
+++ platforms/nuget/build-targets.py
@@ -1,19 +1,43 @@
 # run script
-# .\build-framework.py --package_name "OpenCV CPP" --package_version "0.0.1" --native_build_path "C:\opencv-cpp-build" --packages_path "C:\opencv-cpp-build" --sources_path "D:\Github\opencv"
+# .\build-targets.py --package_name "OpenCV CPP" --package_version "0.0.1" --native_build_path "C:\opencv-cpp-build" --packages_path "C:\opencv-cpp-build" --sources_path "D:\Github\opencv"
 import os, sys, argparse
 
 
 def generate_property_group(list, args):
     files_list = ['<PropertyGroup>']
-    files_list.append(f'<LibraryType Condition="\'$(Configuration)\'==\'Debug\'">debug</LibraryType>')
-    files_list.append(f'<LibraryType Condition="\'$(Configuration)\'==\'Release\'">release</LibraryType>')
+    files_list.append(f'<VisualStudioVersion Condition="\'$(VisualStudioVersion)\' == \'\'">10.0</VisualStudioVersion>')
+    files_list.append(f'<LibraryType Condition="\'$(Configuration)\'==\'Debug\'">d</LibraryType>')
+    files_list.append(f'<LibraryType Condition="\'$(Configuration)\'==\'Release\'"></LibraryType>')
     files_list.append('</PropertyGroup>')
+
+    list += files_list
+
+def generate_item_definition_group(list, args):
+    files_list = ['<ItemDefinitionGroup>']
+    files_list.append(f'<ClCompile>')
+    files_list.append(f'<AdditionalIncludeDirectories>$(MSBuildThisFileDirectory)\include\;%(AdditionalIncludeDirectories)</AdditionalIncludeDirectories>')
+    files_list.append(f'</ClCompile>')
+    files_list.append('</ItemDefinitionGroup>')
+
+    list += files_list
+
+def generate_item_definition_group_2(list, args):
+    files_list = ['<ItemDefinitionGroup>']
+    files_list.append(f'<Link>')
+    files_list.append(f'<AdditionalLibraryDirectories>$(MSBuildThisFileDirectory)\x$(Platform)\vc$(VisualStudioVersion)\lib\;%(AdditionalLibraryDirectories)</AdditionalLibraryDirectories>')
+    files_list.append(f'<AdditionalDependencies>opencv_world*$(LibraryType).lib;%(AdditionalDependencies)</AdditionalDependencies>')
+    files_list.append(f'</Link>')
+    files_list.append('</ItemDefinitionGroup>')
 
     list += files_list
 
 def generate_item_group(list, args):
     files_list = ['<ItemGroup>']
-    files_list.append(f'<FilamentLibs Include="$(MSBuildThisFileDirectory)\\x64\\vc14\\lib\\*.lib" />')
+    files_list.append(f'<None Include="$(MSBuildThisFileDirectory)\\x64\\vc$(VisualStudioVersion)\\bin\\opencv_world*$(LibraryType).dll">')
+    files_list.append(f'<Link>opencv_world*$(LibraryType).dll</Link>')
+    files_list.append(f'<CopyToOutputDirectory>PreserveNewest</CopyToOutputDirectory>')
+    files_list.append(f'<Visible>false</Visible>')
+    files_list.append(f'</None>')
     files_list.append('</ItemGroup>')
 
     list += files_list
@@ -21,9 +45,10 @@ def generate_item_group(list, args):
 
 def generate_targets(args):
     lines = ['<?xml version="1.0"?>']
-    lines.append(f'<Project xmlns="http://schemas.microsoft.com/developer/msbuild/2003"
-        ToolsVersion="15.0">')
+    lines.append(f'<Project xmlns="http://schemas.microsoft.com/developer/msbuild/2003" ToolsVersion="15.0">')
     generate_property_group(lines, args)
+    generate_item_definition_group(lines, args)
+    generate_item_definition_group_2(lines, args)
     generate_item_group(lines, args)
     lines.append(f'</Project>')
     return lines
@@ -43,6 +68,7 @@ def main():
             f.write(line)
             f.write('\n')
 
+    print("stopped parser")
 
 if __name__ == "__main__":
     sys.exit(main())
