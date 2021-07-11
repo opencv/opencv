9a8fa78fedd2ff7f26ad41ea2c2bfbe527173ce4
Rostislav Vasilikhin <rostislav.vasilikhin@intel.com>
2021-07-12 00:06:27 +0300

    KinFu license notion shortened

--- modules/3d/src/rgbd/colored_tsdf.hpp
+++ modules/3d/src/rgbd/colored_tsdf.hpp
@@ -2,9 +2,6 @@
 // It is subject to the license terms in the LICENSE file found in the top-level directory
 // of this distribution and at http://opencv.org/license.html
 
-// This code is also subject to the license terms in the LICENSE_KinectFusion.md file found in this
-// module's directory
-
 #ifndef __OPENCV_KINFU_COLORED_TSDF_H__
 #define __OPENCV_KINFU_COLORED_TSDF_H__
 
--- modules/3d/src/rgbd/fast_icp.cpp
+++ modules/3d/src/rgbd/fast_icp.cpp
@@ -2,7 +2,8 @@
 // It is subject to the license terms in the LICENSE file found in the top-level directory
 // of this distribution and at http://opencv.org/license.html
 
-// This code is also subject to the license terms in the LICENSE_KinectFusion.md file found in this module's directory
+// Partially rewritten from https://github.com/Nerei/kinfu_remake
+// Copyright(c) 2012, Anatoly Baksheev. All rights reserved.
 
 #include "../precomp.hpp"
 #include "fast_icp.hpp"
