d947a77d060a73a7fe1c225e0bc5fec68e6a80cb
Rostislav Vasilikhin <rostislav.vasilikhin@intel.com>
2021-07-15 02:14:54 +0300

    indentation fix in src files

--- modules/3d/src/rgbd/depth_cleaner.cpp
+++ modules/3d/src/rgbd/depth_cleaner.cpp
@@ -9,9 +9,9 @@
 namespace cv
 {
 
-  class DepthCleanerImpl
-  {
-  public:
+class DepthCleanerImpl
+{
+public:
     DepthCleanerImpl(int window_size, int depth, DepthCleaner::DEPTH_CLEANER_METHOD method)
         :
           depth_(depth),
@@ -33,22 +33,22 @@ namespace cv
     {
       return (window_size == window_size_) && (depth == depth_) && (method == method_);
     }
-  protected:
+protected:
     int depth_;
     int window_size_;
     DepthCleaner::DEPTH_CLEANER_METHOD method_;
-  };
+};
 
 ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
-  /** Given a depth image, compute the normals as detailed in the LINEMOD paper
-   * ``Gradient Response Maps for Real-Time Detection of Texture-Less Objects``
-   * by S. Hinterstoisser, C. Cagniart, S. Ilic, P. Sturm, N. Navab, P. Fua, and V. Lepetit
-   */
-  template<typename T>
-  class NIL: public DepthCleanerImpl
-  {
-  public:
+/** Given a depth image, compute the normals as detailed in the LINEMOD paper
+ * ``Gradient Response Maps for Real-Time Detection of Texture-Less Objects``
+ * by S. Hinterstoisser, C. Cagniart, S. Ilic, P. Sturm, N. Navab, P. Fua, and V. Lepetit
+ */
+template<typename T>
+class NIL: public DepthCleanerImpl
+{
+public:
     typedef Vec<T, 3> Vec3T;
     typedef Matx<T, 3, 3> Mat33T;
 
@@ -97,7 +97,7 @@ namespace cv
       }
     }
 
-  private:
+private:
     /** Compute the normals
      * @param r
      * @return
@@ -158,26 +158,25 @@ namespace cv
       }
       Mat(Dw_sum / w_sum).copyTo(depth_out);
     }
-  };
+};
 
 ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
-  /** Default constructor of the Algorithm class that computes normals
-   */
-  DepthCleaner::DepthCleaner(int depth, int window_size, int method_in)
-      :
+/** Default constructor of the Algorithm class that computes normals
+ */
+DepthCleaner::DepthCleaner(int depth, int window_size, int method_in) :
         depth_(depth),
         window_size_(window_size),
         method_(method_in),
         depth_cleaner_impl_(0)
-  {
+{
     CV_Assert(depth == CV_16U || depth == CV_32F || depth == CV_64F);
-  }
+}
 
-  /** Destructor
-   */
-  DepthCleaner::~DepthCleaner()
-  {
+/** Destructor
+ */
+DepthCleaner::~DepthCleaner()
+{
     if (depth_cleaner_impl_ == 0)
       return;
     switch (method_)
@@ -199,11 +198,11 @@ namespace cv
         break;
       }
     }
-  }
+}
 
-  void
-  DepthCleaner::initialize_cleaner_impl() const
-  {
+void
+DepthCleaner::initialize_cleaner_impl() const
+{
     CV_Assert(depth_ == CV_16U || depth_ == CV_32F || depth_ == CV_64F);
     CV_Assert(window_size_ == 1 || window_size_ == 3 || window_size_ == 5 || window_size_ == 7);
     CV_Assert( method_ == DEPTH_CLEANER_NIL);
@@ -228,31 +227,29 @@ namespace cv
     }
 
     reinterpret_cast<DepthCleanerImpl *>(depth_cleaner_impl_)->cache();
-  }
+}
 
-  /** Initializes some data that is cached for later computation
-   * If that function is not called, it will be called the first time normals are computed
-   */
-  void
-  DepthCleaner::initialize() const
-  {
+/** Initializes some data that is cached for later computation
+ * If that function is not called, it will be called the first time normals are computed
+ */
+void DepthCleaner::initialize() const
+{
     if (depth_cleaner_impl_ == 0)
       initialize_cleaner_impl();
     else if (!reinterpret_cast<DepthCleanerImpl *>(depth_cleaner_impl_)->validate(depth_, window_size_, method_))
       initialize_cleaner_impl();
-  }
+}
 
-  /** Given a set of 3d points in a depth image, compute the normals at each point
-   * using the SRI method described in
-   * ``Fast and Accurate Computation of Surface Normals from Range Images``
-   * by H. Badino, D. Huber, Y. Park and T. Kanade
-   * @param depth depth a float depth image. Or it can be rows x cols x 3 is they are 3d points
-   * @param window_size the window size on which to compute the derivatives
-   * @return normals a rows x cols x 3 matrix
-   */
-  void
-  DepthCleaner::apply(InputArray depth_in_array, OutputArray depth_out_array) const
-  {
+/** Given a set of 3d points in a depth image, compute the normals at each point
+ * using the SRI method described in
+ * ``Fast and Accurate Computation of Surface Normals from Range Images``
+ * by H. Badino, D. Huber, Y. Park and T. Kanade
+ * @param depth depth a float depth image. Or it can be rows x cols x 3 is they are 3d points
+ * @param window_size the window size on which to compute the derivatives
+ * @return normals a rows x cols x 3 matrix
+ */
+void DepthCleaner::apply(InputArray depth_in_array, OutputArray depth_out_array) const
+{
     Mat depth_in = depth_in_array.getMat();
     CV_Assert(depth_in.dims == 2);
     CV_Assert(depth_in.channels() == 1);
@@ -283,6 +280,5 @@ namespace cv
         break;
       }
     }
-  }
-
+}
 }
