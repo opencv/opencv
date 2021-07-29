698236f9d51db6aff18f9c3c6e9cd10ce1e21ef4
Chengrui Wang <80876977+crywang@users.noreply.github.com>
2021-07-30 00:59:35 +0800

    Update test_face.cpp

--- modules/objdetect/test/test_face.cpp
+++ modules/objdetect/test/test_face.cpp
@@ -148,5 +148,74 @@ TEST(Objdetect_face_detect, regression)
     }
 }
 
+typedef testing::TestWithParam<String> Objdetect_face_recognition;
+TEST(Objdetect_face_recognition, regression)
+{
+    // Pre-set params
+    float score_thresh = 0.9;
+    float nms_thresh = 0.3;
+    float cosine_similar_thresh = 0.363;
+    float l2norm_similar_thresh = 1.272;
+
+    // Load ground truth labels
+    std::ifstream ifs(findDataFile("dnn_face/face_recognition/cascades_label.txt", true).c_str());
+    CV_Assert(ifs.is_open());
+
+    std::set<String> fSet;
+    std::map<String, Mat> featureMap;
+    std::map<std::pair<String, String>, int> gtMap;
+   
+
+    for (String line, key; getline(ifs, line);)
+    {
+        String fname1, fname2;
+        int label;
+        std::istringstream iss(line);
+        iss>>fname1>>fname2>>label;
+        // std::cout<<fname1<<" "<<fname2<<" "<<label<<std::endl;
+
+        fSet.insert(fname1);
+        fSet.insert(fname2);
+        gtMap[std::make_pair(fname1, fname2)] = label;
+    }
+
+    // Initialize detector
+    String detect_model = findDataFile("dnn_face/face_detection/yunet.onnx", true);
+    Ptr<FaceDetector> faceDetector = FaceDetector::create(detect_model, "", Size(250, 250), score_thresh, nms_thresh);
+
+    String recog_model = findDataFile("dnn_face/face_recognition/face_recognizer_fast.onnx", true);
+    Ptr<FaceRecognizer> faceRecognizer = FaceRecognizer::create(recog_model, "");
+
+    // Detect and match
+    for (auto fname: fSet)
+    {
+        String imagePath = findDataFile("cascadeandhog/images/" + fname, true);
+        Mat image = imread(imagePath);
+
+        Mat faces;
+        faceDetector->detect(image, faces);
+
+        Mat aligned_face;
+        faceRecognizer->alignCrop(image, faces.row(0), aligned_face);
+        
+        Mat feature;
+        faceRecognizer->faceFeature(aligned_face, feature);
+
+        featureMap[fname] = feature.clone();
+    }
+
+    for (auto item: gtMap)
+    {
+        Mat feature1 = featureMap[item.first.first];
+        Mat feature2 = featureMap[item.first.second];
+        int label = item.second;
+
+        double cos_score = faceRecognizer->faceMatch(feature1, feature2, FaceRecognizer::distype::cosine);
+        double L2_score = faceRecognizer->faceMatch(feature1, feature2, FaceRecognizer::distype::norml2);
+
+        EXPECT_TRUE(label == 0 ? cos_score <= cosine_similar_thresh : cos_score > cosine_similar_thresh) << "Cosine match result of images " << item.first.first << " and " << item.first.second << " is different from ground truth (score: "<< cos_score <<";Thresh: "<< cosine_similar_thresh <<").";
+        EXPECT_TRUE(label == 0 ? L2_score > l2norm_similar_thresh : L2_score <= l2norm_similar_thresh) << "L2norm match result of images " << item.first.first << " and " << item.first.second << " is different from ground truth (score: "<< L2_score <<";Thresh: "<< l2norm_similar_thresh <<").";
+    }
+}
 
 }} // namespace ; namespace opencv_test
