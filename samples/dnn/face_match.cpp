47357fafccb16845c7f2fa51b502c2396f82394f
Chengrui Wang <80876977+crywang@users.noreply.github.com>
2021-07-30 01:00:10 +0800

    Update face_match.cpp

--- samples/dnn/face_match.cpp
+++ samples/dnn/face_match.cpp
@@ -37,8 +37,8 @@ int main(int argc, char ** argv)
 
     float score_thresh = 0.9;
     float nms_thresh = 0.3;
-    float cosine_similar_thresh = 0.34;
-    float l2norm_similar_thresh = 1.32;
+    double cosine_similar_thresh = 0.34;
+    double l2norm_similar_thresh = 1.32;
     int top_k = 5000;
 
     // Initialize FaceDetector
