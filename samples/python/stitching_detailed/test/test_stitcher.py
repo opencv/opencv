82961ea87a5659ec75044a69f00b7f06b7eb5a23
Lukas Weber <lukas.weber@baw.de>
2021-08-02 09:15:03 +0200

    add tests for FeatureDetector

--- samples/python/stitching_detailed/test/test_stitcher.py
+++ samples/python/stitching_detailed/test/test_stitcher.py
@@ -3,15 +3,20 @@ import os
 import sys
 
 import numpy as np
+import cv2 as cv
 
 sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__),
                                                 '..', '..')))
 
 from stitching_detailed.stitcher import Stitcher
+from stitching_detailed.feature_detector import FeatureDetector
 
 
 # Eine Klasse erstellen, die von unittest.TestCase erbt
 class TestStitcher(unittest.TestCase):
+    def setUp(self):
+        self.img1, self.img2 = cv.imread("s1.jpg"), cv.imread("s2.jpg")
+
     def tearDown(self):
         try:
             os.remove("result.jpg")
@@ -52,6 +57,18 @@ class TestStitcher(unittest.TestCase):
         np.testing.assert_allclose(stitcher.result.shape,
                                    (700, 1811, 3),
                                    max_image_shape_derivation_per_pixel)
+
+    def test_feature_detector(self):
+        default_number_of_keypoints = 500
+        detector = FeatureDetector("orb")
+        features = detector.detect_features(self.img1)
+        self.assertEqual(len(features.getKeypoints()),
+                         default_number_of_keypoints)
+
+        other_keypoints = 100
+        detector = FeatureDetector("orb", nfeatures=other_keypoints)
+        features = detector.detect_features(self.img1)
+        self.assertEqual(len(features.getKeypoints()), other_keypoints)
 
 
 def starttest():
