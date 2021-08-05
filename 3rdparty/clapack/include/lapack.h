7cf1db0db3beaec6f8eefd1ff36a0d091dd255be
Rostislav Vasilikhin <rostislav.vasilikhin@intel.com>
2021-08-05 15:42:03 +0300

    "small" macro fixed by undef

--- 3rdparty/clapack/include/lapack.h
+++ 3rdparty/clapack/include/lapack.h
@@ -86,7 +86,12 @@ int dhseqr_(char *job, char *compz, int *n, int *ilo, int *
 
 int disnan_(double *din);
 
-int dlabad_(double *_small, double *_large);
+// "small" is a macro defined in Windows headers: https://stackoverflow.com/a/27794577
+#ifdef small
+#undef small
+#endif
+
+int dlabad_(double *small, double *large);
 
 int dlabrd_(int *m, int *n, int *nb, double *a, int *lda,
 	double *d__, double *e, double *tauq, double *taup, double *x, int *
