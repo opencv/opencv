e29a00dc6063b47974e5a72f0d20feafb3e8c8db
Rostislav Vasilikhin <rostislav.vasilikhin@intel.com>
2021-08-06 01:02:32 +0300

    tests for Odometry: no cvtest classes; UMat tests; logging fixed

--- modules/3d/test/test_odometry.cpp
+++ modules/3d/test/test_odometry.cpp
@@ -6,7 +6,6 @@
 
 namespace opencv_test { namespace {
 
-#define SHOW_DEBUG_LOG     0
 #define SHOW_DEBUG_IMAGES  0
 
 static
@@ -114,24 +113,35 @@ void dilateFrame(Mat& image, Mat& depth)
             }
 }
 
-class CV_OdometryTest : public cvtest::BaseTest
+class OdometryTest
 {
 public:
-    CV_OdometryTest(const Ptr<Odometry>& _odometry,
-                    double _maxError1,
-                    double _maxError5,
-                    double _idError = DBL_EPSILON) :
+    OdometryTest(const Ptr<Odometry>& _odometry,
+                 double _maxError1,
+                 double _maxError5,
+                 double _idError = DBL_EPSILON) :
         odometry(_odometry),
         maxError1(_maxError1),
         maxError5(_maxError5),
         idError(_idError)
     { }
 
-protected:
-    bool readData(Mat& image, Mat& depth) const;
+    void readData(Mat& image, Mat& depth) const;
+    static Mat getCameraMatrix()
+    {
+        float fx = 525.0f, // default
+              fy = 525.0f,
+              cx = 319.5f,
+              cy = 239.5f;
+        Matx33f K(fx,  0, cx,
+                   0, fy, cy,
+                   0,  0,  1);
+        return Mat(K);
+    }
     static void generateRandomTransformation(Mat& R, Mat& t);
 
-    virtual void run(int);
+    void run();
+    void checkUMats();
 
     Ptr<Odometry> odometry;
     double maxError1;
@@ -139,26 +149,23 @@ protected:
     double idError;
 };
 
-bool CV_OdometryTest::readData(Mat& image, Mat& depth) const
+
+void OdometryTest::readData(Mat& image, Mat& depth) const
 {
-    std::string imageFilename = ts->get_data_path() + "/cv/rgbd/rgb.png";
-    std::string depthFilename = ts->get_data_path() + "/cv/rgbd/depth.png";
+    std::string dataPath = cvtest::TS::ptr()->get_data_path();
+    std::string imageFilename = dataPath + "/cv/rgbd/rgb.png";
+    std::string depthFilename = dataPath + "/cv/rgbd/depth.png";
 
     image = imread(imageFilename,  0);
     depth = imread(depthFilename, -1);
 
     if(image.empty())
     {
-        ts->printf( cvtest::TS::LOG, "Image %s can not be read.\n", imageFilename.c_str() );
-        ts->set_failed_test_info( cvtest::TS::FAIL_INVALID_TEST_DATA );
-        return false;
+        FAIL() << "Image " << imageFilename.c_str() << " can not be read" << std::endl;
     }
     if(depth.empty())
     {
-        ts->printf( cvtest::TS::LOG, "Depth %s can not be read.\n", depthFilename.c_str() );
-        ts->set_failed_test_info( cvtest::TS::FAIL_INVALID_TEST_DATA );
-        ts->set_gtest_status();
-        return false;
+        FAIL() << "Depth" << depthFilename.c_str() << "can not be read" << std::endl;
     }
 
     CV_DbgAssert(image.type() == CV_8UC1);
@@ -169,11 +176,9 @@ bool CV_OdometryTest::readData(Mat& image, Mat& depth) const
         depth_flt.setTo(std::numeric_limits<float>::quiet_NaN(), depth_flt < FLT_EPSILON);
         depth = depth_flt;
     }
-
-    return true;
 }
 
-void CV_OdometryTest::generateRandomTransformation(Mat& rvec, Mat& tvec)
+void OdometryTest::generateRandomTransformation(Mat& rvec, Mat& tvec)
 {
     const float maxRotation = (float)(3.f / 180.f * CV_PI); //rad
     const float maxTranslation = 0.02f; //m
@@ -189,42 +194,55 @@ void CV_OdometryTest::generateRandomTransformation(Mat& rvec, Mat& tvec)
     normalize(tvec, tvec, rng.uniform(0.008f, maxTranslation));
 }
 
-void CV_OdometryTest::run(int)
+void OdometryTest::checkUMats()
 {
-    float fx = 525.0f, // default
-          fy = 525.0f,
-          cx = 319.5f,
-          cy = 239.5f;
-    Mat K = Mat::eye(3,3,CV_32FC1);
-    {
-        K.at<float>(0,0) = fx;
-        K.at<float>(1,1) = fy;
-        K.at<float>(0,2) = cx;
-        K.at<float>(1,2) = cy;
-    }
+    Mat K = getCameraMatrix();
 
     Mat image, depth;
-    if(!readData(image, depth))
-        return;
+    readData(image, depth);
+
+    odometry->setCameraMatrix(K);
+
+    Mat calcRt;
+
+    UMat uimage, udepth, umask;
+    image.copyTo(uimage);
+    depth.copyTo(udepth);
+    Mat(image.size(), CV_8UC1, Scalar(255)).copyTo(umask);
+
+    bool isComputed = odometry->compute(uimage, udepth, umask,
+                                        uimage, udepth, umask,
+                                        calcRt);
+    ASSERT_TRUE(isComputed);
+    double diff = cv::norm(calcRt, Mat::eye(4, 4, CV_64FC1));
+    if (diff > idError)
+    {
+        FAIL() << "Incorrect transformation between the same frame (not the identity matrix), diff = " << diff << std::endl;
+    }
+}
+
+void OdometryTest::run()
+{
+    Mat K = getCameraMatrix();
+
+    Mat image, depth;
+    readData(image, depth);
 
     odometry->setCameraMatrix(K);
 
     Mat calcRt;
 
     // 1. Try to find Rt between the same frame (try masks also).
-    bool isComputed = odometry->compute(image, depth, Mat(image.size(), CV_8UC1, Scalar(255)),
-                                        image, depth, Mat(image.size(), CV_8UC1, Scalar(255)),
-                                        calcRt);
+    Mat mask(image.size(), CV_8UC1, Scalar(255));
+    bool isComputed = odometry->compute(image, depth, mask, image, depth, mask, calcRt);
     if(!isComputed)
     {
-        ts->printf(cvtest::TS::LOG, "Can not find Rt between the same frame");
-        ts->set_failed_test_info(cvtest::TS::FAIL_INVALID_OUTPUT);
+        FAIL() << "Can not find Rt between the same frame" << std::endl;
     }
     double diff = cv::norm(calcRt, Mat::eye(4,4,CV_64FC1));
     if(diff > idError)
     {
-        ts->printf(cvtest::TS::LOG, "Incorrect transformation between the same frame (not the identity matrix), diff = %f", diff);
-        ts->set_failed_test_info(cvtest::TS::FAIL_BAD_ACCURACY);
+        FAIL() << "Incorrect transformation between the same frame (not the identity matrix), diff = " << diff << std::endl;
     }
 
     // 2. Generate random rigid body motion in some ranges several times (iterCount).
@@ -244,8 +262,7 @@ void CV_OdometryTest::run(int)
         warpFrame(image, depth, rvec, tvec, K, warpedImage, warpedDepth);
         dilateFrame(warpedImage, warpedDepth); // due to inaccuracy after warping
 
-        Mat imageMask(image.size(), CV_8UC1, Scalar(255));
-        isComputed = odometry->compute(image, depth, imageMask, warpedImage, warpedDepth, imageMask, calcRt);
+        isComputed = odometry->compute(image, depth, mask, warpedImage, warpedDepth, mask, calcRt);
         if(!isComputed)
             continue;
 
@@ -273,26 +290,25 @@ void CV_OdometryTest::run(int)
         if(5. * rdiffnorm < rnorm && 5 * tdiffnorm < tnorm)
             better_5times_count++;
 
-#if SHOW_DEBUG_LOG
-        std::cout << "Iter " << iter << std::endl;
-        std::cout << "rdiffnorm " << rdiffnorm << "; rnorm " << rnorm << std::endl;
-        std::cout << "tdiffnorm " << tdiffnorm << "; tnorm " << tnorm << std::endl;
+        CV_LOG_INFO(NULL, "Iter " << iter);
+        CV_LOG_INFO(NULL, "rdiffnorm " << rdiffnorm << "; rnorm " << rnorm);
+        CV_LOG_INFO(NULL, "tdiffnorm " << tdiffnorm << "; tnorm " << tnorm);
 
-        std::cout << "better_1time_count " << better_1time_count << "; better_5time_count " << better_5times_count << std::endl;
-#endif
+        CV_LOG_INFO(NULL, "better_1time_count " << better_1time_count << "; better_5time_count " << better_5times_count);
     }
-
 
     if(static_cast<double>(better_1time_count) < maxError1 * static_cast<double>(iterCount))
     {
-        ts->printf(cvtest::TS::LOG, "\nIncorrect count of accurate poses [1st case]: %f / %f", static_cast<double>(better_1time_count), maxError1 * static_cast<double>(iterCount));
-        ts->set_failed_test_info(cvtest::TS::FAIL_BAD_ACCURACY);
+        FAIL() << "Incorrect count of accurate poses [1st case]: "
+            << static_cast<double>(better_1time_count) << " / "
+            << maxError1 * static_cast<double>(iterCount) << std::endl;
     }
 
     if(static_cast<double>(better_5times_count) < maxError5 * static_cast<double>(iterCount))
     {
-        ts->printf(cvtest::TS::LOG, "\nIncorrect count of accurate poses [2nd case]: %f / %f", static_cast<double>(better_5times_count), maxError5 * static_cast<double>(iterCount));
-        ts->set_failed_test_info(cvtest::TS::FAIL_BAD_ACCURACY);
+        FAIL() << "Incorrect count of accurate poses [2nd case]: "
+            << static_cast<double>(better_5times_count) << " / "
+            << maxError5 * static_cast<double>(iterCount) << std::endl;
     }
 }
 
@@ -302,86 +318,88 @@ void CV_OdometryTest::run(int)
 
 TEST(RGBD_Odometry_Rgbd, algorithmic)
 {
-    CV_OdometryTest test(cv::Odometry::createFromName("RgbdOdometry"), 0.99, 0.89);
-    test.safe_run();
+    OdometryTest test(cv::Odometry::createFromName("RgbdOdometry"), 0.99, 0.89);
+    test.run();
 }
 
 TEST(RGBD_Odometry_ICP, algorithmic)
 {
-    CV_OdometryTest test(cv::Odometry::createFromName("ICPOdometry"), 0.99, 0.99);
-    test.safe_run();
+    OdometryTest test(cv::Odometry::createFromName("ICPOdometry"), 0.99, 0.99);
+    test.run();
 }
 
 TEST(RGBD_Odometry_RgbdICP, algorithmic)
 {
-    CV_OdometryTest test(cv::Odometry::createFromName("RgbdICPOdometry"), 0.99, 0.99);
-    test.safe_run();
+    OdometryTest test(cv::Odometry::createFromName("RgbdICPOdometry"), 0.99, 0.99);
+    test.run();
 }
 
 TEST(RGBD_Odometry_FastICP, algorithmic)
 {
-    CV_OdometryTest test(cv::Odometry::createFromName("FastICPOdometry"), 0.99, 0.99, FLT_EPSILON);
-    test.safe_run();
+    OdometryTest test(cv::Odometry::createFromName("FastICPOdometry"), 0.99, 0.99, FLT_EPSILON);
+    test.run();
 }
+
+TEST(RGBD_Odometry_Rgbd, UMats)
+{
+    OdometryTest test(cv::Odometry::createFromName("RgbdOdometry"), 0.99, 0.89);
+    test.checkUMats();
+}
+
+TEST(RGBD_Odometry_ICP, UMats)
+{
+    OdometryTest test(cv::Odometry::createFromName("ICPOdometry"), 0.99, 0.99);
+    test.checkUMats();
+}
+
+TEST(RGBD_Odometry_RgbdICP, UMats)
+{
+    OdometryTest test(cv::Odometry::createFromName("RgbdICPOdometry"), 0.99, 0.99);
+    test.checkUMats();
+}
+
+TEST(RGBD_Odometry_FastICP, UMats)
+{
+    OdometryTest test(cv::Odometry::createFromName("FastICPOdometry"), 0.99, 0.99, FLT_EPSILON);
+    test.checkUMats();
+}
+
 
 /****************************************************************************************\
 *                                Depth to 3d tests                                       *
 \****************************************************************************************/
 
-class CV_RgbdDepthTo3dTest : public cvtest::BaseTest
-{
-public:
-    CV_RgbdDepthTo3dTest()
-    { }
-    ~CV_RgbdDepthTo3dTest()
-    { }
-protected:
-    void run(int)
-    {
-        try
-        {
-            // K from a VGA Kinect
-            Mat K = (Mat_<float>(3, 3) << 525., 0., 319.5, 0., 525., 239.5, 0., 0., 1.);
-
-            // Create a random depth image
-            RNG rng;
-            Mat_<float> depth(480, 640);
-            rng.fill(depth, RNG::UNIFORM, 0, 100);
-
-            // Create some 3d points on the plane
-            int rows = depth.rows, cols = depth.cols;
-            Mat_<Vec3f> points3d;
-            depthTo3d(depth, K, points3d);
-
-            // Make sure the points belong to the plane
-            Mat points = points3d.reshape(1, rows * cols);
-            Mat image_points;
-            Mat rvec;
-            cv::Rodrigues(Mat::eye(3, 3, CV_32F), rvec);
-            Mat tvec = (Mat_<float>(1, 3) << 0, 0, 0);
-            projectPoints(points, rvec, tvec, K, Mat(), image_points);
-            image_points = image_points.reshape(2, rows);
-
-            float avg_diff = 0;
-            for (int y = 0; y < rows; ++y)
-                for (int x = 0; x < cols; ++x)
-                    avg_diff += (float)cv::norm(image_points.at<Vec2f>(y, x) - Vec2f((float)x, (float)y));
-
-            // Verify the function works
-            ASSERT_LE(avg_diff / rows / cols, 1e-4) << "Average error for ground truth is: " << (avg_diff / rows / cols);
-        }
-        catch (...)
-        {
-            ts->set_failed_test_info(cvtest::TS::FAIL_MISMATCH);
-        }
-        ts->set_failed_test_info(cvtest::TS::OK);
-    }
-};
-
 TEST(RGBD_DepthTo3d, compute)
 {
-    CV_RgbdDepthTo3dTest test;
-    test.safe_run();
+    // K from a VGA Kinect
+    Mat K = OdometryTest::getCameraMatrix();
+
+    // Create a random depth image
+    RNG rng;
+    Mat_<float> depth(480, 640);
+    rng.fill(depth, RNG::UNIFORM, 0, 100);
+
+    // Create some 3d points on the plane
+    int rows = depth.rows, cols = depth.cols;
+    Mat_<Vec3f> points3d;
+    depthTo3d(depth, K, points3d);
+
+    // Make sure the points belong to the plane
+    Mat points = points3d.reshape(1, rows * cols);
+    Mat image_points;
+    Mat rvec;
+    cv::Rodrigues(Mat::eye(3, 3, CV_32F), rvec);
+    Mat tvec = (Mat_<float>(1, 3) << 0, 0, 0);
+    projectPoints(points, rvec, tvec, K, Mat(), image_points);
+    image_points = image_points.reshape(2, rows);
+
+    float avg_diff = 0;
+    for (int y = 0; y < rows; ++y)
+        for (int x = 0; x < cols; ++x)
+            avg_diff += (float)cv::norm(image_points.at<Vec2f>(y, x) - Vec2f((float)x, (float)y));
+
+    // Verify the function works
+    ASSERT_LE(avg_diff / rows / cols, 1e-4) << "Average error for ground truth is: " << (avg_diff / rows / cols);
 }
 
 
