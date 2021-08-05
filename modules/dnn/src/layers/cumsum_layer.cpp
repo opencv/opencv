988d99b8f778e3ff5cc71261bf9b8806220c5d5a
Zane <zane.huang@mail.utoronto.ca>
2021-08-04 17:21:59 -0700

    Address review comments

--- modules/dnn/src/layers/cumsum_layer.cpp
+++ modules/dnn/src/layers/cumsum_layer.cpp
@@ -2,14 +2,9 @@
 // It is subject to the license terms in the LICENSE file found in the top-level directory
 // of this distribution and at http://opencv.org/license.html.
 
-// Copyright (C) 2021, Intel Corporation, all rights reserved.
-// Third party copyrights are property of their respective owners.
-
 #include "../precomp.hpp"
 #include "layers_common.hpp"
 
-#include <float.h>
-#include <algorithm>
 #include <opencv2/dnn/shape_utils.hpp>
 
 namespace cv
@@ -23,8 +18,8 @@ public:
     CumSumLayerImpl(const LayerParams &params)
     {
         axis_raw = params.get<int>("axis", 0);
-        exclusive = params.get<int>("exclusive", 0);
-        reverse = params.get<int>("reverse", 0);
+        exclusive_raw = params.get<int>("exclusive", 0);
+        reverse_raw = params.get<int>("reverse", 0);
         setParamsFrom(params);
     }
 
@@ -57,92 +52,74 @@ public:
         const auto *src_ptr = src_mat.ptr<float>();
 
         // Get axis.
-        const auto axis = normalize_axis(axis_raw, src_mat.dims);
+        const int axis = normalize_axis(axis_raw, src_mat.dims);
 
         // Get y tensor.
         auto &dst_mat = outputs[0];
         src_mat.copyTo(dst_mat);
         auto *dst_ptr = dst_mat.ptr<float>();
 
-        const auto outer_size = src_mat.total(0, axis);
-        const int inner_size = src_mat.size[axis];
-        const auto num_channels = src_mat.total(axis + 1);
-        const auto outer_step = src_mat.total(axis);
-        const auto inner_step = src_mat.total(axis + 1);
+        // Get flags.
+        const auto exclusive = exclusive_raw == 1;
+        const auto reverse = reverse_raw == 1;
+
+        // Get parameters to iterate outer dimension.
+        const size_t outer_size = src_mat.total(0, axis);
+        const size_t outer_step_length = src_mat.total(axis);
+
+        // Get parameters to iterate inner dimension.
+        const size_t inner_size = src_mat.size[axis];
+
+        if (!inner_size)
+            return;
+
+        const size_t inner_step_length = src_mat.total(axis + 1);
+        const int inner_step = (reverse ? -1 : 1) * inner_step_length;
+        const int inner_start = reverse ? inner_size - 1 : 0;
+        const int inner_stop = reverse ? -1 : inner_size;
+        const int inner_delta = reverse ? -1 : 1;
+
+        // Get parameters to populate channels.
+        const size_t num_channels = src_mat.total(axis + 1);
 
         for (size_t outer_dim = 0; outer_dim < outer_size; outer_dim++)
         {
-            auto outer_offset = outer_dim * outer_step;
+            const size_t outer_offset = outer_dim * outer_step_length;
+            size_t src_offset = outer_offset + inner_start * inner_step_length;
 
-            // Compute cumsum along axis.
-            if (reverse)
+            // Populate first element of inner dimension.
+            for (size_t channel = 0; channel < num_channels; channel++)
             {
-                auto src_offset = outer_offset + (inner_size - 1) * inner_step;
-
-                for (int inner_dim = inner_size - 1; inner_dim >= 0; inner_dim--)
+                if (exclusive)
                 {
-                    const auto dst_offset = outer_offset + inner_dim * inner_step;
-
-                    // Duplicate logic across channels.
-                    for (size_t channel = 0; channel < num_channels; channel++)
-                    {
-                        // Cumsum begins by copying the last element from input to output.
-                        if ((inner_size - 1) - inner_dim == exclusive)
-                        {
-                            dst_ptr[dst_offset + channel] = src_ptr[src_offset + channel];
-                            src_offset -= inner_step;
-                        }
-                        else if ((inner_size - 1) - inner_dim > exclusive)
-                        {
-                            const auto previous_dst_offset = dst_offset + inner_step;
-                            dst_ptr[dst_offset + channel] = dst_ptr[previous_dst_offset + channel] +
-                                                            src_ptr[src_offset + channel];
-                            src_offset -= inner_step;
-                        }
-                        else
-                        {
-                            dst_ptr[dst_offset + channel] = 0.0f;
-                        }
-                    }
+                    dst_ptr[src_offset + channel] = 0.0f;
+                }
+                else
+                {
+                    dst_ptr[src_offset + channel] = src_ptr[src_offset + channel];
+                    src_offset += inner_step;
                 }
             }
-            else
+
+            // Populate remaining elements of inner dimension.
+            for (int inner_dim = inner_start + inner_delta; inner_dim != inner_stop; inner_dim += inner_delta)
             {
-                auto src_offset = outer_offset;
+                const size_t dst_offset = outer_offset + inner_dim * inner_step_length;
 
-                for (int inner_dim = 0; inner_dim < inner_size; inner_dim++)
+                for (size_t channel = 0; channel < num_channels; channel++)
                 {
-                    const auto dst_offset = outer_offset + inner_dim * inner_step;
-
-                    // Duplicate logic across channels.
-                    for (size_t channel = 0; channel < num_channels; channel++)
-                    {
-                        // Cumsum begins by copying the first element from input to output.
-                        if (inner_dim == exclusive)
-                        {
-                            dst_ptr[dst_offset + channel] = src_ptr[src_offset + channel];
-                            src_offset += inner_step;
-                        }
-                        else if (inner_dim > exclusive)
-                        {
-                            const auto previous_dst_offset = dst_offset - inner_step;
-                            dst_ptr[dst_offset + channel] = dst_ptr[previous_dst_offset + channel] +
-                                                            src_ptr[src_offset + channel];
-                            src_offset += inner_step;
-                        }
-                        else
-                        {
-                            dst_ptr[dst_offset + channel] = 0.0f;
-                        }
-                    }
+                    const size_t previous_dst_offset = dst_offset - inner_step;
+                    dst_ptr[dst_offset + channel] = dst_ptr[previous_dst_offset + channel] +
+                            src_ptr[src_offset + channel];
+                    src_offset += inner_step;
                 }
             }
         }
     }
 
     int axis_raw;
-    int exclusive;
-    int reverse;
+    int exclusive_raw;
+    int reverse_raw;
 };
 
 Ptr<CumSumLayer> CumSumLayer::create(const LayerParams& params)
