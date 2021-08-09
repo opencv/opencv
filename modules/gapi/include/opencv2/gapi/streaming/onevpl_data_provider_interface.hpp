19538803fd1bcc20783cda1d6c30e8dd496642a8
Sergey Ivanov <sergey.ivanov@intel.com>
2021-08-09 15:56:06 +0300

    Add Copyright

--- modules/gapi/include/opencv2/gapi/streaming/onevpl_data_provider_interface.hpp
+++ modules/gapi/include/opencv2/gapi/streaming/onevpl_data_provider_interface.hpp
@@ -1,3 +1,9 @@
+// This file is part of OpenCV project.
+// It is subject to the license terms in the LICENSE file found in the top-level directory
+// of this distribution and at http://opencv.org/license.html.
+//
+// Copyright (C) 2021 Intel Corporation
+
 #ifndef GAPI_STREAMING_DATA_PROVIDER_INTERFACE_HPP
 #define GAPI_STREAMING_DATA_PROVIDER_INTERFACE_HPP
 #include <stdexcept>
--- modules/gapi/src/streaming/onevpl_file_data_provider.hpp
+++ modules/gapi/src/streaming/onevpl_file_data_provider.hpp
@@ -1,3 +1,9 @@
+// This file is part of OpenCV project.
+// It is subject to the license terms in the LICENSE file found in the top-level directory
+// of this distribution and at http://opencv.org/license.html.
+//
+// Copyright (C) 2021 Intel Corporation
+
 #ifndef GAPI_STREAMING_FILE_DATA_PROVIDER_HPP
 #define GAPI_STREAMING_FILE_DATA_PROVIDER_HPP
 #include <stdio.h>
