8b65a6f01cebb1ad140f48630dd1878c7dd9d572
Lukas Weber <lukas.weber@baw.de>
2021-08-23 10:46:33 +0200

    updated performance test

--- samples/python/stitching_detailed/test/stitching_detailed_old.py
+++ samples/python/stitching_detailed/test/stitching_detailed_old.py
@@ -7,7 +7,7 @@ Show how to use Stitcher API from python.
 # Python 2/3 compatibility
 from __future__ import print_function
 
-import argparse
+from types import SimpleNamespace
 from collections import OrderedDict
 
 import cv2 as cv
@@ -83,154 +83,6 @@ WAVE_CORRECT_CHOICES['vert'] = cv.detail.WAVE_CORRECT_VERT
 
 BLEND_CHOICES = ('multiband', 'feather', 'no',)
 
-parser = argparse.ArgumentParser(
-    prog="stitching_detailed.py", description="Rotation model images stitcher"
-)
-parser.add_argument(
-    'img_names', nargs='+',
-    help="Files to stitch", type=str
-)
-parser.add_argument(
-    '--try_cuda',
-    action='store',
-    default=False,
-    help="Try to use CUDA. The default value is no. All default values are for CPU mode.",
-    type=bool, dest='try_cuda'
-)
-parser.add_argument(
-    '--work_megapix', action='store', default=0.6,
-    help="Resolution for image registration step. The default is 0.6 Mpx",
-    type=float, dest='work_megapix'
-)
-parser.add_argument(
-    '--features', action='store', default=list(FEATURES_FIND_CHOICES.keys())[0],
-    help="Type of features used for images matching. The default is '%s'." % list(FEATURES_FIND_CHOICES.keys())[0],
-    choices=FEATURES_FIND_CHOICES.keys(),
-    type=str, dest='features'
-)
-parser.add_argument(
-    '--matcher', action='store', default='homography',
-    help="Matcher used for pairwise image matching. The default is 'homography'.",
-    choices=('homography', 'affine'),
-    type=str, dest='matcher'
-)
-parser.add_argument(
-    '--estimator', action='store', default=list(ESTIMATOR_CHOICES.keys())[0],
-    help="Type of estimator used for transformation estimation. The default is '%s'." % list(ESTIMATOR_CHOICES.keys())[0],
-    choices=ESTIMATOR_CHOICES.keys(),
-    type=str, dest='estimator'
-)
-parser.add_argument(
-    '--match_conf', action='store',
-    help="Confidence for feature matching step. The default is 0.3 for ORB and 0.65 for other feature types.",
-    type=float, dest='match_conf'
-)
-parser.add_argument(
-    '--conf_thresh', action='store', default=1.0,
-    help="Threshold for two images are from the same panorama confidence.The default is 1.0.",
-    type=float, dest='conf_thresh'
-)
-parser.add_argument(
-    '--ba', action='store', default=list(BA_COST_CHOICES.keys())[0],
-    help="Bundle adjustment cost function. The default is '%s'." % list(BA_COST_CHOICES.keys())[0],
-    choices=BA_COST_CHOICES.keys(),
-    type=str, dest='ba'
-)
-parser.add_argument(
-    '--ba_refine_mask', action='store', default='xxxxx',
-    help="Set refinement mask for bundle adjustment. It looks like 'x_xxx', "
-         "where 'x' means refine respective parameter and '_' means don't refine, "
-         "and has the following format:<fx><skew><ppx><aspect><ppy>. "
-         "The default mask is 'xxxxx'. "
-         "If bundle adjustment doesn't support estimation of selected parameter then "
-         "the respective flag is ignored.",
-    type=str, dest='ba_refine_mask'
-)
-parser.add_argument(
-    '--wave_correct', action='store', default=list(WAVE_CORRECT_CHOICES.keys())[0],
-    help="Perform wave effect correction. The default is '%s'" % list(WAVE_CORRECT_CHOICES.keys())[0],
-    choices=WAVE_CORRECT_CHOICES.keys(),
-    type=str, dest='wave_correct'
-)
-parser.add_argument(
-    '--save_graph', action='store', default=None,
-    help="Save matches graph represented in DOT language to <file_name> file.",
-    type=str, dest='save_graph'
-)
-parser.add_argument(
-    '--warp', action='store', default=WARP_CHOICES[0],
-    help="Warp surface type. The default is '%s'." % WARP_CHOICES[0],
-    choices=WARP_CHOICES,
-    type=str, dest='warp'
-)
-parser.add_argument(
-    '--seam_megapix', action='store', default=0.1,
-    help="Resolution for seam estimation step. The default is 0.1 Mpx.",
-    type=float, dest='seam_megapix'
-)
-parser.add_argument(
-    '--seam', action='store', default=list(SEAM_FIND_CHOICES.keys())[0],
-    help="Seam estimation method. The default is '%s'." % list(SEAM_FIND_CHOICES.keys())[0],
-    choices=SEAM_FIND_CHOICES.keys(),
-    type=str, dest='seam'
-)
-parser.add_argument(
-    '--compose_megapix', action='store', default=-1,
-    help="Resolution for compositing step. Use -1 for original resolution. The default is -1",
-    type=float, dest='compose_megapix'
-)
-parser.add_argument(
-    '--expos_comp', action='store', default=list(EXPOS_COMP_CHOICES.keys())[0],
-    help="Exposure compensation method. The default is '%s'." % list(EXPOS_COMP_CHOICES.keys())[0],
-    choices=EXPOS_COMP_CHOICES.keys(),
-    type=str, dest='expos_comp'
-)
-parser.add_argument(
-    '--expos_comp_nr_feeds', action='store', default=1,
-    help="Number of exposure compensation feed.",
-    type=np.int32, dest='expos_comp_nr_feeds'
-)
-parser.add_argument(
-    '--expos_comp_nr_filtering', action='store', default=2,
-    help="Number of filtering iterations of the exposure compensation gains.",
-    type=float, dest='expos_comp_nr_filtering'
-)
-parser.add_argument(
-    '--expos_comp_block_size', action='store', default=32,
-    help="BLock size in pixels used by the exposure compensator. The default is 32.",
-    type=np.int32, dest='expos_comp_block_size'
-)
-parser.add_argument(
-    '--blend', action='store', default=BLEND_CHOICES[0],
-    help="Blending method. The default is '%s'." % BLEND_CHOICES[0],
-    choices=BLEND_CHOICES,
-    type=str, dest='blend'
-)
-parser.add_argument(
-    '--blend_strength', action='store', default=5,
-    help="Blending strength from [0,100] range. The default is 5",
-    type=np.int32, dest='blend_strength'
-)
-parser.add_argument(
-    '--output', action='store', default='result.jpg',
-    help="The default is 'result.jpg'",
-    type=str, dest='output'
-)
-parser.add_argument(
-    '--timelapse', action='store', default=None,
-    help="Output warped images separately as frames of a time lapse movie, "
-         "with 'fixed_' prepended to input file names.",
-    type=str, dest='timelapse'
-)
-parser.add_argument(
-    '--rangewidth', action='store', default=-1,
-    help="uses range_width to limit number of images to match with.",
-    type=int, dest='rangewidth'
-)
-
-#__doc__ += '\n' + parser.format_help()
-
-
 def get_matcher(args):
     try_cuda = args.try_cuda
     matcher_type = args.matcher
@@ -271,7 +123,38 @@ def get_compensator(args):
 
 
 def main():
-    args = parser.parse_args()
+
+    args = {
+        "img_names":["boat5.jpg", "boat2.jpg",
+                     "boat3.jpg", "boat4.jpg",
+                     "boat1.jpg", "boat6.jpg"],
+        "try_cuda": False,
+        "work_megapix": 0.6,
+        "features": "orb",
+        "matcher": "homography",
+        "estimator": "homography",
+        "match_conf": None,
+        "conf_thresh": 1.0,
+        "ba": "ray",
+        "ba_refine_mask": "xxxxx",
+        "wave_correct": "horiz",
+        "save_graph": None,
+        "warp": "spherical",
+        "seam_megapix": 0.1,
+        "seam": "dp_color",
+        "compose_megapix": 3,
+        "expos_comp": "gain_blocks",
+        "expos_comp_nr_feeds": 1,
+        "expos_comp_nr_filtering": 2,
+        "expos_comp_block_size": 32,
+        "blend": "multiband",
+        "blend_strength": 5,
+        "output": "time_test.jpg",
+        "timelapse": None,
+        "rangewidth": -1
+    }
+
+    args = SimpleNamespace(**args)
     img_names = args.img_names
     work_megapix = args.work_megapix
     seam_megapix = args.seam_megapix
@@ -500,7 +383,7 @@ def main():
         result = None
         result_mask = None
         result, result_mask = blender.blend(result, result_mask)
-        cv.imwrite(result_name, result)
+        # cv.imwrite(result_name, result)
         return result
         # zoom_x = 600.0 / result.shape[1]
         # dst = cv.normalize(src=result, dst=None, alpha=255., norm_type=cv.NORM_MINMAX, dtype=cv.CV_8U)
--- samples/python/stitching_detailed/test/test_performance.py
+++ samples/python/stitching_detailed/test/test_performance.py
@@ -10,29 +10,52 @@ sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__),
                                                 '..', '..')))
 
 from stitching_detailed.stitcher import Stitcher
+from stitching_detailed_old import main
 # %%
 
 
 class TestStitcher(unittest.TestCase):
 
     def test_performance(self):
+
+        print("Run new Stitcher class:")
+
         start = time.time()
         tracemalloc.start()
 
         stitcher = Stitcher(compose_megapix=3)
-        result = stitcher.stitch(["boat5.jpg", "boat2.jpg",
-                                  "boat3.jpg", "boat4.jpg",
-                                  "boat1.jpg", "boat6.jpg"])
-        cv.imwrite("time_test.jpg", result)
+        stitcher.stitch(["boat5.jpg", "boat2.jpg",
+                         "boat3.jpg", "boat4.jpg",
+                         "boat1.jpg", "boat6.jpg"])
 
         current_memory, peak_memory = tracemalloc.get_traced_memory()
         tracemalloc.stop()
         end = time.time()
         time_needed = end - start
 
-        self.assertLessEqual(current_memory / 10**6, 52)
-        self.assertLessEqual(current_memory / 10**6, 136)
-        self.assertLessEqual(time_needed, 3.77)
+        print(f"Peak was {peak_memory / 10**6} MB")
+        print(f"Time was {time_needed} s")
+
+        print("Run original stitching_detailed.py:")
+
+        start = time.time()
+        tracemalloc.start()
+
+        main()
+
+        current_memory_old, peak_memory_old = tracemalloc.get_traced_memory()
+        tracemalloc.stop()
+        end = time.time()
+        time_needed_old = end - start
+
+        print(f"Peak was {peak_memory_old / 10**6} MB")
+        print(f"Time was {time_needed_old} s")
+
+        self.assertLessEqual(current_memory / 10**6, 0.2)
+        self.assertLessEqual(peak_memory / 10**6, peak_memory_old / 10**6)
+        uncertainty_based_on_run = 0.1
+        self.assertLessEqual(time_needed - uncertainty_based_on_run,
+                             time_needed_old)
 
 
 def starttest():
