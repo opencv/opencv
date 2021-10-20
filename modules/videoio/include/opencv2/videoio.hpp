8db3b719a9ce3d949455a483641baf3e422dc3b1
MaximMilashchenko <67949029+MaximMilashchenko@users.noreply.github.com>
2021-10-20 16:18:24 +0300

    Merge pull request #19721 from MaximMilashchenko:Audio
    add audio support in cap_msmf
    
    * audio msmf
    
    * fixed warnings
    
    * minor fix
    
    * fixed SampleTime MSMF
    
    * minor fix, fixed audio test, retrieveAudioFrame
    
    * fixed warnings
    
    * impelemented sync audio and video stream with start offset
    
    * fixed error
    
    * fixed docs
    
    * fixed audio sample
    
    * CAP_PROP_AUDIO_POS, minor fixed
    
    * fixed warnings
    
    * videoio(MSMF): update audio test checks, add debug logging
    
    * fixed
    
    * fixed desynchronization of time positions, warnings
    
    * fixed warnings
    
    * videoio(audio): tune tests checks
    
    * videoio(audio): update properties description
    
    * build warnings
    
    Co-authored-by: Alexander Alekhin <alexander.a.alekhin@gmail.com>

--- modules/videoio/include/opencv2/videoio.hpp
+++ modules/videoio/include/opencv2/videoio.hpp
@@ -189,6 +189,17 @@ enum VideoCaptureProperties {
        CAP_PROP_OPEN_TIMEOUT_MSEC=53, //!< (**open-only**) timeout in milliseconds for opening a video capture (applicable for FFmpeg back-end only)
        CAP_PROP_READ_TIMEOUT_MSEC=54, //!< (**open-only**) timeout in milliseconds for reading from a video capture (applicable for FFmpeg back-end only)
        CAP_PROP_STREAM_OPEN_TIME_USEC =55, //<! (read-only) time in microseconds since Jan 1 1970 when stream was opened. Applicable for FFmpeg backend only. Useful for RTSP and other live streams
+       CAP_PROP_VIDEO_TOTAL_CHANNELS = 56, //!< (read-only) Number of video channels
+       CAP_PROP_VIDEO_STREAM = 57, //!< (**open-only**) Specify video stream, 0-based index. Use -1 to disable video stream from file or IP cameras. Default value is 0.
+       CAP_PROP_AUDIO_STREAM = 58, //!< (**open-only**) Specify stream in multi-language media files, -1 - disable audio processing or microphone. Default value is -1.
+       CAP_PROP_AUDIO_POS = 59, //!< (read-only) Audio position is measured in samples. Accurate audio sample timestamp of previous grabbed fragment. See CAP_PROP_AUDIO_SAMPLES_PER_SECOND and CAP_PROP_AUDIO_SHIFT_NSEC.
+       CAP_PROP_AUDIO_SHIFT_NSEC = 60, //!< (read only) Contains the time difference between the start of the audio stream and the video stream in nanoseconds. Positive value means that audio is started after the first video frame. Negative value means that audio is started before the first video frame.
+       CAP_PROP_AUDIO_DATA_DEPTH = 61, //!< (open, read) Alternative definition to bits-per-sample, but with clear handling of 32F / 32S
+       CAP_PROP_AUDIO_SAMPLES_PER_SECOND = 62, //!< (read-only) determined from file/codec input. If not specified, then selected audio sample rate is 44100
+       CAP_PROP_AUDIO_BASE_INDEX = 63, //!< (read-only) Index of the first audio channel for .retrieve() calls. That audio channel number continues enumeration after video channels.
+       CAP_PROP_AUDIO_TOTAL_CHANNELS = 64, //!< (read-only) Number of audio channels in the selected audio stream (mono, stereo, etc)
+       CAP_PROP_AUDIO_TOTAL_STREAMS = 65, //!< (read-only) Number of audio streams.
+       CAP_PROP_AUDIO_SYNCHRONIZE = 66, //!< (open, read) Enables audio synchronization.
 #ifndef CV_DOXYGEN
        CV__CAP_PROP_LATEST
 #endif
