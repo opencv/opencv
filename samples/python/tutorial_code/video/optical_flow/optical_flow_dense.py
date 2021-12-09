222184471e5c492638224258333c04bfac1b4faf
UncleLLD <duanzhch@tju.edu.cn>
2021-12-09 23:22:21 +0800

    fix cvtColor error after the video end

--- samples/python/tutorial_code/video/optical_flow/optical_flow_dense.py
+++ samples/python/tutorial_code/video/optical_flow/optical_flow_dense.py
@@ -7,22 +7,23 @@ hsv = np.zeros_like(frame1)
 hsv[..., 1] = 255
 while(1):
     ret, frame2 = cap.read()
-    if ret:
-        next = cv.cvtColor(frame2, cv.COLOR_BGR2GRAY)
-        flow = cv.calcOpticalFlowFarneback(prvs, next, None, 0.5, 3, 15, 3, 5, 1.2, 0)
-        mag, ang = cv.cartToPolar(flow[..., 0], flow[..., 1])
-        hsv[..., 0] = ang*180/np.pi/2
-        hsv[..., 2] = cv.normalize(mag, None, 0, 255, cv.NORM_MINMAX)
-        bgr = cv.cvtColor(hsv, cv.COLOR_HSV2BGR)
-        cv.imshow('frame2', bgr)
-        k = cv.waitKey(30) & 0xff
-        if k == 27:
-            break
-        elif k == ord('s'):
-            cv.imwrite('opticalfb.png', frame2)
-            cv.imwrite('opticalhsv.png', bgr)
-        prvs = next
-    else:
+    if not ret:
+        print('No frames grabbed!')
         break
+
+    next = cv.cvtColor(frame2, cv.COLOR_BGR2GRAY)
+    flow = cv.calcOpticalFlowFarneback(prvs, next, None, 0.5, 3, 15, 3, 5, 1.2, 0)
+    mag, ang = cv.cartToPolar(flow[..., 0], flow[..., 1])
+    hsv[..., 0] = ang*180/np.pi/2
+    hsv[..., 2] = cv.normalize(mag, None, 0, 255, cv.NORM_MINMAX)
+    bgr = cv.cvtColor(hsv, cv.COLOR_HSV2BGR)
+    cv.imshow('frame2', bgr)
+    k = cv.waitKey(30) & 0xff
+    if k == 27:
+        break
+    elif k == ord('s'):
+        cv.imwrite('opticalfb.png', frame2)
+        cv.imwrite('opticalhsv.png', bgr)
+    prvs = next
 
 cv.destroyAllWindows()
