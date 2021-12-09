ffe6d7365bf961c8dbaec11d0871bbb2afea4d7e
UncleLLD <duanzhch@tju.edu.cn>
2021-12-09 23:17:36 +0800

    fix cvtColor error after the video end

--- samples/python/tutorial_code/video/optical_flow/optical_flow.py
+++ samples/python/tutorial_code/video/optical_flow/optical_flow.py
@@ -34,34 +34,35 @@ mask = np.zeros_like(old_frame)
 
 while(1):
     ret, frame = cap.read()
-    if ret:
-        frame_gray = cv.cvtColor(frame, cv.COLOR_BGR2GRAY)
-
-        # calculate optical flow
-        p1, st, err = cv.calcOpticalFlowPyrLK(old_gray, frame_gray, p0, None, **lk_params)
-
-        # Select good points
-        if p1 is not None:
-            good_new = p1[st==1]
-            good_old = p0[st==1]
-
-        # draw the tracks
-        for i, (new, old) in enumerate(zip(good_new, good_old)):
-            a, b = new.ravel()
-            c, d = old.ravel()
-            mask = cv.line(mask, (int(a), int(b)), (int(c), int(d)), color[i].tolist(), 2)
-            frame = cv.circle(frame, (int(a), int(b)), 5, color[i].tolist(), -1)
-        img = cv.add(frame, mask)
-
-        cv.imshow('frame', img)
-        k = cv.waitKey(30) & 0xff
-        if k == 27:
-            break
-
-        # Now update the previous frame and previous points
-        old_gray = frame_gray.copy()
-        p0 = good_new.reshape(-1, 1, 2)
-    else:
+    if not ret:
+        print('No frames grabbed!')
         break
+
+    frame_gray = cv.cvtColor(frame, cv.COLOR_BGR2GRAY)
+
+    # calculate optical flow
+    p1, st, err = cv.calcOpticalFlowPyrLK(old_gray, frame_gray, p0, None, **lk_params)
+
+    # Select good points
+    if p1 is not None:
+        good_new = p1[st==1]
+        good_old = p0[st==1]
+
+    # draw the tracks
+    for i, (new, old) in enumerate(zip(good_new, good_old)):
+        a, b = new.ravel()
+        c, d = old.ravel()
+        mask = cv.line(mask, (int(a), int(b)), (int(c), int(d)), color[i].tolist(), 2)
+        frame = cv.circle(frame, (int(a), int(b)), 5, color[i].tolist(), -1)
+    img = cv.add(frame, mask)
+
+    cv.imshow('frame', img)
+    k = cv.waitKey(30) & 0xff
+    if k == 27:
+        break
+
+    # Now update the previous frame and previous points
+    old_gray = frame_gray.copy()
+    p0 = good_new.reshape(-1, 1, 2)
 
 cv.destroyAllWindows()
