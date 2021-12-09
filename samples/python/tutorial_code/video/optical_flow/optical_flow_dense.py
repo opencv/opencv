601e226ecb5699ae0ac0ef9be4929b57cd0f4910
UncleLLD <duanzhch@tju.edu.cn>
2021-12-09 23:12:13 +0800

    reset next variable

--- samples/python/tutorial_code/video/optical_flow/optical_flow_dense.py
+++ samples/python/tutorial_code/video/optical_flow/optical_flow_dense.py
@@ -2,14 +2,14 @@ import numpy as np
 import cv2 as cv
 cap = cv.VideoCapture(cv.samples.findFile("vtest.avi"))
 ret, frame1 = cap.read()
-frame1_gray = cv.cvtColor(frame1, cv.COLOR_BGR2GRAY)
+prvs = cv.cvtColor(frame1, cv.COLOR_BGR2GRAY)
 hsv = np.zeros_like(frame1)
 hsv[..., 1] = 255
 while(1):
     ret, frame2 = cap.read()
     if ret:
-        frame2_gray = cv.cvtColor(frame2, cv.COLOR_BGR2GRAY)
-        flow = cv.calcOpticalFlowFarneback(frame1_gray, frame2_gray, None, 0.5, 3, 15, 3, 5, 1.2, 0)
+        next = cv.cvtColor(frame2, cv.COLOR_BGR2GRAY)
+        flow = cv.calcOpticalFlowFarneback(prvs, next, None, 0.5, 3, 15, 3, 5, 1.2, 0)
         mag, ang = cv.cartToPolar(flow[..., 0], flow[..., 1])
         hsv[..., 0] = ang*180/np.pi/2
         hsv[..., 2] = cv.normalize(mag, None, 0, 255, cv.NORM_MINMAX)
@@ -21,7 +21,7 @@ while(1):
         elif k == ord('s'):
             cv.imwrite('opticalfb.png', frame2)
             cv.imwrite('opticalhsv.png', bgr)
-        frame1_gray = frame2_gray
+        prvs = next
     else:
         break
 
