c8c309a59a2cf9f236e2f81ff506f099842d8129
dwardor <50771662+dwardor@users.noreply.github.com>
2021-12-22 11:28:08 +0100

    Fix typos and grammar in comments

--- cmake/OpenCVFindLAPACK.cmake
+++ cmake/OpenCVFindLAPACK.cmake
@@ -54,17 +54,17 @@ macro(ocv_lapack_check)
     list(APPEND _lapack_content "
 #if defined(LAPACK_GLOBAL) || defined(LAPACK_NAME)
 /*
- * Using netlib's reference LAPACK implementation version >= 3.4.0 (first with C interface)
- * use LAPACK_xxxx to transparently (via predefined lapack macros) deal with pre and post 3.9.1 versions
+ * Using netlib's reference LAPACK implementation version >= 3.4.0 (first with C interface).
+ * Use LAPACK_xxxx to transparently (via predefined lapack macros) deal with pre and post 3.9.1 versions.
  * LAPACK 3.9.1 introduces LAPACK_FORTRAN_STRLEN_END and modifies (through preprocessing) the declarations of the following functions used in opencv
  *        sposv_, dposv_, spotrf_, dpotrf_, sgesdd_, dgesdd_, sgels_, dgels_
- * which end up with and extra parameter...
- * So we also need to preprocess the function calls in opencv coding by adding LAPACK_
- * The good news is the preprocessing works fine whatever netlib's LAPACK version
+ * which end up with an extra parameter.
+ * So we also need to preprocess the function calls in opencv coding by prefixing them with LAPACK_.
+ * The good news is the preprocessing works fine whatever netlib's LAPACK version.
  */
 #define OCV_LAPACK_FUNC(f) LAPACK_##f
 #else
-/* Using other LAAPCK implementations so fall back to opencv's assumption until now */
+/* Using other LAPACK implementations so fall back to opencv's assumption until now */
 #define OCV_LAPACK_FUNC(f) f##_
 #endif
 ")
