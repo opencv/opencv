2d5246f08b17f9d1d5e61fb438fb8510cea8aef8
ubu <artem.saratovtsev>
2022-01-17 18:24:26 +0300

    fully fix python test

--- modules/3d/misc/python/test/test_odometry.py
+++ modules/3d/misc/python/test/test_odometry.py
@@ -11,44 +11,44 @@ class odometry_test(NewOpenCVTests):
     def test_OdometryDepth(self):
         depth_image = self.get_sample('cv/rgbd/depth.png', cv.IMREAD_ANYDEPTH)
         Rt = np.array(
-            [1, 0, 0, 0],
-            [0, 1, 0, 0],
-            [0, 0, 1, 0],
-            [0, 0, 0, 1]
+            [[1, 0, 0, 0],
+             [0, 1, 0, 0],
+             [0, 0, 1, 0],
+             [0, 0, 0, 1]]
         )
         Rt_res = np.zeros((4, 4))
 
         odometry = cv.Odometry(cv.DEPTH)
         isCorrect = odometry.compute(depth_image, depth_image, Rt_res)
 
-        res = cv.norm(Rt, Rt_res)
+        res = (Rt - Rt_res).sum()
 
         eps = 0.05
         self.assertLessEqual(res, eps)
         self.assertTrue(isCorrect)
 
     def test_OdometryRGB(self):
-        rgb_image = self.get_sample('cv/rgbd/rgb.png', cv.IMREAD_ANYDEPTH)
+        rgb_image = self.get_sample('cv/rgbd/rgb.png', cv.IMREAD_ANYCOLOR)
         Rt = np.array(
-            [1, 0, 0, 0],
-            [0, 1, 0, 0],
-            [0, 0, 1, 0],
-            [0, 0, 0, 1]
+            [[1, 0, 0, 0],
+             [0, 1, 0, 0],
+             [0, 0, 1, 0],
+             [0, 0, 0, 1]]
         )
         Rt_res = np.zeros((4, 4))
 
         odometry = cv.Odometry(cv.RGB)
         isCorrect = odometry.compute(rgb_image, rgb_image, Rt_res)
 
-        res = cv.norm(Rt, Rt_res)
-
+        res = (Rt - Rt_res).sum()
+        
         eps = 0.05
         self.assertLessEqual(res, eps)
         self.assertTrue(isCorrect)
 
     def test_OdometryRGB(self):
         depth_image = self.get_sample('cv/rgbd/depth.png', cv.IMREAD_ANYDEPTH)
-        rgb_image = self.get_sample('cv/rgbd/rgb.png', cv.IMREAD_ANYDEPTH)
+        rgb_image = self.get_sample('cv/rgbd/rgb.png', cv.IMREAD_ANYCOLOR)
         Rt = np.array(
             [[1, 0, 0, 0],
              [0, 1, 0, 0],
