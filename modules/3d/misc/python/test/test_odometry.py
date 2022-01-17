4ed618e5d5684bb9f389273a6de0ad5f652f4037
ubu <artem.saratovtsev>
2022-01-17 11:40:59 +0300

    new simple tests created

---
+++ modules/3d/misc/python/test/test_odometry.py
@@ -0,0 +1,75 @@
+#!/usr/bin/env python
+
+import numpy as np
+import cv2 as cv
+import unittest
+
+from tests_common import NewOpenCVTests
+
+class odometry_test(NewOpenCVTests):
+
+    def test_OdometryDepth(self):
+        depth_image = self.get_sample('cv/rgbd/depth.png', cv.IMREAD_ANYDEPTH)
+        Rt = np.array(
+            [1, 0, 0, 0],
+            [0, 1, 0, 0],
+            [0, 0, 1, 0],
+            [0, 0, 0, 1]
+        )
+        Rt_res = np.zeros((4, 4))
+
+        odometry = cv.Odometry(cv.DEPTH)
+        isCorrect = odometry.compute(depth_image, depth_image, Rt_res)
+
+        res = cv.norm(Rt, Rt_res)
+        print(res)
+
+        eps = 0.05
+        self.assertLessEqual(res, eps)
+        self.assettTrue(isCorrect)
+
+    def test_OdometryRGB(self):
+        rgb_image = self.get_sample('cv/rgbd/rgb.png', cv.IMREAD_ANYDEPTH)
+        Rt = np.array(
+            [1, 0, 0, 0],
+            [0, 1, 0, 0],
+            [0, 0, 1, 0],
+            [0, 0, 0, 1]
+        )
+        Rt_res = np.zeros((4, 4))
+
+        odometry = cv.Odometry(cv.RGB)
+        isCorrect = odometry.compute(rgb_image, rgb_image, Rt_res)
+
+        res = cv.norm(Rt, Rt_res)
+        print(res)
+
+        eps = 0.05
+        self.assertLessEqual(res, eps)
+        self.assettTrue(isCorrect)
+
+    def test_OdometryRGB(self):
+        depth_image = self.get_sample('cv/rgbd/depth.png', cv.IMREAD_ANYDEPTH)
+        rgb_image = self.get_sample('cv/rgbd/rgb.png', cv.IMREAD_ANYDEPTH)
+        Rt = np.array(
+            [1, 0, 0, 0],
+            [0, 1, 0, 0],
+            [0, 0, 1, 0],
+            [0, 0, 0, 1]
+        )
+        Rt_res = np.zeros((4, 4))
+
+        odometry = cv.Odometry(cv.DEPTH_RGB)
+        isCorrect = odometry.compute(depth_image, rgb_image, depth_image, rgb_image, Rt_res)
+
+        res = cv.norm(Rt, Rt_res)
+        print(res)
+
+        eps = 0.05
+        self.assertLessEqual(res, eps)
+        self.assettTrue(isCorrect)
+
+if __name__ == '__main__':
+    NewOpenCVTests.bootstrap()
+
+
