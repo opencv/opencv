43533bf8ca63b97ea5c5dded160d8ff23baf3278
Saratovtsev <artem.saratovtsev@intel.com>
2022-01-26 11:47:12 +0300

    volume docs fix

--- modules/3d/include/opencv2/3d/volume.hpp
+++ modules/3d/include/opencv2/3d/volume.hpp
@@ -25,50 +25,76 @@ public:
     Volume(VolumeType vtype, const VolumeSettings& settings);
     ~Volume();
 
-    /** @brief Interrates the input data to the volume.
-    * @param frame the object which consist depth and image etc.
-    * @param pose the pose of camera.
+    /** @brief Integrates the input data to the volume.
+
+    Camera intrinsics are taken from volume settings structure.
+
+    * @param frame the object from which to take depth and image data.
+    * @param pose the pose of camera in global coordinates.
     */
     void integrate(const OdometryFrame& frame, InputArray pose);
-    /** @brief Interrates the input data to the volume.
-    * @param depth the depth frame.
-    * @param pose the pose of camera.
+
+    /** @brief Integrates the input data to the volume.
+
+    Camera intrinsics are taken from volume settings structure.
+
+    * @param depth the depth image.
+    * @param pose the pose of camera in global coordinates.
     */
     void integrate(InputArray depth, InputArray pose);
-    /** @brief Interrates the input data to the volume.
-    * @param depth the depth frame.
-    * @param image the depth frame (only for ColorTSDF).
-    * @param pose the pose of camera.
+
+    /** @brief Integrates the input data to the volume.
+
+    Camera intrinsics are taken from volume settings structure.
+
+    * @param frame the object from which to take depth and image data.
+    * @param depth the depth image.
+    * @param color the color image (only for ColorTSDF).
+    * @param pose the pose of camera in global coordinates.
     */
     void integrate(InputArray depth, InputArray image, InputArray pose);
 
-    /** @brief Extract the points of volume from set position.
-    * @param cameraPose the pose of camera (camera in volume environment, not real).
-    * @param outFrame the object, which store the result data.
+    /** @brief Renders the volume contents into an image. The resulting points and normals are in camera's coordinate system.
+
+    Rendered image size and camera intrinsics are taken from volume settings structure.
+
+    * @param cameraPose the pose of camera in global coordinates.
+    * @param outFrame the object where to store rendered points and normals.
     */
     void raycast(InputArray cameraPose, OdometryFrame& outFrame) const;
-    /** @brief Extract the points of volume from set position.
-    * @param cameraPose the pose of camera (camera in volume environment, not real).
-    * @param points the storage of points in the image.
-    * @param normals the storage of normals (corresponding to points) in the image.
-    * @param colors the storage of colors (corresponding to points) in the image (only for ColorTSDF).
+
+    /** @brief Renders the volume contents into an image. The resulting points and normals are in camera's coordinate system.
+
+    Rendered image size and camera intrinsics are taken from volume settings structure.
+
+    * @param cameraPose the pose of camera in global coordinates.
+    * @param points image to store rendered points.
+    * @param normals image to store rendered normals corresponding to points.
+    * @param colors image to store rendered colors corresponding to points (only for ColorTSDF).
     */
     void raycast(InputArray cameraPose, OutputArray points, OutputArray normals, OutputArray colors = noArray()) const;
 
-    /** @brief Extract the points of volume from set position.
-    * @param cameraPose the pose of camera (camera in volume environment, not real).
+    /** @brief Renders the volume contents into an image. The resulting points and normals are in camera's coordinate system.
+
+    Rendered image size and camera intrinsics are taken from volume settings structure.
+
+    * @param cameraPose the pose of camera in global coordinates.
     * @param height the height of result image.
     * @param width the width of result image.
-    * @param outFrame the object, which store the result data.
+    * @param outFrame the object where to store rendered points and normals.
     */
     void raycast(InputArray cameraPose, int height, int width, OdometryFrame& outFrame) const;
-    /** @brief Extract the points of volume from set position.
-    * @param cameraPose the pose of camera (camera in volume environment, not real).
+
+    /** @brief Renders the volume contents into an image. The resulting points and normals are in camera's coordinate system.
+
+    Rendered image size and camera intrinsics are taken from volume settings structure.
+
+    * @param cameraPose the pose of camera in global coordinates.
     * @param height the height of result image.
     * @param width the width of result image.
-    * @param points the storage of points in the image.
-    * @param normals the storage of normals (corresponding to points) in the image.
-    * @param colors the storage of colors (corresponding to points) in the image (only for ColorTSDF).
+    * @param points image to store rendered points.
+    * @param normals image to store rendered normals corresponding to points.
+    * @param colors image to store rendered colors corresponding to points (only for ColorTSDF).
     */
     void raycast(InputArray cameraPose, int height, int width, OutputArray points, OutputArray normals, OutputArray colors = noArray()) const;
 
@@ -89,15 +115,15 @@ public:
     */
     void fetchPointsNormalsColors(OutputArray points, OutputArray normals, OutputArray colors) const;
 
-    /** @brief clear all data in volume
+    /** @brief clear all data in volume.
     */
     void reset();
 
-    /** @brief return visible blocks in volume
+    /** @brief return visible blocks in volume.
     */
     int getVisibleBlocks() const;
 
-    /** @brief return number of vulmeunits in volume
+    /** @brief return number of vulmeunits in volume.
     */
     size_t getTotalVolumeUnits() const;
 
