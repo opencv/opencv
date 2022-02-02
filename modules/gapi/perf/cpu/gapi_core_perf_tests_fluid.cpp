99a4f336fb6cf5daa0371c68d39643792ddb7a33
Anna Khakimova <anna.khakimova@intel.com>
2022-02-02 21:47:01 +0300

    Merge pull request #21474 from anna-khakimova:ak/simd_for_divc
    GAPI Fluid: SIMD for DivC kernel.
    
    * GAPI Fluid:SIMD for DivC
    
    * Applied comment

--- modules/gapi/perf/cpu/gapi_core_perf_tests_fluid.cpp
+++ modules/gapi/perf/cpu/gapi_core_perf_tests_fluid.cpp
@@ -93,8 +93,8 @@ INSTANTIATE_TEST_CASE_P(DivPerfTestFluid, DivPerfTest,
 INSTANTIATE_TEST_CASE_P(DivCPerfTestFluid, DivCPerfTest,
     Combine(Values(Tolerance_FloatRel_IntAbs(1e-6, 1).to_compare_f()),
             Values(szSmall128, szVGA, sz720p, sz1080p),
-            Values(CV_8UC1, CV_8UC3, CV_16SC1, CV_32FC1),
-            Values(-1, CV_8U, CV_32F),
+            Values(CV_8UC1, CV_8UC3, CV_16UC1, CV_16SC1, CV_32FC1),
+            Values(-1, CV_8U, CV_16U, CV_16S, CV_32F),
             Values(1.0),
             Values(cv::compile_args(CORE_FLUID))));
 
