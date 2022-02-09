ff6e56e79c862e971e2e3f11cf742015e9cb2b47
Anatoliy Talamanov <anatoliy.talamanov@intel.com>
2022-02-09 19:52:23 +0300

    Merge pull request #21579 from TolyaTalamanov:at/handle-errors-in-iebackend
    [G-API] Handle errors in IEBackend & modeling tool
    
    * Handle errors in IEBackend & modeling tool
    
    * Handle exceptions in callback
    
    * Add const cv to exception

--- modules/gapi/src/logger.hpp
+++ modules/gapi/src/logger.hpp
@@ -14,10 +14,12 @@
 #  define GAPI_LOG_INFO(tag, ...)    CV_LOG_INFO(tag, __VA_ARGS__)
 #  define GAPI_LOG_WARNING(tag, ...) CV_LOG_WARNING(tag, __VA_ARGS__)
 #  define GAPI_LOG_DEBUG(tag, ...)    CV_LOG_DEBUG(tag, __VA_ARGS__)
+#  define GAPI_LOG_FATAL(tag, ...)   CV_LOG_FATAL(tag, __VA_ARGS__)
 #else
 #  define GAPI_LOG_INFO(tag, ...)
 #  define GAPI_LOG_WARNING(tag, ...)
 #  define GAPI_LOG_DEBUG(tag, ...)
+#  define GAPI_LOG_FATAL(tag, ...)
 #endif //  !defined(GAPI_STANDALONE)
 
 
