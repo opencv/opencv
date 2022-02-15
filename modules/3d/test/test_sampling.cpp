9ad486706900077752b8de4e360400791d221408
zoom <zhongwl2018@mail.sustech.edu.cn>
2022-02-15 13:46:12 +0800

    Add author information

--- modules/3d/test/test_sampling.cpp
+++ modules/3d/test/test_sampling.cpp
@@ -1,6 +1,8 @@
 // This file is part of OpenCV project.
 // It is subject to the license terms in the LICENSE file found in the top-level directory
 // of this distribution and at http://opencv.org/license.html.
+//
+// Copyright (C) 2021, Wanli Zhong <zhongwl2018@mail.sustech.edu.cn>
 
 #include "test_precomp.hpp"
 
