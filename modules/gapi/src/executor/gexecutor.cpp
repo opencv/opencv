ad80cbb73bcda7a1a1b0dcff1d7e9c94b7e31c9e
Anatoliy Talamanov <anatoliy.talamanov@intel.com>
2022-02-24 20:02:07 +0300

    Rethrow exception in non-streaming executor

--- modules/gapi/src/executor/gexecutor.cpp
+++ modules/gapi/src/executor/gexecutor.cpp
@@ -278,9 +278,17 @@ class cv::gimpl::GExecutor::Output final: public cv::gimpl::GIslandExecutable::I
         out_idx[cv::gimpl::proto::ptr(r)] = idx;
         return r;
     }
-    void post(GRunArgP&&, const std::exception_ptr&) override { } // Do nothing here
+    void post(GRunArgP&&, const std::exception_ptr& eptr) override
+    {
+        if (eptr) {
+            std::rethrow_exception(eptr);
+        }
+    }
     void post(EndOfStream&&) override {} // Do nothing here too
-    void post(Exception&&) override {} // Do nothing here too
+    void post(Exception&& ex) override
+    {
+        std::rethrow_exception(ex.eptr);
+    }
     void meta(const GRunArgP &out, const GRunArg::Meta &m) override
     {
         const auto idx = out_idx.at(cv::gimpl::proto::ptr(out));
