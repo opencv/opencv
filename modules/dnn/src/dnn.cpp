a120adde63abdf6bc877e1d047302302f82542ba
Alexander Alekhin <alexander.a.alekhin@gmail.com>
2022-02-28 09:42:38 +0000

    dnn: add dnn.cpp file with information about git commits history

---
+++ modules/dnn/src/dnn.cpp
@@ -0,0 +1,10 @@
+// This file is part of OpenCV project.
+// It is subject to the license terms in the LICENSE file found in the top-level directory
+// of this distribution and at http://opencv.org/license.html.
+
+#include "precomp.hpp"
+
+// This is a stub file to provide git history information.
+// Content has been moved, see PR: https://github.com/opencv/opencv/pull/21662
+// Base commit: 19926e2979ef049a89dd029e2231555db40c2776
+// Original dnn.cpp content: https://github.com/opencv/opencv/blame/19926e2979ef049a89dd029e2231555db40c2776/modules/dnn/src/dnn.cpp
