c6723ceab17babc778801adfe3e03c4524cbe859
Anatoliy Talamanov <anatoliy.talamanov@intel.com>
2022-04-01 00:00:45 +0300

    Merge pull request #21782 from TolyaTalamanov:at/fix-1d-mat-problems
    [G-API] Fix problems with 1D cv::Mat as graph output
    
    * Fix issues with 1D cv::Mat
    
    * Fix cv::Mat::create
    
    * Fix standalone build
    
    * Add test on 1d mat
    
    * Fix warning
    
    * Add additional condition
    
    * Add more tests

--- modules/gapi/src/api/gbackend.cpp
+++ modules/gapi/src/api/gbackend.cpp
@@ -411,6 +411,12 @@ void createMat(const cv::GMatDesc &desc, cv::Mat& mat)
     {
         GAPI_Assert(!desc.planar);
         mat.create(desc.dims, desc.depth);
+#if !defined(GAPI_STANDALONE)
+        // NB: WA for 1D mats.
+        if (desc.dims.size() == 1u) {
+            mat.dims = 1;
+        }
+#endif
     }
 }
 
