26dc669a115243f01f80bbcec931200318c17c36
Kataev Victor <kataev.victor.1234@gmail.com>
2022-04-12 19:50:23 +0300

    Move undistortPoints test to google test, refactor

--- modules/calib3d/test/test_undistort_points.cpp
+++ modules/calib3d/test/test_undistort_points.cpp
@@ -5,30 +5,19 @@
 
 namespace opencv_test { namespace {
 
-class CV_UndistortTest : public cvtest::BaseTest
+class UndistortPointsTest : public ::testing::Test
 {
-public:
-    CV_UndistortTest();
-    ~CV_UndistortTest();
 protected:
-    void run(int);
-private:
     void generate3DPointCloud(vector<Point3f>& points, Point3f pmin = Point3f(-1,
     -1, 5), Point3f pmax = Point3f(1, 1, 10));
     void generateCameraMatrix(Mat& cameraMatrix);
     void generateDistCoeffs(Mat& distCoeffs, int count);
 
-    double thresh;
+    double thresh = 1.0e-2;
     RNG rng;
 };
 
-CV_UndistortTest::CV_UndistortTest()
-{
-    thresh = 1.0e-2;
-}
-CV_UndistortTest::~CV_UndistortTest() {}
-
-void CV_UndistortTest::generate3DPointCloud(vector<Point3f>& points, Point3f pmin, Point3f pmax)
+void UndistortPointsTest::generate3DPointCloud(vector<Point3f>& points, Point3f pmin, Point3f pmax)
 {
     RNG rng_Point = cv::theRNG(); // fix the seed to use "fixed" input 3D points
     for (size_t i = 0; i < points.size(); i++)
@@ -39,7 +28,8 @@ void CV_UndistortTest::generate3DPointCloud(vector<Point3f>& points, Point3f pmi
         points[i] = Point3f(_x, _y, _z);
     }
 }
-void CV_UndistortTest::generateCameraMatrix(Mat& cameraMatrix)
+
+void UndistortPointsTest::generateCameraMatrix(Mat& cameraMatrix)
 {
     const double fcMinVal = 1e-3;
     const double fcMaxVal = 100;
@@ -51,19 +41,22 @@ void CV_UndistortTest::generateCameraMatrix(Mat& cameraMatrix)
     cameraMatrix.at<double>(1,2) = rng.uniform(fcMinVal, fcMaxVal);
     cameraMatrix.at<double>(2,2) = 1;
 }
-void CV_UndistortTest::generateDistCoeffs(Mat& distCoeffs, int count)
+
+void UndistortPointsTest::generateDistCoeffs(Mat& distCoeffs, int count)
 {
     distCoeffs = Mat::zeros(count, 1, CV_64FC1);
     for (int i = 0; i < count; i++)
         distCoeffs.at<double>(i,0) = rng.uniform(0.0, 1.0e-3);
 }
 
-void CV_UndistortTest::run(int /* start_from */)
+TEST_F(UndistortPointsTest, accuracy)
 {
     Mat intrinsics, distCoeffs;
     generateCameraMatrix(intrinsics);
+
     vector<Point3f> points(500);
     generate3DPointCloud(points);
+
     vector<Point2f> projectedPoints;
     projectedPoints.resize(points.size());
 
@@ -71,10 +64,15 @@ void CV_UndistortTest::run(int /* start_from */)
     for (int idx = 0; idx < 3; idx++)
     {
         generateDistCoeffs(distCoeffs, modelMembersCount[idx]);
-        projectPoints(Mat(points), Mat::zeros(3,1,CV_64FC1), Mat::zeros(3,1,CV_64FC1), intrinsics, distCoeffs, projectedPoints);
+
+        projectPoints(Mat(points), Mat::zeros(3,1,CV_64FC1),
+                      Mat::zeros(3,1,CV_64FC1), intrinsics,
+                      distCoeffs, projectedPoints);
 
         vector<Point2f> realUndistortedPoints;
-        projectPoints(Mat(points), Mat::zeros(3,1,CV_64FC1), Mat::zeros(3,1,CV_64FC1), intrinsics,  Mat::zeros(4,1,CV_64FC1), realUndistortedPoints);
+        projectPoints(Mat(points), Mat::zeros(3,1,CV_64FC1),
+                      Mat::zeros(3,1,CV_64FC1), intrinsics,
+                      Mat::zeros(4,1,CV_64FC1), realUndistortedPoints);
 
         Mat undistortedPoints;
         undistortPoints(Mat(projectedPoints), undistortedPoints, intrinsics, distCoeffs);
@@ -83,43 +81,44 @@ void CV_UndistortTest::run(int /* start_from */)
         perspectiveTransform(undistortedPoints, p, intrinsics);
         undistortedPoints = p;
         double diff = cvtest::norm(Mat(realUndistortedPoints), undistortedPoints, NORM_L2);
-        if (diff > thresh)
-        {
-            ts->set_failed_test_info(cvtest::TS::FAIL_BAD_ACCURACY);
-            return;
-        }
-        ts->set_failed_test_info(cvtest::TS::OK);
+
+        EXPECT_LE(diff, thresh);
     }
 }
 
-TEST(Calib3d_Undistort, accuracy) { CV_UndistortTest test; test.safe_run(); }
-
-TEST(Calib3d_Undistort, stop_criteria)
+TEST_F(UndistortPointsTest, stop_criteria)
 {
     Mat cameraMatrix = (Mat_<double>(3,3,CV_64F) << 857.48296979, 0, 968.06224829,
                                                         0, 876.71824265, 556.37145899,
                                                         0, 0, 1);
     Mat distCoeffs = (Mat_<double>(5,1,CV_64F) <<
                       -2.57614020e-01, 8.77086999e-02, -2.56970803e-04, -5.93390389e-04, -1.52194091e-02);
+
     RNG rng(2);
     Point2d pt_distorted(rng.uniform(0.0, 1920.0), rng.uniform(0.0, 1080.0));
+
     std::vector<Point2d> pt_distorted_vec;
     pt_distorted_vec.push_back(pt_distorted);
+
     const double maxError = 1e-6;
     TermCriteria criteria(TermCriteria::MAX_ITER + TermCriteria::EPS, 100, maxError);
+
     std::vector<Point2d> pt_undist_vec;
     undistortPoints(pt_distorted_vec, pt_undist_vec, cameraMatrix, distCoeffs, noArray(), noArray(), criteria);
 
-    std::vector<Point2d> pt_redistorted_vec;
     std::vector<Point3d> pt_undist_vec_homogeneous;
-    pt_undist_vec_homogeneous.push_back( Point3d(pt_undist_vec[0].x, pt_undist_vec[0].y, 1.0) );
-    projectPoints(pt_undist_vec_homogeneous, Mat::zeros(3,1,CV_64F), Mat::zeros(3,1,CV_64F), cameraMatrix, distCoeffs, pt_redistorted_vec);
+    pt_undist_vec_homogeneous.emplace_back(pt_undist_vec[0].x, pt_undist_vec[0].y, 1.0 );
+
+    std::vector<Point2d> pt_redistorted_vec;
+    projectPoints(pt_undist_vec_homogeneous, Mat::zeros(3,1,CV_64F),
+                  Mat::zeros(3,1,CV_64F), cameraMatrix, distCoeffs, pt_redistorted_vec);
+
     const double obtainedError = sqrt( pow(pt_distorted.x - pt_redistorted_vec[0].x, 2) + pow(pt_distorted.y - pt_redistorted_vec[0].y, 2) );
 
     ASSERT_LE(obtainedError, maxError);
 }
 
-TEST(undistortPoints, regression_14583)
+TEST_F(UndistortPointsTest, regression_14583)
 {
     const int col = 720;
     // const int row = 540;
