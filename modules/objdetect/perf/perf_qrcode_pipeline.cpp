98a95d6c70b98827e287c37eb415d9dba68cea12
AleksandrPanov <alexander.panov@xperience.ai>
2022-06-03 19:12:01 +0300

    add test decode_regression_21929 author Kumataro, add test decode_regression_version_25

--- modules/objdetect/perf/perf_qrcode_pipeline.cpp
+++ modules/objdetect/perf/perf_qrcode_pipeline.cpp
@@ -106,7 +106,7 @@ PERF_TEST_P_(Perf_Objdetect_QRCode_Multi, decodeMulti)
 INSTANTIATE_TEST_CASE_P(/*nothing*/, Perf_Objdetect_QRCode,
     ::testing::Values(
         "version_1_down.jpg", "version_1_left.jpg", "version_1_right.jpg", "version_1_up.jpg", "version_1_top.jpg",
-        "version_5_down.jpg", "version_5_left.jpg",                        "version_5_up.jpg", "version_5_top.jpg",
+        "version_5_down.jpg", "version_5_left.jpg",/*version_5_right.jpg*/ "version_5_up.jpg", "version_5_top.jpg",
         "russian.jpg", "kanji.jpg", "link_github_ocv.jpg", "link_ocv.jpg", "link_wiki_cv.jpg"
     )
 );
