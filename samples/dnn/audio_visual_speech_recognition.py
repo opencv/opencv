295a4440309335d37347240ee9485d9b0af12d7e
Shivanshu Tyagi <shivanshutyagi3@gmail.com>
2022-09-02 03:59:24 +0530

    audio & video simultaneous support

--- samples/dnn/audio_visual_speech_recognition.py
+++ samples/dnn/audio_visual_speech_recognition.py
@@ -16,24 +16,22 @@ class AVSpeechRecognition:
     '''
     Audio Video Speech Recognition based on AVHubert (arXiv:2201.02184 [eess.AS])
     '''
-    def __init__(self, source, type='camera', detector_path='face_detection_yunet_2022mar.onnx', model_path = 'AVSpeechRecog.onnx', margin=20, video_width=640, video_height=480, score_threshold=0.9, nms_threshold=0.3, top_k=5000, show_video=False):
+    def __init__(self, source, detector_path, model_path, margin, video_width, video_height,
+                score_threshold, nms_threshold, top_k, backend, target, show_video=False):
         '''
         params:
-            source: video source (e.g. '0', 'video.mp4')
-            detector_path: face detection model path (default:'face_detection_yunet_2022mar.onnx')
-            margin: margin for temporal window (default:5)
-            video_width: video width (default:640)
-            video_height: video height (default:480)
-            score_threshold: score threshold for face detection (default:0.9)
-            nms_threshold: nms threshold for face detection (default:0.3)
-            top_k: top k faces for face detection (default:5000)
+            source: video source
+            detector_path: face detection model path
+            margin: margin for temporal window
+            video_width: video width
+            video_height: video height
+            score_threshold: score threshold for face detection
+            nms_threshold: nms threshold for face detection
+            top_k: top k faces for face detection
         '''
-        if type not in ['file', 'camera']:
-            raise Exception('type must be file or camera')
-
         self.cap = cv2.VideoCapture(source)
         samplingRate = 16000
-        self.input_type = type
+        fps=30
         self.source=source
         self.params = np.asarray([cv2.CAP_PROP_AUDIO_STREAM, 0,
                 cv2.CAP_PROP_VIDEO_STREAM, 0,
@@ -41,13 +39,21 @@ class AVSpeechRecognition:
                 cv2.CAP_PROP_AUDIO_SAMPLES_PER_SECOND, samplingRate
                 ])
         self.margin = margin
+        self.height = video_height
+        self.width = video_width
         self.cap.set(cv2.CAP_PROP_FRAME_WIDTH, video_width)
         self.cap.set(cv2.CAP_PROP_FRAME_HEIGHT, video_height)
+        self.cap.set(cv2.CAP_PROP_FPS, fps)
+
+        self.model = cv2.dnn.readNetFromONNX(model_path)
         self.detector = cv2.FaceDetectorYN.create(detector_path, "", (video_width, video_height), score_threshold, nms_threshold, top_k)
+        self.model.setPreferableBackend(backend)
+        self.model.setPreferableTarget(target)
+
         self.landmarks_queue = deque(maxlen=margin)
         self.frames_queue = deque(maxlen=margin)
-        self.audio_queue = deque(maxlen=margin)
-        self.model = cv2.dnn.readNetFromONNX(model_path)
+        self.audio_queue = deque(maxlen=margin*samplingRate//fps)
+
         self.show_video = show_video
 
     def warp_image(self, frame, smoothed_landmarks):
@@ -60,10 +66,9 @@ class AVSpeechRecognition:
             warped_frame: warped frame
             warped_landmarks: warped landmarks
         '''
-        # TODO: fix warping
         rotateby = np.arctan((smoothed_landmarks[6][1]-smoothed_landmarks[5][1])/(smoothed_landmarks[6][0]-smoothed_landmarks[5][0]))*180/np.pi
         image_center = tuple((smoothed_landmarks[0]+smoothed_landmarks[1])/2)
-        rot_mat = cv2.getRotationMatrix2D(image_center, rotateby, 1)
+        rot_mat = cv2.getRotationMatrix2D(image_center, rotateby, 2)
         trans_frame = cv2.warpAffine(frame, rot_mat, frame.shape[1::-1], flags=cv2.INTER_LINEAR)
         trans_landmarks = np.hstack((smoothed_landmarks, np.ones(shape=(7,1))))@rot_mat.T
         return trans_frame, trans_landmarks
@@ -103,7 +108,7 @@ class AVSpeechRecognition:
                             int(round(center_x) - round(width)): int(round(center_x) + round(width))])
         return cutted_img
 
-    def preprocess(self, frame):
+    def preprocess(self, frame, audio):
         '''
         preprocesses frame to get landmarks and mouth rois
         params:
@@ -111,8 +116,12 @@ class AVSpeechRecognition:
         return:
             cropped: mouth roi
             smoothed_landmarks: smoothed/averaged landmarks
+            normlized_audio: normalized audio
         '''
         landmarks = self.detector.detect(frame)[-1]
+        cropped = None
+        smoothed_landmarks = None
+        normalized_audio = None
         if landmarks is not None:
             landmarks = landmarks[:,:-1].reshape(landmarks.shape[0],7,2)
             if len(landmarks) == 0:
@@ -123,8 +132,11 @@ class AVSpeechRecognition:
             smoothed_landmarks = np.mean(self.landmarks_queue, axis=0)[0]
             trans_frame, trans_landmarks = self.warp_image(frame, smoothed_landmarks)
             cropped = self.cut_patch(trans_frame, trans_landmarks[-2:], 96//2,96//2)
-            return cropped, smoothed_landmarks
-        return None, None
+        if audio is not None:
+            signal_std = 0. if np.std(audio)==0. else np.std(audio)
+            signal_mean = np.mean(audio)
+            normalized_audio = (audio - signal_mean) / signal_std
+        return cropped, normalized_audio
 
     def predict(self):
         '''
@@ -132,15 +144,10 @@ class AVSpeechRecognition:
         return:
             pred: predicted word
         '''
-        # video_input = cv2.resize(video_input, (96,96))
-        # video_input = video_input.astype(np.float32)
-        # video_input = video_input.transpose(2,0,1)
-        # video_input = video_input.reshape(1,3,96,96)
-        # video_input = video_input/255.0
-        # self.model.setInput(np.array(self.audio_queue), 'audio_input')
-        self.model.setInput(np.random.randn(1,1,12800), 'audio_input')
         video = np.expand_dims(np.expand_dims(np.array(self.frames_queue),0),0)
+        audio = np.expand_dims(np.array(self.audio_queue, dtype=np.float32),0)
         self.model.setInput(video, 'video_input')
+        self.model.setInput(audio, 'audio_input')
         out = self.model.forward()
         pred = out[0].argmax()
         return pred
@@ -149,90 +156,97 @@ class AVSpeechRecognition:
         '''
         Read the video and process it.
         '''
-        while True:
-            # Read frame along with audio and process it
-            # self.cap.open(self.source, cv2.CAP_ANY, self.params)
-            # audioBaseIndex = int(self.cap.get(cv2.CAP_PROP_AUDIO_BASE_INDEX))
-            # cvTickFreq = cv2.getTickFrequency()
-            # sysTimeCurr = cv2.getTickCount()
-            # sysTimePrev = sysTimeCurr
-            # while ((sysTimeCurr - sysTimePrev) / cvTickFreq < 10):
-            #     if (self.cap.grab()):
-            #         frame = np.asarray([])
-            #         # Get the video and audio data
-            #         ret, frame = self.cap.retrieve(frame, audioBaseIndex)
-            #         if not ret:
-            #             break
-            #         # preprocess frame and get landmarks and mouth roi
-            #         cropped, landmarks = self.preprocess(frame)
-            #         if self.show_video:
-            #             cv2.circle(frame, np.mean(self.landmarks_queue, axis=0)[0][5].astype(np.int32), 5, (0,0,255), -1)
-            #             cv2.circle(frame, np.mean(self.landmarks_queue, axis=0)[0][6].astype(np.int32), 5, (0,0,255), -1)
-            #             cv2.imshow('frame', frame)
-            #         if cropped is not None:
-            #             if self.show_video:
-            #                 cv2.imshow('cropped', cropped)
-            #             cropped = cv2.cvtColor(cropped, cv2.COLOR_BGR2GRAY)
-            #             # add to queue
-            #             # self.audio_queue.append(inputAudio)
-            #             # TODO: extract audio here as well
-            #             self.frames_queue.append(cropped)
-            #             self.landmarks_queue.append(landmarks)
-            #             # predict word
-            #             if len(self.frames_queue) == self.margin:
-            #                 pred = self.predict()
-            #                 print(self.labels[pred])
-            #             print(pred)
-            #     else:
-            #         print("Error: Grab error")
-            #         break
-            ret, frame = self.cap.read()
-            if not ret:
-                break
-            cutted_img, _ = self.preprocess(frame)
-            if cutted_img is not None:
-                cv2.imshow('cutted_img', cutted_img)
-                cutted_img = cv2.cvtColor(cutted_img, cv2.COLOR_BGR2GRAY)
-                cutted_img = cv2.resize(cutted_img, (96,96))
-                self.frames_queue.append(cutted_img)
-                if len(self.frames_queue) == self.margin:
+        self.cap.open(self.source, cv2.CAP_MSMF, self.params)
+        if not self.cap.isOpened():
+            print('Cannot open video source')
+            exit(1)
+        audioBaseIndex = int(self.cap.get(cv2.CAP_PROP_AUDIO_BASE_INDEX))
+        audioChannels = int(self.cap.get(cv2.CAP_PROP_AUDIO_TOTAL_CHANNELS))
+
+        while self.cap.isOpened():
+            if self.cap.grab():
+                ret, frame = self.cap.retrieve()
+                frame = cv2.resize(frame, (self.width, self.height))
+                audioFrame = np.asarray([])
+                audioFrame = self.cap.retrieve(audioFrame, audioBaseIndex)
+                audioFrame = audioFrame[1][0] if audioFrame is not None else None
+
+                img, aud = self.preprocess(frame, audioFrame)
+                if img is not None and aud is not None:
+                    img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+                    img = cv2.resize(img, (96,96))
+                    self.frames_queue.append(img)
+                    for i in range(len(aud)):
+                        self.audio_queue.append(aud[i])
+                    if len(self.frames_queue) < self.margin:
+                        continue
                     pred = self.predict()
                     print(pred)
-                cv2.circle(frame, np.mean(self.landmarks_queue, axis=0)[0][5].astype(np.int32), 5, (0,0,255), -1)
-                cv2.circle(frame, np.mean(self.landmarks_queue, axis=0)[0][6].astype(np.int32), 5, (0,0,255), -1)
-            # else:
-            #     self.landmarks_queue.clear()
-            #     self.frames_queue.clear()
-            cv2.imshow('frame', frame)
-
-            if cv2.waitKey(1) & 0xFF == ord('q'):
+                    if self.show_video:
+                        cv2.circle(frame, np.mean(self.landmarks_queue, axis=0)[0][5].astype(np.int32), 2, (0,0,255), -1)
+                        cv2.circle(frame, np.mean(self.landmarks_queue, axis=0)[0][6].astype(np.int32), 2, (0,0,255), -1)
+                        cv2.imshow('frame', frame)
+                        if cv2.waitKey(1) & 0xFF == ord('q'):
+                            break
+                else:
+                    continue
+            else:
                 break
-        self.cap.release()
-        cv2.destroyAllWindows()
-        return 0
+
+def parse_args():
+    '''
+    Parse arguments.
+    '''
+    parser = argparse.ArgumentParser(description='Audio Visual Speech Recognition')
+    # TODO: Test all backends and targets
+    # TODO: Add classes
+    backends = (cv2.dnn.DNN_BACKEND_DEFAULT, cv2.dnn.DNN_BACKEND_INFERENCE_ENGINE, cv2.dnn.DNN_BACKEND_OPENCV)
+    targets = (cv2.dnn.DNN_TARGET_CPU, cv2.dnn.DNN_TARGET_OPENCL, cv2.dnn.DNN_TARGET_OPENCL_FP16)
+
+    parser.add_argument('--input', type=str,
+                        help='Path to input video file. Skip this argument to capture frames from a camera.')
+    parser.add_argument('--model', type=str, default='AVSpeechRecog.onnx',
+                        help='Path to onnx Model file to use for AVSpeech Recognition')
+    parser.add_argument('--detector_model', type=str, default='face_detection_yunet_2022mar.onnx',
+                        help='Path to YUNet Model onnx file to use for face detection')
+    parser.add_argument('--margin', type=int, default=20,
+                        help='Margin for cutting the video')
+    parser.add_argument('--video_width', type=int, default=640,
+                        help='Preprocess frame by resizing to a specific width.')
+    parser.add_argument('--video_height', type=int, default=480,
+                        help='Preprocess frame by resizing to a specific Height.')
+    parser.add_argument('--score_threshold', type=int, default=0.9,
+                        help='score threshold for face detection')
+    parser.add_argument('--nms_threshold', type=float, default=0.3,
+                        help='NMS threshold for face detection')
+    parser.add_argument('--top_k', type=int, default=5000,
+                        help='top k for face detection')
+    parser.add_argument('--show_video', action='store_true',
+                        help='Show video or Not')
+    parser.add_argument('--backend', choices=backends, default=cv2.dnn.DNN_BACKEND_DEFAULT, type=int,
+                        help='Select a computation backend: '
+                        "%d: automatically (by default) "
+                        "%d: OpenVINO Inference Engine "
+                        "%d: OpenCV Implementation " % backends)
+    parser.add_argument('--target', choices=targets, default=cv2.dnn.DNN_TARGET_CPU, type=int,
+                        help='Select a target device: '
+                        "%d: CPU target (by default) "
+                        "%d: OpenCL "
+                        "%d: OpenCL FP16 " % targets)
+
+    args = parser.parse_args()
+    return args
 
 def main():
     '''
     main function
     '''
-    parser = argparse.ArgumentParser(description='Video to text')
-    parser.add_argument('--source', type=str, default='0', help='Source of the video')
-    parser.add_argument('--model', type=str, default='AVSpeechRecog.onnx', help='Model to use')
-    parser.add_argument('--detector_model', type=str, default='face_detection_yunet_2022mar.onnx', help='Model to use')
-    parser.add_argument('--margin', type=int, default=20, help='Margin for cutting the video')
-    parser.add_argument('--video_width', type=int, default=640, help='Video width for cutting the video')
-    parser.add_argument('--video_height', type=int, default=480, help='Video height for cutting the video')
-    parser.add_argument('--score_threshold', type=int, default=0.9, help='score threshold for face detection')
-    parser.add_argument('--nms_threshold', type=float, default=0.3, help='NMS threshold for face detection')
-    parser.add_argument('--top_k', type=int, default=5000, help='top k for face detection')
-    parser.add_argument('--show_video', type=bool, default=False, help='Show video or Not')
-    args = parser.parse_args()
-
-    # source = args.source=='0' and 0 or args.source
-    recognizer = AVSpeechRecognition(0, model_path=args.model, detector_path=args.detector_model,
+    args = parse_args()
+    args.input = args.input if args.input else 0
+    recognizer = AVSpeechRecognition(args.input, model_path=args.model, detector_path=args.detector_model,
                             margin=args.margin, video_width=args.video_width, video_height=args.video_height,
                             score_threshold=args.score_threshold, nms_threshold=args.nms_threshold,
-                            top_k=args.top_k, show_video=args.show_video)
+                            top_k=args.top_k, backend=args.backend, target=args.target, show_video=args.show_video)
     recognizer.run()
 
 if __name__ == '__main__':
