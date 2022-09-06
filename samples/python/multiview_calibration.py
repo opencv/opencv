bd3b2bce560128789abf05481e1039b26d55af6d
Ivashechkin, Maxim (PG/R - Elec Electronic Eng) <m.ivashechkin@surrey.ac.uk>
2022-09-06 21:45:59 +0100

    update visualization

--- samples/python/multiview_calibration.py
+++ samples/python/multiview_calibration.py
@@ -3,14 +3,16 @@ import sys, traceback, cv2 as cv, numpy as np, os, json, argparse, matplotlib.py
 def getDimBox(pts):
     return np.array([[pts[...,k].min(), pts[...,k].max()] for k in range(pts.shape[-1])])
 
-def plotCamerasPosition(R, t, image_sizes):
-    cam_box = np.array([[1, 1, 0], [1, -1, 0], [-1, -1, 0], [-1, 1, 0]],dtype=np.float32); cam_box[:,2] = 3.0
-    cam_box *= 0.15
+def plotCamerasPosition(R, t, image_sizes, pairs, pattern):
+    cam_box = np.array([[1, 1, 0], [1, -1, 0], [-1, -1, 0], [-1, 1, 0]],dtype=np.float32)
+    cam_box[:,2] = 3.0
+    dist_to_pattern = np.linalg.norm(pattern.mean(0))
+    cam_box *= 0.1*dist_to_pattern
     fig = plt.figure()
     ax = fig.add_subplot(111, projection='3d')
     ax_lines = [None for i in range(len(R))]
     ax.set_title('Cameras position', fontsize=20)
-    all_pts = []
+    all_pts = [pattern]
     colors = np.random.RandomState(0).rand(len(R),3)
     for i, cam in enumerate(R):
         cam_box_i = cam_box.copy()
@@ -29,40 +31,57 @@ def plotCamerasPosition(R, t, image_sizes):
         ax.plot([cam_box_Rt[2,0], cam_box_Rt[3,0]], [cam_box_Rt[2,1], cam_box_Rt[3,1]], [cam_box_Rt[2,2], cam_box_Rt[3,2]], '-', color=colors[i])
         ax.plot([cam_box_Rt[3,0], cam_box_Rt[0,0]], [cam_box_Rt[3,1], cam_box_Rt[0,1]], [cam_box_Rt[3,2], cam_box_Rt[0,2]], '-', color=colors[i])
 
-    ax.legend(ax_lines, [str(i) for i in range(len(R))], fontsize=10)
+    for (i,j) in pairs:
+        edge_line = plt.plot([t[i][0,0], t[j][0,0]], [t[i][1,0], t[j][1,0]], [t[i][2,0], t[j][2,0]], '-', color='black')
+    ax.scatter(pattern[:,0], pattern[:,1], pattern[:,2], color='red', marker='o')
+    ax.legend(ax_lines+[edge_line], [str(i) for i in range(len(R))]+['stereo pair'], fontsize=10)
     dim_box = getDimBox(np.concatenate((all_pts)))
     ax.set_xlim(dim_box[0]); ax.set_ylim(dim_box[1]); ax.set_zlim(dim_box[2])
     ax.set_box_aspect((dim_box[0, 1] - dim_box[0, 0], dim_box[1, 1] - dim_box[1, 0], dim_box[2, 1] - dim_box[2, 0]))
     ax.set_xlabel('x', fontsize=20); ax.set_ylabel('y', fontsize=20); ax.set_zlabel('z', fontsize=20)
     ax.view_init(azim=90, elev=-40)
 
-def plotProjection(points_2d, pattern_points, rvec0, tvec0, rvec1, tvec1, K, dist_coeff, is_fisheye, image_size, image=None):
+def plotProjection(points_2d, pattern_points, rvec0, tvec0, rvec1, tvec1, K, dist_coeff, is_fisheye, cam_idx, frame_idx, per_acc, visual_thr=1.5, image=None):
     rvec2, tvec2 = cv.composeRT(rvec0, tvec0, rvec1, tvec1)[:2]
     if is_fisheye:
         points_2d_est = cv.fisheye.projectPoints(pattern_points, rvec2, tvec2, K, dist_coeff.flatten())[0]
     else:
         points_2d_est = cv.projectPoints(pattern_points, rvec2, tvec2, K, dist_coeff)[0].reshape(-1,2)
-
     fig = plt.figure()
+    errs = np.linalg.norm(points_2d - points_2d_est, axis=-1)
+    mean_err = errs.mean()
+    title = "Comparison of given point (start) and back-projected (end). Cam. "+str(cam_idx)+" frame "+\
+            str(frame_idx)+" mean err. (px) %.1f"%mean_err+". In top %.0f"%per_acc+"% accurate frames"
     if image is None:
         ax = fig.add_subplot(111)
-        ax.scatter(points_2d[:,0], points_2d[:,1], color='red', marker='.')
-        ax.scatter(points_2d_est[:,0], points_2d_est[:,1], color='green', marker='+')
-        for pt1, pt2 in zip(points_2d, points_2d_est):
-            ax.plot([pt1[0], pt2[0]], [pt1[1], pt2[1]], color='blue')
-        dim_box = getDimBox(points_2d)
         ax.set_aspect('equal', 'box')
-        # ax.set_xlim(0, image_size[0]); ax.set_ylim(0, image_size[1])
-        ax.set_xlabel('x', fontsize=20); ax.set_ylabel('y', fontsize=20)
-        ax.legend(['given', 'back-projected'], fontsize=15)
-        ax.set_title('Comparison of given points and back-projected', fontsize=17)
+        ax.set_xlabel('x', fontsize=20)
+        ax.set_ylabel('y', fontsize=20)
+        width = .25
+        head_width = 1.25
     else:
-        circle_sz = 5
-        for pt1, pt2, in zip(points_2d, points_2d_est):
-            cv.circle(image, np.array(pt1, dtype=int), circle_sz, (0, 0, 255), -1)
-            cv.circle(image, np.array(pt2, dtype=int), circle_sz, (0, 255, 0), -1)
-            cv.line(image, np.array(pt1, dtype=int), np.array(pt2, dtype=int), (255, 0, 0), 1)
         plt.imshow(image)
+        ax = plt.gca()
+        width = 1
+        head_width = 4
+    arrow_good = arrow_bad = None
+    for k, (pt1, pt2) in enumerate(zip(points_2d, points_2d_est)):
+        color = 'red' if errs[k] > visual_thr else 'green'
+        arrow = ax.arrow(pt1[0], pt1[1], pt2[0]-pt1[0], pt2[1]-pt1[1], color=color, width=width, head_width=head_width)
+        if errs[k] > visual_thr and arrow_bad is None:
+            arrow_bad = arrow
+        elif arrow_good is None:
+            arrow_good = arrow
+    legend = []
+    legend_str = []
+    if arrow_good is not None:
+        legend.append(arrow_good)
+        legend_str.append('less than '+str(visual_thr)+' px')
+    if arrow_bad is not None:
+        legend.append(arrow_bad)
+        legend_str.append('greater than '+str(visual_thr)+' px')
+    ax.legend(legend, legend_str, fontsize=15)
+    ax.set_title(title, loc='center', wrap=True, fontsize=16)
 
 def calibrateFromPoints(pattern_points, image_points, image_sizes, is_fisheye, image_names=None):
     """
@@ -75,19 +94,17 @@ def calibrateFromPoints(pattern_points, image_points, image_sizes, is_fisheye, i
     num_cameras = len(image_points)
     num_frames = len(image_points[0])
     visibility = np.zeros((num_cameras, num_frames), dtype=int)
-    pattern_points_all = [pattern_points for i in range(num_frames)]
-    # pattern_points_all = [pattern_points] * num_frames
+    pattern_points_all = [pattern_points] * num_frames
     for i in range(num_cameras):
         for j in range(num_frames):
             visibility[i,j] = int(len(image_points[i][j]) != 0)
-
     success, rvecs, Ts, Ks, distortions, rvecs0, tvecs0, errors_per_frame, output_pairs = \
         cv.calibrateMultiview(objPoints=pattern_points_all,
                               imagePoints=image_points,
                               imageSize=image_sizes,
                               visibility=visibility,
-                              #Ks=Ks,
-                              #distortions=distortions,
+                              Ks=None,
+                              distortions=None,
                               is_fisheye=np.array(is_fisheye, dtype=int),
                               USE_INTRINSICS_GUESS=False,
                               flags_intrinsics=0)
@@ -96,15 +113,19 @@ def calibrateFromPoints(pattern_points, image_points, image_sizes, is_fisheye, i
     print('tvecs', Ts)
     print('K', Ks)
     print('distortion', distortions)
-    print('mean RMS error over all visible frames %.3E' % errors_per_frame[errors_per_frame > 0].mean())
+    errors = errors_per_frame[errors_per_frame > 0]
+    print('mean RMS error over all visible frames %.3E' % errors.mean())
 
     visibility_idxs = np.stack(np.where(visibility)) # 2 x M, first row is camera idx, second is frame idx
-    plotCamerasPosition(Rs, Ts, image_sizes)
+    frame_idx = visibility_idxs[1,0]
+    R_frame = cv.Rodrigues(rvecs0[frame_idx])[0]
+    pattern_frame = (R_frame @ pattern_points.T + tvecs0[frame_idx]).T
+    plotCamerasPosition(Rs, Ts, image_sizes, output_pairs, pattern_frame)
     def plot(cam_idx, frame_idx):
         image = None if image_names is None else cv.cvtColor(cv.imread(image_names[cam_idx][frame_idx]), cv.COLOR_BGR2RGB)
         plotProjection(image_points[cam_idx][frame_idx], pattern_points, rvecs0[frame_idx],
             tvecs0[frame_idx], rvecs[cam_idx], Ts[cam_idx], Ks[cam_idx], distortions[cam_idx],
-            is_fisheye[cam_idx], image_sizes[cam_idx], image)
+            is_fisheye[cam_idx], cam_idx, frame_idx, 1e2*(errors_per_frame[cam_idx,frame_idx]<errors).sum()/len(errors), 1.5, image)
     plot(visibility_idxs[0,0], visibility_idxs[1,0])
     plt.show()
 
@@ -117,14 +138,14 @@ def circles_grid_points(grid_size, dist_m):
     pattern = []
     for i in range(grid_size[0]):
         for j in range(grid_size[1]):
-            pattern.append([j*dist_m, i*dist_m, 0]);
+            pattern.append([j*dist_m, i*dist_m, 0])
     return np.array(pattern, dtype=np.float32)
 
 def asym_circles_grid_points(grid_size, dist_m):
     pattern = []
     for i in range(grid_size[0]):
         for j in range(grid_size[1]):
-            pattern.append([(2*j + i % 2)*dist_m, i*dist_m, 0]);
+            pattern.append([(2*j + i % 2)*dist_m, i*dist_m, 0])
     return np.array(pattern, dtype=np.float32)
 
 def calibrateFromImages(files_with_images, grid_size, pattern_type, is_fisheye, dist_m, RESIZE_IMAGE=True):
@@ -157,10 +178,6 @@ def calibrateFromImages(files_with_images, grid_size, pattern_type, is_fisheye, 
         img_size = None
         for img_name in images_names:
             print(img_name)
-            if img_name == '': # above we replace new line '\n' with ''
-                image_points_camera.append(np.array([], dtype=np.float32))
-                continue
-
             assert os.path.exists(img_name)
             img = cv.cvtColor(cv.imread(img_name), cv.COLOR_BGR2GRAY)
             if img_size is None:
@@ -220,5 +237,5 @@ if __name__ == '__main__':
         if (params.fisheye is None and params.filenames is None and params.pattern_type is None and \
                 params.pattern_size is None and params.pattern_distance is None):
             assert False and 'Either json file or all other parameters must be set'
-        calibrateFromImages(params.filenames.split(','), [int(v) for v in params.pattern_size.split(',')], 
+        calibrateFromImages(params.filenames.split(','), [int(v) for v in params.pattern_size.split(',')],
             params.pattern_type, [bool(int(v)) for v in params.fisheye.split(',')], params.pattern_distance)
