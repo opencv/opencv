4c74e6d89d35dc45a3889d96158989352f73b1db
Alexey Smirnov <alexey.smirnov@intel.com>
2022-09-23 12:08:29 +0100

    Copy mpashchenkov's changes
    Minor refactoring
    
    Partially address review comments
    
    Move DX-related stuff from the sample to a default source
    
    Simplify the default OneVPL config
    
    Address minor review comments
    
    Add class for the default VPL source
    
    WIP: Add initial stub for tests with description
    
    Removing default vpl source and minor refactoring
    
    Refactor default files
    
    Fix build and application crash
    
    Address review comments
    
    Add test on VPL + OCL interaction compared to CPU behavior
    
    Fix test

--- modules/gapi/src/streaming/onevpl/accelerators/surface/dx11_frame_adapter.cpp
+++ modules/gapi/src/streaming/onevpl/accelerators/surface/dx11_frame_adapter.cpp
@@ -114,17 +114,24 @@ MediaFrame::View VPLMediaFrameDX11Adapter::access(MediaFrame::Access mode) {
     }
 }
 
-cv::util::any VPLMediaFrameDX11Adapter::blobParams() const {
-    /*GAPI_Assert(false && "VPLMediaFrameDX11Adapter::blobParams() is not fully integrated"
-                         "in OpenVINO InferenceEngine and would be temporary disable.");*/
-#ifdef HAVE_INF_ENGINE
+mfxHDLPair VPLMediaFrameDX11Adapter::getHandle() const {
     auto surface_ptr_copy = get_surface();
-    Surface::data_t& data = surface_ptr_copy->get_data();
-    const Surface::info_t& info = surface_ptr_copy->get_info();
+    const Surface::data_t& data = surface_ptr_copy->get_data();
     NativeHandleAdapter* native_handle_getter = reinterpret_cast<NativeHandleAdapter*>(data.MemId);
 
     mfxHDLPair handle{};
     native_handle_getter->get_handle(data.MemId, reinterpret_cast<mfxHDL&>(handle));
+    return handle;
+}
+
+cv::util::any VPLMediaFrameDX11Adapter::blobParams() const {
+    /*GAPI_Assert(false && "VPLMediaFrameDX11Adapter::blobParams() is not fully integrated"
+                         "in OpenVINO InferenceEngine and would be temporary disable.");*/
+#ifdef HAVE_INF_ENGINE
+    mfxHDLPair handle = getHandle();
+
+    auto surface_ptr_copy = get_surface();
+    const Surface::info_t& info = surface_ptr_copy->get_info();
 
     GAPI_Assert(frame_desc.fmt == MediaFormat::NV12 &&
                 "blobParams() for VPLMediaFrameDX11Adapter supports NV12 only");
