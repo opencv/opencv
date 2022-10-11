1db8244b4c2e1aa556dac9bc65b4af54923f965b
Rostislav Vasilikhin <savuor@gmail.com>
2022-10-11 03:58:23 +0200

    pyrType enum docs

--- modules/3d/include/opencv2/3d/odometry_frame.hpp
+++ modules/3d/include/opencv2/3d/odometry_frame.hpp
@@ -9,29 +9,20 @@
 
 namespace cv
 {
-/** Indicates what pyramid is to access using get/setPyramid... methods:
-* @param PYR_IMAGE The pyramid of RGB images
-* @param PYR_DEPTH The pyramid of depth images
-* @param PYR_MASK  The pyramid of masks
-* @param PYR_CLOUD The pyramid of point clouds, produced from the pyramid of depths
-* @param PYR_DIX   The pyramid of dI/dx derivative images
-* @param PYR_DIY   The pyramid of dI/dy derivative images
-* @param PYR_TEXMASK The pyramid of textured masks
-* @param PYR_NORM  The pyramid of normals
-* @param PYR_NORMMASK The pyramid of normals masks
+/** Indicates what pyramid is to access using getPyramidAt() method:
 */
 
 enum OdometryFramePyramidType
 {
-    PYR_IMAGE = 0,
-    PYR_DEPTH = 1,
-    PYR_MASK = 2,
-    PYR_CLOUD = 3,
-    PYR_DIX = 4,
-    PYR_DIY = 5,
-    PYR_TEXMASK = 6,
-    PYR_NORM = 7,
-    PYR_NORMMASK = 8,
+    PYR_IMAGE = 0,    //!< The pyramid of grayscale images
+    PYR_DEPTH = 1,    //!< The pyramid of depth images
+    PYR_MASK = 2,     //!< The pyramid of masks
+    PYR_CLOUD = 3,    //!< The pyramid of point clouds, produced from the pyramid of depths
+    PYR_DIX = 4,      //!< The pyramid of dI/dx derivative images
+    PYR_DIY = 5,      //!< The pyramid of dI/dy derivative images
+    PYR_TEXMASK = 6,  //!< The pyramid of "textured" masks (i.e. additional masks for normals or grayscale images)
+    PYR_NORM = 7,     //!< The pyramid of normals
+    PYR_NORMMASK = 8, //!< The pyramid of normals masks
     N_PYRAMIDS
 };
 
