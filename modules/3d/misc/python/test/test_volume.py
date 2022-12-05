763b4911d02f4f1ca29fd8d513b5b5528f53589d
Rostislav Vasilikhin <savuor@gmail.com>
2022-12-05 12:57:37 +0100

    py tests added

---
+++ modules/3d/misc/python/test/test_volume.py
@@ -0,0 +1,84 @@
+import numpy as np
+import cv2 as cv
+
+from tests_common import NewOpenCVTests
+
+class volume_test(NewOpenCVTests):
+    def test_VolumeDefault(self):
+        depth = self.get_sample('cv/rgbd/depth.png', cv.IMREAD_ANYDEPTH).astype(np.float32)
+
+        Rt = np.array(
+            [[1, 0, 0, 0],
+            [0, 1, 0, 0],
+            [0, 0, 1, 0],
+            [0, 0, 0, 1]])
+        volume = cv.Volume()
+        volume.integrate(depth, Rt)
+
+        size = (480, 640, 4)
+        points  = np.zeros(size, np.float32)
+        normals = np.zeros(size, np.float32)
+
+        volume.raycast(Rt, size[0], size[1], points, normals)
+        volume.raycast(Rt, points, normals)
+
+    def test_VolumeTSDF(self):
+        depth = self.get_sample('cv/rgbd/depth.png', cv.IMREAD_ANYDEPTH).astype(np.float32)
+
+        Rt = np.array(
+            [[1, 0, 0, 0],
+            [0, 1, 0, 0],
+            [0, 0, 1, 0],
+            [0, 0, 0, 1]])
+        volume = cv.Volume(cv.VolumeType_TSDF)
+        volume.integrate(depth, Rt)
+
+        size = (480, 640, 4)
+        points  = np.zeros(size, np.float32)
+        normals = np.zeros(size, np.float32)
+
+        volume.raycast(Rt, size[0], size[1], points, normals)
+        volume.raycast(Rt, points, normals)
+
+    def test_VolumeHashTSDF(self):
+        depth = self.get_sample('cv/rgbd/depth.png', cv.IMREAD_ANYDEPTH).astype(np.float32)
+
+        Rt = np.array(
+            [[1, 0, 0, 0],
+            [0, 1, 0, 0],
+            [0, 0, 1, 0],
+            [0, 0, 0, 1]])
+        volume = cv.Volume(cv.VolumeType_HashTSDF)
+        volume.integrate(depth, Rt)
+
+        size = (480, 640, 4)
+        points  = np.zeros(size, np.float32)
+        normals = np.zeros(size, np.float32)
+
+        volume.raycast(Rt, size[0], size[1], points, normals)
+        volume.raycast(Rt, points, normals)
+
+    def test_VolumeColorTSDF(self):
+        depth = self.get_sample('cv/rgbd/depth.png', cv.IMREAD_ANYDEPTH).astype(np.float32)
+        rgb = self.get_sample('cv/rgbd/rgb.png', cv.IMREAD_ANYCOLOR).astype(np.float32)
+
+        Rt = np.array(
+            [[1, 0, 0, 0],
+            [0, 1, 0, 0],
+            [0, 0, 1, 0],
+            [0, 0, 0, 1]])
+        volume = cv.Volume(cv.VolumeType_ColorTSDF)
+        volume.integrateColor(depth, rgb, Rt)
+
+        size = (480, 640, 4)
+        points  = np.zeros(size, np.float32)
+        normals = np.zeros(size, np.float32)
+        colors = np.zeros(size, np.float32)
+
+        volume.raycastColor(Rt, size[0], size[1], points, normals, colors)
+        volume.raycastColor(Rt, points, normals, colors)
+
+
+if __name__ == '__main__':
+    NewOpenCVTests.bootstrap()
+
---
+++ samples/python/volume.py
@@ -0,0 +1,135 @@
+import numpy as np
+import cv2 as cv
+import argparse
+
+# for more info about this function look cv::Quat::toRotMat3x3(...)
+def calcR(a, b, c, d):
+    return np.array([
+        [1 - 2 * (c * c + d * d), 2 * (b * c - a * d)    , 2 * (b * d + a * c)],
+        [2 * (b * c + a * d)    , 1 - 2 * (b * b + d * d), 2 * (c * d - a * b)],
+        [2 * (b * d - a * c)    , 2 * (c * d + a * b)    , 1 - 2 * (b * b + c * c)]
+    ])
+
+def make_Rt(val):
+    R = calcR(val[6], val[3], val[4] ,val[5])
+    t = np.array([ [val[0]], [val[1]], [val[2]] ])
+    tmp = np.array([0, 0, 0, 1])
+
+    Rt = np.append(R, t , axis=1 )
+    Rt = np.vstack([Rt, tmp])
+
+    return Rt
+
+def get_image_info(path, is_depth):
+    image_info = {}
+    source = 'depth.txt'
+    if not is_depth:
+        source = 'rgb.txt'
+    with open(path+source) as file:
+        lines = file.readlines()
+        for line in lines:
+            words = line.split(' ')
+            if words[0] == '#':
+                continue
+            image_info[float(words[0])] = words[1][:-1]
+    return image_info
+
+def get_groundtruth_info(path):
+    groundtruth_info = {}
+    with open(path+'groundtruth.txt') as file:
+        lines = file.readlines()
+        for line in lines:
+            words = line.split(' ')
+            if words[0] == '#':
+                continue
+            groundtruth_info[float(words[0])] = [float(i) for i in words[1:]]
+    return groundtruth_info
+
+def main():
+
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        '--algo',
+        help="""TSDF      - reconstruct data in volume with bounds,
+                HashTSDF  - reconstruct data in volume without bounds (infinite volume),
+                ColorTSDF - like TSDF but also keeps color data,
+                default - runs TSDF""",
+        default="")
+    parser.add_argument(
+        '-src',
+        '--source_folder',
+        default="")
+    # Use source data from this site:
+    # https://vision.in.tum.de/data/datasets/rgbd-dataset/download
+    #TODO: specify exact data path
+
+    args = parser.parse_args()
+
+    path = args.source_folder
+    if path[-1] != '/':
+        path += '/'
+
+    depth_info = get_image_info(path, True)
+    rgb_info = get_image_info(path, False)
+    groundtruth_info = get_groundtruth_info(path)
+
+    volume_type = cv.VolumeType_TSDF
+    if args.algo == "HashTSDF":
+        volume_type = cv.VolumeType_HashTSDF
+    elif args.algo == "ColorTSDF":
+        volume_type = cv.VolumeType_ColorTSDF
+
+    volume = cv.Volume(volume_type)
+
+    for key in list(depth_info.keys())[:]:
+        Rt = np.array(
+            [[1, 0, 0, 0],
+            [0, 1, 0, 0],
+            [0, 0, 1, 0],
+            [0, 0, 0, 1]]
+        )
+        for key1 in groundtruth_info:
+            if np.abs(key1 - key) < 0.01:
+                Rt = make_Rt(groundtruth_info[key1])
+                break
+
+        rgb_path = ''
+        for key1 in rgb_info:
+            if np.abs(key1 - key) < 0.05:
+                rgb_path = path + rgb_info[key1]
+                break
+
+        depth = cv.imread(path + depth_info[key], cv.IMREAD_ANYDEPTH).astype(np.float32)
+        rgb = cv.imread(rgb_path, cv.IMREAD_COLOR).astype(np.float32)
+
+        if volume_type != cv.VolumeType_ColorTSDF:
+            volume.integrate(depth, Rt)
+        else:
+            volume.integrateColor(depth, rgb, Rt)
+
+        size = (480, 640, 4)
+
+        points  = np.zeros(size, np.float32)
+        normals = np.zeros(size, np.float32)
+        colors = np.zeros(size, np.float32)
+
+        if volume_type != cv.VolumeType_ColorTSDF:
+            volume.raycast(Rt, points, normals)
+        else:
+            volume.raycastColor(Rt, size[0], size[1], points, normals, colors)
+
+        channels = list(cv.split(points))
+
+        cv.imshow("X", np.absolute(channels[0]))
+        cv.imshow("Y", np.absolute(channels[1]))
+        cv.imshow("Z", channels[2])
+
+        if volume_type == cv.VolumeType_ColorTSDF:
+            cv.imshow("Color", colors.astype(np.uint8))
+
+        #TODO: also display normals
+
+        cv.waitKey(10)
+
+if __name__ == '__main__':
+    main()
