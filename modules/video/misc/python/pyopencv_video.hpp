698e88afef00fb2b1cea3fe161789cc769db352e
Zihao Mu <zihaomu@outlook.com>
2022-12-06 13:54:32 +0800

    Merge pull request #22808 from zihaomu:nanotrack
    [teset data in opencv_extra](https://github.com/opencv/opencv_extra/pull/1016)
    
    NanoTrack is an extremely lightweight and fast object-tracking model. 
    The total size is **1.1 MB**.
    And the FPS on M1 chip is **150**, on Raspberry Pi 4 is about **30**. (Float32 CPU only)
    
    With this model, many users can run object tracking on the edge device.
    
    The author of NanoTrack is @HonglinChu.
    The original repo is https://github.com/HonglinChu/NanoTrack.
    
    ### Pull Request Readiness Checklist
    
    See details at https://github.com/opencv/opencv/wiki/How_to_contribute#making-a-good-pull-request
    
    - [x] I agree to contribute to the project under Apache 2 License.
    - [x] To the best of my knowledge, the proposed patch is not based on a code under GPL or another license that is incompatible with OpenCV
    - [x] The PR is proposed to the proper branch
    - [ ] There is a reference to the original bug report and related work
    - [ ] There is accuracy test, performance test and test data in opencv_extra repository, if applicable
          Patch to opencv_extra has the same branch name.
    - [ ] The feature is well documented and sample code can be built with the project CMake

--- modules/video/misc/python/pyopencv_video.hpp
+++ modules/video/misc/python/pyopencv_video.hpp
@@ -2,4 +2,5 @@
 typedef TrackerMIL::Params TrackerMIL_Params;
 typedef TrackerGOTURN::Params TrackerGOTURN_Params;
 typedef TrackerDaSiamRPN::Params TrackerDaSiamRPN_Params;
+typedef TrackerNano::Params TrackerNano_Params;
 #endif
