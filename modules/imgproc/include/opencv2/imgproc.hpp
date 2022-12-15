b3c07c2bd1f6ad6ebe1223e0155e6e638c298e24
nalibl <41193985+nalibl@users.noreply.github.com>
2022-12-15 21:00:58 +0200

    Image arguments wrong
    Image discrete function arguments should be x,x',y,y' since the right hand side has M(x',y') and I(x+x',y+y').

--- modules/imgproc/include/opencv2/imgproc.hpp
+++ modules/imgproc/include/opencv2/imgproc.hpp
@@ -3822,20 +3822,20 @@ enum TemplateMatchModes {
                                   M(x',y')^2)}{\sqrt{\sum_{x',y'} \left( T(x',y') \cdot M(x',y')
                                   \right)^2 \cdot \sum_{x',y'} \left( I(x+x',y+y') \cdot M(x',y')
                                   \right)^2}}\f] */
-    TM_CCOEFF        = 4, /*!< \f[R(x,y)= \sum _{x',y'} (T'(x',y') \cdot I'(x+x',y+y'))\f]
+    TM_CCOEFF        = 4, /*!< \f[R(x,y)= \sum _{x',y'} (T'(x',y') \cdot I'(x,x',y,y'))\f]
                                where
                                \f[\begin{array}{l} T'(x',y')=T(x',y') - 1/(w \cdot h) \cdot \sum _{
-                                  x'',y''} T(x'',y'') \\ I'(x+x',y+y')=I(x+x',y+y') - 1/(w \cdot h)
+                                  x'',y''} T(x'',y'') \\ I'(x,x',y,y')=I(x+x',y+y') - 1/(w \cdot h)
                                   \cdot \sum _{x'',y''} I(x+x'',y+y'') \end{array}\f]
                                with mask:
                                \f[\begin{array}{l} T'(x',y')=M(x',y') \cdot \left( T(x',y') -
                                   \frac{1}{\sum _{x'',y''} M(x'',y'')} \cdot \sum _{x'',y''}
-                                  (T(x'',y'') \cdot M(x'',y'')) \right) \\ I'(x+x',y+y')=M(x',y')
+                                  (T(x'',y'') \cdot M(x'',y'')) \right) \\ I'(x,x',y,y')=M(x',y')
                                   \cdot \left( I(x+x',y+y') - \frac{1}{\sum _{x'',y''} M(x'',y'')}
                                   \cdot \sum _{x'',y''} (I(x+x'',y+y'') \cdot M(x'',y'')) \right)
                                   \end{array} \f] */
-    TM_CCOEFF_NORMED = 5  /*!< \f[R(x,y)= \frac{ \sum_{x',y'} (T'(x',y') \cdot I'(x+x',y+y')) }{
-                                  \sqrt{\sum_{x',y'}T'(x',y')^2 \cdot \sum_{x',y'} I'(x+x',y+y')^2}
+    TM_CCOEFF_NORMED = 5  /*!< \f[R(x,y)= \frac{ \sum_{x',y'} (T'(x',y') \cdot I'(x,x',y,y')) }{
+                                  \sqrt{\sum_{x',y'}T'(x',y')^2 \cdot \sum_{x',y'} I'(x,x',y,y')^2}
                                   }\f] */
 };
 
