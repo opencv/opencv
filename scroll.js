b78eca0f2e8020132fca74c2a93fc09b8fcb4cac
Jean Carlo <117331295+JeanGoumez@users.noreply.github.com>
2022-12-28 13:42:48 -0500

    Create scroll.js

---
+++ scroll.js
@@ -0,0 +1,19 @@
+const callback = function (entries) {
+  entries.forEach((entry) => {
+    console.log(entry);
+
+    if (entry.isIntersecting) {
+      entry.target.classList.add("animate-fadeIn");
+    } else {
+      entry.target.classList.remove("animate-fadeIn");
+    }
+  });
+};
+
+const observer = new IntersectionObserver(callback);
+
+const targets = document.querySelectorAll(".js-show-on-scroll");
+targets.forEach(function (target) {
+  target.classList.add("opacity-0");
+  observer.observe(target);
+});
