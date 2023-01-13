a44944a116004cbbd3c03af1337ce6c277812566
Rostislav Vasilikhin <savuor@gmail.com>
2023-01-13 02:23:00 +0100

    boilerplate for future SIMD code

--- modules/core/src/mathfuncs.cpp
+++ modules/core/src/mathfuncs.cpp
@@ -1685,12 +1685,260 @@ void patchNaNs( InputOutputArray _a, double _val )
     }
 }
 
+#if CV_SIMD
+template <typename _Tp, int cn>
+int nanMaskSIMD_(const _Tp *src, uchar *dst, size_t total, bool maskNans, bool maskInfs, bool maskAll, bool invert);
 
-//TODO: simd
+//TODO: make true SIMD code instead
+template <>
+int nanMaskSIMD_<float, 1>(const float *src, uchar *dst, size_t total, bool maskNans, bool maskInfs, bool maskAll, bool invert)
+{
+    const int vsize = v_float32::nlanes;
+    int i = 0;
+    for(; i <= (int)total - vsize; i += vsize )
+    {
+        for (int j = 0; j < vsize; j++)
+        {
+            bool nan = maskAll ? true : false;
+
+            float val = src[i + j];
+            bool v = (maskNans && cvIsNaN(val)) || (maskInfs && cvIsInf(val));
+            if (maskAll)
+                nan = nan && v;
+            else
+                nan = nan || v;
+
+            nan = invert ? !nan : nan;
+            dst[i + j] = nan ? 255 : 0;
+        }
+    }
+
+    return i;
+}
+
+//TODO: make true SIMD code instead
+template <>
+int nanMaskSIMD_<double, 1>(const double *src, uchar *dst, size_t total, bool maskNans, bool maskInfs, bool maskAll, bool invert)
+{
+    const int vsize = v_float64::nlanes;
+
+    int i = 0;
+    for(; i <= (int)total - vsize; i += vsize )
+    {
+        for (int j = 0; j < vsize; j++)
+        {
+            bool nan = maskAll ? true : false;
+
+            double val = src[i + j];
+            bool v = (maskNans && cvIsNaN(val)) || (maskInfs && cvIsInf(val));
+            if (maskAll)
+                nan = nan && v;
+            else
+                nan = nan || v;
+
+            nan = invert ? !nan : nan;
+            dst[i + j] = nan ? 255 : 0;
+        }
+    }
+
+    return i;
+}
+
+//TODO: make true SIMD code instead
+template <>
+int nanMaskSIMD_<float, 2>(const float *src, uchar *dst, size_t total, bool maskNans, bool maskInfs, bool maskAll, bool invert)
+{
+    const int cn = 2;
+
+    const int vsize = v_float32::nlanes;
+    int i = 0;
+    for(; i <= (int)total - vsize; i += vsize )
+    {
+        for (int j = 0; j < vsize; j++)
+        {
+            bool nan = maskAll ? true : false;
+            for (int c = 0; c < cn; c++)
+            {
+                float val = src[i * cn + j * cn + c];
+                bool v = (maskNans && cvIsNaN(val)) || (maskInfs && cvIsInf(val));
+                if (maskAll)
+                    nan = nan && v;
+                else
+                    nan = nan || v;
+            }
+            nan = invert ? !nan : nan;
+            dst[i + j] = nan ? 255 : 0;
+        }
+    }
+
+    return i;
+}
+
+//TODO: make true SIMD code instead
+template <>
+int nanMaskSIMD_<double, 2>(const double *src, uchar *dst, size_t total, bool maskNans, bool maskInfs, bool maskAll, bool invert)
+{
+    const int cn = 2;
+
+    const int vsize = v_float64::nlanes;
+
+    int i = 0;
+    for(; i <= (int)total - vsize; i += vsize )
+    {
+        for (int j = 0; j < vsize; j++)
+        {
+            bool nan = maskAll ? true : false;
+            for (int c = 0; c < cn; c++)
+            {
+                double val = src[i * cn + j * cn + c];
+                bool v = (maskNans && cvIsNaN(val)) || (maskInfs && cvIsInf(val));
+                if (maskAll)
+                    nan = nan && v;
+                else
+                    nan = nan || v;
+            }
+            nan = invert ? !nan : nan;
+            dst[i + j] = nan ? 255 : 0;
+        }
+    }
+
+    return i;
+}
+
+//TODO: make true SIMD code instead
+template <>
+int nanMaskSIMD_<float, 3>(const float *src, uchar *dst, size_t total, bool maskNans, bool maskInfs, bool maskAll, bool invert)
+{
+    const int cn = 3;
+
+    const int vsize = v_float32::nlanes;
+    int i = 0;
+    for(; i <= (int)total - vsize; i += vsize )
+    {
+        for (int j = 0; j < vsize; j++)
+        {
+            bool nan = maskAll ? true : false;
+            for (int c = 0; c < cn; c++)
+            {
+                float val = src[i * cn + j * cn + c];
+                bool v = (maskNans && cvIsNaN(val)) || (maskInfs && cvIsInf(val));
+                if (maskAll)
+                    nan = nan && v;
+                else
+                    nan = nan || v;
+            }
+            nan = invert ? !nan : nan;
+            dst[i + j] = nan ? 255 : 0;
+        }
+    }
+
+    return i;
+}
+
+//TODO: make true SIMD code instead
+template <>
+int nanMaskSIMD_<double, 3>(const double *src, uchar *dst, size_t total, bool maskNans, bool maskInfs, bool maskAll, bool invert)
+{
+    const int cn = 3;
+
+    const int vsize = v_float64::nlanes;
+
+    int i = 0;
+    for(; i <= (int)total - vsize; i += vsize )
+    {
+        for (int j = 0; j < vsize; j++)
+        {
+            bool nan = maskAll ? true : false;
+            for (int c = 0; c < cn; c++)
+            {
+                double val = src[i * cn + j * cn + c];
+                bool v = (maskNans && cvIsNaN(val)) || (maskInfs && cvIsInf(val));
+                if (maskAll)
+                    nan = nan && v;
+                else
+                    nan = nan || v;
+            }
+            nan = invert ? !nan : nan;
+            dst[i + j] = nan ? 255 : 0;
+        }
+    }
+
+    return i;
+}
+
+//TODO: make true SIMD code instead
+template <>
+int nanMaskSIMD_<float, 4>(const float *src, uchar *dst, size_t total, bool maskNans, bool maskInfs, bool maskAll, bool invert)
+{
+    const int cn = 4;
+
+    const int vsize = v_float32::nlanes;
+    int i = 0;
+    for(; i <= (int)total - vsize; i += vsize )
+    {
+        for (int j = 0; j < vsize; j++)
+        {
+            bool nan = maskAll ? true : false;
+            for (int c = 0; c < cn; c++)
+            {
+                float val = src[i * cn + j * cn + c];
+                bool v = (maskNans && cvIsNaN(val)) || (maskInfs && cvIsInf(val));
+                if (maskAll)
+                    nan = nan && v;
+                else
+                    nan = nan || v;
+            }
+            nan = invert ? !nan : nan;
+            dst[i + j] = nan ? 255 : 0;
+        }
+    }
+
+    return i;
+}
+
+//TODO: make true SIMD code instead
+template <>
+int nanMaskSIMD_<double, 4>(const double *src, uchar *dst, size_t total, bool maskNans, bool maskInfs, bool maskAll, bool invert)
+{
+    const int cn = 4;
+
+    const int vsize = v_float64::nlanes;
+
+    int i = 0;
+    for(; i <= (int)total - vsize; i += vsize )
+    {
+        for (int j = 0; j < vsize; j++)
+        {
+            bool nan = maskAll ? true : false;
+            for (int c = 0; c < cn; c++)
+            {
+                double val = src[i * cn + j * cn + c];
+                bool v = (maskNans && cvIsNaN(val)) || (maskInfs && cvIsInf(val));
+                if (maskAll)
+                    nan = nan && v;
+                else
+                    nan = nan || v;
+            }
+            nan = invert ? !nan : nan;
+            dst[i + j] = nan ? 255 : 0;
+        }
+    }
+
+    return i;
+}
+#endif
+
+
 template <typename _Tp, int cn>
 static void nanMask_(const _Tp *src, uchar *dst, size_t total, bool maskNans, bool maskInfs, bool maskAll, bool invert)
 {
-    for(size_t i = 0; i < total; i++ )
+    size_t i = 0;
+
+#if CV_SIMD
+    i = nanMaskSIMD_<_Tp, cn>(src, dst, total, maskNans, maskInfs, maskAll, invert);
+#endif
+
+    for(; i < total; i++ )
     {
         bool nan = maskAll ? true : false;
         for (int c = 0; c < cn; c++)
