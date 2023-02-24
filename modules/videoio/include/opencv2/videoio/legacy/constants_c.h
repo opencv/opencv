0fe70174cccfe1115b80aea23241c75def5a5a7d
ippei.i <11306271+ippei-i@users.noreply.github.com>
2023-02-24 23:42:11 +0900

    Add Support VideoCapture get/set CAP_PROP_AUTO_WB and set CAP_PROP_WHITE_BALANCE_BLUE_U in Windows 10 CAP_DSHOW.
    cap.set(CAP_PROP_AUTO_WB, 0.0);
    cap.set(CAP_PROP_AUTO_WB, 1.0);
    cap.set(CAP_PROP_WHITE_BALANCE_BLUE_U, 2000);

--- modules/videoio/include/opencv2/videoio/legacy/constants_c.h
+++ modules/videoio/include/opencv2/videoio/legacy/constants_c.h
@@ -113,6 +113,7 @@ enum
     CV_CAP_PROP_AUTOFOCUS     =39,
     CV_CAP_PROP_SAR_NUM       =40,
     CV_CAP_PROP_SAR_DEN       =41,
+    CV_CAP_PROP_AUTO_WB       =44,
 
     CV_CAP_PROP_AUTOGRAB      =1024, // property for videoio class CvCapture_Android only
     CV_CAP_PROP_SUPPORTED_PREVIEW_SIZES_STRING=1025, // readonly, tricky property, returns cpnst char* indeed
