2307867b3618a9f9dfb22f8decb5e4f5695e51fc
kallaballa <amir@viel-zu.org>
2023-03-04 04:17:03 +0100

    disable vaapi name check

--- modules/videoio/src/cap_ffmpeg_hw.hpp
+++ modules/videoio/src/cap_ffmpeg_hw.hpp
@@ -266,12 +266,12 @@ bool hw_check_device(AVBufferRef* ctx, AVHWDeviceType hw_type, const std::string
         ret = (hw_type != AV_HWDEVICE_TYPE_QSV); // disable MFX if we can't check VAAPI for VideoProc entrypoint
 #endif
     }
-    if (ret && !device_subname.empty() && device_name.find(device_subname) == std::string::npos)
-    {
-        CV_LOG_INFO(NULL, "FFMPEG: Skipping '" << hw_name <<
-            "' video acceleration on the following device name as not matching substring '" << device_subname << "': " << device_name);
-        ret = false;  // reject configuration
-    }
+//    if (ret && !device_subname.empty() && device_name.find(device_subname) == std::string::npos)
+//    {
+//        CV_LOG_INFO(NULL, "FFMPEG: Skipping '" << hw_name <<
+//            "' video acceleration on the following device name as not matching substring '" << device_subname << "': " << device_name);
+//        ret = false;  // reject configuration
+//    }
     if (ret)
     {
         if (!device_name.empty()) {
