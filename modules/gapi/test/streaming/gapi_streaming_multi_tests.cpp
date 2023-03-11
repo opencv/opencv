748306f0733c26ce491714c77985e83cc855dbbf
Dmtiry Matveev <dmitry@g-api.org>
2023-03-11 06:41:09 +0000

    G-API: Add multi-streaming test on streams with different frame rate
    - Also refactor the code a little bit.

--- modules/gapi/test/streaming/gapi_streaming_multi_tests.cpp
+++ modules/gapi/test/streaming/gapi_streaming_multi_tests.cpp
@@ -9,6 +9,8 @@
 
 #include "../common/gapi_streaming_tests_common.hpp"
 
+#include <chrono>
+#include <thread>
 #include <unordered_set>
 #include <unordered_map>
 
@@ -28,6 +30,54 @@ namespace opencv_test
 namespace
 {
 
+class MockSource final: public cv::gapi::wip::IStreamSource {
+    cv::Mat m_prototype;
+    int m_frame_rate;
+    int m_num_frames;
+    int m_usec_delay;
+
+    std::chrono::time_point<std::chrono::system_clock> m_last_point;
+    int64_t m_counter;
+
+public:
+    MockSource(const cv::Mat &prototype, int frame_rate, int num_frames)
+        : m_prototype(prototype)
+        , m_frame_rate(frame_rate)
+        , m_num_frames(num_frames) {
+        GAPI_Assert(m_frame_rate > 0);
+        GAPI_Assert(m_num_frames > 0);
+        m_usec_delay = 1000000 / frame_rate;
+    }
+
+    cv::GMetaArg descr_of() const override {
+        return cv::GMetaArg{cv::descr_of(m_prototype)};
+    }
+
+    bool pull(cv::gapi::wip::Data &data) override {
+        if (m_counter == m_num_frames) {
+            return false;
+        }
+
+        // Check how long the source should sleep to maintain the frame rate
+        // (not thinking of frame drops here)
+        using ms_t = std::chrono::microseconds;
+        const auto now = std::chrono::system_clock::now();
+        auto dur = std::chrono::duration_cast<ms_t>(now.time_since_epoch());
+
+        const auto time_passed = std::chrono::duration_cast<ms_t>(now - m_last_point);
+        if (time_passed.count() < m_usec_delay) {
+            std::this_thread::sleep_for(ms_t{m_usec_delay - time_passed.count()});
+        }
+        data = m_prototype;
+
+        // Tag data with seq_id/ts
+        data.meta[cv::gapi::streaming::meta_tag::timestamp] = int64_t{dur.count()};
+        data.meta[cv::gapi::streaming::meta_tag::seq_id]    = int64_t{m_counter++};
+        m_last_point = now;
+        return true;
+    }
+};
+
 struct GAPI_Streaming_Multi_Base {
     GAPI_Streaming_Multi_Base() {
         // Define a simple image processing graph
@@ -46,6 +96,27 @@ struct GAPI_Streaming_Multi_Base {
     cv::gapi::streaming::tag out_tag{};
     cv::Mat out_mat;
     int64_t out_seq_id = 0;
+
+    std::unordered_map<int, int> frames;
+    std::unordered_set<int> end_of_streams;
+
+    template<typename F>
+    void run_with_check(F f) {
+        ccomp.start();
+        // Process and count frames we use
+        while (ccomp.pull(out_tag, cv::gout(out_mat, out_seq_id))) {
+            if (out_tag.eos) {
+                end_of_streams.insert(out_tag.id); // end-of-stream received
+            } else {
+                frames[out_tag.id]++;              // data received
+                f();                               // run check
+            }
+        }
+    }
+
+    void run() {
+        run_with_check([](){});
+    }
 };
 
 struct GAPI_Streaming_Multi_Completion:
@@ -60,26 +131,13 @@ TEST_P(GAPI_Streaming_Multi_Completion, TestEOS)
     std::vector<int> stream_ids;
     try {
         for (int i = 0; i < num_streams; i++) {
-            stream_ids.push_back(ccomp.addSource<cv::gapi::wip::GCaptureSource>(path));
+            stream_ids.push_back
+                (ccomp.addSource<cv::gapi::wip::GCaptureSource>(path));
         }
     } catch(...) {
         throw SkipTestException("Video file can not be opened");
     }
-
-    ccomp.start();
-
-    std::unordered_map<int, int> frames;
-    std::unordered_set<int> end_of_streams;
-
-    while (ccomp.pull(out_tag, cv::gout(out_mat, out_seq_id))) {
-        if (out_tag.eos) {
-            // end-of-stream received
-            end_of_streams.insert(out_tag.id);
-        } else {
-            // data received. just increment frame counter
-            frames[out_tag.id]++;
-        }
-    }
+    run();
 
     // Should receive EOS for every stream
     EXPECT_EQ(num_streams, static_cast<int>(end_of_streams.size()));
@@ -93,6 +151,52 @@ TEST_P(GAPI_Streaming_Multi_Completion, TestEOS)
 
 INSTANTIATE_TEST_CASE_P(TestEOS, GAPI_Streaming_Multi_Completion,
                         Values(1, 2, 3));
+
+struct GAPI_Streaming_Multi: public GAPI_Streaming_Multi_Base,
+                             public ::testing::Test {};
+
+TEST_F(GAPI_Streaming_Multi, TestDifferentCompletionTime_Frames) {
+    // Add streams with a different completion time (based on num frames)
+    cv::Mat proto = cv::Mat::eye(cv::Size(320, 240), CV_8UC3);
+    int stream_ids[2] = {
+        ccomp.addSource<MockSource>(proto, 30, 50), // 30 fps, 50 frames
+        ccomp.addSource<MockSource>(proto, 30, 75), // 30 fps, 75 frames
+    };
+
+    run_with_check([this](){
+        // As one stream finishes earlier than other, check
+        // if finished stream's messages don't pop up here
+        if (end_of_streams.size() > 0u) {
+            EXPECT_EQ(0u, end_of_streams.count(out_tag.id));
+        }
+    });
+
+    // Check completions
+    EXPECT_EQ(50, frames.at(stream_ids[0]));
+    EXPECT_EQ(75, frames.at(stream_ids[1]));
+}
+
+TEST_F(GAPI_Streaming_Multi, TestDifferentCompletionTime_Rate) {
+    // Same test as before but now use sources with different
+    // latency
+    cv::Mat proto = cv::Mat::eye(cv::Size(320, 240), CV_8UC3);
+    int stream_ids[2] = {
+        ccomp.addSource<MockSource>(proto, 30, 50), // 30 fps, 50 frames
+        ccomp.addSource<MockSource>(proto, 60, 50), // 50 fps, 50 frames
+    };
+
+    run_with_check([this](){
+        // As one stream finishes earlier than other, check
+        // if finished stream's messages don't pop up here
+        if (end_of_streams.size() > 0u) {
+            EXPECT_EQ(0u, end_of_streams.count(out_tag.id));
+        }
+    });
+
+    // Check completions
+    EXPECT_EQ(50, frames.at(stream_ids[0]));
+    EXPECT_EQ(50, frames.at(stream_ids[1]));
+}
 
 } // namespace
 
