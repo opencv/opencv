7af6db4f536f1011e3091cff749284860a49e090
Maksim Shabunin <maksim.shabunin@gmail.com>
2023-03-27 18:29:38 +0300

    RISC-V: fix hardcoded options in RVV 0.7.1 toolchain file

--- platforms/linux/riscv64-071-gcc.toolchain.cmake
+++ platforms/linux/riscv64-071-gcc.toolchain.cmake
@@ -1,9 +1,8 @@
-set(CMAKE_SYSTEM_NAME "Linux")
-set(CMAKE_C_COMPILER  riscv64-unknown-linux-gnu-gcc)
+set(CMAKE_SYSTEM_NAME Linux)
+set(CMAKE_SYSTEM_PROCESSOR riscv64)
+
 set(CMAKE_CXX_COMPILER riscv64-unknown-linux-gnu-g++)
+set(CMAKE_C_COMPILER  riscv64-unknown-linux-gnu-gcc)
 
-set(CMAKE_CXX_FLAGS ""    CACHE STRING "")
-set(CMAKE_C_FLAGS ""    CACHE STRING "")
-
-set(CMAKE_CXX_FLAGS "-static -march=rv64gcvxthead -mabi=lp64v -pthread -D__riscv_vector_071")
-set(CMAKE_C_FLAGS "-static -march=rv64gcvxthead -mabi=lp64v -pthread -D__riscv_vector_071")
+set(CMAKE_CXX_FLAGS_INIT "-march=rv64gcv -mabi=lp64d -D__riscv_vector_071")
+set(CMAKE_C_FLAGS_INIT "-march=rv64gcv -mabi=lp64d -D__riscv_vector_071")
