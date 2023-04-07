3bcc3e70f197fefdf6a36c7561297244b189a946
Alexander Smorkalov <alexander.smorkalov@xperience.ai>
2023-04-07 13:56:57 +0300

    Extended setNumThreads documentation according to code review.

--- modules/core/include/opencv2/core/utility.hpp
+++ modules/core/include/opencv2/core/utility.hpp
@@ -181,9 +181,9 @@ CV_EXPORTS void glob(String pattern, std::vector<String>& result, bool recursive
 
 /** @brief OpenCV will try to set the number of threads for subsequent parallel regions.
 
-If threads == 0, OpenCV will disable threading optimizations and run all it's functions
-sequentially. Passing threads \< 0 will reset threads number to system default. This function must
-be called outside of parallel region.
+If threads == 1, OpenCV will disable threading optimizations and run all it's functions
+sequentially. Passing threads \< 0 will reset threads number to system default.
+The function is not thread-safe. It must not be called in parallel region or concurrent threads.
 
 OpenCV will try to run its functions with specified threads number, but some behaviour differs from
 framework:
