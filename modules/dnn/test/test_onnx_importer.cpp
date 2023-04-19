2a4936f66c6a38c9a0c8431f9766967f22eba026
Abduragim Shtanchaev <shtanchaev1994@gmail.com>
2023-04-19 14:25:50 +0300

    Add CUDA skip flag for GRU layer too

--- modules/dnn/test/test_onnx_importer.cpp
+++ modules/dnn/test/test_onnx_importer.cpp
@@ -1271,6 +1271,8 @@ TEST_P(Test_ONNX_layers, gru_cell_batchsize_50_seqlen_1)
             CV_TEST_TAG_DNN_SKIP_IE_NGRAPH, CV_TEST_TAG_DNN_SKIP_IE_VERSION
         );
 #endif
+    if(backend == DNN_BACKEND_CUDA)
+        applyTestTag(CV_TEST_TAG_DNN_SKIP_CUDA);
     testONNXModels("gru_cell_batchsize_50_seqlen_1", npy, 0, 0, false, false);
 }
 
@@ -1283,6 +1285,8 @@ TEST_P(Test_ONNX_layers, gru_cell_batchsize_5_seqlen_5)
             CV_TEST_TAG_DNN_SKIP_IE_NGRAPH, CV_TEST_TAG_DNN_SKIP_IE_VERSION
         );
 #endif
+    if(backend == DNN_BACKEND_CUDA)
+        applyTestTag(CV_TEST_TAG_DNN_SKIP_CUDA);
     testONNXModels("gru_cell_batchsize_5_seqlen_5", npy, 0, 0, false, false);
 }
 
@@ -1295,6 +1299,8 @@ TEST_P(Test_ONNX_layers, gru_cell_batchsize_1_seqlen_50)
             CV_TEST_TAG_DNN_SKIP_IE_NGRAPH, CV_TEST_TAG_DNN_SKIP_IE_VERSION
         );
 #endif
+    if(backend == DNN_BACKEND_CUDA)
+        applyTestTag(CV_TEST_TAG_DNN_SKIP_CUDA);
     testONNXModels("gru_cell_batchsize_1_seqlen_50", npy, 0, 0, false, false);
 }
 
