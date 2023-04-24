d04aecd5a45efd4256c8f6ef9888736e3f39a075
Dmitry Kurtaev <dmitry.kurtaev@gmail.com>
2023-04-24 13:44:10 +0300

    Merge pull request #23409 from dkurt:dnn_tflite_quant
    Import and inference INT8 quantized TFLite model #23409
    
    ### Pull Request Readiness Checklist
    
    * Support quantized TFLite models
    * Enable fused activations (FP32, INT8)
    
    **Merge with extra**: https://github.com/opencv/opencv_extra/pull/1048
    
    ![res](https://user-images.githubusercontent.com/25801568/231433201-566b4bd6-ccff-462c-9e74-adbdcdf3648b.png)
    
    on the image, green boxes are from TFLite and red boxes from OpenCV
    
    See details at https://github.com/opencv/opencv/wiki/How_to_contribute#making-a-good-pull-request
    
    - [x] I agree to contribute to the project under Apache 2 License.
    - [x] To the best of my knowledge, the proposed patch is not based on a code under GPL or another license that is incompatible with OpenCV
    - [x] The PR is proposed to the proper branch
    - [x] There is a reference to the original bug report and related work
    - [x] There is accuracy test, performance test and test data in opencv_extra repository, if applicable
          Patch to opencv_extra has the same branch name.
    - [x] The feature is well documented and sample code can be built with the project CMake

--- modules/dnn/test/test_tflite_importer.cpp
+++ modules/dnn/test/test_tflite_importer.cpp
@@ -130,6 +130,22 @@ TEST(Test_TFLite, max_unpooling)
     }
 }
 
+TEST(Test_TFLite, EfficientDet_int8) {
+    Net net = readNet(findDataFile("dnn/tflite/coco_efficientdet_lite0_v1_1.0_quant_2021_09_06.tflite", false));
+
+    Mat img = imread(findDataFile("dnn/dog416.png"));
+    Mat blob = blobFromImage(img, 1.0, Size(320, 320));
+
+    net.setInput(blob);
+    Mat out = net.forward();
+    Mat_<float> ref({3, 7}, {
+        0, 7, 0.62890625, 0.6014542579650879, 0.13300055265426636, 0.8977657556533813, 0.292389452457428,
+        0, 17, 0.56640625, 0.15983937680721283, 0.35905322432518005, 0.5155506730079651, 0.9409466981887817,
+        0, 1, 0.5, 0.14357104897499084, 0.2240825891494751, 0.7183101177215576, 0.9140362739562988
+    });
+    normAssertDetections(ref, out, "", 0.5, 0.05, 0.1);
+}
+
 }}  // namespace
 
 #endif  // OPENCV_TEST_DNN_TFLITE
