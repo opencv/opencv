1016a33b8469d764c83ea42a86ffd52a11dab3ec
Maksim Shabunin <maksim.shabunin@gmail.com>
2023-05-11 16:17:02 +0300

    fixup! videoio/FFmpeg: increased packet read attempt limit, allow configuring it

--- modules/videoio/src/cap_ffmpeg_impl.hpp
+++ modules/videoio/src/cap_ffmpeg_impl.hpp
@@ -1430,7 +1430,7 @@ bool CvCapture_FFMPEG::grabFrame()
     bool valid = false;
 
     static const size_t max_stream_attempts = cv::utils::getConfigurationParameterSizeT("OPENCV_FFMPEG_STREAM_ATTEMPTS", 4096);
-    static const size_t max_decode_attempts = cv::utils::getConfigurationParameterSizeT("OPENCV_FFMPEG_DECODE_ATTEMPTS", 5);
+    static const size_t max_decode_attempts = cv::utils::getConfigurationParameterSizeT("OPENCV_FFMPEG_DECODE_ATTEMPTS", 64);
     size_t cur_stream_attempts = 0;
     size_t cur_decode_attempts = 0;
 
