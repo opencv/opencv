5025f29378bfbaf0bf5de102370a8a096f1dd0c4
Zihao Mu <zihaomu@outlook.com>
2023-05-19 01:02:27 +0800

    speed up vulkan dnn, and support ios and apple m1 chip. (#23349)

--- modules/dnn/src/layer.cpp
+++ modules/dnn/src/layer.cpp
@@ -50,7 +50,8 @@ Ptr<BackendNode> Layer::initCUDA(
     return Ptr<BackendNode>();
 }
 
-Ptr<BackendNode> Layer::initVkCom(const std::vector<Ptr<BackendWrapper>>&)
+Ptr<BackendNode> Layer::initVkCom(const std::vector<Ptr<BackendWrapper> > &inputs,
+                                  std::vector<Ptr<BackendWrapper> > &outputs)
 {
     CV_Error(Error::StsNotImplemented, "VkCom pipeline of " + type + " layers is not defined.");
     return Ptr<BackendNode>();
