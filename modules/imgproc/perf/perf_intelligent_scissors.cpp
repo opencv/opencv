f5d2ae677b0c8815788c871af62f8068fc136f62
Christine Poerschke <6458642+cpoerschke@users.noreply.github.com>
2023-05-27 20:27:06 +0100

    imgproc: add basic IntelligentScissorsMB performance test

---
+++ modules/imgproc/perf/perf_intelligent_scissors.cpp
@@ -0,0 +1,42 @@
+// This file is part of OpenCV project.
+// It is subject to the license terms in the LICENSE file found in the top-level directory
+// of this distribution and at http://opencv.org/license.html.
+#include "perf_precomp.hpp"
+
+namespace opencv_test { namespace {
+
+
+Mat getTestImage(int flags = IMREAD_COLOR)
+{
+    static Mat m;
+    if (m.empty())
+    {
+        m = imread(findDataFile("cv/shared/lena.png"), flags);
+    }
+    return m.clone();
+}
+
+typedef perf::TestBaseWithParam<bool> TestIntelligentScissorsMB;
+
+PERF_TEST_P(TestIntelligentScissorsMB, buildMap, testing::Values( IMREAD_GRAYSCALE, IMREAD_COLOR ))
+{
+    const int flags = GetParam();
+
+    const Mat image = getTestImage(flags);
+    const Point source_point(275, 63);
+
+    segmentation::IntelligentScissorsMB tool;
+    tool.applyImage(image);
+
+    PERF_SAMPLE_BEGIN()
+    for (size_t idx = 1; idx <= 100; ++idx)
+    {
+        tool.buildMap(source_point);
+    }
+    PERF_SAMPLE_END()
+
+    SANITY_CHECK_NOTHING();
+}
+
+
+}} // namespace
