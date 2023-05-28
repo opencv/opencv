ce6b32b91e0531f8ab314373941f792a2714543c
Gustavo Ribeiro <99373133+gustavosett@users.noreply.github.com>
2023-05-28 00:55:06 -0300

    Enhance Abstract Base Classes with Docstrings
    This commit enhances the abstract base classes `AbstractModel` and `Framework` by adding docstrings.
    
    Key improvements include:
    
    1. Docstring addition: Each class and abstract method now has a docstring that explains what it is/does, providing more context and clarity for developers.
    2. Removed redundant metaclass: Removed the __metaclass__ = ABCMeta line, as it is not needed in Python 3. With the ABC module, you can directly inherit from ABC to create an abstract base class.

--- samples/dnn/dnn_model_runner/dnn_conversion/common/abstract_model.py
+++ samples/dnn/dnn_model_runner/dnn_conversion/common/abstract_model.py
@@ -1,23 +1,39 @@
-from abc import ABC, ABCMeta, abstractmethod
-
+from abc import ABC, abstractmethod
 
 class AbstractModel(ABC):
-
+    '''
+    This is an abstract base class for models. All derived model classes must implement the
+    `get_prepared_models` method.
+    '''
     @abstractmethod
     def get_prepared_models(self):
+        '''
+        Abstract method which needs to be implemented by any class that inherits from `AbstractModel`.
+        This method is intended to return the prepared model(s) for use.
+        '''
         pass
 
 
-class Framework(object):
+class Framework(ABC):
+    '''
+    This is an abstract base class for frameworks. All derived framework classes must implement the
+    `get_name` and `get_output` methods.
+    '''
     in_blob_name = ''
     out_blob_name = ''
 
-    __metaclass__ = ABCMeta
-
     @abstractmethod
     def get_name(self):
+        '''
+        Abstract method which needs to be implemented by any class that inherits from `Framework`.
+        This method is intended to return the name of the framework.
+        '''
         pass
 
     @abstractmethod
     def get_output(self, input_blob):
+        '''
+        Abstract method which needs to be implemented by any class that inherits from `Framework`.
+        This method is intended to process the input blob and return the output.
+        '''
         pass
