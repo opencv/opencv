1ad89f984d6c17e3a3f68c16ea719ebfeeeeafd0
Sujal Rana <118067837+Sujal-Rana-88@users.noreply.github.com>
2023-05-31 11:19:59 +0530

    Update segment_objects.cpp

--- samples/cpp/segment_objects.cpp
+++ samples/cpp/segment_objects.cpp
@@ -11,11 +11,11 @@ using namespace cv;
 static void help(char** argv)
 {
     printf("\n"
-            "This program demonstrated a simple method of connected components clean up of background subtraction\n"
-            "When the program starts, it begins learning the background.\n"
-            "You can toggle background learning on and off by hitting the space bar.\n"
-            "Call\n"
-            "%s [video file, else it reads camera 0]\n\n", argv[0]);
+           "This program demonstrates a simple method of connected components cleanup of background subtraction\n"
+           "When the program starts, it begins learning the background.\n"
+           "You can toggle background learning on and off by hitting the space bar.\n"
+           "Call\n"
+           "%s [video file, else it reads camera 0]\n\n", argv[0]);
 }
 
 static void refineSegments(const Mat& img, Mat& mask, Mat& dst)
@@ -27,15 +27,15 @@ static void refineSegments(const Mat& img, Mat& mask, Mat& dst)
 
     Mat temp;
 
-    dilate(mask, temp, Mat(), Point(-1,-1), niters);
-    erode(temp, temp, Mat(), Point(-1,-1), niters*2);
-    dilate(temp, temp, Mat(), Point(-1,-1), niters);
+    dilate(mask, temp, Mat(), Point(-1, -1), niters);
+    erode(temp, temp, Mat(), Point(-1, -1), niters * 2);
+    dilate(temp, temp, Mat(), Point(-1, -1), niters);
 
-    findContours( temp, contours, hierarchy, RETR_CCOMP, CHAIN_APPROX_SIMPLE );
+    findContours(temp, contours, hierarchy, RETR_CCOMP, CHAIN_APPROX_SIMPLE);
 
     dst = Mat::zeros(img.size(), CV_8UC3);
 
-    if( contours.size() == 0 )
+    if (contours.empty())
         return;
 
     // iterate through all the top-level contours,
@@ -43,20 +43,19 @@ static void refineSegments(const Mat& img, Mat& mask, Mat& dst)
     int idx = 0, largestComp = 0;
     double maxArea = 0;
 
-    for( ; idx >= 0; idx = hierarchy[idx][0] )
+    for (; idx >= 0; idx = hierarchy[idx][0])
     {
         const vector<Point>& c = contours[idx];
         double area = fabs(contourArea(Mat(c)));
-        if( area > maxArea )
+        if (area > maxArea)
         {
             maxArea = area;
             largestComp = idx;
         }
     }
-    Scalar color( 0, 0, 255 );
-    drawContours( dst, contours, largestComp, color, FILLED, LINE_8, hierarchy );
+    Scalar color(0, 0, 255);
+    drawContours(dst, contours, largestComp, color, FILLED, LINE_8, hierarchy);
 }
-
 
 int main(int argc, char** argv)
 {
@@ -73,9 +72,9 @@ int main(int argc, char** argv)
     if (input.empty())
         cap.open(0);
     else
-        cap.open(samples::findFileOrKeep(input));
+        cap.open(input);
 
-    if( !cap.isOpened() )
+    if (!cap.isOpened())
     {
         printf("\nCan not open camera or video file\n");
         return -1;
@@ -84,36 +83,38 @@ int main(int argc, char** argv)
     Mat tmp_frame, bgmask, out_frame;
 
     cap >> tmp_frame;
-    if(tmp_frame.empty())
+    if (tmp_frame.empty())
     {
         printf("can not read data from the video source\n");
         return -1;
     }
 
-    namedWindow("video", 1);
-    namedWindow("segmented", 1);
+    namedWindow("video", WINDOW_NORMAL);
+    namedWindow("segmented", WINDOW_NORMAL);
 
-    Ptr<BackgroundSubtractorMOG2> bgsubtractor=createBackgroundSubtractorMOG2();
+    Ptr<BackgroundSubtractorMOG2> bgsubtractor = createBackgroundSubtractorMOG2();
     bgsubtractor->setVarThreshold(10);
 
-    for(;;)
+    for (;;)
     {
         cap >> tmp_frame;
-        if( tmp_frame.empty() )
+        if (tmp_frame.empty())
             break;
         bgsubtractor->apply(tmp_frame, bgmask, update_bg_model ? -1 : 0);
         refineSegments(tmp_frame, bgmask, out_frame);
         imshow("video", tmp_frame);
         imshow("segmented", out_frame);
         char keycode = (char)waitKey(30);
-        if( keycode == 27 )
+        if (keycode == 27)
             break;
-        if( keycode == ' ' )
+        if (keycode == ' ')
         {
             update_bg_model = !update_bg_model;
-            printf("Learn background is in state = %d\n",update_bg_model);
+            printf("Learn background is in state = %d\n", update_bg_model);
         }
     }
+
+    destroyAllWindows();
 
     return 0;
 }
