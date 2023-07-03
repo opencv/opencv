b31d6ce609bd3ab0242d10aa6352c0384ba40dde
chacha21 <pierre@chachatelier.fr>
2023-07-03 18:29:04 +0200

    make cuda::GpuMatND compatible with InputArray/OutputArray
    continuation of  PR#19259 (https://github.com/opencv/opencv/pull/19259/commits/ddebc2c26bee2b3da653693496212549476d4612)
    
    No new feature for cuda::GpuMatND

--- modules/core/include/opencv2/core/mat.inl.hpp
+++ modules/core/include/opencv2/core/mat.inl.hpp
@@ -151,7 +151,10 @@ inline _InputArray::_InputArray(const cuda::GpuMat& d_mat)
 { init(+CUDA_GPU_MAT + ACCESS_READ, &d_mat); }
 
 inline _InputArray::_InputArray(const std::vector<cuda::GpuMat>& d_mat)
-{	init(+STD_VECTOR_CUDA_GPU_MAT + ACCESS_READ, &d_mat);}
+{ init(+STD_VECTOR_CUDA_GPU_MAT + ACCESS_READ, &d_mat);}
+
+inline _InputArray::_InputArray(const cuda::GpuMatND& d_mat)
+{ init(+CUDA_GPU_MATND + ACCESS_READ, &d_mat); }
 
 inline _InputArray::_InputArray(const ogl::Buffer& buf)
 { init(+OPENGL_BUFFER + ACCESS_READ, &buf); }
@@ -197,6 +200,7 @@ inline bool _InputArray::isVector() const { return kind() == _InputArray::STD_VE
                                                    (kind() == _InputArray::MATX && (sz.width <= 1 || sz.height <= 1)); }
 inline bool _InputArray::isGpuMat() const { return kind() == _InputArray::CUDA_GPU_MAT; }
 inline bool _InputArray::isGpuMatVector() const { return kind() == _InputArray::STD_VECTOR_CUDA_GPU_MAT; }
+inline bool _InputArray::isGpuMatND() const { return kind() == _InputArray::CUDA_GPU_MATND; }
 
 ////////////////////////////////////////////////////////////////////////////////////////
 
@@ -275,7 +279,10 @@ inline _OutputArray::_OutputArray(cuda::GpuMat& d_mat)
 { init(+CUDA_GPU_MAT + ACCESS_WRITE, &d_mat); }
 
 inline _OutputArray::_OutputArray(std::vector<cuda::GpuMat>& d_mat)
-{	init(+STD_VECTOR_CUDA_GPU_MAT + ACCESS_WRITE, &d_mat);}
+{ init(+STD_VECTOR_CUDA_GPU_MAT + ACCESS_WRITE, &d_mat);}
+
+inline _OutputArray::_OutputArray(cuda::GpuMatND& d_mat)
+{ init(+CUDA_GPU_MATND + ACCESS_WRITE, &d_mat); }
 
 inline _OutputArray::_OutputArray(ogl::Buffer& buf)
 { init(+OPENGL_BUFFER + ACCESS_WRITE, &buf); }
@@ -298,6 +305,8 @@ inline _OutputArray::_OutputArray(const std::vector<UMat>& vec)
 inline _OutputArray::_OutputArray(const cuda::GpuMat& d_mat)
 { init(FIXED_TYPE + FIXED_SIZE + CUDA_GPU_MAT + ACCESS_WRITE, &d_mat); }
 
+inline _OutputArray::_OutputArray(const cuda::GpuMatND& d_mat)
+{ init(+FIXED_TYPE + FIXED_SIZE + CUDA_GPU_MATND + ACCESS_WRITE, &d_mat); }
 
 inline _OutputArray::_OutputArray(const ogl::Buffer& buf)
 { init(FIXED_TYPE + FIXED_SIZE + OPENGL_BUFFER + ACCESS_WRITE, &buf); }
@@ -400,6 +409,9 @@ _InputOutputArray::_InputOutputArray(const _Tp* vec, int n)
 inline _InputOutputArray::_InputOutputArray(cuda::GpuMat& d_mat)
 { init(+CUDA_GPU_MAT + ACCESS_RW, &d_mat); }
 
+inline _InputOutputArray::_InputOutputArray(cuda::GpuMatND& d_mat)
+{ init(+CUDA_GPU_MATND + ACCESS_RW, &d_mat); }
+
 inline _InputOutputArray::_InputOutputArray(ogl::Buffer& buf)
 { init(+OPENGL_BUFFER + ACCESS_RW, &buf); }
 
@@ -426,6 +438,9 @@ inline _InputOutputArray::_InputOutputArray(const std::vector<cuda::GpuMat>& d_m
 
 template<> inline _InputOutputArray::_InputOutputArray(std::vector<cuda::GpuMat>& d_mat)
 { init(FIXED_TYPE + FIXED_SIZE + STD_VECTOR_CUDA_GPU_MAT + ACCESS_RW, &d_mat);}
+
+inline _InputOutputArray::_InputOutputArray(const cuda::GpuMatND& d_mat)
+{ init(+FIXED_TYPE + FIXED_SIZE + CUDA_GPU_MATND + ACCESS_RW, &d_mat); }
 
 inline _InputOutputArray::_InputOutputArray(const ogl::Buffer& buf)
 { init(FIXED_TYPE + FIXED_SIZE + OPENGL_BUFFER + ACCESS_RW, &buf); }
