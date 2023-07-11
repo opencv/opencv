d84fd896f88803f3f7dbc90afef8cb11191c2de4
Liutong HAN <liutong2020@iscas.ac.cn>
2023-07-11 16:10:27 +0800

    Add missing ”v_popcount“ for RVV and enable tests.

--- modules/core/test/test_intrin_utils.hpp
+++ modules/core/test/test_intrin_utils.hpp
@@ -2048,6 +2048,7 @@ void test_hal_intrin_uint64()
         .test_rotate<0>().test_rotate<1>()
         .test_extract_n<0>().test_extract_n<1>()
         .test_extract_highest()
+        .test_popcount()
         //.test_broadcast_element<0>().test_broadcast_element<1>()
         ;
 }
@@ -2069,6 +2070,7 @@ void test_hal_intrin_int64()
         .test_extract_highest()
         //.test_broadcast_element<0>().test_broadcast_element<1>()
         .test_cvt64_double()
+        .test_popcount()
         ;
 }
 
