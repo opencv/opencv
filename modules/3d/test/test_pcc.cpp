3767c4b930d45b995a60a4793919b0ee7a5fc4ed
Yuhang Wang <1482173319@qq.com>
2023-08-09 11:35:52 +0800

    Improve test_pcc

--- modules/3d/test/test_pcc.cpp
+++ modules/3d/test/test_pcc.cpp
@@ -12,61 +12,26 @@ namespace opencv_test { namespace {
 
 using namespace cv;
 
-class PccEntropyCodingTest: public testing::Test
+class PccTest: public testing::Test
 {
 protected:
     void SetUp() override
     {
-        //setbuf(stdout, nullptr);
-        // set test char vector to encode and decode
         testCharVector = {'a', 'a', 'b', 'b', 'b', 'c', '1', '2', '3'};
-        String FileBase = R"(C:\Users\WYH\Desktop\PointCloud\dress)";
-        // TODO lew resolution(big number) causes error!!
-        double resolution = 1;
-        String label = "Test_Color_";
+
+        resolution = 0.001;
+        inputFilename=cvtest::TS::ptr()->get_data_path()+"pointcloudio/sphere";
+
+        //The point cloud is generated randomly by the function generateSphere()
+
+        //generateSphere(pointCloud,vector<float>{0.f,0.f,0.f,10.f},0.5,10000,vector<float>{-10.f,10.f,-10.f,10.f,-10.f,10.f});
+        //savePointCloud(inputFilename+".ply",pointCloud, normal_placeholder, restore_color_attribute);
+
+        loadPointCloud(inputFilename+".ply",pointCloud,normal_placeholder);
         String res_str = std::to_string(resolution);
         res_str.erase(res_str.find_last_not_of('0') + 1);
         res_str.erase(res_str.find('.'), 1);
-
-        auto start = std::chrono::high_resolution_clock::now();
-        //load .ply file
-        String loadFileName = FileBase + ".ply";
-        // color: rgb
-        loadPointCloud(loadFileName, pointCloud, normal_placeholder);
-        auto stop = std::chrono::high_resolution_clock::now();
-        auto duration = std::chrono::duration_cast<std::chrono::microseconds>(stop - start);
-        std::cout << "Time taken by loadPointCloud: "
-                  << duration.count()/1e6<< std::endl;
-
-        PointCloudCompression pcc;
-
-        start = std::chrono::high_resolution_clock::now();
-        std::ofstream vectorToStream;
-        vectorToStream.open(FileBase + label + res_str + ".bin", std::ios_base::binary);
-        pcc.compress(pointCloud,resolution,vectorToStream);
-        vectorToStream.close();
-        stop = std::chrono::high_resolution_clock::now();
-        duration = std::chrono::duration_cast<std::chrono::microseconds>(stop - start);
-        std::cout << "Time taken by Compress: "
-                  << duration.count()/1e6<< std::endl;
-
-        start = std::chrono::high_resolution_clock::now();
-        std::ifstream streamToVector;
-        streamToVector.open(FileBase + label + res_str + ".bin", std::ios_base::binary);
-        pcc.decompress(streamToVector,restorePointCloudData);
-        streamToVector.close();
-        stop = std::chrono::high_resolution_clock::now();
-        duration = std::chrono::duration_cast<std::chrono::microseconds>(stop - start);
-        std::cout << "Time taken by Decompress: "
-                  << duration.count()/1e6<< std::endl;
-
-        start = std::chrono::high_resolution_clock::now();
-        String saveFileName= FileBase + label + res_str + ".ply";
-        savePointCloud(saveFileName,restorePointCloudData, normal_placeholder, restore_color_attribute);
-        stop = std::chrono::high_resolution_clock::now();
-        duration = std::chrono::duration_cast<std::chrono::microseconds>(stop - start);
-        std::cout << "Time taken by SavePointCloud: "
-                  << duration.count()/1e6<< std::endl;
+        outputFilename=inputFilename+"_"+res_str+ ".ply";
     }
 
 public:
@@ -75,25 +40,67 @@ public:
     std::vector<unsigned char> restoreCharVector;
     EntropyCoder testEntropyCoder;
 
+    String inputFilename;
+    String outputFilename;
+    double resolution;
+    PointCloudCompression pcc;
+    std::ofstream vectorToStream;
+    std::ifstream streamToVector;
+
     //Origin point cloud data
     std::vector<Point3f> pointCloud;
-
     //Point cloud data from octree
     std::vector<Point3f> restorePointCloudData;
     std::vector<Point3f> normal_placeholder;
     vector<Point3f> restore_color_attribute;
 };
 
-TEST_F(PccEntropyCodingTest, EntropyEncodingTest){
+TEST_F(PccTest, EntropyEncodingTest){
     EXPECT_NO_THROW(testEntropyCoder.encodeCharVectorToStream(testCharVector, binaryStream));
     binaryStream.seekg(0, std::ios::beg);
     EXPECT_NO_THROW(testEntropyCoder.decodeStreamToCharVector(binaryStream, restoreCharVector));
     EXPECT_EQ(testCharVector, restoreCharVector);
+};
+
+TEST_F(PccTest, PointCloudCompressionTest){
+
+    vectorToStream.open(inputFilename+".bin", std::ios_base::binary);
+    pcc.compress(pointCloud,resolution,vectorToStream);
+    vectorToStream.close();
+
+    streamToVector.open(inputFilename+".bin", std::ios_base::binary);
+    pcc.decompress(streamToVector,restorePointCloudData);
+    streamToVector.close();
+
+    savePointCloud(outputFilename,restorePointCloudData, normal_placeholder, restore_color_attribute);
+
+    // BPP (Bits Per Point)
+    // BPP is used to quantify the compression efficiency of point cloud compression algorithms.
+    // It indicates the number of bits required to store a single point. BPP is calculated as the ratio of the file size
+    // to the number of points in the point cloud file. A smaller BPP value corresponds to higher compression efficiency.
+    std::ifstream pccFile(inputFilename+".bin", std::ios::binary | std::ios::ate);
+    std::streampos pccFileSize = pccFile.tellg();
+    pccFile.close();
+
+    std::ifstream oriFile(inputFilename+".ply", std::ios::binary | std::ios::ate);
+    std::streampos oriFileSize = oriFile.tellg();
+    oriFile.close();
+
+    float pcc_bpp=(float)pccFileSize/(float)pointCloud.size();
+    float ori_bpp=(float)oriFileSize/(float)pointCloud.size();
+    CV_LOG_INFO(nullptr,"The BPP of pccFile is "+std::to_string(pcc_bpp));
+    CV_LOG_INFO(nullptr,"The BPP of oriFile is "+std::to_string(ori_bpp));
+
+    // PSNR (Peak signal-to-noise ratio)
+    // PSNR is commonly used to quantify reconstruction quality for images and video subject to lossy compression.
+    // The same applies to point clouds.
+    // There is a tool provided by MPEG to calculate PSNR in pcc, see details in https://github.com/minhkstn/mpeg-pcc-dmetric.
+
+
+
 }
 
-//TEST_F(PccTest, PointCloudCompression){
-//
-//}
+
 
 } // namespace
 } // opencv_test
