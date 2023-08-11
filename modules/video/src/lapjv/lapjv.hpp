d31de75d1037a2ece308bc720f09c3ae4b2dad1a
guanarp <joserios@fiuna.edu.py>
2023-08-11 15:58:20 -0400

    removed opencv license

--- modules/video/src/lapjv/lapjv.hpp
+++ modules/video/src/lapjv/lapjv.hpp
@@ -1,7 +1,3 @@
-// This file is part of OpenCV project.
-// It is subject to the license terms in the LICENSE file found in the top-level directory
-// of this distribution and at http://opencv.org/license.html.
-
 /** @brief Linear Assignment Problem solver
  *
  *  lap is a linear assignment problem solver using Jonker-Volgenant algorithm for dense (LAPJV) or
