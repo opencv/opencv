a7b9c3a2610cc8954e98cdb5d62b4256892bdef2
fengyuentau <yuantao.feng@opencv.org.cn>
2023-08-28 15:43:35 +0800

    add backends: cuda, cann, inf_ngraph and vkcom

--- modules/dnn/src/layers/gemm_layer.cpp
+++ modules/dnn/src/layers/gemm_layer.cpp
@@ -4,6 +4,11 @@
 
 #include "../precomp.hpp"
 #include "layers_common.hpp"
+// backends
+#include "../op_cuda.hpp"
+#include "../op_cann.hpp"
+#include "../ie_ngraph.hpp"
+#include "../op_vkcom.hpp"
 
 #include <opencv2/dnn/shape_utils.hpp>
 #include "cpu_kernels/fast_gemm.hpp"
@@ -20,15 +25,19 @@ public:
         alpha = params.get<float>("alpha", 1.0f);
         beta = params.get<float>("beta", 1.0f);
 
-        const_B = params.get<bool>("constB", false);
-        const_C = params.get<bool>("constC", false);
-        have_bias = params.get<bool>("have_bias", false);
+        const_B = params.get<bool>("constB", false); // true means blobs[0] is B
+        const_C = params.get<bool>("constC", false); // true means blobs.back() is C
+        have_bias = params.get<bool>("have_bias", false); // NOTE: have_bias being true does not mean bias is constant
 
         real_ndims_C = params.get<int>("real_ndims_C", -1);
     }
 
     virtual bool supportBackend(int backendId) CV_OVERRIDE {
-        return backendId == DNN_BACKEND_OPENCV;
+        return backendId == DNN_BACKEND_OPENCV ||
+               (backendId == DNN_BACKEND_CUDA && const_B && !trans_a) ||
+               backendId == DNN_BACKEND_CANN ||
+               backendId == DNN_BACKEND_INFERENCE_ENGINE_NGRAPH ||
+               (backendId == DNN_BACKEND_VKCOM && haveVulkan() && !have_bias && !trans_a);
     }
 
     // TODO: replace with cv::Mat.broadcast() once https://github.com/opencv/opencv/pull/23965 is merged.
@@ -103,6 +112,8 @@ public:
         // Check whether A and B are two dimensional
         const auto shape_A = inputs[0];
         const auto shape_B = const_B ? shape(blobs[0]) : inputs[1];
+        CV_CheckEQ(shape_A.size(), static_cast<size_t>(2), "DNN/Gemm: Tensor A must be two dimensional");
+        CV_CheckEQ(shape_B.size(), static_cast<size_t>(2), "DNN/Gemm: Tensor B must be two dimensional");
 
         // Check legal matrix multiplication
         int ma = shape_A[0], na = shape_A[1];
@@ -180,6 +191,129 @@ public:
             fast_gemm(trans_a, trans_b, alpha, A, inputs[1], beta, Y);
         }
     }
+
+#ifdef HAVE_CUDA
+    // Y = A * B + C. A and B should be guaranteed as two dimensional.
+    Ptr<BackendNode> initCUDA(void *context_,
+                              const std::vector<Ptr<BackendWrapper>>& inputs,
+                              const std::vector<Ptr<BackendWrapper>>& outputs) CV_OVERRIDE {
+        CV_CheckFalse(trans_A, "DNN/Gemm/Cuda: does not support transA");
+        CV_CheckTure(const_B, "DNN/Gemm/Cuda: input B (weight) is required to be constant");
+        auto context = reinterpret_cast<csl::CSLContext*>(context_);
+        auto B = blobs[0];
+        auto C = have_bias && const_C ? blobs[1] : Mat();
+
+        if (!trans_b)
+            cv::transpose(B, B);
+        return make_cuda_node<cuda4dnn::InnerProductOp>(preferableTarget, std::move(context->stream), std::move(context->cublas_handle), 0, B, C);
+    }
+#endif // HAVE_CUDA
+
+#ifdef HAVE_CANN
+    // Y = A * B + C.
+    virtual Ptr<BackendNode> initCann(const std::vector<Ptr<BackendWrapper> > &inputs,
+                                      const std::vector<Ptr<BackendWrapper> > &outputs,
+                                      const std::vector<Ptr<BackendNode> >& nodes) CV_OVERRIDE {
+        auto x1 = inputs[0].dynamicCast<CannBackendWrapper>();
+        auto desc_x1 = x1->getTensorDesc();
+        auto op_x1 = nodes[0].dynamicCast<CannBackendNode>()->getOp();
+
+        auto op = std::make_shared<ge::op::MatMulV2>(name);
+
+        // set attributes
+        op->set_attr_transpose_x1(trans_a);
+        op->set_attr_transpose_x2(trans_b);
+
+        // set inputs
+        // set inputs : x1
+        op->set_input_x1_by_name(*op_x1, x1->name.c_str());
+        op->update_input_desc_x1(*desc_x1);
+        // set inputs : x2
+        if (const_B) {
+            auto B = blobs[0];
+            auto op_const_B = std::make_shared<CannConstOp>(B.data, B.type(), shape(B), cv::format("%s_w", name.c_str()));
+            op->set_input_x2_by_name(*(op_const_B->getOp()), "y");
+            op->update_input_desc_x2(*(op_const_B->getTensorDesc()));
+        } else {
+            CV_CheckGE(inputs.size(), static_cast<size_t>(2), "DNN/Gemm/CANN: input B is required since it is not constant");
+            CV_CheckGE(nodes.size(), static_cast<size_t>(2), "DNN/Gemm/CANN: input B is required since it is not constant");
+            auto op_x2 = nodes[1].dynamicCast<CannBackendNode>()->getOp();
+            auto desc_x2 = inputs[1].dynamicCast<CannBackendWrapper>()->getTensorDesc();
+            op->set_input_x2_by_name(*op_x2, "y");
+            op->update_input_desc_x2(*desc_x2);
+        }
+        // set inputs : bias
+        auto mat_C = have_bias && const_C ? blobs.back() : Mat::zeros(1, 1, CV_32F);
+        auto op_const_C = std::make_shared<CannConstOp>(mat_C.data, mat_C.type(), shape(mat_C), cv::format("%s_b", name.c_str()));
+        op->set_input_bias(*(op_const_C->getOp()));
+        op->update_input_desc_bias(*(op_const_C->getTensorDesc()));
+
+        // set outputs
+        op->update_output_desc_y(*output_desc);
+        return Ptr<BackendNode>(new CannBackendNode(op));
+    }
+#endif // HAVE_CANN
+
+#ifdef HAVE_DNN_NGRAPH
+    virtual Ptr<BackendNode> initNgraph(const std::vector<Ptr<BackendWrapper> >& inputs,
+                                        const std::vector<Ptr<BackendNode> >& nodes) CV_OVERRIDE
+    {
+        auto& ieInpNode = nodes[0].dynamicCast<InfEngineNgraphNode>()->node;
+        std::shared_ptr<ngraph::Node> matmul;
+        int axis = -2;
+
+        if (nodes.size() == 2)
+        {
+            auto& inp2 = nodes[1].dynamicCast<InfEngineNgraphNode>()->node;
+            matmul = std::make_shared<ngraph::op::MatMul>(ieInpNode, inp2, transA, transB);
+        }
+        else
+        {
+            std::vector<int> shape(1 + normalize_axis(axis, ieInpNode->get_shape().size()), 0);
+            shape[shape.size() - 1] = -1;
+            auto inp = std::make_shared<ngraph::op::v1::Reshape>(
+                ieInpNode,
+                std::make_shared<ngraph::op::Constant>(ngraph::element::i32, ngraph::Shape{shape.size()}, shape.data()),
+                true
+            );
+
+            std::vector<size_t> weight_shape{(size_t)blobs[0].size[0], (size_t)blobs[0].size[1]};
+            auto ieWeights = std::make_shared<ngraph::op::Constant>(ngraph::element::f32, weight_shape, blobs[0].data);
+            matmul = std::make_shared<ngraph::op::MatMul>(inp, ieWeights, transA, transB);
+        }
+
+        if (have_bias && const_C) {
+            auto bias_node = std::make_shared<ngraph::op::Constant>(ngraph::element::f32,
+                                              ngraph::Shape{(size_t)blobs.back().size[1]}, blobs.back().data);
+            matmul = std::make_shared<ngraph::op::v1::Add>(matmul, bias_node, ngraph::op::AutoBroadcastType::NUMPY);
+        }
+        return Ptr<BackendNode>(new InfEngineNgraphNode(matmul));
+    }
+#endif // HAVE_DNN_NGRAPH
+
+#ifdef HAVE_VULKAN
+    // Y = A * B + C. Currently support 2d matrix multiplication without bias.
+    virtual Ptr<BackendNode> initVkCom(const std::vector<Ptr<BackendWrapper> > &inputs,
+                                       std::vector<Ptr<BackendWrapper> > &outputs) CV_OVERRIDE
+    {
+        // does not support with bias; only 2d matmul
+        auto wrapper_Y = outputs[0].dynamicCast<VkComBackendWrapper>();
+        auto shape_Y = shape(*(wrapper_Y->getMat()));
+        if (have_bias || shape_Y.size() > static_cast<size_t>(2)) {
+            return Ptr<BackendNode>();
+        }
+
+        std::vector<Mat> vkBlobs;
+        if (const_B) {
+            vkBlobs.push_back(blobs[0]);
+        }
+
+        auto wrapper_A = inputs[0].dynamicCast<VkComBackendWrapper>();
+        auto shape_A = shape(*wrapper_A->getMat());
+        Ptr<vkcom::OpBase> op = (new vkcom::OpMatMul(vkBlobs, shape_A[0], shape_A[1], shape_Y[1]));
+        return Ptr<BackendNode>(new VkComBackendNode(inputs, op, outputs));
+    }
+#endif
 
 private:
     bool const_B;
