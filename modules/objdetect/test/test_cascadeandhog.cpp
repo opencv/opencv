ca459ab0fef855355376db4ce1ec39b3a06ec64b
Yuriy Chernyshov <thegeorg@yandex-team.com>
2023-09-06 13:45:28 +0300

    Add missing std namespace qualifiers

--- modules/objdetect/test/test_cascadeandhog.cpp
+++ modules/objdetect/test/test_cascadeandhog.cpp
@@ -355,7 +355,7 @@ int CV_DetectorTest::validate( int detectorIdx, vector<vector<Rect> >& objects )
                     map[minIdx] = 1;
             }
         }
-        noPair += (int)count_if( map.begin(), map.end(), isZero );
+        noPair += (int)std::count_if( map.begin(), map.end(), isZero );
         totalNoPair += noPair;
 
         /*if( noPair > cvRound(valRects.size()*eps.noPair)+1 )
