7cd221364e4b97d4a60ef1ad7b38d67407deaf50
Dmitry Kurtaev <dmitry.kurtaev@gmail.com>
2023-09-14 12:31:21 +0300

    Correct fallback to OpenCV for OpenVINO < 2023

--- modules/dnn/src/net_impl_backend.cpp
+++ modules/dnn/src/net_impl_backend.cpp
@@ -177,9 +177,9 @@ void Net::Impl::setPreferableBackend(Net& net, int backendId)
         backendId = DNN_BACKEND_OPENCV;
     }
 #ifdef HAVE_DNN_NGRAPH
-    if (backendId == DNN_BACKEND_INFERENCE_ENGINE_NGRAPH && INF_ENGINE_VER_MAJOR_LT(INF_ENGINE_RELEASE_2023_0))
+    if (netWasQuantized && backendId == DNN_BACKEND_INFERENCE_ENGINE_NGRAPH && INF_ENGINE_VER_MAJOR_LT(INF_ENGINE_RELEASE_2023_0))
     {
-        CV_LOG_WARNING(NULL, "DNN: OpenVINO 2023.0 and higher supports quantized networks");
+        CV_LOG_WARNING(NULL, "DNN: OpenVINO 2023.0 and higher is required to supports quantized networks");
         backendId = DNN_BACKEND_OPENCV;
     }
 #endif
