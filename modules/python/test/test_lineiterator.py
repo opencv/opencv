20c0e39722e21f9d83c8efab07475e69a8a02772
unknown <laurent.berger@univ-lemans.fr>
2023-09-17 19:53:47 +0200

    update test

--- modules/python/test/test_lineiterator.py
+++ modules/python/test/test_lineiterator.py
@@ -19,10 +19,18 @@ class lineiterator_test(NewOpenCVTests):
         p2 = (100, 100)
         d1 = cv.LineIterator(p1, p2)
         self.assertEqual(d1.pos(), p1)
-        self.assertEqual(d1.next(), (1.0, 1.0))
+        self.assertEqual(d1.next(), True)
+        self.assertEqual(d1.pos(), (1.0, 1.0))
         d2 = cv.LineIterator(p2, p1)
         self.assertEqual(d2.pos(), p2)
-        self.assertEqual(d2.next(), (99.0, 99.0))
+        self.assertEqual(d2.next(), True)
+        self.assertEqual(d2.pos(), (99.0, 99.0))
+        count = 0
+        for p in cv.LineIterator(p1, p2):
+            count = count + 1
+            if count == 10:
+                self.assertEqual(p.pos(), (10.0, 10.0))
+                break
 
 if __name__ == '__main__':
     NewOpenCVTests.bootstrap()
