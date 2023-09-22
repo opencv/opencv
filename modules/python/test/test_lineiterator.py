8537aa4c15801567495ece628319bc3cbf316afa
unknown <laurent.berger@univ-lemans.fr>
2023-09-22 17:24:37 +0200

    empty test

--- modules/python/test/test_lineiterator.py
+++ modules/python/test/test_lineiterator.py
@@ -15,27 +15,7 @@ class lineiterator_test(NewOpenCVTests):
         orig = 0
         p1 = (orig, orig)
         p2 = (orig + n, orig + n)
-        d1 = cv.LineIterator(p1, p2)
-        self.assertEqual(d1.count, n + 1)
-        count = 0
-        for p in d1:
-            self.assertEqual(p, (count, count))
-            count = count + 1
-        self.assertEqual(d1.count, d1.iter)
-        count = 0
-        d1 = iter(cv.LineIterator(p1, p2))
-        d2 = iter(cv.LineIterator(p2, p1))
-        for p1, p2 in zip(d1, d2):
-            self.assertEqual(p1, (orig + count, orig + count))
-            self.assertEqual(p2, (orig + d2.count - count - 1,
-                                  orig + d2.count - count - 1))
-            count = count + 1
-        self.assertEqual(d1.count, d1.iter)
-        d2 = iter(cv.LineIterator(p1, p2))
-        count = 0
-        for p in d2:
-            count = count + 1
-        self.assertEqual(d1.count, d2.iter)
+        cv.norm(p1 - p2, cv.NORM_L2)
 
 
 if __name__ == '__main__':
