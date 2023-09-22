5f812cbbdbc141a7e23f9c31fea81fa0fc74d763
unknown <laurent.berger@univ-lemans.fr>
2023-09-22 21:31:51 +0200

    python 2

--- modules/python/test/test_lineiterator.py
+++ modules/python/test/test_lineiterator.py
@@ -16,22 +16,22 @@ class lineiterator_test(NewOpenCVTests):
         p1 = (orig, orig)
         p2 = (orig + n, orig + n)
         d1 = cv.LineIterator(p1, p2)
+        self.assertEqual(d1.count, n + 1)
         if sys.version_info[0] >= 3:
-            self.assertEqual(d1.count, n + 1)
             count = 0
             for p in d1:
                 self.assertEqual(p, (count, count))
                 count = count + 1
             self.assertEqual(d1.count, d1.iter)
-        count = 0
-        d1 = iter(cv.LineIterator(p1, p2))
-        d2 = iter(cv.LineIterator(p2, p1))
-        for p1, p2 in zip(d1, d2):
-            self.assertEqual(p1, (orig + count, orig + count))
-            self.assertEqual(p2, (orig + d2.count - count - 1,
-                                  orig + d2.count - count - 1))
-            count = count + 1
-        self.assertEqual(d1.count, d1.iter)
+            count = 0
+            d1 = iter(cv.LineIterator(p1, p2))
+            d2 = iter(cv.LineIterator(p2, p1))
+            for p1, p2 in zip(d1, d2):
+                self.assertEqual(p1, (orig + count, orig + count))
+                self.assertEqual(p2, (orig + d2.count - count - 1,
+                                      orig + d2.count - count - 1))
+                count = count + 1
+            self.assertEqual(d1.count, d1.iter)
 
 
 if __name__ == '__main__':
