305948d3bc483082c825e173dd72a9f9fe1ab3c8
unknown <laurent.berger@univ-lemans.fr>
2023-09-22 18:51:48 +0200

    one more line

--- modules/python/test/test_lineiterator.py
+++ modules/python/test/test_lineiterator.py
@@ -3,7 +3,6 @@
 # Python 2/3 compatibility
 from __future__ import print_function
 
-import numpy as np
 import cv2 as cv
 
 from tests_common import NewOpenCVTests
@@ -17,7 +16,7 @@ class lineiterator_test(NewOpenCVTests):
         p1 = (orig, orig)
         p2 = (orig + n, orig + n)
         d1 = cv.LineIterator(p1, p2)
-
+        self.assertEqual(d1.next(), True)
 
 if __name__ == '__main__':
     NewOpenCVTests.bootstrap()
