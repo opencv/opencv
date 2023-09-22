1296f36d0e9a050c9a258517c4cff191bf81abe0
unknown <laurent.berger@univ-lemans.fr>
2023-09-22 20:27:50 +0200

    4 more lines

--- modules/python/test/test_lineiterator.py
+++ modules/python/test/test_lineiterator.py
@@ -16,7 +16,12 @@ class lineiterator_test(NewOpenCVTests):
         p1 = (orig, orig)
         p2 = (orig + n, orig + n)
         d1 = cv.LineIterator(p1, p2)
-        self.assertEqual(d1.next(), True)
+        self.assertEqual(d1.count, n + 1)
+        count = 0
+        for p in d1:
+            self.assertEqual(p, (count, count))
+            count = count + 1
+        self.assertEqual(d1.count, d1.iter)
 
 if __name__ == '__main__':
     NewOpenCVTests.bootstrap()
