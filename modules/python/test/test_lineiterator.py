5108773159b8d0329d95a0cba27f92fea0e55c57
unknown <laurent.berger@univ-lemans.fr>
2023-09-22 21:00:25 +0200

    disable python2 sys.version_info

--- modules/python/test/test_lineiterator.py
+++ modules/python/test/test_lineiterator.py
@@ -2,7 +2,7 @@
 
 # Python 2/3 compatibility
 from __future__ import print_function
-
+import sys
 import cv2 as cv
 
 from tests_common import NewOpenCVTests
@@ -16,12 +16,14 @@ class lineiterator_test(NewOpenCVTests):
         p1 = (orig, orig)
         p2 = (orig + n, orig + n)
         d1 = cv.LineIterator(p1, p2)
-        self.assertEqual(d1.count, n + 1)
-        count = 0
-        for p in d1:
-            self.assertEqual(p, (count, count))
-            count = count + 1
-        self.assertEqual(d1.count, d1.iter)
+        if sys.version_info[0] >= 3:
+            self.assertEqual(d1.count, n + 1)
+            count = 0
+            for p in d1:
+                self.assertEqual(p, (count, count))
+                count = count + 1
+            self.assertEqual(d1.count, d1.iter)
+
 
 if __name__ == '__main__':
     NewOpenCVTests.bootstrap()
