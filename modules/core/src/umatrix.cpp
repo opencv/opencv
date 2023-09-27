c70cae949c230c7b075573cd5f67d7ab7379304d
kallaballa <amir@viel-zu.com>
2023-09-27 09:40:00 +0200

    use the right comparision to determine matching contexts

--- modules/core/src/umatrix.cpp
+++ modules/core/src/umatrix.cpp
@@ -1171,7 +1171,9 @@ void UMat::copyTo(OutputArray _dst) const
         if( u == dst.u && dst.offset == offset )
             return;
 
-        if (u->allocatorContext == dst.u->allocatorContext && u->currAllocator == dst.u->currAllocator)
+        std::shared_ptr<ocl::OpenCLExecutionContext> pExecCtxSrc = std::static_pointer_cast<ocl::OpenCLExecutionContext>(u->allocatorContext);
+        std::shared_ptr<ocl::OpenCLExecutionContext> pExecCtxDst = std::static_pointer_cast<ocl::OpenCLExecutionContext>(dst.u->allocatorContext);
+        if (pExecCtxSrc->getContext().ptr() == pExecCtxDst->getContext().ptr() && u->currAllocator == dst.u->currAllocator)
         {
             dst.ndoffset(dstofs);
             dstofs[dims-1] *= esz;
