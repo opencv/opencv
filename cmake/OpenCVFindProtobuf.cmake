41e8e2006726241ef3be02a52d36dee7e6586409
Kumataro <Kumataro@users.noreply.github.com>
2023-10-07 10:11:25 +0900

    3rdparty: supporting protobuf v22 and later

--- cmake/OpenCVFindProtobuf.cmake
+++ cmake/OpenCVFindProtobuf.cmake
@@ -67,6 +67,38 @@ else()
   endif()
 endif()
 
+# See https://github.com/opencv/opencv/issues/24369
+# In Protocol Buffers v22.0 and later drops C++11 support and depends abseil-cpp.
+#   Details: https://protobuf.dev/news/2022-08-03/
+# And if std::text_view is in abseil-cpp requests C++17 and later.
+if(HAVE_PROTOBUF)
+    if("${Protobuf_VERSION}" MATCHES [[[0-9]+.([0-9]+).[0-9]+]])
+        string(COMPARE GREATER_EQUAL "${CMAKE_MATCH_1}" "22" REQUEST_ABSL)
+
+        if(REQUEST_ABSL)
+            string(COMPARE GREATER_EQUAL "${CMAKE_CXX_STANDARD}" "17" USED_AFTER_CXX17)
+            if(NOT USED_AFTER_CXX17)
+                message("CMAKE_CXX_STANDARD : ${CMAKE_CXX_STANDARD}")
+                message("protobuf           : ${Protobuf_VERSION}")
+                message(FATAL_ERROR "protobuf(v22 and later) and abseil-cpp request CMAKE_CXX_STANDARD=17 and later.")
+            endif()
+
+            ocv_check_modules(ABSL_STRINGS absl_strings)
+            if(NOT ABSL_STRINGS_FOUND)
+                message(FATAL_ERROR "protobuf(v22 and later) requests abseil-cpp(strings), but missing.")
+            endif()
+
+            ocv_check_modules(ABSL_LOG absl_log)
+            if(NOT ABSL_LOG_FOUND)
+                message(FATAL_ERROR "protobuf(v22 and later) requests abseil-cpp(log), but missing.")
+            endif()
+
+        endif()
+    else()
+        message(FATAL_ERROR "Protobuf version(${Protobuf_VERSION}) is unexpected to split.")
+    endif()
+endif()
+
 if(HAVE_PROTOBUF AND PROTOBUF_UPDATE_FILES AND NOT COMMAND PROTOBUF_GENERATE_CPP)
   message(FATAL_ERROR "Can't configure protobuf dependency (BUILD_PROTOBUF=${BUILD_PROTOBUF} PROTOBUF_UPDATE_FILES=${PROTOBUF_UPDATE_FILES})")
 endif()
@@ -88,4 +120,9 @@ if(HAVE_PROTOBUF)
   list(APPEND CUSTOM_STATUS_protobuf "    Protobuf:"
     BUILD_PROTOBUF THEN "build (${Protobuf_VERSION})"
     ELSE "${__location} (${Protobuf_VERSION})")
+endif()
+
+if(HAVE_ABSL_STRINGS AND HAVE_ABSL_LOG)
+  list(APPEND CUSTOM_STATUS absl)
+  list(APPEND CUSTOM_STATUS_absl "      abseil-cpp:" "YES (${ABSL_STRINGS_VERSION})" )
 endif()
