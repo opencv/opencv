522b48c1649bc6893a72a6298c76ddbd838ac6dd
Yuantao Feng <yuantao.feng@opencv.org.cn>
2023-10-13 03:56:10 -0500

    Merge pull request #1107 from fengyuentau:fix_dtype_nary_eltwise
    Merge with opencv/opencv#24386
    
    Also noted that random seed is added in this pr, so previous data is regenerated to keep consistency.

--- testdata/dnn/onnx/generate_onnx_models_with_onnxscript.py
+++ testdata/dnn/onnx/generate_onnx_models_with_onnxscript.py
@@ -4,11 +4,13 @@ import onnx
 import onnxscript as ost
 from onnxscript import opset19 as op # opset19 is the lastest by 202309
 
+np.random.seed(0)
+
 def make_model_and_data(model, *args, **kwargs):
     name = model._name
 
     # TODO: support multiple outputs
-    output = model(*args, **kwargs) # eager mode
+    output = model(*args) # eager mode
 
     # Save model
     model_proto = model.to_model_proto()
@@ -24,13 +26,20 @@ def make_model_and_data(model, *args, **kwargs):
         onnx.save(model_proto_, save_path)
 
     # Save inputs and output
+    inputs = args
+    if "force_saving_input_as_dtype_float32" in kwargs and kwargs["force_saving_input_as_dtype_float32"]:
+        inputs = []
+        for input in args:
+            inputs.append(input.astype(np.float32))
     if len(args) == 1:
         input_file = os.path.join("data", "input_" + name)
-        np.save(input_file, args[0])
+        np.save(input_file, inputs[0])
     else:
-        for idx, input in enumerate(args, start=0):
+        for idx, input in enumerate(inputs, start=0):
             input_files = os.path.join("data", "input_" + name + "_" + str(index))
             np.save(input_files, input)
+    if "force_saving_output_as_dtype_float32" in kwargs and kwargs["force_saving_output_as_dtype_float32"]:
+        output = output.astype(np.float32)
     output_files = os.path.join("data", "output_" + name)
     np.save(output_files, output)
 
@@ -48,3 +57,14 @@ def gather_shared_indices(x: ost.FLOAT[2, 1, 3, 4]) -> ost.FLOAT[3, 4]:
     y1 = op.Gather(y0, indices, axis=0)
     return y1
 make_model_and_data(gather_shared_indices, np.random.rand(2, 1, 3, 4).astype(np.float32))
+
+'''
+    [Input] -> Greater(B=61) -> [Output]
+                        \
+                        dtype=np.int64
+'''
+@ost.script()
+def greater_input_dtype_int64(x: ost.FLOAT[27, 9]) ->ost.BOOL[27, 9]:
+    y = op.Greater(x, op.Constant(value=onnx.helper.make_tensor("", onnx.TensorProto.INT64, [], np.array([61], dtype=np.int64))))
+    return y
+make_model_and_data(greater_input_dtype_int64, np.random.randint(0, 100, size=[27, 9], dtype=np.int64), force_saving_input_as_dtype_float32=True, force_saving_output_as_dtype_float32=True)
