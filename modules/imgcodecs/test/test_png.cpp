4d17d23fb4c98399e3ccb3d16e8639bd5caa3fd3
thewoz <leonardo.parisi@gmail.com>
2023-10-20 14:52:02 +0200

    Update test_png.cpp

--- modules/imgcodecs/test/test_png.cpp
+++ modules/imgcodecs/test/test_png.cpp
@@ -11,8 +11,8 @@ TEST(imRead, passing_Mat)
 {
   Mat img = Mat::zeros(1000, 1000, CV_8UC3);
   Mat subImg = img.rowRange(0, 512).colRange(0, 512);
-  imread("../cv/shared/lena.png", subImg);
-  Mat ori = imread("../cv/shared/lena.png");
+  imread(samples::findFile("lena.png"), subImg);
+  Mat ori = imread(samples::findFile("lena.png"));
   EXPECT_EQ(cv::norm(ori, subImg, NORM_INF), 0)
   EXPECT_EQ(0, sum(img.rowRange(0, 1000).colRange(512, 1000))[0]);
   EXPECT_EQ(0, sum(img.rowRange(0, 512).colRange(0, 512))[0]);
