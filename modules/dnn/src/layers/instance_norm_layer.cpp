5edd13c1a0e0c4d47841ae5707e255d295ef9dd1
fengyuentau <yuantao.feng@opencv.org.cn>
2023-11-02 12:19:40 +0800

    add openvino backend impl

--- modules/dnn/src/layers/instance_norm_layer.cpp
+++ modules/dnn/src/layers/instance_norm_layer.cpp
@@ -18,6 +18,10 @@ public:
     }
 
     virtual bool supportBackend(int backendId) CV_OVERRIDE {
+#ifdef HAVE_INF_ENGINE
+        if (backendId == DNN_BACKEND_INFERENCE_ENGINE_NGRAPH)
+            return true;
+#endif
         return backendId == DNN_BACKEND_OPENCV;
     }
 
@@ -60,6 +64,41 @@ public:
 
         fastNormChannel(input, scale, bias, outputs[0], epsilon);
     }
+
+#ifdef HAVE_DNN_NGRAPH
+    virtual Ptr<BackendNode> initNgraph(const std::vector<Ptr<BackendWrapper> >& inputs,
+                                        const std::vector<Ptr<BackendNode> >& nodes) CV_OVERRIDE {
+        // onnx to openvino convertion: https://github.com/openvinotoolkit/openvino/blob/2023.1.0/src/frontends/onnx/frontend/src/op/instance_norm.cpp
+
+        auto &ieInpNode = nodes[0].dynamicCast<InfEngineNgraphNode>()->node;
+        const auto &input_shape = ieInpNode.get_shape();
+        std::shared_ptr<ngraph::Node> mvn, result;
+
+        // mvn
+#if INF_ENGINE_VER_MAJOR_LE(INF_ENGINE_RELEASE_2021_2)
+        // https://docs.openvino.ai/2021.4/api/ngraph_python_api/_autosummary/ngraph.opset3.mvn.html?highlight=mvn#ngraph.opset3.mvn
+        bool across_channels = false;
+        bool normalize_variance = true;
+        auto mvn = std::make_shared<ngraph::op::MVN>(ieInpNode, across_channels, normalize_variance, epsilon);
+#else
+        // https://docs.openvino.ai/2023.1/openvino_docs_ops_normalization_MVN_6.html
+        std::vector<int64_t> axes_v(input_shape.size() - 2);
+        std::iota(axes_v.begin(), axes_v.end(), 2); // {2, 3, ...} for nd input tensor, n>=3
+        auto axes = std::make_shared<ngraph::op::Constant>(ngraph::element::i64, ngraph::Shape{axes_v.size()}, axes_v.data());
+        bool normalize_variance = true;
+        auto mvn = std::make_shared<ngraph::op::v6::MVN>(ieInpNode, axes, normalize_variance, eps, ngraph::op::MVNEpsMode::INSIDE_SQRT);
+#endif
+
+        // instance norm = scale * mvn + bias
+        auto &scale = nodes[1].dynamicCast<InfEngineNgraphNode>()->node; // WARNING: possible size mismatch, for example [C, 1]
+        result = std::make_shared<ngraph::op::v1::Multiply>(mvn, scale);
+        auto &bias = nodes[2].dynamicCast<InfEngineNgraphNode>()->node; // WARNING: possible size mismatch, for example [C, 1]
+        result = std::make_shared<ngraph::op::v1::Add>(result, bias);
+
+        return Ptr<BackendNode>(new InfEngineNgraphNode(result));
+    }
+#endif // HAVE_DNN_NGRAPH
+
 };
 
 Ptr<InstanceNormLayer> InstanceNormLayer::create(const LayerParams &params) {
