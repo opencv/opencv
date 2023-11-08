fb6fd98cd63b26dd62ac98c98fd276eec9853504
Rostislav Vasilikhin <rostislav.vasilikhin@opencv.ai>
2023-11-09 00:34:24 +0100

    3 demos joined in one, in progress

--- samples/opengl/opengl_clipping_sample.cpp
+++ samples/opengl/opengl_clipping_sample.cpp
@@ -16,15 +16,12 @@
 
 #include "opencv2/core.hpp"
 #include "opencv2/core/opengl.hpp"
-#include "opencv2/core/cuda.hpp"
+#include "opencv2/imgproc.hpp"
 #include "opencv2/highgui.hpp"
 
 using namespace std;
 using namespace cv;
 using namespace cv::cuda;
-
-const int win_width = 700;
-const int win_height = 700;
 
 struct DrawData
 {
@@ -38,26 +35,75 @@ void draw(void* userdata)
 {
     DrawData* data = static_cast<DrawData*>(userdata);
 
+    glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
     ogl::render(data->arr, data->indices, ogl::TRIANGLES);
 }
 
-int main()
+int main(int argc, char* argv[])
 {
+    int win_width = 700;
+    int win_height = 700;
+
+    if (argc != 4)
+    {
+        std::cout << "Wrong input for the demo. please input again" << std::endl;
+        return 0;
+    }
+    std::string testMode = argv[1];
+    win_width = std::atoi(argv[2]), win_height = std::atoi(argv[3]);
+
     namedWindow("OpenGL", WINDOW_OPENGL);
     resizeWindow("OpenGL", win_width, win_height);
 
-    Mat_<Vec3f> vertex(1, 9);
-    vertex << Vec3f(2.0, 0, -2.0), Vec3f(0, -6, -2), Vec3f(-2, 0, -2),
-              Vec3f(3.5, -1, -5),  Vec3f(2.5, -2.5, -5), Vec3f(-1, 1, -5),
-              Vec3f(-6.5, -1, -3), Vec3f(-2.5, -2, -3), Vec3f(1, 1, -5);
-
-    Mat_<int> indices(1, 9);
-    indices << 0, 1, 2, 3, 4, 5, 6, 7, 8;
-
-    Mat_<Vec4f> colors(1, 9);
-    colors << Vec4f(0.725f, 0.933f, 0.851f, 1.0f), Vec4f(0.725f, 0.933f, 0.851f, 1.0f), Vec4f(0.725f, 0.933f, 0.851f, 1.0f),
+    Mat_<Vec3f> vertex;
+    Mat_<Vec4f> colors;
+    Mat_<int> indices;
+    double fovy = 0.0;
+    std::string fname;
+    if (clipping)
+    {
+        vertex << Vec3f(2.0, 0, -2.0), Vec3f(0, -6, -2), Vec3f(-2, 0, -2),
+                Vec3f(3.5, -1, -5),  Vec3f(2.5, -2.5, -5), Vec3f(-1, 1, -5),
+                Vec3f(-6.5, -1, -3), Vec3f(-2.5, -2, -3), Vec3f(1, 1, -5);
+        colors << Vec4f(0.725f, 0.933f, 0.851f, 1.0f), Vec4f(0.725f, 0.933f, 0.851f, 1.0f), Vec4f(0.725f, 0.933f, 0.851f, 1.0f),
         Vec4f(0.933f, 0.851f, 0.725f, 1.0f), Vec4f(0.933f, 0.851f, 0.725f, 1.0f), Vec4f(0.933f, 0.851f, 0.725f, 1.0f),
         Vec4f(0.933f, 0.039f, 0.588f, 1.0f), Vec4f(0.933f, 0.039f, 0.588f, 1.0f), Vec4f(0.933f, 0.039f, 0.588f, 1.0f);
+
+        indices << 0, 1, 2, 3, 4, 5, 6, 7, 8;
+        fovy = 45.0;
+        fname = "example_image_clipping.png";
+    }
+    else if (color)
+    {
+        vertex << Vec3f(2.0, 0, -2.0), Vec3f(0, 2, -3),
+                Vec3f(-2, 0, -2), Vec3f(0, -2, 1.0);
+        colors << Vec3f(1.0f, 0.0f, 0.0f), Vec3f(0.0f, 1.0f, 0.0f), Vec3f(0.0f, 0.0f, 1.0f), Vec3f(0.0f, 1.0f, 0.0f);
+
+        indices << 0, 1, 2, 0, 2, 3;
+
+        fovy = 60.0;
+        fname = "example_image_color.png";
+    }
+    else if (depth)
+    {
+        vertex << Vec3f(2.0, 0, -2.0), Vec3f(0, -2, -2),
+                  Vec3f(-2, 0, -2), Vec3f(3.5, -1, -5),
+                  Vec3f(2.5, -1.5, -5), Vec3f(-1, 0.5, -5);
+
+        indices << 0, 1, 2, 3, 4, 5;
+
+        colors << Vec4f(0.851f, 0.933f, 0.725f, 1.0f), Vec4f(0.851f, 0.933f, 0.725f, 1.0f),
+                  Vec4f(0.851f, 0.933f, 0.725f, 1.0f), Vec4f(0.725f, 0.851f, 0.933f, 1.0f),
+                  Vec4f(0.725f, 0.851f, 0.933f, 1.0f), Vec4f(0.725f, 0.851f, 0.933f, 1.0f);
+
+        fovy = 45.0;
+        fname = "example_image_depth.png";
+    }
+    else
+    {
+        std::cout << "Wrong mode: " << mode << std::endl;
+        return -1;
+    }
 
     DrawData data;
 
@@ -65,9 +111,10 @@ int main()
     data.arr.setColorArray(colors);
     data.indices.copyFrom(indices);
 
+    float zNear = 0.1, zFar = 50;
     glMatrixMode(GL_PROJECTION);
     glLoadIdentity();
-    gluPerspective(45.0, (double)win_width / win_height, 0.1, 50);
+    gluPerspective(fovy, (double)win_width / win_height, zNear, zFar);
 
     glMatrixMode(GL_MODELVIEW);
     glLoadIdentity();
@@ -80,13 +127,27 @@ int main()
 
     for (;;)
     {
+        updateWindow("OpenGL");
+
         std::vector<uint8_t> pixels(win_width * win_height * 3);
         glReadPixels(0, 0, win_width, win_height, GL_RGB, GL_UNSIGNED_BYTE, pixels.data());
         cv::Mat image(win_height, win_width, CV_8UC3, pixels.data());
+        cv::cvtColor(image, image, cv::COLOR_RGB2BGR);
         cv::flip(image, image, 0);
+        cv::imwrite(fname, image);
 
-        cv::imwrite("example_image_clipping.png", image);
-        updateWindow("OpenGL");
+        if(depth)
+        {
+            cv::Mat depthMat(win_height, win_width, CV_32F);
+            glReadPixels(0, 0, win_width, win_height, GL_DEPTH_COMPONENT, GL_FLOAT, depthMat.ptr());
+            for (auto it = depthMat.begin<float>(); it != depthMat.end<float>(); ++it)
+            {
+                *it = zNear * zFar / ((*it) * (zNear - zFar) + zFar);
+            }
+            cv::flip(depthMat, depthMat, 0);
+            cv::imwrite(depthFname, depthMat);
+        }
+
         char key = (char)waitKey(40);
         if (key == 27)
             break;
