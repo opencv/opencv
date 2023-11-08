3e765638a9c2b20cc9b11611c11355c40c877e43
Alexander Smorkalov <alexander.smorkalov@xperience.ai>
2023-11-08 17:47:16 +0300

    Added legal headers to new fuzzing code.

--- modules/imgproc/fuzz/fuzz_precomp.hpp
+++ modules/imgproc/fuzz/fuzz_precomp.hpp
@@ -1,6 +1,7 @@
 // This file is part of OpenCV project.
 // It is subject to the license terms in the LICENSE file found in the top-level directory
 // of this distribution and at http://opencv.org/license.html.
+
 #ifndef __OPENCV_IMGPROC_FUZZ_PRECOMP_HPP__
 #define __OPENCV_IMGPROC_FUZZ_PRECOMP_HPP__
 
