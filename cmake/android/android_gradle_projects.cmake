bfff1e77b4ee5f4a12368bd8253781ee838eb095
Alexander Smorkalov <alexander.smorkalov@xperience.ai>
2023-11-16 15:59:55 +0300

    Update Gradle again due to https://github.com/gradle/gradle/issues/21116#issuecomment-1228005104 bug.

--- cmake/android/android_gradle_projects.cmake
+++ cmake/android/android_gradle_projects.cmake
@@ -13,7 +13,7 @@ else()
   set(KOTLIN_STD_LIB "" CACHE STRING "Kotlin Standard Library dependency")
 endif()
 
-set(GRADLE_VERSION "7.5.1" CACHE STRING "Gradle version")
+set(GRADLE_VERSION "7.6.3" CACHE STRING "Gradle version")
 message(STATUS "Gradle version: ${GRADLE_VERSION}")
 
 set(ANDROID_COMPILE_SDK_VERSION "26" CACHE STRING "Android compileSdkVersion")
