cf66b3c6a1705fbfe42334d2b19e4debd12bc32d
thewoz <leonardo.parisi@gmail.com>
2023-11-20 21:18:53 +0100

    Update calibinit.cpp

--- modules/calib3d/src/calibinit.cpp
+++ modules/calib3d/src/calibinit.cpp
@@ -508,8 +508,18 @@ bool findChessboardCorners(InputArray image_, Size pattern_size,
     int prev_sqr_size = 0;
 
     Mat thresh_img_new = img.clone();
-    if(!is_plain)
-        icvBinarizationHistogramBased(thresh_img_new); // process image in-place
+  
+    if (flags & CALIB_CB_ADAPTIVE_THRESH)
+    {
+      // Assume that the checkerboard occupies 5% of the image
+      int min_size = cvRound((img.cols * img.rows * 0.05) / ((pattern_size.width+1) * (pattern_size.height+1)));
+      if(min_size%2==0) min_size += 1;
+      adaptiveThreshold(img, thresh_img_new, 255, ADAPTIVE_THRESH_MEAN_C, THRESH_BINARY, min_size, 0);
+    }
+
+    if (!is_plain)
+      icvBinarizationHistogramBased(thresh_img_new); // process image in-place
+    
     SHOW("New binarization", thresh_img_new);
 
     if (flags & CALIB_CB_FAST_CHECK && !is_plain)
@@ -538,7 +548,7 @@ bool findChessboardCorners(InputArray image_, Size pattern_size,
     for (int dilations = min_dilations; dilations <= max_dilations; dilations++)
     {
         //USE BINARY IMAGE COMPUTED USING icvBinarizationHistogramBased METHOD
-        if(!is_plain && dilations > 0)
+        if (!is_plain && dilations > 0)
             dilate( thresh_img_new, thresh_img_new, Mat(), Point(-1, -1), 1 );
 
         // So we can find rectangles that go to the edge, we draw a white line around the image edge.
