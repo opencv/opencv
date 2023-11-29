7dcab412f9665b94b4560b22ffad2671e859c0e4
thewoz <leonardo.parisi@gmail.com>
2023-11-29 16:21:21 +0100

    update py test

--- modules/python/test/test_imread.py
+++ modules/python/test/test_imread.py
@@ -18,14 +18,14 @@ class dft_test(NewOpenCVTests):
  
         img = np.zeros((1000, 1000), dtype=np.uint8)
         subImg = img[64:576, 128:640]
-        subImg = cv2.imread(self.find_file("cv/shared/lena.png"), subImg, cv.IMREAD_GRAYSCALE)
-        ori = cv2.imread(self.find_file("cv/shared/lena.png"), cv.IMREAD_GRAYSCALE)
-        self.assertEqual cv2.norm(ori, subImg, cv2.NORM_INF) == 0
-        self.assertEqual cv2.countNonZero(img[0:1000, 0:128]) == 0
-        self.assertEqual cv2.countNonZero(img[0:1000, 640:1000]) == 0
-        self.assertEqual cv2.countNonZero(img[0:64, 128:640]) == 0
-        self.assertEqual cv2.countNonZero(img[576:1000, 128:640]) == 0
-
+        subImg = cv.imread(self.find_file("cv/shared/lena.png"), subImg, cv.IMREAD_GRAYSCALE)
+        ori = cv.imread(self.find_file("cv/shared/lena.png"), cv.IMREAD_GRAYSCALE)
+        self.assertEqual(cv.norm(ori, subImg, cv2.NORM_INF), 0)
+        self.assertEqual(cv.countNonZero(img[0:1000, 0:128]), 0)
+        self.assertEqual(cv.countNonZero(img[0:1000, 640:1000]), 0)
+        self.assertEqual(cv.countNonZero(img[0:64, 128:640]), 0)
+        self.assertEqual(cv.countNonZero(img[576:1000, 128:640]), 0)
 
 if __name__ == '__main__':
     NewOpenCVTests.bootstrap()
+
