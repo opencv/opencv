a2edf4d929ae22079dd1a301b88593e46e316256
Yuantao Feng <yuantao.feng@opencv.org.cn>
2023-12-06 04:46:24 -0600

    Merge pull request #24647 from fengyuentau:cuda_sub
    dnn cuda: support Sub #24647
    
    Related https://github.com/opencv/opencv/issues/24606#issuecomment-1837390257
    
    ### Pull Request Readiness Checklist
    
    See details at https://github.com/opencv/opencv/wiki/How_to_contribute#making-a-good-pull-request
    
    - [x] I agree to contribute to the project under Apache 2 License.
    - [x] To the best of my knowledge, the proposed patch is not based on a code under GPL or another license that is incompatible with OpenCV
    - [x] The PR is proposed to the proper branch
    - [x] There is a reference to the original bug report and related work
    - [x] There is accuracy test, performance test and test data in opencv_extra repository, if applicable
          Patch to opencv_extra has the same branch name.
    - [x] The feature is well documented and sample code can be built with the project CMake

--- modules/dnn/src/cuda4dnn/primitives/eltwise.hpp
+++ modules/dnn/src/cuda4dnn/primitives/eltwise.hpp
@@ -27,6 +27,7 @@ namespace cv { namespace dnn { namespace cuda4dnn {
         PRODUCT,
         DIV,
         MIN,
+        SUB,
     };
 
     class EltwiseOpBase : public CUDABackendNode {
@@ -88,6 +89,7 @@ namespace cv { namespace dnn { namespace cuda4dnn {
                     else
                         kernels::eltwise_sum_coeff_2<T>(stream, output, coeffs[0], input_x, coeffs[1], input_y);
                     break;
+                case EltwiseOpType::SUB: kernels::eltwise_sub_2<T>(stream, output, input_x, input_y); break;
                 }
             }
             else
@@ -119,6 +121,7 @@ namespace cv { namespace dnn { namespace cuda4dnn {
                             kernels::eltwise_sum_coeff_2<T>(stream, output, coeff_x, output, coeffs[i], input);
                         }
                         break;
+                    case EltwiseOpType::SUB: kernels::eltwise_sub_2<T>(stream, output, output, input); break;
                     }
                 }
             }
