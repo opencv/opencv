cd7cea22b866c5b616966e5e31596e1cad91a7c7
Rostislav Vasilikhin <rostislav.vasilikhin@opencv.ai>
2024-01-06 03:53:57 +0100

    test data generator rewritten

--- samples/opengl/opengl_testdata_generator.cpp
+++ samples/opengl/opengl_testdata_generator.cpp
@@ -24,6 +24,170 @@ using namespace std;
 using namespace cv;
 using namespace cv::cuda;
 
+// model data should be identical to the code from tests
+enum class ModelType
+{
+    Empty = 0,
+    File = 1,
+    Clipping = 2,
+    Color = 3,
+    Centered = 4
+};
+
+enum class ShadingType
+{
+    White = 0,
+    Flat = 1,
+    Shaded = 2
+};
+
+class ModelData
+{
+public:
+    ModelData(ModelType type = ModelType::Empty, std::string objPath = { })
+    {
+        switch (type)
+        {
+        case ModelType::Empty:
+        {
+            position = Vec3f(0.0, 0.0,  0.0);
+            lookat   = Vec3f(0.0, 0.0,  0.0);
+            upVector = Vec3f(0.0, 1.0,  0.0);
+
+            fovy = 45.0;
+
+            vertices = std::vector<Vec3f>(4, {2.0f, 0, -2.0f});
+            colors   = std::vector<Vec3f>(4, {0, 0, 255.0f});
+            indices = { };
+        }
+        break;
+        case ModelType::File:
+        {
+            position = Vec3f( 2.4, 0.7, 1.2);
+            lookat   = Vec3f( 0.0, 0.0, 0.3);
+            upVector = Vec3f( 0.0, 1.0, 0.0);
+
+            fovy = 45.0;
+
+            objectPath = objPath;
+            std::vector<vector<int>> indvec;
+            // using per-vertex normals as colors
+            loadMesh(objectPath, vertices, colors, indvec);
+            if (vertices.size() != colors.size())
+            {
+                std::runtime_error("Model should contain normals for each vertex");
+            }
+            for (const auto &vec : indvec)
+            {
+                indices.push_back({vec[0], vec[1], vec[2]});
+            }
+
+            for (auto &color : colors)
+            {
+                color = Vec3f(abs(color[0]), abs(color[1]), abs(color[2])) * 255.0f;
+            }
+        }
+        break;
+        case ModelType::Clipping:
+        {
+            position = Vec3f(0.0, 0.0, 5.0);
+            lookat   = Vec3f(0.0, 0.0, 0.0);
+            upVector = Vec3f(0.0, 1.0, 0.0);
+
+            fovy = 45.0;
+
+            vertices =
+            {
+                { 2.0,  0.0, -2.0}, { 0.0, -6.0, -2.0}, {-2.0,  0.0, -2.0},
+                { 3.5, -1.0, -5.0}, { 2.5, -2.5, -5.0}, {-1.0,  1.0, -5.0},
+                {-6.5, -1.0, -3.0}, {-2.5, -2.0, -3.0}, { 1.0,  1.0, -5.0},
+            };
+
+            indices = { {0, 1, 2}, {3, 4, 5}, {6, 7, 8} };
+
+            Vec3f col1(217.0, 238.0, 185.0);
+            Vec3f col2(185.0, 217.0, 238.0);
+            Vec3f col3(150.0,  10.0, 238.0);
+            colors =
+            {
+                col1, col2, col3,
+                col2, col3, col1,
+                col3, col1, col2,
+            };
+        }
+        break;
+        case ModelType::Centered:
+        {
+            position = Vec3f(0.0, 0.0, 5.0);
+            lookat   = Vec3f(0.0, 0.0, 0.0);
+            upVector = Vec3f(0.0, 1.0, 0.0);
+
+            fovy = 45.0;
+
+            vertices =
+            {
+                { 2.0,  0.0, -2.0}, { 0.0, -2.0, -2.0}, {-2.0,  0.0, -2.0},
+                { 3.5, -1.0, -5.0}, { 2.5, -1.5, -5.0}, {-1.0,  0.5, -5.0},
+            };
+
+            indices = { {0, 1, 2}, {3, 4, 5} };
+
+            Vec3f col1(217.0, 238.0, 185.0);
+            Vec3f col2(185.0, 217.0, 238.0);
+            colors =
+            {
+                col1, col2, col1,
+                col2, col1, col2,
+            };
+        }
+        break;
+        case ModelType::Color:
+        {
+            position = Vec3f(0.0, 0.0, 5.0);
+            lookat   = Vec3f(0.0, 0.0, 0.0);
+            upVector = Vec3f(0.0, 1.0, 0.0);
+
+            fovy = 60.0;
+
+            vertices =
+            {
+                { 2.0,  0.0, -2.0},
+                { 0.0,  2.0, -3.0},
+                {-2.0,  0.0, -2.0},
+                { 0.0, -2.0,  1.0},
+            };
+
+            indices = { {0, 1, 2}, {0, 2, 3} };
+
+            colors =
+            {
+                {  0.0f,   0.0f, 255.0f},
+                {  0.0f, 255.0f,   0.0f},
+                {255.0f,   0.0f,   0.0f},
+                {  0.0f, 255.0f,   0.0f},
+            };
+        }
+        break;
+
+        default:
+            break;
+        }
+    }
+
+    Vec3f position;
+    Vec3f lookat;
+    Vec3f upVector;
+
+    double fovy;
+
+    std::vector<Vec3f> vertices;
+    std::vector<Vec3i> indices;
+    std::vector<Vec3f> colors;
+
+    string objectPath;
+};
+
+
 struct DrawData
 {
     ogl::Arrays arr;
@@ -40,13 +204,117 @@ void draw(void* userdata)
     ogl::render(data->arr, data->indices, ogl::TRIANGLES);
 }
 
+void generateImage(cv::Size imgSz, ShadingType shadingType, ModelType modelType, std::string modelPath,
+                   cv::Mat& colorImage, cv::Mat& depthImage);
+void generateImage(cv::Size imgSz, ShadingType shadingType, ModelType modelType, std::string modelPath,
+                   cv::Mat& colorImage, cv::Mat& depthImage)
+{
+    namedWindow("OpenGL", WINDOW_OPENGL);
+    resizeWindow("OpenGL", imgSz.width, imgSz.height);
+
+    ModelData modelData(modelType, modelPath);
+
+    DrawData data;
+
+    std::vector<Vec3f> vertices;
+    std::vector<Vec4f> colors4f;
+    std::vector<int> idxLinear;
+
+    if (shadingType == ShadingType::Flat)
+    {
+        // rearrange vertices and colors for flat shading
+        int ctr = 0;
+        for (const auto& idx : modelData.indices)
+        {
+            for (int i = 0; i < 3; i++)
+            {
+                vertices.push_back(modelData.vertices[idx[i]]);
+                idxLinear.push_back(ctr++);
+            }
+
+            Vec3f ci = modelData.colors[idx[0]] * (1.f/255.f);
+            for (int i = 0; i < 3; i++)
+            {
+                colors4f.emplace_back(ci[0], ci[1], ci[2], 1.f);
+            }
+        }
+    }
+    else
+    {
+        vertices = modelData.vertices;
+        for (const auto& c : modelData.colors)
+        {
+            Vec3f ci = (shadingType == ShadingType::Shaded) ? c * (1.f/255.f) : cv::Vec3f::all(1.f);
+            colors4f.emplace_back(ci[0], ci[1], ci[2], 1.0);
+        }
+
+        for (const auto& idx : modelData.indices)
+        {
+            for (int i = 0; i < 3; i++)
+            {
+                idxLinear.push_back(idx[i]);
+            }
+        }
+    }
+
+    data.arr.setVertexArray(vertices);
+    data.arr.setColorArray(colors4f);
+    data.indices.copyFrom(idxLinear);
+
+    float zNear = 0.1, zFar = 50;
+    glMatrixMode(GL_PROJECTION);
+    glLoadIdentity();
+    gluPerspective(modelData.fovy, (double)imgSz.width / imgSz.height, zNear, zFar);
+
+    glMatrixMode(GL_MODELVIEW);
+    glLoadIdentity();
+
+    //gluLookAt(0, 0, 5, 0, 0, 0, 0, 1, 0);
+    gluLookAt(modelData.position[0], modelData.position[1], modelData.position[2],
+              modelData.lookat  [0], modelData.lookat  [1], modelData.lookat  [2],
+              modelData.upVector[0], modelData.upVector[1], modelData.upVector[2]);
+
+    glDisable(GL_CULL_FACE);
+    glEnable(GL_DEPTH_TEST);
+
+    cv::setOpenGlDrawCallback("OpenGL", draw, &data);
+
+    const int framesToSkip = 10;
+    for (int f = 0; f < framesToSkip; f++)
+    {
+        updateWindow("OpenGL");
+
+        colorImage = cv::Mat(imgSz.height, imgSz.width, CV_8UC3);
+        glReadPixels(0, 0, imgSz.width, imgSz.height, GL_RGB, GL_UNSIGNED_BYTE, colorImage.data);
+        cv::cvtColor(colorImage, colorImage, cv::COLOR_RGB2BGR);
+        cv::flip(colorImage, colorImage, 0);
+
+        depthImage = cv::Mat(imgSz.height, imgSz.width, CV_32F);
+        glReadPixels(0, 0, imgSz.width, imgSz.height, GL_DEPTH_COMPONENT, GL_FLOAT, depthImage.data);
+        for (auto it = depthImage.begin<float>(); it != depthImage.end<float>(); ++it)
+        {
+            *it = zNear * zFar / ((*it) * (zNear - zFar) + zFar);
+        }
+        cv::flip(depthImage, depthImage, 0);
+        depthImage.convertTo(depthImage, CV_16U, 1000.0);
+
+        char key = (char)waitKey(40);
+        if (key == 27)
+            break;
+    }
+
+
+    cv::setOpenGlDrawCallback("OpenGL", 0, 0);
+    cv::destroyAllWindows();
+}
+
+
 int main(int argc, char* argv[])
 {
     cv::CommandLineParser parser(argc, argv,
             "{ help h usage ? |      | show this message }"
-            "{ width          | 700  | resulting image width }"
-            "{ height         | 700  | resulting image height }"
-            
+            "{ outPath        |      | output path for generated images }"
+            "{ modelPath      |      | path to 3d model to render }"
     );
     parser.about("This app is used to generate test data for triangleRasterize() function");
 
@@ -56,131 +324,75 @@ int main(int argc, char* argv[])
         return 0;
     }
 
-    parser.get<string>("colorFname");
-    parser.get<string>("depthFname");
-    parser.get<string>("clip");
-    int win_width  = parser.get<int>("width");
-    int win_height = parser.get<int>("height");
-
-    namedWindow("OpenGL", WINDOW_OPENGL);
-    resizeWindow("OpenGL", win_width, win_height);
-
-    Mat_<Vec3f> vertex;
-    Mat_<Vec4f> colors;
-    Mat_<int> indices;
-    double fovy = 0.0;
-    std::string fname;
-    if (clipping)
+    std::string modelPath = parser.get<std::string>("modelPath");
+    if (modelPath.empty())
     {
-        vertex = {
-            { 2.0,  0.0, -2.0}, { 0.0  -6.0, -2.0}, {-2.0,  0.0, -2.0},
-            { 3.5, -1.0, -5.0}, { 2.5, -2.5, -5.0}, {-1.0,  1.0, -5.0},
-            {-6.5, -1.0, -3.0}, {-2.5, -2.0, -3.0}, { 1.0,  1.0, -5.0},
-        };
-        Vec4f col1(0.725f, 0.933f, 0.851f, 1.0f);
-        Vec4f col2(0.933f, 0.851f, 0.725f, 1.0f);
-        Vec4f col3(0.933f, 0.039f, 0.588f, 1.0f);
-        colors = {
-            col1, col1, col1,
-            col2, col2, col2,
-            col3, col3, col3,
-        };
-        indices = {0, 1, 2, 3, 4, 5, 6, 7, 8};
-
-        fovy = 45.0;
-        fname = "example_image_clipping.png";
-    }
-    else if (color)
-    {
-        vertex = {
-            { 2.0,  0.0, -2.0},
-            { 0.0,  2.0, -3.0},
-            {-2.0,  0.0, -2.0},
-            { 0.0, -2.0,  1.0},
-        };
-        colors = {
-            {1.0f, 0.0f, 0.0f},
-            {0.0f, 1.0f, 0.0f},
-            {0.0f, 0.0f, 1.0f},
-            {0.0f, 1.0f, 0.0f},
-        };
-        indices = {0, 1, 2, 0, 2, 3};
-
-        fovy = 60.0;
-        fname = "example_image_color.png";
-    }
-    else if (depth)
-    {
-        vertex = {
-            { 2.0,  0.0, -2.0}, { 0.0, -2.0, -2.0}, {-2.0,  0.0, -2.0},
-            { 3.5, -1.0, -5.0}, { 2.5, -1.5, -5.0}, {-1.0,  0.5, -5.0},
-        };
-        Vec4f col1(0.851f, 0.933f, 0.725f, 1.0f);
-        Vec4f col2(0.725f, 0.851f, 0.933f, 1.0f);
-        colors = {
-            col1, col1, col1,
-            col2, col2, col2
-        };
-        indices = { 0, 1, 2, 3, 4, 5 };
-
-        fovy = 45.0;
-        fname = "example_image_depth.png";
-    }
-    else
-    {
-        std::cout << "Wrong mode: " << mode << std::endl;
+        std::cout << "No model path given" << std::endl;
         return -1;
     }
 
-    DrawData data;
-
-    data.arr.setVertexArray(vertex);
-    data.arr.setColorArray(colors);
-    data.indices.copyFrom(indices);
-
-    float zNear = 0.1, zFar = 50;
-    glMatrixMode(GL_PROJECTION);
-    glLoadIdentity();
-    gluPerspective(fovy, (double)win_width / win_height, zNear, zFar);
-
-    glMatrixMode(GL_MODELVIEW);
-    glLoadIdentity();
-    gluLookAt(0, 0, 5, 0, 0, 0, 0, 1, 0);
-
-    glDisable(GL_CULL_FACE);
-    glEnable(GL_DEPTH_TEST);
-
-    setOpenGlDrawCallback("OpenGL", draw, &data);
-
-    for (;;)
+    std::string outPath = parser.get<std::string>("outPath");
+    if (outPath.empty())
     {
-        updateWindow("OpenGL");
-
-        cv::Mat colorData(win_height, win_width, CV_8UC3, Scalar());
-        glReadPixels(0, 0, win_width, win_height, GL_RGB, GL_UNSIGNED_BYTE, colorData.data());
-        cv::cvtColor(colorData, colorData, cv::COLOR_RGB2BGR);
-        cv::flip(colorData, colorData, 0);
-        cv::imwrite(colorFname, colorData);
-
-        if(depth)
-        {
-            cv::Mat depthMat(win_height, win_width, CV_32F);
-            glReadPixels(0, 0, win_width, win_height, GL_DEPTH_COMPONENT, GL_FLOAT, depthMat.ptr());
-            for (auto it = depthMat.begin<float>(); it != depthMat.end<float>(); ++it)
-            {
-                *it = zNear * zFar / ((*it) * (zNear - zFar) + zFar);
-            }
-            cv::flip(depthMat, depthMat, 0);
-            cv::imwrite(depthFname, depthMat);
-        }
-
-        char key = (char)waitKey(40);
-        if (key == 27)
-            break;
+        std::cout << "No output path given" << std::endl;
+        return -1;
     }
 
-    setOpenGlDrawCallback("OpenGL", 0, 0);
-    destroyAllWindows();
+    std::array<cv::Size, 2> resolutions = { cv::Size {700, 700}, cv::Size {640, 480} };
+    for (const auto& res : resolutions)
+    {
+        for (const auto shadingType : {
+                ShadingType::White,
+                ShadingType::Flat,
+                ShadingType::Shaded
+            })
+        {
+            std::string shadingName;
+            switch (shadingType)
+            {
+            case ShadingType::White:  shadingName = "White";  break;
+            case ShadingType::Flat:   shadingName = "Flat";   break;
+            case ShadingType::Shaded: shadingName = "Shaded"; break;
+            default:
+                break;
+            }
+
+            for (const auto modelType : {
+                         ModelType::File,
+                         ModelType::Clipping,
+                         ModelType::Color,
+                         ModelType::Centered,
+                })
+            {
+                std::string modelName;
+                switch (modelType)
+                {
+                case ModelType::File:     modelName = "File";     break;
+                case ModelType::Clipping: modelName = "Clipping"; break;
+                case ModelType::Color:    modelName = "Color";    break;
+                case ModelType::Centered: modelName = "Centered"; break;
+                default:
+                    break;
+                }
+
+                std::string widthStr  = std::to_string(res.width);
+                std::string heightStr = std::to_string(res.height);
+
+                std::string suffix = modelName + "_" + widthStr + "x" + heightStr;
+
+                std::cout << suffix + "_" + shadingName << "..." << std::endl;
+
+                cv::Mat colorImage, depthImage;
+                generateImage(res, shadingType, modelType, modelPath, colorImage, depthImage);
+
+                std::string gtPathColor = outPath + "/example_image_" + suffix + "_" + shadingName + ".png";
+                std::string gtPathDepth = outPath + "/depth_image_"   + suffix + ".png";
+
+                cv::imwrite(gtPathColor, colorImage);
+                cv::imwrite(gtPathDepth, depthImage);
+            }
+        }
+    }
 
     return 0;
 }
