e89017c3f5f4f66870f1b9cde55da0a3acb15103
Rostislav Vasilikhin <rostislav.vasilikhin@opencv.ai>
2024-01-06 23:17:45 +0100

    perf: shading type, outputs needed

--- modules/3d/perf/perf_rendering.cpp
+++ modules/3d/perf/perf_rendering.cpp
@@ -15,20 +15,128 @@ Matx43f makeCamMatrix_TODO_rewrite_it_later(Vec3f position, Vec3f lookat, Vec3f 
     return m;
 }
 
-typedef perf::TestBaseWithParam<std::tuple<std::tuple<int, int>, bool, bool>> RenderingTest;
+enum class ShadingType
+{
+    White = 0,
+    Flat = 1,
+    Shaded = 2
+};
+
+// that was easier than using CV_ENUM() macro
+namespace
+{
+    using namespace cv;
+    struct ShadingTypeEnum
+    {
+        static const std::array<ShadingType, 3> vals;
+        static const std::array<std::string, 3> svals;
+
+        ShadingTypeEnum(ShadingType v = ShadingType::White) : val(v) {}
+        operator ShadingType() const { return val; }
+        void PrintTo(std::ostream *os) const
+        {
+            int v = int(val);
+            if (v >= 0 && v < 5)
+            {
+                *os << svals[v];
+            }
+            else
+            {
+                *os << "UNKNOWN";
+            }
+        }
+        static ::testing::internal::ParamGenerator<ShadingTypeEnum> all()
+        {
+            return ::testing::Values(ShadingTypeEnum(vals[0]),
+                                     ShadingTypeEnum(vals[1]),
+                                     ShadingTypeEnum(vals[2]));
+        }
+
+    private:
+        ShadingType val;
+    };
+
+    const std::array<ShadingType, 3> ShadingTypeEnum::vals{ ShadingType::White,
+                                                            ShadingType::Flat,
+                                                            ShadingType::Shaded
+                                                          };
+    const std::array<std::string, 3> ShadingTypeEnum::svals{ std::string("White"),
+                                                             std::string("Flat"),
+                                                             std::string("Shaded")
+                                                           };
+
+    static inline void PrintTo(const ShadingTypeEnum &t, std::ostream *os) { t.PrintTo(os); }
+}
+
+enum class Outputs
+{
+    DepthOnly  = 0,
+    ColorOnly  = 1,
+    DepthColor = 2,
+};
+
+// that was easier than using CV_ENUM() macro
+namespace
+{
+    using namespace cv;
+    struct OutputsEnum
+    {
+        static const std::array<Outputs, 3> vals;
+        static const std::array<std::string, 3> svals;
+
+        OutputsEnum(Outputs v = Outputs::DepthColor) : val(v) {}
+        operator Outputs() const { return val; }
+        void PrintTo(std::ostream *os) const
+        {
+            int v = int(val);
+            if (v >= 0 && v < 5)
+            {
+                *os << svals[v];
+            }
+            else
+            {
+                *os << "UNKNOWN";
+            }
+        }
+        static ::testing::internal::ParamGenerator<OutputsEnum> all()
+        {
+            return ::testing::Values(OutputsEnum(vals[0]),
+                                     OutputsEnum(vals[1]),
+                                     OutputsEnum(vals[2]));
+        }
+
+    private:
+        Outputs val;
+    };
+
+    const std::array<Outputs, 3> OutputsEnum::vals{ Outputs::DepthOnly,
+                                                    Outputs::ColorOnly,
+                                                    Outputs::DepthColor
+                                                  };
+    const std::array<std::string, 3> OutputsEnum::svals{ std::string("DepthOnly"),
+                                                         std::string("ColorOnly"),
+                                                         std::string("DepthColor")
+                                                       };
+
+    static inline void PrintTo(const OutputsEnum &t, std::ostream *os) { t.PrintTo(os); }
+}
+
+
+// resolution, shading type, outputs needed
+typedef perf::TestBaseWithParam<std::tuple<std::tuple<int, int>, ShadingTypeEnum, OutputsEnum>> RenderingTest;
 
 PERF_TEST_P(RenderingTest, rasterizeTriangles, ::testing::Combine(
     ::testing::Values(std::make_tuple(1920, 1080), std::make_tuple(1024, 768), std::make_tuple(640, 480)),
-    ::testing::Bool(), // shading
-    ::testing::Bool() // have colors
+    ShadingTypeEnum::all(),
+    OutputsEnum::all()
     ))
 {
     auto t = GetParam();
     auto wh = std::get<0>(t);
     int width = std::get<0>(wh);
     int height = std::get<1>(wh);
-    bool shadingMode = std::get<1>(t);
-    bool haveColors = std::get<2>(t);
+    auto shadingType = std::get<1>(t);
+    auto outputs = std::get<2>(t);
 
     string objectPath = findDataFile("rendering/model/spot.obj");
 
@@ -48,16 +156,16 @@ PERF_TEST_P(RenderingTest, rasterizeTriangles, ::testing::Combine(
         indices.push_back({vec[0], vec[1], vec[2]});
     }
 
-    if (haveColors)
+    if (shadingType == ShadingType::White)
+    {
+        colors.clear();
+    }
+    else
     {
         for (auto &color : colors)
         {
             color = Vec3f(abs(color[0]), abs(color[1]), abs(color[2])) * 255.0f;
         }
-    }
-    else
-    {
-        colors.clear();
     }
 
     double zNear = 0.1, zFar = 50;
@@ -73,7 +181,9 @@ PERF_TEST_P(RenderingTest, rasterizeTriangles, ::testing::Combine(
 
         startTimer();
         cv::triangleRasterize(vertices, indices, colors, cameraMatrix, width, height,
-                              shadingMode, depth_buf, haveColors ? color_buf : noArray());
+                              (shadingType == ShadingType::Shaded),
+                              (outputs != Outputs::ColorOnly) ? depth_buf : noArray(),
+                              (outputs != Outputs::DepthOnly) ? color_buf : noArray());
         stopTimer();
     }
 
@@ -85,9 +195,15 @@ PERF_TEST_P(RenderingTest, rasterizeTriangles, ::testing::Combine(
         depth_buf.convertTo(depth_buf, CV_8UC1, 1.0);
         cv::flip(depth_buf, depth_buf, 0);
 
-        std::string suffix = std::to_string(width) + "x" + std::to_string(height) + " ";
-        suffix += (shadingMode ? "shaded" : "flat");
-        suffix += (haveColors ? "colored" : "white");
+        std::string shadingName;
+        {
+            std::stringstream ss;
+            shadingType.PrintTo(&ss);
+            ss >> shadingName;
+        }
+        std::string widthStr  = std::to_string(width);
+        std::string heightStr = std::to_string(height);
+        std::string suffix = widthStr + "x" + heightStr + "_" + shadingName;
 
         imwrite("color_image_" + suffix + ".png", color_buf);
         imwrite("depth_image_" + suffix + ".png", depth_buf);
