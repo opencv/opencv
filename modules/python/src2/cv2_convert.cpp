b2283549561020cf51956057c0f79933d244cc62
Alexander Smorkalov <alexander.smorkalov@xperience.ai>
2024-01-09 20:03:14 +0300

    Handle build with old Python3 interpreter.

--- modules/python/src2/cv2_convert.cpp
+++ modules/python/src2/cv2_convert.cpp
@@ -702,7 +702,7 @@ bool pyopencv_to(PyObject* obj, String &value, const ArgInfo& info)
     }
     std::string str;
 
-#if !defined(Py_LIMITED_API) || (Py_LIMITED_API >= 0x03060000)
+#if ((PY_VERSION_HEX >= 0x03060000) && !defined(Py_LIMITED_API)) || (Py_LIMITED_API >= 0x03060000)
     if (info.pathlike)
     {
         obj = PyOS_FSPath(obj);
