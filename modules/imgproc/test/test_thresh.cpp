bd27b429b2459578d92b7a42628dbb88aaf471b9
Dhanwanth1803 <dhanwanthvarala@gmail,com>
2024-01-19 15:07:39 +0530

    added test fixes-#24742

--- modules/imgproc/test/test_thresh.cpp
+++ modules/imgproc/test/test_thresh.cpp
@@ -542,3 +542,80 @@ TEST(Imgproc_Threshold, regression_THRESH_TOZERO_IPP_21258_Max)
 }
 
 }} // namespace
+
+//new test for adaptive threshold
+static void test_adaptiveThreshold(const Mat& _src, Mat& _dst,
+                                    double maxValue, int method, int type, int blockSize, double delta)
+{
+    int i, j;
+    int depth = _src.depth(), cn = _src.channels();
+    int width_n = _src.cols * cn, height = _src.rows;
+
+    CV_Assert(_src.type() == CV_8UC1);
+    CV_Assert(blockSize % 2 == 1 && blockSize > 1);
+
+    _dst.create(size, _src.type());
+    Mat dst = _dst.getMat();
+
+    if (maxValue < 0)
+    {
+        dst = Scalar(0);
+        return;
+    }
+
+    CALL_HAL(adaptiveThreshold, cv_hal_adaptiveThreshold, _src.data, _src.step, dst.data, dst.step,
+             _src.cols, _src.rows, maxValue, method, type, blockSize, delta);
+
+    Mat mean;
+
+    if (_src.data != dst.data)
+        mean = dst;
+
+    if (method == ADAPTIVE_THRESH_MEAN_C)
+        boxFilter(_src, mean, _src.type(), Size(blockSize, blockSize),
+                   Point(-1, -1), true, BORDER_REPLICATE | BORDER_ISOLATED);
+    else if (method == ADAPTIVE_THRESH_GAUSSIAN_C)
+    {
+        Mat srcfloat, meanfloat;
+        _src.convertTo(srcfloat, CV_32F);
+        meanfloat = srcfloat;
+        GaussianBlur(srcfloat, meanfloat, Size(blockSize, blockSize), 0, 0, BORDER_REPLICATE | BORDER_ISOLATED);
+        meanfloat.convertTo(mean, _src.type());
+    }
+    else
+        CV_Error(CV_StsBadFlag, "Unknown/unsupported adaptive threshold method");
+
+    uchar imaxval = saturate_cast<uchar>(maxValue);
+    int idelta = type == THRESH_BINARY ? cvCeil(delta) : cvFloor(delta);
+    uchar tab[768];
+
+    if (type == CV_THRESH_BINARY)
+        for (i = 0; i < 768; i++)
+            tab[i] = (uchar)(i - 255 > -idelta ? imaxval : 0);
+    else if (type == CV_THRESH_BINARY_INV)
+        for (i = 0; i < 768; i++)
+            tab[i] = (uchar)(i - 255 <= -idelta ? imaxval : 0);
+    else
+        CV_Error(CV_StsBadFlag, "Unknown/unsupported threshold type");
+
+    if (_src.isContinuous() && mean.isContinuous() && dst.isContinuous())
+    {
+        size.width *= size.height;
+        size.height = 1;
+    }
+
+    // Actual testing logic
+    for (i = 0; i < height; i++)
+    {
+        const uchar* sdata = _src.ptr(i);
+        const uchar* mdata = mean.ptr(i);
+        uchar* ddata = dst.ptr(i);
+
+        for (j = 0; j < width_n; j++)
+        {
+            // I am not sure of the testing logic exactly. 
+            // I am just comparing '_dst' values.
+            assert(ddata[j] == tab[sdata[j] - mdata[j] + 255]);
+        }
+    }
+}
