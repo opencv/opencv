2236c9c6ce67dbfe522e05107801d14d81135a1b
wuhao <wuhao@orbbec.com>
2024-01-20 13:59:58 +0800

    resolve the issue of unable to find orbbecsdk

--- 3rdparty/orbbecsdk/orbbecsdk.cmake
+++ 3rdparty/orbbecsdk/orbbecsdk.cmake
@@ -1,34 +1,18 @@
 function(download_orbbec_sdk root_var)
-
-    # detect git
-    find_package(Git)
-    if(NOT GIT_FOUND)
-        message(STATUS "Git not found, cannot download orbbec sdk")
-        return()
-    endif()
-
-    # download orbbec sdk
-    set(THE_ROOT "${OpenCV_BINARY_DIR}/3rdparty/orbbecsdk")
-    set(THE_URL "https://github.com/orbbec/OrbbecSDK.git")
-
-    if(EXISTS "${THE_ROOT}/.git")
-        # update repo
-        message(STATUS "Updating orbbec sdk")
-        execute_process(COMMAND ${GIT_EXECUTABLE} pull
-                            WORKING_DIRECTORY "${THE_ROOT}"
-                            RESULT_VARIABLE res)
-        message(STATUS "Updating orbbec sdk - done (${res})")
-    else()
-        # clone repo
-        message(STATUS "Cloning orbbec sdk")
-        execute_process(COMMAND ${GIT_EXECUTABLE} clone "${THE_URL}" "${THE_ROOT}"
-                            RESULT_VARIABLE res)
-        message(STATUS "Cloning orbbec sdk - done (${res})")
-    endif()
+    set(ORBBECSDK_DOWNLOAD_DIR "${OpenCV_BINARY_DIR}/3rdparty/orbbecsdk")
+    set(ORBBECSDK_FILE_HASH_CMAKE "07bb601b7ed72ae783eea70008cc26b3")
+    ocv_download(FILENAME "v1.9.3.tar.gz"
+                HASH ${ORBBECSDK_FILE_HASH_CMAKE}
+                URL "https://github.com/orbbec/OrbbecSDK/archive/refs/tags/v1.9.3/"
+                DESTINATION_DIR ${ORBBECSDK_DOWNLOAD_DIR}
+                ID OrbbecSDK
+                STATUS res
+                UNPACK RELATIVE_URL
+                )
     if(${res})
-        message(FATAL_ERROR "Failed to download orbbec sdk")
+        message(STATUS "orbbec sdk downloaded to: ${ORBBECSDK_DOWNLOAD_DIR}")
+        set(${root_var} "${ORBBECSDK_DOWNLOAD_DIR}/OrbbecSDK-1.9.3" PARENT_SCOPE)
     else()
-        message(STATUS "orbbec sdk downloaded to: ${THE_ROOT}")
-        set(${root_var} "${THE_ROOT}" PARENT_SCOPE)
+        message(FATAL_ERROR "Failed to download orbbec sdk")   
     endif()
 endfunction()
