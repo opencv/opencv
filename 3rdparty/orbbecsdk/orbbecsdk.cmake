efb2136ee3ba9ef077bf9e8ab161140a70d6f123
wuhao <wuhao@orbbec.com>
2024-01-24 10:36:32 +0800

    switch OrbbecSDK version to 1.9.4

--- 3rdparty/orbbecsdk/orbbecsdk.cmake
+++ 3rdparty/orbbecsdk/orbbecsdk.cmake
@@ -1,9 +1,9 @@
 function(download_orbbec_sdk root_var)
     set(ORBBECSDK_DOWNLOAD_DIR "${OpenCV_BINARY_DIR}/3rdparty/orbbecsdk")
-    set(ORBBECSDK_FILE_HASH_CMAKE "07bb601b7ed72ae783eea70008cc26b3")
-    ocv_download(FILENAME "v1.9.3.tar.gz"
+    set(ORBBECSDK_FILE_HASH_CMAKE "2624c84837d3416fd8b3e95750e6e725")
+    ocv_download(FILENAME "v1.9.4.tar.gz"
                 HASH ${ORBBECSDK_FILE_HASH_CMAKE}
-                URL "https://github.com/orbbec/OrbbecSDK/archive/refs/tags/v1.9.3/"
+                URL "https://github.com/orbbec/OrbbecSDK/archive/refs/tags/v1.9.4/"
                 DESTINATION_DIR ${ORBBECSDK_DOWNLOAD_DIR}
                 ID OrbbecSDK
                 STATUS res
@@ -11,7 +11,7 @@ function(download_orbbec_sdk root_var)
                 )
     if(${res})
         message(STATUS "orbbec sdk downloaded to: ${ORBBECSDK_DOWNLOAD_DIR}")
-        set(${root_var} "${ORBBECSDK_DOWNLOAD_DIR}/OrbbecSDK-1.9.3" PARENT_SCOPE)
+        set(${root_var} "${ORBBECSDK_DOWNLOAD_DIR}/OrbbecSDK-1.9.4" PARENT_SCOPE)
     else()
         message(FATAL_ERROR "Failed to download orbbec sdk")   
     endif()
