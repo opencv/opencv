4976a69e669d8d40ab28834fbb225313b8ac9cca
Maksim Shabunin <maksim.shabunin@gmail.com>
2024-02-16 23:22:29 +0300

    doc: various fixes for modern doxygen 1.10

--- modules/imgcodecs/include/opencv2/imgcodecs.hpp
+++ modules/imgcodecs/include/opencv2/imgcodecs.hpp
@@ -297,7 +297,7 @@ It also demonstrates how to save multiple images in a TIFF file:
 CV_EXPORTS_W bool imwrite( const String& filename, InputArray img,
               const std::vector<int>& params = std::vector<int>());
 
-/// @overload multi-image overload for bindings
+//! @brief multi-image overload for bindings
 CV_WRAP static inline
 bool imwritemulti(const String& filename, InputArrayOfArrays img,
                   const std::vector<int>& params = std::vector<int>())
