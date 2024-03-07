b34ec6e2e47f6d3d284613a22c879e80bee4136f
Dmitry Kurtaev <dmitry.kurtaev@gmail.com>
2024-03-07 22:39:20 +0300

    Move implementation from header. Fix Windows build

--- modules/dnn/test/test_common.hpp
+++ modules/dnn/test/test_common.hpp
@@ -5,11 +5,6 @@
 #ifndef __OPENCV_TEST_COMMON_HPP__
 #define __OPENCV_TEST_COMMON_HPP__
 
-#ifdef _WIN32
-#include <windows.h>
-#include <psapi.h>
-#endif  // _WIN32
-
 #include "opencv2/dnn/utils/inference_engine.hpp"
 
 #ifdef HAVE_OPENCL
@@ -237,29 +232,7 @@ public:
             expectNoFallbacks(net);
     }
 
-    size_t getTopMemoryUsageMB()
-    {
-#ifdef _WIN32
-        PROCESS_MEMORY_COUNTERS proc;
-        GetProcessMemoryInfo(GetCurrentProcess(), &proc, sizeof(proc));
-        return proc.PeakWorkingSetSize / pow(1024, 2);  // bytes to megabytes
-#else
-        std::ifstream status("/proc/self/status");
-        std::string line, title;
-        while (std::getline(status, line))
-        {
-            std::istringstream iss(line);
-            iss >> title;
-            if (title == "VmHWM:")
-            {
-                size_t mem;
-                iss >> mem;
-                return mem / 1024;
-            }
-        }
-#endif
-        return 0l;
-    }
+    size_t getTopMemoryUsageMB();
 
 protected:
     void checkBackend(Mat* inp = 0, Mat* ref = 0)
