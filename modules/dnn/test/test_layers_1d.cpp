c081ba215f4ce444b9b16e3db7d1febad16bd4eb
Abdurrahheem <shtanchaev1994@gmail.com>
2024-03-13 11:39:49 +0400

    Concat layer test. dims=0 not working

--- modules/dnn/test/test_layers_1d.cpp
+++ modules/dnn/test/test_layers_1d.cpp
@@ -245,4 +245,32 @@ INSTANTIATE_TEST_CASE_P(/*nothing*/, Layer_Elemwise_1d_Test, Combine(
 /*operation*/           Values("div", "prod", "max", "min", "sum")
 ));
 
+TEST(Layer_Concat_1d_Test, Accuracy)
+{
+    LayerParams lp;
+    lp.type = "Concat";
+    lp.name = "ConcatLayer";
+    lp.set("axis", 0);
+
+    Ptr<ConcatLayer> layer = ConcatLayer::create(lp);
+    int dims = 0;
+    std::vector<int> input_shape = {1};
+    std::vector<int> output_shape = {3 * input_shape[0]};
+
+    Mat input1(dims, input_shape.data(), CV_32F, 1.0);
+    Mat input2(dims, input_shape.data(), CV_32F, 2.0);
+    Mat input3(dims, input_shape.data(), CV_32F, 3.0);
+
+    float data[] = {1.0, 2.0, 3.0};
+    Mat output_ref(output_shape, CV_32F, data);
+
+    std::vector<Mat> inputs{input1, input2, input3};
+    std::vector<Mat> outputs;
+
+    runLayer(layer, inputs, outputs);
+    std::cout << "output: " << outputs[0] << std::endl;
+    ASSERT_EQ(shape(output_ref), shape(outputs[0]));
+    normAssert(output_ref, outputs[0]);
+}
+
 }}
