3af5be7ab88f339e18e12b256a0d6784ae3749fb
Abdurrahheem <shtanchaev1994@gmail.com>
2024-03-15 16:07:19 +0400

    working test for const layer

--- modules/dnn/test/test_layers_1d.cpp
+++ modules/dnn/test/test_layers_1d.cpp
@@ -271,4 +271,37 @@ TEST(Layer_Reshape_Test, Accuracy)
     normAssert(output_ref, outputs[0]);
 }
 
+typedef testing::TestWithParam<tuple<int>> Layer_Const_Test;
+TEST_P(Layer_Const_Test, Accuracy)
+{
+    int dims = get<0>(GetParam());
+
+    LayerParams lp;
+    lp.type = "Const";
+    lp.name = "ConstLayer";
+
+    std::vector<int> input_shape;
+    Mat constBlob, output_ref;
+    if (dims > 1){
+        input_shape = {1, 10}; // 10 to keep test fast
+        constBlob = Mat(input_shape, CV_32F);
+    } else {
+        input_shape = {dims};
+        constBlob = Mat(dims, input_shape.data(), CV_32F, 1.0);
+    }
+    cv::randn(constBlob, 0.0, 1.0);
+    output_ref = constBlob.clone();
+
+    lp.blobs.push_back(constBlob);
+    Ptr<Layer> layer = ConstLayer::create(lp);
+
+    std::vector<Mat> inputs; // No inputs are needed for a ConstLayer
+    std::vector<Mat> outputs;
+    runLayer(layer, inputs, outputs);
+    ASSERT_EQ(shape(output_ref), shape(outputs[0]));
+    normAssert(output_ref, outputs[0]);
+}
+INSTANTIATE_TEST_CASE_P(/*nothing*/, Layer_Const_Test,
+/* dims */                          Values(0, 1, 2));
+
 }}
