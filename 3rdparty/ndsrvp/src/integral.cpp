faf18dabc8562011ff85acf02668d567270c51d2
Junyan721113 <llh721113@outlook.com>
2024-03-27 15:58:56 +0800

    fix: Integral Performance

--- 3rdparty/ndsrvp/src/integral.cpp
+++ 3rdparty/ndsrvp/src/integral.cpp
@@ -7,7 +7,12 @@ int ndsrvp_integral(int depth, int sdepth, int sqdepth,
     uchar* _tilted, size_t,
     int width, int height, int cn)
 {
+    // 8-bit unsigned integer, 32-bit signed integer only
     if (!(depth == CV_8U && sdepth == CV_32S))
+        return CV_HAL_ERROR_NOT_IMPLEMENTED;
+
+    // too small image
+    if (!(width >> 8 || height >> 8 || cn == 4))
         return CV_HAL_ERROR_NOT_IMPLEMENTED;
 
     int* sum = (int*)_sum;
@@ -25,26 +30,51 @@ int ndsrvp_integral(int depth, int sdepth, int sqdepth,
     if (cn == 1) {
         for (int i = 0; i < height; ++i) {
             const uchar* src_row = src + _srcstep * i;
-            int* prev_sum_row = (int*)((uchar*)sum + _sumstep * i) + 1;
-            int* sum_row = (int*)((uchar*)sum + _sumstep * (i + 1)) + 1;
+            int* prev_sum_row = (int*)((uchar*)sum + _sumstep * i) + cn;
+            int* sum_row = (int*)((uchar*)sum + _sumstep * (i + 1)) + cn;
 
             sum_row[-1] = 0;
 
             int32x2_t prev = { 0, 0 };
             int j = 0;
 
-            for (; j + 4 <= width; j += 4) {
-                uint8x4_t vs8x4 = *(uint8x4_t*)(src_row + j);
-                int16x4_t vs16x4 = (int16x4_t)__nds__pkbb32(__nds__zunpkd832((unsigned int)vs8x4),
-                    __nds__zunpkd810((unsigned int)vs8x4));
+            for (; j + 8 <= width; j += 8) {
+                unsigned long vs8x8 = *(unsigned long*)(src_row + j); 
+
+                unsigned long vs810 = __nds__zunpkd810(vs8x8);
+                unsigned long vs832 = __nds__zunpkd832(vs8x8);
+
+                int16x4_t vs16x4 = (int16x4_t)__nds__pkbb32(vs832, vs810);
 
                 vs16x4 += (int16x4_t)((unsigned long)vs16x4 << 16); // gcc vector extension
                 vs16x4 += (int16x4_t)((unsigned long)vs16x4 << 32); // '+' is add16
 
-                *(int32x2_t*)(sum_row + j) = (int32x2_t) { vs16x4[0], vs16x4[1] } + *(int32x2_t*)(prev_sum_row + j) + prev; // '+' is add32
-                *(int32x2_t*)(sum_row + j + 2) = (int32x2_t) { vs16x4[2], vs16x4[3] } + *(int32x2_t*)(prev_sum_row + j + 2) + prev;
+                //*(int32x2_t*)(sum_row + j) = (int32x2_t) { vs16x4[0], vs16x4[1] } + *(int32x2_t*)(prev_sum_row + j) + prev;
+                //*(int32x2_t*)(sum_row + j + 2) = (int32x2_t) { vs16x4[2], vs16x4[3] } + *(int32x2_t*)(prev_sum_row + j + 2) + prev;
+                // performance loss for unknown reason, commented out, use the following code instead
+
+                sum_row[j] = prev_sum_row[j] + prev[0] + vs16x4[0];
+                sum_row[j + 1] = prev_sum_row[j + 1] + prev[1] + vs16x4[1];
+                sum_row[j + 2] = prev_sum_row[j + 2] + prev[0] + vs16x4[2];
+                sum_row[j + 3] = prev_sum_row[j + 3] + prev[1] + vs16x4[3];
 
                 prev += vs16x4[3]; // prev += (int32x2_t){vs16x4[3], vs16x4[3]};
+
+                vs16x4 = (int16x4_t)__nds__pktt32(vs832, vs810);
+
+                vs16x4 += (int16x4_t)((unsigned long)vs16x4 << 16);
+                vs16x4 += (int16x4_t)((unsigned long)vs16x4 << 32);
+
+                //*(int32x2_t*)(sum_row + j + 4) = (int32x2_t) { vs16x4[0], vs16x4[1] } + *(int32x2_t*)(prev_sum_row + j + 4) + prev;
+                //*(int32x2_t*)(sum_row + j + 6) = (int32x2_t) { vs16x4[2], vs16x4[3] } + *(int32x2_t*)(prev_sum_row + j + 6) + prev;
+                // performance loss for unknown reason, commented out, use the following code instead
+
+                sum_row[j + 4] = prev_sum_row[j + 4] + prev[0] + vs16x4[0];
+                sum_row[j + 5] = prev_sum_row[j + 5] + prev[1] + vs16x4[1];
+                sum_row[j + 6] = prev_sum_row[j + 6] + prev[0] + vs16x4[2];
+                sum_row[j + 7] = prev_sum_row[j + 7] + prev[1] + vs16x4[3];
+
+                prev += vs16x4[3];
             }
 
             for (int v = sum_row[j - 1] - prev_sum_row[j - 1]; j < width; ++j)
@@ -60,7 +90,7 @@ int ndsrvp_integral(int depth, int sdepth, int sqdepth,
 
             int32x2_t prev = { 0, 0 };
             int j = 0;
-            for (; j + 4 * cn <= width; j += 4 * cn) {
+            for (; j + 8 <= width; j += 8) {
                 uint8x8_t vs8x8 = *(uint8x8_t*)(src_row + j);
 
                 uint16x4_t vs16x4_1 = __nds__v_zunpkd820(vs8x8);
@@ -88,6 +118,8 @@ int ndsrvp_integral(int depth, int sdepth, int sqdepth,
             }
         }
     } else if (cn == 3) {
+        return CV_HAL_ERROR_NOT_IMPLEMENTED;
+        /* disabled because of unaligned memory access, difficulty in vectorization, etc.
         for (int i = 0; i < height; ++i) {
             const uchar* src_row = src + _srcstep * i;
             int* prev_sum_row = (int*)((uchar*)sum + _sumstep * i) + cn;
@@ -98,7 +130,10 @@ int ndsrvp_integral(int depth, int sdepth, int sqdepth,
             int32x2_t prev_ptr[2] = { { 0, 0 }, { 0, 0 } };
             int j = 0;
             for (; j + 3 <= width; j += 3) {
-                int8x4_t vs8x4 = *(int8x4_t*)(src_row + j);
+                //uint8x4_t vs8x4 = *(uint8x4_t*)(src_row + j);
+                // performance loss for unknown reason, commented out, use the following code instead
+
+                uint8x4_t vs8x4 = (uint8x4_t){ src_row[j], src_row[j + 1], src_row[j + 2], 0};
 
                 // [ 0 | 2 | 1 | 3 ]
                 int16x4_t vs16x4 = (int16x4_t)__nds__pkbb32(__nds__zunpkd831((unsigned int)vs8x4), __nds__zunpkd820((unsigned int)vs8x4));
@@ -107,7 +142,12 @@ int ndsrvp_integral(int depth, int sdepth, int sqdepth,
                 prev_ptr[0] += (int32x2_t)__nds__pkbb16(0, (unsigned long)vs16x4);
                 prev_ptr[1] += (int32x2_t)__nds__pktt16(0, (unsigned long)vs16x4);
 
-                *(int32x4_t*)(sum_row + j) = *(int32x4_t*)(prev_sum_row + j) + *(int32x4_t*)prev_ptr;
+                //*(int32x4_t*)(sum_row + j) = *(int32x4_t*)(prev_sum_row + j) + *(int32x4_t*)prev_ptr;
+                // performance loss for unknown reason, commented out, use the following code instead
+
+                sum_row[j] = prev_sum_row[j] + prev_ptr[0][0];
+                sum_row[j + 1] = prev_sum_row[j + 1] + prev_ptr[0][1];
+                sum_row[j + 2] = prev_sum_row[j + 2] + prev_ptr[1][0];
             }
 
             for (int v3 = sum_row[j - 1] - prev_sum_row[j - 1],
@@ -118,7 +158,7 @@ int ndsrvp_integral(int depth, int sdepth, int sqdepth,
                 sum_row[j + 1] = (v2 += src_row[j + 1]) + prev_sum_row[j + 1];
                 sum_row[j + 2] = (v3 += src_row[j + 2]) + prev_sum_row[j + 2];
             }
-        }
+        }*/
     } else if (cn == 4) {
         for (int i = 0; i < height; ++i) {
             const uchar* src_row = src + _srcstep * i;
@@ -130,7 +170,7 @@ int ndsrvp_integral(int depth, int sdepth, int sqdepth,
             int32x2_t prev_ptr[2] = { { 0, 0 }, { 0, 0 } };
             int j = 0;
             for (; j + 4 <= width; j += 4) {
-                int8x4_t vs8x4 = *(int8x4_t*)(src_row + j);
+                uint8x4_t vs8x4 = *(uint8x4_t*)(src_row + j);
 
                 // [ 0 | 2 | 1 | 3 ]
                 int16x4_t vs16x4 = (int16x4_t)__nds__pkbb32(__nds__zunpkd831((unsigned int)vs8x4), __nds__zunpkd820((unsigned int)vs8x4));
@@ -153,8 +193,6 @@ int ndsrvp_integral(int depth, int sdepth, int sqdepth,
                 sum_row[j + 3] = (v4 += src_row[j + 3]) + prev_sum_row[j + 3];
             }
         }
-    } else {
-        return CV_HAL_ERROR_NOT_IMPLEMENTED;
     }
     return CV_HAL_ERROR_OK;
 }
