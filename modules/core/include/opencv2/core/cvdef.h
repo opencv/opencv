197626a5bf6e6347122937fe44a204474c73b7ac
Yuantao Feng <yuantao.feng@opencv.org.cn>
2024-04-11 19:02:44 +0800

    Merge pull request #25387 from fengyuentau:complete-float16_t-renaming
    Rename remaining float16_t for future proof #25387
    
    Resolves comment: https://github.com/opencv/opencv/pull/25217#discussion_r1547733187.
    
    `std::float16_t` and `std::bfloat16_t` are introduced since c++23: https://en.cppreference.com/w/cpp/types/floating-point.
    
    ### Pull Request Readiness Checklist
    
    See details at https://github.com/opencv/opencv/wiki/How_to_contribute#making-a-good-pull-request
    
    - [x] I agree to contribute to the project under Apache 2 License.
    - [x] To the best of my knowledge, the proposed patch is not based on a code under GPL or another license that is incompatible with OpenCV
    - [x] The PR is proposed to the proper branch
    - [x] There is a reference to the original bug report and related work
    - [x] There is accuracy test, performance test and test data in opencv_extra repository, if applicable
          Patch to opencv_extra has the same branch name.
    - [x] The feature is well documented and sample code can be built with the project CMake

--- modules/core/include/opencv2/core/cvdef.h
+++ modules/core/include/opencv2/core/cvdef.h
@@ -900,7 +900,9 @@ inline hfloat hfloatFromBits(ushort w) {
 #endif
 }
 
+#if !defined(__OPENCV_BUILD) && !(defined __STDCPP_FLOAT16_T__) && !(defined __ARM_NEON)
 typedef hfloat float16_t;
+#endif
 
 }
 #endif
