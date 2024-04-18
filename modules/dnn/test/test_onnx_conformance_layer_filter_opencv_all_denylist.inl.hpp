6251f1d80d753f70be2ee656443fc03008a222ae
Abdurrahheem <shtanchaev1994@gmail.com>
2024-04-18 12:31:59 +0400

    comment tests that are passing inside conformance denity list for opencv all backend.

--- modules/dnn/test/test_onnx_conformance_layer_filter_opencv_all_denylist.inl.hpp
+++ modules/dnn/test/test_onnx_conformance_layer_filter_opencv_all_denylist.inl.hpp
@@ -7,10 +7,10 @@
 "test_castlike_FLOAT_to_STRING_expanded",
 "test_castlike_STRING_to_FLOAT_expanded",
 "test_concat_1d_axis_negative_1", // 1d support is required
-"test_div_uint8",  // output type mismatch
-"test_maxpool_2d_dilations",
-"test_maxpool_2d_same_lower",
-"test_maxpool_2d_uint8",  // output type mismatch
+"test_div_uint8",  // output type mismatch //FAILS
+"test_maxpool_2d_dilations", //FAILS
+"test_maxpool_2d_same_lower", //FAILS
+"test_maxpool_2d_uint8",  // output type mismatch //FAILS
 "test_maxpool_with_argmax_2d_precomputed_pads",
 "test_maxpool_with_argmax_2d_precomputed_strides",
 "test_maxunpool_export_with_output_shape",  // exception during net.forward() call
