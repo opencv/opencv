b009a63e6b751191e03cd333306995524cd90d4f
Abduragim Shtanchaev <44877829+Abdurrahheem@users.noreply.github.com>
2024-04-18 17:22:56 +0300

    Merge pull request #25442 from Abdurrahheem:ash/comf-denylist-reduce
    Conformance test denylist reduce #25442
    
    Comment out all passing tests in `test_onnx_conformance_layer_filter_opencv_all_denylist.inl.hpp` file. 
    
    
    ### Pull Request Readiness Checklist
    
    See details at https://github.com/opencv/opencv/wiki/How_to_contribute#making-a-good-pull-request
    
    - [x] I agree to contribute to the project under Apache 2 License.
    - [x] To the best of my knowledge, the proposed patch is not based on a code under GPL or another license that is incompatible with OpenCV
    - [x] The PR is proposed to the proper branch
    - [x] There is a reference to the original bug report and related work
    - [x] There is accuracy test, performance test and test data in opencv_extra repository, if applicable
          Patch to opencv_extra has the same branch name.
    - [x] The feature is well documented and sample code can be built with the project CMake

--- modules/dnn/test/test_onnx_conformance_layer_filter_opencv_all_denylist.inl.hpp
+++ modules/dnn/test/test_onnx_conformance_layer_filter_opencv_all_denylist.inl.hpp
@@ -1,39 +1,4 @@
-"test_add_bcast",
 "test_add_uint8",  // output type mismatch
-#if 1  // output type mismatch CV_32F vs expected 32S
-"test_argmax_default_axis_example",
-"test_argmax_default_axis_example_select_last_index",
-"test_argmax_default_axis_random",
-"test_argmax_default_axis_random_select_last_index",
-"test_argmax_keepdims_example",
-"test_argmax_keepdims_example_select_last_index",
-"test_argmax_keepdims_random",
-"test_argmax_keepdims_random_select_last_index",
-"test_argmax_negative_axis_keepdims_example",
-"test_argmax_negative_axis_keepdims_example_select_last_index",
-"test_argmax_negative_axis_keepdims_random",
-"test_argmax_negative_axis_keepdims_random_select_last_index",
-"test_argmax_no_keepdims_example",
-"test_argmax_no_keepdims_example_select_last_index",
-"test_argmax_no_keepdims_random",
-"test_argmax_no_keepdims_random_select_last_index",
-"test_argmin_default_axis_example",
-"test_argmin_default_axis_example_select_last_index",
-"test_argmin_default_axis_random",
-"test_argmin_default_axis_random_select_last_index",
-"test_argmin_keepdims_example",
-"test_argmin_keepdims_example_select_last_index",
-"test_argmin_keepdims_random",
-"test_argmin_keepdims_random_select_last_index",
-"test_argmin_negative_axis_keepdims_example",
-"test_argmin_negative_axis_keepdims_example_select_last_index",
-"test_argmin_negative_axis_keepdims_random",
-"test_argmin_negative_axis_keepdims_random_select_last_index",
-"test_argmin_no_keepdims_example",
-"test_argmin_no_keepdims_example_select_last_index",
-"test_argmin_no_keepdims_random",
-"test_argmin_no_keepdims_random_select_last_index",
-#endif
 "test_averagepool_2d_pads_count_include_pad",
 "test_averagepool_2d_precomputed_pads_count_include_pad",
 "test_averagepool_2d_same_lower",
@@ -50,8 +15,5 @@
 "test_maxpool_with_argmax_2d_precomputed_strides",
 "test_maxunpool_export_with_output_shape",  // exception during net.forward() call
 "test_mul_uint8",  // output type mismatch
-"test_sub_bcast",
 "test_sub_uint8",  // output type mismatch
 "test_upsample_nearest",
-"test_div_bcast", // remove when 1D Mat is supported
-"test_mul_bcast", // remove when 1D Mat is supported
