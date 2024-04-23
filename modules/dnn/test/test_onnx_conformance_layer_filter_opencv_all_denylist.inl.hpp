89e00bf8b9aa8770d7d599e888dacb0cf03051fc
Abdurrahheem <shtanchaev1994@gmail.com>
2024-04-23 23:36:30 +0400

    add test failure logs to opencv_all_dynylist

--- modules/dnn/test/test_onnx_conformance_layer_filter_opencv_all_denylist.inl.hpp
+++ modules/dnn/test/test_onnx_conformance_layer_filter_opencv_all_denylist.inl.hpp
@@ -1,19 +1,19 @@
-"test_add_uint8",  // output type mismatch
-"test_averagepool_2d_pads_count_include_pad",
-"test_averagepool_2d_precomputed_pads_count_include_pad",
-"test_averagepool_2d_same_lower",
-"test_cast_FLOAT_to_STRING",
-"test_cast_STRING_to_FLOAT",
-"test_castlike_FLOAT_to_STRING_expanded",
-"test_castlike_STRING_to_FLOAT_expanded",
-"test_concat_1d_axis_negative_1", // 1d support is required
-"test_div_uint8",  // output type mismatch //FAILS
-"test_maxpool_2d_dilations", //FAILS
-"test_maxpool_2d_same_lower", //FAILS
-"test_maxpool_2d_uint8",  // output type mismatch //FAILS
-"test_maxpool_with_argmax_2d_precomputed_pads",
-"test_maxpool_with_argmax_2d_precomputed_strides",
-"test_maxunpool_export_with_output_shape",  // exception during net.forward() call
+"test_add_uint8", // output size mismatch in NORMASSERT
+"test_averagepool_2d_pads_count_include_pad",  // wrong output
+"test_averagepool_2d_precomputed_pads_count_include_pad", // wrong output
+"test_averagepool_2d_same_lower", // wrong output
+"test_cast_FLOAT_to_STRING", // Unsupported type in function 'parseCast'
+"test_cast_STRING_to_FLOAT", // unexception during net.forward() call
+"test_castlike_FLOAT_to_STRING_expanded", // Unsupported type in function 'parseCast'
+"test_castlike_STRING_to_FLOAT_expanded", // unexception during net.forward() call
+// "test_concat_1d_axis_negative_1",
+"test_div_uint8",  // output type mismatch
+"test_maxpool_2d_dilations", // output size mismatch in NORMASSERT
+"test_maxpool_2d_same_lower", // wrong output
+"test_maxpool_2d_uint8",  // output type mismatch
+// "test_maxpool_with_argmax_2d_precomputed_pads",
+"test_maxpool_with_argmax_2d_precomputed_strides", // wrong output
+"test_maxunpool_export_with_output_shape",  // unexception during net.forward() call
 "test_mul_uint8",  // output type mismatch
 "test_sub_uint8",  // output type mismatch
-"test_upsample_nearest",
+"test_upsample_nearest", // Dimension mismatch of input
