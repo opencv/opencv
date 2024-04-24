669226cb40effe2eb1645cf8ba6c4d0f12a8db57
Alexander Smorkalov <alexander.smorkalov@xperience.ai>
2024-04-24 10:34:08 +0300

    Code review fixes.

--- modules/dnn/test/test_onnx_conformance_layer_filter_opencv_all_denylist.inl.hpp
+++ modules/dnn/test/test_onnx_conformance_layer_filter_opencv_all_denylist.inl.hpp
@@ -6,12 +6,10 @@
 "test_cast_STRING_to_FLOAT", // unexception during net.forward() call
 "test_castlike_FLOAT_to_STRING_expanded", // Unsupported type in function 'parseCast'
 "test_castlike_STRING_to_FLOAT_expanded", // unexception during net.forward() call
-// "test_concat_1d_axis_negative_1",
 "test_div_uint8",  // output type mismatch
 "test_maxpool_2d_dilations", // output size mismatch in NORMASSERT
 "test_maxpool_2d_same_lower", // wrong output
 "test_maxpool_2d_uint8",  // output type mismatch
-// "test_maxpool_with_argmax_2d_precomputed_pads",
 "test_maxpool_with_argmax_2d_precomputed_strides", // wrong output
 "test_maxunpool_export_with_output_shape",  // unexception during net.forward() call
 "test_mul_uint8",  // output type mismatch
