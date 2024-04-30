8469569986c7f3916098898840a006d32839cd3f
Gursimar Singh <gursimar@bigvisionllc.com>
2024-04-30 13:33:01 +0530

    Added onnx model functionality

---
+++ samples/dnn/edge_detection.cpp
@@ -0,0 +1,199 @@
+#include <opencv2/dnn.hpp>
+#include <opencv2/imgproc.hpp>
+#include <opencv2/highgui.hpp>
+#include <iostream>
+#include <string>
+#include <cmath>
+#include <vector>
+
+// Define namespace to simplify code
+using namespace cv;
+using namespace cv::dnn;
+using namespace std;
+
+struct UserData {
+            Mat gray;
+            int thrs1 = 100;
+            int thrs2 = 200;
+        };
+
+// Preprocess the image
+Mat preprocess(const Mat& img, int imageSize) {
+    Mat resizedImg;
+    resize(img, resizedImg, Size(imageSize, imageSize));
+    resizedImg.convertTo(resizedImg, CV_32F);
+    subtract(resizedImg, Scalar(103.939, 116.779, 123.68), resizedImg);
+    return resizedImg;
+}
+
+// Function to apply sigmoid activation
+static void sigmoid(Mat& input) {
+    exp(-input, input); // e^-input
+    input = 1.0 / (1.0 + input); // 1 / (1 + e^-input)
+}
+
+// Function to process the neural network output to generate edge maps
+pair<Mat, Mat> postProcess(const vector<Mat>& output, int height, int width) {
+    const float epsilon = 1e-12;
+    vector<Mat> preds;
+    preds.reserve(output.size());
+
+    for (const auto& p : output) {
+        Mat img;
+
+        // Correctly handle 4D tensor assuming it's always in the format [1, 1, height, width]
+        Mat processed;
+        if (p.dims == 4 && p.size[0] == 1 && p.size[1] == 1) {
+            // Use only the spatial dimensions
+            processed = p.reshape(0, {p.size[2], p.size[3]});
+        } else {
+            processed = p.clone();
+        }
+
+        sigmoid(processed);
+
+        double minVal, maxVal;
+        minMaxLoc(processed, &minVal, &maxVal); // Find min and max values
+
+        // Normalize the image to [0, 255]
+        img = (processed - minVal) * 255.0 / (maxVal - minVal + epsilon);
+        img.convertTo(img, CV_8U); // Convert to 8-bit image
+
+        resize(img, img, Size(width, height)); // Resize to the original size
+        preds.push_back(img);
+    }
+
+    Mat fuse = preds.back(); // Last element as the fused result
+
+    // Calculate the average of the predictions
+    Mat ave = Mat::zeros(height, width, CV_32F);
+    for (auto& pred : preds) {
+        Mat temp;
+        pred.convertTo(temp, CV_32F);
+        ave += temp;
+    }
+    ave /= preds.size();
+    ave.convertTo(ave, CV_8U);
+
+    return {fuse, ave}; // Return both fused and average edge maps
+}
+
+
+// Callback for the first threshold adjustment
+static void cannyDetectionThresh1(int position, void* userdata) {
+    UserData* data = reinterpret_cast<UserData*>(userdata);
+    Mat output;
+    Canny(data->gray, output, position, data->thrs2);
+    data->thrs1 = position;
+    imshow("Output", output);
+}
+
+// Callback for the second threshold adjustment
+static void cannyDetectionThresh2(int position, void* userdata) {
+    UserData* data = reinterpret_cast<UserData*>(userdata);
+    Mat output;
+    Canny(data->gray, output, data->thrs1, position);
+    data->thrs2 = position;
+    imshow("Output", output);
+}
+
+int main(int argc, char** argv) {
+
+    const string about =
+        "This sample demonstrates edge detection with dexined and canny edge detection techniques.\n"
+        "Script is based on https://github.com/axinc-ai/ailia-models/blob/master/line_segment_detection/dexined/dexined.py"
+        "To download the onnx model, see: https://storage.googleapis.com/ailia-models/dexined/model.onnx";
+
+    const string param_keys =
+        "{ help h          |            | Print help message. }"
+        "{ input i         | baboon.jpg | Path to the input image }"
+        "{ model       |            | Path to the ONNX model. Required. }"
+        "{ method          |   dexined  | Choose methd: dexined or canny}"
+        "{ imageSize       |   512      | Image Size}";
+
+    const string backend_keys = format(
+        "{ backend         | 0 | Choose one of computation backends: "
+                                    "%d: automatically (by default), "
+                                    "%d: Intel's Deep Learning Inference Engine (https://software.intel.com/openvino-toolkit), "
+                                    "%d: OpenCV implementation, "
+                                    "%d: VKCOM, "
+                                    "%d: CUDA, "
+                                    "%d: WebNN }",
+        cv::dnn::DNN_BACKEND_DEFAULT, cv::dnn::DNN_BACKEND_INFERENCE_ENGINE, cv::dnn::DNN_BACKEND_OPENCV,
+        cv::dnn::DNN_BACKEND_VKCOM, cv::dnn::DNN_BACKEND_CUDA, cv::dnn::DNN_BACKEND_WEBNN);
+
+    const string target_keys = format(
+        "{ target          | 0 | Choose one of target computation devices: "
+                              "%d: CPU target (by default), "
+                              "%d: OpenCL, "
+                              "%d: OpenCL fp16 (half-float precision), "
+                              "%d: VPU, "
+                              "%d: Vulkan, "
+                              "%d: CUDA, "
+                              "%d: CUDA fp16 (half-float preprocess) }",
+        cv::dnn::DNN_TARGET_CPU, cv::dnn::DNN_TARGET_OPENCL, cv::dnn::DNN_TARGET_OPENCL_FP16,
+        cv::dnn::DNN_TARGET_MYRIAD, cv::dnn::DNN_TARGET_VULKAN, cv::dnn::DNN_TARGET_CUDA,
+        cv::dnn::DNN_TARGET_CUDA_FP16);
+
+    const string keys = param_keys + backend_keys + target_keys;
+
+    CommandLineParser parser(argc, argv, keys);
+    parser.about(about);
+
+    string inputImagePath = parser.get<string>("input");
+    string onnxModel = parser.get<string>("model");
+    string method =  parser.get<string>("method");
+    int backend = parser.get<int>("backend");
+    int target = parser.get<int>("target");
+    int imageSize =  parser.get<int>("imageSize");
+
+    Mat image = imread(samples::findFile(inputImagePath));
+    if (image.empty()) {
+        cout << "Could not read the image: " << inputImagePath << endl;
+        return 1;
+    }
+
+    if (method == "dexined") {
+        Net net = readNetFromONNX(onnxModel);
+        net.setPreferableBackend(backend);
+        net.setPreferableTarget(target);
+        Mat preprocessed = preprocess(image, imageSize);
+
+        Mat blob = blobFromImage(preprocessed);
+        net.setInput(blob);
+
+        Mat result = net.forward();
+
+        vector<Mat> outputs;
+        net.forward(outputs); // Get all output layers
+        int originalWidth = image.cols;
+        int originalHeight = image.rows;
+        auto [fusedOutput, averageOutput] = postProcess(outputs, originalHeight, originalWidth);
+
+        imshow("Input", image);
+        imshow("Output", fusedOutput);
+        waitKey(0);
+    } else if (method == "canny") {
+        Mat gray;
+        cvtColor(image, gray, COLOR_BGR2GRAY);
+
+        UserData user_data;
+        user_data.gray = gray;
+
+        namedWindow("Output", WINDOW_NORMAL);
+        namedWindow("Input", WINDOW_NORMAL);
+
+         // Create trackbars
+        createTrackbar("thrs1", "Output",0, 255, cannyDetectionThresh1, &user_data);
+        createTrackbar("thrs2", "Output",0, 255, cannyDetectionThresh2, &user_data);
+
+        // Set initial positions of trackbars
+        setTrackbarPos("thrs1", "Output", 100);
+        setTrackbarPos("thrs2", "Output", 200);
+
+        imshow("Input", image);
+        waitKey(0);
+    }
+
+    return 0;
+}
--- samples/dnn/edge_detection.py
+++ samples/dnn/edge_detection.py
@@ -1,69 +1,155 @@
+# Script is based on https://github.com/axinc-ai/ailia-models/blob/master/line_segment_detection/dexined/dexined.py
+# To download the onnx model, see: https://storage.googleapis.com/ailia-models/dexined/model.onnx
+# python edge_detection.py --model model.onnx --input chicky_512.png --method dexined
 import cv2 as cv
 import argparse
+import numpy as np
 
-parser = argparse.ArgumentParser(
-        description='This sample shows how to define custom OpenCV deep learning layers in Python. '
-                    'Holistically-Nested Edge Detection (https://arxiv.org/abs/1504.06375) neural network '
-                    'is used as an example model. Find a pre-trained model at https://github.com/s9xie/hed.')
-parser.add_argument('--input', help='Path to image or video. Skip to capture frames from camera')
-parser.add_argument('--prototxt', help='Path to deploy.prototxt', required=True)
-parser.add_argument('--caffemodel', help='Path to hed_pretrained_bsds.caffemodel', required=True)
-parser.add_argument('--width', help='Resize input image to a specific width', default=500, type=int)
-parser.add_argument('--height', help='Resize input image to a specific height', default=500, type=int)
-args = parser.parse_args()
+def parse_args():
+     # Define supported computation backends and target devices for OpenCV DNN
+    backends = (cv.dnn.DNN_BACKEND_DEFAULT, cv.dnn.DNN_BACKEND_INFERENCE_ENGINE,
+                    cv.dnn.DNN_BACKEND_OPENCV, cv.dnn.DNN_BACKEND_VKCOM, cv.dnn.DNN_BACKEND_CUDA)
 
-#! [CropLayer]
-class CropLayer(object):
-    def __init__(self, params, blobs):
-        self.xstart = 0
-        self.xend = 0
-        self.ystart = 0
-        self.yend = 0
+    targets = (cv.dnn.DNN_TARGET_CPU, cv.dnn.DNN_TARGET_OPENCL, cv.dnn.DNN_TARGET_OPENCL_FP16, cv.dnn.DNN_TARGET_MYRIAD,
+                cv.dnn.DNN_TARGET_HDDL, cv.dnn.DNN_TARGET_VULKAN, cv.dnn.DNN_TARGET_CUDA, cv.dnn.DNN_TARGET_CUDA_FP16)
 
-    # Our layer receives two inputs. We need to crop the first input blob
-    # to match a shape of the second one (keeping batch size and number of channels)
-    def getMemoryShapes(self, inputs):
-        inputShape, targetShape = inputs[0], inputs[1]
-        batchSize, numChannels = inputShape[0], inputShape[1]
-        height, width = targetShape[2], targetShape[3]
+    # Define supported edge detection methods
+    methods = ("dexined", "canny")
 
-        self.ystart = (inputShape[2] - targetShape[2]) // 2
-        self.xstart = (inputShape[3] - targetShape[3]) // 2
-        self.yend = self.ystart + height
-        self.xend = self.xstart + width
+    # Set up argument parser
+    parser = argparse.ArgumentParser(
+            description='This sample shows how to use the '
+                        'Dense Extreme Inception Network for Edge Detection (Dexened Edge Detection) (https://arxiv.org/abs/2112.02250)'
+                        'model in Python. Find the pre-trained model at https://storage.googleapis.com/ailia-models/dexined/model.onnx')
 
-        return [[batchSize, numChannels, height, width]]
+    parser.add_argument('--input', help='Path to image', default="chicky_512.png", required= False)
 
-    def forward(self, inputs):
-        return [inputs[0][:,:,self.ystart:self.yend,self.xstart:self.xend]]
-#! [CropLayer]
+    parser.add_argument('--model', help='Path to onnx model', required=True)
 
-#! [Register]
-cv.dnn_registerLayer('Crop', CropLayer)
-#! [Register]
+    parser.add_argument('--backend', choices=backends, default=cv.dnn.DNN_BACKEND_DEFAULT, type=int,
+                            help="Choose one of computation backends: "
+                                "%d: automatically (by default), "
+                                "%d: Intel's Deep Learning Inference Engine (https://software.intel.com/openvino-toolkit), "
+                                "%d: OpenCV implementation, "
+                                "%d: VKCOM, "
+                                "%d: CUDA" % backends)
 
-# Load the model.
-net = cv.dnn.readNet(cv.samples.findFile(args.prototxt), cv.samples.findFile(args.caffemodel))
+    parser.add_argument('--target', choices=targets, default=cv.dnn.DNN_TARGET_CPU, type=int,
+                            help='Choose one of target computation devices: '
+                                '%d: CPU target (by default), '
+                                '%d: OpenCL, '
+                                '%d: OpenCL fp16 (half-float precision), '
+                                '%d: NCS2 VPU, '
+                                '%d: HDDL VPU, '
+                                '%d: Vulkan, '
+                                '%d: CUDA, '
+                                '%d: CUDA fp16 (half-float preprocess)'% targets)
 
-kWinName = 'Holistically-Nested Edge Detection'
-cv.namedWindow('Input', cv.WINDOW_NORMAL)
-cv.namedWindow(kWinName, cv.WINDOW_NORMAL)
+    parser.add_argument('--image_size', help='Resize input image to a size', default=512, type=int)
 
-cap = cv.VideoCapture(args.input if args.input else 0)
-while cv.waitKey(1) < 0:
-    hasFrame, frame = cap.read()
-    if not hasFrame:
-        cv.waitKey()
-        break
+    parser.add_argument('--method', choices=methods, default= "dexined", type=str,
+                        help = 'Choose one of the method : '
+                        '%s: dexined, '
+                        '%s: canny'% methods)
 
-    cv.imshow('Input', frame)
+    args = parser.parse_args()
 
-    inp = cv.dnn.blobFromImage(frame, scalefactor=1.0, size=(args.width, args.height),
-                               mean=(104.00698793, 116.66876762, 122.67891434),
-                               swapRB=False, crop=False)
-    net.setInput(inp)
+    return args
 
-    out = net.forward()
-    out = out[0, 0]
-    out = cv.resize(out, (frame.shape[1], frame.shape[0]))
-    cv.imshow(kWinName, out)
+def preprocess(img):
+    # Resize and normalize the image
+    im_h, im_w, _ = img.shape
+
+    IMAGE_SIZE = args.image_size
+    img = cv.resize(img, (IMAGE_SIZE, IMAGE_SIZE))
+
+    img = img.astype(np.float32)
+    img[:, :, 0] = (img[:, :, 0] - 103.939)
+    img[:, :, 1] = (img[:, :, 1] - 116.779)
+    img[:, :, 2] = (img[:, :, 2] - 123.68)
+
+    return img
+
+def sigmoid(x):
+    # Apply the sigmoid function
+    return 1.0 / (1.0 + np.exp(-x))
+
+def post_processing(output, shape):
+    # Process network output to generate edge maps
+    h, w = shape
+
+    preds = []
+    epsilon = 1e-12
+    for p in output:
+        img = sigmoid(p)
+        img = np.squeeze(img)
+        img = (img - np.min(img)) * 255 / \
+              ((np.max(img) - np.min(img)) + epsilon)
+        img = img.astype(np.uint8)
+        img = cv.resize(img, (w, h))
+        preds.append(img)
+
+    fuse = preds[-1]
+
+    ave = np.array(preds, dtype=np.float32)
+    ave = np.uint8(np.mean(ave, axis=0))
+
+    return fuse, ave
+
+def canny_detection_thresh1(position, user_data):
+    # Threshold adjustment callback for Canny edge detection
+    out = cv.Canny(user_data["gray"],position,user_data["thrs2"])
+    user_data["thrs1"] =  position
+
+    cv.imshow('Output', out)
+
+def canny_detection_thresh2(position, user_data):
+    # Threshold adjustment callback for Canny edge detection
+    out = cv.Canny(user_data["gray"],user_data["thrs1"],position)
+    user_data["thrs2"] =  position
+    cv.imshow('Output', out)
+
+
+if __name__ == '__main__':
+    args = parse_args()
+    method = args.method
+    user_data = {"gray": None, "thrs1": 100, "thrs2": 200}
+
+    cv.namedWindow('Output', cv.WINDOW_NORMAL)
+    cv.namedWindow('Input', cv.WINDOW_NORMAL)
+
+    if method == "canny":
+        # Load and process the image for Canny edge detection
+        img = cv.imread(cv.samples.findFile(args.input))
+        gray = cv.cvtColor(img, cv.COLOR_BGR2GRAY)
+
+        user_data["gray"] = gray
+
+        cv.createTrackbar('thrs1', 'Output', 0, 255, lambda value: canny_detection_thresh1(value, user_data))
+        cv.setTrackbarPos('thrs1', 'Output', 100)
+        cv.createTrackbar('thrs2', 'Output', 0, 255, lambda value: canny_detection_thresh2(value, user_data))
+        cv.setTrackbarPos('thrs2', 'Output', 200)
+        cv.imshow("Input", img)
+        cv.waitKey(0)
+
+    elif method == "dexined":
+        # Load the model
+        session = cv.dnn.readNetFromONNX(args.model)
+        session.setPreferableBackend(args.backend)
+        session.setPreferableTarget(args.target)
+        # Load and process the image using DexiNed model
+        orig_img = cv.imread(cv.samples.findFile(args.input))
+        # Prepocess the image
+        img = preprocess(orig_img)
+
+        inp = cv.dnn.blobFromImage(img, swapRB=False, crop=False)
+
+        session.setInput(inp)
+
+        out = session.forward()
+        # Post processing on the model output
+        out = post_processing(out, (orig_img.shape[1], orig_img.shape[0]))
+
+        cv.imshow("Input", orig_img)
+        cv.imshow("Output", out[1])
+        cv.waitKey(0)
