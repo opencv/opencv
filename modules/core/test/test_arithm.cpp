1785822e0aece615ae925d0ca76eec0b44630f9b
Rostislav Vasilikhin <rostislav.vasilikhin@opencv.ai>
2024-05-29 17:58:53 +0200

    non-continuous test

--- modules/core/test/test_arithm.cpp
+++ modules/core/test/test_arithm.cpp
@@ -2881,6 +2881,28 @@ TEST(Core_MinMaxIdx, rows_overflow)
     }
 }
 
+TEST(Core_MinMaxIdx, non_continuous)
+{
+    Mat big(32, 32, CV_32FC1, Scalar(15));
+    Mat small = big(Range(8, 8+16), Range(8, 8+16));
+
+    small = 50;
+    small.at<float>(8, 8) = 100;
+    small.at<float>(9, 9) = 25;
+
+    double minVal = 0, maxVal = 0;
+    int minIdx[CV_MAX_DIM] = { 0 }, maxIdx[CV_MAX_DIM] = { 0 };
+    cv::minMaxIdx(small, &minVal, &maxVal, minIdx, maxIdx);
+
+    ASSERT_FLOAT_EQ(minVal, 25);
+    ASSERT_FLOAT_EQ(maxVal, 100);
+
+    ASSERT_EQ(minIdx[0], 9);
+    ASSERT_EQ(minIdx[1], 9);
+    ASSERT_EQ(maxIdx[0], 8);
+    ASSERT_EQ(maxIdx[1], 8);
+}
+
 TEST(Core_Magnitude, regression_19506)
 {
     for (int N = 1; N <= 64; ++N)
