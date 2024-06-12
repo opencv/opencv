e186539c404f0ca84f6a39af0b669ae26d35ae8e
Junyan721113 <llh721113@outlook.com>
2024-06-12 16:01:43 +0800

    feat: Part 1.5 - New Interfaces

--- 3rdparty/ndsrvp/ndsrvp_hal.hpp
+++ 3rdparty/ndsrvp/ndsrvp_hal.hpp
@@ -1,12 +1,13 @@
 // This file is part of OpenCV project.
 // It is subject to the license terms in the LICENSE file found in the top-level directory
-// of this distribution and at http://opencv.org/license.html.	
+// of this distribution and at http://opencv.org/license.html.
 
 #ifndef OPENCV_NDSRVP_HAL_HPP
 #define OPENCV_NDSRVP_HAL_HPP
 
-#include "opencv2/core/mat.hpp"
 #include <nds_intrinsic.h>
+
+#include "opencv2/core/hal/interface.h"
 
 #include "include/core.hpp"
 #include "include/imgproc.hpp"
