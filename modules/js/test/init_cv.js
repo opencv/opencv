fa65e15b26603e5049ec811d490678d0d11e6aa3
Dmitry Kurtaev <dmitry.kurtaev@gmail.com>
2024-06-15 11:01:05 +0300

    Update init_cv.js

--- modules/js/test/init_cv.js
+++ modules/js/test/init_cv.js
@@ -2,21 +2,18 @@
 // It is subject to the license terms in the LICENSE file found in the top-level directory
 // of this distribution and at http://opencv.org/license.html.
 
-if (cv.getBuildInformation === undefined) {
-    // WASM
-    QUnit.test("init_cv", (assert) => {
-        if (cv instanceof Promise) {
-            const done = assert.async();
-            cv.then((ready_cv) => {
-                cv = ready_cv;
-                done();
-            });
-        } else {
-            const done = assert.async();
-            cv['onRuntimeInitialized'] = () => {
-                done();
-            }
+QUnit.test("init_cv", (assert) => {
+    if (cv instanceof Promise) {
+        const done = assert.async();
+        cv.then((ready_cv) => {
+            cv = ready_cv;
+            done();
+        });
+    } else if (cv.getBuildInformation === undefined) {
+        const done = assert.async();
+        cv['onRuntimeInitialized'] = () => {
+            done();
         }
-        assert.ok(true);
-    });
-}
+    }
+    assert.ok(true);
+});
