0de5a51dd9e751de607d5939098428d9642569e5
kozinov.e <evgeniy.kozinov@gmail.com>
2024-06-19 12:50:26 +0000

    fixed trailing whitespace~

--- modules/highgui/cmake/detect_framebuffer.cmake
+++ modules/highgui/cmake/detect_framebuffer.cmake
@@ -8,7 +8,7 @@ if(WITH_FRAMEBUFFER OR WITH_FRAMEBUFFER_XVFB)
     if(HAVE_FRAMEBUFFER_XVFB)
       message(STATUS "Check virtual framebuffer - done")
     else()
-      message(STATUS 
+      message(STATUS
       "Check virtual framebuffer - faild\n"
       "Please install the xorg-x11-proto-devel or x11proto-dev package\n")
     endif()
