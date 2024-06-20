3386df5247f33e207c08fc11feabdd1513895bfd
Kadian <ujjayant.kadian@intel.com>
2024-06-20 11:34:50 +0100

    Addressed some docstring comments

--- modules/gapi/include/opencv2/gapi/infer/onnx.hpp
+++ modules/gapi/include/opencv2/gapi/infer/onnx.hpp
@@ -637,7 +637,7 @@ public:
 
     /** @brief Configures session options for ONNX Runtime.
 
-    This function allows setting various session options for the ONNX Runtime
+    This function is used for setting various session options for the ONNX Runtime
     session by accepting a map of key-value pairs.
 
     @param options A map of session option to be applied to the ONNX Runtime session.
@@ -675,7 +675,26 @@ public:
     @param model_path path to model file (.onnx file).
     */
     Params(const std::string& tag, const std::string& model_path)
-        : desc{model_path, 0u, 0u, {}, {}, {}, {}, {}, {}, {}, {}, {}, true, {}, {}, {}, {}, false}, m_tag(tag) {}
+        : desc{
+            model_path, // model_path
+            0u,         // num_in
+            0u,         // num_out
+            {},         // input_names
+            {},         // output_names
+            {},         // const_inputs
+            {},         // mean
+            {},         // stdev
+            {},         // out_metas
+            {},         // custom_post_proc
+            {},         // normalize
+            {},         // names_to_remap
+            true,       // is_generic
+            {},         // generic_mstd
+            {},         // generic_norm
+            {},         // session_options
+            {},         // execution_providers
+            false       // disable_mem_pattern
+        }, m_tag(tag) {}
 
     /** @see onnx::Params::cfgMeanStdDev. */
     void cfgMeanStdDev(const std::string &layer,
