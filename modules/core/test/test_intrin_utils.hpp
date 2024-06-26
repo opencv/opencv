772cfb6c87549951a3990befea7a84163d404427
Wanli <wanli.zhong.1999@gmail.com>
2024-06-26 15:33:35 +0800

    refine test

--- modules/core/test/test_intrin_utils.hpp
+++ modules/core/test/test_intrin_utils.hpp
@@ -1716,7 +1716,12 @@ template<typename R> struct TheTest
             for (int j = 0; j < n; ++j) {
                 SCOPED_TRACE(cv::format("Overflow/Underflow test value: %f", i));
                 EXPECT_TRUE(resOverflow[j] > 0 && std::isinf(resOverflow[j]));
-                EXPECT_EQ(0, resUnderflow[j]);
+                if(resUnderflow[j] != 0)
+                    // standard implementation
+                    EXPECT_EQ(std::exp(dataLowerBound[j]), resUnderflow[j]);
+                else
+                    // universal intrinsics implementation
+                    EXPECT_EQ(0, resUnderflow[j]);
             }
         }
 
