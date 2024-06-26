acaaefdec398e52533abbddf409613c70097fa2f
kaingwade <jiawu83@gmail.com>
2024-06-26 11:11:03 +0800

    Revert "Add orbbec_astra.cpp into videocapture_depth.cpp"
    This reverts commit f5d6ca9d6cf2e080d44f172dbef80bbcb54ad28d.

--- samples/cpp/videocapture_depth.cpp
+++ samples/cpp/videocapture_depth.cpp
@@ -2,29 +2,11 @@
 #include "opencv2/highgui.hpp"
 #include "opencv2/imgproc.hpp"
 
-#include <list>
 #include <iostream>
-
-#if defined(HAVE_THREADS)
-#define USE_THREADS 1
-#endif
-
-#ifdef USE_THREADS
-#include <thread>
-#include <mutex>
-#include <condition_variable>
-#include <atomic>
-#endif
 
 using namespace cv;
 using namespace std;
 
-// Stores frames along with their timestamps
-struct Frame
-{
-    int64 timestamp;
-    Mat frame;
-};
 
 static void colorizeDisparity(const Mat& gray, Mat& rgb, double maxDisp=-1.f)
 {
@@ -55,13 +37,13 @@ static float getMaxDisparity(VideoCapture& capture)
     return b * F / minDistance;
 }
 
-static int camOpenni()
+static void openni_cam()
 {
     VideoCapture capture(CAP_OPENNI2);
    if (!capture.isOpened())
    {
-        cerr << "ERROR: Failed to open a capture object." << endl;
-        return -1;
+        cerr << "Failed to open a capture object." << endl;
+        return;
    }
 
     capture.set(CAP_OPENNI_IMAGE_GENERATOR_OUTPUT_MODE, CAP_OPENNI_VGA_30HZ);
@@ -78,8 +60,8 @@ static int camOpenni()
     {
         if (!capture.grab())
         {
-            cerr << "ERROR: Can not grab images." << endl;
-            return -1;
+            cerr << "Can not grab images." << endl;
+            return;
         }
         else
         {
@@ -115,196 +97,15 @@ static int camOpenni()
         if(waitKey(30) >= 0)
             break;
     }
-
-    return 0;
 }
 
-static int camOpenniOrbbec()
-{
-    //! [Open streams]
-    // Open depth stream
-    VideoCapture depthStream(CAP_OPENNI2_ASTRA);
-    // Open color stream
-    VideoCapture colorStream(0, CAP_V4L2);
-    //! [Open streams]
-
-    // Check that stream has opened
-    if (!colorStream.isOpened())
-    {
-        cerr << "ERROR: Unable to open color stream" << endl;
-        return -1;
-    }
-
-    // Check that stream has opened
-    if (!depthStream.isOpened())
-    {
-        cerr << "ERROR: Unable to open depth stream" << endl;
-        return -1;
-    }
-
-    //! [Setup streams]
-    // Set color and depth stream parameters
-    colorStream.set(CAP_PROP_FRAME_WIDTH,  640);
-    colorStream.set(CAP_PROP_FRAME_HEIGHT, 480);
-    depthStream.set(CAP_PROP_FRAME_WIDTH,  640);
-    depthStream.set(CAP_PROP_FRAME_HEIGHT, 480);
-    depthStream.set(CAP_PROP_OPENNI2_MIRROR, 0);
-    //! [Setup streams]
-
-    // Print color stream parameters
-    cout << "Color stream: "
-         << colorStream.get(CAP_PROP_FRAME_WIDTH) << "x" << colorStream.get(CAP_PROP_FRAME_HEIGHT)
-         << " @" << colorStream.get(CAP_PROP_FPS) << " fps" << endl;
-
-    //! [Get properties]
-    // Print depth stream parameters
-    cout << "Depth stream: "
-         << depthStream.get(CAP_PROP_FRAME_WIDTH) << "x" << depthStream.get(CAP_PROP_FRAME_HEIGHT)
-         << " @" << depthStream.get(CAP_PROP_FPS) << " fps" << endl;
-    //! [Get properties]
-
-    //! [Read streams]
-    // Create two lists to store frames
-    std::list<Frame> depthFrames, colorFrames;
-    const std::size_t maxFrames = 64;
-
-    // Synchronization objects
-    std::mutex mtx;
-    std::condition_variable dataReady;
-    std::atomic<bool> isFinish;
-
-    isFinish = false;
-
-    // Start depth reading thread
-    std::thread depthReader([&]
-    {
-        while (!isFinish)
-        {
-            // Grab and decode new frame
-            if (depthStream.grab())
-            {
-                Frame f;
-                f.timestamp = cv::getTickCount();
-                depthStream.retrieve(f.frame, CAP_OPENNI_DEPTH_MAP);
-                if (f.frame.empty())
-                {
-                    cerr << "ERROR: Failed to decode frame from depth stream" << endl;
-                    break;
-                }
-
-                {
-                    std::lock_guard<std::mutex> lk(mtx);
-                    if (depthFrames.size() >= maxFrames)
-                        depthFrames.pop_front();
-                    depthFrames.push_back(f);
-                }
-                dataReady.notify_one();
-            }
-        }
-    });
-
-    // Start color reading thread
-    std::thread colorReader([&]
-    {
-        while (!isFinish)
-        {
-            // Grab and decode new frame
-            if (colorStream.grab())
-            {
-                Frame f;
-                f.timestamp = cv::getTickCount();
-                colorStream.retrieve(f.frame);
-                if (f.frame.empty())
-                {
-                    cerr << "ERROR: Failed to decode frame from color stream" << endl;
-                    break;
-                }
-
-                {
-                    std::lock_guard<std::mutex> lk(mtx);
-                    if (colorFrames.size() >= maxFrames)
-                        colorFrames.pop_front();
-                    colorFrames.push_back(f);
-                }
-                dataReady.notify_one();
-            }
-        }
-    });
-    //! [Read streams]
-
-    //! [Pair frames]
-    // Pair depth and color frames
-    while (!isFinish)
-    {
-        std::unique_lock<std::mutex> lk(mtx);
-        while (!isFinish && (depthFrames.empty() || colorFrames.empty()))
-            dataReady.wait(lk);
-
-        while (!depthFrames.empty() && !colorFrames.empty())
-        {
-            if (!lk.owns_lock())
-                lk.lock();
-
-            // Get a frame from the list
-            Frame depthFrame = depthFrames.front();
-            int64 depthT = depthFrame.timestamp;
-
-            // Get a frame from the list
-            Frame colorFrame = colorFrames.front();
-            int64 colorT = colorFrame.timestamp;
-
-            // Half of frame period is a maximum time diff between frames
-            const int64 maxTdiff = int64(1000000000 / (2 * colorStream.get(CAP_PROP_FPS)));
-            if (depthT + maxTdiff < colorT)
-            {
-                depthFrames.pop_front();
-                continue;
-            }
-            else if (colorT + maxTdiff < depthT)
-            {
-                colorFrames.pop_front();
-                continue;
-            }
-            depthFrames.pop_front();
-            colorFrames.pop_front();
-            lk.unlock();
-
-            //! [Show frames]
-            // Show depth frame
-            Mat d8, dColor;
-            depthFrame.frame.convertTo(d8, CV_8U, 255.0 / 2500);
-            applyColorMap(d8, dColor, COLORMAP_OCEAN);
-            imshow("Depth (colored)", dColor);
-
-            // Show color frame
-            imshow("Color", colorFrame.frame);
-            //! [Show frames]
-
-            // Exit on Esc key press
-            int key = waitKey(1);
-            if (key == 27) // ESC
-            {
-                isFinish = true;
-                break;
-            }
-        }
-    }
-    //! [Pair frames]
-
-    dataReady.notify_one();
-    depthReader.join();
-    colorReader.join();
-
-    return 0;
-}
-
-static int camRealsense()
+static void realsense_cam()
 {
    VideoCapture capture(CAP_INTELPERC);
    if (!capture.isOpened())
    {
-        cerr << "ERROR: Failed to open RealSense camera." << endl;
-        return -1;
+        cerr << "Failed to open RealSense camera." << endl;
+        return;
    }
 
     Mat depthMap;
@@ -333,17 +134,15 @@ static int camRealsense()
         if(waitKey(30) >= 0)
             break;
     }
-
-    return 0;
 }
 
-static int camOrbbec()
+static void orbbec_cam()
 {
     VideoCapture capture(0, CAP_OBSENSOR);
     if(!capture.isOpened())
     {
-        cerr << "ERROR: Failed to open Orbbec camera."  << endl;
-        return -1;
+        cerr << "Failed to open Orbbec camera."  << endl;
+        return;
     }
 
     // get the intrinsic parameters of Orbbec camera
@@ -379,18 +178,15 @@ static int camOrbbec()
         if(waitKey(30) >= 0)
             break;
     }
-
-    return 0;
 }
 
-const char* keys = "{camType | | Camera Type: OpenNI, OpenNI-Ob, RealSense, Orbbec}";
+const char* keys = "{camType | | Camera Type: OpenNI, RealSense, Orbbec}";
 
 const char* about =
             "\nThis example demostrates how to get data from 3D cameras via OpenCV.\n"
             "Currently OpenCV supports 3 types of 3D cameras:\n"
-            "1. Depth sensors compatible with OpenNI:\n"
-            "\t - Kinect, XtionPRO. Users must install OpenNI library and PrimeSensorModule for OpenNI and configure OpenCV with WITH_OPENNI flag ON in CMake.\n"
-            "\t - Orbbec Astra Series. Users must install Orbbec OpenNI SDK and configure accordingly.\n"
+            "1. Depth sensors compatible with OpenNI (Kinect, XtionPRO). "
+            "Users must install OpenNI library and PrimeSensorModule for OpenNI and configure OpenCV with WITH_OPENNI flag ON in CMake.\n"
             "2. Depth sensors compatible with Intel® RealSense SDK (RealSense). "
             "Users must install Intel RealSense SDK 2.0 and configure OpenCV with WITH_LIBREALSENSE flag ON in CMake.\n"
             "3. Orbbec depth sensors.\n";
@@ -408,28 +204,19 @@ int main(int argc, char *argv[])
     String camType = parser.get<String>("camType");
     if (camType == "OpenNI")
     {
-        camOpenni();
-    }
-    else if (camType == "OpenNI-Ob")
-    {
-#ifdef USE_THREADS
-        camOpenniOrbbec();
-#else
-        cerr << "ERROR: No threading support. Sample code is disabled." << endl;
-        return -1;
-#endif
+        openni_cam();
     }
     else if (camType == "RealSense")
     {
-        camRealsense();
+        realsense_cam();
     }
     else if (camType == "Orbbec")
     {
-        camOrbbec();
+        orbbec_cam();
     }
     else
     {
-        cerr << "ERROR: Invalid input." << endl;
+        cerr << "Invalid input." << endl;
         return -1;
     }
 
