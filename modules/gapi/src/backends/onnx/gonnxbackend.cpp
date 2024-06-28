341874a292f9a3766a1d73515312daa250ea240e
lamiayous <lamia.yous@intel.com>
2024-06-28 10:04:30 +0100

    Revert "Changing GAPI ASSERT message"
    This reverts commit dd242b3c07cb36cc108b9dd5a931766cd9a7d5d4.

--- modules/gapi/src/backends/onnx/gonnxbackend.cpp
+++ modules/gapi/src/backends/onnx/gonnxbackend.cpp
@@ -332,7 +332,7 @@ inline void preprocess(const cv::Mat& src,
         if (tensor_dims.size() == ti.dims.size()) {
             for (size_t i = 0; i < ti.dims.size(); ++i) {
                 GAPI_Assert((ti.dims[i] == -1 || ti.dims[i] == tensor_dims[i]) &&
-                            "non-U8 tensor dimensions should match with all non-dynamic NN input dimensions");
+                            "32F tensor dimensions should match with all non-dynamic NN input dimensions");
             }
         } else {
             GAPI_Error("32F tensor size should match with NN input");
@@ -470,8 +470,7 @@ inline Ort::Value createTensor(const Ort::MemoryInfo& memory_info,
     case ONNX_TENSOR_ELEMENT_DATA_TYPE_INT32:
         return createTensor<int32_t>(memory_info, tensor_params, data);
     case ONNX_TENSOR_ELEMENT_DATA_TYPE_INT64:
-        // Conversion to int64 is done later
-        return createTensor<int32_t>(memory_info, tensor_params, data); 
+        return createTensor<int64_t>(memory_info, tensor_params, data);
     default:
         GAPI_Error("ONNX. Unsupported data type");
     }
