2e2330a3fc41f5dc7c2b009d46c6fa36e058c846
Gursimar Singh <gursimar@bigvisionllc.com>
2024-07-10 17:05:30 +0530

    Reverted changes for opencv_version.cpp

--- samples/cpp/opencv_version.cpp
+++ samples/cpp/opencv_version.cpp
@@ -1,35 +1,17 @@
-/**
-@file opencv_version.cpp
-@brief a program to print version, build configuration and environment details
-@modified by Gursimar Singh
-*/
+// This file is part of OpenCV project.
+// It is subject to the license terms in the LICENSE file found in the top-level directory
+// of this distribution and at http://opencv.org/license.html
 
 #include <opencv2/core/utility.hpp>
-#include <opencv2/core.hpp>
 #include <iostream>
-#include <thread>
 
-#ifdef _WIN32
-#include <windows.h>
-#elif defined(__linux__) || defined(__unix__)
-#include <unistd.h>
-#endif
-
-// Check if OpenCV is built with CUDA support
-#ifdef HAVE_OPENCV_CUDAARITHM
-#include <opencv2/core/cuda.hpp>
-#endif
-
-static const std::string keys =
-    "{ b build   | | print complete build info              }"
-    "{ e env     | | print information about the environment, CPU, GPU, etc. }"
-    "{ h help    | | print this help                        }";
+static const std::string keys = "{ b build | | print complete build info }"
+                                "{ h help  | | print this help           }";
 
 int main(int argc, const char* argv[])
 {
     cv::CommandLineParser parser(argc, argv, keys);
-    parser.about("This sample outputs OpenCV version, build configuration, and environment info.");
-
+    parser.about("This sample outputs OpenCV version and build configuration.");
     if (parser.has("help"))
     {
         parser.printMessage();
@@ -41,31 +23,6 @@ int main(int argc, const char* argv[])
     else if (parser.has("build"))
     {
         std::cout << cv::getBuildInformation() << std::endl;
-    }
-    else if (parser.has("env"))
-    {
-        unsigned int nThreads = std::thread::hardware_concurrency();
-        std::cout << "Number of concurrent threads supported: " << nThreads << std::endl;
-
-        int nProcessors = cv::getNumThreads();
-        std::cout << "Number of logical processors: " << nProcessors << std::endl;
-
-        #ifdef HAVE_OPENCV_CUDAARITHM
-        std::cout << "Environment: GPU" << std::endl;
-        std::cout << "Number of OpenCV-supported GPUs: " << cv::cuda::getCudaEnabledDeviceCount() << std::endl;
-        if (cv::cuda::getCudaEnabledDeviceCount() > 0)
-        {
-            cv::cuda::DeviceInfo deviceInfo(0); // Querying the first GPU
-            std::cout << "GPU Name: " << deviceInfo.name() << std::endl;
-            std::cout << "Is integrated: " << (deviceInfo.integrated() ? "True" : "False") << std::endl;
-            std::cout << "Supports OpenGL: " << (deviceInfo.canMapHostMemory() ? "True" : "False") << std::endl;
-            std::cout << "Compute Compatibility: " << deviceInfo.majorVersion()<<"."<< deviceInfo.minorVersion() << std::endl;
-            std::cout << "Total Memory: " << deviceInfo.totalMemory() / (1024.0 * 1024.0) << " MB" << std::endl;
-
-        }
-        #else
-        std::cout << "Environment: CPU" << std::endl;
-        #endif
     }
     else
     {
