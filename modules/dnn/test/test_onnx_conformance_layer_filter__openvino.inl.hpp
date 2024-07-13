66a635149959174fb656b4a13d48dcbb102717fc
DaniAffCH <danieleaffinita2000@gmail.com>
2024-07-13 10:19:40 +0200

    filter out unsupported backends

--- modules/dnn/test/test_onnx_conformance_layer_filter__openvino.inl.hpp
+++ modules/dnn/test/test_onnx_conformance_layer_filter__openvino.inl.hpp
@@ -551,9 +551,11 @@ CASE(test_depthtospace_dcr_mode)
 CASE(test_depthtospace_example)
     // no filter
 CASE(test_dequantizelinear)
-    // no filter
+    SKIP;
 CASE(test_dequantizelinear_axis)
-    // no filter
+    SKIP;
+CASE(test_dequantizelinear_blocked)
+    SKIP;
 CASE(test_det_2d)
     // no filter
 CASE(test_det_nd)
@@ -1290,9 +1292,11 @@ CASE(test_qlinearmatmul_2D)
 CASE(test_qlinearmatmul_3D)
     // no filter
 CASE(test_quantizelinear)
-    // no filter
+    SKIP;
 CASE(test_quantizelinear_axis)
-    // no filter
+    SKIP;
+CASE(test_quantizelinear_blocked)
+    SKIP;
 CASE(test_range_float_type_positive_delta)
     // no filter
 CASE(test_range_float_type_positive_delta_expanded)
