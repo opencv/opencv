259ac2bf4a63a8a2204a093ed1f730468fcd496e
Yuantao Feng <yuantao.feng@outlook.com>
2024-07-14 17:25:32 +0800

    fix cuda tests

--- modules/dnn/test/test_onnx_conformance_layer_filter__cuda_fp16_denylist.inl.hpp
+++ modules/dnn/test/test_onnx_conformance_layer_filter__cuda_fp16_denylist.inl.hpp
@@ -7,6 +7,8 @@
 "test_dropout_default_ratio",
 "test_logsoftmax_large_number", // fp16 accuracy issue
 "test_logsoftmax_large_number_expanded", // fp16 accuracy issue
+"test_maxpool_with_argmax_2d_precomputed_pads", // assertion failed mat.type() == CV_32F
+"test_nllloss_NCd1d2d3d4d5_none_no_weight_expanded", // crash: https://github.com/opencv/opencv/issues/25471
 "test_reduce_prod_default_axes_keepdims_example", // fallback to cpu, accuracy
 "test_reduce_prod_default_axes_keepdims_random", // fallback to cpu, accuracy
 "test_reduce_sum_square_default_axes_keepdims_random", // fallback to cpu, accuracy
