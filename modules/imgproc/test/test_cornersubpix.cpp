59af066b9bc95bdc28877c619a438c5a89c1d91c
Kumataro <Kumataro@users.noreply.github.com>
2024-08-13 09:32:16 +0900

    imgproc: use float32 to determine whether the corners points are within src

--- modules/imgproc/test/test_cornersubpix.cpp
+++ modules/imgproc/test/test_cornersubpix.cpp
@@ -62,7 +62,34 @@ TEST(Imgproc_CornerSubPix, out_of_image_corners)
     cv::cornerSubPix(image, corners, win, zeroZone, criteria);
 
     ASSERT_EQ(corners.size(), 1u);
-    ASSERT_TRUE(Rect(0, 0, image.cols, image.rows).contains(corners.front()));
+    ASSERT_TRUE(Rect2f(0, 0, image.cols, image.rows).contains(corners.front()));
+}
+
+// See https://github.com/opencv/opencv/issues/26016
+TEST(Imgproc_CornerSubPix, corners_on_the_edge)
+{
+    cv::Mat image(500, 500, CV_8UC1);
+    cv::Size win(1, 1);
+    cv::Size zeroZone(-1, -1);
+    cv::TermCriteria criteria;
+
+    std::vector<cv::Point2f> cornersOK1 = { cv::Point2f(250, std::nextafter(499.5f, 499.5f - 1.0f)) };
+    EXPECT_NO_THROW( cv::cornerSubPix(image, cornersOK1, win, zeroZone, criteria) ) << cornersOK1;
+
+    std::vector<cv::Point2f> cornersOK2 = { cv::Point2f(250, 499.5f) };
+    EXPECT_NO_THROW( cv::cornerSubPix(image, cornersOK2, win, zeroZone, criteria) ) << cornersOK2;
+
+    std::vector<cv::Point2f> cornersOK3 = { cv::Point2f(250, std::nextafter(499.5f, 499.5f + 1.0f)) };
+    EXPECT_NO_THROW( cv::cornerSubPix(image, cornersOK3, win, zeroZone, criteria) ) << cornersOK3;
+
+    std::vector<cv::Point2f> cornersOK4 = { cv::Point2f(250, std::nextafter(500.0f, 500.0f - 1.0f)) };
+    EXPECT_NO_THROW( cv::cornerSubPix(image, cornersOK4, win, zeroZone, criteria) ) << cornersOK4;
+
+    std::vector<cv::Point2f> cornersNG1 = { cv::Point2f(250, 500.0f) };
+    EXPECT_ANY_THROW( cv::cornerSubPix(image, cornersNG1, win, zeroZone, criteria) ) << cornersNG1;
+
+    std::vector<cv::Point2f> cornersNG2 = { cv::Point2f(250, std::nextafter(500.0f, 500.0f + 1.0f)) };
+    EXPECT_ANY_THROW( cv::cornerSubPix(image, cornersNG2, win, zeroZone, criteria) ) << cornersNG2;
 }
 
 }} // namespace
