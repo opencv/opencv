bee9df726c5a44c9e2501ca687413587417782ee
Maksim Shabunin <maksim.shabunin@gmail.com>
2024-08-27 18:13:18 +0300

    restore dispatch for aarch64, share base flags between cross and native compilation

--- platforms/linux/riscv64-gcc.toolchain.cmake
+++ platforms/linux/riscv64-gcc.toolchain.cmake
@@ -1,16 +1,6 @@
 set(CMAKE_SYSTEM_NAME Linux)
-set(CMAKE_SYSTEM_VERSION 1)
+set(CMAKE_SYSTEM_PROCESSOR riscv64)
 set(GNU_MACHINE riscv64-unknown-linux-gnu CACHE STRING "GNU compiler triple")
 
-message(STATUS "!!! ENABLE_RVV=${ENABLE_RVV}")
-message(STATUS "!!! RISCV_RVV_SCALABLE=${RISCV_RVV_SCALABLE}")
-
-set(PLATFORM_STR "rv64gc")
-if(ENABLE_RVV OR RISCV_RVV_SCALABLE)
-  set(PLATFORM_STR "rv64gcv")
-endif()
-
-set(CMAKE_C_FLAGS_INIT "-march=${PLATFORM_STR}")
-set(CMAKE_CXX_FLAGS_INIT "-march=${PLATFORM_STR}")
-
+include("${CMAKE_CURRENT_LIST_DIR}/flags-riscv64.cmake")
 include("${CMAKE_CURRENT_LIST_DIR}/riscv-gnu.toolchain.cmake")
