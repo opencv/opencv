7824199cf3fda786cc037dde499a68fb93edc5c2
Alexander Smorkalov <alexander.smorkalov@xperience.ai>
2024-10-09 15:08:38 +0300

    Code review fixes.

---
+++ modules/features2d/misc/python/test/test_algorithm_rw.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python
+"""Algorithm serialization test."""
+import tempfile
+import os
+import cv2 as cv
+from tests_common import NewOpenCVTests
+
+
+class algorithm_rw_test(NewOpenCVTests):
+    def test_algorithm_rw(self):
+        fd, fname = tempfile.mkstemp(prefix="opencv_python_algorithm_", suffix=".yml")
+        os.close(fd)
+
+        # some arbitrary non-default parameters
+        gold = cv.features2d.ORB_create(nfeatures=200, scaleFactor=1.3f, nlevels=5, edgeThreshold=28)
+        gold.write(cv.FileStorage(fname, cv.FILE_STORAGE_WRITE), "ORB")
+
+        fs = cv.FileStorage(fname, cv.FILE_STORAGE_READ)
+        algorithm = cv.features2d.ORB_create()
+        algorithm.read(fs.getNode("ORB"))
+
+        self.assertEqual(algorithm.getMaxFeatures(), 200)
+        self.assertEqual(algorithm.getScaleFactor(), 1.3f)
+        self.assertEqual(algorithm.getNLevels(), 5)
+        self.assertEqual(algorithm.getEdgeThreshold(), 28)
+
+        os.remove(fname)
