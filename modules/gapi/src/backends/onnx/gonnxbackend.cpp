c1be0c95dae3dcc0abc28e3c0ee991f69e1f494b
Septimiu Neaga <septimiu.neaga@intel.com>
2024-10-10 15:28:12 +0300

    Adding optimization level configuration for ONNX runtime

--- modules/gapi/src/backends/onnx/gonnxbackend.cpp
+++ modules/gapi/src/backends/onnx/gonnxbackend.cpp
@@ -701,6 +701,31 @@ namespace cv {
 namespace gimpl {
 namespace onnx {
 
+static GraphOptimizationLevel convertToGraphOptimizationLevel(const int opt_level) {
+    switch (opt_level) {
+    case ORT_DISABLE_ALL:
+        return ORT_DISABLE_ALL;
+        break;
+    case ORT_ENABLE_BASIC:
+        return ORT_ENABLE_BASIC;
+        break;
+    case ORT_ENABLE_EXTENDED:
+        return ORT_ENABLE_EXTENDED;
+        break;
+    case ORT_ENABLE_ALL:
+        return ORT_ENABLE_ALL;
+        break;
+    default:
+        if (opt_level > ORT_ENABLE_ALL) {  // relax constraint
+            return ORT_ENABLE_ALL;
+        }
+        else {
+            cv::util::throw_error(std::invalid_argument("Invalid argument opt_level = " + std::to_string(opt_level)));
+            return ORT_ENABLE_ALL;
+        }
+    }
+}
+
 ONNXCompiled::ONNXCompiled(const gapi::onnx::detail::ParamDesc &pp)
     : params(pp) {
     // Validate input parameters before allocating any resources
@@ -725,6 +750,10 @@ ONNXCompiled::ONNXCompiled(const gapi::onnx::detail::ParamDesc &pp)
 
     if (pp.disable_mem_pattern) {
         session_options.DisableMemPattern();
+    }
+
+    if (pp.opt_level.has_value()) {
+        session_options.SetGraphOptimizationLevel(convertToGraphOptimizationLevel(pp.opt_level.value()));
     }
     this_env = Ort::Env(ORT_LOGGING_LEVEL_WARNING, "");
 #ifndef _WIN32
