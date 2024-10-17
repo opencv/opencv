2155417a3072ad105facd0367c7671cc5cff45fa
Alexander Smorkalov <alexander.smorkalov@xperience.ai>
2024-10-17 16:16:40 +0300

    Finally dropped convertFp16 function in favor cv::Mat::convertTo().

--- modules/core/src/convert.dispatch.cpp
+++ modules/core/src/convert.dispatch.cpp
@@ -53,34 +53,6 @@ BinaryFunc getConvertFunc(int sdepth, int ddepth)
 }
 
 #ifdef HAVE_OPENCL
-static bool ocl_convertFp16( InputArray _src, OutputArray _dst, int sdepth, int ddepth )
-{
-    int type = _src.type(), cn = CV_MAT_CN(type);
-
-    _dst.createSameSize( _src, CV_MAKETYPE(ddepth, cn) );
-    int kercn = 1;
-    int rowsPerWI = 1;
-    String build_opt = format("-D HALF_SUPPORT -D srcT=%s -D dstT=%s -D rowsPerWI=%d%s",
-                              sdepth == CV_32F ? "float" : "half",
-                              sdepth == CV_32F ? "half" : "float",
-                              rowsPerWI,
-                              sdepth == CV_32F ? " -D FLOAT_TO_HALF " : "");
-    ocl::Kernel k(sdepth == CV_32F ? "convertFp16_FP32_to_FP16" : "convertFp16_FP16_to_FP32", ocl::core::halfconvert_oclsrc, build_opt);
-    if (k.empty())
-        return false;
-
-    UMat src = _src.getUMat();
-    UMat dst = _dst.getUMat();
-
-    ocl::KernelArg srcarg = ocl::KernelArg::ReadOnlyNoSize(src),
-    dstarg = ocl::KernelArg::WriteOnly(dst, cn, kercn);
-
-    k.args(srcarg, dstarg);
-
-    size_t globalsize[2] = { (size_t)src.cols * cn / kercn, ((size_t)src.rows + rowsPerWI - 1) / rowsPerWI };
-    return k.run(2, globalsize, NULL, false);
-}
-
 static bool ocl_convertTo(InputArray src_, OutputArray dst_, int ddepth, bool noScale, double alpha, double beta)
 {
     CV_INSTRUMENT_REGION();
@@ -252,67 +224,6 @@ void UMat::convertTo(OutputArray dst, int type_, double alpha, double beta) cons
     Mat m = getMat(ACCESS_READ);
     m.convertTo(dst, type_, alpha, beta);
     (void)src;
-}
-
-//==================================================================================================
-
-void convertFp16(InputArray _src, OutputArray _dst)
-{
-    CV_INSTRUMENT_REGION();
-
-    int sdepth = _src.depth(), ddepth = 0;
-    BinaryFunc func = 0;
-
-    switch( sdepth )
-    {
-    case CV_32F:
-        if(_dst.fixedType())
-        {
-            ddepth = _dst.depth();
-            CV_Assert(ddepth == CV_16S || ddepth == CV_16F);
-            CV_Assert(_dst.channels() == _src.channels());
-        }
-        else
-            ddepth =  CV_16S;
-        func = getConvertFunc(CV_32F, CV_16F);
-        break;
-    case CV_16S:
-    case CV_16F:
-        ddepth = CV_32F;
-        func = getConvertFunc(CV_16F, CV_32F);
-        break;
-    default:
-        CV_Error(Error::StsUnsupportedFormat, "Unsupported input depth");
-        return;
-    }
-
-    CV_OCL_RUN(_src.dims() <= 2 && _dst.isUMat(),
-               ocl_convertFp16(_src, _dst, sdepth, ddepth))
-
-    Mat src = _src.getMat();
-
-    int type = CV_MAKETYPE(ddepth, src.channels());
-    _dst.create( src.dims, src.size, type );
-    Mat dst = _dst.getMat();
-    int cn = src.channels();
-
-    CV_Assert( func != 0 );
-
-    if( src.dims <= 2 )
-    {
-        Size sz = getContinuousSize2D(src, dst, cn);
-        func( src.data, src.step, 0, 0, dst.data, dst.step, sz, 0);
-    }
-    else
-    {
-        const Mat* arrays[] = {&src, &dst, 0};
-        uchar* ptrs[2] = {};
-        NAryMatIterator it(arrays, ptrs);
-        Size sz((int)(it.size*cn), 1);
-
-        for( size_t i = 0; i < it.nplanes; i++, ++it )
-            func(ptrs[0], 0, 0, 0, ptrs[1], 0, sz, 0);
-    }
 }
 
 } // namespace cv
