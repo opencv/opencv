aed3c125d14b7fdfa5fed6907f06ef4faf30be52
pumaraymond <raymond12raymond@gmail.com>
2024-11-11 15:07:34 +0800

    testing

--- apps/traincascade/traincascade.cpp
+++ apps/traincascade/traincascade.cpp
@@ -9,7 +9,8 @@ traincascade.cpp is the source file of the program used for cascade training.
 User has to provide training input in form of positive and negative training images,
 and other data related to training in form of command line argument.
 */
-//testing
+//testing1
+//testing2
 int main( int argc, char* argv[] )
 {
     CvCascadeClassifier classifier;
