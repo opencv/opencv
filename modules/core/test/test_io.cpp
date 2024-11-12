c2f2d46a8e4e18cf2813b7ef7f01a366d4ec1185
Dmitry Kurtaev <dmitry.kurtaev@gmail.com>
2024-11-12 17:51:10 +0300

    Merge pull request #26446 from dkurt:file_storage_empty_and_1d_mat
    * Change style of empty and 1d Mat in FileStorage
    
    * Remove misleading 1d Mat test

--- modules/core/test/test_io.cpp
+++ modules/core/test/test_io.cpp
@@ -2018,7 +2018,25 @@ T fsWriteRead(const T& expectedValue, const char* ext)
     return value;
 }
 
+void testExactMat(const Mat& src, const char* ext)
+{
+    bool srcIsEmpty = src.empty();
+    Mat dst = fsWriteRead(src, ext);
+    EXPECT_EQ(dst.empty(), srcIsEmpty);
+    EXPECT_EQ(src.dims, dst.dims);
+    EXPECT_EQ(src.size, dst.size);
+    if (!srcIsEmpty)
+    {
+        EXPECT_EQ(0.0, cv::norm(src, dst, NORM_INF));
+    }
+}
+
 typedef testing::TestWithParam<const char*> FileStorage_exact_type;
+TEST_P(FileStorage_exact_type, empty_mat)
+{
+    testExactMat(Mat(), GetParam());
+}
+
 TEST_P(FileStorage_exact_type, long_int)
 {
     for (const int64_t expected : std::vector<int64_t>{INT64_MAX, INT64_MIN, -1, 1, 0})
