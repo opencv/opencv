0a6e75812a169494beb1e108f04ece9671194ecc
Abdurrahheem <abduragim.shtanchaev@xperience.ai>
2024-11-25 06:40:05 +0000

    enable int64 type in python bindings

--- modules/python/src2/cv2_convert.cpp
+++ modules/python/src2/cv2_convert.cpp
@@ -135,6 +135,7 @@ bool pyopencv_to(PyObject* o, Mat& m, const ArgInfo& info)
                typenum == NPY_SHORT ? CV_16S :
                typenum == NPY_INT ? CV_32S :
                typenum == NPY_INT32 ? CV_32S :
+               typenum == NPY_INT64 ? CV_64S :
                typenum == NPY_HALF ? CV_16F :
                typenum == NPY_FLOAT ? CV_32F :
                typenum == NPY_DOUBLE ? CV_64F :
