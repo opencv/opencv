e5766e810a8fa4c0e99e2a9f0526d3188568d148
Maksim Shabunin <maksim.shabunin@gmail.com>
2024-11-28 19:20:14 +0300

    Fixed several cases of unaligned pointer cast

--- modules/imgcodecs/src/utils.hpp
+++ modules/imgcodecs/src/utils.hpp
@@ -137,7 +137,11 @@ uchar* FillGrayRow1( uchar* data, uchar* indices, int len, uchar* palette );
 
 CV_INLINE bool  isBigEndian( void )
 {
-    return (((const int*)"\0\x1\x2\x3\x4\x5\x6\x7")[0] & 255) != 0;
+#ifdef WORDS_BIGENDIAN
+    return true;
+#else
+    return false;
+#endif
 }
 
 }  // namespace
