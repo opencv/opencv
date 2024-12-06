13abe1a666f7d7409ccdd7bb71600884fc7a7a69
MurtazaSaherwala <murtazasaherwala516@gmail.com>
2024-12-06 22:14:42 +0530

    oops4

--- samples/cpp/tutorial_code/HighGUI/AddingImagesTrackbar.cpp
+++ samples/cpp/tutorial_code/HighGUI/AddingImagesTrackbar.cpp
@@ -65,13 +65,13 @@ int main(void)
     namedWindow("Linear Blend", WINDOW_AUTOSIZE);//Create Window
     //![window]
 
-    //![Create_trackbar]
+    //![create_trackbar]
     char TrackbarName[50];
     snprintf(TrackbarName, sizeof(TrackbarName), "Alpha x %d", alpha_slider_max);
     // Example userdata: Pass a pointer to an integer as userdata
     int user_value = 42; // Arbitrary value for demonstration
     createTrackbar(TrackbarName, "Linear Blend", &alpha_slider, alpha_slider_max, on_trackbar, &user_value);
-    //![Create_trackbar]
+    //![create_trackbar]
 
     /// Show initial result
     on_trackbar(alpha_slider, nullptr);
