f823143766172c8f331901246ee39f837b9f04a2
FantasqueX <fantasquex@gmail.com>
2024-12-07 02:29:20 +0800

    Clean up cmake after upgrading cmake_minimum_required to 3.13

--- cmake/OpenCVFindLibsPerf.cmake
+++ cmake/OpenCVFindLibsPerf.cmake
@@ -55,10 +55,7 @@ endif(WITH_CUDA)
 
 # --- Eigen ---
 if(WITH_EIGEN AND NOT HAVE_EIGEN)
-  if((OPENCV_FORCE_EIGEN_FIND_PACKAGE_CONFIG
-      OR NOT (CMAKE_VERSION VERSION_LESS "3.0.0")  # Eigen3Targets.cmake required CMake 3.0.0+
-      ) AND NOT OPENCV_SKIP_EIGEN_FIND_PACKAGE_CONFIG
-  )
+  if(NOT OPENCV_SKIP_EIGEN_FIND_PACKAGE_CONFIG)
     find_package(Eigen3 CONFIG QUIET)  # Ceres 2.0.0 CMake scripts doesn't work with CMake's FindEigen3.cmake module (due to missing EIGEN3_VERSION_STRING)
   endif()
   if(NOT Eigen3_FOUND)
