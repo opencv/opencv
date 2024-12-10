4b2c042fc04561fdb0f6d9d0b6af59f13c425bd7
Rüdiger Ihle <r.ihle@s-t.de>
2024-12-10 07:51:41 +0100

    videoio(Android): Make exposure settings become effective immediately
    Fixes the issue, that CAP_PROP_AUTO_EXPOSURE, CAP_PROP_EXPOSURE and CAP_PROP_ISO_SPEED
    were not commited to the device until the next resolution change. Introduce a common
    mechanism for such changes also to be used by CAP_PROP_ANDROID_DEVICE_TORCH.

--- modules/videoio/src/cap_android_camera.cpp
+++ modules/videoio/src/cap_android_camera.cpp
@@ -38,18 +38,20 @@ using namespace cv;
 #define FOURCC_YV12 CV_FOURCC_MACRO('Y','V','1','2')
 #define FOURCC_UNKNOWN  0xFFFFFFFF
 
-template <typename T> class RangeValue {
-public:
+template <typename T> struct RangeValue {
     T min, max;
     /**
      * return absolute value from relative value
      * * value: in percent (50 for 50%)
      * */
     T value(int percent) {
-        return static_cast<T>(min + (max - min) * percent / 100);
+        return static_cast<T>(min + ((max - min) * percent) / 100);
     }
-    RangeValue() { min = max = static_cast<T>(0); }
-    bool Supported(void) const { return (min != max); }
+    RangeValue(T minv = 0, T maxv = 0) : min(minv), max(maxv) {}
+    bool Supported() const { return (min != max); }
+    T clamp( T value ) const {
+        return (value > max) ? max : ((value < min) ? min : value);
+    }
 };
 
 static inline void deleter_ACameraManager(ACameraManager *cameraManager) {
@@ -165,8 +167,7 @@ void OnCaptureFailed(void* context,
  *     on camera. For this sample purpose, clamp to a range showing visible
  *     video on preview: 100000ns ~ 250000000ns
  */
-static const long kMinExposureTime = 1000000L;
-static const long kMaxExposureTime = 250000000L;
+static const RangeValue<int64_t> exposureTimeLimits = { 1000000, 250000000 };
 
 static double elapsedTimeFrom(std::chrono::time_point<std::chrono::system_clock> start) {
     return std::chrono::duration<double>(std::chrono::system_clock::now() - start).count();
@@ -199,8 +200,8 @@ class AndroidCameraCapture : public IVideoCapture
     int desiredWidth = 640;
     int desiredHeight = 480;
     uint8_t flashMode = ACAMERA_FLASH_MODE_OFF;
-    bool autoExposure = true;
-    int64_t exposureTime = 0L;
+    uint8_t aeMode = ACAMERA_CONTROL_AE_MODE_ON;
+    int64_t exposureTime = 0;
     RangeValue<int64_t> exposureRange;
     int32_t sensitivity = 0;
     RangeValue<int32_t> sensitivityRange;
@@ -409,7 +410,7 @@ public:
             case CAP_PROP_FRAME_HEIGHT:
                 return isOpened() ? frameHeight : desiredHeight;
             case CAP_PROP_AUTO_EXPOSURE:
-                return autoExposure ? 1 : 0;
+                return (aeMode == ACAMERA_CONTROL_AE_MODE_ON) ? 1 : 0;
             case CAP_PROP_EXPOSURE:
                 return exposureTime;
             case CAP_PROP_ISO_SPEED:
@@ -481,35 +482,29 @@ public:
                     }
                 }
             case CAP_PROP_AUTO_EXPOSURE:
-                autoExposure = (value != 0);
+                aeMode = (value != 0) ? ACAMERA_CONTROL_AE_MODE_ON : ACAMERA_CONTROL_AE_MODE_OFF;
                 if (isOpened()) {
-                    uint8_t aeMode = autoExposure ? ACAMERA_CONTROL_AE_MODE_ON : ACAMERA_CONTROL_AE_MODE_OFF;
-                    camera_status_t status = ACaptureRequest_setEntry_u8(captureRequest.get(), ACAMERA_CONTROL_AE_MODE, 1, &aeMode);
-                    return status == ACAMERA_OK;
+                    return submitRequest(ACaptureRequest_setEntry_u8, ACAMERA_CONTROL_AE_MODE, aeMode);
                 }
                 return true;
             case CAP_PROP_EXPOSURE:
                 if (isOpened() && exposureRange.Supported()) {
-                    exposureTime = (int64_t)value;
+                    exposureTime = exposureRange.clamp(static_cast<int64_t>(value));
                     LOGI("Setting CAP_PROP_EXPOSURE will have no effect unless CAP_PROP_AUTO_EXPOSURE is off");
-                    camera_status_t status = ACaptureRequest_setEntry_i64(captureRequest.get(), ACAMERA_SENSOR_EXPOSURE_TIME, 1, &exposureTime);
-                    return status == ACAMERA_OK;
+                    return submitRequest(ACaptureRequest_setEntry_i64, ACAMERA_SENSOR_EXPOSURE_TIME, exposureTime);
                 }
                 return false;
             case CAP_PROP_ISO_SPEED:
                 if (isOpened() && sensitivityRange.Supported()) {
-                    sensitivity = (int32_t)value;
+                    sensitivity = sensitivityRange.clamp(static_cast<int32_t>(value));
                     LOGI("Setting CAP_PROP_ISO_SPEED will have no effect unless CAP_PROP_AUTO_EXPOSURE is off");
-                    camera_status_t status = ACaptureRequest_setEntry_i32(captureRequest.get(), ACAMERA_SENSOR_SENSITIVITY, 1, &sensitivity);
-                    return status == ACAMERA_OK;
+                    return submitRequest(ACaptureRequest_setEntry_i32, ACAMERA_SENSOR_SENSITIVITY, sensitivity);
                 }
                 return false;
             case CAP_PROP_ANDROID_DEVICE_TORCH:
                 flashMode = (value != 0) ? ACAMERA_FLASH_MODE_TORCH : ACAMERA_FLASH_MODE_OFF;
                 if (isOpened()) {
-                    ACaptureRequest *request = captureRequest.get();
-                    return ACaptureRequest_setEntry_u8(request, ACAMERA_FLASH_MODE, 1, &flashMode) == ACAMERA_OK &&
-                           ACameraCaptureSession_setRepeatingRequest(captureSession.get(), GetCaptureCallback(), 1, &request, nullptr) == ACAMERA_OK;
+                    return submitRequest(ACaptureRequest_setEntry_u8, ACAMERA_FLASH_MODE, flashMode);
                 }
                 return true;
             default:
@@ -572,7 +567,7 @@ public:
             return false;
         }
         std::shared_ptr<ACameraMetadata> cameraMetadata = std::shared_ptr<ACameraMetadata>(metadata, deleter_ACameraMetadata);
-        ACameraMetadata_const_entry entry;
+        ACameraMetadata_const_entry entry = {};
         ACameraMetadata_getConstEntry(cameraMetadata.get(), ACAMERA_SCALER_AVAILABLE_STREAM_CONFIGURATIONS, &entry);
 
         double bestScore = std::numeric_limits<double>::max();
@@ -605,25 +600,19 @@ public:
         }
         LOGI("Best resolution match: %dx%d", bestMatchWidth, bestMatchHeight);
 
-        ACameraMetadata_const_entry val = { 0, };
-        camera_status_t status = ACameraMetadata_getConstEntry(cameraMetadata.get(), ACAMERA_SENSOR_INFO_EXPOSURE_TIME_RANGE, &val);
-        if (status == ACAMERA_OK) {
-            exposureRange.min = val.data.i64[0];
-            if (exposureRange.min < kMinExposureTime) {
-                exposureRange.min = kMinExposureTime;
-            }
-            exposureRange.max = val.data.i64[1];
-            if (exposureRange.max > kMaxExposureTime) {
-                exposureRange.max = kMaxExposureTime;
-            }
+        ACameraMetadata_const_entry val;
+        cStatus = ACameraMetadata_getConstEntry(cameraMetadata.get(), ACAMERA_SENSOR_INFO_EXPOSURE_TIME_RANGE, &val);
+        if (cStatus == ACAMERA_OK) {
+            exposureRange.min = exposureTimeLimits.clamp(val.data.i64[0]);
+            exposureRange.max = exposureTimeLimits.clamp(val.data.i64[1]);
             exposureTime = exposureRange.value(2);
         } else {
             LOGW("Unsupported ACAMERA_SENSOR_INFO_EXPOSURE_TIME_RANGE");
-            exposureRange.min = exposureRange.max = 0l;
-            exposureTime = 0l;
+            exposureRange.min = exposureRange.max = 0;
+            exposureTime = 0;
         }
-        status = ACameraMetadata_getConstEntry(cameraMetadata.get(), ACAMERA_SENSOR_INFO_SENSITIVITY_RANGE, &val);
-        if (status == ACAMERA_OK){
+        cStatus = ACameraMetadata_getConstEntry(cameraMetadata.get(), ACAMERA_SENSOR_INFO_SENSITIVITY_RANGE, &val);
+        if (cStatus == ACAMERA_OK){
             sensitivityRange.min = val.data.i32[0];
             sensitivityRange.max = val.data.i32[1];
             sensitivity = sensitivityRange.value(2);
@@ -704,10 +693,10 @@ public:
             return false;
         }
         captureSession = std::shared_ptr<ACameraCaptureSession>(session, deleter_ACameraCaptureSession);
-        uint8_t aeMode = autoExposure ? ACAMERA_CONTROL_AE_MODE_ON : ACAMERA_CONTROL_AE_MODE_OFF;
+
         ACaptureRequest_setEntry_u8(captureRequest.get(), ACAMERA_CONTROL_AE_MODE, 1, &aeMode);
         ACaptureRequest_setEntry_i32(captureRequest.get(), ACAMERA_SENSOR_SENSITIVITY, 1, &sensitivity);
-        if (!autoExposure) {
+        if (aeMode != ACAMERA_CONTROL_AE_MODE_ON) {
             ACaptureRequest_setEntry_i64(captureRequest.get(), ACAMERA_SENSOR_EXPOSURE_TIME, 1, &exposureTime);
         }
         ACaptureRequest_setEntry_u8(captureRequest.get(), ACAMERA_FLASH_MODE, 1, &flashMode);
@@ -743,6 +732,18 @@ public:
         cameraDevice = nullptr;
         cameraManager = nullptr;
         imageReader = nullptr;
+    }
+
+    template<typename FuncT, typename T>
+    bool submitRequest(FuncT setFn, uint32_t tag, const T &data)
+    {
+        ACaptureRequest *request = captureRequest.get();
+
+        return request &&
+               setFn(request, tag, 1, &data) == ACAMERA_OK &&
+               ACameraCaptureSession_setRepeatingRequest(captureSession.get(),
+                                                         GetCaptureCallback(),
+                                                         1, &request, nullptr) == ACAMERA_OK;
     }
 };
 
