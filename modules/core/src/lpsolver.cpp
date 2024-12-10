7d50ef7d9f7742dc14e3c21c5824b611c368c189
shyama7004 <bisoyi7004@gmail.com>
2024-12-10 16:51:50 +0530

    fixed typo

--- modules/core/src/lpsolver.cpp
+++ modules/core/src/lpsolver.cpp
@@ -16,10 +16,10 @@
 // Redistribution and use in source and binary forms, with or without modification,
 // are permitted provided that the following conditions are met:
 //
-//   * Redistribution's of source code must retain the above copyright notice,
+//   * Redistributions of source code must retain the above copyright notice,
 //     this list of conditions and the following disclaimer.
 //
-//   * Redistribution's in binary form must reproduce the above copyright notice,
+//   * Redistributions in binary form must reproduce the above copyright notice,
 //     this list of conditions and the following disclaimer in the documentation
 //     and/or other materials provided with the distribution.
 //
