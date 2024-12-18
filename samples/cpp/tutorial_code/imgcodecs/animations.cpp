8ea9fbb9a3270945f9f83801f8d1765a14c33c0c
Suleyman TURKMEN <sturkmen@hotmail.com>
2024-12-18 20:36:26 +0300

    Update animations.cpp

--- samples/cpp/tutorial_code/imgcodecs/animations.cpp
+++ samples/cpp/tutorial_code/imgcodecs/animations.cpp
@@ -14,12 +14,12 @@ int main( int argc, const char** argv )
     {
         Animation animation_to_save;
         Mat image(128, 256, CV_8UC4, Scalar(150, 150, 150, 255));
-        int timestamp = 200;
+        int duration = 200;
 
         for (int i = 0; i < 10; ++i) {
             animation_to_save.frames.push_back(image.clone());
             putText(animation_to_save.frames[i], format("Frame %d", i), Point(30, 80), FONT_HERSHEY_SIMPLEX, 1.5, Scalar(255, 100, 0, 255), 2);
-            animation_to_save.timestamps.push_back(timestamp);
+            animation_to_save.durations.push_back(duration);
         }
         imwriteanimation("animated_image.webp", animation_to_save, { IMWRITE_WEBP_QUALITY, 100 });
     }
@@ -38,7 +38,7 @@ int main( int argc, const char** argv )
     while (true)
     for (size_t i = 0; i < animation.frames.size(); ++i) {
         imshow("Animation", animation.frames[i]);
-        int key_code = waitKey(animation.timestamps[i]); // Delay between frames
+        int key_code = waitKey(animation.durations[i]); // Delay between frames
         if (key_code == 27)
             exit(0);
     }
