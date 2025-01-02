d3092deb4f4cb405c6e99ef8866cd0686733011a
MaximSmolskiy <mithridatus@mail.ru>
2025-01-02 22:29:53 +0300

    Fix and add tests for ellipse fitting

--- modules/imgproc/test/test_fitellipse.cpp
+++ modules/imgproc/test/test_fitellipse.cpp
@@ -21,15 +21,19 @@ static bool check_pt_in_ellipse(const Point2f& pt, const RotatedRect& el) {
 
 // Return true if mass center of fitted points lies inside ellipse
 static bool fit_and_check_ellipse(const vector<Point2f>& pts) {
-    RotatedRect ellipse = fitEllipseDirect(pts); // fitEllipseAMS() also works fine
-
     Point2f mass_center;
     for (size_t i = 0; i < pts.size(); i++) {
         mass_center += pts[i];
     }
     mass_center /= (float)pts.size();
 
-    return check_pt_in_ellipse(mass_center, ellipse);
+    for (const auto& fit_ellipse : {fitEllipse, fitEllipseAMS, fitEllipseDirect}) {
+        const RotatedRect ellipse = fit_ellipse(pts);
+        if (!check_pt_in_ellipse(mass_center, ellipse)) {
+            return false;
+        }
+    }
+    return true;
 }
 
 TEST(Imgproc_FitEllipse_Issue_4515, accuracy) {
@@ -100,6 +104,56 @@ TEST(Imgproc_FitEllipse_JavaCase, accuracy) {
     EXPECT_NEAR(e.center.y, 0, 0.01);
     EXPECT_NEAR(e.size.width, sqrt(2.)*2, 0.4);
     EXPECT_NEAR(e.size.height, sqrt(2.)*2, 0.4);
+}
+
+TEST(Imgproc_FitEllipse_Issue_26078, accuracy) {
+    vector<Point2f> pts({
+        {1434, 308}, {1434, 309}, {1433, 310}, {1427, 310}, {1427, 312}, {1426, 313}, {1422, 313}, {1422, 314},
+        {1421, 315}, {1415, 315}, {1415, 316}, {1414, 317}, {1408, 317}, {1408, 319}, {1407, 320}, {1403, 320},
+        {1403, 321}, {1402, 322}, {1396, 322}, {1396, 323}, {1395, 324}, {1389, 324}, {1389, 326}, {1388, 327},
+        {1382, 327}, {1382, 328}, {1381, 329}, {1376, 329}, {1376, 330}, {1375, 331}, {1369, 331}, {1369, 333},
+        {1368, 334}, {1362, 334}, {1362, 335}, {1361, 336}, {1359, 336}, {1359, 1016}, {1365, 1016}, {1366, 1017},
+        {1366, 1019}, {1430, 1019}, {1430, 1017}, {1431, 1016}, {1440, 1016}, {1440, 308},
+    });
+
+    EXPECT_TRUE(fit_and_check_ellipse(pts));
+}
+
+TEST(Imgproc_FitEllipse_Issue_26360_1, accuracy) {
+    vector<Point2f> pts({
+        {37, 111}, {37, 112}, {36, 113}, {37, 114}, {37, 115}, {37, 116}, {37, 117}, {37, 118},
+        {36, 119}, {35, 120}, {34, 120}, {33, 121}, {32, 121}, {31, 121}, {30, 122}, {29, 123},
+        {28, 123}, {27, 124}, {26, 124}, {25, 125}, {25, 126}, {25, 127}, {25, 128}, {25, 129},
+        {254, 121}, {255, 120}, {255, 119}, {256, 118}, {256, 117}, {256, 116}, {256, 115}, {256, 114},
+        {256, 113}, {256, 112}, {256, 111}, {256, 110}, {256, 109}, {256, 108}, {256, 107}, {257, 106},
+        {257, 105}, {256, 104}, {257, 103}, {257, 102}, {257, 101},
+    });
+
+    EXPECT_TRUE(fit_and_check_ellipse(pts));
+}
+
+TEST(Imgproc_FitEllipse_Issue_26360_2, accuracy) {
+    vector<Point2f> pts({
+        {37, 105}, {38, 106}, {38, 107}, {25, 129}, {254, 121}, {257, 103}, {257, 102}, {257, 101},
+    });
+
+    EXPECT_TRUE(fit_and_check_ellipse(pts));
+}
+
+TEST(Imgproc_FitEllipse_Issue_26360_3, accuracy) {
+    vector<Point2f> pts({
+        {37, 105}, {38, 106}, {38, 107}, {257, 103}, {257, 102}, {257, 101},
+    });
+
+    EXPECT_TRUE(fit_and_check_ellipse(pts));
+}
+
+TEST(Imgproc_FitEllipse_Issue_26360_4, accuracy) {
+    vector<Point2f> pts({
+        {30, 105}, {25, 110}, {30, 115}, {250, 105}, {255, 110}, {250, 115},
+    });
+
+    EXPECT_TRUE(fit_and_check_ellipse(pts));
 }
 
 }} // namespace
