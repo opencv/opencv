b96e9fd1ce774713770c6df67ecd7d513fbfa984
Kumataro <Kumataro@users.noreply.github.com>
2025-01-03 13:33:54 +0900

    imgcodecs: drop bundled OpenEXR

--- modules/imgcodecs/src/grfmt_exr.cpp
+++ modules/imgcodecs/src/grfmt_exr.cpp
@@ -83,27 +83,6 @@
 namespace cv
 {
 
-static bool isOpenEXREnabled()
-{
-    static const bool PARAM_ENABLE_OPENEXR = utils::getConfigurationParameterBool("OPENCV_IO_ENABLE_OPENEXR",
-#ifdef OPENCV_IMGCODECS_USE_OPENEXR
-        true
-#else
-        false
-#endif
-    );
-    return PARAM_ENABLE_OPENEXR;
-}
-static void initOpenEXR()
-{
-    if (!isOpenEXREnabled())
-    {
-        const char* message = "imgcodecs: OpenEXR codec is disabled. You can enable it via 'OPENCV_IO_ENABLE_OPENEXR' option. Refer for details and cautions here: https://github.com/opencv/opencv/issues/21326";
-        CV_LOG_WARNING(NULL, message);
-        CV_Error(Error::StsNotImplemented, message);
-    }
-}
-
 /////////////////////// ExrDecoder ///////////////////
 
 ExrDecoder::ExrDecoder()
@@ -692,7 +671,6 @@ void  ExrDecoder::RGBToGray( float *in, float *out )
 
 ImageDecoder ExrDecoder::newDecoder() const
 {
-    initOpenEXR();
     return makePtr<ExrDecoder>();
 }
 
@@ -869,7 +847,6 @@ bool  ExrEncoder::write( const Mat& img, const std::vector<int>& params )
 
 ImageEncoder ExrEncoder::newEncoder() const
 {
-    initOpenEXR();
     return makePtr<ExrEncoder>();
 }
 
