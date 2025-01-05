2e128abc6ce18d59c13e1d31630d51f1e99b3275
Junrou Nishida <homuler@users.noreply.github.com>
2025-01-05 18:14:40 +0900

    Ensure header files are generated correctly under specific directories.

--- modules/objc/generator/gen_objc.py
+++ modules/objc/generator/gen_objc.py
@@ -1662,7 +1662,7 @@ if __name__ == "__main__":
                h_files += [os.path.join(root, filename) for filename in fnmatch.filter(filenames, '*.h')]
                hpp_files += [os.path.join(root, filename) for filename in fnmatch.filter(filenames, '*.hpp')]
             srcfiles = h_files + hpp_files
-            srcfiles = [f for f in srcfiles if not re_bad.search(f.replace('\\', '/'))]
+            srcfiles = [f for f in srcfiles if not re_bad.search(os.path.relpath(f, module_location).replace('\\', '/'))]
         logging.info("\nFiles (%d):\n%s", len(srcfiles), pformat(srcfiles))
 
         common_headers_fname = os.path.join(misc_location, 'filelist_common')
