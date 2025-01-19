5fd12c9e99109b562e5127aee03746e8bb21544a
shyama7004 <bisoyi7004@gmail.com>
2025-01-19 10:06:03 +0530

    set-cap-orientation-auto-true-by-default

--- modules/videoio/test/test_orientation.cpp
+++ modules/videoio/test/test_orientation.cpp
@@ -70,6 +70,36 @@ TEST_P(VideoCaptureAPITests, mp4_orientation_no_rotation)
     ASSERT_EQ(480, frame.cols);
     ASSERT_EQ(270, frame.rows);
 }
+TEST_P(VideoCaptureAPITests, mp4_orientation_auto_by_default)
+{
+    cv::VideoCaptureAPIs api = GetParam();
+    if (!videoio_registry::hasBackend(api))
+        throw SkipTestException("backend " + std::to_string(static_cast<int>(api)) + " was not found");
+    std::string video_file = std::string(cvtest::TS::ptr()->get_data_path()) + "video/rotated_metadata.mp4";
+
+    VideoCapture cap;
+    EXPECT_NO_THROW(cap.open(video_file, api));
+    ASSERT_TRUE(cap.isOpened()) << "Can't open the video: " << video_file
+                                << " with backend " << api << std::endl;
+
+    double autoRotate = cap.get(cv::CAP_PROP_ORIENTATION_AUTO);
+    EXPECT_EQ(1.0, autoRotate)
+        << "Expected CAP_PROP_ORIENTATION_AUTO to be on (1.0) by default.";
+
+    EXPECT_EQ(90, cap.get(cv::CAP_PROP_ORIENTATION_META));
+    cv::Size size;
+    EXPECT_NO_THROW(size = cv::Size(
+        static_cast<int>(cap.get(cv::CAP_PROP_FRAME_WIDTH)),
+        static_cast<int>(cap.get(cv::CAP_PROP_FRAME_HEIGHT))
+    ));
+    EXPECT_EQ(270, size.width);
+    EXPECT_EQ(480, size.height);
+    cv::Mat frame;
+    cap >> frame;
+    ASSERT_FALSE(frame.empty()) << "Failed to retrieve a frame!";
+    EXPECT_EQ(270, frame.cols);
+    EXPECT_EQ(480, frame.rows);
+}
 
 INSTANTIATE_TEST_CASE_P(videoio, VideoCaptureAPITests, testing::Values(CAP_FFMPEG, CAP_AVFOUNDATION));
 
