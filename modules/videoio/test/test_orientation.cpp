e6230934c8e5da456141b596dd71688f58f1d11d
Alexander Smorkalov <alexander.smorkalov@opencv.ai>
2025-01-21 12:04:22 +0300

    Even more cleanup and build fix for Windows.

--- modules/videoio/test/test_orientation.cpp
+++ modules/videoio/test/test_orientation.cpp
@@ -8,82 +8,75 @@ using namespace std;
 
 namespace opencv_test { namespace {
 
-typedef TestWithParam<cv::VideoCaptureAPIs> VideoCaptureAPITests;
-
-static void videoOrientationCheck(cv::VideoCapture& cap, double angle, int width, int height)
+struct VideoCaptureAPITests: TestWithParam<cv::VideoCaptureAPIs>
 {
-    EXPECT_EQ(angle, cap.get(CAP_PROP_ORIENTATION_META));
-    EXPECT_EQ(width, (int)cap.get(CAP_PROP_FRAME_WIDTH));
-    EXPECT_EQ(height, (int)cap.get(CAP_PROP_FRAME_HEIGHT));
+    void SetUp()
+    {
+        cv::VideoCaptureAPIs api = GetParam();
+        if (!videoio_registry::hasBackend(api))
+            throw SkipTestException("backend " + std::to_string(int(api)) + " was not found");
 
-    Mat frame;
-    cap >> frame;
+        string video_file = string(cvtest::TS::ptr()->get_data_path()) + "video/rotated_metadata.mp4";
 
-    ASSERT_EQ(width, frame.cols);
-    ASSERT_EQ(height, frame.rows);
-}
+        EXPECT_NO_THROW(cap.open(video_file, api));
+        ASSERT_TRUE(cap.isOpened()) << "Can't open the video: " << video_file << " with backend " << api << std::endl;
+    }
+
+    void tearDown()
+    {
+        cap.release();
+    }
+
+    void orientationCheck(double angle, int width, int height)
+    {
+        EXPECT_EQ(angle, cap.get(CAP_PROP_ORIENTATION_META));
+        EXPECT_EQ(width, (int)cap.get(CAP_PROP_FRAME_WIDTH));
+        EXPECT_EQ(height, (int)cap.get(CAP_PROP_FRAME_HEIGHT));
+
+        printf("before read frame!\n");
+        Mat frame;
+        cap >> frame;
+        printf("after read frame!\n");
+
+        ASSERT_EQ(width, frame.cols);
+        ASSERT_EQ(height, frame.rows);
+    }
+
+    VideoCapture cap;
+};
 
 // Related issues:
 // - https://github.com/opencv/opencv/issues/26795
 // - https://github.com/opencv/opencv/issues/15499
 TEST_P(VideoCaptureAPITests, mp4_orientation_default_auto)
 {
-    cv::VideoCaptureAPIs api = GetParam();
-    if (!videoio_registry::hasBackend(api))
-        throw SkipTestException("backend " + std::to_string(int(api)) + " was not found");
-
-    string video_file = string(cvtest::TS::ptr()->get_data_path()) + "video/rotated_metadata.mp4";
-
-    VideoCapture cap;
-    EXPECT_NO_THROW(cap.open(video_file, api));
-    ASSERT_TRUE(cap.isOpened()) << "Can't open the video: " << video_file << " with backend " << api << std::endl;
     EXPECT_TRUE(cap.get(CAP_PROP_ORIENTATION_AUTO));
-
-    videoOrientationCheck(cap, 90., 270, 480);
+    orientationCheck(90., 270, 480);
+    printf("Check done!\n");
 }
 
 TEST_P(VideoCaptureAPITests, mp4_orientation_forced)
 {
-    cv::VideoCaptureAPIs api = GetParam();
-    if (!videoio_registry::hasBackend(api))
-        throw SkipTestException("backend " + std::to_string(int(api)) + " was not found");
-
-    string video_file = string(cvtest::TS::ptr()->get_data_path()) + "video/rotated_metadata.mp4";
-
-    VideoCapture cap;
-    EXPECT_NO_THROW(cap.open(video_file, api));
-    ASSERT_TRUE(cap.isOpened()) << "Can't open the video: " << video_file << " with backend " << api << std::endl;
     EXPECT_TRUE(cap.set(CAP_PROP_ORIENTATION_AUTO, false));
-
-    videoOrientationCheck(cap, 90., 480, 270);
+    orientationCheck(90., 480, 270);
 }
 
 TEST_P(VideoCaptureAPITests, mp4_orientation_switch)
 {
-    cv::VideoCaptureAPIs api = GetParam();
-    if (!videoio_registry::hasBackend(api))
-        throw SkipTestException("backend " + std::to_string(int(api)) + " was not found");
-
-    string video_file = string(cvtest::TS::ptr()->get_data_path()) + "video/rotated_metadata.mp4";
-
-    VideoCapture cap;
-    EXPECT_NO_THROW(cap.open(video_file, api));
-    ASSERT_TRUE(cap.isOpened()) << "Can't open the video: " << video_file << " with backend " << api << std::endl;
-
-    videoOrientationCheck(cap, 90., 270, 480);
-
+    orientationCheck(90., 270, 480);
     EXPECT_TRUE(cap.set(CAP_PROP_ORIENTATION_AUTO, false));
     EXPECT_FALSE(cap.get(CAP_PROP_ORIENTATION_AUTO));
-    videoOrientationCheck(cap, 90., 480, 270);
+    orientationCheck(90., 480, 270);
 }
 
 
-INSTANTIATE_TEST_CASE_P(videoio, VideoCaptureAPITests,
-                            testing::Values(
-#if defined(__APPLE__)
-                                            CAP_AVFOUNDATION,
+static cv::VideoCaptureAPIs supported_backends[] = {
+#ifdef HAVE_AVFOUNDATION
+    CAP_AVFOUNDATION,
 #endif
-                                            CAP_FFMPEG
-                                            ));
+    CAP_FFMPEG
+};
+
+INSTANTIATE_TEST_CASE_P(videoio, VideoCaptureAPITests, testing::ValuesIn(supported_backends));
 
 }} // namespace
