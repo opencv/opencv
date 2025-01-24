ccba6491ec2c51c6e9512bcf736a990e63e97fbf
gursimarsingh <gursimar@bigvisionllc.com>
2025-01-24 12:40:39 +0530

    updated license terms

--- samples/dnn/inpainting.cpp
+++ samples/dnn/inpainting.cpp
@@ -1,4 +1,8 @@
 /*
+This file is part of OpenCV project.
+It is subject to the license terms in the LICENSE file found in the top-level directory
+of this distribution and at http://opencv.org/license.html.
+
 This sample inpaints the masked area in the given image.
 
 Copyright (C) 2025, Bigvision LLC.
--- samples/dnn/inpainting.py
+++ samples/dnn/inpainting.py
@@ -1,5 +1,9 @@
 #!/usr/bin/env python
 '''
+This file is part of OpenCV project.
+It is subject to the license terms in the LICENSE file found in the top-level directory
+of this distribution and at http://opencv.org/license.html.
+
 This sample inpaints the masked area in the given image.
 
 Copyright (C) 2025, Bigvision LLC.
