875a17dfc03eb6cf2dbcb24c5eed3da21fb6d734
天音あめ <i@amane-a.me>
2025-02-07 00:34:54 +0800

    Merge pull request #26804 from amane-ame:norm_hal_rvv
    Add RISC-V HAL implementation for cv::norm and cv::normalize #26804
    
    This patch implements `cv::norm` with norm types `NORM_INF/NORM_L1/NORM_L2/NORM_L2SQR` and `Mat::convertTo` function in RVV_HAL using native intrinsic, optimizing the performance for `cv::norm(src)`, `cv::norm(src1, src2)`, and `cv::normalize(src)` with data types `8UC1/8UC4/32FC1`.
    
    `cv::normalize` also calls `minMaxIdx`, #26789 implements RVV_HAL for this.
    
    Tested on MUSE-PI for both gcc 14.2 and clang 20.0.
    
    ```
    $ opencv_test_core --gtest_filter="*Norm*"
    $ opencv_perf_core --gtest_filter="*norm*" --perf_min_samples=300 --perf_force_samples=300
    ```
    
    The head of the perf table is shown below since the table is too long.
    
    View the full perf table here: [hal_rvv_norm.pdf](https://github.com/user-attachments/files/18468255/hal_rvv_norm.pdf)
    
    <img width="1304" alt="Untitled" src="https://github.com/user-attachments/assets/3550b671-6d96-4db3-8b5b-d4cb241da650" />
    
    ### Pull Request Readiness Checklist
    
    See details at https://github.com/opencv/opencv/wiki/How_to_contribute#making-a-good-pull-request
    
    - [x] I agree to contribute to the project under Apache 2 License.
    - [x] To the best of my knowledge, the proposed patch is not based on a code under GPL or another license that is incompatible with OpenCV
    - [ ] The PR is proposed to the proper branch
    - [ ] There is a reference to the original bug report and related work
    - [ ] There is accuracy test, performance test and test data in opencv_extra repository, if applicable
          Patch to opencv_extra has the same branch name.
    - [ ] The feature is well documented and sample code can be built with the project CMake

--- 3rdparty/hal_rvv/hal_rvv.hpp
+++ 3rdparty/hal_rvv/hal_rvv.hpp
@@ -22,6 +22,9 @@
 #if defined(__riscv_v) && __riscv_v == 1000000
 #include "hal_rvv_1p0/merge.hpp" // core
 #include "hal_rvv_1p0/mean.hpp" // core
+#include "hal_rvv_1p0/norm.hpp" // core
+#include "hal_rvv_1p0/norm_diff.hpp" // core
+#include "hal_rvv_1p0/convert_scale.hpp" // core
 #include "hal_rvv_1p0/minmax.hpp" // core
 #include "hal_rvv_1p0/atan.hpp" // core
 #endif
