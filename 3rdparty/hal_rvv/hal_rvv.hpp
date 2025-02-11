4b1d44740cd2076171c088e1433025bf192a74c3
lve-gh <160332675+lve-gh@users.noreply.github.com>
2025-02-11 17:57:05 +0300

    Merge pull request #26884 from lve-gh:split8u_rvv_hal
    [HAL] split8u RVV 1.0 #26884
    
    ### Pull Request Readiness Checklist
    * Banana Pi BF3 (SpacemiT K1)
    * Compiler: Syntacore Clang 18.1.4 (build 2024.12)
    ```
    Geometric mean (ms)
    
                      Name of Test                   baseline  hal      hal
                                                        ui               vs
                                                                      baseline 
                                                                         ui
                                                                     (x-factor)
    split::Size_Depth_Channels::(127x61, 8UC1, 2)     0.012   0.004     3.12   
    split::Size_Depth_Channels::(127x61, 8UC1, 3)     0.019   0.006     2.91   
    split::Size_Depth_Channels::(127x61, 8UC1, 4)     0.028   0.011     2.64   
    split::Size_Depth_Channels::(127x61, 8UC1, 5)     0.067   0.033     2.02   
    split::Size_Depth_Channels::(127x61, 8UC1, 6)     0.084   0.040     2.11   
    split::Size_Depth_Channels::(127x61, 8UC1, 7)     0.103   0.055     1.88   
    split::Size_Depth_Channels::(127x61, 8UC1, 8)     0.113   0.032     3.50   
    split::Size_Depth_Channels::(640x480, 8UC1, 2)    0.454   0.179     2.54   
    split::Size_Depth_Channels::(640x480, 8UC1, 3)    0.677   0.298     2.27   
    split::Size_Depth_Channels::(640x480, 8UC1, 4)    0.901   0.410     2.20   
    split::Size_Depth_Channels::(640x480, 8UC1, 5)    3.781   3.010     1.26   
    split::Size_Depth_Channels::(640x480, 8UC1, 6)    4.886   4.009     1.22   
    split::Size_Depth_Channels::(640x480, 8UC1, 7)    5.777   4.770     1.21   
    split::Size_Depth_Channels::(640x480, 8UC1, 8)    4.596   1.330     3.46   
    split::Size_Depth_Channels::(1280x720, 8UC1, 2)   1.377   0.709     1.94   
    split::Size_Depth_Channels::(1280x720, 8UC1, 3)   2.091   1.034     2.02   
    split::Size_Depth_Channels::(1280x720, 8UC1, 4)   2.744   1.573     1.74   
    split::Size_Depth_Channels::(1280x720, 8UC1, 5)   9.542   6.284     1.52   
    split::Size_Depth_Channels::(1280x720, 8UC1, 6)   11.114  7.850     1.42   
    split::Size_Depth_Channels::(1280x720, 8UC1, 7)   14.083  11.879    1.19   
    split::Size_Depth_Channels::(1280x720, 8UC1, 8)   13.524  3.865     3.50   
    split::Size_Depth_Channels::(1920x1080, 8UC1, 2)  3.108   1.395     2.23   
    split::Size_Depth_Channels::(1920x1080, 8UC1, 3)  4.659   2.128     2.19   
    split::Size_Depth_Channels::(1920x1080, 8UC1, 4)  6.127   2.818     2.17   
    split::Size_Depth_Channels::(1920x1080, 8UC1, 5)  26.733  16.625    1.61   
    split::Size_Depth_Channels::(1920x1080, 8UC1, 6)  31.242  22.414    1.39   
    split::Size_Depth_Channels::(1920x1080, 8UC1, 7)  35.968  27.658    1.30   
    split::Size_Depth_Channels::(1920x1080, 8UC1, 8)  29.997  8.655     3.47
    ```
    See details at https://github.com/opencv/opencv/wiki/How_to_contribute#making-a-good-pull-request
    
    - [x] I agree to contribute to the project under Apache 2 License.
    - [x] To the best of my knowledge, the proposed patch is not based on a code under GPL or another license that is incompatible with OpenCV
    - [x] The PR is proposed to the proper branch
    - [x] There is a reference to the original bug report and related work
    - [x] There is accuracy test, performance test and test data in opencv_extra repository, if applicable
          Patch to opencv_extra has the same branch name.
    - [x] The feature is well documented and sample code can be built with the project CMake

--- 3rdparty/hal_rvv/hal_rvv.hpp
+++ 3rdparty/hal_rvv/hal_rvv.hpp
@@ -28,6 +28,7 @@
 #include "hal_rvv_1p0/convert_scale.hpp" // core
 #include "hal_rvv_1p0/minmax.hpp" // core
 #include "hal_rvv_1p0/atan.hpp" // core
+#include "hal_rvv_1p0/split.hpp" // core
 #endif
 
 
