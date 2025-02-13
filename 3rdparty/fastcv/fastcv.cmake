b109ff7d0cfc1183299c056f1840b4ac8f76fbe8
Alexander Smorkalov <alexander.smorkalov@opencv.ai>
2025-02-13 09:17:08 +0300

    Switch to static instance of FastCV on Linux.

--- 3rdparty/fastcv/fastcv.cmake
+++ 3rdparty/fastcv/fastcv.cmake
@@ -1,7 +1,7 @@
 function(download_fastcv root_dir)
 
   # Commit SHA in the opencv_3rdparty repo
-  set(FASTCV_COMMIT "dc5d58018f3af915a8d209386d2c58c0501c0f2c")
+  set(FASTCV_COMMIT "f4413cc2ab7233fdfc383a4cded402c072677fb0")
 
   # Define actual FastCV versions
   if(ANDROID)
@@ -16,14 +16,15 @@ function(download_fastcv root_dir)
     endif()
   elseif(UNIX AND NOT APPLE AND NOT IOS AND NOT XROS)
     if(AARCH64)
-      set(FCV_PACKAGE_NAME  "fastcv_linux_aarch64_2024_12_11.tgz")
-      set(FCV_PACKAGE_HASH  "7b33ad833e6f15ab6d4ec64fa3c17acd")
+      set(FCV_PACKAGE_NAME  "fastcv_linux_aarch64_2025_02_12.tgz")
+      set(FCV_PACKAGE_HASH  "33ac2a59cf3e7d6402eee2e010de1202")
     else()
       message("FastCV: fastcv lib for 32-bit Linux is not supported for now!")
     endif()
   endif(ANDROID)
 
   # Download Package
+
   set(OPENCV_FASTCV_URL "https://raw.githubusercontent.com/opencv/opencv_3rdparty/${FASTCV_COMMIT}/fastcv/")
 
   ocv_download( FILENAME        ${FCV_PACKAGE_NAME}
