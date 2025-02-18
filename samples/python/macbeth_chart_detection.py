6dbb79fbdf7df3937d0d53864472f2e89cfef154
Gursimar Singh <gursimar@bigvisionllc.com>
2025-02-18 18:07:43 +0530

    Added models.yml support to samples

--- samples/python/macbeth_chart_detection.py
+++ samples/python/macbeth_chart_detection.py
@@ -1,54 +1,88 @@
 import cv2 as cv
 import argparse
+import sys
+import os
+sys.path.append(os.path.join(os.path.dirname(__file__), ".."))
+from dnn.common import *
 
-def main():
-    parser = argparse.ArgumentParser(
-        description='Basic chart detection using neural network')
+def get_args_parser(func_args):
+    backends = ("default", "openvino", "opencv", "vkcom", "cuda")
+    targets = ("cpu", "opencl", "opencl_fp16", "ncs2_vpu", "hddl_vpu", "vulkan", "cuda", "cuda_fp16")
 
-    parser.add_argument('-t', type=int, default=0,
+    parser = argparse.ArgumentParser(add_help=False)
+    parser.add_argument('--zoo', default=os.path.join(os.path.dirname(os.path.abspath(__file__)), '../dnn', 'models.yml'),
+                        help='An optional path to file with preprocessing parameters.')
+    parser.add_argument('--input', help='Path to input image or video file. Skip this argument to capture frames from a camera.', default=0, required=False)
+    parser.add_argument('--method', help='choose method: dexined or canny', default='canny', required=False)
+    parser.add_argument('--type', type=int, default=0,
                         help='chartType: 0-Standard, 1-DigitalSG, 2-Vinyl, default:0')
-    parser.add_argument('-m', type=str, default='',
-                        help='File path of the .pb model file')
-    parser.add_argument('-pb', type=str, default='',
-                        help='File path of the .pbtxt file')
-    parser.add_argument('-v', type=str, default='',
-                        help='Input video file. If omitted, input comes from camera')
-    parser.add_argument('-ci', type=int, default=0,
-                        help='Camera id if input does not come from video (-v)')
-    parser.add_argument('-nc', type=int, default=1,
+    parser.add_argument('--num_charts', type=int, default=1,
                         help='Maximum number of charts in the image')
     parser.add_argument('--use_gpu', action='store_true',
                         help='Use this flag to run DNN inference on CUDA')
+    parser.add_argument('--backend', default="default", type=str, choices=backends,
+                    help="Choose one of computation backends: "
+                         "default: automatically (by default), "
+                         "openvino: Intel's Deep Learning Inference Engine (https://software.intel.com/openvino-toolkit), "
+                         "opencv: OpenCV implementation, "
+                         "vkcom: VKCOM, "
+                         "cuda: CUDA, "
+                         "webnn: WebNN")
+    parser.add_argument('--target', default="cpu", type=str, choices=targets,
+                    help="Choose one of target computation devices: "
+                         "cpu: CPU target (by default), "
+                         "opencl: OpenCL, "
+                         "opencl_fp16: OpenCL fp16 (half-float precision), "
+                         "ncs2_vpu: NCS2 VPU, "
+                         "hddl_vpu: HDDL VPU, "
+                         "vulkan: Vulkan, "
+                         "cuda: CUDA, "
+                         "cuda_fp16: CUDA fp16 (half-float preprocess)")
 
-    args = parser.parse_args()
+    args, _ = parser.parse_known_args()
+    add_preproc_args(args.zoo, parser, 'mcc', 'mcc')
+    parser = argparse.ArgumentParser(parents=[parser],
+                                     description='''
+        To run:
+            Default:
+                python macbeth_chart_detection.py --input=path/to/your/input/image/or/video (don't give --input flag if want to use device camera)
+            DNN model:
+                python macbeth_chart_detection.py mcc --input=path/to/your/input/image/or/video
 
-    if not (0 <= args.t <= 2):
+        Model path can also be specified using --model argument. And config path can be specified using --config.
+        ''', formatter_class=argparse.RawTextHelpFormatter)
+    return parser.parse_args(func_args)
+
+def main(func_args=None):
+    args = get_args_parser(func_args)
+
+    if not (0 <= args.type <= 2):
         raise ValueError("chartType must be 0, 1, or 2")
 
-    chartType = args.t
-
-    model_path = args.m
-    pbtxt_path = args.pb
-    camera_id = args.ci
-    max_charts = args.nc
-    video_path = args.v
-    use_gpu = args.use_gpu
-
     # Open input video stream (camera or file)
-    if video_path:
-        cap = cv.VideoCapture(video_path)
-        wait_time = 10
+    cap = cv.VideoCapture(findFile(args.input) if args.input else 0)
+    wait_time = 10
+
+    if os.getenv('OPENCV_SAMPLES_DATA_PATH') is not None:
+        try:
+            args.model = findModel(args.model, args.sha1)
+            args.config = findModel(args.config, args.config_sha1)
+        except:
+            print("[WARN] Model file not provided, using default detector. Pass model using --model and config using --config to use dnn based detector.\n\n")
+            args.model = None
+            args.config = None
     else:
-        cap = cv.VideoCapture(camera_id)
-        wait_time = 10
+        args.model = None
+        args.config = None
+        print("[WARN] Model file not provided, using default detector. Pass model using --model and config using --config to use dnn based detector. Or, set OPENCV_SAMPLES_DATA_PATH environment variable.\n\n")
 
-    if model_path and pbtxt_path:
+    if args.model and args.config:
         # Load the DNN from TensorFlow model
-        net = cv.dnn.readNetFromTensorflow(model_path, pbtxt_path)
+        net = cv.dnn.readNetFromTensorflow(args.model, args.config)
 
-        if use_gpu:
-            net.setPreferableBackend(cv.dnn.DNN_BACKEND_CUDA)
-            net.setPreferableTarget(cv.dnn.DNN_TARGET_CUDA)
+        if args.use_gpu:
+            net.setPreferableBackend(get_backend_id(args.backend))
+            net.setPreferableTarget(get_target_id(args.target))
 
         detector = cv.mcc_CCheckerDetector.create(net)
         print("Detecting checkers using neural network.")
@@ -59,12 +93,11 @@ def main():
     while True:
         ret, frame = cap.read()
         if not ret:
-            print("No more frames or failed to grab frame.")
             break
 
         image_copy = frame.copy()
 
-        if not detector.process(frame, chartType, max_charts, True):
+        if not detector.process(frame, args.type, args.num_charts, True):
             print("ChartColor not detected.")
         else:
             checkers = detector.getListColorChecker()
@@ -72,7 +105,6 @@ def main():
                 cdraw = cv.mcc_CCheckerDraw.create(checker)
                 cdraw.draw(frame)
 
-        # Show results
         cv.imshow("image result | Press ESC to quit", frame)
         cv.imshow("original", image_copy)
         key = cv.waitKey(wait_time) & 0xFF
