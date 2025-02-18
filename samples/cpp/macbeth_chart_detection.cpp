71eb4ab9f0021c9988835c218e7c8c611a45e578
Gursimar Singh <gursimar@bigvisionllc.com>
2025-02-18 18:07:43 +0530

    Added models.yml support to samples

--- samples/cpp/macbeth_chart_detection.cpp
+++ samples/cpp/macbeth_chart_detection.cpp
@@ -4,90 +4,107 @@
 #include <opencv2/mcc.hpp>
 #include <opencv2/dnn.hpp>
 #include <iostream>
+#include "../dnn/common.hpp"
 
 using namespace std;
 using namespace cv;
+using namespace cv::dnn;
 using namespace mcc;
 
-const char *about = "Basic chart detection using neural network";
-const char *keys = {
-    "{ help h usage ? |    | show this message }"
-    "{t       | 0   | chartType: 0-Standard, 1-DigitalSG, 2-Vinyl, default:0}"
-    "{m       |     | File path of model, if you don't have the model you can \
-                      find the link in the documentation}"
-    "{pb      |     | File path of pbtxt file, available along with with the model \
-                      file }"
-    "{v       |     | Input from video file, if ommited, input comes from camera }"
-    "{ci      | 0   | Camera id if input doesnt come from video (-v) }"
-    "{nc      | 1   | Maximum number of charts in the image }"
-    "{use_gpu |     | Add this flag if you want to use gpu}"};
+const string about =
+    "This sample demonstrates mcc checker detection with DNN based model and thresholding (default) techniques.\n\n"
+    "To run default:\n"
+    "\t ./example_cpp_macbeth_chart_detection --input=path/to/your/input/image/or/video (don't give --input flag if want to use device camera)\n"
+    "With DNN model:\n"
+    "\t ./example_cpp_macbeth_chart_detection mcc --input=path/to/your/input/image/or/video\n\n"
+    "Model path can also be specified using --model argument. And config path can be specified using --config. Download it using python download_models.py mcc from dnn samples directory\n\n";
+
+const string param_keys =
+    "{ help h          |                   | Print help message. }"
+    "{ @alias          |                   | An alias name of model to extract preprocessing parameters from models.yml file. }"
+    "{ zoo             | ../dnn/models.yml | An optional path to file with preprocessing parameters }"
+    "{ input i         |                   | Path to input image or video file. Skip this argument to capture frames from a camera.}"
+    "{ type            |         0         | chartType: 0-Standard, 1-DigitalSG, 2-Vinyl, default:0 }"
+    "{ num_charts      |         1         | Maximum number of charts in the image }"
+    "{use_gpu          |                   | Add this flag if you want to use gpu}"
+    "{ model           |                   | Path to the model file for using dnn model. }";
+
+const string backend_keys = format(
+    "{ backend          | default | Choose one of computation backends: "
+                            "default: automatically (by default), "
+                            "openvino: Intel's Deep Learning Inference Engine (https://software.intel.com/openvino-toolkit), "
+                            "opencv: OpenCV implementation, "
+                            "vkcom: VKCOM, "
+                            "cuda: CUDA, "
+                            "webnn: WebNN }");
+
+const string target_keys = format(
+    "{ target           | cpu | Choose one of target computation devices: "
+                            "cpu: CPU target (by default), "
+                            "opencl: OpenCL, "
+                            "opencl_fp16: OpenCL fp16 (half-float precision), "
+                            "vpu: VPU, "
+                            "vulkan: Vulkan, "
+                            "cuda: CUDA, "
+                            "cuda_fp16: CUDA fp16 (half-float preprocess) }");
+
+string keys = param_keys + backend_keys + target_keys;
 
 int main(int argc, char *argv[])
 {
-    // ----------------------------------------------------------
-    // Scroll down a bit (~50 lines) to find actual relevant code
-    // ----------------------------------------------------------
-
     CommandLineParser parser(argc, argv, keys);
     parser.about(about);
 
     if (parser.has("help"))
     {
+        cout << about << endl;
         parser.printMessage();
         return -1;
     }
+    string modelName = parser.get<String>("@alias");
+    string zooFile = parser.get<String>("zoo");
+    const char* path = getenv("OPENCV_SAMPLES_DATA_PATH");
+    if ((path != NULL) || parser.has("@alias") || (parser.get<String>("model") != "")) {
+        zooFile = findFile(zooFile);
+    }
+    else{
+        cout<<"[WARN] set the environment variables or pass the arguments --model, --config and models.yml file using --zoo for using dnn based detector. Continuing with default detector.\n\n";
+    }
 
-    int t = parser.get<int>("t");
+    keys += genPreprocArguments(modelName, zooFile);
+    parser = CommandLineParser(argc, argv, keys);
+
+    int t = parser.get<int>("type");
 
     CV_Assert(0 <= t && t <= 2);
     COLORCHART chartType = COLORCHART(t);
 
-    string model_path = parser.get<string>("m");
-    string pbtxt_path = parser.get<string>("pb");
+    const string sha1 = parser.get<String>("sha1");
+    const string model_path = findModel(parser.get<string>("model"), sha1);
+    const string config_sha1 = parser.get<String>("config_sha1");
+    const string pbtxt_path = findModel(parser.get<string>("config"), config_sha1);
 
-    int camId = parser.get<int>("ci");
-    int nc = parser.get<int>("nc");
-
-    String video;
-
-    if (parser.has("v"))
-        video = parser.get<String>("v");
-
+    int nc = parser.get<int>("num_charts");
     bool use_gpu = parser.has("use_gpu");
 
-    if (!parser.check())
-    {
-        parser.printErrors();
-        return 0;
-    }
-
-    VideoCapture inputVideo;
-    int waitTime;
-    if (!video.empty())
-    {
-        inputVideo.open(video);
-        waitTime = 10;
-    }
+    int waitTime=10;
+    VideoCapture cap;
+    if (parser.has("input"))
+        cap.open(findFile(parser.get<String>("input")));
     else
-    {
-        inputVideo.open(camId);
-        waitTime = 10;
-    }
-
-    //--------------------------------------------------------------------------
-    //-------------------------Actual Relevant Code-----------------------------
-    //--------------------------------------------------------------------------
-
-    //load the network
+        cap.open(0);
 
     Ptr<CCheckerDetector> detector;
     if (model_path != "" && pbtxt_path != ""){
-        cv::dnn::Net net = cv::dnn::readNetFromTensorflow(model_path, pbtxt_path);
+        Net net = readNetFromTensorflow(model_path, pbtxt_path);
 
         if (use_gpu)
         {
-            net.setPreferableBackend(dnn::DNN_BACKEND_CUDA);
-            net.setPreferableTarget(dnn::DNN_TARGET_CUDA);
+            net.setPreferableBackend(getBackendID("cuda"));
+            net.setPreferableTarget(getTargetID("cuda"));
+        }else{
+            net.setPreferableBackend(getBackendID(parser.get<String>("backend")));
+            net.setPreferableTarget(getTargetID(parser.get<String>("target")));
         }
 
         detector = CCheckerDetector::create(net);
@@ -97,10 +114,10 @@ int main(int argc, char *argv[])
         detector = CCheckerDetector::create();
     }
 
-    while (inputVideo.grab())
+    while (cap.grab())
     {
         Mat image, imageCopy;
-        inputVideo.retrieve(image);
+        cap.retrieve(image);
 
         imageCopy = image.clone();
 
@@ -113,8 +130,8 @@ int main(int argc, char *argv[])
         {
 
             // get checker
-            std::vector<Ptr<mcc::CChecker>> checkers = detector->getListColorChecker();
-            for (Ptr<mcc::CChecker> checker : checkers)
+            vector<Ptr<CChecker>> checkers = detector->getListColorChecker();
+            for (Ptr<CChecker> checker : checkers)
             {
                 // current checker
 
