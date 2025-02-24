097edb3bb084f15df1a68604286ea214d0b51901
NanQin555 <3135247064@qq.com>
2025-02-24 10:53:59 +0000

    hal_rvv: cv::hal::warpAffine remove unnecessary function.     static int warpAffine (int src_type, const uchar * src_data, size_t src_step, int src_width, int src_height, uchar * dst_data, size_t dst_step, int dst_width, int dst_height, const double M[6], int interpolation, int borderType, const double borderValue[4])

--- 3rdparty/hal_rvv/hal_rvv_1p0/warpAffine.hpp
+++ 3rdparty/hal_rvv/hal_rvv_1p0/warpAffine.hpp
@@ -9,19 +9,10 @@ namespace cv { namespace cv_hal_rvv {
 #define INTER_BITS 5
 #define INTER_TAB_SIZE (1 << INTER_BITS)
 
-#undef cv_hal_warpAffine
-#define cv_hal_warpAffine cv::cv_hal_rvv::warpAffine
 #undef cv_hal_warpAffineBlocklineNN
 #define cv_hal_warpAffineBlocklineNN cv::cv_hal_rvv::warpAffineBlocklineNN
 #undef cv_hal_warpAffineBlockline
 #define cv_hal_warpAffineBlockline cv::cv_hal_rvv::warpAffineBlockline
-
-static int warpAffine(int src_type,
-                const uchar * src_data, size_t src_step, int src_width, int src_height,
-                uchar * dst_data, size_t dst_step, int dst_width, int dst_height,
-                const double M[6], int interpolation, int borderType, const double borderValue[4]) {
-    return CV_HAL_ERROR_NOT_IMPLEMENTED;
-}   
 
 static int warpAffineBlocklineNN(int *adelta, int *bdelta, short* xy, int X0, int Y0, int bw) {
     constexpr int AB_BITS = MAX(10, static_cast<int>(INTER_BITS));
