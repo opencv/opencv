ea73f706744fe1b960a5a013e18f1da0010bf8dd
GenshinImpactStarts <annan.plct@isrc.iscas.ac.cn>
2025-03-03 07:20:09 +0000

    reuse atan | impl cart_to_polar | add perf test
    Co-authored-by: Liutong HAN <liutong2020@iscas.ac.cn>

--- 3rdparty/hal_rvv/hal_rvv.hpp
+++ 3rdparty/hal_rvv/hal_rvv.hpp
@@ -30,6 +30,7 @@
 #include "hal_rvv_1p0/atan.hpp" // core
 #include "hal_rvv_1p0/split.hpp" // core
 #include "hal_rvv_1p0/flip.hpp" // core
+#include "hal_rvv_1p0/cart_to_polar.hpp" // core
 
 #include "hal_rvv_1p0/pyramids.hpp" // imgproc
 #endif
