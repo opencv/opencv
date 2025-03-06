c3f5fbce2a8a368d33e4c40287a33e2abd4a1edb
GenshinImpactStarts <annan.plct@isrc.iscas.ac.cn>
2025-03-06 18:03:29 +0000

    fix input 0, inf | more reusable
    Co-authored-by: Liutong HAN <liutong2020@iscas.ac.cn>

--- 3rdparty/hal_rvv/hal_rvv_1p0/sqrt.hpp
+++ 3rdparty/hal_rvv/hal_rvv_1p0/sqrt.hpp
@@ -9,19 +9,20 @@
 namespace cv { namespace cv_hal_rvv {
 
 #undef cv_hal_sqrt32f
-#define cv_hal_sqrt32f cv::cv_hal_rvv::sqrt32f
+#define cv_hal_sqrt32f cv::cv_hal_rvv::sqrt<cv::cv_hal_rvv::Sqrt32f>
 #undef cv_hal_sqrt64f
-#define cv_hal_sqrt64f cv::cv_hal_rvv::sqrt64f
+#define cv_hal_sqrt64f cv::cv_hal_rvv::sqrt<cv::cv_hal_rvv::Sqrt64f>
 #undef cv_hal_invSqrt32f
-#define cv_hal_invSqrt32f cv::cv_hal_rvv::invSqrt32f
+#define cv_hal_invSqrt32f cv::cv_hal_rvv::invSqrt<cv::cv_hal_rvv::InvSqrt32f>
 #undef cv_hal_invSqrt64f
-#define cv_hal_invSqrt64f cv::cv_hal_rvv::invSqrt64f
+#define cv_hal_invSqrt64f cv::cv_hal_rvv::invSqrt<cv::cv_hal_rvv::InvSqrt64f>
 
 namespace detail {
 
 // Newton-Raphson method
+// Use 4 LMUL registers
 template <size_t iter_times, typename VEC_T>
-inline VEC_T invSqrt(VEC_T x, size_t vl)
+inline VEC_T sqrt(VEC_T x, size_t vl)
 {
     auto x2 = __riscv_vfmul(x, 0.5, vl);
     auto y = __riscv_vfrsqrt7(x, vl);
@@ -33,87 +34,106 @@ inline VEC_T invSqrt(VEC_T x, size_t vl)
         t = __riscv_vfrsub(t, 1.5, vl);
         y = __riscv_vfmul(t, y, vl);
     }
-    return y;
+    // just to prevent the compiler from calculating mask before the invSqrt, which will run out
+    // of registers and cause memory access.
+    asm volatile("" ::: "memory");
+    auto mask = __riscv_vmfne(x, 0.0, vl);
+    mask = __riscv_vmfne_mu(mask, mask, x, INFINITY, vl);
+    return __riscv_vfmul_mu(mask, x, x, y, vl);
 }
 
 // Newton-Raphson method
-template <size_t iter_times, typename VEC_T, typename MASK_T>
-inline VEC_T invSqrt(VEC_T x, MASK_T mask, size_t vl)
+// Use 3 LMUL registers and 1 mask register
+template <size_t iter_times, typename VEC_T>
+inline VEC_T invSqrt(VEC_T x, size_t vl)
 {
+    auto mask = __riscv_vmfne(x, 0.0, vl);
+    mask = __riscv_vmfne_mu(mask, mask, x, INFINITY, vl);
     auto x2 = __riscv_vfmul(x, 0.5, vl);
     auto y = __riscv_vfrsqrt7(x, vl);
 #pragma unroll
     for (size_t i = 0; i < iter_times; i++)
     {
         auto t = __riscv_vfmul(y, y, vl);
-        t = __riscv_vfmul_mu(mask, t, t, x2, vl);
+        t = __riscv_vfmul(t, x2, vl);
         t = __riscv_vfrsub(t, 1.5, vl);
-        y = __riscv_vfmul(t, y, vl);
+        y = __riscv_vfmul_mu(mask, y, t, y, vl);
     }
     return y;
 }
 
 }  // namespace detail
 
-inline int sqrt32f(const float* src, float* dst, int _len)
+struct Sqrt_f32m4
 {
-    size_t vl = __riscv_vsetvlmax_e32m8();
+    using ElemType = float;
+    static constexpr size_t iter_times = 2;
+    static inline size_t setvl(size_t len) { return __riscv_vsetvl_e32m4(len); }
+    static inline vfloat32m4_t vload(const float* ptr, size_t vl) { return __riscv_vle32_v_f32m4(ptr, vl); }
+    static inline void vstore(float* ptr, vfloat32m4_t v, size_t vl) { __riscv_vse32(ptr, v, vl); }
+};
+
+struct Sqrt_f32m8
+{
+    using ElemType = float;
+    static constexpr size_t iter_times = 2;
+    static inline size_t setvl(size_t len) { return __riscv_vsetvl_e32m8(len); }
+    static inline vfloat32m8_t vload(const float* ptr, size_t vl) { return __riscv_vle32_v_f32m8(ptr, vl); }
+    static inline void vstore(float* ptr, vfloat32m8_t v, size_t vl) { __riscv_vse32(ptr, v, vl); }
+};
+
+struct Sqrt_f64m4
+{
+    using ElemType = double;
+    static constexpr size_t iter_times = 3;
+    static inline size_t setvl(size_t len) { return __riscv_vsetvl_e64m4(len); }
+    static inline vfloat64m4_t vload(const double* ptr, size_t vl) { return __riscv_vle64_v_f64m4(ptr, vl); }
+    static inline void vstore(double* ptr, vfloat64m4_t v, size_t vl) { __riscv_vse64(ptr, v, vl); }
+};
+
+struct Sqrt_f64m8
+{
+    using ElemType = double;
+    static constexpr size_t iter_times = 3;
+    static inline size_t setvl(size_t len) { return __riscv_vsetvl_e64m8(len); }
+    static inline vfloat64m8_t vload(const double* ptr, size_t vl) { return __riscv_vle64_v_f64m8(ptr, vl); }
+    static inline void vstore(double* ptr, vfloat64m8_t v, size_t vl) { __riscv_vse64(ptr, v, vl); }
+};
+
+using Sqrt32f = Sqrt_f32m8;
+using Sqrt64f = Sqrt_f64m8;
+#ifdef __clang__
+// Strange bug in clang: invSqrt use 2 LMUL registers to store mask, which will cause memory access.
+using InvSqrt32f = Sqrt_f32m4;
+using InvSqrt64f = Sqrt_f64m4;
+#else
+using InvSqrt32f = Sqrt_f32m8;
+using InvSqrt64f = Sqrt_f64m8;
+#endif
+
+template <typename RVV_T, typename Elem = typename RVV_T::ElemType>
+inline int sqrt(const Elem* src, Elem* dst, int _len)
+{
+    size_t vl;
     for (size_t len = _len; len > 0; len -= vl, src += vl, dst += vl)
     {
-        vl = __riscv_vsetvl_e32m8(len);
-        auto x = __riscv_vle32_v_f32m8(src, vl);
-        auto y = detail::invSqrt<2>(x, vl);
-        // just to prevent the compiler from calculating mask before the invSqrt, which will run out
-        // of registers and cause memory access.
-        asm volatile("" ::: "memory");
-        auto mask = __riscv_vmfne(__riscv_vmfne(x, 0.0, vl), x, INFINITY, vl);
-        __riscv_vse32(dst, __riscv_vfmul_mu(mask, x, x, y, vl), vl);
+        vl = RVV_T::setvl(len);
+        auto x = RVV_T::vload(src, vl);
+        RVV_T::vstore(dst, detail::sqrt<RVV_T::iter_times>(x, vl), vl);
     }
 
     return CV_HAL_ERROR_OK;
 }
 
-inline int sqrt64f(const double* src, double* dst, int _len)
+template <typename RVV_T, typename Elem = typename RVV_T::ElemType>
+inline int invSqrt(const Elem* src, Elem* dst, int _len)
 {
-    size_t vl = __riscv_vsetvlmax_e64m8();
+    size_t vl;
     for (size_t len = _len; len > 0; len -= vl, src += vl, dst += vl)
     {
-        vl = __riscv_vsetvl_e64m8(len);
-        auto x = __riscv_vle64_v_f64m8(src, vl);
-        auto y = detail::invSqrt<3>(x, vl);
-        // just to prevent the compiler from calculating mask before the invSqrt, which will run out
-        // of registers and cause memory access.
-        asm volatile("" ::: "memory");
-        auto mask = __riscv_vmfne(__riscv_vmfne(x, 0.0, vl), x, INFINITY, vl);
-        __riscv_vse64(dst, __riscv_vfmul_mu(mask, x, x, y, vl), vl);
-    }
-
-    return CV_HAL_ERROR_OK;
-}
-
-inline int invSqrt32f(const float* src, float* dst, int _len)
-{
-    size_t vl = __riscv_vsetvlmax_e32m8();
-    for (size_t len = _len; len > 0; len -= vl, src += vl, dst += vl)
-    {
-        vl = __riscv_vsetvl_e32m8(len);
-        auto x = __riscv_vle32_v_f32m8(src, vl);
-        auto mask = __riscv_vmfne(__riscv_vmfne(x, 0.0, vl), x, INFINITY, vl);
-        __riscv_vse32(dst, detail::invSqrt<2>(x, mask, vl), vl);
-    }
-
-    return CV_HAL_ERROR_OK;
-}
-
-inline int invSqrt64f(const double* src, double* dst, int _len)
-{
-    size_t vl = __riscv_vsetvlmax_e64m8();
-    for (size_t len = _len; len > 0; len -= vl, src += vl, dst += vl)
-    {
-        vl = __riscv_vsetvl_e64m8(len);
-        auto x = __riscv_vle64_v_f64m8(src, vl);
-        auto mask = __riscv_vmfne(__riscv_vmfne(x, 0.0, vl), x, INFINITY, vl);
-        __riscv_vse64(dst, detail::invSqrt<3>(x, mask, vl), vl);
+        vl = RVV_T::setvl(len);
+        auto x = RVV_T::vload(src, vl);
+        RVV_T::vstore(dst, detail::invSqrt<RVV_T::iter_times>(x, vl), vl);
     }
 
     return CV_HAL_ERROR_OK;
