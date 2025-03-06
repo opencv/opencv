2f6f825ea11f5f4caf175e47974e9c3f72190fdc
GenshinImpactStarts <annan.plct@isrc.iscas.ac.cn>
2025-03-06 09:12:38 +0000

    fix invSqrt when input is 0 or inf
    Co-authored-by: Liutong HAN <liutong2020@iscas.ac.cn>

--- 3rdparty/hal_rvv/hal_rvv_1p0/sqrt.hpp
+++ 3rdparty/hal_rvv/hal_rvv_1p0/sqrt.hpp
@@ -37,8 +37,8 @@ inline VEC_T invSqrt(VEC_T x, size_t vl)
 }
 
 // Newton-Raphson method
-template <size_t iter_times, typename VEC_T>
-inline VEC_T invSqrt(VEC_T x, VEC_T half_three, size_t vl)
+template <size_t iter_times, typename VEC_T, typename MASK_T>
+inline VEC_T invSqrt(VEC_T x, MASK_T mask, size_t vl)
 {
     auto x2 = __riscv_vfmul(x, 0.5, vl);
     auto y = __riscv_vfrsqrt7(x, vl);
@@ -46,7 +46,8 @@ inline VEC_T invSqrt(VEC_T x, VEC_T half_three, size_t vl)
     for (size_t i = 0; i < iter_times; i++)
     {
         auto t = __riscv_vfmul(y, y, vl);
-        t = __riscv_vfnmsub(t, x2, half_three, vl);
+        t = __riscv_vfmul_mu(mask, t, t, x2, vl);
+        t = __riscv_vfrsub(t, 1.5, vl);
         y = __riscv_vfmul(t, y, vl);
     }
     return y;
@@ -93,27 +94,26 @@ inline int sqrt64f(const double* src, double* dst, int _len)
 inline int invSqrt32f(const float* src, float* dst, int _len)
 {
     size_t vl = __riscv_vsetvlmax_e32m8();
-    auto half_three = __riscv_vfmv_v_f_f32m8(1.5, vl);
     for (size_t len = _len; len > 0; len -= vl, src += vl, dst += vl)
     {
         vl = __riscv_vsetvl_e32m8(len);
         auto x = __riscv_vle32_v_f32m8(src, vl);
-        __riscv_vse32(dst, detail::invSqrt<2>(x, half_three, vl), vl);
+        auto mask = __riscv_vmfne(__riscv_vmfne(x, 0.0, vl), x, INFINITY, vl);
+        __riscv_vse32(dst, detail::invSqrt<2>(x, mask, vl), vl);
     }
 
     return CV_HAL_ERROR_OK;
 }
 
-// Newton-Raphson method
 inline int invSqrt64f(const double* src, double* dst, int _len)
 {
     size_t vl = __riscv_vsetvlmax_e64m8();
-    auto half_three = __riscv_vfmv_v_f_f64m8(1.5, vl);
     for (size_t len = _len; len > 0; len -= vl, src += vl, dst += vl)
     {
         vl = __riscv_vsetvl_e64m8(len);
         auto x = __riscv_vle64_v_f64m8(src, vl);
-        __riscv_vse64(dst, detail::invSqrt<3>(x, half_three, vl), vl);
+        auto mask = __riscv_vmfne(__riscv_vmfne(x, 0.0, vl), x, INFINITY, vl);
+        __riscv_vse64(dst, detail::invSqrt<3>(x, mask, vl), vl);
     }
 
     return CV_HAL_ERROR_OK;
