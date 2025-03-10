49587ec67907b8ec52b5907511a7c719b5682a2e
chacha21 <pierre@chachatelier.fr>
2025-03-10 14:51:22 +0100

    updated threshold() doc
    cv::threshold() supports more depths than the doc stated

--- modules/imgproc/include/opencv2/imgproc.hpp
+++ modules/imgproc/include/opencv2/imgproc.hpp
@@ -3030,9 +3030,10 @@ Also, the special values #THRESH_OTSU or #THRESH_TRIANGLE may be combined with o
 above values. In these cases, the function determines the optimal threshold value using the Otsu's
 or Triangle algorithm and uses it instead of the specified thresh.
 
-@note Currently, the Otsu's and Triangle methods are implemented only for 8-bit single-channel images.
+@note Currently, the Otsu's method is implemented only for 8-bit and 16-bit single-channel images,
+and the Triangle's method is implemented only for 8-bit single-channel images.
 
-@param src input array (multiple-channel, 8-bit or 32-bit floating point).
+@param src input array (multiple-channel, CV_8U, CV_16S, CV_16U, CV_32F or CV_64F).
 @param dst output array of the same size  and type and the same number of channels as src.
 @param thresh threshold value.
 @param maxval maximum value to use with the #THRESH_BINARY and #THRESH_BINARY_INV thresholding
