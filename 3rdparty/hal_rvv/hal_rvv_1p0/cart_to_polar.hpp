b842ccd62a9f1939698073a2bfd9674bb50c171b
GenshinImpactStarts <annan.plct@isrc.iscas.ac.cn>
2025-03-12 10:02:38 +0000

    enable ui | use hal sqrt
    Co-authored-by: Liutong HAN <liutong2020@iscas.ac.cn>

--- 3rdparty/hal_rvv/hal_rvv_1p0/cart_to_polar.hpp
+++ 3rdparty/hal_rvv/hal_rvv_1p0/cart_to_polar.hpp
@@ -1,56 +1,48 @@
 // This file is part of OpenCV project.
 // It is subject to the license terms in the LICENSE file found in the top-level directory
 // of this distribution and at http://opencv.org/license.html.
-#pragma once
+
+// Copyright (C) 2025, Institute of Software, Chinese Academy of Sciences.
+
+#ifndef OPENCV_HAL_RVV_CART_TO_POLAR_HPP_INCLUDED
+#define OPENCV_HAL_RVV_CART_TO_POLAR_HPP_INCLUDED
 
 #include <riscv_vector.h>
 
 #include "hal_rvv_1p0/atan.hpp"
+#include "hal_rvv_1p0/sqrt.hpp"
+#include "hal_rvv_1p0/types.hpp"
 
 namespace cv { namespace cv_hal_rvv {
 
 #undef cv_hal_cartToPolar32f
-#define cv_hal_cartToPolar32f cv::cv_hal_rvv::cartToPolar<cv::cv_hal_rvv::CartToPolar32f>
+#define cv_hal_cartToPolar32f cv::cv_hal_rvv::cartToPolar<cv::cv_hal_rvv::RVV_F32M4>
 #undef cv_hal_cartToPolar64f
-#define cv_hal_cartToPolar64f cv::cv_hal_rvv::cartToPolar<cv::cv_hal_rvv::CartToPolar64f>
+#define cv_hal_cartToPolar64f cv::cv_hal_rvv::cartToPolar<cv::cv_hal_rvv::RVV_F64M8>
 
-struct CartToPolar32f
+template <typename RVV_T, typename T = typename RVV_T::ElemType>
+inline int cartToPolar(const T* x, const T* y, T* mag, T* angle, int len, bool angleInDegrees)
 {
-    using ElemType = float;
-    static inline size_t setvl(size_t len) { return __riscv_vsetvl_e32m4(len); }
-    static inline vfloat32m4_t vload(const float* ptr, size_t vl) { return __riscv_vle32_v_f32m4(ptr, vl); }
-    static inline void vstore(float* ptr, vfloat32m4_t v, size_t vl) { __riscv_vse32(ptr, v, vl); }
-};
-
-struct CartToPolar64f
-{
-    using ElemType = double;
-    static inline size_t setvl(size_t len) { return __riscv_vsetvl_e64m8(len); }
-    static inline vfloat32m4_t vload(const double* ptr, size_t vl) { return __riscv_vfncvt_f(__riscv_vle64_v_f64m8(ptr, vl), vl); }
-    static inline void vstore(double* ptr, vfloat32m4_t v, size_t vl) { __riscv_vse64(ptr, __riscv_vfwcvt_f(v, vl), vl); }
-};
-
-template <typename RVV_T, typename Elem = typename RVV_T::ElemType>
-inline int
-    cartToPolar(const Elem* x, const Elem* y, Elem* mag, Elem* angle, int len, bool angleInDegrees)
-{
+    using CalType = RVV_SameLen<float, RVV_T>;
     auto atan_params = angleInDegrees ? detail::atan_params_deg : detail::atan_params_rad;
     size_t vl;
     for (; len > 0; len -= (int)vl, x += vl, y += vl, mag += vl, angle += vl)
     {
         vl = RVV_T::setvl(len);
 
-        auto vx = RVV_T::vload(x, vl);
-        auto vy = RVV_T::vload(y, vl);
+        auto vx = CalType::cast(RVV_T::vload(x, vl), vl);
+        auto vy = CalType::cast(RVV_T::vload(y, vl), vl);
 
-        auto vmag = __riscv_vfsqrt(__riscv_vfmadd(vx, vx, __riscv_vfmul(vy, vy, vl), vl), vl);
-        RVV_T::vstore(mag, vmag, vl);
+        auto vmag = detail::sqrt<2>(__riscv_vfmadd(vx, vx, __riscv_vfmul(vy, vy, vl), vl), vl);
+        RVV_T::vstore(mag, RVV_T::cast(vmag, vl), vl);
 
         auto vangle = detail::rvv_atan(vy, vx, vl, atan_params);
-        RVV_T::vstore(angle, vangle, vl);
+        RVV_T::vstore(angle, RVV_T::cast(vangle, vl), vl);
     }
 
     return CV_HAL_ERROR_OK;
 }
 
 }}  // namespace cv::cv_hal_rvv
+
+#endif  // OPENCV_HAL_RVV_CART_TO_POLAR_HPP_INCLUDED
