c10b80083719d5862ce4e37572b43803b94fe86b
Vincent Rabaud <vrabaud@google.com>
2025-03-20 15:31:47 +0100

    Merge pull request #27081 from vrabaud:lzw
    GIF: Make sure to resize lzwExtraTable before each block #27081
    
    This fixes https://g-issues.oss-fuzz.com/issues/403364362
    
    ### Pull Request Readiness Checklist
    
    See details at https://github.com/opencv/opencv/wiki/How_to_contribute#making-a-good-pull-request
    
    - [x] I agree to contribute to the project under Apache 2 License.
    - [x] To the best of my knowledge, the proposed patch is not based on a code under GPL or another license that is incompatible with OpenCV
    - [x] The PR is proposed to the proper branch
    - [x] There is a reference to the original bug report and related work
    - [ ] There is accuracy test, performance test and test data in opencv_extra repository, if applicable
          Patch to opencv_extra has the same branch name.
    - [ ] The feature is well documented and sample code can be built with the project CMake

--- modules/imgcodecs/src/grfmt_gif.cpp
+++ modules/imgcodecs/src/grfmt_gif.cpp
@@ -320,11 +320,18 @@ bool GifDecoder::lzwDecode() {
     const int lzwMaxSize = (1 << 12); // 4096 is the maximum size of the LZW table (12 bits)
     int lzwCodeSize = lzwMinCodeSize + 1;
     CV_Assert(lzwCodeSize > 2 && lzwCodeSize <= 12);
-    int clearCode = 1 << lzwMinCodeSize;
-    int exitCode = clearCode + 1;
+    const int clearCode = 1 << lzwMinCodeSize;
+    const int exitCode = clearCode + 1;
     std::vector<lzwNodeD> lzwExtraTable(lzwMaxSize + 1);
-    int colorTableSize = clearCode;
+    const int colorTableSize = clearCode;
     int lzwTableSize = exitCode;
+    auto clear = [&]() {
+        lzwExtraTable.clear();
+        lzwExtraTable.resize(lzwMaxSize + 1);
+        // reset the code size, the same as that in the initialization part
+        lzwCodeSize  = lzwMinCodeSize + 1;
+        lzwTableSize = exitCode;
+    };
 
     idx = 0;
     int leftBits = 0;
@@ -345,18 +352,12 @@ bool GifDecoder::lzwDecode() {
 
             // clear code
             if (!(code ^ clearCode)) {
-                lzwExtraTable.clear();
-                lzwExtraTable.resize(lzwMaxSize + 1);
-                // reset the code size, the same as that in the initialization part
-                lzwCodeSize  = lzwMinCodeSize + 1;
-                lzwTableSize = exitCode;
+                clear();
                 continue;
             }
             // end of information
             if (!(code ^ exitCode)) {
-                lzwExtraTable.clear();
-                lzwCodeSize  = lzwMinCodeSize + 1;
-                lzwTableSize = exitCode;
+                clear();
                 break;
             }
 
