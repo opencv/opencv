7876fc531b4772d0e380b640ac2e927fc65cf883
Alexander Smorkalov <alexander.smorkalov@opencv.ai>
2025-03-22 17:29:42 +0300

    Move IPP norm to HAL.

--- 3rdparty/ipphal/include/ipp_hal_core.hpp
+++ 3rdparty/ipphal/include/ipp_hal_core.hpp
@@ -10,6 +10,12 @@ int ipp_hal_meanStdDev(const uchar* src_data, size_t src_step, int width, int he
 
 #undef cv_hal_meanStdDev
 #define cv_hal_meanStdDev ipp_hal_meanStdDev
+
+int ipp_hal_norm(const uchar* src, size_t src_step, const uchar* mask, size_t mask_step,
+                 int width, int height, int type, int norm_type, double* result);
+
+#undef cv_hal_norm
+#define cv_hal_norm ipp_hal_norm
 #endif
 
 #endif
