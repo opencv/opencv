c6ac0a50f8694f0e333024ba3b0eaea40bf9dd45
fengyuentau <yuantao.feng@outlook.com>
2025-03-24 22:46:25 +0800

    added copyright

--- modules/core/src/norm.simd.hpp
+++ modules/core/src/norm.simd.hpp
@@ -1,6 +1,9 @@
 // This file is part of OpenCV project.
 // It is subject to the license terms in the LICENSE file found in the top-level directory
 // of this distribution and at http://opencv.org/license.html
+//
+// Copyright (C) 2025, SpaceMIT Inc., all rights reserved.
+// Third party copyrights are property of their respective owners.
 
 #include "precomp.hpp"
 
