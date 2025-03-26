b2a44fb86b60a24ac214a065c756ecb5ed0b59ad
Advait Dhamorikar <advaitdhamorikar@gmail.com>
2025-03-26 10:53:00 +0530

    update license to Apache 2.0

--- modules/videoio/src/cap_libcamera.cpp
+++ modules/videoio/src/cap_libcamera.cpp
@@ -1,32 +1,13 @@
 /*
- *  cap_libcamera.cpp
- *  For Video I/O
- *  by Advait Dhamorikar advaitdhamorikar[at]gmail[dot]com
- *  and Umang Jain email[at]uajain[dot]com
- *  Copyright 2025. All rights reserved.
+ * cap_libcamera.cpp
+ * For Video I/O
  *
- * Redistribution and use in source and binary forms, with or without
- * modification, are permitted provided that the following conditions are met:
+ * This file is part of the OpenCV project.
+ * It is subject to the license terms in the LICENSE file found in the top-level directory
+ * of this distribution and at http://opencv.org/license.html.
  *
- * 1. Redistributions of source code must retain the above copyright notice,
- *    this list of conditions and the following disclaimer.
- * 2. Redistributions in binary form must reproduce the above copyright notice,
- *    this list of conditions and the following disclaimer in the documentation
- *    and/or other materials provided with the distribution.
- * 3. The name of the author may not be used to endorse or promote products
- *    derived from this software without specific prior written permission.
- *
- * THIS SOFTWARE IS PROVIDED BY THE AUTHOR "AS IS" AND ANY EXPRESS OR IMPLIED
- * WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF
- * MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO
- * EVENT SHALL THE AUTHOR BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
- * SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO,
- * PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS;
- * OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY,
- * WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR
- * OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF
- * ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
- *
+ * Copyright 2025 Advait Dhamorikar <advaitdhamorikar[at]gmail.com>
+ * Copyright 2025 Umang Jain <email[at]uajain.com>
  */
 
 #include "precomp.hpp"
