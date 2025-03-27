c9b19054cf713611b55a57376823479df420d024
onikolskyy <onikolskyy@gmail.com>
2025-03-27 15:33:14 +0100

    test metadata parsing

---
+++ modules/dnn/test/test_gguf_importer.cpp
@@ -0,0 +1,58 @@
+#include "test_precomp.hpp"
+#include "opencv2/dnn.hpp"
+#include <fstream>
+
+
+namespace opencv_test { namespace {
+// Helper function to mimic the _tf function.
+template<typename TString>
+static std::string _tf(TString filename, bool required = true)
+{
+    return findDataFile(std::string("dnn/gguf/") + filename, required);
+}
+
+
+
+class Test_GGUFImporter : public ::testing::Test
+{
+protected:
+    // You can do common initialization here if needed.
+};
+
+// This test creates a new GGUFImporter, parses a file using _tf to locate it,
+// then calls parse_attn_qkv() and checks that a Gemm layer has been added to the net.
+TEST_F(Test_GGUFImporter, readNetFromGGUF)
+{
+    // Locate the GGUF file; this should be in the directory dnn/gguf/ (adjust the filename as needed)
+    std::string filePath = _tf("dummy.gguf", true);
+    
+    Net net = readNetFromGGUF(filePath.c_str());
+    
+    // // Parse the file (throws if file reading or parsing fails)
+    // ASSERT_NO_THROW(importer.parseFile(filePath.c_str()));
+    
+    // // Create a dummy LayerParams object for the attn_qkv layer
+    // LayerParams lp;
+    
+    // // Call parse_attn_qkv to simulate the test parsing step
+    // ASSERT_NO_THROW(importer.parse_attn_qkv(lp));
+    
+    // // After parse_attn_qkv, the importer.net should have a mainGraph set.
+    // Ptr<Graph> graph = importer.net.getImpl()->mainGraph;
+    // ASSERT_FALSE(graph.empty()) << "Graph should not be empty after parsing attn_qkv.";
+    
+    // // The graph should contain at least one layer.
+    // std::vector<Ptr<Layer>> prog = graph->getProg();
+    // ASSERT_FALSE(prog.empty()) << "Graph program should contain at least one layer.";
+    
+    // // We expect that parse_attn_qkv creates a Gemm layer.
+    // EXPECT_EQ(prog[0]->type, "Gemm");
+}
+    
+// Main entry point for tests.
+GTEST_API_ int main(int argc, char **argv)
+{
+    testing::InitGoogleTest(&argc, argv);
+    return RUN_ALL_TESTS();
+}
+}}
