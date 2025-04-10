61da2dfbc22a8a95db16949e48be2010133c70cc
Gursimar Singh <gursimar@bigvisionllc.com>
2025-04-10 20:05:43 +0530

    Added test with colorPatchMask

--- modules/photo/test/test_ccm.cpp
+++ modules/photo/test/test_ccm.cpp
@@ -102,6 +102,52 @@ TEST(Photo_ColorCorrection, test_model)
     -0.27627010, 0.00603427, 2.74272981);
     EXPECT_MAT_NEAR(colorCorrectionMat, refColorMat, 1e-4);
 }
+
+TEST(Photo_ColorCorrection, test_model2)
+{
+    Mat dstData = (Mat_<Vec3d>(24, 1) <<
+        Vec3d(37.986, 13.555, 14.059),
+        Vec3d(65.711, 18.13, 17.81),
+        Vec3d(49.927, -4.88, -21.925),
+        Vec3d(43.139, -13.095, 21.905),
+        Vec3d(55.112, 8.843999999999999, -25.399),
+        Vec3d(70.71899999999999, -33.397, -0.199),
+        Vec3d(62.661, 36.067, 57.096),
+        Vec3d(40.02, 10.41, -45.964),
+        Vec3d(51.124, 48.239, 16.248),
+        Vec3d(30.325, 22.976, -21.587),
+        Vec3d(72.532, -23.709, 57.255),
+        Vec3d(71.941, 19.363, 67.857),
+        Vec3d(28.778, 14.179, -50.297),
+        Vec3d(55.261, -38.342, 31.37),
+        Vec3d(42.101, 53.378, 28.19),
+        Vec3d(81.733, 4.039, 79.819),
+        Vec3d(51.935, 49.986, -14.574),
+        Vec3d(51.038, -28.631, -28.638),
+        Vec3d(96.539, -0.425, 1.186),
+        Vec3d(81.25700000000001, -0.638, -0.335),
+        Vec3d(66.76600000000001, -0.734, -0.504),
+        Vec3d(50.867, -0.153, -0.27),
+        Vec3d(35.656, -0.421, -1.231),
+        Vec3d(20.461, -0.079, -0.973)
+    );
+
+    Mat coloredMask = (Mat_<uchar>(24, 1) <<
+        1, 1, 1, 1, 1, 1,
+        1, 1, 1, 1, 1, 1,
+        1, 1, 1, 1, 1, 1,
+        0, 0, 0, 0, 0, 0);
+
+    cv::ccm::ColorCorrectionModel model(s/255, dstData, cv::ccm::COLOR_SPACE_LAB_D50_2, coloredMask);
+    Mat colorCorrectionMat = model.compute();
+
+    Mat refColorMat = (Mat_<double>(3, 3) <<
+    0.37406520, 0.02066507, 0.05804047,
+    0.12719672, 0.77389268, -0.01569404,
+    -0.27627010, 0.00603427, 2.74272981);
+    EXPECT_MAT_NEAR(colorCorrectionMat, refColorMat, 1e-4);
+}
+
 TEST(Photo_ColorCorrection, test_masks_weights_1)
 {
     Mat weightsList_ = (Mat_<double>(24, 1) <<
