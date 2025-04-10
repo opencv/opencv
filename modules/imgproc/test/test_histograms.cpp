4a346e7cb8af0a2098b4d4f02b6d632849caa151
Kumataro <Kumataro@users.noreply.github.com>
2025-04-10 20:55:44 +0900

    fix C4244

--- modules/imgproc/test/test_histograms.cpp
+++ modules/imgproc/test/test_histograms.cpp
@@ -2103,12 +2103,12 @@ TEST(Imgproc_Hist_Compare, intersect_regression_24757)
     cv::Mat src2 = cv::Mat(128,1, CV_32FC1, cv::Scalar(std::numeric_limits<double>::max()));
 
                                              // Ideal result        Wrong result
-    src1.at<float>(32 * 0,0) = +1.0;         // work = +1.0         +1.0
-    src1.at<float>(32 * 1,0) = +55555555.5;  // work = +55555556.5  +55555555.5
-    src1.at<float>(32 * 2,0) = -55555555.5;  // work = +1.0         0.0
-    src1.at<float>(32 * 3,0) = -1.0;         // work = 0.0          -1.0
+    src1.at<float>(32 * 0,0) = +1.0f;        // work = +1.0         +1.0
+    src1.at<float>(32 * 1,0) = +55555555.5f; // work = +55555556.5  +55555555.5
+    src1.at<float>(32 * 2,0) = -55555555.5f; // work = +1.0         0.0
+    src1.at<float>(32 * 3,0) = -1.0f;        // work = 0.0          -1.0
 
-    EXPECT_FLOAT_EQ(compareHist(src1, src2, cv::HISTCMP_INTERSECT), 0.0);
+    EXPECT_FLOAT_EQ(compareHist(src1, src2, cv::HISTCMP_INTERSECT), 0.0f);
 }
 
 }} // namespace
