2489569d1ebdf17ec4a36cac02b55cf4cc946103
Suleyman TURKMEN <sturkmen@hotmail.com>
2025-04-13 10:59:08 +0300

    Update grfmt_spng.cpp

--- modules/imgcodecs/src/grfmt_spng.cpp
+++ modules/imgcodecs/src/grfmt_spng.cpp
@@ -268,14 +268,14 @@ bool SPngDecoder::readData(Mat &img)
                     Mat tmp(m_height, m_width, CV_16UC4);
                     if (SPNG_OK != spng_decode_image(png_ptr, tmp.data, tmp.total() * tmp.elemSize(), fmt, 0))
                         return false;
-                    cvtColor(tmp, img, COLOR_RGBA2GRAY);
+                    spngCvt_BGRA2Gray_8u_C4C1R(tmp.data,tmp.step1(), img.data, img.step1(), Size(m_width, m_height), 2);
                 }
                 else
                 {
                     Mat tmp(m_height, m_width, CV_8UC3);
                     if (SPNG_OK != spng_decode_image(png_ptr, tmp.data, image_size, fmt, 0))
                         return false;
-                    cvtColor(tmp, img, COLOR_RGB2GRAY);
+                    spngCvt_BGR2Gray_8u_C3C1R(tmp.data,tmp.step1(), img.data, img.step1(), Size(m_width, m_height), 2);
                 }
                 return true;
             }
@@ -286,8 +286,6 @@ bool SPngDecoder::readData(Mat &img)
                 if (SPNG_OK != spng_decode_image(png_ptr, tmp.data, image_size, SPNG_FMT_PNG, 0))
                     return false;
                 cvtColor(tmp, img, COLOR_GRAY2BGR);
-                //icvCvt_Gray2BGR_16u_C1C3R(reinterpret_cast<const ushort*>(tmp.data), 0,
-                    //reinterpret_cast<ushort*>(img.data), 0, Size(m_width, 1));
                 return true;
             }
 
