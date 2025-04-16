b98e8abd5b2a03bbd65f4b2edd7cb0f42ab3c830
Ivan Avdeev <avdeeff@internet.ru>
2025-04-16 12:17:03 +0300

    Update build_sdk.py

--- platforms/android/build_sdk.py
+++ platforms/android/build_sdk.py
@@ -505,7 +505,6 @@ if __name__ == "__main__":
         builder.build_library(abi, do_install, args.no_media_ndk)
 
     builder.gather_results()
-    check_have_ipp_flag()
 
     if args.build_doc:
         builder.build_javadoc()
