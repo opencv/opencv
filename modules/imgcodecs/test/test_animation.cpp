2ee08aa828da48e531473bd07ef37b28993e45e4
Suleyman TURKMEN <sturkmen@hotmail.com>
2025-04-17 16:40:36 +0300

    Update test_animation.cpp

--- modules/imgcodecs/test/test_animation.cpp
+++ modules/imgcodecs/test/test_animation.cpp
@@ -676,12 +676,7 @@ TEST(Imgcodecs_APNG, imread_animation_16u)
     img = imread(filename, IMREAD_ANYDEPTH);
     ASSERT_FALSE(img.empty());
     EXPECT_TRUE(img.type() == CV_16UC1);
-#ifdef HAVE_PNG
     EXPECT_EQ(19519, img.at<ushort>(0, 0));
-#endif
-#ifdef HAVE_SPNG
-    EXPECT_EQ(19517, img.at<ushort>(0, 0));
-#endif
 
     img = imread(filename, IMREAD_COLOR | IMREAD_ANYDEPTH);
     ASSERT_FALSE(img.empty());
