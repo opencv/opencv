f149e248f90401fd3747f3ed5d4d5ae65d5ed14f
Ivan Avdeev <avdeeff@internet.ru>
2025-04-17 16:42:05 +0300

    Update default.config.py

--- platforms/android/default.config.py
+++ platforms/android/default.config.py
@@ -1,6 +1,6 @@
 ABIs = [
     ABI("2", "armeabi-v7a", None, 21, cmake_vars=dict(ANDROID_ABI='armeabi-v7a with NEON')),
     ABI("3", "arm64-v8a",   None, 21, cmake_vars=dict(ANDROID_SUPPORT_FLEXIBLE_PAGE_SIZES='ON')),
-    ABI("5", "x86_64",      None, 21, cmake_vars=dict(ANDROID_SUPPORT_FLEXIBLE_PAGE_SIZES='ON')),
-    ABI("4", "x86",         None, 21),
+    ABI("5", "x86_64",      None, 21, cmake_vars=dict(ANDROID_SUPPORT_FLEXIBLE_PAGE_SIZES='ON',WITH_IPP='OFF'),
+    ABI("4", "x86",         None, 21), cmake_vars=dict(WITH_IPP='OFF')
 ]
