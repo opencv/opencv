3bdfb98a312389894adff070c06100acac0e7519
fengyuentau <yuantao.feng@outlook.com>
2025-04-25 21:44:32 +0800

    perf: further optimized flipY (C3)

--- hal/riscv-rvv/hal_rvv_1p0/flip.hpp
+++ hal/riscv-rvv/hal_rvv_1p0/flip.hpp
@@ -46,6 +46,63 @@ CV_HAL_RVV_FLIPY_C1(16UC1, ushort, RVV_U16M8)
 CV_HAL_RVV_FLIPY_C1(32UC1, unsigned, RVV_U32M8)
 CV_HAL_RVV_FLIPY_C1(64UC1, uint64_t, RVV_U64M8)
 
+struct RVV_C3_U8M2 : RVV_U8M2 {
+    static inline vuint8m2x3_t vload3(const uint8_t *base, size_t vl) { return __riscv_vlseg3e8_v_u8m2x3(base, vl); }
+    static inline vuint8m2x3_t vflip3(const vuint8m2x3_t &v_tuple, const vuint8m2_t &indices, size_t vl) {
+        return __riscv_vcreate_v_u8m2x3(__riscv_vrgather(__riscv_vget_u8m2(v_tuple, 0), indices, vl),
+                                        __riscv_vrgather(__riscv_vget_u8m2(v_tuple, 1), indices, vl),
+                                        __riscv_vrgather(__riscv_vget_u8m2(v_tuple, 2), indices, vl));
+    }
+    static inline void vstore3(uint8_t *base, const vuint8m2x3_t &v_tuple, size_t vl) { __riscv_vsseg3e8(base, v_tuple, vl); }
+};
+struct RVV_C3_U16M2 : RVV_U16M2 {
+    static inline vuint16m2x3_t vload3(const uint16_t *base, size_t vl) { return __riscv_vlseg3e16_v_u16m2x3(base, vl); }
+    static inline vuint16m2x3_t vflip3(const vuint16m2x3_t &v_tuple, const vuint16m2_t &indices, size_t vl) {
+        return __riscv_vcreate_v_u16m2x3(__riscv_vrgather(__riscv_vget_u16m2(v_tuple, 0), indices, vl),
+                                         __riscv_vrgather(__riscv_vget_u16m2(v_tuple, 1), indices, vl),
+                                         __riscv_vrgather(__riscv_vget_u16m2(v_tuple, 2), indices, vl));
+    }
+    static inline void vstore3(uint16_t *base, const vuint16m2x3_t &v_tuple, size_t vl) { __riscv_vsseg3e16(base, v_tuple, vl); }
+};
+struct RVV_C3_U32M2 : RVV_U32M2 {
+    static inline vuint32m2x3_t vload3(const uint32_t *base, size_t vl) { return __riscv_vlseg3e32_v_u32m2x3(base, vl); }
+    static inline vuint32m2x3_t vflip3(const vuint32m2x3_t &v_tuple, const vuint32m2_t &indices, size_t vl) {
+        return __riscv_vcreate_v_u32m2x3(__riscv_vrgather(__riscv_vget_u32m2(v_tuple, 0), indices, vl),
+                                         __riscv_vrgather(__riscv_vget_u32m2(v_tuple, 1), indices, vl),
+                                         __riscv_vrgather(__riscv_vget_u32m2(v_tuple, 2), indices, vl));
+    }
+    static inline void vstore3(uint32_t *base, const vuint32m2x3_t &v_tuple, size_t vl) { __riscv_vsseg3e32(base, v_tuple, vl); }
+};
+struct RVV_C3_U64M2 : RVV_U64M2 {
+    static inline vuint64m2x3_t vload3(const uint64_t *base, size_t vl) { return __riscv_vlseg3e64_v_u64m2x3(base, vl); }
+    static inline vuint64m2x3_t vflip3(const vuint64m2x3_t &v_tuple, const vuint64m2_t &indices, size_t vl) {
+        return __riscv_vcreate_v_u64m2x3(__riscv_vrgather(__riscv_vget_u64m2(v_tuple, 0), indices, vl),
+                                         __riscv_vrgather(__riscv_vget_u64m2(v_tuple, 1), indices, vl),
+                                         __riscv_vrgather(__riscv_vget_u64m2(v_tuple, 2), indices, vl));
+    }
+    static inline void vstore3(uint64_t *base, const vuint64m2x3_t &v_tuple, size_t vl) { __riscv_vsseg3e64(base, v_tuple, vl); }
+};
+#define CV_HAL_RVV_FLIPY_C3(name, _Tps, RVV) \
+inline void flipY_##name(const uchar* src_data, size_t src_step, uchar* dst_data, size_t dst_step, int src_width, int src_height) { \
+    int vlmax = RVV::setvlmax(); \
+    for (int h = 0; h < src_height; h++) { \
+        const _Tps* src_row = (const _Tps*)(src_data + src_step * h); \
+        _Tps* dst_row = (_Tps*)(dst_data + dst_step * (h + 1)); \
+        int vl; \
+        for (int w = 0; w < src_width; w += vl) { \
+            vl = RVV::setvl(src_width - w); \
+            RVV::VecType indices = __riscv_vrsub(RVV::vid(vl), vl - 1, vl); \
+            auto v = RVV::vload3(src_row + 3 * w, vl); \
+            auto flipped = RVV::vflip3(v, indices, vl); \
+            RVV::vstore3(dst_row - 3 * (w + vl), flipped, vl); \
+        } \
+    } \
+}
+CV_HAL_RVV_FLIPY_C3(8UC3, uchar, RVV_C3_U8M2)
+CV_HAL_RVV_FLIPY_C3(16UC3, ushort, RVV_C3_U16M2)
+CV_HAL_RVV_FLIPY_C3(32UC3, unsigned, RVV_C3_U32M2)
+CV_HAL_RVV_FLIPY_C3(64UC3, uint64_t, RVV_C3_U64M2)
+
 struct FlipVlen256
 {
     using SrcType = RVV_U8M8;
@@ -218,12 +275,28 @@ inline int flip(int src_type,
                 flipY_16UC1(src_data, src_step, dst_data, dst_step, src_width, src_height);
                 return CV_HAL_ERROR_OK;
             }
+            case 3: {
+                flipY_8UC3(src_data, src_step, dst_data, dst_step, src_width, src_height);
+                return CV_HAL_ERROR_OK;
+            }
             case 4: {
                 flipY_32UC1(src_data, src_step, dst_data, dst_step, src_width, src_height);
                 return CV_HAL_ERROR_OK;
             }
+            case 6: {
+                flipY_16UC3(src_data, src_step, dst_data, dst_step, src_width, src_height);
+                return CV_HAL_ERROR_OK;
+            }
             case 8: {
                 flipY_64UC1(src_data, src_step, dst_data, dst_step, src_width, src_height);
+                return CV_HAL_ERROR_OK;
+            }
+            case 12: {
+                flipY_32UC3(src_data, src_step, dst_data, dst_step, src_width, src_height);
+                return CV_HAL_ERROR_OK;
+            }
+            case 24: {
+                flipY_64UC3(src_data, src_step, dst_data, dst_step, src_width, src_height);
                 return CV_HAL_ERROR_OK;
             }
             // no default
