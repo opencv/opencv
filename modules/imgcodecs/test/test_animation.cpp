d71e1a4f6922545308cdc767197bea99b27f3283
Suleyman TURKMEN <sturkmen@hotmail.com>
2025-05-12 22:36:04 +0300

    keep old behaviour of imread

--- modules/imgcodecs/test/test_animation.cpp
+++ modules/imgcodecs/test/test_animation.cpp
@@ -678,48 +678,48 @@ TEST(Imgcodecs_APNG, imread_animation_16u)
     Mat img = imread(filename, IMREAD_UNCHANGED);
     ASSERT_FALSE(img.empty());
     EXPECT_TRUE(img.type() == CV_16UC4);
-    EXPECT_EQ(0, img.at<ushort>(0, 0));
-    EXPECT_EQ(0, img.at<ushort>(0, 1));
-    EXPECT_EQ(0, img.at<ushort>(0, 2));
+    EXPECT_EQ(0,     img.at<ushort>(0, 0));
+    EXPECT_EQ(0,     img.at<ushort>(0, 1));
+    EXPECT_EQ(65280, img.at<ushort>(0, 2));
     EXPECT_EQ(65535, img.at<ushort>(0, 3));
 
     img = imread(filename, IMREAD_GRAYSCALE);
     ASSERT_FALSE(img.empty());
     EXPECT_TRUE(img.type() == CV_8UC1);
-    EXPECT_EQ(0, img.at<uchar>(0, 0));
+    EXPECT_EQ(76, img.at<uchar>(0, 0));
 
     img = imread(filename, IMREAD_COLOR);
     ASSERT_FALSE(img.empty());
     EXPECT_TRUE(img.type() == CV_8UC3);
-    EXPECT_EQ(0, img.at<uchar>(0, 0));
-    EXPECT_EQ(0, img.at<uchar>(0, 1));
-    EXPECT_EQ(0, img.at<uchar>(0, 2));
+    EXPECT_EQ(0,   img.at<uchar>(0, 0));
+    EXPECT_EQ(0,   img.at<uchar>(0, 1));
+    EXPECT_EQ(255, img.at<uchar>(0, 2));
 
     img = imread(filename, IMREAD_COLOR_RGB);
     ASSERT_FALSE(img.empty());
     EXPECT_TRUE(img.type() == CV_8UC3);
-    EXPECT_EQ(0, img.at<uchar>(0, 0));
-    EXPECT_EQ(0, img.at<uchar>(0, 1));
-    EXPECT_EQ(0, img.at<uchar>(0, 2));
+    EXPECT_EQ(255, img.at<uchar>(0, 0));
+    EXPECT_EQ(0,   img.at<uchar>(0, 1));
+    EXPECT_EQ(0,   img.at<uchar>(0, 2));
 
     img = imread(filename, IMREAD_ANYDEPTH);
     ASSERT_FALSE(img.empty());
     EXPECT_TRUE(img.type() == CV_16UC1);
-    EXPECT_EQ(0, img.at<ushort>(0, 0));
+    EXPECT_EQ(19517, img.at<ushort>(0, 0));
 
     img = imread(filename, IMREAD_COLOR | IMREAD_ANYDEPTH);
     ASSERT_FALSE(img.empty());
     EXPECT_TRUE(img.type() == CV_16UC3);
-    EXPECT_EQ(0, img.at<ushort>(0, 0));
-    EXPECT_EQ(0, img.at<ushort>(0, 1));
-    EXPECT_EQ(0, img.at<ushort>(0, 2));
+    EXPECT_EQ(0,     img.at<ushort>(0, 0));
+    EXPECT_EQ(0,     img.at<ushort>(0, 1));
+    EXPECT_EQ(65280, img.at<ushort>(0, 2));
 
     img = imread(filename, IMREAD_COLOR_RGB | IMREAD_ANYDEPTH);
     ASSERT_FALSE(img.empty());
     EXPECT_TRUE(img.type() == CV_16UC3);
-    EXPECT_EQ(0, img.at<ushort>(0, 0));
-    EXPECT_EQ(0, img.at<ushort>(0, 1));
-    EXPECT_EQ(0, img.at<ushort>(0, 2));
+    EXPECT_EQ(65280, img.at<ushort>(0, 0));
+    EXPECT_EQ(0,     img.at<ushort>(0, 1));
+    EXPECT_EQ(0,     img.at<ushort>(0, 2));
 }
 
 #endif // HAVE_PNG || HAVE_SPNG
