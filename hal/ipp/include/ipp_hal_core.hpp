fbcc32833093a50ad0754a436d9f3770403fc7d7
Alexander Smorkalov <alexander.smorkalov@opencv.ai>
2025-05-24 16:57:49 +0300

    Added default copyright header to IPP HAL.

--- hal/ipp/include/ipp_hal_core.hpp
+++ hal/ipp/include/ipp_hal_core.hpp
@@ -1,3 +1,7 @@
+// This file is part of OpenCV project.
+// It is subject to the license terms in the LICENSE file found in the top-level directory
+// of this distribution and at http://opencv.org/license.html
+
 #ifndef __IPP_HAL_CORE_HPP__
 #define __IPP_HAL_CORE_HPP__
 
