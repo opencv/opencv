a45855d8a376114ffe06f78c93514941d8b16cd7
assriram_amdeng <Ashish.Sriram@amd.com>
2025-06-11 18:46:54 +0530

    imgproc:threshold Added AMD copyright

--- modules/imgproc/src/thresh.cpp
+++ modules/imgproc/src/thresh.cpp
@@ -12,6 +12,7 @@
 //
 // Copyright (C) 2000-2008, Intel Corporation, all rights reserved.
 // Copyright (C) 2009, Willow Garage Inc., all rights reserved.
+// Copyright (C) 2025, Advanced Micro Devices, all rights reserved.
 // Third party copyrights are property of their respective owners.
 //
 // Redistribution and use in source and binary forms, with or without modification,
