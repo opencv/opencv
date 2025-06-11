452882c0076f9df6925b6de3ff844ee26b8eef8d
Kavyansh Tyagi <142140238+KAVYANSHTYAGI@users.noreply.github.com>
2025-06-11 15:54:29 +0530

    Add diamond structuring element

--- modules/imgproc/include/opencv2/imgproc.hpp
+++ modules/imgproc/include/opencv2/imgproc.hpp
@@ -235,8 +235,9 @@ enum MorphShapes {
     MORPH_RECT    = 0, //!< a rectangular structuring element:  \f[E_{ij}=1\f]
     MORPH_CROSS   = 1, //!< a cross-shaped structuring element:
                        //!< \f[E_{ij} = \begin{cases} 1 & \texttt{if } {i=\texttt{anchor.y } {or } {j=\texttt{anchor.x}}} \\0 & \texttt{otherwise} \end{cases}\f]
-    MORPH_ELLIPSE = 2 //!< an elliptic structuring element, that is, a filled ellipse inscribed
+    MORPH_ELLIPSE = 2, //!< an elliptic structuring element, that is, a filled ellipse inscribed
                       //!< into the rectangle Rect(0, 0, esize.width, esize.height)
+    MORPH_DIAMOND = 3  //!< a diamond structuring element defined by Manhattan distance
 };
 
 //! @} imgproc_filter
