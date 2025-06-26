6f33653c0d7024dc12e7a873cf7ed44b7cd4e827
MykhailoTrushch <mykhailo.trushch@tum.de>
2025-06-26 20:20:45 +0200

    improved tests

--- modules/photo/perf/perf_chromatic_aberration.cpp
+++ modules/photo/perf/perf_chromatic_aberration.cpp
@@ -2,6 +2,44 @@
 
 namespace opencv_test
 {
+namespace
+{
+PERF_TEST(Photo, ChromaticAberrationCorrector)
+{
+    std::string calib_file = getDataPath("cv/cameracalibration/chromatic_aberration/calib_result.yaml");
+    std::string image_file = getDataPath("cv/cameracalibration/chromatic_aberration/ca_photo.png");
 
+    Mat src = imread(image_file);
+    ASSERT_FALSE(src.empty()) << "Could not load input image";
 
+    cv::ChromaticAberrationCorrector corrector;
+    ASSERT_TRUE(corrector.loadCalibration(calib_file));
+
+    Mat dst;
+
+    TEST_CYCLE()
+    {
+        dst = corrector.correctImage(src);
+    }
+
+    SANITY_CHECK_NOTHING();
+}
+
+PERF_TEST(Photo, CorrectChromaticAberrationFunction)
+{
+    std::string calib_file = getDataPath("cv/cameracalibration/chromatic_aberration/calib_result.yaml");
+    std::string image_file = getDataPath("cv/cameracalibration/chromatic_aberration/ca_photo.png");
+
+    Mat src = imread(image_file);
+    ASSERT_FALSE(src.empty()) << "Could not load input image";
+
+    TEST_CYCLE()
+    {
+        Mat dst = cv::correctChromaticAberration(src, calib_file);
+        (void)dst;
+    }
+
+    SANITY_CHECK_NOTHING();
+}
 } // namespace
+} // namespace opencv_test
