cb074c233b84a06ebe2f339432fb634c82655d13
Alexander Smorkalov <2536374+asmorkalov@users.noreply.github.com>
2025-07-01 08:30:31 +0300

    Update hal/ipp/include/ipp_hal_imgproc.hpp
    Co-authored-by: victorget <victor.getmanskiy@intel.com>

--- hal/ipp/include/ipp_hal_imgproc.hpp
+++ hal/ipp/include/ipp_hal_imgproc.hpp
@@ -15,7 +15,7 @@ int ipp_hal_warpAffine(int src_type, const uchar *src_data, size_t src_step, int
 
 #if IPP_VERSION_X100 >= 810
 int ipp_hal_warpPerspective(int src_type, const uchar *src_data, size_t src_step, int src_width, int src_height, uchar *dst_data, size_t dst_step, int dst_width,
-                            int dst_height, const double M[6], int interpolation, int borderType, const double borderValue[4]);
+                            int dst_height, const double M[9], int interpolation, int borderType, const double borderValue[4]);
 #undef cv_hal_warpPerspective
 #define cv_hal_warpPerspective ipp_hal_warpPerspective
 #endif
