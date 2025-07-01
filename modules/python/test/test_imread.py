ab6b932be07568ac025c3764610bd0f33383e15b
Alexander Smorkalov <alexander.smorkalov@opencv.ai>
2025-07-01 13:12:03 +0300

    Smoke test for cv.imreadWithMetadata in Python.

--- modules/python/test/test_imread.py
+++ modules/python/test/test_imread.py
@@ -22,6 +22,17 @@ class imread_test(NewOpenCVTests):
         cv.imread(path, img)
         self.assertEqual(cv.norm(ref, img, cv.NORM_INF), 0.0)
 
+    def test_imread_with_meta(self):
+        path = self.extraTestDataPath + '/highgui/readwrite/testExifOrientation_1.jpg'
+        img, meta_types, meta_data = cv.imreadWithMetadata(path)
+        self.assertTrue(meta_types is not None)
+        self.assertTrue(meta_data is not None)
+
+        path = self.extraTestDataPath + '/highgui/readwrite/testExifOrientation_1.png'
+        img, meta_types, meta_data = cv.imreadWithMetadata(path)
+        self.assertTrue(meta_types is not None)
+        self.assertTrue(meta_data is not None)
+
 
 if __name__ == '__main__':
     NewOpenCVTests.bootstrap()
