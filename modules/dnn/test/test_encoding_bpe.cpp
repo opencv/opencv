680cc8486f2e569597e1fba4fb973ea7c99d8bd4
JorgeV92 <jorgevmg31@gmail.com>
2025-07-08 23:14:21 -0500

    Added fixed to the tests

--- modules/dnn/test/test_encoding_bpe.cpp
+++ modules/dnn/test/test_encoding_bpe.cpp
@@ -1,6 +1,8 @@
 #include "test_precomp.hpp"
-#include "../src/tokenizertokens/core_bpe.hpp"
-#include "../src/tokenizertokens/encoding.hpp"
+#include "../src/tokenizer/core_bpe.hpp"
+#include "../src/tokenizer/encoding.hpp"
+#include <fstream>
+#include <sstream>
 
 namespace opencv_test { namespace  {
 
@@ -18,6 +20,34 @@ TEST(EncodingBPE, EncodingDecode_GPT2) {
     std::string sent = enc.decode({31373, 995});
     std::string expected = "hello world";
     EXPECT_EQ(sent, expected);
+}
+
+TEST(EncodingBPE, TrainAndEncodeDecode_Simple) {
+    // Pattern string (same as your Python example)
+    std::string gpt2_pattern = 
+        "'s|'t|'re|'ve|'m|'ll|'d| ?[\\p{L}]+| ?[\\p{N}]+| ?[^\\s\\p{L}\\p{N}]+|\\s+(?!\\S)|\\s+";
+
+    // Read this source file as training data
+    std::ifstream f(__FILE__);
+    ASSERT_TRUE(f.is_open());
+    std::stringstream buffer;
+    buffer << f.rdbuf();
+    std::string data = buffer.str();
+
+    // Train encoding
+    int vocab_size = 600;
+    Encoding enc(data, vocab_size, gpt2_pattern);
+
+    // Encode and decode "hello world"
+    std::vector<Rank> tokens = enc.encodeOrdinary("hello world");
+    std::string decoded = enc.decode(tokens);
+
+    EXPECT_EQ(decoded, "hello world");
+
+    std::vector<std::uint8_t> bytes = enc.decodeBytes(tokens);
+    std::string bytes_str(bytes.begin(), bytes.end());
+    std::string res = replaceGWithSpace(bytes_str);
+    EXPECT_EQ(res, "hello world");
 }
 
 }}
