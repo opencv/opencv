a6f2f9c1a06075adf2b32c6d3bae9260a0c40c33
Suleyman TURKMEN <sturkmen@hotmail.com>
2025-07-09 19:54:44 +0300

    Update imgcodecs.hpp

--- modules/imgcodecs/include/opencv2/imgcodecs.hpp
+++ modules/imgcodecs/include/opencv2/imgcodecs.hpp
@@ -811,13 +811,6 @@ public:
     CV_WRAP size_t size() const;
 
     /**
-     * @brief Returns the number of frames as a 32-bit integer.
-     *
-     * @return Number of frames, cast to `int`. Useful for bindings with languages that don't support `size_t`.
-     */
-    int size32() const { return (int)size(); }
-
-    /**
      * @brief Returns the width of frames in the collection.
      *
      * @return Width in pixels.
