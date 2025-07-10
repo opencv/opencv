6f637218fb16a316b18bc5565db548d0395defef
onikolskyy <onikolskyy@gmail.com>
2025-07-10 11:02:49 +0200

    trilu

--- modules/dnn/src/layers/trilu_layer.cpp
+++ modules/dnn/src/layers/trilu_layer.cpp
@@ -3,20 +3,15 @@
 // of this distribution and at http://opencv.org/license.html.
 
 #include "../precomp.hpp"
-
+#include <opencv2/dnn/shape_utils.hpp>
+using namespace std;
 namespace cv { namespace dnn {
-
-
-static void triluKernel(){
-
-}
-
 
 class TriluLayerImpl CV_FINAL : public TriluLayer {
     public:
-        AttentionLayerImpl(const LayerParams &params) {
+        TriluLayerImpl(const LayerParams &params) {
             setParamsFrom(params);
-            upper = params.get<bool>("transA", true);
+            uppertri = params.get<bool>("upper", true);
 
         }
 
@@ -25,6 +20,7 @@ class TriluLayerImpl CV_FINAL : public TriluLayer {
                                     std::vector<MatShape> &outputs,
                                     std::vector<MatShape> &internals) const CV_OVERRIDE {
             outputs.assign(1, inputs[0]);
+            return false;
         }
 
 
@@ -33,41 +29,56 @@ class TriluLayerImpl CV_FINAL : public TriluLayer {
             inputs_arr.getMatVector(inputs);
             outputs_arr.getMatVector(outputs);
             inputs[0].copyTo(outputs[0]);
+            int k = inputs.size() > 1 ? inputs[1].at<int64>(0,0) : 0;
             const auto shape_input = shape(inputs[0]);
-            const size_t dims = max(shape_input.size() - 2, 1);
-            const size_t h = static_cast<size_t>(shape_input[1]);
-            const size_t w = static_cast<size_t>(shape_input[1]);
-            const size_t m = std::min(h,w);
-            size_t channels = 1;
-            for (size_t i = 0; i < dims; ++i) 
-                total_elements *= static_cast<size_t>(shape_input[i]);
+            const int cdims = std::max(int(shape_input.size()) - 2, 0);
+            const int h = inputs[0].size[shape_input.size() - 2];
+            const int w = inputs[0].size[shape_input.size() - 1];
+            const int m = std::min(h,w);
+            int loops = 1;
+            for (int i = 0; i < cdims; ++i)
+                loops *= shape_input[i];
             
-            float *dst = inputs[0].ptr<float>();
-            int l0 = upper ? 0 : m-1;
-            l0 += (upper ? -k : k);
-            const int sgn = (upper ? 1 : -1);
-            const int lmax = (upper ? m-1 : 0);
+            float *dst = outputs[0].ptr<float>();
+            auto r = Range(0, static_cast<int>(loops));
             auto fn = [&](const Range &r) {
 
                 for (int i = r.start; i < r.end; i++) {
-                    for(int l=l0; ; (sgn)l <= lmax; l+= sgn) {
-                        const int cmin = upper ? 0 : (l + k + 1);
-                        const int cmax = upper ? (l-k-1) : w;
+                    for(int l=0; l < m; l+=1) {
+                        int cmin = uppertri ? 0 : (l + k + 1);
+                        cmin = std::max(cmin, 0);
+                        const int cmax = uppertri ? min(l+ k -1, w-1) : w-1;
                         const int num_zeros = cmax - cmin + 1;
-                        auto *cur_dst = dst + (w * l + cmin) * sizeof(float); 
-                        if (num_zeros > 0)
-                            std::memset(cur_dst, 0, sizeof(float) * num_zeros);            
+                        auto *cur_dst = dst + ((w * h) * i + (w * l + cmin));
+                        if (cmin < w && num_zeros > 0)
+                            std::memset(cur_dst, 0, sizeof(float) * num_zeros);
                     }
                 }
             };
 
-            double nstripes = ;
+            double nstripes = loops * h * w / 1024.0;
             parallel_for_(Range(0, loops), fn, nstripes);
         }
+
+
+        void getTypes(const std::vector<MatType>& inputs,
+            const int requiredOutputs,
+            const int requiredInternals,
+            std::vector<MatType>& outputs,
+            std::vector<MatType>& internals) const CV_OVERRIDE
+        {
+            outputs.assign(1, CV_32F);
+            // outputs.assign(2, CV_64SC1);
+        }
     private:
-        bool upper;
+        bool uppertri;
 
+};
+
+
+Ptr<TriluLayer> TriluLayer::create(const LayerParams& params)
+{
+    return makePtr<TriluLayerImpl>(params);
 }
-
 
 }}
