77ef7334c96abee441cb9cfdd44f85547b1c8955
Suleyman TURKMEN <sturkmen@hotmail.com>
2025-07-21 01:58:53 +0300

    Update test_ImageCollection.py

--- modules/python/test/test_ImageCollection.py
+++ modules/python/test/test_ImageCollection.py
@@ -16,17 +16,35 @@ from tests_common import NewOpenCVTests
 class imread_test(NewOpenCVTests):
     def test_ImageCollection(self):
         path = self.extraTestDataPath + '/highgui/readwrite/033.png'
+
+        # Load animation using imreadanimation
         animation = cv.Animation()
         success, animation = cv.imreadanimation(path)
         self.assertTrue(success)
+        self.assertEqual(len(animation.frames), 2)
 
-        ic = cv.ImageCollection(path, cv.IMREAD_UNCHANGED)
-        self.assertEqual(2, ic.size())
-        self.assertEqual(128, ic.getWidth())
-        self.assertEqual(64, ic.getHeight())
-        self.assertEqual(cv.CV_16UC4, ic.getType())
-        self.assertEqual(cv.norm(animation.frames[0], ic.at(0), cv.NORM_INF), 0.0)
-        self.assertEqual(cv.norm(animation.frames[1], ic.at(1), cv.NORM_INF), 0.0)
+        # Test ImageCollection
+        ic = cv.ImageCollection()
+        self.assertEqual(ic.size(), 0)
+        self.assertEqual(ic.getWidth(), 0)
+        self.assertEqual(ic.getHeight(), 0)
+        self.assertEqual(ic.getType(), 0)
+
+        ic.init(path, cv.IMREAD_UNCHANGED)
+        self.assertEqual(ic.size(), 2)
+
+        # Check width, height and type
+        self.assertEqual(ic.getWidth(), animation.frames[0].shape[1])
+        self.assertEqual(ic.getHeight(), animation.frames[0].shape[0])
+        self.assertEqual(ic.getType(), animation.frames[0].dtype)
+
+        # Compare frames pixel-wise
+        for i in range(ic.size()):
+            self.assertEqual(
+                cv.norm(animation.frames[i], ic.at(i), cv.NORM_INF),
+                0.0,
+                msg="Mismatch in frame {}".format(i)
+            )
 
 if __name__ == '__main__':
     NewOpenCVTests.bootstrap()
