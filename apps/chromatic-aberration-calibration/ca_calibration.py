0b19961cf872670312d7b9738b708a32a3ca4c05
MykhailoTrushch <mykhailo.trushch@tum.de>
2025-07-28 12:20:51 +0200

    added license

--- apps/chromatic-aberration-calibration/ca_calibration.py
+++ apps/chromatic-aberration-calibration/ca_calibration.py
@@ -1,4 +1,6 @@
-#!/usr/bin/env python
+# This file is part of OpenCV project.
+# It is subject to the license terms in the LICENSE file found in the top-level directory
+# of this distribution and at http://opencv.org/license.html.
 
 '''
 Camera calibration for chromatic aberration correction
