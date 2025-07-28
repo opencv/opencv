fae5ee4ea850b274183957a46ee98b20f56be3c5
Dmitry Kurtaev <dmitry.kurtaev@gmail.com>
2025-07-28 21:13:03 +0300

    GpuMatND

--- modules/core/misc/python/pyopencv_cuda.hpp
+++ modules/core/misc/python/pyopencv_cuda.hpp
@@ -35,11 +35,11 @@ CV_PY_FROM_CLASS(cuda::HostMem)
 
 CV_PY_FROM_CLASS_PTR(cuda::GpuMat::Allocator)
 
-static DLDataType GetDLDataType(Ptr<cv::cuda::GpuMat> m) {
+static DLDataType GetDLDataType(int elemSize1, int depth) {
     DLDataType dtype;
-    dtype.bits = 8 * m->elemSize1();
+    dtype.bits = 8 * elemSize1;
     dtype.lanes = 1;
-    switch (m->depth())
+    switch (depth)
     {
         case CV_8S: case CV_16S: case CV_32S: /*case CV_64S:*/ dtype.code = kDLInt; break;
         case CV_8U: case CV_16U: /*case CV_32U: case CV_64U:*/ dtype.code = kDLUInt; break;
@@ -51,18 +51,53 @@ static DLDataType GetDLDataType(Ptr<cv::cuda::GpuMat> m) {
     return dtype;
 }
 
-static PyObject* pyGpuMatDLPack(PyObject* self, PyObject* py_args, PyObject* kw) {
-    Ptr<cv::cuda::GpuMat> * self1 = 0;
-    if (!pyopencv_cuda_GpuMat_getp(self, self1))
-        return failmsgp("Incorrect type of self (must be 'cuda_GpuMat' or its derivative)");
-    Ptr<cv::cuda::GpuMat> _self_ = *(self1);
+static void buildTensor(Ptr<cv::cuda::GpuMat> src, DLManagedTensor* tensor)
+{
+    tensor->manager_ctx = 0;
+    tensor->deleter = 0;
+    tensor->dl_tensor.data = src->cudaPtr();
+    tensor->dl_tensor.device.device_type = kDLCUDA;
+    tensor->dl_tensor.device.device_id = 0;  // TODO: which id?
+    tensor->dl_tensor.ndim = 3;
+    tensor->dl_tensor.dtype = GetDLDataType(src->elemSize1(), src->depth());
+    tensor->dl_tensor.shape = new int64_t[3];
+    tensor->dl_tensor.shape[0] = src->rows;
+    tensor->dl_tensor.shape[1] = src->cols;
+    tensor->dl_tensor.shape[2] = src->channels();
+    tensor->dl_tensor.strides = new int64_t[3];
+    tensor->dl_tensor.strides[0] = src->step1();
+    tensor->dl_tensor.strides[1] = src->channels();
+    tensor->dl_tensor.strides[2] = 1;
+    tensor->dl_tensor.byte_offset = 0;
+}
 
+static void buildTensor(Ptr<cv::cuda::GpuMatND> src, DLManagedTensor* tensor)
+{
+    tensor->manager_ctx = 0;
+    tensor->deleter = 0;
+    tensor->dl_tensor.data = src->getDevicePtr();
+    tensor->dl_tensor.device.device_type = kDLCUDA;
+    tensor->dl_tensor.device.device_id = 0;  // TODO: which id?
+    tensor->dl_tensor.ndim = src->dims;
+    tensor->dl_tensor.dtype = GetDLDataType(src->elemSize1(), CV_MAT_DEPTH(src->flags));
+    tensor->dl_tensor.shape = new int64_t[src->dims];
+    for (int i = 0; i < src->dims; ++i)
+        tensor->dl_tensor.shape[i] = src->size[i];
+    tensor->dl_tensor.strides = new int64_t[src->dims];
+    for (int i = 0; i < src->dims; ++i)
+        tensor->dl_tensor.strides[i] = src->step[i];
+    tensor->dl_tensor.byte_offset = 0;
+}
+
+template<typename T>
+static PyObject* to_dlpack(const T& src, PyObject* self, PyObject* py_args, PyObject* kw)
+{
     int stream = 0;
     PyObject* maxVersion = nullptr;
     PyObject* dlDevice = nullptr;
     bool copy = false;
     const char* keywords[] = { "stream", "max_version", "dl_device", "copy", NULL };
-    if (!PyArg_ParseTupleAndKeywords(py_args, kw, "|iOOp:cuda_GpuMat.__dlpack__", (char**)keywords, &stream, &maxVersion, &dlDevice, &copy))
+    if (!PyArg_ParseTupleAndKeywords(py_args, kw, "|iOOp:__dlpack__", (char**)keywords, &stream, &maxVersion, &dlDevice, &copy))
         return nullptr;
 
     if (dlDevice && dlDevice != Py_None && PyTuple_Check(dlDevice))
@@ -76,22 +111,7 @@ static PyObject* pyGpuMatDLPack(PyObject* self, PyObject* py_args, PyObject* kw)
         return nullptr;
     }
     DLManagedTensor* tensor = reinterpret_cast<DLManagedTensor*>(ptr);
-    tensor->manager_ctx = 0;
-    tensor->deleter = 0;
-    tensor->dl_tensor.data = _self_->cudaPtr();
-    tensor->dl_tensor.device.device_type = kDLCUDA;
-    tensor->dl_tensor.device.device_id = 0;  // TODO: which id?
-    tensor->dl_tensor.ndim = 3;
-    tensor->dl_tensor.dtype = GetDLDataType(_self_);
-    tensor->dl_tensor.shape = new int64_t[3];
-    tensor->dl_tensor.shape[0] = _self_->rows;
-    tensor->dl_tensor.shape[1] = _self_->cols;
-    tensor->dl_tensor.shape[2] = _self_->channels();
-    tensor->dl_tensor.strides = new int64_t[3];
-    tensor->dl_tensor.strides[0] = _self_->step1();
-    tensor->dl_tensor.strides[1] = _self_->channels();
-    tensor->dl_tensor.strides[2] = 1;
-    tensor->dl_tensor.byte_offset = 0;
+    buildTensor(src, tensor);
 
     PyObject* capsule = PyCapsule_New(ptr, "dltensor", nullptr);
     if (!capsule) {
@@ -105,12 +125,30 @@ static PyObject* pyGpuMatDLPack(PyObject* self, PyObject* py_args, PyObject* kw)
     return capsule;
 }
 
-static PyObject* pyGpuMatDLPackDevice() {
+static PyObject* pyDLPackGpuMat(PyObject* self, PyObject* py_args, PyObject* kw) {
+    Ptr<cv::cuda::GpuMat> * self1 = 0;
+    if (!pyopencv_cuda_GpuMat_getp(self, self1))
+        return failmsgp("Incorrect type of self (must be 'cuda_GpuMat' or its derivative)");
+    return to_dlpack(*(self1), self, py_args, kw);
+}
+
+static PyObject* pyDLPackGpuMatND(PyObject* self, PyObject* py_args, PyObject* kw) {
+    Ptr<cv::cuda::GpuMatND> * self1 = 0;
+    if (!pyopencv_cuda_GpuMatND_getp(self, self1))
+        return failmsgp("Incorrect type of self (must be 'cuda_GpuMatND' or its derivative)");
+    return to_dlpack(*(self1), self, py_args, kw);
+}
+
+static PyObject* pyDLPackDeviceCUDA() {
     return pyopencv_from(std::tuple<int, int>(kDLCUDA, 0));
 }
 
 #define PYOPENCV_EXTRA_METHODS_cuda_GpuMat \
-  {"__dlpack__", CV_PY_FN_WITH_KW(pyGpuMatDLPack), ""}, \
-  {"__dlpack_device__", CV_PY_FN_WITH_KW(pyGpuMatDLPackDevice), ""}, \
+  {"__dlpack__", CV_PY_FN_WITH_KW(pyDLPackGpuMat), ""}, \
+  {"__dlpack_device__", CV_PY_FN_WITH_KW(pyDLPackDeviceCUDA), ""}, \
+
+#define PYOPENCV_EXTRA_METHODS_cuda_GpuMatND \
+  {"__dlpack__", CV_PY_FN_WITH_KW(pyDLPackGpuMatND), ""}, \
+  {"__dlpack_device__", CV_PY_FN_WITH_KW(pyDLPackDeviceCUDA), ""}, \
 
 #endif
