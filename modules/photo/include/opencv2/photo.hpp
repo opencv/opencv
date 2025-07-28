0459403887614341584c9257cf6fc89355fb7066
MykhailoTrushch <mykhailo.trushch@tum.de>
2025-07-28 12:27:36 +0200

    added license and moved sample to correct directory

--- modules/photo/include/opencv2/photo.hpp
+++ modules/photo/include/opencv2/photo.hpp
@@ -898,7 +898,7 @@ CV_EXPORTS_W void stylization(InputArray src, OutputArray dst, float sigma_s = 6
 //! @addtogroup photo_ca_correction Chromatic Aberration Correction
 //! @{
 
-/** @example samples/cpp/tutorial_code/photo/chromatic_aberration_correction/chromatic_aberration_correction.cpp
+/** @example samples/cpp/chromatic_aberration_correction.cpp
 An example correcting chromatic aberration
 */
 /** @brief Corrects lateral chromatic aberration in an image using polynomial distortion model.
--- samples/cpp/chromatic_aberration_correction.cpp
+++ samples/cpp/chromatic_aberration_correction.cpp
@@ -1,3 +1,7 @@
+// This file is part of OpenCV project.
+// It is subject to the license terms in the LICENSE file found in the top-level directory
+// of this distribution and at http://opencv.org/license.html
+
 #include "opencv2/core.hpp"
 #include "opencv2/photo.hpp"
 #include "opencv2/imgcodecs.hpp"
