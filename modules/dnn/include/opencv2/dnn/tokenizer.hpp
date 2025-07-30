d660365c669be690a7ff30bf84b4af9c7116e870
JorgeV92 <jorgevmg31@gmail.com>
2025-07-29 23:11:22 -0500

    Fixed errors and issues with includes sugguested by asmorkalov

--- modules/dnn/include/opencv2/dnn/tokenizer.hpp
+++ modules/dnn/include/opencv2/dnn/tokenizer.hpp
@@ -1,59 +1,48 @@
 #pragma once
 
+#include <opencv2/core.hpp>
 #include <memory>
 #include <string>
 #include <vector>
+#include <unordered_map>
+#include <unordered_set>
+#include <limits>
 
-// #include "../../src/tokenizer/encoding.hpp"
-#include "../../../src/tokenizer/encoding.hpp"
 
 namespace cv { namespace dnn { namespace tokenizer {
+
+class Encoding;
 
 class CV_EXPORTS_W_SIMPLE Tokenizer {
 public:
 
-    CV_WRAP Tokenizer() : enc_(nullptr), tokenizer_name("") {}
-    explicit Tokenizer(std::shared_ptr<Encoding> e,
-                   std::string model_name = "")
-    : enc_(std::move(e)),
-      tokenizer_name(std::move(model_name)) {}
+    CV_WRAP Tokenizer();
+    Tokenizer(std::shared_ptr<Encoding> e,
+                   std::string model_name = "");
 
     CV_WRAP static Tokenizer from_pretrained(const std::string& name, const std::string& pretrained_model_path); 
     CV_EXPORTS static Tokenizer train_bpe_from_corpus(const std::string& corpus,
                                    int vocab_sz,
-                                   const std::string& pattern) {
-        auto enc = std::make_shared<Encoding>(corpus, vocab_sz, pattern);
-        return Tokenizer(std::move(enc));
-    }
+                                   const std::string& pattern);
     CV_EXPORTS static Tokenizer train_bpe_from_corpus(const std::vector<std::string>& corpus,
                                    int vocab_sz,
                                    const std::string& pattern,
                                    int min_freq=2, 
                                    int max_token_length=std::numeric_limits<int>::max(),
-                                    bool verbose=false){
-        auto enc = std::make_shared<Encoding>(corpus, vocab_sz, pattern, min_freq, max_token_length, verbose);
-        return Tokenizer(std::move(enc));
-    }
+                                    bool verbose=false);
     // Encoding
-    CV_WRAP std::vector<uint32_t> encode(const std::string& text,
-                            bool add_special_tokens=false) {
-        if (!add_special_tokens) {
-            return this->encode(text, {}, {});
-        }
-        std::unordered_set<std::string> allowedSpecial = {"__ALL__"};
-        return this->encode(text, allowedSpecial, {});
-    }
-    std::vector<Rank> encode(const std::string& text,
+    CV_WRAP std::vector<int> encode(const std::string& text,
+                            bool add_special_tokens=false);
+    std::vector<int> encode(const std::string& text,
                                      const std::unordered_set<std::string>& allowedSpecial={},
-                                     const std::unordered_set<std::string>& disallowedSpecial={}) const {
-        return enc_->encode(text, allowedSpecial, disallowedSpecial);
-    };
-    std::vector<Rank> encodeOrdinary(const std::string& text) const {return enc_->encodeOrdinary(text); }
-    Rank encodeSingleToken(const std::vector<std::uint8_t>& bytes) const { return enc_->encodeSingleToken(bytes); }
+                                     const std::unordered_set<std::string>& disallowedSpecial={}) const;
+    std::vector<uint32_t> encodeOrdinary(const std::string& text) const;
+    uint32_t encodeSingleToken(const std::vector<std::uint8_t>& bytes) const;
     // Decoding
-    std::string decode(const std::vector<Rank>& tokens) { return enc_->decode(tokens); };
-    std::vector<std::uint8_t> decodeBytes(const std::vector<Rank>& tokens) const { return enc_->decodeBytes(tokens); } 
-    std::vector<std::uint8_t> decodeSingleTokenBytes(Rank token) const { return enc_->decodeSingleTokenBytes(token); }
+    std::string decode(const std::vector<int>& tokens);
+    // std::string decode(const std::vector<uint32_t>& tokens) { return enc_->decode(tokens); };
+    std::vector<std::uint8_t> decodeBytes(const std::vector<uint32_t>& tokens) const;
+    std::vector<std::uint8_t> decodeSingleTokenBytes(uint32_t token) const;
 
     // Accessors
     Encoding& encoding() {return *enc_;}
--- modules/dnn/src/tokenizer/tokenizer.cpp
+++ modules/dnn/src/tokenizer/tokenizer.cpp
@@ -1,7 +1,69 @@
 #include "../../include/opencv2/dnn/tokenizer.hpp"
+#include "encoding.hpp"
 #include "gpt2_tokenizer_fast.hpp"
 
 namespace cv { namespace dnn { namespace tokenizer {
+
+Tokenizer::Tokenizer() : enc_(nullptr), tokenizer_name("") {}
+
+Tokenizer::Tokenizer(std::shared_ptr<Encoding> e,
+                     std::string model_name)
+    : enc_(std::move(e)), tokenizer_name(std::move(model_name)) {
+}
+
+Tokenizer Tokenizer::train_bpe_from_corpus(const std::string& corpus,
+                                   int vocab_sz,
+                                   const std::string& pattern) {
+    auto enc = std::make_shared<Encoding>(corpus, vocab_sz, pattern);
+    return Tokenizer(std::move(enc));
+}
+
+Tokenizer Tokenizer::train_bpe_from_corpus(const std::vector<std::string>& corpus,
+                                   int vocab_sz,
+                                   const std::string& pattern,
+                                   int min_freq, 
+                                   int max_token_length,
+                                    bool verbose){
+    auto enc = std::make_shared<Encoding>(corpus, vocab_sz, pattern, min_freq, max_token_length, verbose);
+    return Tokenizer(std::move(enc));
+}
+
+std::vector<int> Tokenizer::encode(const std::string& text,
+                            bool add_special_tokens) {
+    if (!add_special_tokens) {
+        return this->encode(text, {}, {});
+    }
+    std::unordered_set<std::string> allowedSpecial = {"__ALL__"};
+    return this->encode(text, allowedSpecial, {});
+}
+
+std::vector<int> Tokenizer::encode(const std::string& text,
+                                     const std::unordered_set<std::string>& allowedSpecial,
+                                     const std::unordered_set<std::string>& disallowedSpecial) const {
+    std::vector<uint32_t> tok = enc_->encode(text, allowedSpecial, disallowedSpecial);
+    return std::vector<int>(tok.begin(), tok.end());
+};
+
+std::vector<uint32_t> Tokenizer::encodeOrdinary(const std::string& text) const {
+    return enc_->encodeOrdinary(text); 
+}
+
+uint32_t Tokenizer::encodeSingleToken(const std::vector<std::uint8_t>& bytes) const { 
+    return enc_->encodeSingleToken(bytes); 
+}
+
+std::string Tokenizer::decode(const std::vector<int>& tokens) { 
+        std::vector<uint32_t> tokens32(tokens.begin(), tokens.end());
+        return enc_->decode(tokens32); 
+};
+
+std::vector<std::uint8_t> Tokenizer::decodeBytes(const std::vector<uint32_t>& tokens) const { 
+    return enc_->decodeBytes(tokens); 
+} 
+
+std::vector<std::uint8_t> Tokenizer::decodeSingleTokenBytes(uint32_t token) const { 
+    return enc_->decodeSingleTokenBytes(token); 
+}
 
 Tokenizer Tokenizer::from_pretrained(const std::string& name, const std::string& pretrained_model_path) {
     // We most load files json into FileStorge
