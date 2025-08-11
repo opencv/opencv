375bb920b25588f1e9744473a687fbf4e5efad4b
Abhishek Gola <abhishek.gola@bigvisionllc.com>
2025-08-11 12:16:00 +0530

    Added fully functional convTranspose layer

--- modules/dnn/test/test_onnx_conformance_layer_filter_opencv_classic_denylist.inl.hpp
+++ modules/dnn/test/test_onnx_conformance_layer_filter_opencv_classic_denylist.inl.hpp
@@ -733,3 +733,13 @@
 "test_reduce_sum_square_empty_set",
 "test_reduce_sum_square_empty_set_expanded",
 "test_reduce_log_sum_exp_empty_set_expanded",
+"test_convtranspose", // Issue::Parser::Weights are required as inputs
+"test_convtranspose_1d", // Issue::Parser::Weights are required as inputs
+"test_convtranspose_3d", // Issue::Parser::Weights are required as inputs
+"test_convtranspose_autopad_same", // Issue::Parser::Weights are required as inputs
+"test_convtranspose_dilations", // Issue::Parser::Weights are required as inputs
+"test_convtranspose_kernel_shape", // Issue::Parser::Weights are required as inputs
+"test_convtranspose_output_shape", // Issue::Parser::Weights are required as inputs
+"test_convtranspose_pad", // Issue::Parser::Weights are required as inputs
+"test_convtranspose_pads", // Issue::Parser::Weights are required as inputs
+"test_convtranspose_with_kernel", // Issue::Parser::Weights are required as inputs
