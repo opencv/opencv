3b44afb0d51ac841abdbd93b3dfdc9561179def5
JorgeV92 <jorgevmg31@gmail.com>
2025-08-26 17:44:46 -0500

    Fixed python test for tokenizer to read data correctly

--- modules/python/test/test_tokenizer.py
+++ modules/python/test/test_tokenizer.py
@@ -12,36 +12,32 @@ import os
 
 from tests_common import NewOpenCVTests
 
-def _tf_gpt2(filename=""):
-    base = os.environ.get('OPENCV_DNN_TEST_DATA_PATH') or \
-           os.environ.get('OPENCV_TEST_DATA_PATH', os.getcwd())
-    return os.path.join(base, "testdata", "dnn", "llm", "gpt2", filename)
-
-def _tf_gpt4(filename=""):
-    base = os.environ.get('OPENCV_DNN_TEST_DATA_PATH') or \
-           os.environ.get('OPENCV_TEST_DATA_PATH', os.getcwd())
-    return os.path.join(base, "testdata", "dnn", "llm", "gpt4", filename)
+def _tf(filename=""):
+    base = (os.environ.get("OPENCV_DNN_TEST_DATA_PATH")
+            or os.environ.get("OPENCV_TEST_DATA_PATH")
+            or os.getcwd())
+    return os.path.join(base, "dnn", "llm", filename)
 
 class TokenizerBindingTest(NewOpenCVTests):
     def test_tokenizer_binding(self):
         try:
             tokenizer = cv.dnn.Tokenizer
             print("Tokenizer binding is available.")
-            gpt2_dir = _tf_gpt2("")
-            tokenizer = cv.dnn.Tokenizer.load(gpt2_dir)
-            print("Tokenizer loaded from:", gpt2_dir)
+            gpt2_model = _tf("gpt2/config.json")
+            tokenizer = cv.dnn.Tokenizer.load(gpt2_model)
+            print("Tokenizer loaded from:", gpt2_model)
         except AttributeError:
             self.fail("Tokenizer binding is NOT available.")
 
     def test_tokenizer_gpt2(self):
-        tok = cv.dnn.Tokenizer.load((_tf_gpt2("")))
+        tok = cv.dnn.Tokenizer.load((_tf("gpt2/config.json")))
         ids = tok.encode("hello world")
         print(ids)
         txt = tok.decode(ids)
         self.assertEqual(txt, "hello world")
 
     def test_encoding_gpt4(self):
-        tok = cv.dnn.Tokenizer.load(_tf_gpt4())
+        tok = cv.dnn.Tokenizer.load(_tf("gpt4/config.json"))
         tokens = tok.encode("hello world")
         # expects {15339, 1917}
         self.assertEqual(list(tokens), [15339, 1917])
@@ -54,7 +50,7 @@ class TokenizerBindingTest(NewOpenCVTests):
 
     #     encoding = encoding_for_model("gpt2")
     #     hf_tokenizer = AutoTokenizer.from_pretrained("openai-community/gpt2", use_fast=True)
-    #     cv_tokenizer = cv.dnn.Tokenizer.load(_tf_gpt2())
+    #     cv_tokenizer = cv.dnn.Tokenizer.load(_tf_gpt2("gpt2/config.json"))
     #     sentence = "Young man, in mathematics you don't understand things. You just get used to them."
     #     ids_tik = encoding.encode(sentence)     # returns <class 'list'>
     #     ids_cv = cv_tokenizer.encode(sentence)  # returns <class 'numpy.ndarray'>
