254a49d9a51673fbc3969296d8d4e6cd7e633192
nmizonov <nadezhda.mizonova@intel.com>
2025-08-27 15:24:57 -0700

    Rename tiles var and add a comment for the tiles number calculation

--- modules/imgproc/src/bilateral_filter.dispatch.cpp
+++ modules/imgproc/src/bilateral_filter.dispatch.cpp
@@ -412,13 +412,14 @@ static bool ipp_bilateralFilter(Mat &src, Mat &dst, int d, double sigmaColor, do
 
             // Tile height can't be smaller than the radius.
             // Otherwise, the second tile has mixed top border (pixels from both
-            // inmem and outside should be used), which is not supported in IPP
+            // inmem and outside should be used), which is not supported in IPP.
             int maxTiles = (int)iwDst.m_size.height / radius;
-            int tiles = threads * 4;
-            if (tiles > maxTiles) {
-                tiles = (maxTiles / threads) * threads;
+            int numTiles = threads * 4;
+            if (numTiles > maxTiles) {
+                // Keep the tiles number as multiple of threads for the better workload balance.
+                numTiles = (maxTiles / threads) * threads;
             }
-            parallel_for_(range, invoker, tiles);
+            parallel_for_(range, invoker, numTiles);
 
             if(!ok)
                 return false;
