b20cbb87e2b43ffd010d87897b3ea400b32bbbe6
Dmitry Kurtaev <dmitry.kurtaev@gmail.com>
2025-09-01 21:33:11 +0300

    Copy prims and trc

--- modules/videoio/src/cap_ffmpeg_impl.hpp
+++ modules/videoio/src/cap_ffmpeg_impl.hpp
@@ -1934,6 +1934,8 @@ bool CvCapture_FFMPEG::retrieveFrame(int flag, unsigned char** data, int* step, 
         // Some sws_scale optimizations have some assumptions about alignment of data/step/width/height
         // Also we use coded_width/height to workaround problem with legacy ffmpeg versions (like n0.8)
         int buffer_width = context->coded_width, buffer_height = context->coded_height;
+        buffer_width = video_st->CV_FFMPEG_CODEC_FIELD->width;
+        buffer_height = video_st->CV_FFMPEG_CODEC_FIELD->height;
 
 #if LIBSWSCALE_BUILD >= CALC_FFMPEG_VERSION(8, 12 ,100)
         img_convert_ctx = sws_alloc_context();
@@ -1957,9 +1959,11 @@ bool CvCapture_FFMPEG::retrieveFrame(int flag, unsigned char** data, int* step, 
         rgb_picture.format = result_format;
         rgb_picture.width = buffer_width;
         rgb_picture.height = buffer_height;
-        // rgb_picture.color_range = AVCOL_RANGE_JPEG;
-        // rgb_picture.color_primaries = sw_picture->color_primaries;
-        // rgb_picture.color_trc = sw_picture->color_trc;
+        rgb_picture.color_range = AVCOL_RANGE_JPEG;
+        rgb_picture.colorspace = AVCOL_SPC_RGB;
+        // rgb_picture.chroma_location = (AVChromaLocation)0;
+        rgb_picture.color_primaries = sw_picture->color_primaries;
+        rgb_picture.color_trc = sw_picture->color_trc;
         if (0 != av_frame_get_buffer(&rgb_picture, 32))
         {
             CV_WARN("OutOfMemory");
