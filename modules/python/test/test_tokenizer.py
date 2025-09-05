9fd5a40bd5fa57f422a99f840be9ae25c843ff17
JorgeV92 <jorgevmg31@gmail.com>
2025-09-05 17:20:44 -0500

    Removed loading huggingFace and tiktoken for test

--- modules/python/test/test_tokenizer.py
+++ modules/python/test/test_tokenizer.py
@@ -9,6 +9,7 @@ from __future__ import print_function
 import cv2 as cv
 import sys
 import os
+import json
 
 from tests_common import NewOpenCVTests
 
@@ -45,40 +46,19 @@ class TokenizerBindingTest(NewOpenCVTests):
         self.assertEqual(sent, "hello world")
 
     def test_with_hf_tiktoken(self):
-        from tiktoken import encoding_for_model
-        from transformers import AutoTokenizer
+        tok = cv.dnn.Tokenizer.load(_tf("gpt2/config.json"))
+        with open(_tf("gpt2/gpt2_hf_tik_testdata.json"), "r", encoding="utf-8") as f:
+            golden = json.load(f)
 
-        tik_tokenizer = encoding_for_model("gpt2")
-        hf_tokenizer = AutoTokenizer.from_pretrained("openai-community/gpt2", use_fast=True)
-        cv_tokenizer = cv.dnn.Tokenizer.load(_tf("gpt2/config.json"))
-
-        sentences = {
-            "Spanish": "¡Hola, mundo! ¿Cómo estás?",
-            "Chinese": "你好，世界！今天天气不错。",
-            "Japanese": "こんにちは世界。今日はいい天気ですね。",
-            "Arabic": "مرحبًا بالعالم! كيف الحال؟",
-            "Hindi": "नमस्ते दुनिया! आप कैसे हैं?",
-            "Russian": "Привет, мир! Как дела?",
-            "Korean": "안녕하세요 세계! 오늘은 날씨가 좋아요.",
-            "Thai": "สวัสดีโลก วันนี้อากาศดี",
-            "Greek": "Γεια σου κόσμε! Τι κάνεις;",
-            "Hebrew": "שלום עולם! מה שלומך?",
-            "Emojis": "👩🏽‍💻✨🚀 — coding time!",
-            "Vietnamese": "Xin chào thế giới! Hôm nay trời đẹp.",
-            "Turkish": "Merhaba dünya! Nasılsın?",
-        }
-
-        for i, (name, text) in enumerate(sentences.items()):
-            cv_ids = cv_tokenizer.encode(text)
-            tik_ids = tik_tokenizer.encode(text)
-            hf_ids = hf_tokenizer.encode(text)
-
-            print(f"--- {name} ---")
-            print("text: ", text)
-            print(f"cv token count: {len(cv_ids)} | tiktoken token count {len(tik_ids)} | hf token count {len(hf_ids)}")
-            if i == 0: # First iteration output the types returned by encode for all 3 Tokenizer libraries
-                print(f"type of cv ids [{type(cv_ids)}] | type of tiktoken ids [{type(tik_ids)}] | type of hf ids [{type(hf_ids)}]")
-            self.assertEqual(cv_ids.tolist(), tik_ids, hf_ids)
+        for s in golden["samples"]:
+            text = s["text"]
+            expected = s["ids"]
+            got = tok.encode(text).tolist()
+            self.assertEqual(
+                got, expected,
+                msg=f"Mismatch for sample '{s['name']}'"
+            )
+            self.assertEqual(tok.decode(expected), text)
             
 if __name__ == '__main__':
     NewOpenCVTests.bootstrap()
