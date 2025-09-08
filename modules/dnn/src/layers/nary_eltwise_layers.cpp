786e11e2a7dc7a646dc4451f3f84d9b333fac4b8
Abhishek Gola <abhishek.gola@bigvisionllc.com>
2025-09-08 18:27:17 +0530

    updated broadcasting

--- modules/dnn/src/layers/nary_eltwise_layers.cpp
+++ modules/dnn/src/layers/nary_eltwise_layers.cpp
@@ -34,40 +34,6 @@ static int _mod(int x, int y) {
     return res;
 }
 
-// Pow helpers (adapted from legacy PowLayer)
-static void fillBroadcastSteps(const Mat& m, const std::vector<int>& outputShape, std::vector<size_t>& strides)
-{
-    std::vector<int> ms(m.dims);
-    for (int i = 0; i < m.dims; ++i) ms[i] = m.size[i];
-    const size_t dim = std::max(outputShape.size(), ms.size());
-    std::vector<int> msx = ms; msx.insert(msx.begin(), dim - msx.size(), 1);
-    std::vector<size_t> sx(dim, 0);
-    std::vector<size_t> orig(m.step.p, m.step.p + m.dims);
-    std::vector<size_t> origx = orig; origx.insert(origx.begin(), dim - origx.size(), 0);
-    for (size_t i = 0; i < dim; ++i)
-    {
-        if (msx[i] == 1) sx[i] = 0;
-        else sx[i] = origx[i];
-    }
-    strides = sx;
-}
-
-template<typename T>
-static void applyPowSaturate(const double* a, const double* b, Mat& out)
-{
-    T* o = (T*)out.data;
-    const size_t total = out.total();
-    for (size_t i = 0; i < total; i++) o[i] = saturate_cast<T>(std::pow(a[i], b[i]));
-}
-
-static inline void powPlaneFloat(const float* a, const float* b, float* o, int plane_size, size_t dpA, size_t dpB, size_t dpO)
-{
-    if (dpA == 1 && dpB == 1 && dpO == 1) {
-        for (int i = 0; i < plane_size; i++) o[i] = std::pow(a[i], b[i]);
-    } else {
-        for (int i = 0; i < plane_size; i++, a += dpA, b += dpB, o += dpO) *o = std::pow(*a, *b);
-    }
-}
 }
 
 class NaryEltwiseHelper CV_FINAL
@@ -316,8 +282,7 @@ public:
         if (backendId == DNN_BACKEND_CUDA) {
             return op == OPERATION::MAX  || op == OPERATION::MIN  || op == OPERATION::SUM ||
                    op == OPERATION::PROD || op == OPERATION::DIV  || op == OPERATION::ADD ||
-                   op == OPERATION::SUB  || op == OPERATION::MOD || op == OPERATION::FMOD ||
-                   op == OPERATION::POW;
+                   op == OPERATION::SUB  || op == OPERATION::MOD || op == OPERATION::FMOD;
         }
         return backendId == DNN_BACKEND_OPENCV;
     }
@@ -823,87 +788,22 @@ public:
             return;
         }
 
+        std::vector<Mat> used_inputs = inputs;
         if (op == OPERATION::POW) {
-            CV_Assert(inputs.size() == 2);
-            Mat A = inputs[0];
-            Mat B = inputs[1];
-
-            if (A.type() != outputs[0].type()) A.convertTo(A, outputs[0].type());
-            if (B.type() != outputs[0].type()) B.convertTo(B, outputs[0].type());
-
-            CV_Assert(outputs[0].isContinuous());
-            const int nd = outputs[0].dims;
-            std::vector<int> shape(outputs[0].size.p, outputs[0].size.p + nd);
-            std::vector<size_t> step_out(outputs[0].step.p, outputs[0].step.p + nd);
-            std::vector<size_t> step_a(nd, 0), step_b(nd, 0);
-
-            fillBroadcastSteps(A, shape, step_a);
-            fillBroadcastSteps(B, shape, step_b);
-
-            const size_t total = outputs[0].total();
-            size_t dpA = nd ? step_a.back() / outputs[0].elemSize() : 0;
-            size_t dpB = nd ? step_b.back() / outputs[0].elemSize() : 0;
-            size_t dpO = nd ? step_out.back() / outputs[0].elemSize() : 0;
-
-            if (outputs[0].type() == CV_32F) {
-                const float* a = (const float*)A.data;
-                const float* b = (const float*)B.data;
-                float* o = (float*)outputs[0].data;
-                const size_t a_total = A.total();
-                const size_t b_total = B.total();
-                if (a_total == total && b_total == total && dpA == 1 && dpB == 1 && dpO == 1) {
-                    powPlaneFloat(a, b, o, (int)total, 1, 1, 1);
-                } else {
-                    const char* pa0 = (const char*)A.data;
-                    const char* pb0 = (const char*)B.data;
-                    char* po0 = (char*)outputs[0].data;
-                    int plane_size = nd ? shape.back() : 1;
-                    size_t nplanes = 1;
-                    if (nd >= 2) nplanes = std::accumulate(shape.begin(), shape.end()-1, 1, std::multiplies<size_t>());
-                    parallel_for_(Range(0, (int)nplanes), [&](const Range& r){
-                        for (int p = r.start; p < r.end; ++p) {
-                            const char* pa = pa0;
-                            const char* pb = pb0;
-                            char* po = po0;
-                            size_t idx = (size_t)p;
-                            for (int k = nd - 2; k >= 0; k--) {
-                                size_t next_idx = idx / shape[k];
-                                size_t i_k = (int)(idx - next_idx * shape[k]);
-                                pa += i_k * step_a[k];
-                                pb += i_k * step_b[k];
-                                po += i_k * step_out[k];
-                                idx = next_idx;
-                            }
-                            const float* a_ = (const float*)pa;
-                            const float* b_ = (const float*)pb;
-                            float* o_ = (float*)po;
-                            powPlaneFloat(a_, b_, o_, plane_size, dpA, dpB, dpO);
-                        }
-                    });
-                }
-            } else {
-                Mat Ad, Bd; A.convertTo(Ad, CV_64F); B.convertTo(Bd, CV_64F);
-                const double* a = (const double*)Ad.data;
-                const double* b = (const double*)Bd.data;
-                switch (outputs[0].type()) {
-                case CV_32S: applyPowSaturate<int>(a, b, outputs[0]); break;
-                case CV_64S: applyPowSaturate<int64>(a, b, outputs[0]); break;
-                case CV_8U:  applyPowSaturate<uchar>(a, b, outputs[0]); break;
-                case CV_8S:  applyPowSaturate<schar>(a, b, outputs[0]); break;
-                case CV_16U: applyPowSaturate<ushort>(a, b, outputs[0]); break;
-                case CV_16S: applyPowSaturate<short>(a, b, outputs[0]); break;
-                case CV_32U: applyPowSaturate<uint32_t>(a, b, outputs[0]); break;
-                case CV_64U: applyPowSaturate<uint64_t>(a, b, outputs[0]); break;
-                case CV_64F: applyPowSaturate<double>(a, b, outputs[0]); break;
-                default:
-                    CV_Error(Error::StsNotImplemented, "Unsupported integer type for Pow operation");
-                }
+            CV_Assert(used_inputs.size() == 2);
+            const int out_type = outputs[0].type();
+            if (used_inputs[0].type() != out_type || used_inputs[1].type() != out_type) {
+                Mat a_conv, b_conv;
+                used_inputs[0].convertTo(a_conv, out_type);
+                used_inputs[1].convertTo(b_conv, out_type);
+                used_inputs = {a_conv, b_conv};
+                helper.init(used_inputs, outputs);
+                CV_CheckTrue(helper.prepare_for_broadcast_op(), "NaryEltwiseLayer: Preparation for broadcasting failed");
             }
-            return;
         }
 
-        int type_for_dispatch = op == OPERATION::WHERE ? outputs.front().type() : inputs.front().type();
-        typeDispatch(type_for_dispatch, inputs.size(), inputs, outputs);
+        int type_for_dispatch = (op == OPERATION::WHERE || op == OPERATION::POW) ? outputs.front().type() : used_inputs.front().type();
+        typeDispatch(type_for_dispatch, used_inputs.size(), used_inputs, outputs);
     }
 
     template<typename T, typename... Args>
@@ -937,7 +837,7 @@ public:
                     break;
                 }
                 case OPERATION::POW: {
-                    auto pow = [] (const T& a, const T& b) { return std::pow(a, b); };
+                    auto pow = [] (const T& a, const T& b) { return saturate_cast<T>(std::pow((double)a, (double)b)); };
                     binary_forward<T, T>(pow, std::forward<Args>(args)..., 1e5);
                     break;
                 }
@@ -1079,6 +979,11 @@ public:
                 CV_Assert(op != OPERATION::BITSHIFT && op != OPERATION::AND &&
                           op != OPERATION::OR && op != OPERATION::XOR);
                 opDispatch<float>(std::forward<Args>(args)...);
+                break;
+            case CV_64F:
+                CV_Assert(op != OPERATION::BITSHIFT && op != OPERATION::AND &&
+                          op != OPERATION::OR && op != OPERATION::XOR);
+                opDispatch<double>(std::forward<Args>(args)...);
                 break;
             case CV_16S:
                 opDispatch<int16_t>(std::forward<Args>(args)...);
