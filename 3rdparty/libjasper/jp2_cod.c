f12014893e9bb0a55ef8d266bc68e6dc612cae32
AminKA90 <aminuddinbaharum90@gmail.com>
2025-09-28 15:25:10 +0800

    Fourth PR.

--- 3rdparty/libjasper/jp2_cod.c
+++ 3rdparty/libjasper/jp2_cod.c
@@ -585,7 +585,7 @@ static int jp2_jp_putdata(jp2_box_t *box, jas_stream_t *out)
 {
     jp2_jp_t *jp = &box->data.jp;
     if (jp2_putuint32(out, jp->magic)) {
-        return -1;
+        return 0;
     }
     return 0;
 }
