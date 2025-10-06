9c33ddea31e35ea1c0070f9686f764462a4edf5f
Alexander Smorkalov <2536374+asmorkalov@users.noreply.github.com>
2025-10-06 15:20:06 +0300

    Merge pull request #27874 from asmorkalov:as/3dobject_pose
    3D object pose estimation tutorial update #27874
    
    1. Use cv::loadMesh instead of custom csv-based reader.
    2. Use code snippets
    
    ### Pull Request Readiness Checklist
    
    See details at https://github.com/opencv/opencv/wiki/How_to_contribute#making-a-good-pull-request
    
    - [x] I agree to contribute to the project under Apache 2 License.
    - [x] To the best of my knowledge, the proposed patch is not based on a code under GPL or another license that is incompatible with OpenCV
    - [ ] The PR is proposed to the proper branch
    - [ ] There is a reference to the original bug report and related work
    - [ ] There is accuracy test, performance test and test data in opencv_extra repository, if applicable
          Patch to opencv_extra has the same branch name.
    - [ ] The feature is well documented and sample code can be built with the project CMake

--- samples/cpp/tutorial_code/calib3d/real_time_pose_estimation/src/RobustMatcher.cpp
+++ samples/cpp/tutorial_code/calib3d/real_time_pose_estimation/src/RobustMatcher.cpp
@@ -84,6 +84,7 @@ void RobustMatcher::symmetryTest( const std::vector<std::vector<cv::DMatch> >& m
     }
 }
 
+//! [robust_match]
 void RobustMatcher::robustMatch( const cv::Mat& frame, std::vector<cv::DMatch>& good_matches,
                                  std::vector<cv::KeyPoint>& keypoints_frame, const cv::Mat& descriptors_model,
                                  const std::vector<cv::KeyPoint>& keypoints_model)
@@ -118,6 +119,7 @@ void RobustMatcher::robustMatch( const cv::Mat& frame, std::vector<cv::DMatch>& 
         cv::drawMatches(frame, keypoints_frame, training_img_, keypoints_model, good_matches, img_matching_);
     }
 }
+//! [robust_match]
 
 void RobustMatcher::fastRobustMatch( const cv::Mat& frame, std::vector<cv::DMatch>& good_matches,
                                      std::vector<cv::KeyPoint>& keypoints_frame,
