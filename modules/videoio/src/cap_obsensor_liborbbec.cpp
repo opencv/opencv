a1b7cc691952b87de31531936b32a4467fbcd1b1
baiye <sirudoi@163.com>
2025-10-27 16:44:36 +0800

    obsensor: add macOS option to switch between OrbbecSDK v1 and v2 for EOL cameras and Gemini330

--- modules/videoio/src/cap_obsensor_liborbbec.cpp
+++ modules/videoio/src/cap_obsensor_liborbbec.cpp
@@ -38,7 +38,9 @@ VideoCapture_obsensor::VideoCapture_obsensor(int, const cv::VideoCaptureParamete
     ob::Context::setLoggerToFile(OB_LOG_SEVERITY_OFF, "");
     config = std::make_shared<ob::Config>();
     pipe = std::make_shared<ob::Pipeline>();
+#if ORBBEC_SDK_VERSION_MAJOR != 1
     alignFilter = std::make_shared<ob::Align>(OB_STREAM_COLOR);
+#endif
 
     int color_width = params.get<double>(CAP_PROP_FRAME_WIDTH, OB_WIDTH_ANY);
     int color_height = params.get<double>(CAP_PROP_FRAME_HEIGHT, OB_HEIGHT_ANY);
@@ -75,16 +77,26 @@ VideoCapture_obsensor::VideoCapture_obsensor(int, const cv::VideoCaptureParamete
         auto depthProfile = depthProfiles->getVideoStreamProfile(depth_width, depth_height, OB_FORMAT_Y14, depth_fps);
         config->enableStream(depthProfile->as<ob::VideoStreamProfile>());
     }
-    config->setFrameAggregateOutputMode(OB_FRAME_AGGREGATE_OUTPUT_ALL_TYPE_FRAME_REQUIRE);
 
+#if ORBBEC_SDK_VERSION_MAJOR == 1
+    config->setAlignMode(ALIGN_D2C_SW_MODE);
+#else
+    config->setFrameAggregateOutputMode(OB_FRAME_AGGREGATE_OUTPUT_ALL_TYPE_FRAME_REQUIRE);
     pipe->enableFrameSync();
+#endif
+
     pipe->start(config, [&](std::shared_ptr<ob::FrameSet> frameset) {
         std::unique_lock<std::mutex> lk(videoFrameMutex);
+#if ORBBEC_SDK_VERSION_MAJOR == 1
+        colorFrame = frameset->colorFrame();
+        depthFrame = frameset->depthFrame();
+#else 
         auto alignFrameSet = alignFilter->process(frameset);
         if (alignFrameSet) {
             colorFrame = alignFrameSet->as<ob::FrameSet>()->colorFrame();
             depthFrame = alignFrameSet->as<ob::FrameSet>()->depthFrame();
         }
+#endif
     });
 
     auto param = pipe->getCameraParam();
