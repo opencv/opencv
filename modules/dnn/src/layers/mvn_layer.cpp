4a35c69a74c8e549dd7795038823550c25f6fc01
Abhishek Gola <abhishek.gola@bigvisionllc.com>
2025-11-03 15:58:10 +0530

    fixes for making CI green

--- modules/dnn/src/layers/mvn_layer.cpp
+++ modules/dnn/src/layers/mvn_layer.cpp
@@ -133,9 +133,14 @@ public:
         if (backendId == DNN_BACKEND_INFERENCE_ENGINE_NGRAPH)
             return true;
 #endif
+        if (backendId == DNN_BACKEND_CUDA)
         {
-            return backendId == DNN_BACKEND_OPENCV || backendId == DNN_BACKEND_CUDA;
+            EngineType engine_forced = (EngineType)utils::getConfigurationParameterSizeT("OPENCV_FORCE_DNN_ENGINE", ENGINE_AUTO);
+            if (engine_forced != ENGINE_CLASSIC)
+                return false;
+            return true;
         }
+        return backendId == DNN_BACKEND_OPENCV;
     }
 
 #ifdef HAVE_OPENCL
