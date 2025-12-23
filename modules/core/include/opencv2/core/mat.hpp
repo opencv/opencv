eaccbe24b2ccea4d17b489302b3aa231e5df514a
Vadim Pisarevsky <vadim.pisarevsky@gmail.com>
2025-12-23 14:25:07 +0300

    Merge pull request #28242 from vpisarev:fix_input_array_std_vector
    modified Input/OutputArray methods to handle 'std::vector<T>' or 'std::vector<std::vector<T>>' properly #28242
    
    This is port of #26408 with some further improvements (all switch-by-vector-type statements are consolidated in a single macro)
    
    ### Pull Request Readiness Checklist
    
    See details at https://github.com/opencv/opencv/wiki/How_to_contribute#making-a-good-pull-request
    
    - [x] I agree to contribute to the project under Apache 2 License.
    - [x] To the best of my knowledge, the proposed patch is not based on a code under GPL or another license that is incompatible with OpenCV
    - [ ] The PR is proposed to the proper branch
    - [ ] There is a reference to the original bug report and related work
    - [ ] There is accuracy test, performance test and test data in opencv_extra repository, if applicable
          Patch to opencv_extra has the same branch name.
    - [ ] The feature is well documented and sample code can be built with the project CMake

--- modules/core/include/opencv2/core/mat.hpp
+++ modules/core/include/opencv2/core/mat.hpp
@@ -246,6 +246,7 @@ public:
     bool isContinuous(int i=-1) const;
     bool isSubmatrix(int i=-1) const;
     bool empty() const;
+    bool empty(int i) const;
     void copyTo(const _OutputArray& arr) const;
     void copyTo(const _OutputArray& arr, const _InputArray & mask) const;
     size_t offset(int i=-1) const;
