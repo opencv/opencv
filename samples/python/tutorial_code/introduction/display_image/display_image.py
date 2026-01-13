5f3a820b6532277aec905aa11ddd8c0bb675f417
parth-bansal081 <25bcs081@nith.ac.in>
2026-01-13 23:53:49 +0530

    Improve image loading error handling
    Updated image loading to handle file not found errors and provide user instructions.

--- samples/python/tutorial_code/introduction/display_image/display_image.py
+++ samples/python/tutorial_code/introduction/display_image/display_image.py
@@ -1,19 +1,23 @@
-## [imports]
 import cv2 as cv
 import sys
-## [imports]
-## [imread]
-img = cv.imread(cv.samples.findFile("starry_night.jpg"))
-## [imread]
-## [empty]
+
+# Try to find the file using OpenCV's internal search path
+# Note: This often fails in pip-installed versions if samples are not included
+try:
+    img_path = cv.samples.findFile("starry_night.jpg")
+except cv.error:
+    # If not found, provide a clear instruction instead of a crash
+    print("Could not find 'starry_night.jpg'.")
+    print("Please download it from: https://github.com/opencv/opencv/blob/4.x/samples/data/starry_night.jpg")
+    sys.exit(1)
+
+img = cv.imread(img_path)
+
 if img is None:
     sys.exit("Could not read the image.")
-## [empty]
-## [imshow]
+
 cv.imshow("Display window", img)
 k = cv.waitKey(0)
-## [imshow]
-## [imsave]
+
 if k == ord("s"):
     cv.imwrite("starry_night.png", img)
-## [imsave]
