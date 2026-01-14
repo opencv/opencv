7a96cf89df12cede6f6458d008a30541fa09bddb
parth-bansal081 <25bcs081@nith.ac.in>
2026-01-14 11:32:46 +0530

    Docs: Refactor error handling using required=False as suggested by reviewer
    Updated image file search to prevent crashes and provide instructions if the image is not found.

--- samples/python/tutorial_code/introduction/display_image/display_image.py
+++ samples/python/tutorial_code/introduction/display_image/display_image.py
@@ -1,12 +1,11 @@
 import cv2 as cv
 import sys
 
-# Try to find the file using OpenCV's internal search path
-# Note: This often fails in pip-installed versions if samples are not included
-try:
-    img_path = cv.samples.findFile("starry_night.jpg")
-except cv.error:
-    # If not found, provide a clear instruction instead of a crash
+# Use required=False to prevent a C++ exception crash. 
+# It will return an empty string if the file is not found.
+img_path = cv.samples.findFile("starry_night.jpg", required=False)
+
+if not img_path:
     print("Could not find 'starry_night.jpg'.")
     print("Please download it from: https://github.com/opencv/opencv/blob/4.x/samples/data/starry_night.jpg")
     sys.exit(1)
