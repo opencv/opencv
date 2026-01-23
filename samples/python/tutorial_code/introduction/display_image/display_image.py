b543896b54c1755cb172a4e7932d341878a73892
atharvajadhao1 <atharvjadhav085@gmail.com>
2026-01-23 23:42:03 +0530

    docs:make Python display_image tutorial robust for pip installs

--- samples/python/tutorial_code/introduction/display_image/display_image.py
+++ samples/python/tutorial_code/introduction/display_image/display_image.py
@@ -2,17 +2,32 @@
 import cv2 as cv
 import sys
 ## [imports]
+
 ## [imread]
-img = cv.imread(cv.samples.findFile("starry_night.jpg"))
+img_path = cv.samples.findFile("starry_night.jpg", required=False)
+
+if not img_path:
+    print("Error: sample image 'starry_night.jpg' not found.")
+    print(
+        "Download it from:\n"
+        "https://raw.githubusercontent.com/opencv/opencv/master/samples/data/starry_night.jpg"
+    )
+    sys.exit("Sample image not found. See instructions above.")
+
+
+img = cv.imread(img_path)
 ## [imread]
+
 ## [empty]
 if img is None:
     sys.exit("Could not read the image.")
 ## [empty]
+
 ## [imshow]
 cv.imshow("Display window", img)
 k = cv.waitKey(0)
 ## [imshow]
+
 ## [imsave]
 if k == ord("s"):
     cv.imwrite("starry_night.png", img)
