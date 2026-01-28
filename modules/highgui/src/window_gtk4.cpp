ccf7932dc8d1a557bf3214d77c53fb63bc6749f9
AdityaMishra3000 <mr.ba8man@gmail.com>
2026-01-28 19:10:27 +0530

    highgui: add GTK4 backend skeleton

---
+++ modules/highgui/src/window_gtk4.cpp
@@ -0,0 +1,15 @@
+#include "precomp.hpp"
+
+#if defined(HAVE_GTK) && defined(HAVE_GTK4)
+
+#include <gtk/gtk.h>
+
+namespace cv {
+namespace impl {
+
+// GTK4 HighGUI backend — implementation to follow
+
+} // namespace impl
+} // namespace cv
+
+#endif // HAVE_GTK && HAVE_GTK4
