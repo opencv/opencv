8645a2db1b9cb76bfa3209532de78cd9e34eb32d
satyam yadav <satyamyadavruler@gmail.com>
2026-02-19 16:55:03 +0530

    Update apple_conversions.h

--- modules/imgcodecs/src/apple_conversions.h
+++ modules/imgcodecs/src/apple_conversions.h
@@ -8,10 +8,16 @@
 #include "opencv2/core.hpp"
 #import <Accelerate/Accelerate.h>
 
+// Guard AVFoundation import based on CMake configuration
 #ifdef HAVE_AVFOUNDATION
 #import <AVFoundation/AVFoundation.h>
 #endif
+
+// Guard ImageIO import: available on iOS and macOS 10.8+
+#include <TargetConditionals.h>
+#if TARGET_OS_IPHONE || (defined(MAC_OS_X_VERSION_MAX_ALLOWED) && MAC_OS_X_VERSION_MAX_ALLOWED >= 1080)
 #import <ImageIO/ImageIO.h>
+#endif
 
 CV_EXPORTS CGImageRef MatToCGImage(const cv::Mat& image) CF_RETURNS_RETAINED;
 CV_EXPORTS void CGImageToMat(const CGImageRef image, cv::Mat& m, bool alphaExist);
