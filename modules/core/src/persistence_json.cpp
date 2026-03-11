5bb6c5a2dba516b7354e52ce50e8e0857877a471
onikolskyy <onikolskyy@gmail.com>
2026-03-11 16:41:59 +0100

    typo

--- modules/core/src/persistence_json.cpp
+++ modules/core/src/persistence_json.cpp
@@ -23,7 +23,7 @@ public:
 
         struct_flags = (struct_flags & (FileNode::TYPE_MASK|FileNode::FLOW)) | FileNode::EMPTY;
         if( !FileNode::isCollection(struct_flags))
-            CV_Error( cv::Errors::StsBadArg,
+            CV_Error( cv::Error::StsBadArg,
                      "Some collection type - FileNode::SEQ or FileNode::MAP, must be specified" );
 
         if( type_name && *type_name == '\0' )
