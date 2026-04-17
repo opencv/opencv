864bd01879ca591fb6e239bf9dd43d8bdf6c6976
Souriya Trinh <souriya.trinh+github@gmail.com>
2026-04-17 14:59:20 +0200

    Add support for DICT_ARUCO_MIP_36h12 in more locations.

--- samples/python/aruco_detect_board_charuco.py
+++ samples/python/aruco_detect_board_charuco.py
@@ -36,7 +36,8 @@ def main():
     parser.add_argument("-d", help="dictionary: DICT_4X4_50=0, DICT_4X4_100=1, DICT_4X4_250=2,  DICT_4X4_1000=3,"
                                    "DICT_5X5_50=4, DICT_5X5_100=5, DICT_5X5_250=6, DICT_5X5_1000=7, DICT_6X6_50=8,"
                                    "DICT_6X6_100=9, DICT_6X6_250=10, DICT_6X6_1000=11, DICT_7X7_50=12, DICT_7X7_100=13,"
-                                   "DICT_7X7_250=14, DICT_7X7_1000=15, DICT_ARUCO_ORIGINAL = 16}",
+                                   "DICT_7X7_250=14, DICT_7X7_1000=15, DICT_ARUCO_ORIGINAL=16,"
+                                   "DICT_APRILTAG_16h5=17, DICT_APRILTAG_25h9=18, DICT_APRILTAG_36h10=19, DICT_APRILTAG_36h11=20, DICT_ARUCO_MIP_36h12=21}",
                         default="0", action="store", dest="d", type=int)
     parser.add_argument("-ci", help="Camera id if input doesnt come from video (-v)", default="0", action="store",
                         dest="ci", type=int)
