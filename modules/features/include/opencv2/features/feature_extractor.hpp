e3e8e8ca28afecea2920fe794c2288646ac4e281
Yang Guanyuhan <1523543870@qq.com>
2026-04-21 21:54:20 +0800

    fix: indentation in feature matcher and extractor headers

--- modules/features/include/opencv2/features/feature_extractor.hpp
+++ modules/features/include/opencv2/features/feature_extractor.hpp
@@ -13,49 +13,49 @@ namespace cv
 namespace features
 {
 
-    class CV_EXPORTS_W FeatureExtractor : public Algorithm
+class CV_EXPORTS_W FeatureExtractor : public Algorithm
+{
+public:
+    virtual ~FeatureExtractor();
+
+    CV_WRAP virtual void extract(InputArray image,
+                                 CV_OUT std::vector<KeyPoint> &keypoints,
+                                 OutputArray descriptors,
+                                 InputArray mask = noArray()) const = 0;
+};
+
+class CV_EXPORTS_W TraditionalFeatureExtractor : public FeatureExtractor
+{
+public:
+    CV_WRAP static Ptr<TraditionalFeatureExtractor> create(const Ptr<Feature2D> &feature2D);
+
+    CV_WRAP virtual void setFeature2D(const Ptr<Feature2D> &feature2D) = 0;
+    CV_WRAP virtual Ptr<Feature2D> getFeature2D() const = 0;
+};
+
+class CV_EXPORTS_W SuperPoint : public FeatureExtractor
+{
+public:
+    struct Params
     {
-    public:
-        virtual ~FeatureExtractor();
+        Params();
 
-        CV_WRAP virtual void extract(InputArray image,
-                                     CV_OUT std::vector<KeyPoint> &keypoints,
-                                     OutputArray descriptors,
-                                     InputArray mask = noArray()) const = 0;
-    };
-
-    class CV_EXPORTS_W TraditionalFeatureExtractor : public FeatureExtractor
-    {
-    public:
-        CV_WRAP static Ptr<TraditionalFeatureExtractor> create(const Ptr<Feature2D> &feature2D);
-
-        CV_WRAP virtual void setFeature2D(const Ptr<Feature2D> &feature2D) = 0;
-        CV_WRAP virtual Ptr<Feature2D> getFeature2D() const = 0;
-    };
-
-    class CV_EXPORTS_W SuperPoint : public FeatureExtractor
-    {
-    public:
-        struct Params
-        {
-            Params();
-
-            String modelPath;
+        String modelPath;
 #ifdef HAVE_OPENCV_DNN
-            int dnnEngine;
-            int dnnBackend;
-            int dnnTarget;
+        int dnnEngine;
+        int dnnBackend;
+        int dnnTarget;
 #endif
-            Size inputSize;
-            bool preferGrayInput;
-        };
-
-        CV_WRAP static Ptr<SuperPoint> create();
-        static Ptr<SuperPoint> create(const SuperPoint::Params &params);
-
-        CV_WRAP virtual void setModel(const String &modelPath) = 0;
-        CV_WRAP virtual String getModel() const = 0;
+        Size inputSize;
+        bool preferGrayInput;
     };
+
+    CV_WRAP static Ptr<SuperPoint> create();
+    static Ptr<SuperPoint> create(const SuperPoint::Params &params);
+
+    CV_WRAP virtual void setModel(const String &modelPath) = 0;
+    CV_WRAP virtual String getModel() const = 0;
+};
 
 } // namespace features
 } // namespace cv
--- modules/features/include/opencv2/features/feature_matcher.hpp
+++ modules/features/include/opencv2/features/feature_matcher.hpp
@@ -13,58 +13,58 @@ namespace cv
 namespace features
 {
 
-    class CV_EXPORTS_W FeatureMatcher : public Algorithm
+class CV_EXPORTS_W FeatureMatcher : public Algorithm
+{
+public:
+    virtual ~FeatureMatcher();
+
+    CV_WRAP virtual void match(InputArray queryDesc,
+                               InputArray trainDesc,
+                               CV_OUT std::vector<DMatch> &matches,
+                               InputArray mask = noArray()) const = 0;
+
+    CV_WRAP virtual void match(InputArray queryKpts,
+                               InputArray queryDesc,
+                               InputArray trainKpts,
+                               InputArray trainDesc,
+                               CV_OUT std::vector<DMatch> &matches,
+                               InputArray mask = noArray(),
+                               Size queryImageSize = Size(),
+                               Size trainImageSize = Size()) const;
+};
+
+class CV_EXPORTS_W TraditionalFeatureMatcher : public FeatureMatcher
+{
+public:
+    CV_WRAP static Ptr<TraditionalFeatureMatcher> create(const Ptr<DescriptorMatcher> &descriptorMatcher);
+
+    CV_WRAP virtual void setDescriptorMatcher(const Ptr<DescriptorMatcher> &descriptorMatcher) = 0;
+    CV_WRAP virtual Ptr<DescriptorMatcher> getDescriptorMatcher() const = 0;
+};
+
+class CV_EXPORTS_W LightGlue : public FeatureMatcher
+{
+public:
+    struct Params
     {
-    public:
-        virtual ~FeatureMatcher();
+        Params();
 
-        CV_WRAP virtual void match(InputArray queryDesc,
-                                   InputArray trainDesc,
-                                   CV_OUT std::vector<DMatch> &matches,
-                                   InputArray mask = noArray()) const = 0;
-
-        CV_WRAP virtual void match(InputArray queryKpts,
-                                   InputArray queryDesc,
-                                   InputArray trainKpts,
-                                   InputArray trainDesc,
-                                   CV_OUT std::vector<DMatch> &matches,
-                                   InputArray mask = noArray(),
-                                   Size queryImageSize = Size(),
-                                   Size trainImageSize = Size()) const;
-    };
-
-    class CV_EXPORTS_W TraditionalFeatureMatcher : public FeatureMatcher
-    {
-    public:
-        CV_WRAP static Ptr<TraditionalFeatureMatcher> create(const Ptr<DescriptorMatcher> &descriptorMatcher);
-
-        CV_WRAP virtual void setDescriptorMatcher(const Ptr<DescriptorMatcher> &descriptorMatcher) = 0;
-        CV_WRAP virtual Ptr<DescriptorMatcher> getDescriptorMatcher() const = 0;
-    };
-
-    class CV_EXPORTS_W LightGlue : public FeatureMatcher
-    {
-    public:
-        struct Params
-        {
-            Params();
-
-            String modelPath;
+        String modelPath;
 #ifdef HAVE_OPENCV_DNN
-            int dnnEngine;
-            int dnnBackend;
-            int dnnTarget;
+        int dnnEngine;
+        int dnnBackend;
+        int dnnTarget;
 #endif
-            bool disableWinograd;
-            float scoreThreshold;
-        };
-
-        CV_WRAP static Ptr<LightGlue> create();
-        static Ptr<LightGlue> create(const LightGlue::Params &params);
-
-        CV_WRAP virtual void setModel(const String &modelPath) = 0;
-        CV_WRAP virtual String getModel() const = 0;
+        bool disableWinograd;
+        float scoreThreshold;
     };
+
+    CV_WRAP static Ptr<LightGlue> create();
+    static Ptr<LightGlue> create(const LightGlue::Params &params);
+
+    CV_WRAP virtual void setModel(const String &modelPath) = 0;
+    CV_WRAP virtual String getModel() const = 0;
+};
 
 } // namespace features
 } // namespace cv
