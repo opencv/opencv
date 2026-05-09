075a810a7e2cb04f09f72231533697ff1225ea21
srujan-rai <raisrujan@gmail.com>
2026-05-09 11:15:40 +0530

    core(opengl): fix memory leak in OpenCL extensions gathering
    `extensions` buffer allocated with `new char[]` was leaked when
    `clGetDeviceInfo` returned a non-success status, because `continue`
    jumped past the manual `delete[]`. Replace raw pointer with
    `std::unique_ptr<char[]>` so the buffer is freed automatically on
    all exit paths including the error and exception cases.

--- modules/core/src/opengl.cpp
+++ modules/core/src/opengl.cpp
@@ -1682,27 +1682,13 @@ Context& initializeContextFromGL()
 
                 if(extensionSize > 0)
                 {
-                    char* extensions = nullptr;
+                    cv::AutoBuffer<char> extensions(extensionSize);
 
-                    try {
-                        extensions = new char[extensionSize];
+                    status = clGetDeviceInfo(devices[j], CL_DEVICE_EXTENSIONS, extensionSize, extensions.data(), &extensionSize);
+                    if (status != CL_SUCCESS)
+                        continue;
 
-                        status = clGetDeviceInfo(devices[j], CL_DEVICE_EXTENSIONS, extensionSize, extensions, &extensionSize);
-                        if (status != CL_SUCCESS)
-                            continue;
-                    } catch(...) {
-                        CV_Error(cv::Error::OpenCLInitError, "OpenCL: Exception thrown during device extensions gathering");
-                    }
-
-                    std::string devString;
-
-                    if(extensions != nullptr) {
-                        devString = extensions;
-                        delete[] extensions;
-                    }
-                    else {
-                        CV_Error(cv::Error::OpenCLInitError, "OpenCL: Unexpected error during device extensions gathering");
-                    }
+                    std::string devString(extensions.data());
 
                     size_t oldPos = 0;
                     size_t spacePos = devString.find(' ', oldPos); // extensions string is space delimited
