0be0a0767adfdcf92a8339c734cdc1754fc76736
Alexander Smorkalov <alexander.smorkalov@opencv.ai>
2026-06-10 16:39:07 +0300

    Use local std::string directly without extra copy as C++11 allows it.

--- modules/core/src/opengl.cpp
+++ modules/core/src/opengl.cpp
@@ -1682,13 +1682,11 @@ Context& initializeContextFromGL()
 
                 if(extensionSize > 0)
                 {
-                    cv::AutoBuffer<char> extensions(extensionSize);
-
-                    status = clGetDeviceInfo(devices[j], CL_DEVICE_EXTENSIONS, extensionSize, extensions.data(), &extensionSize);
+                    std::string devString(extensionSize+1, 0);
+                    char* extensionsBuffer = const_cast<char*>(devString.c_str());
+                    status = clGetDeviceInfo(devices[j], CL_DEVICE_EXTENSIONS, extensionSize, extensionsBuffer, &extensionSize);
                     if (status != CL_SUCCESS)
                         continue;
-
-                    std::string devString(extensions.data());
 
                     size_t oldPos = 0;
                     size_t spacePos = devString.find(' ', oldPos); // extensions string is space delimited
