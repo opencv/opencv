72d730bf06c4159af7e5c8a85d162030d8790965
vrooomy <vj.bro.833@gmail.com>
2026-06-16 16:59:23 +0530

    add copyright header

--- modules/core/src/hal_backend.cpp
+++ modules/core/src/hal_backend.cpp
@@ -1,3 +1,8 @@
+// This file is part of OpenCV project.
+// It is subject to the license terms in the LICENSE file found in the top-level directory
+// of this distribution and at http://opencv.org/license.html.
+// Copyright (C) 2026, BigVision LLC, all rights reserved.
+// Third party copyrights are property of their respective owners.
 #include "precomp.hpp"
 #include "opencv2/core/hal/backend_registry.hpp"
 #include <vector>
