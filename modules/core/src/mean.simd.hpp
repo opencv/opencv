a93dcece8bea5199a69800d11dddb576d0da212a
Madan mohan Manokar <madanmohan.manokar@amd.com>
2026-06-18 11:19:44 +0000

    core: vectorize meanStdDev for 16U/16S/32S/32F and 3-channel inputs
    Add SIMD SumSqr_SIMD reductions (previously scalar) plus AVX-512 dispatch for
    the mean TU. Uses universal intrinsics, so all SIMD backends benefit.

--- modules/core/src/mean.simd.hpp
+++ modules/core/src/mean.simd.hpp
@@ -2,6 +2,7 @@
 // It is subject to the license terms in the LICENSE file found in the top-level directory
 // of this distribution and at http://opencv.org/license.html
 
+// Copyright (C) 2026, Advanced Micro Devices, Inc., all rights reserved.
 
 #include "precomp.hpp"
 #include "stat.hpp"
@@ -31,7 +32,35 @@ struct SumSqr_SIMD<uchar, int, int>
 {
     int operator () (const uchar * src0, const uchar * mask, int * sum, int * sqsum, int len, int cn) const
     {
-        if (mask || (cn != 1 && cn != 2 && cn != 4))
+        if (mask)
+            return 0;
+        // cn==3: deinterleave so each lane belongs to one channel, then reduce per
+        // channel. sum/sqsum stay exact in s32 within the dispatcher's 1<<15 block.
+        if (cn == 3)
+        {
+            const int vl8 = VTraits<v_uint8>::vlanes();
+            v_int32 vs0 = vx_setzero_s32(), vs1 = vx_setzero_s32(), vs2 = vx_setzero_s32();
+            v_int32 vq0 = vx_setzero_s32(), vq1 = vx_setzero_s32(), vq2 = vx_setzero_s32();
+            auto acc = [](const v_uint8& ch, v_int32& vs, v_int32& vq)
+            {
+                v_uint16 lo, hi; v_expand(ch, lo, hi);
+                v_uint32 s0, s1, s2, s3; v_expand(lo, s0, s1); v_expand(hi, s2, s3);
+                vs = v_add(vs, v_reinterpret_as_s32(v_add(v_add(s0, s1), v_add(s2, s3))));
+                v_int16 d0 = v_reinterpret_as_s16(lo), d1 = v_reinterpret_as_s16(hi);
+                vq = v_add(vq, v_add(v_dotprod(d0, d0), v_dotprod(d1, d1)));
+            };
+            int e = 0;
+            for (; e <= len - vl8; e += vl8)
+            {
+                v_uint8 a, b, c; v_load_deinterleave(src0 + e * 3, a, b, c);
+                acc(a, vs0, vq0); acc(b, vs1, vq1); acc(c, vs2, vq2);
+            }
+            sum[0] += v_reduce_sum(vs0); sum[1] += v_reduce_sum(vs1); sum[2] += v_reduce_sum(vs2);
+            sqsum[0] += v_reduce_sum(vq0); sqsum[1] += v_reduce_sum(vq1); sqsum[2] += v_reduce_sum(vq2);
+            v_cleanup();
+            return e;
+        }
+        if (cn != 1 && cn != 2 && cn != 4)
             return 0;
         len *= cn;
 
@@ -98,7 +127,32 @@ struct SumSqr_SIMD<schar, int, int>
 {
     int operator () (const schar * src0, const uchar * mask, int * sum, int * sqsum, int len, int cn) const
     {
-        if (mask || (cn != 1 && cn != 2 && cn != 4))
+        if (mask)
+            return 0;
+        if (cn == 3)
+        {
+            const int vl8 = VTraits<v_int8>::vlanes();
+            v_int32 vs0 = vx_setzero_s32(), vs1 = vx_setzero_s32(), vs2 = vx_setzero_s32();
+            v_int32 vq0 = vx_setzero_s32(), vq1 = vx_setzero_s32(), vq2 = vx_setzero_s32();
+            auto acc = [](const v_int8& ch, v_int32& vs, v_int32& vq)
+            {
+                v_int16 lo, hi; v_expand(ch, lo, hi);
+                v_int32 s0, s1, s2, s3; v_expand(lo, s0, s1); v_expand(hi, s2, s3);
+                vs = v_add(vs, v_add(v_add(s0, s1), v_add(s2, s3)));
+                vq = v_add(vq, v_add(v_dotprod(lo, lo), v_dotprod(hi, hi)));
+            };
+            int e = 0;
+            for (; e <= len - vl8; e += vl8)
+            {
+                v_int8 a, b, c; v_load_deinterleave(src0 + e * 3, a, b, c);
+                acc(a, vs0, vq0); acc(b, vs1, vq1); acc(c, vs2, vq2);
+            }
+            sum[0] += v_reduce_sum(vs0); sum[1] += v_reduce_sum(vs1); sum[2] += v_reduce_sum(vs2);
+            sqsum[0] += v_reduce_sum(vq0); sqsum[1] += v_reduce_sum(vq1); sqsum[2] += v_reduce_sum(vq2);
+            v_cleanup();
+            return e;
+        }
+        if (cn != 1 && cn != 2 && cn != 4)
             return 0;
         len *= cn;
 
@@ -159,6 +213,306 @@ struct SumSqr_SIMD<schar, int, int>
         return x / cn;
     }
 };
+
+#if (CV_SIMD_64F || CV_SIMD_SCALABLE_64F)
+template <>
+struct SumSqr_SIMD<ushort, int, double>
+{
+    int operator () (const ushort* src0, const uchar* mask, int* sum, double* sqsum, int len, int cn) const
+    {
+        if (mask)
+            return 0;
+        if (cn == 3)
+        {
+            const int vl = VTraits<v_uint16>::vlanes();
+            v_uint32 vs0 = vx_setzero_u32(), vs1 = vx_setzero_u32(), vs2 = vx_setzero_u32();
+            v_float64 vq0 = vx_setzero_f64(), vq1 = vx_setzero_f64(), vq2 = vx_setzero_f64();
+            auto acc = [](const v_uint16& ch, v_uint32& vs, v_float64& vq)
+            {
+                v_uint32 lo, hi; v_expand(ch, lo, hi);
+                vs = v_add(vs, v_add(lo, hi));
+                v_float64 f0 = v_cvt_f64(v_reinterpret_as_s32(lo)), f1 = v_cvt_f64_high(v_reinterpret_as_s32(lo));
+                v_float64 f2 = v_cvt_f64(v_reinterpret_as_s32(hi)), f3 = v_cvt_f64_high(v_reinterpret_as_s32(hi));
+                vq = v_fma(f0, f0, v_fma(f1, f1, v_fma(f2, f2, v_fma(f3, f3, vq))));
+            };
+            int e = 0;
+            for (; e <= len - vl; e += vl)
+            {
+                v_uint16 a, b, c; v_load_deinterleave(src0 + e * 3, a, b, c);
+                acc(a, vs0, vq0); acc(b, vs1, vq1); acc(c, vs2, vq2);
+            }
+            sum[0] += (int)v_reduce_sum(vs0); sum[1] += (int)v_reduce_sum(vs1); sum[2] += (int)v_reduce_sum(vs2);
+            sqsum[0] += v_reduce_sum(vq0); sqsum[1] += v_reduce_sum(vq1); sqsum[2] += v_reduce_sum(vq2);
+            v_cleanup();
+            return e;
+        }
+        if (cn != 1 && cn != 2 && cn != 4)
+            return 0;
+        len *= cn;
+
+        const int vl16 = VTraits<v_uint16>::vlanes();
+        const int vl32 = VTraits<v_uint32>::vlanes();
+        const int vl64 = VTraits<v_float64>::vlanes();
+
+        v_uint32 v_sum = vx_setzero_u32();
+        v_float64 v_sq0 = vx_setzero_f64(), v_sq1 = vx_setzero_f64();
+        v_float64 v_sq2 = vx_setzero_f64(), v_sq3 = vx_setzero_f64();
+
+        int x = 0;
+        for (; x <= len - vl16; x += vl16)
+        {
+            v_uint16 v_src = vx_load(src0 + x);
+            v_uint32 lo, hi;
+            v_expand(v_src, lo, hi);
+            v_sum = v_add(v_sum, v_add(lo, hi));
+
+            v_float64 f0 = v_cvt_f64(v_reinterpret_as_s32(lo));
+            v_float64 f1 = v_cvt_f64_high(v_reinterpret_as_s32(lo));
+            v_float64 f2 = v_cvt_f64(v_reinterpret_as_s32(hi));
+            v_float64 f3 = v_cvt_f64_high(v_reinterpret_as_s32(hi));
+            v_sq0 = v_fma(f0, f0, v_sq0);
+            v_sq1 = v_fma(f1, f1, v_sq1);
+            v_sq2 = v_fma(f2, f2, v_sq2);
+            v_sq3 = v_fma(f3, f3, v_sq3);
+        }
+
+        unsigned CV_DECL_ALIGNED(CV_SIMD_WIDTH) sbuf[VTraits<v_uint32>::max_nlanes];
+        double   CV_DECL_ALIGNED(CV_SIMD_WIDTH) qbuf[VTraits<v_float64>::max_nlanes * 4];
+        v_store_aligned(sbuf, v_sum);
+        v_store_aligned(qbuf + vl64 * 0, v_sq0);
+        v_store_aligned(qbuf + vl64 * 1, v_sq1);
+        v_store_aligned(qbuf + vl64 * 2, v_sq2);
+        v_store_aligned(qbuf + vl64 * 3, v_sq3);
+
+        // sbuf lane j (j<vl32) holds samples j and j+vl32 -> channel j%cn (vl32%cn==0).
+        // qbuf index i (i<vl16) holds sample lane i           -> channel i%cn (vl16%cn==0).
+        for (int i = 0; i < vl32; ++i)
+            sum[i % cn] += (int)sbuf[i];
+        for (int i = 0; i < vl16; ++i)
+            sqsum[i % cn] += qbuf[i];
+
+        v_cleanup();
+        return x / cn;
+    }
+};
+
+template <>
+struct SumSqr_SIMD<short, int, double>
+{
+    int operator () (const short* src0, const uchar* mask, int* sum, double* sqsum, int len, int cn) const
+    {
+        if (mask)
+            return 0;
+        if (cn == 3)
+        {
+            const int vl = VTraits<v_int16>::vlanes();
+            v_int32 vs0 = vx_setzero_s32(), vs1 = vx_setzero_s32(), vs2 = vx_setzero_s32();
+            v_float64 vq0 = vx_setzero_f64(), vq1 = vx_setzero_f64(), vq2 = vx_setzero_f64();
+            auto acc = [](const v_int16& ch, v_int32& vs, v_float64& vq)
+            {
+                v_int32 lo, hi; v_expand(ch, lo, hi);
+                vs = v_add(vs, v_add(lo, hi));
+                v_float64 f0 = v_cvt_f64(lo), f1 = v_cvt_f64_high(lo);
+                v_float64 f2 = v_cvt_f64(hi), f3 = v_cvt_f64_high(hi);
+                vq = v_fma(f0, f0, v_fma(f1, f1, v_fma(f2, f2, v_fma(f3, f3, vq))));
+            };
+            int e = 0;
+            for (; e <= len - vl; e += vl)
+            {
+                v_int16 a, b, c; v_load_deinterleave(src0 + e * 3, a, b, c);
+                acc(a, vs0, vq0); acc(b, vs1, vq1); acc(c, vs2, vq2);
+            }
+            sum[0] += v_reduce_sum(vs0); sum[1] += v_reduce_sum(vs1); sum[2] += v_reduce_sum(vs2);
+            sqsum[0] += v_reduce_sum(vq0); sqsum[1] += v_reduce_sum(vq1); sqsum[2] += v_reduce_sum(vq2);
+            v_cleanup();
+            return e;
+        }
+        if (cn != 1 && cn != 2 && cn != 4)
+            return 0;
+        len *= cn;
+
+        const int vl16 = VTraits<v_int16>::vlanes();
+        const int vl32 = VTraits<v_int32>::vlanes();
+        const int vl64 = VTraits<v_float64>::vlanes();
+
+        v_int32 v_sum = vx_setzero_s32();
+        v_float64 v_sq0 = vx_setzero_f64(), v_sq1 = vx_setzero_f64();
+        v_float64 v_sq2 = vx_setzero_f64(), v_sq3 = vx_setzero_f64();
+
+        int x = 0;
+        for (; x <= len - vl16; x += vl16)
+        {
+            v_int16 v_src = vx_load(src0 + x);
+            v_int32 lo, hi;
+            v_expand(v_src, lo, hi);
+            v_sum = v_add(v_sum, v_add(lo, hi));
+
+            v_float64 f0 = v_cvt_f64(lo);
+            v_float64 f1 = v_cvt_f64_high(lo);
+            v_float64 f2 = v_cvt_f64(hi);
+            v_float64 f3 = v_cvt_f64_high(hi);
+            v_sq0 = v_fma(f0, f0, v_sq0);
+            v_sq1 = v_fma(f1, f1, v_sq1);
+            v_sq2 = v_fma(f2, f2, v_sq2);
+            v_sq3 = v_fma(f3, f3, v_sq3);
+        }
+
+        int    CV_DECL_ALIGNED(CV_SIMD_WIDTH) sbuf[VTraits<v_int32>::max_nlanes];
+        double CV_DECL_ALIGNED(CV_SIMD_WIDTH) qbuf[VTraits<v_float64>::max_nlanes * 4];
+        v_store_aligned(sbuf, v_sum);
+        v_store_aligned(qbuf + vl64 * 0, v_sq0);
+        v_store_aligned(qbuf + vl64 * 1, v_sq1);
+        v_store_aligned(qbuf + vl64 * 2, v_sq2);
+        v_store_aligned(qbuf + vl64 * 3, v_sq3);
+
+        for (int i = 0; i < vl32; ++i)
+            sum[i % cn] += sbuf[i];
+        for (int i = 0; i < vl16; ++i)
+            sqsum[i % cn] += qbuf[i];
+
+        v_cleanup();
+        return x / cn;
+    }
+};
+
+// 32-bit int / float: both sum and sqsum accumulate in f64 to match the scalar
+// reference's double accumulation. Values are widened to f64 before squaring so
+// the square is computed in double (same as scalar (double)v*v).
+template <>
+struct SumSqr_SIMD<int, double, double>
+{
+    int operator () (const int* src0, const uchar* mask, double* sum, double* sqsum, int len, int cn) const
+    {
+        if (mask)
+            return 0;
+        if (cn == 3)
+        {
+            const int vl = VTraits<v_int32>::vlanes();
+            v_float64 vs0 = vx_setzero_f64(), vs1 = vx_setzero_f64(), vs2 = vx_setzero_f64();
+            v_float64 vq0 = vx_setzero_f64(), vq1 = vx_setzero_f64(), vq2 = vx_setzero_f64();
+            auto acc = [](const v_int32& ch, v_float64& vs, v_float64& vq)
+            {
+                v_float64 f0 = v_cvt_f64(ch), f1 = v_cvt_f64_high(ch);
+                vs = v_add(vs, v_add(f0, f1));
+                vq = v_fma(f0, f0, v_fma(f1, f1, vq));
+            };
+            int e = 0;
+            for (; e <= len - vl; e += vl)
+            {
+                v_int32 a, b, c; v_load_deinterleave(src0 + e * 3, a, b, c);
+                acc(a, vs0, vq0); acc(b, vs1, vq1); acc(c, vs2, vq2);
+            }
+            sum[0] += v_reduce_sum(vs0); sum[1] += v_reduce_sum(vs1); sum[2] += v_reduce_sum(vs2);
+            sqsum[0] += v_reduce_sum(vq0); sqsum[1] += v_reduce_sum(vq1); sqsum[2] += v_reduce_sum(vq2);
+            v_cleanup();
+            return e;
+        }
+        if (cn != 1 && cn != 2 && cn != 4)
+            return 0;
+        len *= cn;
+
+        const int vl32 = VTraits<v_int32>::vlanes();
+        const int vl64 = VTraits<v_float64>::vlanes();
+
+        v_float64 vs0 = vx_setzero_f64(), vs1 = vx_setzero_f64();
+        v_float64 vq0 = vx_setzero_f64(), vq1 = vx_setzero_f64();
+
+        int x = 0;
+        for (; x <= len - vl32; x += vl32)
+        {
+            v_int32 v_src = vx_load(src0 + x);
+            v_float64 f0 = v_cvt_f64(v_src);
+            v_float64 f1 = v_cvt_f64_high(v_src);
+            vs0 = v_add(vs0, f0);
+            vs1 = v_add(vs1, f1);
+            vq0 = v_fma(f0, f0, vq0);
+            vq1 = v_fma(f1, f1, vq1);
+        }
+
+        double CV_DECL_ALIGNED(CV_SIMD_WIDTH) sbuf[VTraits<v_float64>::max_nlanes * 2];
+        double CV_DECL_ALIGNED(CV_SIMD_WIDTH) qbuf[VTraits<v_float64>::max_nlanes * 2];
+        v_store_aligned(sbuf, vs0);          v_store_aligned(sbuf + vl64, vs1);
+        v_store_aligned(qbuf, vq0);          v_store_aligned(qbuf + vl64, vq1);
+
+        for (int i = 0; i < vl32; ++i)
+        {
+            sum[i % cn]   += sbuf[i];
+            sqsum[i % cn] += qbuf[i];
+        }
+
+        v_cleanup();
+        return x / cn;
+    }
+};
+
+template <>
+struct SumSqr_SIMD<float, double, double>
+{
+    int operator () (const float* src0, const uchar* mask, double* sum, double* sqsum, int len, int cn) const
+    {
+        if (mask)
+            return 0;
+        if (cn == 3)
+        {
+            const int vl = VTraits<v_float32>::vlanes();
+            v_float64 vs0 = vx_setzero_f64(), vs1 = vx_setzero_f64(), vs2 = vx_setzero_f64();
+            v_float64 vq0 = vx_setzero_f64(), vq1 = vx_setzero_f64(), vq2 = vx_setzero_f64();
+            auto acc = [](const v_float32& ch, v_float64& vs, v_float64& vq)
+            {
+                v_float64 f0 = v_cvt_f64(ch), f1 = v_cvt_f64_high(ch);
+                vs = v_add(vs, v_add(f0, f1));
+                vq = v_fma(f0, f0, v_fma(f1, f1, vq));
+            };
+            int e = 0;
+            for (; e <= len - vl; e += vl)
+            {
+                v_float32 a, b, c; v_load_deinterleave(src0 + e * 3, a, b, c);
+                acc(a, vs0, vq0); acc(b, vs1, vq1); acc(c, vs2, vq2);
+            }
+            sum[0] += v_reduce_sum(vs0); sum[1] += v_reduce_sum(vs1); sum[2] += v_reduce_sum(vs2);
+            sqsum[0] += v_reduce_sum(vq0); sqsum[1] += v_reduce_sum(vq1); sqsum[2] += v_reduce_sum(vq2);
+            v_cleanup();
+            return e;
+        }
+        if (cn != 1 && cn != 2 && cn != 4)
+            return 0;
+        len *= cn;
+
+        const int vl32 = VTraits<v_float32>::vlanes();
+        const int vl64 = VTraits<v_float64>::vlanes();
+
+        v_float64 vs0 = vx_setzero_f64(), vs1 = vx_setzero_f64();
+        v_float64 vq0 = vx_setzero_f64(), vq1 = vx_setzero_f64();
+
+        int x = 0;
+        for (; x <= len - vl32; x += vl32)
+        {
+            v_float32 v_src = vx_load(src0 + x);
+            v_float64 f0 = v_cvt_f64(v_src);
+            v_float64 f1 = v_cvt_f64_high(v_src);
+            vs0 = v_add(vs0, f0);
+            vs1 = v_add(vs1, f1);
+            vq0 = v_fma(f0, f0, vq0);
+            vq1 = v_fma(f1, f1, vq1);
+        }
+
+        double CV_DECL_ALIGNED(CV_SIMD_WIDTH) sbuf[VTraits<v_float64>::max_nlanes * 2];
+        double CV_DECL_ALIGNED(CV_SIMD_WIDTH) qbuf[VTraits<v_float64>::max_nlanes * 2];
+        v_store_aligned(sbuf, vs0);          v_store_aligned(sbuf + vl64, vs1);
+        v_store_aligned(qbuf, vq0);          v_store_aligned(qbuf + vl64, vq1);
+
+        for (int i = 0; i < vl32; ++i)
+        {
+            sum[i % cn]   += sbuf[i];
+            sqsum[i % cn] += qbuf[i];
+        }
+
+        v_cleanup();
+        return x / cn;
+    }
+};
+
+#endif // CV_SIMD_64F
 
 #endif
 
