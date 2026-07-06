203e6ae4219b8c3189a2ed34233a274b45fcf43c
Adel-Ayoub <adelayoub.maaziz@gmail.com>
2026-07-06 07:52:20 +0100

    core: snapshot all non-Mat/UMat operands in Mat::mul()

--- modules/core/src/matrix_expressions.cpp
+++ modules/core/src/matrix_expressions.cpp
@@ -1739,10 +1739,11 @@ MatExpr Mat::mul(InputArray m, double scale) const
     CV_INSTRUMENT_REGION();
 
     Mat b = m.getMat();
-    // A MATX-kind argument (a scalar bound to _InputArray(const double&), a Matx or a Vec)
-    // is a non-owning view of caller stack memory that the returned MatExpr can outlive,
-    // so snapshot it. See https://github.com/opencv/opencv/issues/23577
-    if( m.isMatx() )
+    // Unless the argument is a refcounted Mat/UMat, the header returned by getMat() may be
+    // a non-owning view of caller memory (e.g. a scalar bound to _InputArray(const double&),
+    // a Matx or a Vec) that the returned MatExpr can outlive, so snapshot it.
+    // See https://github.com/opencv/opencv/issues/23577
+    if( !m.isMat() && !m.isUMat() )
         b = b.clone();
 
     MatExpr e;
