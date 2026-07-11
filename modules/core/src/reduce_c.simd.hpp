3d513bfb9cabcc4db80f4fc83d62548f095b0499
Yuantao Feng <yuantao.feng@outlook.com>
2026-07-11 11:57:51 +0800

    core: add reduceC C4 native kernels

--- modules/core/src/reduce_c.simd.hpp
+++ modules/core/src/reduce_c.simd.hpp
@@ -691,6 +691,68 @@ static void reduceColMinMax_8uC4(const Mat& srcmat, Mat& dstmat)
         }
     });
     v_cleanup();
+#elif CV_AVX2
+    const int cols = srcmat.cols;
+    const uchar initial = isMax ? 0 : UCHAR_MAX;
+    const __m256i accInit = _mm256_set1_epi8((char)initial);
+    const __m256i maskInvalid = _mm256_broadcastsi128_si256(
+            _mm_setr_epi8(0, 0, 0, 0, -1, -1, -1, -1,
+                          -1, -1, -1, -1, -1, -1, -1, -1));
+    const __m256i mask0 = _mm256_broadcastsi128_si256(
+            _mm_setr_epi8(0, 4, 8, 12, -1, -1, -1, -1,
+                          -1, -1, -1, -1, -1, -1, -1, -1));
+    const __m256i mask1 = _mm256_broadcastsi128_si256(
+            _mm_setr_epi8(1, 5, 9, 13, -1, -1, -1, -1,
+                          -1, -1, -1, -1, -1, -1, -1, -1));
+    const __m256i mask2 = _mm256_broadcastsi128_si256(
+            _mm_setr_epi8(2, 6, 10, 14, -1, -1, -1, -1,
+                          -1, -1, -1, -1, -1, -1, -1, -1));
+    const __m256i mask3 = _mm256_broadcastsi128_si256(
+            _mm_setr_epi8(3, 7, 11, 15, -1, -1, -1, -1,
+                          -1, -1, -1, -1, -1, -1, -1, -1));
+
+    parallel_for_(Range(0, srcmat.rows), [&](const Range& range) {
+        for (int y = range.start; y < range.end; y++)
+        {
+            const uchar* src = srcmat.ptr<uchar>(y);
+            uchar* dst = dstmat.ptr<uchar>(y);
+            __m256i acc0 = accInit, acc1 = accInit, acc2 = accInit, acc3 = accInit;
+            int x = 0;
+            for (; x <= cols - 8; x += 8)
+            {
+                __m256i v = _mm256_loadu_si256((const __m256i*)(src + x * 4));
+                __m256i v0 = _mm256_shuffle_epi8(v, mask0);
+                __m256i v1 = _mm256_shuffle_epi8(v, mask1);
+                __m256i v2 = _mm256_shuffle_epi8(v, mask2);
+                __m256i v3 = _mm256_shuffle_epi8(v, mask3);
+                if (!isMax)
+                {
+                    v0 = _mm256_or_si256(v0, maskInvalid);
+                    v1 = _mm256_or_si256(v1, maskInvalid);
+                    v2 = _mm256_or_si256(v2, maskInvalid);
+                    v3 = _mm256_or_si256(v3, maskInvalid);
+                }
+                acc0 = isMax ? _mm256_max_epu8(acc0, v0) : _mm256_min_epu8(acc0, v0);
+                acc1 = isMax ? _mm256_max_epu8(acc1, v1) : _mm256_min_epu8(acc1, v1);
+                acc2 = isMax ? _mm256_max_epu8(acc2, v2) : _mm256_min_epu8(acc2, v2);
+                acc3 = isMax ? _mm256_max_epu8(acc3, v3) : _mm256_min_epu8(acc3, v3);
+            }
+
+            __m256i accs[4] = {acc0, acc1, acc2, acc3};
+            for (int c = 0; c < 4; c++)
+            {
+                uchar lanes[32];
+                _mm256_storeu_si256((__m256i*)lanes, accs[c]);
+                uchar result = initial;
+                for (int i = 0; i < 32; i++)
+                    result = reduceScalarMinMax<isMax>(result, lanes[i]);
+                for (int i = x; i < cols; i++)
+                    result = reduceScalarMinMax<isMax>(result, src[i * 4 + c]);
+                dst[c] = result;
+            }
+        }
+    });
+    v_cleanup();
 #else
     reduceColMinMax_8uFallback<isMax>(srcmat, dstmat);
 #endif
@@ -781,6 +843,93 @@ static void reduceColMinMax_32fC1(const Mat& srcmat, Mat& dstmat)
 }
 
 template<bool isMax>
+static void reduceColMinMax_32fC4(const Mat& srcmat, Mat& dstmat)
+{
+#if CV_NEON
+    const int cols = srcmat.cols;
+    const float initial = isMax ? std::numeric_limits<float>::lowest() : std::numeric_limits<float>::max();
+    parallel_for_(Range(0, srcmat.rows), [&](const Range& range) {
+        for (int y = range.start; y < range.end; y++)
+        {
+            const float* src = srcmat.ptr<float>(y);
+            float* dst = dstmat.ptr<float>(y);
+            float32x4x4_t acc = {{
+                vdupq_n_f32(initial), vdupq_n_f32(initial),
+                vdupq_n_f32(initial), vdupq_n_f32(initial)
+            }};
+            int x = 0;
+            for (; x <= cols - 4; x += 4)
+            {
+                float32x4x4_t v = vld4q_f32(src + x * 4);
+                for (int c = 0; c < 4; c++)
+                    acc.val[c] = isMax ? vmaxq_f32(acc.val[c], v.val[c])
+                                       : vminq_f32(acc.val[c], v.val[c]);
+            }
+            for (int c = 0; c < 4; c++)
+            {
+                float lanes[4];
+                vst1q_f32(lanes, acc.val[c]);
+                float result = initial;
+                for (int i = 0; i < 4; i++)
+                    result = reduceScalarMinMax<isMax>(result, lanes[i]);
+                for (int i = x; i < cols; i++)
+                    result = reduceScalarMinMax<isMax>(result, src[i * 4 + c]);
+                dst[c] = result;
+            }
+        }
+    });
+    v_cleanup();
+#elif CV_AVX2
+    const int cols = srcmat.cols;
+    const float initial = isMax ? std::numeric_limits<float>::lowest() : std::numeric_limits<float>::max();
+    const __m256 accInit = _mm256_set1_ps(initial);
+    const __m256 validMask = _mm256_castsi256_ps(_mm256_setr_epi32(-1, -1, 0, 0, 0, 0, 0, 0));
+    const __m256i idx0 = _mm256_setr_epi32(0, 4, 0, 0, 0, 0, 0, 0);
+    const __m256i idx1 = _mm256_setr_epi32(1, 5, 0, 0, 0, 0, 0, 0);
+    const __m256i idx2 = _mm256_setr_epi32(2, 6, 0, 0, 0, 0, 0, 0);
+    const __m256i idx3 = _mm256_setr_epi32(3, 7, 0, 0, 0, 0, 0, 0);
+
+    parallel_for_(Range(0, srcmat.rows), [&](const Range& range) {
+        for (int y = range.start; y < range.end; y++)
+        {
+            const float* src = srcmat.ptr<float>(y);
+            float* dst = dstmat.ptr<float>(y);
+            __m256 acc0 = accInit, acc1 = accInit, acc2 = accInit, acc3 = accInit;
+            int x = 0;
+            for (; x <= cols - 2; x += 2)
+            {
+                __m256 v = _mm256_loadu_ps(src + x * 4);
+                __m256 v0 = _mm256_blendv_ps(accInit, _mm256_permutevar8x32_ps(v, idx0), validMask);
+                __m256 v1 = _mm256_blendv_ps(accInit, _mm256_permutevar8x32_ps(v, idx1), validMask);
+                __m256 v2 = _mm256_blendv_ps(accInit, _mm256_permutevar8x32_ps(v, idx2), validMask);
+                __m256 v3 = _mm256_blendv_ps(accInit, _mm256_permutevar8x32_ps(v, idx3), validMask);
+                acc0 = isMax ? _mm256_max_ps(acc0, v0) : _mm256_min_ps(acc0, v0);
+                acc1 = isMax ? _mm256_max_ps(acc1, v1) : _mm256_min_ps(acc1, v1);
+                acc2 = isMax ? _mm256_max_ps(acc2, v2) : _mm256_min_ps(acc2, v2);
+                acc3 = isMax ? _mm256_max_ps(acc3, v3) : _mm256_min_ps(acc3, v3);
+            }
+
+            __m256 accs[4] = {acc0, acc1, acc2, acc3};
+            for (int c = 0; c < 4; c++)
+            {
+                float lanes[8];
+                _mm256_storeu_ps(lanes, accs[c]);
+                float result = initial;
+                for (int i = 0; i < 8; i++)
+                    result = reduceScalarMinMax<isMax>(result, lanes[i]);
+                for (int i = x; i < cols; i++)
+                    result = reduceScalarMinMax<isMax>(result, src[i * 4 + c]);
+                dst[c] = result;
+            }
+        }
+    });
+    v_cleanup();
+#else
+    reduceColMinMax_32fFallback<isMax>(srcmat, dstmat);
+#endif
+}
+
+template<bool isMax>
 static void reduceColMinMax_32f(const Mat& srcmat, Mat& dstmat)
 {
     const int cn = srcmat.channels();
@@ -789,7 +938,7 @@ static void reduceColMinMax_32f(const Mat& srcmat, Mat& dstmat)
     else if (cn == 3)
         reduceColMinMax_32fFallback<isMax>(srcmat, dstmat);
     else if (cn == 4)
-        reduceColMinMax_32fFallback<isMax>(srcmat, dstmat);
+        reduceColMinMax_32fC4<isMax>(srcmat, dstmat);
     else
         reduceColMinMax_32fFallback<isMax>(srcmat, dstmat);
 }
@@ -898,6 +1047,70 @@ static void reduceColSum2_8uC4(const Mat& srcmat, Mat& dstmat)
         }
     });
     v_cleanup();
+#elif CV_AVX2
+    const int cols = srcmat.cols;
+    const __m256i mask0 = _mm256_broadcastsi128_si256(
+            _mm_setr_epi8(0, 4, 8, 12, -1, -1, -1, -1,
+                          -1, -1, -1, -1, -1, -1, -1, -1));
+    const __m256i mask1 = _mm256_broadcastsi128_si256(
+            _mm_setr_epi8(1, 5, 9, 13, -1, -1, -1, -1,
+                          -1, -1, -1, -1, -1, -1, -1, -1));
+    const __m256i mask2 = _mm256_broadcastsi128_si256(
+            _mm_setr_epi8(2, 6, 10, 14, -1, -1, -1, -1,
+                          -1, -1, -1, -1, -1, -1, -1, -1));
+    const __m256i mask3 = _mm256_broadcastsi128_si256(
+            _mm_setr_epi8(3, 7, 11, 15, -1, -1, -1, -1,
+                          -1, -1, -1, -1, -1, -1, -1, -1));
+
+    parallel_for_(Range(0, srcmat.rows), [&](const Range& range) {
+        for (int y = range.start; y < range.end; y++)
+        {
+            const uchar* src = srcmat.ptr<uchar>(y);
+            DT* dst = dstmat.ptr<DT>(y);
+            __m256i acc0 = _mm256_setzero_si256();
+            __m256i acc1 = _mm256_setzero_si256();
+            __m256i acc2 = _mm256_setzero_si256();
+            __m256i acc3 = _mm256_setzero_si256();
+            int x = 0;
+            for (; x <= cols - 8; x += 8)
+            {
+                __m256i v = _mm256_loadu_si256((const __m256i*)(src + x * 4));
+                __m256i channels[4] = {
+                    _mm256_shuffle_epi8(v, mask0),
+                    _mm256_shuffle_epi8(v, mask1),
+                    _mm256_shuffle_epi8(v, mask2),
+                    _mm256_shuffle_epi8(v, mask3)
+                };
+                __m256i* accs[4] = {&acc0, &acc1, &acc2, &acc3};
+                for (int c = 0; c < 4; c++)
+                {
+                    __m128i lo = _mm256_castsi256_si128(channels[c]);
+                    __m128i hi = _mm256_extracti128_si256(channels[c], 1);
+                    __m256i lo16 = _mm256_cvtepu8_epi16(lo);
+                    __m256i hi16 = _mm256_cvtepu8_epi16(hi);
+                    *accs[c] = _mm256_add_epi32(*accs[c], _mm256_madd_epi16(lo16, lo16));
+                    *accs[c] = _mm256_add_epi32(*accs[c], _mm256_madd_epi16(hi16, hi16));
+                }
+            }
+
+            __m256i accs[4] = {acc0, acc1, acc2, acc3};
+            for (int c = 0; c < 4; c++)
+            {
+                uint32_t lanes[8];
+                uint32_t result = 0;
+                _mm256_storeu_si256((__m256i*)lanes, accs[c]);
+                for (int i = 0; i < 8; i++)
+                    result += lanes[i];
+                for (int i = x; i < cols; i++)
+                {
+                    uint32_t value = src[i * 4 + c];
+                    result += value * value;
+                }
+                dst[c] = (DT)(int32_t)result;
+            }
+        }
+    });
+    v_cleanup();
 #else
     reduceColSum2_8uFallback<DT>(srcmat, dstmat);
 #endif
@@ -981,6 +1194,89 @@ static void reduceColSum2_32f32fC1(const Mat& srcmat, Mat& dstmat)
 #endif
 }
 
+static void reduceColSum2_32f32fC4(const Mat& srcmat, Mat& dstmat)
+{
+#if CV_NEON
+    const int cols = srcmat.cols;
+    parallel_for_(Range(0, srcmat.rows), [&](const Range& range) {
+        for (int y = range.start; y < range.end; y++)
+        {
+            const float* src = srcmat.ptr<float>(y);
+            float* dst = dstmat.ptr<float>(y);
+            float32x4x4_t acc = {{
+                vdupq_n_f32(0), vdupq_n_f32(0),
+                vdupq_n_f32(0), vdupq_n_f32(0)
+            }};
+            int x = 0;
+            for (; x <= cols - 4; x += 4)
+            {
+                float32x4x4_t v = vld4q_f32(src + x * 4);
+                for (int c = 0; c < 4; c++)
+                    acc.val[c] = vmlaq_f32(acc.val[c], v.val[c], v.val[c]);
+            }
+            for (int c = 0; c < 4; c++)
+            {
+                float lanes[4];
+                float result = 0;
+                vst1q_f32(lanes, acc.val[c]);
+                for (int i = 0; i < 4; i++)
+                    result += lanes[i];
+                for (int i = x; i < cols; i++)
+                    result += src[i * 4 + c] * src[i * 4 + c];
+                dst[c] = result;
+            }
+        }
+    });
+    v_cleanup();
+#elif CV_AVX2
+    const int cols = srcmat.cols;
+    const __m256 zero = _mm256_setzero_ps();
+    const __m256 validMask = _mm256_castsi256_ps(_mm256_setr_epi32(-1, -1, 0, 0, 0, 0, 0, 0));
+    const __m256i idx0 = _mm256_setr_epi32(0, 4, 0, 0, 0, 0, 0, 0);
+    const __m256i idx1 = _mm256_setr_epi32(1, 5, 0, 0, 0, 0, 0, 0);
+    const __m256i idx2 = _mm256_setr_epi32(2, 6, 0, 0, 0, 0, 0, 0);
+    const __m256i idx3 = _mm256_setr_epi32(3, 7, 0, 0, 0, 0, 0, 0);
+
+    parallel_for_(Range(0, srcmat.rows), [&](const Range& range) {
+        for (int y = range.start; y < range.end; y++)
+        {
+            const float* src = srcmat.ptr<float>(y);
+            float* dst = dstmat.ptr<float>(y);
+            __m256 acc0 = zero, acc1 = zero, acc2 = zero, acc3 = zero;
+            int x = 0;
+            for (; x <= cols - 2; x += 2)
+            {
+                __m256 v = _mm256_loadu_ps(src + x * 4);
+                __m256 v0 = _mm256_blendv_ps(zero, _mm256_permutevar8x32_ps(v, idx0), validMask);
+                __m256 v1 = _mm256_blendv_ps(zero, _mm256_permutevar8x32_ps(v, idx1), validMask);
+                __m256 v2 = _mm256_blendv_ps(zero, _mm256_permutevar8x32_ps(v, idx2), validMask);
+                __m256 v3 = _mm256_blendv_ps(zero, _mm256_permutevar8x32_ps(v, idx3), validMask);
+                acc0 = _mm256_add_ps(acc0, _mm256_mul_ps(v0, v0));
+                acc1 = _mm256_add_ps(acc1, _mm256_mul_ps(v1, v1));
+                acc2 = _mm256_add_ps(acc2, _mm256_mul_ps(v2, v2));
+                acc3 = _mm256_add_ps(acc3, _mm256_mul_ps(v3, v3));
+            }
+
+            __m256 accs[4] = {acc0, acc1, acc2, acc3};
+            for (int c = 0; c < 4; c++)
+            {
+                float lanes[8];
+                float result = 0;
+                _mm256_storeu_ps(lanes, accs[c]);
+                for (int i = 0; i < 8; i++)
+                    result += lanes[i];
+                for (int i = x; i < cols; i++)
+                    result += src[i * 4 + c] * src[i * 4 + c];
+                dst[c] = result;
+            }
+        }
+    });
+    v_cleanup();
+#else
+    reduceColSum2_32f32fFallback(srcmat, dstmat);
+#endif
+}
+
 static void reduceColSum2_32f32f(const Mat& srcmat, Mat& dstmat)
 {
     const int cn = srcmat.channels();
@@ -989,7 +1285,7 @@ static void reduceColSum2_32f32f(const Mat& srcmat, Mat& dstmat)
     else if (cn == 3)
         reduceColSum2_32f32fFallback(srcmat, dstmat);
     else if (cn == 4)
-        reduceColSum2_32f32fFallback(srcmat, dstmat);
+        reduceColSum2_32f32fC4(srcmat, dstmat);
     else
         reduceColSum2_32f32fFallback(srcmat, dstmat);
 }
