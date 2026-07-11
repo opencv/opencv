fbfee192bce416d98daf4dcf4310177c207376e3
Yuantao Feng <yuantao.feng@outlook.com>
2026-07-11 12:15:24 +0800

    core: add reduceC 8UC3 native kernels

--- modules/core/src/reduce_c.simd.hpp
+++ modules/core/src/reduce_c.simd.hpp
@@ -655,6 +655,106 @@ static void reduceColMinMax_8uC1(const Mat& srcmat, Mat& dstmat)
 }
 
 template<bool isMax>
+static void reduceColMinMax_8uC3(const Mat& srcmat, Mat& dstmat)
+{
+#if CV_NEON
+    const int cols = srcmat.cols;
+    parallel_for_(Range(0, srcmat.rows), [&](const Range& range) {
+        for (int y = range.start; y < range.end; y++)
+        {
+            const uchar* src = srcmat.ptr<uchar>(y);
+            uchar* dst = dstmat.ptr<uchar>(y);
+            const uchar initial = isMax ? 0 : UCHAR_MAX;
+            uint8x16x3_t acc = {{
+                vdupq_n_u8(initial), vdupq_n_u8(initial), vdupq_n_u8(initial)
+            }};
+            int x = 0;
+            for (; x <= cols - 16; x += 16)
+            {
+                uint8x16x3_t v = vld3q_u8(src + x * 3);
+                for (int c = 0; c < 3; c++)
+                    acc.val[c] = isMax ? vmaxq_u8(acc.val[c], v.val[c])
+                                       : vminq_u8(acc.val[c], v.val[c]);
+            }
+            for (int c = 0; c < 3; c++)
+            {
+                uchar lanes[16];
+                vst1q_u8(lanes, acc.val[c]);
+                uchar result = initial;
+                for (int i = 0; i < 16; i++)
+                    result = reduceScalarMinMax<isMax>(result, lanes[i]);
+                for (int i = x; i < cols; i++)
+                    result = reduceScalarMinMax<isMax>(result, src[i * 3 + c]);
+                dst[c] = result;
+            }
+        }
+    });
+    v_cleanup();
+#elif CV_AVX2
+    const int cols = srcmat.cols;
+    const uchar initial = isMax ? 0 : UCHAR_MAX;
+    const __m256i accInit = _mm256_set1_epi8((char)initial);
+    const __m256i mask0 = _mm256_setr_epi8(
+            0, 3, 6, 9, 12, 15, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1,
+            2, 5, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1);
+    const __m256i mask1 = _mm256_setr_epi8(
+            1, 4, 7, 10, 13, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1,
+            0, 3, 6, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1);
+    const __m256i mask2 = _mm256_setr_epi8(
+            2, 5, 8, 11, 14, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1,
+            1, 4, 7, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1);
+    const __m256i invalid0 = _mm256_setr_epi8(
+            0, 0, 0, 0, 0, 0, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1,
+            0, 0, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1);
+    const __m256i invalid12 = _mm256_setr_epi8(
+            0, 0, 0, 0, 0, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1,
+            0, 0, 0, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1);
+
+    parallel_for_(Range(0, srcmat.rows), [&](const Range& range) {
+        for (int y = range.start; y < range.end; y++)
+        {
+            const uchar* src = srcmat.ptr<uchar>(y);
+            uchar* dst = dstmat.ptr<uchar>(y);
+            __m256i acc0 = accInit, acc1 = accInit, acc2 = accInit;
+            int x = 0;
+            for (; x <= cols - 11; x += 8)
+            {
+                __m256i v = _mm256_loadu_si256((const __m256i*)(src + x * 3));
+                __m256i v0 = _mm256_shuffle_epi8(v, mask0);
+                __m256i v1 = _mm256_shuffle_epi8(v, mask1);
+                __m256i v2 = _mm256_shuffle_epi8(v, mask2);
+                if (!isMax)
+                {
+                    v0 = _mm256_or_si256(v0, invalid0);
+                    v1 = _mm256_or_si256(v1, invalid12);
+                    v2 = _mm256_or_si256(v2, invalid12);
+                }
+                acc0 = isMax ? _mm256_max_epu8(acc0, v0) : _mm256_min_epu8(acc0, v0);
+                acc1 = isMax ? _mm256_max_epu8(acc1, v1) : _mm256_min_epu8(acc1, v1);
+                acc2 = isMax ? _mm256_max_epu8(acc2, v2) : _mm256_min_epu8(acc2, v2);
+            }
+
+            __m256i accs[3] = {acc0, acc1, acc2};
+            for (int c = 0; c < 3; c++)
+            {
+                uchar lanes[32];
+                _mm256_storeu_si256((__m256i*)lanes, accs[c]);
+                uchar result = initial;
+                for (int i = 0; i < 32; i++)
+                    result = reduceScalarMinMax<isMax>(result, lanes[i]);
+                for (int i = x; i < cols; i++)
+                    result = reduceScalarMinMax<isMax>(result, src[i * 3 + c]);
+                dst[c] = result;
+            }
+        }
+    });
+    v_cleanup();
+#else
+    reduceColMinMax_8uFallback<isMax>(srcmat, dstmat);
+#endif
+}
+
+template<bool isMax>
 static void reduceColMinMax_8uC4(const Mat& srcmat, Mat& dstmat)
 {
 #if CV_NEON
@@ -765,7 +865,7 @@ static void reduceColMinMax_8u(const Mat& srcmat, Mat& dstmat)
     if (cn == 1)
         reduceColMinMax_8uC1<isMax>(srcmat, dstmat);
     else if (cn == 3)
-        reduceColMinMax_8uFallback<isMax>(srcmat, dstmat);
+        reduceColMinMax_8uC3<isMax>(srcmat, dstmat);
     else if (cn == 4)
         reduceColMinMax_8uC4<isMax>(srcmat, dstmat);
     else
@@ -1018,6 +1118,100 @@ static void reduceColSum2_8uC1(const Mat& srcmat, Mat& dstmat)
 }
 
 template<typename DT>
+static void reduceColSum2_8uC3(const Mat& srcmat, Mat& dstmat)
+{
+#if CV_NEON && (defined(__aarch64__) || defined(_M_ARM64))
+    const int cols = srcmat.cols;
+    parallel_for_(Range(0, srcmat.rows), [&](const Range& range) {
+        for (int y = range.start; y < range.end; y++)
+        {
+            const uchar* src = srcmat.ptr<uchar>(y);
+            DT* dst = dstmat.ptr<DT>(y);
+            uint32_t results[3] = {0, 0, 0};
+            int x = 0;
+            for (; x <= cols - 16; x += 16)
+            {
+                uint8x16x3_t v = vld3q_u8(src + x * 3);
+                for (int c = 0; c < 3; c++)
+                    results[c] += reduceSum2_8u_NEON(v.val[c]);
+            }
+            for (int c = 0; c < 3; c++)
+            {
+                for (int i = x; i < cols; i++)
+                {
+                    uint32_t value = src[i * 3 + c];
+                    results[c] += value * value;
+                }
+                dst[c] = (DT)(int32_t)results[c];
+            }
+        }
+    });
+    v_cleanup();
+#elif CV_AVX2
+    const int cols = srcmat.cols;
+    const __m256i mask0 = _mm256_setr_epi8(
+            0, 3, 6, 9, 12, 15, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1,
+            2, 5, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1);
+    const __m256i mask1 = _mm256_setr_epi8(
+            1, 4, 7, 10, 13, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1,
+            0, 3, 6, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1);
+    const __m256i mask2 = _mm256_setr_epi8(
+            2, 5, 8, 11, 14, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1,
+            1, 4, 7, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1);
+
+    parallel_for_(Range(0, srcmat.rows), [&](const Range& range) {
+        for (int y = range.start; y < range.end; y++)
+        {
+            const uchar* src = srcmat.ptr<uchar>(y);
+            DT* dst = dstmat.ptr<DT>(y);
+            __m256i acc0 = _mm256_setzero_si256();
+            __m256i acc1 = _mm256_setzero_si256();
+            __m256i acc2 = _mm256_setzero_si256();
+            int x = 0;
+            for (; x <= cols - 11; x += 8)
+            {
+                __m256i v = _mm256_loadu_si256((const __m256i*)(src + x * 3));
+                __m256i channels[3] = {
+                    _mm256_shuffle_epi8(v, mask0),
+                    _mm256_shuffle_epi8(v, mask1),
+                    _mm256_shuffle_epi8(v, mask2)
+                };
+                __m256i* accs[3] = {&acc0, &acc1, &acc2};
+                for (int c = 0; c < 3; c++)
+                {
+                    __m128i lo = _mm256_castsi256_si128(channels[c]);
+                    __m128i hi = _mm256_extracti128_si256(channels[c], 1);
+                    __m256i lo16 = _mm256_cvtepu8_epi16(lo);
+                    __m256i hi16 = _mm256_cvtepu8_epi16(hi);
+                    *accs[c] = _mm256_add_epi32(*accs[c], _mm256_madd_epi16(lo16, lo16));
+                    *accs[c] = _mm256_add_epi32(*accs[c], _mm256_madd_epi16(hi16, hi16));
+                }
+            }
+
+            __m256i accs[3] = {acc0, acc1, acc2};
+            for (int c = 0; c < 3; c++)
+            {
+                uint32_t lanes[8];
+                uint32_t result = 0;
+                _mm256_storeu_si256((__m256i*)lanes, accs[c]);
+                for (int i = 0; i < 8; i++)
+                    result += lanes[i];
+                for (int i = x; i < cols; i++)
+                {
+                    uint32_t value = src[i * 3 + c];
+                    result += value * value;
+                }
+                dst[c] = (DT)(int32_t)result;
+            }
+        }
+    });
+    v_cleanup();
+#else
+    reduceColSum2_8uFallback<DT>(srcmat, dstmat);
+#endif
+}
+
+template<typename DT>
 static void reduceColSum2_8uC4(const Mat& srcmat, Mat& dstmat)
 {
 #if CV_NEON && (defined(__aarch64__) || defined(_M_ARM64))
@@ -1123,7 +1317,7 @@ static void reduceColSum2_8u(const Mat& srcmat, Mat& dstmat)
     if (cn == 1)
         reduceColSum2_8uC1<DT>(srcmat, dstmat);
     else if (cn == 3)
-        reduceColSum2_8uFallback<DT>(srcmat, dstmat);
+        reduceColSum2_8uC3<DT>(srcmat, dstmat);
     else if (cn == 4)
         reduceColSum2_8uC4<DT>(srcmat, dstmat);
     else
