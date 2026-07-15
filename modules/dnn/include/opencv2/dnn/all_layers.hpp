1e70bdc86300574a213d4210871e56d7eb4df409
Prasadayus <22bcs191@iiitdmj.ac.in>
2026-07-15 12:52:39 +0530

    Add support and test for Yolo models

--- modules/dnn/include/opencv2/dnn/all_layers.hpp
+++ modules/dnn/include/opencv2/dnn/all_layers.hpp
@@ -1486,7 +1486,8 @@ CV__DNN_INLINE_NS_BEGIN
             WHERE,
             BITWISE_AND,
             BITWISE_OR,
-            BITWISE_XOR
+            BITWISE_XOR,
+            NOT_EQUAL
         };
         OPERATION op;
 
