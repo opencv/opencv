a119e9be8655fdc2fd8a362aaeb8d78f3dee4572
Prasoon Bodke <243719055+Prasoon-Bodke@users.noreply.github.com>
2026-08-07 20:16:48 +0530

    samples: fix trailing whitespace

--- samples/python/tutorial_code/imgProc/hough_line_transform/probabilistic_hough_line_transform.py
+++ samples/python/tutorial_code/imgProc/hough_line_transform/probabilistic_hough_line_transform.py
@@ -7,5 +7,5 @@ edges = cv.Canny(gray,50,150,apertureSize = 3)
 lines = cv.HoughLinesP(edges,1,np.pi/180,100,minLineLength=100,maxLineGap=10)
 for x1, y1, x2, y2 in np.asarray(lines).reshape(-1, 4):
     cv.line(img,(x1,y1),(x2,y2),(0,255,0),2)
-    
+
 cv.imwrite('houghlines5.jpg',img)
