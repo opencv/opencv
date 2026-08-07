4229df9096c0c328ecd36729088ae79af031f4fa
Prasoon Bodke <243719055+Prasoon-Bodke@users.noreply.github.com>
2026-08-07 12:07:42 +0530

    samples: fix remaining HoughLines/HoughLinesP snippets for 5.0 shape change

--- samples/python/snippets/houghlines.py
+++ samples/python/snippets/houghlines.py
@@ -29,24 +29,21 @@ def main():
 
     if True: # HoughLinesP
         lines = cv.HoughLinesP(dst, 1, math.pi/180.0, 40, np.array([]), 50, 10)
-        a, b, _c = lines.shape
-        for i in range(a):
-            cv.line(cdst, (lines[i][0][0], lines[i][0][1]), (lines[i][0][2], lines[i][0][3]), (0, 0, 255), 3, cv.LINE_AA)
+        if lines is not None:
+            for x1, y1, x2, y2 in np.asarray(lines).reshape(-1, 4):
+                cv.line(cdst, (x1, y1), (x2, y2), (0, 0, 255), 3, cv.LINE_AA)
 
     else:    # HoughLines
         lines = cv.HoughLines(dst, 1, math.pi/180.0, 50, np.array([]), 0, 0)
         if lines is not None:
-            a, b, _c = lines.shape
-            for i in range(a):
-                rho = lines[i][0][0]
-                theta = lines[i][0][1]
+            for rho, theta in np.asarray(lines).reshape(-1, 2):
                 a = math.cos(theta)
                 b = math.sin(theta)
                 x0, y0 = a*rho, b*rho
                 pt1 = ( int(x0+1000*(-b)), int(y0+1000*(a)) )
                 pt2 = ( int(x0-1000*(-b)), int(y0-1000*(a)) )
                 cv.line(cdst, pt1, pt2, (0, 0, 255), 3, cv.LINE_AA)
-
+                
     cv.imshow("detected lines", cdst)
 
     cv.imshow("source", src)
