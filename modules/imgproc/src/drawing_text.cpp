opencv/opencv

commit c2bf59ca0cf08fcbe3e7c5e1609dc3ee6cfc9eb0

Do not include headers in cv namespace
That creates some conflict on some windows platform with blaze.

modules/imgproc/src/drawing_text.cpp
@@ -42,9 +42,6 @@
 #include <unordered_map>
 #include "zlib.h"
 
-namespace cv
-{
-
 #ifdef HAVE_HARFBUZZ
 
 #include <hb.h>
@@ -58,6 +55,9 @@ namespace cv
 #endif
 
 //////////////////////////////////////////////////////////////////////////////////////
+
+namespace cv
+{
 
 typedef struct BuiltinFontData
 {

commit 2334bd504f2816c08beb3b8806f96a4b6592c004

Fixed compilation error when HAVE_HARFBUZZ == 0
I believe this was regressed by c2bf59ca0cf08fcbe3e7c5e1609dc3ee6cfc9eb0.

modules/imgproc/src/drawing_text.cpp
@@ -1480,7 +1480,12 @@ Rect getTextSize(Size imgsize, const String& str, Point org,
     return bbox;
 }
 
+} // namespace cv
+
 #else // HAVE_HARFBUZZ
+
+namespace cv
+{
 
 // Text rendering is implemented entirely on top of HarfBuzz. When OpenCV is
 // built without it, the public text API is present but throws.
@@ -1510,9 +1515,9 @@ Rect getTextSize(Size, const String&, Point, FontFace&, int, int, PutTextFlags, 
     CV_Error(Error::StsNotImplemented, OPENCV_NO_TEXT_RENDERING_MSG);
 }
 
-#endif // HAVE_HARFBUZZ
+} // namespace cv
 
-}
+#endif // HAVE_HARFBUZZ
 
 //////////////////////////// text drawing functions for backward compatibility ///////////////////////////
 
@@ -1611,4 +1616,4 @@ double getFontScaleFromHeight(const int fontFace, const int pixelHeight, const i
     return (double)pixelHeight/ttsize;
 }
 
-}
+} // namespace cv